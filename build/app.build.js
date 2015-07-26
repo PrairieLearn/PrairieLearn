c9d6b32e3f618a10184d697d15f1620077b7aadc
Matthew West <mwest@illinois.edu>
2015-07-25 21:36:18 -0500

    fix build config to not use app.js config because of undefined 'document'

--- build/app.build.js
+++ build/app.build.js
@@ -1,7 +1,44 @@
 ({
     appDir: "../frontend",
     baseUrl: "require",
-    mainConfigFile: "../frontend/require/app.js",
+    map: {
+        '*': {
+            'backbone': 'browser/backbone',
+            'underscore': 'browser/underscore',
+            'numeric': 'numeric-1.2.6.min'
+        }
+    },
+    shim: {
+        'numeric-1.2.6.min': {
+            exports: 'numeric'
+        },
+        'gamma': {
+            exports: 'module'
+        },
+        'd3': {
+            exports: 'd3'
+        },
+        'bootstrap' : {
+            deps: ['jquery'],
+            exports: 'bootstrap'
+        },
+        'browser/backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'browser/underscore': {
+            exports: '_'
+        },
+        'Tween': {
+            exports: 'TWEEN'
+        },
+        'jquery.cookie': {
+            deps: ['jquery']
+        },
+        'sha1': {
+            exports: 'Sha1',
+        },
+    },
     dir: "../frontend-build",
     inlineText: true,
     logLevel: 0,
@@ -10,7 +47,9 @@
             name: "app",
             include: [
                 "SimpleClient",
-                "SimpleFigure"
+                "SimpleFigure",
+                "QServer",
+                "MCQServer"
             ]
         }
     ]
