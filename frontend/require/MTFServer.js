197fb63f7433fdd4aed2f4fc598d372700db27ad
Terence Nip <terence.nip@gmail.com>
2016-01-15 18:05:04 -0600

    grading for MTF

--- frontend/require/MTFServer.js
+++ frontend/require/MTFServer.js
@@ -30,8 +30,15 @@ define(["underscore", "QServer", "PrairieRandom"], function(_, QServer, PrairieR
         allStatements = _(allStatements).map(function(value, index) {
               return {key: 'q' + index.toString(), statement: value};
         });
+        var perm = rand.shuffle(allStatements);
 
-        console.log(allStatements);
+        var trueAnswer = [];
+
+        // For every index we have, if it's less than the length of true list,
+        // we deem it a true statement. Else, it's false.
+        for(var i = 0; i < perm.length; i++) {
+          trueAnswer.push(perm[i] < options.trueStatements.length);
+        }
 
         var params = {
             statements: allStatements
@@ -39,17 +46,33 @@ define(["underscore", "QServer", "PrairieRandom"], function(_, QServer, PrairieR
 
         var questionData = {
             params: params,
-            trueAnswer: {}
+            trueAnswer: trueAnswer
         };
 
         return questionData;
     }
 
-    /*
-    MTFServer.prototype.transformTrueAnswer = function(vid, params, trueAns) {
-        return {key: trueAns.key};
+    MTFServer.prototype.gradeAnswer = function(vid, params, trueAnswer, submittedAnswer, options) {
+        var finalScore = 0.0;
+        var numberCorrect = 0.0;
+
+        // Assumptions:
+        //  trueAnswer[i]: T, submittedAnswer[qi-true]: T -> Correct
+        //  trueAnswer[i]: F, submittedAnswer[qi-false]: T -> Correct
+        //  Else: Incorrect
+        for (var i = 0; i < trueAnswer.length; i++) {
+          var questionId = 'q' + i.toString();
+
+          if ((trueAnswer[i] && submittedAnswer[questionId + '-true']) ||
+            (!trueAnswer[i] && submittedAnswer[questionId + '-false'])) {
+            numberCorrect++;
+          }
+        }
+
+        finalScore = numberCorrect / (trueAnswer.length * 1.0);
+
+        return { 'score' : finalScore };
     }
-    */
 
     return MTFServer;
 });
