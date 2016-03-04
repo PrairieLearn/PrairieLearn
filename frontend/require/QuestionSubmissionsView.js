76df48703bef0c124a4fbb7815a536c1e1f551d8
rbessick5 <rbessick5@gmail.com>
2016-03-04 12:30:50 -0600

    Past Submissions Rendering 1.0
    Submissions now render; auto re-draw works after the first submission is
    saved (probably on showSubmissions change), but not on subsequent saves
    (pastSubmissions change).

--- frontend/require/QuestionSubmissionsView.js
+++ frontend/require/QuestionSubmissionsView.js
@@ -1,5 +1,5 @@
 
-define(['underscore', 'backbone', 'mustache', 'renderer', 'spinController', 'text!QuestionSubmissionsView.html'], function(_, Backbone, Mustache, renderer, spinController, questionSubmissionsViewTemplate) {
+define(['underscore', 'backbone', 'mustache', 'moment', 'renderer', 'spinController', 'text!QuestionSubmissionsView.html'], function(_, Backbone, Mustache, moment, renderer, spinController, questionSubmissionsViewTemplate) {
 
     var QuestionSubmissionsView = Backbone.View.extend({
 
@@ -11,13 +11,35 @@ define(['underscore', 'backbone', 'mustache', 'renderer', 'spinController', 'tex
         },
 
         render: function() {
-            var data = {
-                showSubmissions: this.model.get("showSubmissions"),
-            };
-            var html = Mustache.render(questionSubmissionsViewTemplate, data);
-            this.$el.html(html);
-            var qClient = this.model.get("qClient");
-			// More to come here
+			that = this;
+			var showSubmissions = this.model.get("showSubmissions");
+			var pastSubmissions = this.model.get("pastSubmissions");
+			var qClient = this.model.get("qClient");
+			if (qClient && pastSubmissions && showSubmissions) {
+				var submissionTemplate = qClient.getSubmissionTemplate();
+				var templateData = {
+					showSubmissions: showSubmissions,
+					pastSubmissions: pastSubmissions.toJSON(),
+				}
+				var templateHTML = Mustache.render(questionSubmissionsViewTemplate, templateData);
+				this.$el.html(templateHTML);
+				
+				// For each submission, render template into div id='#<sid>submissionBody'
+				pastSubmissions.each( function(submission, index, pastSubmissions) {
+					var sid = submission.get("sid");
+					var divID = "#"+sid+"submissionBody";
+					
+					var submissionTemplateData = submission.toJSON();
+					// Draw newest submission first, but call oldest submisssion "Submission 1";
+					submissionTemplateData.index = pastSubmissions.length-index;
+					// Fix the datestamp to look nicer
+					prettyDate = moment(submissionTemplateData.date).format('lll');
+					submissionTemplateData.date = prettyDate;
+					
+					var submissionHTML = Mustache.render(submissionTemplate, submissionTemplateData);
+					that.$(divID).html(submissionHTML);
+				});
+			}
         },
 
         close: function() {
