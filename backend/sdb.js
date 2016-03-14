5c94561c2d6db591f2d80589b2221896e0340416
Matthew West <mwest@illinois.edu>
2016-03-14 08:39:48 -0500

    split models into separate files

--- backend/sdb.js
+++ backend/sdb.js
@@ -2,41 +2,14 @@ var _ = require('underscore');
 var async = require('async');
 var moment = require('moment-timezone');
 var Promise = require('bluebird');
-var pg = require('pg');
-var Sequelize = require('sequelize');
+var models = require('./models');
 
 var config = require('./config');
 var logger = require('./logger');
 
-var sequelize = new Sequelize(config.sdbAddress, {
-    define: {
-        underscored: true,
-        updatedAt: 'updated_at',
-        deletedAt: 'deleted_at',
-    },
-});
-
-var Semester = sequelize.define('semester', {
-    shortName: {type: Sequelize.STRING, unique: true},
-    longName: Sequelize.STRING,
-    startDate: Sequelize.DATE,
-    endDate: Sequelize.DATE,
-});
-
-var Course = sequelize.define('course', {
-    shortName: {type: Sequelize.STRING, unique: true},
-    title: Sequelize.STRING,
-});
-
-var CourseInstance = sequelize.define('course_instance', {
-});
-
-CourseInstance.belongsTo(Course);
-CourseInstance.belongsTo(Semester);
-
 module.exports = {
     init: function(callback) {
-        sequelize.sync().then(function() {
+        models.sequelize.sync().then(function() {
             callback(null);
         }).catch(function(err) {
             callback(err);
@@ -44,20 +17,20 @@ module.exports = {
     },
 
     initSemesters: function(callback) {
-        Semester.upsert({
+        models.Semester.upsert({
             shortName: 'Fa15',
             longName: 'Fall 2015',
             startDate: moment.tz('2015-08-24T00:00:01', config.timezone).format(),
             endDate: moment.tz('2016-12-18T23:59:59', config.timezone).format(),
         }).then(function() {
-            return Semester.upsert({
+            return models.Semester.upsert({
                 shortName: 'Sp16',
                 longName: 'Spring 2016',
                 startDate: moment.tz('2016-01-19T00:00:01', config.timezone).format(),
                 endDate: moment.tz('2016-05-13T23:59:59', config.timezone).format(),
             });
         }).then(function() {
-            return Semester.upsert({
+            return models.Semester.upsert({
                 shortName: 'Su16',
                 longName: 'Summer 2016',
                 startDate: moment.tz('2016-06-13T00:00:01', config.timezone).format(),
@@ -71,11 +44,11 @@ module.exports = {
     },
 
     initCourseInfo: function(courseInfo, callback) {
-        var upsertCourse = Course.upsert({shortName: courseInfo.name, title: courseInfo.title});
-        var course = Course.findOne({where: {shortName: courseInfo.name}});
-        var semester = Semester.findOne({where: {shortName: config.semester}});
+        var upsertCourse = models.Course.upsert({shortName: courseInfo.name, title: courseInfo.title});
+        var course = models.Course.findOne({where: {shortName: courseInfo.name}});
+        var semester = models.Semester.findOne({where: {shortName: config.semester}});
         Promise.join(upsertCourse, course, semester, function(courseCreated, course, semester) {
-            return CourseInstance.findOrCreate({where: {
+            return models.CourseInstance.findOrCreate({where: {
                 course_id: course.id,
                 semester_id: semester.id,
             }, defaults: {}});
