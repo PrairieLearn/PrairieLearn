6fc6564be353a099517006fd5a4cb831e517ca88
Matthew West <mwest@illinois.edu>
2016-03-19 12:39:52 -0500

    use server-side bootstrap.css

--- backend/server.js
+++ backend/server.js
@@ -615,6 +615,7 @@ if (config.authType === 'eppn') {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/', routes);
 app.use('/testsNew', tests);
 
