f3f2b373bd36f42ffd8767816b6bee4dbe98ddcb
Kevin Wang <kevin@kevinwang.com>
2016-03-24 18:49:37 -0400

    Disallow multiple submissions for an Exam/Game qInstance.
    This required refactoring all *TestServer.js files to be CommonJS
    modules instead of AMD modules. In the future, we should limit AMD
    modules to frontend code only, as AMD modules can only require other
    frontend modules and not backend modules, which is severely limiting for
    backend code.
    
    This also modifies `updateWithSubmission()` to be asynchronous.
    
    Closes #307.

--- backend/GameTestServer.js
+++ backend/GameTestServer.js
@@ -1,58 +1,64 @@
+var _ = require("underscore");
+var moment = require("moment-timezone");
+var db = require("./db");
 
-define(["underscore", "moment-timezone"], function(_, moment) {
-
-    var GameTestServer = {};
-
-    GameTestServer.getDefaultOptions = function() {
-        return {
-            allowQuestionSubmit: true,
-        };
+exports.getDefaultOptions = function() {
+    return {
+        allowQuestionSubmit: true,
     };
+};
 
-    GameTestServer.updateTest = function(test, options) {
-        test.qids = _(options.questions).pluck("qid");
-        test.availDate = moment.tz(options.availDate, options.timezone).format();
-        test.qParams = _(options.questions).indexBy('qid');
-        test.maxScore = options.maxScore;
-        test.maxQScoresByQID = _.chain(options.questions)
-            .map(function(q) {return [q.qid, q.maxScore];})
-            .object()
-            .value();
-        test.text = options.text;
-    };
+exports.updateTest = function(test, options) {
+    test.qids = _(options.questions).pluck("qid");
+    test.availDate = moment.tz(options.availDate, options.timezone).format();
+    test.qParams = _(options.questions).indexBy('qid');
+    test.maxScore = options.maxScore;
+    test.maxQScoresByQID = _.chain(options.questions)
+        .map(function(q) {return [q.qid, q.maxScore];})
+        .object()
+        .value();
+    test.text = options.text;
+};
 
-    GameTestServer.updateTInstance = function(tInstance, test, options, questionDB) {
-        if (_(tInstance).has('score') && !_(tInstance).has('scorePerc')) {
-            // upgrade a score to a scorePerc, without applying credit limits
-            tInstance.scorePerc = Math.floor(tInstance.score / test.maxScore * 100);
+exports.updateTInstance = function(tInstance, test, options, questionDB) {
+    if (_(tInstance).has('score') && !_(tInstance).has('scorePerc')) {
+        // upgrade a score to a scorePerc, without applying credit limits
+        tInstance.scorePerc = Math.floor(tInstance.score / test.maxScore * 100);
+    }
+    _(tInstance).defaults({
+        score: 0,
+        scorePerc: 0,
+        qData: {},
+    });
+    _(options.questions).forEach(function(question) {
+        tInstance.qData[question.qid] = tInstance.qData[question.qid] || {};
+        tInstance.qData[question.qid].value = tInstance.qData[question.qid].value || 0;
+        tInstance.qData[question.qid].score = tInstance.qData[question.qid].score || 0;
+
+        tInstance.qData[question.qid].value = Math.max(tInstance.qData[question.qid].value, question.initValue);
+        tInstance.qData[question.qid].score = Math.min(tInstance.qData[question.qid].score, question.maxScore);
+    });
+    tInstance.questions = [];
+    _(test.qids).each(function(qid) {
+        tInstance.questions.push({
+            qid: qid,
+            title: questionDB[qid] && questionDB[qid].title,
+        });
+    });
+    return tInstance;
+};
+
+exports.updateWithSubmission = function(tInstance, test, submission, options, callback) {
+    var qid = submission.qid;
+    if (!_(test.qids).contains(qid)) {
+        return callback(new Error("Invalid QID"));
+    }
+
+    var qiid = submission.qiid;
+    db.sCollect.findOne({qiid: qiid}, function(err, existingSubmission) {
+        if (existingSubmission) {
+            return callback(new Error("Submission already exists for this question"));
         }
-        _(tInstance).defaults({
-            score: 0,
-            scorePerc: 0,
-            qData: {},
-        });
-        _(options.questions).forEach(function(question) {
-            tInstance.qData[question.qid] = tInstance.qData[question.qid] || {};
-            tInstance.qData[question.qid].value = tInstance.qData[question.qid].value || 0;
-            tInstance.qData[question.qid].score = tInstance.qData[question.qid].score || 0;
-
-            tInstance.qData[question.qid].value = Math.max(tInstance.qData[question.qid].value, question.initValue);
-            tInstance.qData[question.qid].score = Math.min(tInstance.qData[question.qid].score, question.maxScore);
-        });
-        tInstance.questions = [];
-        _(test.qids).each(function(qid) {
-            tInstance.questions.push({
-                qid: qid,
-                title: questionDB[qid] && questionDB[qid].title,
-            });
-        });
-        return tInstance;
-    };
-
-    GameTestServer.updateWithSubmission = function(tInstance, test, submission, options) {
-        var qid = submission.qid;
-        if (!_(test.qids).contains(qid))
-            throw Error("Invalid QID");
 
         if (_(test.credit).isFinite() && test.credit > 0) {
             var correct = (submission.score >= 0.5);
@@ -81,7 +87,8 @@ define(["underscore", "moment-timezone"], function(_, moment) {
             }
             tInstance.scorePerc = Math.max(tInstance.scorePerc, newScorePerc);
         }
-    };
 
-    return GameTestServer;
-});
+        callback(null);
+    });
+
+};
