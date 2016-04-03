3b88f85c59bd61ca1bd8dc18fc08e5cdbbba7077
Matthew West <mwest@illinois.edu>
2016-04-03 11:45:50 -0500

    add sync of question instances to SQL

--- backend/server.js
+++ backend/server.js
@@ -3454,6 +3454,7 @@ var syncDiskToSQL = function(callback) {
 
 var syncUsers = require('./sync/fromMongo/users');
 var syncTestInstances = require('./sync/fromMongo/testInstances');
+var syncQuestionInstances = require('./sync/fromMongo/questionInstances');
 
 var syncMongoToSQL = function(callback) {
     logger.infoOverride("Starting sync of Mongo to SQL");
@@ -3462,6 +3463,8 @@ var syncMongoToSQL = function(callback) {
         syncUsers.sync.bind(null, courseDB.courseInfo, uidToRole),
         function(callback) {logger.infoOverride("Syncing test instances from Mongo to SQL DB"); callback(null);},
         syncTestInstances.sync.bind(null, courseDB.courseInfo, courseDB.testDB),
+        function(callback) {logger.infoOverride("Syncing question instances from Mongo to SQL DB"); callback(null);},
+        syncQuestionInstances.sync.bind(null, courseDB.courseInfo, courseDB.testDB, courseDB.questionDB),
     ], function(err) {
         if (err) return callback(err);
         logger.infoOverride("Completed sync of Mongo to SQL");
