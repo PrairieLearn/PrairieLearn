b7120109a1c649c444326993fb229562f25ae289
Matthew West <mwest@illinois.edu>
2016-04-10 12:34:27 -0500

    split out testStats as a view

--- backend/sqldb.js
+++ backend/sqldb.js
@@ -1,13 +1,14 @@
 var Promise = require('bluebird');
 var models = require('./models');
 var histogram = require('./sprocs/histogram');
+var testStats = require('./sprocs/testStats');
 
 module.exports = {
     init: function(callback) {
         Promise.try(function() {
             return models.sequelize.sync();
         }).then(function() {
-            return models.sequelize.query(histogram.sql);
+            return models.sequelize.query(testStats.sql);
         }).then(function() {
             callback(null);
         }).catch(function(err) {
