cf503ab996050009f10638c223b7332d657e9f3f
Matthew West <mwest@illinois.edu>
2016-07-15 09:52:36 -0500

    fix navbar course list

--- backend/middlewares/courseList.sql
+++ backend/middlewares/courseList.sql
@@ -1,16 +1,19 @@
-WITH q AS (
-SELECT c.id AS course_id,c.short_name
-FROM enrollments AS e
-    JOIN users AS u ON (e.user_id = u.id)
-    JOIN course_instances AS ci ON (e.course_instance_id = ci.id)
-    JOIN courses AS c ON (ci.course_id = c.id)
-WHERE uid = $1
+WITH courses_with_instances
+AS (
+SELECT
+    c.*, ci.id AS course_instance_id,
+    row_number() OVER (PARTITION BY c.id ORDER BY ci.number DESC)
+-- for each course, order by course_instances.number and number the rows
+FROM
+    enrollments AS e
+    JOIN users AS u ON (u.id = e.user_id)
+    JOIN course_instances AS ci ON (ci.id = e.course_instance_id)
+    JOIN courses AS c ON (c.id = ci.course_id)
+WHERE
+    u.uid = $1
     AND role >= 'TA'
     AND ci.deleted_at IS NULL
-GROUP BY c.id
-)
-SELECT q.short_name,ci.id AS course_instance_id
-FROM q
-    JOIN course_instances AS ci ON (ci.course_id = q.course_id)
-WHERE ci.deleted_at IS NULL
-ORDER BY q.short_name;
+    )
+SELECT * FROM courses_with_instances
+WHERE row_number = 1;
+-- only keep one row per course, which will be the one with the biggest course_instance.number
