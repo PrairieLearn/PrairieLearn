18d98238d326ab5d2aaa8d7690e931bd6db7961a
Matthew West <mwest@illinois.edu>
2016-09-01 00:13:34 -0500

    fix foreign key constraint for variants.instance_question_id

--- backend/models/variants.sql
+++ backend/models/variants.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS variants (
     id SERIAL PRIMARY KEY,
     qiid varchar(255) UNIQUE, -- temporary, delete after Mongo import
     date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    instance_question_id INTEGER NOT NULL REFERENCES assessment_questions ON DELETE CASCADE ON UPDATE CASCADE,
+    instance_question_id INTEGER NOT NULL REFERENCES instance_questions ON DELETE CASCADE ON UPDATE CASCADE,
     available BOOLEAN DEFAULT TRUE,
     number INTEGER,
     variant_seed varchar(255),
