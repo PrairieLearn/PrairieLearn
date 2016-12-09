623522232e023c6ff50e4c486be6f118e52c29e5
Matthew West <mwest@illinois.edu>
2016-12-09 08:35:26 -0600

    add final exam IP ranges

--- v2/middlewares/mode.js
+++ v2/middlewares/mode.js
@@ -19,15 +19,23 @@ module.exports = function(req, res, next) {
                 if (n1 == 192 && n2 == 17 && n3 == 180 && n4 >= 128 && n4 <= 255) {
                     serverMode = 'Exam';
                 }
-                if (moment.tz("2016-05-06T00:00:01", config.timezone).isBefore()
-                    && moment.tz("2016-05-13T23:59:59", config.timezone).isAfter()) {
+                if (moment.tz("2016-12-10T00:00:01", config.timezone).isBefore()
+                    && moment.tz("2016-12-16T23:59:59", config.timezone).isAfter()) {
+                    // DCL L416
+                    if (n1 == 130 && n2 == 126 && n3 == 246 && n4 >= 150 && n4 <= 190) {
+                        serverMode = 'Exam';
+                    }
+                    // DCL L422
+                    if (n1 == 130 && n2 == 126 && n3 == 246 && n4 >= 191 && n4 <= 194) {
+                        serverMode = 'Exam';
+                    }
                     // DCL L520
-                    if (n1 == 130 && n2 == 126 && n3 == 246 && n4 >= 36 && n4 <= 76) {
+                    if (n1 == 130 && n2 == 126 && n3 == 246 && n4 >= 36 && n4 <= 65) {
                         serverMode = 'Exam';
                     }
                 }
-                if (moment.tz("2016-05-09T00:00:01", config.timezone).isBefore()
-                    && moment.tz("2016-05-13T23:59:59", config.timezone).isAfter()) {
+                if (moment.tz("2016-12-13T00:00:01", config.timezone).isBefore()
+                    && moment.tz("2016-12-16T23:59:59", config.timezone).isAfter()) {
                     // DCL L440
                     if (n1 == 130 && n2 == 126 && n3 == 246 && n4 == 144) {
                         serverMode = 'Exam';
