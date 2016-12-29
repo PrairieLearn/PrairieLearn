ccd4fa98368a60b95a83807bd4152565ab854b58
Matthew West <mwest@illinois.edu>
2016-12-28 19:11:23 -0600

    fix pages/questionText for new questionFilePath() from 9dec9fcf

--- pages/questionText/questionText.js
+++ pages/questionText/questionText.js
@@ -11,11 +11,10 @@ var filePaths = require('../../lib/file-paths');
 router.get('/:filename', function(req, res, next) {
     var question = res.locals.question;
     var course = res.locals.course;
-    var filename = req.params.filename;
-    filePaths.questionPath(question.directory, course.path, function(err, questionPath) {
+    var filename = 'text/' + req.params.filename;
+    filePaths.questionFilePath(filename, question.directory, course.path, question, function(err, fullPath, effectiveFilename, rootPath) {
         if (ERR(err, next)) return;
-        var rootPath = path.join(questionPath, "text");
-        res.sendFile(filename, {root: rootPath});
+        res.sendFile(effectiveFilename, {root: rootPath});
     });
 });
 
