PrairieLearn/PrairieLearn

commit 9dec9fcfc16d33da326f02f24294ddc03cb10e5c

re-activate question templating and default calculation questions; close #371, close #363, close #450

question-servers/calculation.js
@@ -27,17 +27,19 @@ module.exports = {
     },
 
     getData: function(question, course, vid, callback) {
+        var questionDir = path.join(course.path, 'questions', question.directory);
         questionHelper.loadServer(question, course, function(err, server) {
             if (ERR(err, callback)) return;
+            var options = question.options || {};
             try {
-                var questionData = server.getData(vid, question.options, 'INVALID QUESTION DIRECTORY');
+                var questionData = server.getData(vid, options, questionDir);
             } catch (e) {
-                return callback(new Error('Error in question getData(): ' + String(e)));
+                return ERR(e, callback);
             }
             var data = {
                 params: questionData.params,
                 true_answer: questionData.trueAnswer,
-                options: questionData.options,
+                options: options,
             };
             callback(null, data);
         });

lib/config.js
@@ -26,7 +26,7 @@ config.serverPort = '3000';
 config.cronIntervalMS = 10 * 60 * 1000;
 config.courseDir = "../exampleCourse";
 config.frontendDir = "../frontend";
-config.questionDefaultsDir = "questionDefaults";
+config.questionDefaultsDir = "question-servers/default-calculation";
 config.polyfillGitShow = false;
 config.secretKey = "THIS_IS_THE_SECRET_KEY"; // override in config.json
 config.skipUIDs = {};

lib/file-paths.js
@@ -1,30 +1,41 @@
+var ERR = require('async-stacktrace');
 var _ = require('lodash');
 var fs = require('fs');
 var path = require('path');
 
+var error = require('./error');
 var config = require("./config");
 var logger = require('./logger');
 var courseDB = require("./course-db");
 
 module.exports = {};
 
-module.exports.questionFilePath = function(qid, filename, callback, nTemplates) {
+/* returns (fullPath, effectiveFilename, rootPath)
+
+   fullPath is the full path (including filename) of the file to load
+
+   (effectiveFilename, rootPath) are equivalent data broken out so that
+       fullPath = effectiveFilename + '/' + rootPath
+   These can by used like:
+       res.sendFile(effectiveFilename, {root: rootPath});
+   for safety.
+*/
+module.exports.questionFilePath = function(filename, questionDirectory, coursePath, question, callback, nTemplates) {
     nTemplates = (nTemplates === undefined) ? 0 : nTemplates;
     if (nTemplates > 10) {
-        return callback("Too-long template recursion for qid: " + qid);
+        return callback(new Error("Too-long template recursion for qid: " + qid));
     }
-    var info = courseDB.questionDB[qid];
-    if (info === undefined) {
-        return callback("QID not found in questionDB: " + qid);
-    }
-    var questionPath = path.join(config.questionsDir, qid);
-    var fullFilePath = path.join(questionPath, filename);
-    fs.stat(fullFilePath, function(err, stats) {
+    var rootPath = path.join(coursePath, "questions", questionDirectory);
+    var fullPath = path.join(rootPath, filename);
+    fs.stat(fullPath, function(err, stats) {
         if (err) {
             // couldn't find the file
-            if (info.template !== undefined) {
+            if (question.template_directory) {
                 // have a template, try it
-                return module.exports.questionFilePath(info.template, filename, callback, nTemplates + 1);
+                return module.exports.questionFilePath(filename, question.template_directory, coursePath, question, function(err, fullPath, effectiveFilename, rootPath) {
+                    if (ERR(err, callback)) return;
+                    callback(null, fullPath, effectiveFilename, rootPath);
+                }, nTemplates + 1);
             } else {
                 // no template, try default files
                 var filenameToSuffix = {
@@ -32,32 +43,22 @@ module.exports.questionFilePath = function(qid, filename, callback, nTemplates) 
                     "server.js": 'Server.js',
                 };
                 if (filenameToSuffix[filename] === undefined) {
-                    return callback("file not found: " + fullFilePath);
+                    return callback(new Error("file not found: " + fullPath));
                 }
-                var defaultFilename = info.type + filenameToSuffix[filename];
+                var defaultFilename = question.type + filenameToSuffix[filename];
                 var fullDefaultFilePath = path.join(config.questionDefaultsDir, defaultFilename);
                 fs.stat(fullDefaultFilePath, function(err, stats) {
                     if (err) {
                         // no default file, give up
-                        return callback("file not found: " + fullFilePath);
+                        return callback(error.makeWithData("file not found", {fullPath, fullDefaultFilePath}));
                     }
                     // found a default file
-                    return callback(null, {filePath: defaultFilename, qid: qid, filename: filename, root: config.questionDefaultsDir});
+                    return callback(null, fullDefaultFilePath, defaultFilename, config.questionDefaultsDir);
                 });
             }
         } else {
             // found the file
-            return callback(null, {filePath: filename, qid: qid, filename: filename, root: questionPath});
+            return callback(null, fullPath, filename, rootPath);
         }
     });
-};
-
-module.exports.questionPath = function(questionDirectory, coursePath, callback) {
-    var fullPath = path.join(coursePath, "questions", questionDirectory);
-    callback(null, fullPath);
-};
-
-module.exports.questionFilePathNEW = function(filename, questionDirectory, coursePath, callback) {
-    var fullPath = path.join(coursePath, "questions", questionDirectory, filename);
-    callback(null, fullPath);
 };

lib/questionHelper.js
@@ -11,7 +11,7 @@ var requireFrontend = require("./require-frontend");
 
 module.exports = {
     loadServer: function(question, course, callback) {
-        filePaths.questionFilePathNEW("server.js", question.directory, course.path, function(err, questionServerPath) {
+        filePaths.questionFilePath("server.js", question.directory, course.path, question, function(err, questionServerPath) {
             if (ERR(err, callback)) return;
             requireFrontend([questionServerPath], function(server) {
                 if (server === undefined) return callback("Unable to load 'server.js' for qid: " + question.qid);
@@ -29,7 +29,7 @@ module.exports = {
 
     renderRivetsTemplate: function(filename, context, question, course, locals, callback) {
         var that = this;
-        filePaths.questionFilePathNEW(filename, question.directory, course.path, function(err, templatePath) {
+        filePaths.questionFilePath(filename, question.directory, course.path, question, function(err, templatePath) {
             if (ERR(err, callback)) return;
             fs.readFile(templatePath, 'utf8', function(err, template) {
                 if (ERR(err, callback)) return;

models/questions.sql
@@ -3,9 +3,11 @@ CREATE TABLE IF NOT EXISTS questions (
     uuid UUID NOT NULL UNIQUE,
     qid text,
     directory text,
+    template_directory text,
     type enum_question_type,
     title text,
     config JSONB,
+    options JSONB,
     client_files TEXT[] DEFAULT ARRAY[]::TEXT[],
     number INTEGER,
     grading_method enum_grading_method NOT NULL DEFAULT 'Internal',
@@ -19,6 +21,10 @@ CREATE TABLE IF NOT EXISTS questions (
 ALTER TABLE questions ADD COLUMN IF NOT EXISTS uuid UUID UNIQUE;
 
 ALTER TABLE questions DROP CONSTRAINT IF EXISTS questions_qid_course_id_key;
+
+ALTER TABLE questions ADD COLUMN IF NOT EXISTS template_directory text;
+ALTER TABLE questions ADD COLUMN IF NOT EXISTS options jsonb;
+ALTER TABLE questions DROP COLUMN IF EXISTS config;
 
 ALTER TABLE questions ALTER COLUMN id SET DATA TYPE BIGINT;
 ALTER TABLE questions ALTER COLUMN course_id SET DATA TYPE BIGINT;

pages/instructorQuestion/instructorQuestion.ejs
@@ -10,7 +10,7 @@
     <script type="text/javascript" src="/javascripts/lodash.min.js"></script>
     <script type="text/javascript" src="/localscripts/question.js"></script>
     <%- extraHeaders %>
-    <script type="text/javascript" src="/localscripts/question<%= question.type %>.js"></script>
+    <script type="text/javascript" src="/localscripts/question<%= effectiveQuestionType %>.js"></script>
     <div id="content" class="container">
       <div class="panel panel-primary">
         <div class="panel-heading">

pages/instructorQuestion/instructorQuestion.js
@@ -60,32 +60,37 @@ var render = function(req, res, next, variant, submission, submissionHtml, answe
     var params = [res.locals.question.id, res.locals.course_instance.id];
     sqldb.queryOneRow(sql.select_question, params, function(err, result) {
         if (ERR(err, next)) return;
-        questionServers.getModule(res.locals.question.type, function(err, questionModule) {
+        questionServers.getEffectiveQuestionType(res.locals.question.type, function(err, effectiveQuestionType) {
             if (ERR(err, next)) return;
-            questionModule.renderExtraHeaders(res.locals.question, res.locals.course, res.locals, function(err, extraHeaders) {
+            questionServers.getModule(res.locals.question.type, function(err, questionModule) {
                 if (ERR(err, next)) return;
-                questionModule.renderQuestion(variant, res.locals.question, null, res.locals.course, res.locals, function(err, questionHtml) {
+                questionModule.renderExtraHeaders(res.locals.question, res.locals.course, res.locals, function(err, extraHeaders) {
                     if (ERR(err, next)) return;
+                    questionModule.renderQuestion(variant, res.locals.question, null, res.locals.course, res.locals, function(err, questionHtml) {
+                        if (ERR(err, next)) return;
 
-                    res.locals.result = result.rows[0];
-                    res.locals.submission = submission;
-                    res.locals.extraHeaders = extraHeaders;
-                    res.locals.questionHtml = questionHtml;
-                    res.locals.submissionHtml = submissionHtml;
-                    res.locals.answerHtml = answerHtml;
-                    res.locals.postUrl = res.locals.urlPrefix + "/instructor/question/" + res.locals.question.id + "/";
-                    res.locals.questionJson = JSON.stringify({
-                        questionFilePath: res.locals.urlPrefix + "/instructor/question/" + res.locals.question.id + "/file",
-                        question: res.locals.question,
-                        course: res.locals.course,
-                        courseInstance: res.locals.course_instance,
-                        variant: variant,
-                        submittedAnswer: submission ? submission.submitted_answer : null,
-                        feedback: submission ? submission.feedback : null,
-                        trueAnswer: variant.true_answer,
-                        submissions: submission ? res.locals.submissions : null,
+                        res.locals.result = result.rows[0];
+                        res.locals.effectiveQuestionType = effectiveQuestionType,
+                        res.locals.submission = submission;
+                        res.locals.extraHeaders = extraHeaders;
+                        res.locals.questionHtml = questionHtml;
+                        res.locals.submissionHtml = submissionHtml;
+                        res.locals.answerHtml = answerHtml;
+                        res.locals.postUrl = res.locals.urlPrefix + "/instructor/question/" + res.locals.question.id + "/";
+                        res.locals.questionJson = JSON.stringify({
+                            questionFilePath: res.locals.urlPrefix + "/instructor/question/" + res.locals.question.id + "/file",
+                            question: res.locals.question,
+                            effectiveQuestionType: effectiveQuestionType,
+                            course: res.locals.course,
+                            courseInstance: res.locals.course_instance,
+                            variant: variant,
+                            submittedAnswer: submission ? submission.submitted_answer : null,
+                            feedback: submission ? submission.feedback : null,
+                            trueAnswer: variant.true_answer,
+                            submissions: submission ? res.locals.submissions : null,
+                        });
+                        res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
                     });
-                    res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
                 });
             });
         });
@@ -99,20 +104,19 @@ router.get('/file/:filename', function(req, res, next) {
     var question = res.locals.question;
     var course = res.locals.course;
     var filename = req.params.filename;
-    filePaths.questionPath(question.directory, course.path, function(err, questionPath) {
+    filePaths.questionFilePath(filename, question.directory, course.path, question, function(err, fullPath, effectiveFilename, rootPath) {
         if (ERR(err, next)) return;
-        res.sendFile(filename, {root: questionPath});
+        res.sendFile(effectiveFilename, {root: rootPath});
     });
 });
 
 router.get('/text/:filename', function(req, res, next) {
     var question = res.locals.question;
     var course = res.locals.course;
-    var filename = req.params.filename;
-    filePaths.questionPath(question.directory, course.path, function(err, questionPath) {
+    var filename = 'text/' + req.params.filename;
+    filePaths.questionFilePath(filename, question.directory, course.path, question, function(err, fullPath, effectiveFilename, rootPath) {
         if (ERR(err, next)) return;
-        var rootPath = path.join(questionPath, "text");
-        res.sendFile(filename, {root: rootPath});
+        res.sendFile(effectiveFilename, {root: rootPath});
     });
 });
 

pages/questionFile/questionFile.js
@@ -25,9 +25,9 @@ router.get('/:filename', function(req, res, next) {
         if (ERR(err, next)) return;
         if (!result.rows[0].access_allowed) return next(error.make(403, 'Access denied', {locals: res.locals, filename: filename}));
 
-        filePaths.questionPath(question.directory, course.path, function(err, questionPath) {
+        filePaths.questionFilePath(filename, question.directory, course.path, question, function(err, fullPath, effectiveFilename, rootPath) {
             if (ERR(err, next)) return;
-            res.sendFile(filename, {root: questionPath});
+            res.sendFile(effectiveFilename, {root: rootPath});
         });
     });
 });

commit 62a95473a9235a80385dd4901918aef94080e91c

fix broken variant.options introduced in 9dec9fcf

question-servers/calculation.js
@@ -39,7 +39,7 @@ module.exports = {
             var data = {
                 params: questionData.params,
                 true_answer: questionData.trueAnswer,
-                options: options,
+                options: questionData.options,
             };
             callback(null, data);
         });