aa425fe5648914a1f237c5f380b1a8047794eb5e
Matthew West <mwest@illinois.edu>
2017-01-13 09:46:43 -0600

    fix NOT NULL constraint on assessment_questions.alternative_group_id

--- models/assessment_questions.sql
+++ models/assessment_questions.sql
@@ -5,9 +5,11 @@ CREATE TABLE IF NOT EXISTS assessment_questions (
     points_list DOUBLE PRECISION[],
     init_points DOUBLE PRECISION,
     assessment_id BIGINT NOT NULL REFERENCES assessments ON DELETE CASCADE ON UPDATE CASCADE,
-    alternative_group_id BIGINT NOT NULL REFERENCES alternative_groups ON DELETE SET NULL ON UPDATE CASCADE,
+    alternative_group_id BIGINT REFERENCES alternative_groups ON DELETE SET NULL ON UPDATE CASCADE,
     number_in_alternative_group INTEGER,
     question_id BIGINT NOT NULL REFERENCES questions ON DELETE CASCADE ON UPDATE CASCADE,
     deleted_at TIMESTAMP WITH TIME ZONE,
     UNIQUE (question_id, assessment_id)
 );
+
+ALTER TABLE assessment_questions ALTER COLUMN alternative_group_id DROP NOT NULL;
