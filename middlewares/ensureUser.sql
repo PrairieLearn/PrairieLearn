61a9ccf9d2fcf789e666ae3317c1865f24d81116
Matthew West <mwest@illinois.edu>
2017-01-15 11:55:11 -0600

    remove unused middlewares/ensure*

--- middlewares/ensureUser.sql
+++
@@ -1,9 +0,0 @@
--- BLOCK get
-SELECT * FROM users WHERE uid = $uid;
-
--- BLOCK set
-INSERT INTO users (uid, name)
-VALUES ($uid, $name)
-ON CONFLICT (uid) DO UPDATE
-SET name = EXCLUDED.name
-RETURNING *;
