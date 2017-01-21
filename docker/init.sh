c46542b3322a1784b0a54eb4410af62f4d24e3a1
Matthew West <mwest@illinois.edu>
2017-01-21 01:11:41 -0600

    wait for postgres to be ready inside docker container

--- docker/init.sh
+++ docker/init.sh
@@ -2,5 +2,8 @@
 
 su postgres -c '/usr/pgsql-9.6/bin/pg_ctl -D /var/postgres -l /var/postgres/pg_log/logfile start'
 
+# wait for postgres to start
+until /usr/pgsql-9.6/bin/pg_isready -q ; do sleep 1 ; done
+
 cd /PrairieLearn
 node server.js
