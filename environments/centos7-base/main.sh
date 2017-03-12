6fa0cc978201ea29cb622a21b1a074f24548fd26
Nathan Walters <nwalters512@gmail.com>
2017-03-12 17:44:30 -0500

    Made main.sh look in several places for init.sh

--- environments/centos7-base/main.sh
+++ environments/centos7-base/main.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # NOTE: ${0##*/} expands to the name of this script file
-#
 
 echo "[${0##*/}] started"
 
@@ -40,13 +39,33 @@ fi
 # Unzip the downloaded archive
 tar -xf /job.tar.gz -C /grade/
 
-if [ -f /grade/init.sh ]; then
-  sh /grade/init.sh
+# Users can specify init.sh scripts in several locations:
+# 1. in a question's /tests directory (ends up in /grade/tests/)
+# 2. in an autograder (ends up in /grade/shared/)
+# 3. in an environment (ends up in /grade/)
+# We'll only ever run one script; people writing init.sh scripts can choose
+# to run others from the one that we call. Which one we run is determined by the
+# above ordering: if we find /grade/tests/init.sh, we'll run that, otherwise
+# if we find /grade/shared/init.sh, we'll run that, and so on.
+
+if [ -f /grade/tests/init.sh ]; then
+  echo "[${0##*/}] running /grade/tests/init.sh"
+  chmod +x /grade/tests/init.sh
+  /grade/tests/init.sh
+elif [ -f /grade/shared/init.sh ]; then
+  echo "[${0##*/}] running /grade/shared/init.sh"
+  chmod +x /grade/shared/init.sh
+  /grade/shared/init.sh
+elif [ -f /grade/init.sh ]; then
+  echo "[${0##*/}] running /grade/init.sh"
+  chmod +x /grade/init.sh
+  /grade/init.sh
 else
-  echo "[${0##*/}] /grade/init.sh not specified; skipping"
+  echo "[${0##*/}] init.sh not found in /grade/tests/, /grade/shared/, or /grade/; skipping"
 fi
 
 if [ -f /grade/run.sh ]; then
+  chmod +x /grade/run.sh
   sh /grade/run.sh
 else
   echo "[${0##*/}] ERR: /grade/run.sh missing"
