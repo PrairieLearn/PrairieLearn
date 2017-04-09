12522bd9b929b46235f14b38da99b9e72fd228fd
Nathan Walters <nwalters512@gmail.com>
2017-04-09 16:23:14 -0500

    Don't use schema to validate grading webhook callback

--- webhooks/grading/grading.js
+++ webhooks/grading/grading.js
@@ -6,7 +6,6 @@ var express = require('express');
 var router = express.Router();
 var AWS = require('aws-sdk');
 
-var validate = require('../../lib/validator').validateFromFile;
 var config = require('../../lib/config');
 var logger = require('../../lib/logger');
 var filePaths = require('../../lib/file-paths');
@@ -26,37 +25,36 @@ function processResults(data) {
 
 router.post('/', function(req, res, next) {
 
+    const data = req.body;
+
     if (req.body.event === 'grading_result') {
-        validate(req.body, 'schemas/webhookAutograderResult.json', (err, data) => {
-            if (ERR(err, next)) return;
+        if (data.job_id === undefined || data.job_id === null || !Number.isInteger(data.job_id)) {
+            return next(new Error('Grading result does not contain a valid grading job id.'));
+        }
 
-            // TODO actually process the results
-            logger.info(JSON.stringify(data, null, 4));
+        // It's possible that the results data was specified in the body;
+        // if that's the case, we can process it directly. Otherwise, we
+        // have to download it from S3 first.
 
-            // It's possible that the results data was specified in the body;
-            // if that's the case, we can process it directly. Otherwise, we
-            // have to download it from S3 first.
+        if (data.data) {
+            // We have the data!
+            processResults(data.data)
 
-            if (data.data) {
-                // We have the data!
-                processResults(data.data)
-
-            } else {
-                // We should fetch it from S3, and then process it
-                const params = {
-                    Bucket: config.externalGraderResultsS3Bucket,
-                    Key: `job_${data.job_id}.json`,
-                    ResponseContentType: 'application/json',
-                }
-                new AWS.S3().getObject(params, (err, data) =>{
-                    if (ERR(err, next)) return;
-                    processResults(JSON.parse(data.Body))
-                });
+        } else {
+            // We should fetch it from S3, and then process it
+            const params = {
+                Bucket: config.externalGraderResultsS3Bucket,
+                Key: `job_${data.job_id}.json`,
+                ResponseContentType: 'application/json',
             }
+            new AWS.S3().getObject(params, (err, data) => {
+                if (ERR(err, next)) return;
+                processResults(JSON.parse(data.Body))
+            });
+        }
 
-            res.status(200);
-            res.send();
-        })
+        res.status(200);
+        res.send();
     } else {
         return next(new Error('Unknown event'));
     }
