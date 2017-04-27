074d40419b8e5502c30072a0ff66c08c6d4d9319
Nathan Walters <nwalters512@gmail.com>
2017-04-27 01:45:53 -0500

    Fixed bug where actual start_time was not being used

--- webhooks/grading/grading.sql
+++ webhooks/grading/grading.sql
@@ -1,6 +1,6 @@
 -- BLOCK update_grading_start_time
 UPDATE grading_logs AS gl
 SET
-    grading_started_at = CURRENT_TIMESTAMP
+    grading_started_at = $start_time
 WHERE
     gl.id = $grading_log_id
