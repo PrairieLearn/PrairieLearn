c426f8a13db1e9ec7f4e2a7115edff5823ff7417
Nathan Walters <nwalters512@gmail.com>
2017-05-01 12:01:05 -0500

    Added column verification for databases

--- tests/testDatabase.sql
+++ tests/testDatabase.sql
@@ -4,3 +4,8 @@ WHERE
     table_type = 'BASE TABLE'
 AND
     table_schema NOT IN ('pg_catalog', 'information_schema');
+
+-- BLOCK get_columns
+SELECT * FROM information_schema.columns
+WHERE
+    table_name = $table_name;
