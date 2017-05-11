067fd8626d7c29f517a3336057f6dcf1ce59483d
Nathan Walters <nwalters512@gmail.com>
2017-05-11 14:13:59 -0500

    Added ability to ignore enums and tables while diffing

--- tests/databaseDiff.js
+++ tests/databaseDiff.js
@@ -45,6 +45,14 @@ function diff(db1, db2, options, callback) {
             });
         },
         (callback) => {
+            // Remove ignored tables and enums
+            description1.tables = _.pickBy(description1.tables, (value, key) => options.ignoreTables.indexOf(key) == -1);
+            description2.tables = _.pickBy(description2.tables, (value, key) => options.ignoreTables.indexOf(key) == -1);
+            description1.tables = _.pickBy(description1.tables, (value, key) => options.ignoreEnums.indexOf(key) == -1);
+            description2.tables = _.pickBy(description2.tables, (value, key) => options.ignoreEnums.indexOf(key) == -1);
+            callback(null);
+        },
+        (callback) => {
             // Determine if both databases have the same tables
             let missingFrom1 = _.difference(_.keys(description2.tables), _.keys(description1.tables));
             let missingFrom2 = _.difference(_.keys(description1.tables), _.keys(description2.tables));
