74afa42c53735bd7466e4798acd516366eababa5
Timothy Bretl <tbretl@illinois.edu>
2017-07-23 14:47:23 -0500

    WIP: more updates

--- exampleCourse/questions/multiplyTwoNumbers/server.py
+++ exampleCourse/questions/multiplyTwoNumbers/server.py
@@ -1,34 +1,10 @@
-import numpy as np
-
 def get_data(data, options):
-
-    # Number of digits after the decimal
-    nDigits = 1
-
-    # Range of entries
-    numMax = 10
-
-    # The two matrices to be multiplied
-    a = np.random.random_integers(-numMax,numMax)/(10**nDigits)
-    b = np.random.random_integers(-numMax,numMax)/(10**nDigits)
-
-    # Product of these two matrices
-    c = a*b
-
     params = {
-        "a": str(a),
-        "a": b
-    }
-
-    true_answer = {
-        "c": c
+        "stuff": "this is a test"
     }
 
     data = {
-        "params": params,
-        "true_answer": true_answer
+        "params": params
     }
 
     return data
-
-    # return {}
