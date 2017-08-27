7f3c086a953f91ec7f351c56a4f07f38dc0b9eb3
Nathan Walters <nwalters512@gmail.com>
2017-08-26 21:54:49 -0500

    Remove unneeded get_dependencies from course element

--- exampleCourse/elements/course_element/course_element.py
+++ exampleCourse/elements/course_element/course_element.py
@@ -1,12 +1,6 @@
 import random
 import chevron
 
-def get_dependencies(element_html, element_index, data):
-    return {
-        'styles': ['course_element.css'],
-        'scripts': ['course_element.js']
-    }
-
 def render(element_html, element_index, data):
     html_params = { 'number': random.random() }
     with open('course_element.mustache','r') as f:
