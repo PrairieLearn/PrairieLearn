24fc06d327c89c0f39c80ebef9b1a910d753b7ea
Paras <psud@guidewire.com>
2017-08-27 17:37:27 -0700

    Refactoring of assessment_questions_calculate_stats.sql

---
+++ pages/instructorQuestion/instructorQuestion.sql
@@ -0,0 +1,24 @@
+-- BLOCK assessment_question_stats
+SELECT
+    aset.name || ' ' || a.number || ': ' || title AS title,
+    ci.short_name AS course_title,
+    a.id AS assessment_id,
+    aset.color,
+    (aset.abbreviation || a.number) as label,
+    admin_assessment_question_number(aq.id) as number,
+    aq.*
+FROM
+    assessment_questions AS aq
+    JOIN assessments AS a ON (a.id = aq.assessment_id)
+    JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
+    JOIN course_instances AS ci ON (ci.id = a.course_instance_id)
+WHERE
+    aq.question_id=$question_id
+    AND aq.deleted_at IS NULL
+GROUP BY
+    a.id,
+    aq.id,
+    aset.id,
+    ci.id
+ORDER BY
+    admin_assessment_question_number(aq.id);
