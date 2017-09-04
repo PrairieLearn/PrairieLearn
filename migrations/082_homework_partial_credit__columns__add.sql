cdfbe43b17f10e6ddbfabf80e36d1735eb2095d1
Timothy Bretl <tbretl@illinois.edu>
2017-09-04 13:45:38 -0500

    add columns to db (tables and migrations)

---
+++ migrations/082_homework_partial_credit__columns__add.sql
@@ -0,0 +1,4 @@
+ALTER TABLE assessment ADD COLUMN constant_question_value boolean default false;
+ALTER TABLE variants ADD COLUMN num_tries integer default 0;
+ALTER TABLE assessment_questions ADD COLUMN tries_per_variant integer default 1;
+ALTER TABLE instance_questions ADD COLUMN variants_points_list double precision[];
