7755f6fdc13b62ff30b79f48abc8ddd2fb4336b3
Timothy Bretl <tbretl@illinois.edu>
2017-09-05 00:54:09 -0500

    finished grading with variants_points_list

--- sprocs/instance_questions_points_homework.sql
+++ sprocs/instance_questions_points_homework.sql
@@ -34,31 +34,6 @@ BEGIN
     open := TRUE;
     points_list := NULL;
 
-    -- variants_points_list := iq.variants_points_list;
-
-    -- if variants_points_list has cardinality > 0
-    -- if variants_points_list[end] < value
-
-    -- if score is 1
-    --     if end of variants_points_list is non-empty and less than init_points
-    --         replace end of variants_points_list with value (which will be init_points)
-    --     else
-    --         append value to end of variants_points_list
-    -- ELSE
-    --     if end of variants_points_list is non-empty and less than init_points
-    --         if (score*value) is greater than end of variants_points_list
-    --             replace end with score*value
-    --         else do nothing
-    --     else append (score*value) to end
-
-    -- All of this can be reduced to:
-    --
-    -- if (score < 1) set current_value to init_points
-    -- if variants_points_list is non-empty and end is less than init_points
-    --     replace end with greater of it and score*current_value
-    -- else
-    --     append score*current_value to end of variants_points_list
-
     -- if the submission was not correct, then immediately reset current_value
     correct := (submission_score >= 1.0);
     IF NOT correct THEN
@@ -72,8 +47,10 @@ BEGIN
     length := cardinality(variants_points_list);
     var_points_old := coalesce(variants_points_list[length], 0);
     var_points_new := submission_score*current_value;
-    IF (length > 0) AND (var_points_old < least(aq.init_points, var_points_new)) THEN
-        variants_points_list[length] = var_points_new;
+    IF (length > 0) AND (var_points_old < aq.init_points) THEN
+        IF (var_points_old < var_points_new) THEN
+            variants_points_list[length] = var_points_new;
+        END IF;
     ELSE
         variants_points_list := array_append(variants_points_list, var_points_new);
     END IF;
@@ -107,60 +84,5 @@ BEGIN
             status := 'incorrect';
         END IF;
     END IF;
-
-    -- Still need to compute:
-    --      points
-    --      score_perc
-    --      status
-
-
-
-
-
-    -- IF (cardinality(variants_points_list) > 0) THEN
-    --     IF (variants_points_list[cardinality(variants_points_list)] < aq.init_points) THEN
-    --         IF (variants_points_list[cardinality(variants_points_list)] < (submission_score*current_value)) THEN
-    --             variants_points_list[cardinality(variants_points_list)] := submission_score*current_value
-    --         END IF;
-    --     END IF;
-    -- ELSE
-    --     array_append(variants_points_list,submission_score*current_value)
-    -- END IF;
-
-
-
-
-    -- -- get property that says if we should change current_value or not
-    -- SELECT a.constant_question_value INTO constant_question_value FROM assessments AS a WHERE a.id = aq.assessment_id;
-    --
-    -- correct := (submission_score >= 0.5);
-    --
-    -- IF correct THEN
-    --     points := least(iq.points + iq.current_value, aq.max_points);
-    --     score_perc := points / (CASE WHEN aq.max_points > 0 THEN aq.max_points ELSE 1 END) * 100;
-    --
-    --     IF NOT constant_question_value THEN
-    --         current_value := least(iq.current_value + aq.init_points, aq.max_points);
-    --     ELSE
-    --         current_value := iq.current_value;
-    --     END IF;
-    --
-    --     status := 'correct';
-    -- ELSE
-    --     points := iq.points;
-    --     score_perc := iq.score_perc;
-    --
-    --     IF NOT constant_question_value THEN
-    --         current_value := aq.init_points;
-    --     ELSE
-    --         current_value := iq.current_value;
-    --     END IF;
-    --
-    --     -- use current status unless it's 'unanswered'
-    --     status := iq.status;
-    --     IF iq.status = 'unanswered' THEN
-    --         status := 'incorrect';
-    --     END IF;
-    -- END IF;
 END;
 $$ LANGUAGE plpgsql STABLE;
