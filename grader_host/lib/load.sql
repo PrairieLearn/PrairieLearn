449eda362d82c98e82ba6dc6dabd6a79bd29e839
Matthew West <mwest@illinois.edu>
2017-10-02 10:44:45 -0500

    update date when reporting load

--- grader_host/lib/load.sql
+++ grader_host/lib/load.sql
@@ -5,6 +5,7 @@ VALUES
     ($instance_id, $queue_name, $average_jobs, $max_jobs)
 ON CONFLICT (instance_id) DO UPDATE
 SET
+    date = now(),
     queue_name = EXCLUDED.queue_name,
     average_jobs = EXCLUDED.average_jobs,
     max_jobs = EXCLUDED.max_jobs;
