850f681e8d2db47f5bddbf0eab025a8417581e66
Matthew West <mwest@illinois.edu>
2017-10-18 10:55:01 -0500

    re-enable all tests

--- tests/index.js
+++ tests/index.js
@@ -1,12 +1,12 @@
 var logger = require('../lib/logger');
 logger.transports.console.level = 'warn';
 
-// require('./testDatabase');
-// require('./testLoadCourse');
-// require('./testSyncCourseInfo');
-// require('./testGetHomepage');
+require('./testDatabase');
+require('./testLoadCourse');
+require('./testSyncCourseInfo');
+require('./testGetHomepage');
 require('./testQuestions');
-// require('./testInstructorQuestions');
-// require('./testHomework');
-// require('./testExam');
-// require('./testAccess');
+require('./testInstructorQuestions');
+require('./testHomework');
+require('./testExam');
+require('./testAccess');
