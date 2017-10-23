6410095a79fd4fbc074038ee4f44c2532f6aa4d0
Matthew West <mwest@illinois.edu>
2017-10-23 11:39:16 -0500

    switch back to pure-SQL version of instance_questions_calculate_stats

--- migrations/097_instance_questions__stats__run_migration.sql
+++ migrations/097_instance_questions__stats__run_migration.sql
@@ -38,51 +38,40 @@ CREATE OR REPLACE FUNCTION
         instance_question_id_param bigint
     ) RETURNS VOID
 AS $$
-DECLARE
-    some_submission_var boolean;
-    some_perfect_submission_var boolean;
-    some_nonzero_submission_var boolean;
-    submission_score_array_var double precision[];
-    max_submission_score_var double precision;
-    average_submission_score_var double precision;
-    incremental_submission_score_array_var double precision[];
-BEGIN
+WITH first_calculation AS (
     SELECT
-        count(s.id) > 0,
-        coalesce(bool_or(s.score = 1), FALSE),
-        coalesce(bool_or(s.score != 0), FALSE),
-        array_agg(s.score ORDER BY s.date),
-        max(s.score),
-        avg(s.score)
-    INTO
-        some_submission_var,
-        some_perfect_submission_var,
-        some_nonzero_submission_var,
-        submission_score_array_var,
-        max_submission_score_var,
-        average_submission_score_var
+        count(s.id) > 0                        AS some_submission_var,
+        coalesce(bool_or(s.score = 1), FALSE)  AS some_perfect_submission_var,
+        coalesce(bool_or(s.score != 0), FALSE) AS some_nonzero_submission_var,
+        array_agg(s.score ORDER BY s.date)     AS submission_score_array_var,
+        max(s.score)                           AS max_submission_score_var,
+        avg(s.score)                           AS average_submission_score_var
     FROM
         variants AS v
         JOIN submissions AS s ON (s.variant_id = v.id)
-    WHERE v.instance_question_id = instance_question_id_param;
-
-    incremental_submission_score_array_var := migration_097_array_increments_above_max(submission_score_array_var);
-
-    UPDATE instance_questions AS iq
-    SET
-        some_submission = some_submission_var,
-        some_perfect_submission = some_perfect_submission_var,
-        some_nonzero_submission = some_nonzero_submission_var,
-        first_submission_score = submission_score_array_var[1],
-        last_submission_score = submission_score_array_var[array_length(submission_score_array_var, 1)],
-        max_submission_score = max_submission_score_var,
-        average_submission_score = average_submission_score_var,
-        submission_score_array = submission_score_array_var,
-        incremental_submission_score_array = incremental_submission_score_array_var,
-        incremental_submission_points_array = migration_097_array_product(incremental_submission_score_array_var, iq.points_list_original)
-    WHERE iq.id = instance_question_id_param;
-END
-$$ LANGUAGE plpgsql VOLATILE;
+    WHERE v.instance_question_id = instance_question_id_param
+),
+second_calculation AS (
+    SELECT migration_097_array_increments_above_max(submission_score_array_var) AS incremental_submission_score_array_var
+    FROM first_calculation
+)
+UPDATE instance_questions AS iq
+SET
+    some_submission = some_submission_var,
+    some_perfect_submission = some_perfect_submission_var,
+    some_nonzero_submission = some_nonzero_submission_var,
+    first_submission_score = submission_score_array_var[1],
+    last_submission_score = submission_score_array_var[array_length(submission_score_array_var, 1)],
+    max_submission_score = max_submission_score_var,
+    average_submission_score = average_submission_score_var,
+    submission_score_array = submission_score_array_var,
+    incremental_submission_score_array = incremental_submission_score_array_var,
+    incremental_submission_points_array = migration_097_array_product(incremental_submission_score_array_var, iq.points_list_original)
+FROM
+    first_calculation,
+    second_calculation
+WHERE iq.id = instance_question_id_param;
+$$ LANGUAGE SQL VOLATILE;
 
 ------------------------------------------------------------------------------
 ------------------------------------------------------------------------------
