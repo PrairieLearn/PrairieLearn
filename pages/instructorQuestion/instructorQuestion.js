f8af1d194228cc5f08e3364af11cd31d1b8ee0e1
Dave Mussulman <mussulma@illinois.edu>
2017-10-28 18:56:54 -0500

    Moved all to instructorQuestion.js

--- pages/instructorQuestion/instructorQuestion.js
+++ pages/instructorQuestion/instructorQuestion.js
@@ -111,6 +111,12 @@ router.get('/', function(req, res, next) {
     // req.query.variant_id might be undefined, which will generate a new variant
     question.getAndRenderVariant(req.query.variant_id, res.locals, function(err) {
         if (ERR(err, next)) return;
+
+        locals.questionGHLink = null;
+        var GHfound = locals.course.repository.match(/^git@github.com:(.+?)(.git)?$/);
+        if (GHfound) {
+            locals.questionGHLink = 'https://github.com/' + GHfound[1] + '/tree/master/questions/' + locals.question.qid;
+        }
         res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
     });
 });
