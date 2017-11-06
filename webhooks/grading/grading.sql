0ae9c2d2d76cf5c5e8f98ceea113ff3fbeac67db
Matthew West <mwest@illinois.edu>
2017-11-06 08:29:57 -0600

    SQL fixups

--- webhooks/grading/grading.sql
+++ webhooks/grading/grading.sql
@@ -3,21 +3,21 @@ UPDATE grading_jobs AS gj
 SET
     grading_started_at = $start_time
 WHERE
-    gj.id = $grading_job_id
+    gj.id = $grading_job_id;
 
 -- BLOCK get_job_details
 SELECT
     (graded_at IS NOT NULL) AS was_graded,
-    s3_bucket AS s3_bucket,
-    s3_root_key AS s3_root_key
+    s3_bucket,
+    s3_root_key
 FROM
     grading_jobs
 WHERE
-    id = $grading_job_id
+    id = $grading_job_id;
 
 -- BLOCK update_job_output
 UPDATE grading_jobs
 SET
     output = $output
 WHERE
-    id = $grading_job_id
+    id = $grading_job_id;
