4c026ee9e1f3fb5a4a544d096ce93f0f305a49cb
Nathan Walters <nwalters512@gmail.com>
2017-11-26 14:02:38 -0600

    Fix problem with panel rendering query

--- lib/question.sql
+++ lib/question.sql
@@ -77,7 +77,7 @@ FROM
     LEFT JOIN assessment_questions AS aq ON (iq.assessment_question_id = aq.id)
     LEFT JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
     LEFT JOIN assessments AS a ON (a.id = ai.assessment_id)
-    JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
+    LEFT JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
     LEFT JOIN course_instances AS ci ON (ci.id = a.course_instance_id)
     JOIN pl_courses AS c ON (c.id = q.course_id)
 WHERE
