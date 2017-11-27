0a6f398c255bbf05342bb89b50798f99ab9271f0
Timothy Bretl <tbretl@illinois.edu>
2017-11-27 15:27:00 -0600

    enable matlab and python output with copy button

--- elements/pl_matrix_output/pl_matrix_output.py
+++ elements/pl_matrix_output/pl_matrix_output.py
@@ -1,6 +1,7 @@
 import prairielearn as pl
 import lxml.html
 import numpy as np
+import chevron
 
 
 def prepare(element_html, element_index, data):
@@ -13,17 +14,24 @@ def render(element_html, element_index, data):
     element = lxml.html.fragment_fromstring(element_html)
     digits = pl.get_integer_attrib(element, 'digits', 2)
 
-    html = '<pre>\n% Data in MATLAB format\n'
+    matlab_data = ''
+    python_data = 'import numpy as np\n\n'
     for child in element:
         if child.tag == 'variable':
             pl.check_attribs(child, required_attribs=['params_name'], optional_attribs=[])
             var_name = pl.get_string_attrib(child, 'params_name')
             var_data = data['params'].get(var_name, None)
             if var_data is None:
-                raise Exception('No value in data["params"] for variable %s in matrix_output element' % var_name)
-            html += pl.inner_html(child) \
-                + ' = ' \
-                + pl.numpy_to_matlab((var_data if np.isscalar(var_data) else np.array(var_data)), ndigits=digits) + ';' \
-                + '\n'
-    html += '</pre>'
+                raise Exception('No value in data["params"] for variable %s in pl_matrix_output element' % var_name)
+            if np.isscalar(var_data):
+                matlab_data += pl.inner_html(child) + ' = ' + pl.numpy_to_matlab(var_data, ndigits=digits) + ';\n'
+                python_data += pl.inner_html(child) + ' = ' + str(np.array(var_data).round(digits).tolist()) + '\n'
+            else:
+                matlab_data += pl.inner_html(child) + ' = ' + pl.numpy_to_matlab(np.array(var_data), ndigits=digits) + ';\n'
+                python_data += pl.inner_html(child) + ' = np.array(' + str(np.array(var_data).round(digits).tolist()) + ')\n'
+
+    html_params = {'default_is_matlab': True, 'matlab_data': matlab_data, 'python_data': python_data, 'element_index': element_index}
+    with open('pl_matrix_output.mustache', 'r') as f:
+        html = chevron.render(f, html_params).strip()
+
     return html
