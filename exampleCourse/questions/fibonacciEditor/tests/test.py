708e7803c841bd1022a20b083a70d8a9f4e37cbc
Nathan Walters <nwalters512@gmail.com>
2017-12-08 17:44:43 -0600

    Turns out fibonacci and factorial are different things

--- exampleCourse/questions/fibonacciEditor/tests/test.py
+++ exampleCourse/questions/fibonacciEditor/tests/test.py
@@ -5,16 +5,16 @@ from bin.fib import fib
 
 class Test(unittest.TestCase):
     @points(1)
-    @name('Check 0!')
+    @name('Check fib(0)')
     def test_zero(self):
         self.assertEqual(fib(0), 0)
 
     @points(1)
-    @name('Check 1!')
+    @name('Check fib(1)')
     def test_one(self):
         self.assertEqual(fib(1), 1)
 
     @points(3)
-    @name('Check factorial of an integer > 1')
+    @name('Check fibonacci of an integer > 1')
     def test_integer(self):
         self.assertEqual(fib(7), 13)
