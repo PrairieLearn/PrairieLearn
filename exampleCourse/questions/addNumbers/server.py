db444f780b09a99b1b3761e487040d5133afbe82
Timothy Bretl <tbretl@illinois.edu>
2017-12-10 12:52:03 -0600

    update addNumbers in example course to not return data

--- exampleCourse/questions/addNumbers/server.py
+++ exampleCourse/questions/addNumbers/server.py
@@ -2,12 +2,16 @@ import random
 
 def generate(data):
 
+    # Sample two random integers between 5 and 10 (inclusive)
     a = random.randint(5, 10)
     b = random.randint(5, 10)
-    data["params"]["a"] = a
-    data["params"]["b"] = b
 
+    # Put these two integers into data['params']
+    data['params']['a'] = a
+    data['params']['b'] = b
+
+    # Compute the sum of these two integers
     c = a + b
-    data["correct_answers"]["c"] = c
 
-    return data
+    # Put the sum into data['correct_answers']
+    data['correct_answers']['c'] = c
