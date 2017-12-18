PrairieLearn/PrairieLearn

commit af6e9c8a8fe24747b6b6169cf923671a338ba64d

link gradebook scores to assessment_instances

pages/instructorGradebook/instructorGradebook.sql
@@ -25,28 +25,33 @@ course_assessments AS (
     AND a.course_instance_id = $course_instance_id
 ),
 course_scores AS (
-    SELECT ai.user_id, a.id AS assessment_id, max(ai.score_perc) AS score_perc
+    SELECT DISTINCT ON (ai.user_id, a.id)
+        ai.user_id, a.id AS assessment_id, ai.score_perc, ai.id AS assessment_instance_id
     FROM
         assessment_instances AS ai
         JOIN assessments AS a ON (a.id = ai.assessment_id)
     WHERE
         a.course_instance_id = $course_instance_id
-    GROUP BY
-        ai.user_id, a.id
+    ORDER BY
+        ai.user_id, a.id, ai.score_perc, ai.id
 ),
 scores AS (
     SELECT u.user_id,u.uid,u.user_name,u.role,
         a.id AS assessment_id,a.assessment_order_by,a.assessment_set_number,
-        s.score_perc
+        s.score_perc, s.assessment_instance_id
     FROM
         course_users AS u
         CROSS JOIN course_assessments AS a
         LEFT JOIN course_scores AS s ON (s.user_id = u.user_id AND s.assessment_id = a.id)
 )
 SELECT user_id,uid,user_name,role,
-    ARRAY_AGG(score_perc
-          ORDER BY (assessment_set_number, assessment_order_by, assessment_id)
-    ) AS score_percs
+    ARRAY_AGG(
+        json_build_object(
+            'score_perc', score_perc,
+            'assessment_instance_id', assessment_instance_id
+        )
+        ORDER BY (assessment_set_number, assessment_order_by, assessment_id)
+    ) AS scores
 FROM scores
 GROUP BY user_id,uid,user_name,role
 ORDER BY role DESC, uid;

pages/instructorAssessmentInstance/instructorAssessmentInstance.ejs
@@ -93,7 +93,7 @@
                   <%= instance_question.question_number %>.
                   <%= instance_question.question_title %>
                 </a>
-                (<a href="<%= urlPrefix %>/question/<%= instance_question.question_id %>/"><%= instance_question.qid %></a>)
+                (QID: <a href="<%= urlPrefix %>/question/<%= instance_question.question_id %>/"><%= instance_question.qid %></a>)
               </td>
               <td class="cell-center">
                 <%- include('../partials/examQuestionAwardedPoints', {points: instance_question.points, max_points: instance_question.max_points}); %>

pages/instructorGradebook/instructorGradebook.ejs
@@ -33,12 +33,14 @@
                 <td><%= row.uid %></td>
                 <td><%= row.user_name %></td>
                 <td><%= row.role %></td>
-                <% row.score_percs.forEach(function(score_perc) { %>
+                <% row.scores.forEach(function(score) { %>
                 <td>
-                  <% if (score_perc == null) { %>
+                  <% if (score.score_perc == null) { %>
                   —
                   <% } else { %>
-                  <%= Math.floor(score_perc) %>%
+                  <a href="<%= urlPrefix %>/assessment_instance/<%= score.assessment_instance_id %>">
+                    <%= Math.floor(score.score_perc) %>%
+                  </a>
                   <% } %>
                 </td>
                 <% }); %>

pages/instructorGradebook/instructorGradebook.js
@@ -46,7 +46,8 @@ router.get('/:filename', function(req, res, next) {
 
                 var csvHeaders = ['UID', 'Name', 'Role'].concat(_.map(courseAssessments, 'label'));
                 var csvData = _.map(userScores, function(row) {
-                    return [row.uid, row.user_name, row.role].concat(row.score_percs);
+                    const score_percs = _.map(row.scores, s => s.score_perc);
+                    return [row.uid, row.user_name, row.role].concat(score_percs);
                 });
                 csvData.splice(0, 0, csvHeaders);
                 csvStringify(csvData, function(err, csv) {

commit f406dc89ad92a11927382810d744aa938cf28c0c

fix gradebook regression from af6e9c8a that was choosing worst score

pages/instructorGradebook/instructorGradebook.sql
@@ -27,7 +27,7 @@ course_scores AS (
     WHERE
         a.course_instance_id = $course_instance_id
     ORDER BY
-        ai.user_id, a.id, ai.score_perc, ai.id
+        ai.user_id, a.id, ai.score_perc DESC, ai.id
 ),
 user_ids AS (
     (SELECT DISTINCT user_id FROM course_scores)