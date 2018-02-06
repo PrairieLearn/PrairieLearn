bbc6dc07fb505d1ce4d3696600e630ec198414a6
James J Balamuta <balamut2@illinois.edu>
2018-02-06 14:45:57 -0600

    Add list of R package dependencies

---
+++ r-requirements.R
@@ -0,0 +1,12 @@
+#! /usr/bin/env RScript
+
+# R Code Ahead to install packages!
+
+# The following are packages used in STAT 385
+pkg_list = c('tidyverse', 'RcppArmadillo', 
+             'rbenchmark', 'microbenchmark', 
+             'maps', 'maptools', 'mapproj', 'mapdata', 'ggmap',
+             'fivethirtyeight')
+
+install.packages(pkg_list, repos = "https://cran.rstudio.com")
+
