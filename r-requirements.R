92d7cd113608c019e6e162592d6f3432ca630807
James J Balamuta <balamut2@illinois.edu>
2018-02-06 19:31:37 -0600

    Add rmarkdown as a dependency for formatting

--- r-requirements.R
+++ r-requirements.R
@@ -3,7 +3,7 @@
 # R Code Ahead to install packages!
 
 # The following are packages used in STAT 385
-pkg_list = c('tidyverse', 'RcppArmadillo', 
+pkg_list = c('tidyverse', 'RcppArmadillo', 'rmarkdown',
              'rbenchmark', 'microbenchmark', 
              'maps', 'maptools', 'mapproj', 'mapdata', 'ggmap',
              'fivethirtyeight')
