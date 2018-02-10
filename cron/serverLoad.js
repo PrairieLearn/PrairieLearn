4dd5b100d32d1fafd20a93fc5b41cb4fd5a30ef3
Matthew West <mwest@illinois.edu>
2018-02-10 09:39:46 -0600

    fix cron/serverLoad lodash require

--- cron/serverLoad.js
+++ cron/serverLoad.js
@@ -1,8 +1,9 @@
-var ERR = require('async-stacktrace');
+const _ = require('lodash');
+const ERR = require('async-stacktrace');
 const AWS = require('aws-sdk');
 
-var config = require('../lib/config');
-var sqldb = require('../lib/sqldb');
+const config = require('../lib/config');
+const sqldb = require('../lib/sqldb');
 
 module.exports = {};
 
