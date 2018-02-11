ec992249a2b09ac82bc4978ca1423564dfc66b55
Matthew West <mwest@illinois.edu>
2018-02-11 10:26:47 -0600

    use config.json in /efs if it exists

--- docker/init.sh
+++ docker/init.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 cd /PrairieLearn
-docker/start_postgres.sh
-# Uncomment to start redis to test message passing
-# redis-server --daemonize yes
-if [[ -n $NODEMON ]] && [[ $NODEMON == "true" ]]; then
-  npm run start-nodemon
-else
-  npm start
+
+if [[ -f /efs/container/config.json ]] ; then
+    # we are running in production mode
+    node server --config /efs/container/config.json
+elif
+    # we are running in local development mode
+    docker/start_postgres.sh
+
+    # Uncomment to start redis to test message passing
+    # redis-server --daemonize yes
+
+    if [[ -n $NODEMON ]] && [[ $NODEMON == "true" ]]; then
+        npm run start-nodemon
+    else
+        npm start
+    fi
 fi
