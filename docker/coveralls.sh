8284c8e83948db95a4cc3b7799c0d29bbfd4ef92
Dave Mussulman <mussulma@illinois.edu>
2018-02-26 11:36:29 -0600

    More debugging

--- docker/coveralls.sh
+++ docker/coveralls.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
+echo TRAVIS $TRAVIS
+echo TRAVIS_JOB_ID $TRAVIS_JOB_ID
+echo TRAVIS_PULL_REQUEST $TRAVIS_PULL_REQUEST
+echo TRAVIS_BRANCH $TRAVIS_BRANCH
+
 cd /PrairieLearn
+ls -l coverage/lcov.info
 cat coverage/lcov.info | ./node_modules/.bin/coveralls -v
