623659189fc022fdd376191c2095a2802e66d6dc
Paras Sud <psud@guidewire.com>
2018-03-04 19:07:16 -0800

    Re-enable all test

--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,13 @@
 var logger = require('../lib/logger');
 logger.transports.console.level = 'warn';
 
-// require('./testDatabase');
-// require('./testLoadCourse');
-// require('./testSyncCourseInfo');
-// require('./testGetHomepage');
-// require('./testQuestions');
+require('./testDatabase');
+require('./testLoadCourse');
+require('./testSyncCourseInfo');
+require('./testGetHomepage');
+require('./testQuestions');
 require('./testInstructorQuestions');
-// require('./testInstructorAssessment');
-// require('./testHomework');
-// require('./testExam');
-// require('./testAccess');
+require('./testInstructorAssessment');
+require('./testHomework');
+require('./testExam');
+require('./testAccess');
