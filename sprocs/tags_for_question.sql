f065137524408837ded16168cc2c5bcc65764024
Dave Mussulman <mussulma@illinois.edu>
2018-03-08 08:18:46 -0600

    Change tag order to match infoQuestion.json order

--- sprocs/tags_for_question.sql
+++ sprocs/tags_for_question.sql
@@ -7,9 +7,8 @@ SELECT
     JSONB_AGG(JSONB_BUILD_OBJECT(
         'name',tag.name,
         'id',tag.id,
-        'color',tag.color,
-        'number', tag.number
-    ) ORDER BY tag.number, tag.id)
+        'color',tag.color
+    ) ORDER BY qt.number, tag.id)
 FROM
     tags AS tag
     JOIN question_tags AS qt ON (qt.tag_id = tag.id AND qt.question_id = tags_for_question.question_id)
