ecc6b1b9f65c6cba3a8d52787d5d591efd46a469
y1sreal <32115746+y1sreal@users.noreply.github.com>
2018-03-26 16:28:25 -0500

    Calendar
    Calendar

--- pages/home/home.sql
+++ pages/home/home.sql
@@ -10,10 +10,8 @@ course_permissions_for_user AS (
 ),
 courses_list AS (
     SELECT
-        coalesce(jsonb_agg(jsonb_build_object(
-            'label', c.short_name || ': ' || c.title,
-            'id', c.id
-        ) ORDER BY c.short_name, c.title, c.id), '[]'::jsonb) AS courses
+        coalesce(jsonb_agg(jsonb_build_object('label', c.short_name || ': ' || c.title,'id', c.id) ORDER BY c.short_name, c.title, c.id), '[]'::jsonb)
+         AS courses
     FROM
         pl_courses AS c
         LEFT JOIN course_permissions_for_user AS cp ON (cp.course_id = c.id)
@@ -54,10 +52,50 @@ course_instances_list AS (
                 AND check_course_instance_access(ci.id, e.role, e.uid, $req_date)
             )
         )
+),
+
+
+rules AS (
+        SELECT
+
+            coalesce(jsonb_agg(jsonb_build_object(
+              'ed',r.end_date,'credit',aa.credit,'string',aa.credit_date_string,'color',aset.color,'abb', aset.abbreviation, 'course_name', c.short_name, 'id', r.id,'title', ass.title, 'deadline', format_date_full_compact(r.end_date, coalesce(ci.display_timezone)) , 'assessmentID', r.assessment_id, 'courseID', ass.course_instance_id
+            ) ORDER BY r.end_date), '[]'::jsonb) AS aar
+        FROM
+          pl_courses AS c
+          JOIN  course_instances AS ci ON (c.id = ci.course_id)
+          JOIN enrollments_for_user AS efu ON (ci.id = efu.course_instance_id)
+          JOIN assessments AS ass ON (ass.course_instance_id = ci.id)
+          JOIN assessment_access_rules as r ON (r.assessment_id = ass.id AND (DATE(CURRENT_TIMESTAMP) + 3000 >= r.end_date) AND (DATE(CURRENT_TIMESTAMP) <= r.end_date))
+          JOIN assessment_sets AS aset ON (aset.id = ass.assessment_set_id)
+          LEFT JOIN LATERAL authz_assessment(ass.id, $authz_data, $req_date, ci.display_timezone) AS aa ON TRUE
+
+),
+
+rules_passed AS (
+  SELECT
+
+      coalesce(jsonb_agg(jsonb_build_object(
+          'ed',r.end_date,'credit',aa.credit,'string',aa.credit_date_string,'color', aset.color,'abb', aset.abbreviation, 'course_name', c.short_name, 'id', r.id,'title', ass.title, 'deadline', format_date_full_compact(r.end_date, coalesce(ci.display_timezone)), 'assessmentID', r.assessment_id, 'courseID', ass.course_instance_id
+      ) ORDER BY r.end_date), '[]'::jsonb) AS assessment_access_rules_passed
+  FROM
+      pl_courses AS c
+    JOIN  course_instances AS ci ON (c.id = ci.course_id)
+    JOIN enrollments_for_user AS efu ON (ci.id = efu.course_instance_id)
+    JOIN assessments AS ass ON (ass.course_instance_id = ci.id)
+    JOIN assessment_access_rules as r ON (r.assessment_id = ass.id AND (DATE(CURRENT_TIMESTAMP) - 50000 <= r.end_date) AND (DATE(CURRENT_TIMESTAMP) >= r.end_date))
+    JOIN assessment_sets AS aset ON (aset.id = ass.assessment_set_id)
+    LEFT JOIN LATERAL authz_assessment(ass.id, $authz_data, $req_date, ci.display_timezone) AS aa ON TRUE
 )
+
+
 SELECT
     cl.courses,
-    cil.course_instances
+    cil.course_instances,
+    ar.aar,
+    rp.assessment_access_rules_passed
 FROM
+    rules_passed AS rp,
+    rules AS ar,
     courses_list AS cl,
     course_instances_list AS cil;
