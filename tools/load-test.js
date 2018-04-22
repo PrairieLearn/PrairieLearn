e53be2bce4f15daee93d726372f32700879815ea
Matthew West <mwest@illinois.edu>
2018-04-22 13:33:24 -0500

    add full load-test statistics

--- tools/load-test.js
+++ tools/load-test.js
@@ -1,6 +1,5 @@
 /* eslint no-console: 0 */ // allow console.log() in this file
 
-const _ = require('lodash');
 const request = require('request-promise-native');
 const cheerio = require('cheerio');
 const assert = require('assert');
@@ -58,8 +57,62 @@ const cookies = request.jar();
 const loadTestToken = csrf.generateToken('load_test', config.secretKey);
 cookies.setCookie(request.cookie(`load_test_token=${loadTestToken}`), siteUrl);
 
-function avg(values) {
-    return _.reduce(values, (a,b) => (a+b)) / _.size(values);
+/* Takes [1, 2, 3]
+ * and returns 6
+ */
+function sum(values) {
+    return values.reduce((a,b) => (a+b));
+}
+
+/* Takes [1, 2, 3]
+ * and returns {mean: 2, stddev: 0.816}
+ */
+function stats(values) {
+    if (values.length == 0) return {mean: NaN, stddev: NaN};
+    const mean = sum(values) / values.length;
+    const stddev = Math.sqrt(sum(values.map(x => (x-mean)**2)) / values.length);
+    return {mean, stddev};
+}
+
+/* Takes:
+ * {
+ *     key1: [1, 2, 3],
+ *     key2: [5, 6, 7, 8, 9],
+ * }
+ * and returns:
+ * {
+ *     key1: {mean: 2, stddev: 0.816},
+ *     key2: {mean: 7, stddev: 1.414},
+ * }
+ */
+function objectStats(obj) {
+    const ret = {};
+    for (let k in obj) {
+        ret[k] = stats(obj[k]);
+    }
+    return ret;
+}
+
+/* Takes:
+ * [
+ *     {key1: [1, 2, 3], key2: [5, 6, 7, 8, 9]},
+ *     {key2: [10, 11], key2: [12, 13, 14]},
+ * ]
+ * and returns:
+ * {
+ *     key1: [1, 2, 3, 10, 11],
+ *     key2: [5, 6, 7, 8, 9, 12, 13, 14],
+ * }
+ */
+function aggregate(objs) {
+    const ret = {};
+    for (let obj of objs) {
+        for (let key in obj) {
+            if (!(key in ret)) ret[key] = [];
+            ret[key] = ret[key].concat(obj[key]);
+        }
+    }
+    return ret;
 }
 
 async function sleep(sec) {
@@ -69,31 +122,31 @@ async function sleep(sec) {
 async function getCourseInstanceUrl() {
     const body = await request({uri: baseUrl, jar: cookies});
     const $ = cheerio.load(body);
-    const elemList = $(`td a:contains("${exampleCourseName}")`);
-    assert(elemList.length == 1);
-    return serverUrl + elemList[0].attribs.href;
+    const elemListCourse = $(`td a:contains("${exampleCourseName}")`);
+    assert(elemListCourse.length == 1);
+    return serverUrl + elemListCourse[0].attribs.href;
 }
 
 async function getQuestionUrl(courseInstanceUrl) {
     const questionsUrl = courseInstanceUrl + '/instructor/questions';
     const body = await request({uri: questionsUrl, jar: cookies});
     const $ = cheerio.load(body);
-    const elemList = $(`td a:contains("${questionTitle}")`);
-    assert(elemList.length == 1);
-    return serverUrl + elemList[0].attribs.href;
+    const elemListQuestion = $(`td a:contains("${questionTitle}")`);
+    assert(elemListQuestion.length == 1);
+    return serverUrl + elemListQuestion[0].attribs.href;
 }
 
 async function getQuestionSubmitInfo(questionUrl) {
     const body = await request({uri: questionUrl, jar: cookies});
     const $ = cheerio.load(body);
 
-    let elemList = $('.question-form input[name="__variant_id"]');
-    assert(elemList.length == 1);
-    const variant_id = Number.parseInt(elemList[0].attribs.value);
+    const elemListVariantId = $('.question-form input[name="__variant_id"]');
+    assert(elemListVariantId.length == 1);
+    const variant_id = Number.parseInt(elemListVariantId[0].attribs.value);
 
-    elemList = $('.question-form input[name="__csrf_token"]');
-    assert(elemList.length == 1);
-    const csrf_token = elemList[0].attribs.value;
+    const elemListCsrfToken = $('.question-form input[name="__csrf_token"]');
+    assert(elemListCsrfToken.length == 1);
+    const csrf_token = elemListCsrfToken[0].attribs.value;
 
     return {questionUrl, variant_id, csrf_token};
 }
@@ -107,42 +160,71 @@ async function postQuestionAnswer(questionSubmitInfo) {
     };
     const body = await request.post({uri: questionSubmitInfo.questionUrl, jar: cookies, form, followAllRedirects: true});
     const $ = cheerio.load(body);
-    let elemList = $('.question-form input[name="__variant_id"]');
-    assert(elemList.length == 1);
+    const elemListVariantId = $('.question-form input[name="__variant_id"]');
+    assert(elemListVariantId.length == 1);
 }
 
 async function singleRequest() {
-    const startMS = Date.now();
+    const totalStartMS = Date.now();
+    let startMS;
+
+    startMS = Date.now();
     const courseInstanceUrl = await getCourseInstanceUrl();
+    const timeHomepage = (Date.now() - startMS) / 1000;
+
+    startMS = Date.now();
     const questionUrl = await getQuestionUrl(courseInstanceUrl);
+    const timeQuestions = (Date.now() - startMS) / 1000;
+
+    startMS = Date.now();
     const questionSubmitInfo = await getQuestionSubmitInfo(questionUrl);
+    const timeQuestion = (Date.now() - startMS) / 1000;
+
+    startMS = Date.now();
     await postQuestionAnswer(questionSubmitInfo);
-    const endMS = Date.now();
-    return (endMS - startMS) / 1000;
+    const timeSubmit = (Date.now() - startMS) / 1000;
+
+    const timeTotal = (Date.now() - totalStartMS) / 1000;
+
+    return {timeHomepage, timeQuestions, timeQuestion, timeSubmit, timeTotal};
 }
 
 async function singleClientTest(iterations, iClient) {
-    let totalTime = 0;
+    let results = {
+        success: [],
+        timeHomepage: [],
+        timeQuestions: [],
+        timeQuestion: [],
+        timeSubmit: [],
+        timeTotal: [],
+    };
     for (let i = 0; i < iterations; i++) {
         console.log(`start (iteration ${i}, client ${iClient})`);
         try {
-            totalTime += await singleRequest();
+            const r = await singleRequest();
+            results.success.push(1);
+            results.timeHomepage.push(r.timeHomepage);
+            results.timeQuestions.push(r.timeQuestions);
+            results.timeQuestion.push(r.timeQuestion);
+            results.timeSubmit.push(r.timeSubmit);
+            results.timeTotal.push(r.timeTotal);
         } catch (e) {
             console.log('Error', e);
-            totalTime = NaN
+            results.success.push(0);
         }
         console.log(`end (iteration ${i}, client ${iClient})`);
     }
-    return totalTime / iterations;
+    return results;
 }
 
 async function singleTest(clients, iterations) {
-    let clientTimes = [];
+    const clientArray = [];
     for (let i = 0; i < clients; i++) {
-        clientTimes.push(singleClientTest(iterations, i));
+        clientArray.push(singleClientTest(iterations, i));
     }
-    let times = await Promise.all(clientTimes);
-    return avg(times);
+    const clientResults = await Promise.all(clientArray);
+    const aggResults = aggregate(clientResults);
+    return objectStats(aggResults);
 }
 
 async function main() {
@@ -153,13 +235,21 @@ async function main() {
             console.log(`Starting test with ${c} clients and ${argv.iterations} iterations`);
             console.log(`Sleeping for ${sleepTimeSec} seconds...`);
             await sleep(sleepTimeSec);
-            const time = await singleTest(c, argv.iterations);
-            console.log(`Average request time for ${c} clients and ${argv.iterations} iterations: ${time} seconds`);
-            results.push({clients: c, iterations: argv.iterations, time});
+            const result = await singleTest(c, argv.iterations);
+            console.log(`${c} clients, ${argv.iterations} iterations, ${result.success.mean} success`);
+            console.log(`homepage: ${result.timeHomepage.mean} +/- ${result.timeHomepage.stddev} s`);
+            console.log(`questions: ${result.timeQuestions.mean} +/- ${result.timeQuestions.stddev} s`);
+            console.log(`question: ${result.timeQuestion.mean} +/- ${result.timeQuestion.stddev} s`);
+            console.log(`submit: ${result.timeSubmit.mean} +/- ${result.timeSubmit.stddev} s`);
+            console.log(`total: ${result.timeTotal.mean} +/- ${result.timeTotal.stddev} s`);
+            result.clients = c;
+            result.iterations = argv.iterations;
+            results.push(result);
         }
         console.log('######################################################################');
-        for (let r of results) {
-            console.log(`Average request time for ${r.clients} clients and ${r.iterations} iterations: ${r.time} seconds`);
+        console.log('clients,iterations,success,homepage mean,homepage stddev,questions mean,questions stddev,question mean,question stddev,submit mean,submit stddev,total mean,total stddev');
+        for (let result of results) {
+            console.log(`${result.clients},${result.iterations},${result.success.mean},${result.timeHomepage.mean},${result.timeHomepage.stddev},${result.timeQuestions.mean},${result.timeQuestions.stddev},${result.timeQuestion.mean},${result.timeQuestion.stddev},${result.timeSubmit.mean},${result.timeSubmit.stddev},${result.timeTotal.mean},${result.timeTotal.stddev}`);
         }
     } catch (e) {
         console.log('Error', e);
