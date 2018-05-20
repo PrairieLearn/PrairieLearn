e67858833fbf5d8a6e3045f915faaf929bf472f5
Nathan Walters <nwalters512@gmail.com>
2018-05-20 13:16:56 -0500

    Add config loader from PrairieGrader (#6)

--- prairielib/index.js
+++ prairielib/index.js
@@ -2,3 +2,4 @@ module.exports.sqlDb = require('./lib/sql-db');
 module.exports.sqlLoader = require('./lib/sql-loader');
 module.exports.error = require('./lib/error');
 module.exports.util = require('./lib/util');
+module.exports.config = require('./lib/config');
