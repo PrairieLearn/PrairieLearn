acb72351d39e835aad047196311a02b30464b22c
Matthew West <mwest@illinois.edu>
2018-05-21 14:02:06 -0500

    add sqldb as an alias for sqlDb

--- prairielib/index.js
+++ prairielib/index.js
@@ -1,4 +1,5 @@
 module.exports.sqlDb = require('./lib/sql-db');
+module.exports.sqldb = require('./lib/sql-db');
 module.exports.sqlLoader = require('./lib/sql-loader');
 module.exports.error = require('./lib/error');
 module.exports.util = require('./lib/util');
