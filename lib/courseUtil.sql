f18bd89612fae74c0ed832dfa484d58647e2aca3
Nathan Walters <nwalters512@gmail.com>
2018-05-27 20:46:34 -0500

    Another commit for testing

--- lib/courseUtil.sql
+++ lib/courseUtil.sql
@@ -1,6 +1,6 @@
 -- BLOCK update_course_commit_hash
-UPDATE pl_courses as c
+UPDATE pl_courses
 SET
-    c.commit_hash = $commit_hash
+    commit_hash = $commit_hash
 WHERE
-    c.id = $course_id;
+    id = $course_id;
