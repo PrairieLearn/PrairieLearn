40e4d5e170b497ca67588ba5aaf237fea5138ee2
Mariana Silva <mfsilva@illinois.edu>
2018-06-09 12:23:17 -0500

    Component matrix input element including partial credit and feedback

--- exampleCourse/questions/gradientVectorFunction/server.py
+++ exampleCourse/questions/gradientVectorFunction/server.py
@@ -7,14 +7,27 @@ import scipy.linalg as sla
 
 def generate(data):
 
-    f = np.array([[1],[2],[3]])
-    g = np.random.rand(3,2)
-    g = np.array([[2],[1]])
+    a1 = random.randint(2,12)
+    a2 = random.randint(2,12)
+    b1 = random.randint(2,12)
+    b2 = random.randint(2,12)
+    c1 = random.randint(2,12)
+    c2 = random.randint(2,12)
 
-    g = [[3.22,2.42345],[4,3.444433],[4,4]]
-    #g = [[3.22,2.42345],[4,3.444433],[4]]
+    x1 = random.choice([0,1,2])
+    x2 = random.choice([0,1,2])
 
-    data["params"]["f"] = pl.to_json(f)
+    g = [[2*a1*x1, a2],[-b2*2*x1,3*b1*(x2**2)],[-2*c1*x1,c2]]
+    #g = [[1,2],[3,4],[5,6]]
+
+    data["params"]["a1"] = a1
+    data["params"]["a2"] = a2
+    data["params"]["b1"] = b1
+    data["params"]["b2"] = b2
+    data["params"]["c1"] = c1
+    data["params"]["c2"] = c2
+    data["params"]["x1"] = x1
+    data["params"]["x2"] = x2
 
     data["correct_answers"]["g"] = pl.to_json(g)
 
