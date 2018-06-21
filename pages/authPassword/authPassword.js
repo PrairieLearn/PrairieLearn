876bd105dc905630f9d01d787c1d60f70113a72a
Dave Mussulman <mussulma@illinois.edu>
2018-06-21 12:58:45 -0500

    Add timeouts to CSRF and cookie

--- pages/authPassword/authPassword.js
+++ pages/authPassword/authPassword.js
@@ -16,9 +16,10 @@ router.post('/', function(req, res) {
     if (req.body.__action == 'assessmentPassword') {
 
         var pl_pw_origUrl = req.cookies.pl_pw_origUrl;
+        var maxAge = 1000 * 60 * 60 * 12; // 12 hours
 
-        var pwCookie = csrf.generateToken({password: req.body.password}, config.secretKey);
-        res.cookie('pl_assessmentpw', pwCookie);
+        var pwCookie = csrf.generateToken({password: req.body.password, maxAge: maxAge}, config.secretKey);
+        res.cookie('pl_assessmentpw', pwCookie, {maxAge: maxAge});
         res.clearCookie('pl_pw_origUrl');
         return res.redirect(pl_pw_origUrl);
     }
