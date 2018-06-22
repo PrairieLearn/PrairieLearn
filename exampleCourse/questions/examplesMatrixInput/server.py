2b0f9734eb81fadbb2e82a1cc091c55bb7afe197
Mariana Silva <mfsilva@illinois.edu>
2018-06-22 14:03:04 -0500

    updated example question

--- exampleCourse/questions/examplesMatrixInput/server.py
+++ exampleCourse/questions/examplesMatrixInput/server.py
@@ -8,26 +8,17 @@ import to_precision
 
 def generate(data):
 
-
     A =  np.random.rand(2,2)
-
-    dig = 3
-    sf = 1
-    B = np.round(A,dig)
-    C = B.copy()
-
-    for ix,iy in np.ndindex(B.shape):
-        C[ix,iy]=to_precision.to_precision(C[ix,iy],sf)
-
+    sf = 2
+    B = np.round(A,sf)
     x =  np.array([[1,2,3,4]])
 
-    data["params"]["dig"] = dig
     data["params"]["sf"] = sf
     data["params"]["in"] = pl.to_json(B)
     data["params"]["x"] = pl.to_json(x)
 
-    data["correct_answers"]["out1"] = pl.to_json(C)
-    data["correct_answers"]["out2"] = pl.to_json(C)
+    data["correct_answers"]["out1"] = pl.to_json(B)
+    data["correct_answers"]["out2"] = pl.to_json(B)
     data["correct_answers"]["out3"] = pl.to_json(x)
 
     return data
