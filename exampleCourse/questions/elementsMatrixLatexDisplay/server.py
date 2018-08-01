d3bbf409fb72aaee5a0e034f53961f52e6ed9b1e
Mariana Silva <mfsilva@illinois.edu>
2018-07-31 19:48:38 -0500

    added requested changes

--- exampleCourse/questions/elementsMatrixLatexDisplay/server.py
+++ exampleCourse/questions/elementsMatrixLatexDisplay/server.py
@@ -12,22 +12,26 @@ def generate(data):
     # Matrix shape
     M = 3
 
+    myNumber = 2.148233
+
     # Generating the orthogonal matrix U
     # (numbers rounded with 2 decimal digits)
-    X = np.random.rand(M,M)
+    X = np.random.rand(M, M)
     Q,R = sla.qr(X)
-    U = np.around(Q,sf+1)
+    U = np.around(Q, sf + 1)
 
-    b = np.random.rand(M+3)
-    c = np.random.rand(M,1)
+    b = np.random.rand(M)
+    bc = b.reshape((M, 1))
+    br = b.reshape(1, M)
 
     x = np.array([['x'],['y'],['z']])
 
-    data["params"]["sf"] = sf
-    data["params"]["M"] = M
-    data["params"]["U"] = pl.to_json(U)
-    data["params"]["b"] = pl.to_json(b)
-    data["params"]["c"] = pl.to_json(c)
-    data["params"]["x"] = pl.to_json(x)
+    data['params']['sf'] = sf
+    data['params']['M'] = M
+    data['params']['U'] = pl.to_json(U)
+    data['params']['myNumber'] = pl.to_json(myNumber)
+    data['params']['b'] = pl.to_json(br)
+    data['params']['c'] = pl.to_json(bc)
+    data['params']['x'] = pl.to_json(x)
 
     return data
