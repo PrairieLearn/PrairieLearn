8b50ac828589cbeb385e4c07ea2a8a190a434aee
Timothy Bretl <tbretl@illinois.edu>
2018-09-07 23:23:36 -0500

    restore all unit tests

--- tests/index.js
+++ tests/index.js
@@ -1,15 +1,15 @@
 var logger = require('../lib/logger');
 logger.transports.console.level = 'warn';
 
-// require('./testDatabase');
-// require('./testLoadCourse');
-// require('./testSyncCourseInfo');
-// require('./testGetHomepage');
-// require('./testQuestions');
-// require('./testInstructorQuestions');
-// require('./testInstructorAssessment');
-// require('./testHomework');
-// require('./testExam');
-// require('./testAccess');
-// require('./testZoneGradingHomework');
+require('./testDatabase');
+require('./testLoadCourse');
+require('./testSyncCourseInfo');
+require('./testGetHomepage');
+require('./testQuestions');
+require('./testInstructorQuestions');
+require('./testInstructorAssessment');
+require('./testHomework');
+require('./testExam');
+require('./testAccess');
+require('./testZoneGradingHomework');
 require('./testZoneGradingExam');
