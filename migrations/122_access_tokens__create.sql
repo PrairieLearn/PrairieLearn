74c62744431bd5868e1be07d6cfb937f344f2e2b
Nathan Walters <nwalters512@gmail.com>
2018-09-09 12:59:04 -0500

    Track token last used time; fixes from review

--- migrations/122_access_tokens__create.sql
+++ migrations/122_access_tokens__create.sql
@@ -4,5 +4,6 @@ CREATE TABLE access_tokens (
     user_id BIGINT NOT NULL REFERENCES users ON DELETE CASCADE ON UPDATE CASCADE,
     name TEXT NOT NULL,
     token TEXT,
-    token_hash TEXT UNIQUE NOT NULL
+    token_hash TEXT UNIQUE NOT NULL,
+    last_used_at timestamp with time zone
 )
--- pages/userSettings/userSettings.sql
+++ pages/userSettings/userSettings.sql
@@ -4,7 +4,8 @@ SELECT
     name,
     token,
     token_hash,
-    format_date_full_compact(created_at, config_select('display_timezone')) AS created_at
+    format_date_full_compact(created_at, config_select('display_timezone')) AS created_at,
+    format_date_full_compact(last_used_at, config_select('display_timezone')) AS last_used_at
 FROM
     access_tokens
 WHERE
