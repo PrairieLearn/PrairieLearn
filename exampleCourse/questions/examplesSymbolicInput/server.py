e65b44d7bb3f84a75f6aec87801b808c1f9fed68
Timothy Bretl <tbretl@illinois.edu>
2018-09-13 23:50:24 -0500

    create example question

---
+++ exampleCourse/questions/examplesSymbolicInput/server.py
@@ -0,0 +1,9 @@
+import sympy
+import prairielearn as pl
+
+def generate(data):
+    sympy.var('y b a m')
+    x = (y - b) / a
+    z = m * (sympy.cos(a) + sympy.I * sympy.sin(a))
+    data['correct_answers']['x'] = pl.to_json(x)
+    data['correct_answers']['z'] = pl.to_json(z)
