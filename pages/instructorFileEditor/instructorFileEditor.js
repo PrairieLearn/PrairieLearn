d0962a317c3ab6133b51398b92aa0e49221b4ec7
Timothy Bretl <tbretl@illinois.edu>
2018-10-07 10:52:57 -0500

    prototype of creating a file edit with storage on local disk

--- pages/instructorFileEditor/instructorFileEditor.js
+++ pages/instructorFileEditor/instructorFileEditor.js
@@ -2,41 +2,42 @@ var ERR = require('async-stacktrace');
 var _ = require('lodash');
 var express = require('express');
 var router = express.Router();
+const async = require('async');
 
 var error = require('@prairielearn/prairielib/error');
 var sqldb = require('@prairielearn/prairielib/sql-db');
 var sqlLoader = require('@prairielearn/prairielib/sql-loader');
-var fs = require('fs');
+var fs = require('fs-extra');
+const os = require('os');
 var path = require('path');
 const uuidv4 = require('uuid/v4');
 // const serverJobs = require('../../lib/server-jobs');
+const debug = require('debug')('prairielearn:instructorFileEditor');
 
 const namedLocks = require('../../lib/named-locks');
 
-const { exec } = require('child_process');
+const {
+    exec
+} = require('child_process');
 
 var sql = sqlLoader.loadSqlEquiv(__filename);
 
-const b64EncodeUnicode = function(str) {
+function b64EncodeUnicode(str) {
     // (1) use encodeURIComponent to get percent-encoded UTF-8
     // (2) convert percent encodings to raw bytes
     // (3) convert raw bytes to Base64
     return Buffer.from(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
         function toSolidBytes(match, p1) {
             return String.fromCharCode('0x' + p1);
-    })).toString('base64');
-};
+        })).toString('base64');
+}
 
-const getData = function(filename, callback) {
-    fs.readFile(filename, {
-        encoding: 'utf8'
-    }, function(err, data) {
-        if (err) {
-            return callback('Error reading file: ' + filename + ': ' + err);
-        }
-        callback(null, data);
-    });
-};
+function b64DecodeUnicode(str) {
+    // Going backwards: from bytestream, to percent-encoding, to original string.
+    return decodeURIComponent(Buffer.from(str, 'base64').toString().split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+}
 
 router.get('/', function(req, res, next) {
     if (_.isEmpty(req.query)) {
@@ -46,40 +47,208 @@ router.get('/', function(req, res, next) {
         }));
     }
 
-    const file_path = path.join(res.locals.course.path, req.query.file);
-    fs.stat(file_path, function(err, stat) {
-        if (err == null) {
+    // async.waterfall([
+    //     function(callback) {
+    //         callback(null, 'one', 'two');
+    //     },
+    //     function(arg1, arg2, callback) {
+    //         // arg1 now equals 'one' and arg2 now equals 'two'
+    //         callback(null, 'three');
+    //     },
+    //     function(arg1, callback) {
+    //         // arg1 now equals 'three'
+    //         callback(null, 'done');
+    //     }
+    // ], function(err, result) {
+    //     // result now equals 'done'
+    // });
 
-            // Read file before getting hash so we are conservative later when
-            // deciding whether or not this file has been changed.
+    // async.series([
+    //         function(callback) {
+    //             // do some stuff ...
+    //             callback(null, 'one');
+    //         },
+    //         function(callback) {
+    //             // do some more stuff ...
+    //             callback(null, 'two');
+    //         }
+    //     ],
+    //     // optional callback
+    //     function(err, results) {
+    //         // results is now equal to ['one', 'two']
+    //     }
+    // );
 
-            getData(file_path, function(err, data) {
-                if (ERR(err, next)) return;
+    /**
+    TODO:
+    - consider naming with uuid? (I'm concerned about orphaned files, which may
+      result in duplicate names / overwrites)
+    **/
 
-                getCommitHash(res.locals.course.path, req.query.file, (err, hash) => {
-                    if (ERR(err, next)) return;
-
-                    res.locals.editorData = {
-                        'uuid': uuidv4(),
-                        'file': req.query.file,
-                        'contents': b64EncodeUnicode(data),
-                        'hash': hash,
-                    };
-
-                    // FIXME: add these...
-                    // job_sequence_id: req.params.job_sequence_id,
-                    // course_id: res.locals.course ? res.locals.course.id : null,
-
-                    res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
-                });
+    res.locals.editorData = {
+        uuid: uuidv4(),
+    };
+    const full_path_to_file = path.join(res.locals.course.path, req.query.file);
+    debug("Full path to original file: " + full_path_to_file);
+    async.series([
+        (callback) => {
+            debug("Check if original file exists");
+            // FIXME: don't do this (https://nodejs.org/api/fs.html#fs_fs_stat_path_options_callback) - instead, handle error on fs.readFile
+            fs.stat(full_path_to_file, (err, stat) => {
+                if (ERR(err, callback)) return;
+                res.locals.editorData.dirName = path.dirname(req.query.file);
+                debug(`Directory name is ${res.locals.editorData.dirName}`);
+                res.locals.editorData.fileName = path.basename(req.query.file);
+                debug(`File name is ${res.locals.editorData.fileName}`);
+                callback(null);
             });
-        } else {
-            return next(error.make(400, 'file ' + req.query.file + ' does not exist', {
-                locals: res.locals,
-                body: req.body
-            }));
+        },
+        (callback) => {
+            debug("Read original file");
+            fs.readFile(full_path_to_file, 'utf8', (err, data) => {
+                if (ERR(err, callback)) return;
+                res.locals.editorData.originalContents = b64EncodeUnicode(data);
+                callback(null);
+            });
+        },
+        (callback) => {
+            debug("Get commit hash of original file");
+            const execOptions = {
+                cwd: res.locals.course.path,
+                env: process.env,
+            };
+            exec('git rev-parse HEAD:' + path.join(res.locals.editorData.dirName, res.locals.editorData.fileName), execOptions, (err, stdout) => {
+                if (ERR(err, callback)) return;
+                res.locals.editorData.originalHash = stdout.trim();
+                callback(null);
+            });
+        },
+        (callback) => {
+            debug("Query database for file edit");
+            sqldb.query(sql.select_file_edit, {
+                user_id: res.locals.user.user_id,
+                course_id: res.locals.course.id,
+                dir_name: res.locals.editorData.dirName,
+                file_name: res.locals.editorData.fileName,
+            }, (err, result) => {
+                if (ERR(err, callback)) return;
+                if (result.rows.length > 0) {
+                    debug("Found file edit with id: " + String(result.rows[0].id));
+                    if (result.rows[0].commit_hash == res.locals.editorData.originalHash) {
+                        res.locals.editorData.editID = result.rows[0].id;
+                        res.locals.editorData.editHash = result.rows[0].commit_hash;
+                    } else {
+                        debug("Original and edit commit_hash are different");
+                        // FIXME: const dir = getDevFileEditsDirectory(res.locals.user.user_id, res.locals.course.id, result.rows[0].id);
+
+                        // FIXME: delete edit...
+                    }
+                }
+                callback(null);
+            });
+        },
+        (callback) => {
+            if (!('editID' in res.locals.editorData)) {
+                debug("Create new edit");
+                async.series([
+                    (callback) => {
+                        const params = {
+                            user_id: res.locals.user.user_id,
+                            course_id: res.locals.course.id,
+                            dir_name: res.locals.editorData.dirName,
+                            file_name: res.locals.editorData.fileName,
+                            commit_hash: res.locals.editorData.originalHash
+                        };
+                        sqldb.queryOneRow(sql.insert_file_edit, params, function(err, result) {
+                            if (ERR(err, callback)) return;
+                            res.locals.editorData.editID = result.rows[0].id;
+                            res.locals.editorData.editHash = result.rows[0].commit_hash;
+                            debug("Created file edit in database with id " + String(res.locals.editorData.editID));
+                            callback(null);
+                        });
+                    },
+                    (callback) => {
+                        debug("Ensure file edit directory exists");
+                        fs.mkdirs(getDevDirName(), (err) => {
+                            if (ERR(err, callback)) return;
+                            callback(null);
+                        });
+                    },
+                    (callback) => {
+                        const dirName = getDevDirName();
+                        const fileName = getDevFileName(res.locals.user.user_id, res.locals.course.id, res.locals.editorData.editID, res.locals.editorData.fileName);
+                        const contents = b64DecodeUnicode(res.locals.editorData.originalContents);
+                        fs.writeFile(path.join(dirName, fileName), contents, 'utf8', (err) => {
+                            if (ERR(err, callback)) return;
+                            debug(`Created file edit on local disk with name ${fileName}`);
+                            callback(null);
+                        });
+                    }
+                ], (err) => {
+                    if (ERR(err, callback)) return;
+                    callback(null);
+                });
+            }
+        },
+        (callback) => {
+
+
+
+
+            // if there is an active file edit:
+            //  - if commit hash differs from what was found above, then (1) make edit inactive, (2) set contents from original, (3) set flag to tell user what happened
+            //  - else, set contents from edit
+            // else:
+            //  - add edit to database
+            //  - add edit to S3/local
+
+            res.locals.editorData.hash = res.locals.editorData.originalHash;
+            res.locals.editorData.contents = res.locals.editorData.originalContents;
+            callback(null);
         }
+    ], (err) => {
+        if (ERR(err, next)) return;
+        debug("Render");
+        res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
     });
+
+
+
+
+
+    // fs.stat(file_path, function(err, stat) {
+    //     if (err == null) {
+    //
+    //         // Read file before getting hash so we are conservative later when
+    //         // deciding whether or not this file has been changed.
+    //
+    //         getData(file_path, function(err, data) {
+    //             if (ERR(err, next)) return;
+    //
+    //             getCommitHash(res.locals.course.path, req.query.file, (err, hash) => {
+    //                 if (ERR(err, next)) return;
+    //
+    //                 res.locals.editorData = {
+    //                     'uuid': uuidv4(),
+    //                     'file': req.query.file,
+    //                     'contents': b64EncodeUnicode(data),
+    //                     'hash': hash,
+    //                 };
+    //
+    //                 // FIXME: add these...
+    //                 // job_sequence_id: req.params.job_sequence_id,
+    //                 // course_id: res.locals.course ? res.locals.course.id : null,
+    //
+    //                 res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+    //             });
+    //         });
+    //     } else {
+    //         return next(error.make(400, 'file ' + req.query.file + ' does not exist', {
+    //             locals: res.locals,
+    //             body: req.body
+    //         }));
+    //     }
+    // });
 });
 
 const getCommitHash = function(course_path, file_path, callback) {
@@ -244,8 +413,6 @@ const saveAndSync = function() {
 
 
 
-
-
 router.post('/', function(req, res, next) {
     if (!res.locals.authz_data.has_course_permission_own) return next(new Error('Insufficient permissions'));
     if (req.body.__action == 'save_and_sync') {
@@ -272,7 +439,7 @@ router.post('/', function(req, res, next) {
             if (lock == null) {
                 // logger.verbose(`Did not acquire lock ${lockName}`);
                 // callback(new Error(`Another user is already syncing or modifying the course: ${courseDir}`));
-                return next(error.make(400, 'Another user is already syncing or modifying the course: ${res.locals.course.path}', {
+                return next(error.make(400, `Another user is already syncing or modifying the course: ${res.locals.course.path}`, {
                     locals: res.locals,
                     body: req.body
                 }));
@@ -392,5 +559,39 @@ router.post('/', function(req, res, next) {
     //     return next(error.make(400, 'unknown __action', {locals: res.locals, body: req.body}));
     // }
 });
+
+
+
+
+/**
+ * Returns the path to the directory where the file edits should be written to
+ * while running in development (local) mode.
+ *
+ * If we're running natively, this return $HOME/.pl_file_edits/edit_<editID>.
+ * If we're running in Docker, this return /file_edits/edit_<editID>.
+ *
+ * On Windows, we use $USERPROFILE instead of $HOME.
+ */
+function getDevDirName() {
+    if (process.env.HOST_JOBS_DIR) {
+        // We're probably running in Docker
+        return '/file_edits';
+    } else {
+        // We're probably running natively
+        if (process.env.FILE_EDITS_DIR) {
+            // The user wants to use a custom edits dir
+            return process.env.FILE_EDITS_DIR;
+        } else {
+            return path.resolve(path.join(os.homedir(), '.pl_file_edits'));
+        }
+    }
+}
+
+function getDevFileName(userID, courseID, editID, fileName) {
+    return `edit_${userID}_${courseID}_${editID}_${fileName}`;
+}
+
+
+
 
 module.exports = router;
