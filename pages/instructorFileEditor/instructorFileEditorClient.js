efa36d09b8cc7dfcd5165c46c3b1fe41869c10d0
Timothy Bretl <tbretl@illinois.edu>
2018-10-28 15:12:14 -0500

    fix one more function notation

--- pages/instructorFileEditor/instructorFileEditorClient.js
+++ pages/instructorFileEditor/instructorFileEditorClient.js
@@ -62,7 +62,7 @@ window.InstructorFileEditor.prototype.b64EncodeUnicode = function(str) {
     // first we use encodeURIComponent to get percent-encoded UTF-8,
     // then we convert the percent encodings into raw bytes which
     // can be fed into btoa.
-    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (match, p1) => {
         return String.fromCharCode('0x' + p1);
     }));
 };
