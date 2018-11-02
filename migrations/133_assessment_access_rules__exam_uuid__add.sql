e9da0d0cf770de99a7973396ec299d59b884f066
Dave Mussulman <mussulma@illinois.edu>
2018-11-02 17:13:34 -0500

    migration number update

---
+++ migrations/133_assessment_access_rules__exam_uuid__add.sql
@@ -0,0 +1,2 @@
+ALTER TABLE exams ADD COLUMN IF NOT EXISTS uuid uuid UNIQUE;
+ALTER TABLE assessment_access_rules ADD COLUMN exam_uuid uuid;
--- migrations/900_assessment_access_rules__exam_uuid__add.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE exams ADD COLUMN IF NOT EXISTS uuid uuid NULL UNIQUE;
-ALTER TABLE assessment_access_rules ADD COLUMN exam_uuid uuid NULL;
