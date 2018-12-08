61b34e49ab114b53c83bf3f8eea906d93576c417
Nathan Walters <nwalters512@gmail.com>
2018-12-07 20:22:45 -0600

    Add python tests to Travis

--- question-servers/freeformPythonLib/prairielearn_test.py
+++ question-servers/freeformPythonLib/prairielearn_test.py
@@ -1,12 +1,21 @@
+# lol
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../lib')))
+print(sys.path)
+
+import prairielearn as pl
 import unittest
 import lxml.html
-import prairielearn as pl
 
 class TestPrairielearnLib(unittest.TestCase):
 
   def test_inner_html(self):
-    e = lxml.html.fragment_fromstring('test')
+    e = lxml.html.fragment_fromstring('<div>test</div>')
     self.assertEqual(pl.inner_html(e), 'test')
+
+    e = lxml.html.fragment_fromstring('<div>test&gt;test</div>')
+    self.assertEqual(pl.inner_html(e), 'test&gt;test')
 
 if __name__ == '__main__':
     unittest.main()
