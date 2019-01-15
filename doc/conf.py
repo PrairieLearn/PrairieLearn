9b8a2ec8a4bc47c949e604461f26b67a56705567
Eric Huber <echuber2@illinois.edu>
2019-01-14 20:55:51 -0600

    Creating conf.py for Sphinx to detect /docs
    Trying to get Sphinx to detect docs as the documentation root instead of doc for the sake of the "Edit on GitHub" links; I believe it will automatically change the effective documentation root it understand to the location of this file, which previously existed nowhere, presumably leading to default configuration.

---
+++ doc/conf.py
@@ -0,0 +1,8 @@
+
+# Trying to get Sphinx to detect docs as the documentation root instead of doc
+# for the sake of the "Edit on GitHub" links; I believe it will automatically
+# change the effective documentation root it understand to the location of this file,
+# which previously existed nowhere, presumably leading to default configuration.
+
+useless_red_herring = "useless_red_herring"
+
