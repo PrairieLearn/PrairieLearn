e0f907da457aafc373794f7a672733452342126c
Paras Sud <sud2@illinois.edu>
2019-03-24 08:55:45 -0700

    Added python script to capture logging output when creating assessment instances

--- sprocs/get_generated_aq_ids_multiple_reps.sql
+++ sprocs/get_generated_aq_ids_multiple_reps.sql
@@ -5,6 +5,7 @@ CREATE OR REPLACE FUNCTION get_generated_aq_ids_multiple_reps(
 AS $$
 BEGIN
     FOR counter in 1..num_reps LOOP
+        RAISE NOTICE '% %% done', (100*counter/num_reps);
         RETURN QUERY SELECT get_generated_aq_ids(assessment_id_var);
     END LOOP;
 END;
