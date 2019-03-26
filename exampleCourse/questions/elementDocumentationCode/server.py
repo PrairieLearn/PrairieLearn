f3e4fcc47939b605a4c14ae39f318ab06cc85c54
James Balamuta <balamut2@illinois.edu>
2019-03-26 15:26:31 -0500

    Improve server.py documentation
    Documented imports and what correct answers are being used.

--- exampleCourse/questions/elementDocumentationCode/server.py
+++ exampleCourse/questions/elementDocumentationCode/server.py
@@ -1,12 +1,15 @@
 import prairielearn as pl
+
+# Required for symbolic input
 import sympy
 
 def generate(data):
 
-    # Inputs
+    # Fill in the Blank Inputs
     data["correct_answers"]["numericvalue"] = 3.14
     data["correct_answers"]["integervalue"] = 42
     data["correct_answers"]["stringvalue"] = "PrairieLearn"
     
+    # Symbolic
     sympy.var('x y')
     data['correct_answers']['mathexpvalue'] = pl.to_json(x + y + 1)
