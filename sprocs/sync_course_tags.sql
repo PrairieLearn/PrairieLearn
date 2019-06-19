f47f8e6b5ff7c22905eca3d680ccd93bb411f3b0
Nathan Walters <nwalters512@gmail.com>
2019-06-19 14:51:08 +0100

    Use more efficient format for transporting tags to sproc

--- sprocs/sync_course_tags.sql
+++ sprocs/sync_course_tags.sql
@@ -9,13 +9,14 @@ CREATE OR REPLACE FUNCTION
     )
 AS $$
 DECLARE
+    tag_number integer := 1;
     new_tag_id bigint;
     tag JSONB;
 BEGIN
     FOR tag IN SELECT * FROM JSONB_ARRAY_ELEMENTS(new_tags) LOOP
         INSERT INTO tags
                 (name, number, color, description, course_id)
-        VALUES (tag->>'name', (tag->>'number')::integer, tag->>'color', tag->>'description', new_course_id)
+        VALUES (tag->>0, tag_number, tag->>1, tag->>2, new_course_id)
         ON CONFLICT (name, course_id) DO UPDATE
         SET
             number = EXCLUDED.number,
@@ -24,6 +25,7 @@ BEGIN
         RETURNING id INTO new_tag_id;
 
         new_tag_ids := array_append(new_tag_ids, new_tag_id);
+        tag_number := tag_number + 1;
     END LOOP;
 
     -- Remove any tags from this course that aren't in use
--- sync/fromDisk/tags.js
+++ sync/fromDisk/tags.js
@@ -1,24 +1,7 @@
-var ERR = require('async-stacktrace');
-var _ = require('lodash');
-var async = require('async');
-
-var logger = require('../../lib/logger');
-var sqldb = require('@prairielearn/prairielib/sql-db');
-var sqlLoader = require('@prairielearn/prairielib/sql-loader');
+const sqldb = require('@prairielearn/prairielib/sql-db');
+const sqlLoader = require('@prairielearn/prairielib/sql-loader');
 
 var sql = sqlLoader.loadSqlEquiv(__filename);
-
-function asyncCallOneRow(functionName, params) {
-    return new Promise((resolve, reject) => {
-        sqldb.callOneRow(functionName, params, (err, result) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
-    });
-}
 
 function asyncQueryOneRow(sql, params) {
     return new Promise((resolve, reject) => {
@@ -45,14 +28,18 @@ function safeAsync(func, callback) {
 module.exports = { 
     sync: function(courseInfo, questionDB, callback) {
         safeAsync(async () => {
+            const start = new Date();
+
             // Aggregate all tags into a form that we can pass in one go to our sproc
+            // Since all this data will be sent over the wire, we'll send it in a
+            // compact form of an array of arrays, where each array will correspond to
+            // one tag: [tag_name, tag_color_description]
             const tags = courseInfo.tags || [];
-            const paramTags = tags.map((tag, index) => ({
-                name: tag.name,
-                number: index + 1,
-                color: tag.color,
-                description: tag.description,
-            }));
+            const paramTags = tags.map(tag => ([
+                tag.name,
+                tag.color,
+                tag.description,
+            ]));
             const tagParams = {
                 // node-postgres will try to convert to postgres arrays, so we
                 // need to explicitly serialize ourselves: see
@@ -61,17 +48,16 @@ module.exports = {
                 course_id: courseInfo.courseId,
             };
 
-            const res = await asyncQueryOneRow(sql.update_tags, tagParams);
-            console.log(res);
+            console.log(tagParams);
 
-            // We'll get back some rows, each one containing the ID of the
-            // corresponding tag in the tags array
+            const res = await asyncQueryOneRow(sql.update_tags, tagParams);
+
+            // We'll get back a single row containing an array of IDs of the tags
+            // in order.
             const tagIdsByName = res.rows[0].new_tag_ids.reduce((acc, id, index) => {
-                acc[paramTags[index].name] = id;
+                acc[paramTags[index][0]] = id;
                 return acc;
             }, {});
-
-            console.log('tagIdsByName', tagIdsByName);
 
             // Iterate over all the questions and validate that they all have
             // valid tags. As we go, build up an array of all the information that
@@ -96,96 +82,8 @@ module.exports = {
                 new_question_tags: JSON.stringify(paramQuestionTags),
             }
             await asyncQueryOneRow(sql.update_question_tags, questionTagParams);
+
+            console.log(`Elapsed: ${new Date() - start}ms`);
         }, callback);
     }
 }
-
-module.exportsss = {
-    sync: function(courseInfo, questionDB, callback) {
-        async.series([
-            function(callback) {
-                courseInfo.tags = courseInfo.tags || [];
-                var tagIds = [];
-                async.forEachOfSeries(courseInfo.tags, function(tag, i, callback) {
-                    logger.debug('Syncing tag ', tag.name);
-                    var params = {
-                        name: tag.name,
-                        number: i + 1,
-                        color: tag.color,
-                        description: tag.description,
-                        course_id: courseInfo.courseId,
-                    };
-                    sqldb.query(sql.insert_tag, params, function(err, result) {
-                        if (ERR(err, callback)) return;
-                        tag.id = result.rows[0].id;
-                        tagIds.push(tag.id);
-                        callback(null);
-                    });
-                }, function(err) {
-                    if (ERR(err, callback)) return;
-
-                    // delete tags from the DB that aren't on disk
-                    var params = {
-                        course_id: courseInfo.courseId,
-                        keep_tag_ids: tagIds,
-                    };
-                    sqldb.query(sql.delete_unused_tags, params, function(err, _result) {
-                        if (ERR(err, callback)) return;
-                        callback(null);
-                    });
-                });
-            },
-            function(callback) {
-                logger.debug('Syncing question_tags');
-                var tagsByName = _.keyBy(courseInfo.tags, 'name');
-                async.forEachOfSeries(questionDB, function(q, qid, callback) {
-                    logger.debug('Syncing tags for question ' + qid);
-                    q.tags = q.tags || [];
-                    var questionTagIds = [];
-
-                    // Figure out if any specified tags don't exist in the course
-                    const unknownTags = q.tags.filter(tag => tag in tagsByName);
-                    if (unknownTags.length > 0) {
-                        callback(new Error(`Question ${qid} has unknown tags: ${unknownTags.join(', ')}`))
-                        return;
-                    }
-
-                    async.forEachOfSeries(q.tags, function(tagName, i, callback) {
-                        if (!_(tagsByName).has(tagName)) {
-                            return callback(new Error('Question ' + qid + ', unknown tag: ' + tagName));
-                        }
-                        var params = {
-                            question_id: q.id,
-                            tag_id: tagsByName[tagName].id,
-                            number: i + 1,
-                        };
-                        sqldb.query(sql.insert_question_tag, params, function(err, result) {
-                            if (ERR(err, callback)) return;
-                            questionTagIds.push(result.rows[0].id);
-                            callback(null);
-                        });
-                    }, function(err) {
-                        if (ERR(err, callback)) return;
-
-                        // delete question tags from the DB that aren't on disk
-                        logger.debug('Deleting unused tags');
-                        var params = {
-                            question_id: q.id,
-                            keep_question_tag_ids: questionTagIds,
-                        };
-                        sqldb.query(sql.delete_unused_question_tags, params, function(err, _result) {
-                            if (ERR(err, callback)) return;
-                            callback(null);
-                        });
-                    });
-                }, function(err) {
-                    if (ERR(err, callback)) return;
-                    callback(null);
-                });
-            },
-        ], function(err) {
-            if (ERR(err, callback)) return;
-            callback(null);
-        });
-    },
-};
