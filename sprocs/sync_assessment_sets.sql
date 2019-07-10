6a965b503ec15396fe6170ca71e75a0ea92fd049
Nathan Walters <nwalters512@gmail.com>
2019-07-10 11:23:52 -0500

    Use batched insert for sync_assessment_sets sproc

--- sprocs/sync_assessment_sets.sql
+++ sprocs/sync_assessment_sets.sql
@@ -4,32 +4,28 @@ CREATE OR REPLACE FUNCTION
         IN new_course_id bigint
     ) returns void
 AS $$
-DECLARE
-    assessment_set JSONB;
 BEGIN
-    FOR assessment_set IN SELECT * FROM JSONB_ARRAY_ELEMENTS(assessment_sets) LOOP
-        -- Insert assessment set
-        INSERT INTO assessment_sets (
-            abbreviation,
-            name,
-            heading,
-            color,
-            number,
-            course_id
-        ) VALUES (
-            assessment_set->>'abbreviation',
-            assessment_set->>'name',
-            assessment_set->>'heading',
-            assessment_set->>'color',
-            (assessment_set->>'number')::integer,
-            new_course_id
-        ) ON CONFLICT (name, course_id) DO UPDATE
-        SET
-            abbreviation = EXCLUDED.abbreviation,
-            heading = EXCLUDED.heading,
-            color = EXCLUDED.color,
-            number = EXCLUDED.number;
-    END LOOP;
+    INSERT INTO assessment_sets (
+        abbreviation,
+        name,
+        heading,
+        color,
+        number,
+        course_id
+    ) SELECT
+        assessment_set->>'abbreviation',
+        assessment_set->>'name',
+        assessment_set->>'heading',
+        assessment_set->>'color',
+        (assessment_set->>'number')::integer,
+        new_course_id
+    FROM JSONB_ARRAY_ELEMENTS(sync_assessment_sets.assessment_sets) AS assessment_set
+    ON CONFLICT (name, course_id) DO UPDATE
+    SET
+        abbreviation = EXCLUDED.abbreviation,
+        heading = EXCLUDED.heading,
+        color = EXCLUDED.color,
+        number = EXCLUDED.number;
 
     -- Delete excess assessment sets
     DELETE FROM assessment_sets
