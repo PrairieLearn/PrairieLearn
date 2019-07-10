f7dd920f555bab8bd4979073dd6ee9b33f3cfb6c
Nathan Walters <nwalters512@gmail.com>
2019-07-10 01:08:27 -0500

    Add tests for question syncing

--- tests/sync/questionSync.js
+++ tests/sync/questionSync.js
@@ -1,6 +1,10 @@
-const { assert } = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const chai = require('chai');
+chai.use(chaiAsPromised);
 const util = require('./util');
 const helperDb = require('../helperDb');
+
+const { assert } = chai;
 
 describe('Question syncing', () => {
   beforeEach('set up testing database', helperDb.before);
@@ -16,5 +20,25 @@ describe('Question syncing', () => {
     const question = snapshot.questions.find(question => question.qid === 'test');
     assert.isOk(question);
     assert.isNotNull(question.deleted_at);
+  });
+
+  it('fails if same UUID is used in multiple questions', async () => {
+    const courseData = util.getCourseData();
+    courseData.questions['test2'] = courseData.questions['test'];
+    const courseDir = await util.writeCourseToTempDirectory(courseData);
+    await assert.isRejected(util.syncCourseData(courseDir));
+  });
+
+  it('allows the same UUID to be used in different courses', async () => {
+    // We'll just sync the same course from two different directories.
+    // Since courses are identified by directory, this will create two
+    // separate courses.
+    const courseData = util.getCourseData();
+    const firstDirectory = await util.writeCourseToTempDirectory(courseData);
+    const secondDirectory = await util.writeCourseToTempDirectory(courseData);
+    await util.syncCourseData(firstDirectory);
+    await util.syncCourseData(secondDirectory);
+    // No need for assertions - either sync succeeds, or it'll fail and throw
+    // an error, thus failing the test.
   });
 });
