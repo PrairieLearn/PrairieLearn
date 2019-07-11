6d075d492f8376aa3be7b717a74010ee24193e76
Nathan Walters <nwalters512@gmail.com>
2019-07-11 16:10:34 -0500

    Remove outdated tests

--- tests/testLoadCourse.js
+++ tests/testLoadCourse.js
@@ -94,121 +94,80 @@ describe('courseDB.loadFullCourse() on brokenCourse', function() {
             var filename = 'courseInstances/Fa18/assessments/quiz1/infoAssessment.json';
 
             loadHelper(courseDir, filename, assessmentJson, /invalid "set":/, callback);
-         });
+        });
 
-         it('assessment: access rule: invalid startDate should fail', function(callback) {
+        it('assessment: access rule: invalid startDate should fail', function(callback) {
 
-             var assessmentJson = {
-                 'uuid': 'bee70f4d-4220-47f1-b4ed-59c88ce08657',
-                 'type': 'Exam',
-                 'number': '1',
-                 'title': 'Test quiz 1',
-                 'set': 'Quiz',
-                 'allowAccess': [
-                     { 'startDate': 'NotADate',
-                       'endDate': '2019-01-01T00:00:00'},
-                 ],
-             };
-             var filename = 'courseInstances/Fa18/assessments/quiz1/infoAssessment.json';
+            var assessmentJson = {
+                'uuid': 'bee70f4d-4220-47f1-b4ed-59c88ce08657',
+                'type': 'Exam',
+                'number': '1',
+                'title': 'Test quiz 1',
+                'set': 'Quiz',
+                'allowAccess': [
+                    { 'startDate': 'NotADate',
+                      'endDate': '2019-01-01T00:00:00'},
+                ],
+            };
+            var filename = 'courseInstances/Fa18/assessments/quiz1/infoAssessment.json';
 
-             loadHelper(courseDir, filename, assessmentJson, /invalid allowAccess startDate/, callback);
-          });
+           loadHelper(courseDir, filename, assessmentJson, /invalid allowAccess startDate/, callback);
+        });
 
-          it('assessment: access rule: invalid endDate should fail', function(callback) {
+        it('assessment: access rule: invalid endDate should fail', function(callback) {
 
-              var assessmentJson = {
-                  'uuid': 'bee70f4d-4220-47f1-b4ed-59c88ce08657',
-                  'type': 'Exam',
-                  'number': '1',
-                  'title': 'Test quiz 1',
-                  'set': 'Quiz',
-                  'allowAccess': [
-                      { 'startDate': '2019-01-01T22:22:22',
-                        'endDate': 'AlsoReallyNotADate'},
-                  ],
-              };
-              var filename = 'courseInstances/Fa18/assessments/quiz1/infoAssessment.json';
+            var assessmentJson = {
+                'uuid': 'bee70f4d-4220-47f1-b4ed-59c88ce08657',
+                'type': 'Exam',
+                'number': '1',
+                'title': 'Test quiz 1',
+                'set': 'Quiz',
+                'allowAccess': [
+                    { 'startDate': '2019-01-01T22:22:22',
+                      'endDate': 'AlsoReallyNotADate'},
+                ],
+            };
+            var filename = 'courseInstances/Fa18/assessments/quiz1/infoAssessment.json';
 
-              loadHelper(courseDir, filename, assessmentJson, /invalid allowAccess endDate/, callback);
-           });
+            loadHelper(courseDir, filename, assessmentJson, /invalid allowAccess endDate/, callback);
+        });
 
-          it('assessment: access rule: startDate after endDate should fail', function(callback) {
+        it('assessment: access rule: startDate after endDate should fail', function(callback) {
 
-              var assessmentJson = {
-                  'uuid': 'bee70f4d-4220-47f1-b4ed-59c88ce08657',
-                  'type': 'Exam',
-                  'number': '1',
-                  'title': 'Test quiz 1',
-                  'set': 'Quiz',
-                  'allowAccess': [
-                      { 'startDate': '2020-01-01T11:11:11',
-                        'endDate': '2019-01-01T00:00:00'},
-                  ],
-              };
-              var filename = 'courseInstances/Fa18/assessments/quiz1/infoAssessment.json';
+            var assessmentJson = {
+                'uuid': 'bee70f4d-4220-47f1-b4ed-59c88ce08657',
+                'type': 'Exam',
+                'number': '1',
+                'title': 'Test quiz 1',
+                'set': 'Quiz',
+                'allowAccess': [
+                    { 'startDate': '2020-01-01T11:11:11',
+                      'endDate': '2019-01-01T00:00:00'},
+                ],
+            };
+            var filename = 'courseInstances/Fa18/assessments/quiz1/infoAssessment.json';
 
-              loadHelper(courseDir, filename, assessmentJson, /must not be after/, callback);
-           });
-
-           it('question: topic not in courseInfo should fail', function(callback) {
-
-               var questionJson = {
-                   'uuid': 'ba0b8e5b-6348-43f8-b483-083e0bea6332',
-                   'title': 'Basic V3 question',
-                   'topic': 'notARealTopic',
-                   'type': 'v3',
-               };
-               var filename = 'questions/basicV3/info.json';
-
-               loadHelper(courseDir, filename, questionJson, /invalid "topic"/, callback);
-           });
-
-           it('question: secondaryTopics not in courseInfo should fail', function(callback) {
-
-               var questionJson = {
-                   'uuid': 'ba0b8e5b-6348-43f8-b483-083e0bea6332',
-                   'title': 'Basic V3 question',
-                   'topic': 'basic',
-                   'secondaryTopics': ['basic', 'notARealTopic'],
-                   'type': 'v3',
-               };
-               var filename = 'questions/basicV3/info.json';
-
-               loadHelper(courseDir, filename, questionJson, /invalid "secondaryTopics"/, callback);
-           });
-
-           it('question: tag not in courseInfo should fail', function(callback) {
-
-               var questionJson = {
-                   'uuid': 'ba0b8e5b-6348-43f8-b483-083e0bea6332',
-                   'title': 'Basic V3 question',
-                   'topic': 'basic',
-                   'type': 'v3',
-                   'tags': ['NotARealTag'],
-               };
-               var filename = 'questions/basicV3/info.json';
-
-               loadHelper(courseDir, filename, questionJson, /invalid "tags"/, callback);
-           });
+            loadHelper(courseDir, filename, assessmentJson, /must not be after/, callback);
+        });
     });
 });
 
 var loadHelper = function(courseDir, filename, contents, expectedErrorRE, callback) {
 
     fs.writeFile(courseDir + '/' + filename, JSON.stringify(contents), function(err) {
-             if (err) { return callback(err); }
+        if (err) { return callback(err); }
 
-             courseDB.loadFullCourse(courseDir, logger, function(err, _c) {
-                 if (err) {
-                     if (expectedErrorRE.test(err)) {
-                         callback(null);
-                     } else {
-                         callback(new Error('unexpected error ' + err));
-                     }
-                 } else {
-                     callback(new Error('returned successfully, which should not happen'));
-                 }
-             });
+        courseDB.loadFullCourse(courseDir, logger, function(err, _c) {
+            if (err) {
+                if (expectedErrorRE.test(err)) {
+                    callback(null);
+                } else {
+                    callback(new Error('unexpected error ' + err));
+                }
+            } else {
+                callback(new Error('returned successfully, which should not happen'));
+            }
+        });
      });
 
 };
