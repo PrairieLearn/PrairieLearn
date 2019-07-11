132cb506a90cce6294e59dc081ee9132499c79c1
Nathan Walters <nwalters512@gmail.com>
2019-07-10 23:58:41 -0500

    Work on course instance syncing improvements

--- sync/fromDisk/courseInstances.js
+++ sync/fromDisk/courseInstances.js
@@ -11,7 +11,43 @@ var sql = sqlLoader.loadSqlEquiv(__filename);
 
 module.exports.sync = function(courseInfo, courseInstanceDB, callback) {
     safeAsync(async () => {
+        _(courseInstanceDB)
+            .groupBy('uuid')
+            .each(function(courseInstances, uuid) {
+                if (courseInstances.length > 1) {
+                    const directories = courseInstances.map(ci => ci.directory).join(', ')
+                    throw new Error(`UUID ${uuid} is used in multiple course instanced: ${directories}`);
+                }
+            });
 
+        const courseInstancesParam = Object.keys(courseInstanceDB).map(courseInstance => {
+            const accessRules = (courseInstance.allowAccess || []).map(accessRule => ({
+                role: _(accessRule).has('role') ? accessRule.role : null,
+                uids: _(accessRule).has('uids') ? accessRule.uids : null,
+                start_date: _(accessRule).has('startDate') ? accessRule.startDate : null,
+                end_date: _(accessRule).has('endDate') ? accessRule.endDate : null,
+                institution: _(accessRule).has('institution') ? accessRule.institution : 'UIUC',
+            }));
+
+            return {
+                uuid: courseInstance.uuid,
+                short_name: courseInstanceShortName,
+                long_name: courseInstance.longName,
+                number: courseInstance.number,
+                display_timezone: courseInstance.timezone || courseInfo.timezone || 'America/Chicago',
+                access_rules: accessRules,
+            };
+        });
+
+        const params = [
+            JSON.stringify(courseInstancesParam),
+            courseInfo.courseId,
+        ];
+        const syncCourseInstancesResult = await sqldb.callOneRowAsync('sync_course_instances', params);
+        const newCourseInstanceIds = syncCourseInstancesResult.rows[0].new_course_instance_ids;
+        courseInstancesParam.forEach((courseInstanceParam, index) => {
+            courseInstanceDB[courseInstanceParam.short_name].id = newCourseInstanceIds[index];
+        });
     }, callback);
 }
 
