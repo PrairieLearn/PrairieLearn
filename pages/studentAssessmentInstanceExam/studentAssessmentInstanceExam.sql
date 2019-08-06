5a1ebd7241a536b04750b12f61a33ae297ef0d25
Matthew West <mwest@illinois.edu>
2019-08-06 09:02:05 -0500

    show icon next to questions with attached files

--- pages/studentAssessmentInstanceExam/studentAssessmentInstanceExam.sql
+++ pages/studentAssessmentInstanceExam/studentAssessmentInstanceExam.sql
@@ -11,7 +11,8 @@ SELECT
     z.max_points AS zone_max_points,
     (z.max_points IS NOT NULL) AS zone_has_max_points,
     z.best_questions AS zone_best_questions,
-    (z.best_questions IS NOT NULL) AS zone_has_best_questions
+    (z.best_questions IS NOT NULL) AS zone_has_best_questions,
+    (SELECT count(*) FROM files AS f WHERE f.instance_question_id = iq.id) AS file_count
 FROM
     instance_questions AS iq
     JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
