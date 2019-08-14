5cc0f1cf37ca1ca6c339238385a3ce7d057d9d9d
Dave Mussulman <trombonekenny@users.noreply.github.com>
2019-08-14 13:27:36 -0500

    Assessment password cookie clearing fix (#1637)
    * Assessment password cookie clearing fix
    
    * Constant cookies_to_ignore

--- middlewares/clearCookies.js
+++ middlewares/clearCookies.js
@@ -1,8 +1,13 @@
-var _ = require('lodash');
+const _ = require('lodash');
+
+const cookies_to_ignore = ['pl_authn', 'pl_assessmentpw'];
 
 module.exports = function(req, res, next) {
     _(req.cookies).each(function(value, key) {
-        if (/^pl_/.test(key) && key != 'pl_authn') {
+        if (/^pl_/.test(key)) {
+            if (cookies_to_ignore.includes(key)) {
+                return;
+            }
             res.clearCookie(key);
         }
     });
