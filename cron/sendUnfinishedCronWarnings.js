017621e3246a24523e9a2045022dc827db19bd97
Matthew West <mwest@illinois.edu>
2019-08-14 09:19:46 -0500

    convert cron/sendUnfinishedCronWarnings.js to async/await

--- cron/sendUnfinishedCronWarnings.js
+++ cron/sendUnfinishedCronWarnings.js
@@ -1,4 +1,4 @@
-const ERR = require('async-stacktrace');
+const { callbackify } = require('util');
 
 const logger = require('../lib/logger');
 const opsbot = require('../lib/opsbot');
@@ -10,23 +10,20 @@ const sql = sqlLoader.loadSqlEquiv(__filename);
 module.exports = {};
 
 module.exports.run = (callback) => {
-    if (!opsbot.canSendMessages()) return callback(null);
-    sqldb.query(sql.select_unfinished_cron_jobs, [], (err, result) => {
-        if (ERR(err, callback)) return;
-
-        if (result.rowCount <= 0) return callback(null);
+    callbackify(async () => {
+        if (!opsbot.canSendMessages()) return;
+        const result = await sqldb.queryAsync(sql.select_unfinished_cron_jobs, []);
+        if (result.rowCount <= 0) return;
 
         let msg = `_Unfinished cron jobs:_\n`;
         for (const row of result.rows) {
             msg += `    *${row.name}:* started at ${row.formatted_started_at} but not finished\n`;
+            logger.error('cron:sendUnfinishedCronJobs job not finished', row);
         }
 
-        opsbot.sendMessage(msg, (err, res, body) => {
-            if (ERR(err, callback)) return;
-            if (res.statusCode != 200) {
-                logger.error('Error posting unfinished cron jobs to slack [status code ${res.statusCode}]', body);
-            }
-            callback(null);
-        });
-    });
+        const [res, body] = await opsbot.sendMessageAsync(msg);
+        if (res.statusCode != 200) {
+            logger.error(`Error posting unfinished cron jobs to slack [status code ${res.statusCode}]`, {res, body});
+        }
+    })(callback);
 };
