b999c3d46fd1d3f601fc6346c602c7d6b8db7f8a
James J Balamuta <balamut2@illinois.edu>
2019-08-31 18:42:40 -0500

    pl-variable-output: mathematica

--- elements/pl-variable-output/pl-variable-output.py
+++ elements/pl-variable-output/pl-variable-output.py
@@ -6,6 +6,7 @@ import chevron
 
 DIGITS_DEFAULT = 2
 SHOW_MATLAB_DEFAULT = True
+SHOW_MATHEMATICA_DEFAULT = True
 def prepare(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     required_attribs = []
@@ -15,11 +16,11 @@ def prepare(element_html, data):
 
 def render(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
-    show_mathematica = pl.get_boolean_attrib(element, 'show-mathematica', True)
     show_python = pl.get_boolean_attrib(element, 'show-python', True)
     default_tab = pl.get_string_attrib(element, 'default-tab', 'matlab')
     digits = pl.get_integer_attrib(element, 'digits', DIGITS_DEFAULT)
     show_matlab = pl.get_boolean_attrib(element, 'show-matlab', SHOW_MATLAB_DEFAULT)
+    show_mathematica = pl.get_boolean_attrib(element, 'show-mathematica', SHOW_MATHEMATICA_DEFAULT)
 
     tab_list = ['matlab', 'mathematica', 'python']
     if default_tab not in tab_list:
