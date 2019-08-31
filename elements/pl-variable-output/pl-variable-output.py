a7ea1e42f4aac2aa987e0c7b818a452604c79087
James J Balamuta <balamut2@illinois.edu>
2019-08-31 18:42:22 -0500

    pl-variable-output: digits

--- elements/pl-variable-output/pl-variable-output.py
+++ elements/pl-variable-output/pl-variable-output.py
@@ -4,6 +4,7 @@ import numpy as np
 import chevron
 
 
+DIGITS_DEFAULT = 2
 def prepare(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     required_attribs = []
@@ -13,11 +14,11 @@ def prepare(element_html, data):
 
 def render(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
-    digits = pl.get_integer_attrib(element, 'digits', 2)
     show_matlab = pl.get_boolean_attrib(element, 'show-matlab', True)
     show_mathematica = pl.get_boolean_attrib(element, 'show-mathematica', True)
     show_python = pl.get_boolean_attrib(element, 'show-python', True)
     default_tab = pl.get_string_attrib(element, 'default-tab', 'matlab')
+    digits = pl.get_integer_attrib(element, 'digits', DIGITS_DEFAULT)
 
     tab_list = ['matlab', 'mathematica', 'python']
     if default_tab not in tab_list:
