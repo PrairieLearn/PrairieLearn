5753538c28bda1466a7f8e6a8e9cb6641568a2e4
James J Balamuta <balamut2@illinois.edu>
2019-08-31 17:31:26 -0500

    pl-multiple-choice: inline

--- elements/pl-multiple-choice/pl-multiple-choice.py
+++ elements/pl-multiple-choice/pl-multiple-choice.py
@@ -4,6 +4,8 @@ import random
 import math
 
 WEIGHT_DEFAULT = 1
+INLINE_DEFAULT = False
+
 
 def prepare(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
@@ -75,7 +77,7 @@ def render(element_html, data):
     name = pl.get_string_attrib(element, 'answers-name')
 
     answers = data['params'].get(name, [])
-    inline = pl.get_boolean_attrib(element, 'inline', False)
+    inline = pl.get_boolean_attrib(element, 'inline', INLINE_DEFAULT)
 
     submitted_key = data['submitted_answers'].get(name, None)
     correct_key = data['correct_answers'].get(name, {'key': None}).get('key', None)
