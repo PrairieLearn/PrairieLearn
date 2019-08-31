7c1d7ca178fabcc1a1deb8e9749a99936228cbc9
James J Balamuta <balamut2@illinois.edu>
2019-08-31 17:23:53 -0500

    pl-matrix-input: weight

--- elements/pl-matrix-input/pl-matrix-input.py
+++ elements/pl-matrix-input/pl-matrix-input.py
@@ -7,6 +7,7 @@ import math
 import chevron
 
 
+WEIGHT_DEFAULT = 1
 def prepare(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     required_attribs = ['answers-name']
@@ -215,7 +216,7 @@ def grade(element_html, data):
     name = pl.get_string_attrib(element, 'answers-name')
 
     # Get weight
-    weight = pl.get_integer_attrib(element, 'weight', 1)
+    weight = pl.get_integer_attrib(element, 'weight', WEIGHT_DEFAULT)
 
     # Get true answer (if it does not exist, create no grade - leave it
     # up to the question code)
@@ -270,7 +271,7 @@ def grade(element_html, data):
 def test(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     name = pl.get_string_attrib(element, 'answers-name')
-    weight = pl.get_integer_attrib(element, 'weight', 1)
+    weight = pl.get_integer_attrib(element, 'weight', WEIGHT_DEFAULT)
 
     # Get correct answer
     a_tru = data['correct_answers'][name]
