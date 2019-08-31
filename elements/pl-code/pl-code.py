28b604856afe81363bd9f9a97758cf9430a87d82
James J Balamuta <balamut2@illinois.edu>
2019-08-31 16:39:55 -0500

    pl-code: highlight-lines-color

--- elements/pl-code/pl-code.py
+++ elements/pl-code/pl-code.py
@@ -9,6 +9,8 @@ NO_HIGHLIGHT_DEFAULT = False
 SOURCE_FILE_NAME_DEFAULT = None
 PREVENT_SELECT_DEFAULT = False
 HIGHLIGHT_LINES_DEFAULT = None
+HIGHLIGHT_LINES_COLOR_DEFAULT = '#b3d7ff'
+
 allowed_languages = [
     'armasm',
     'bash',
@@ -128,10 +130,10 @@ def render(element_html, data):
     language = pl.get_string_attrib(element, 'language', LANGUAGE_DEFAULT)
     no_highlight = pl.get_boolean_attrib(element, 'no-highlight', NO_HIGHLIGHT_DEFAULT)
     specify_language = (language is not None) and (not no_highlight)
-    highlight_lines_color = pl.get_string_attrib(element, 'highlight-lines-color', '#b3d7ff')
     source_file_name = pl.get_string_attrib(element, 'source-file-name', SOURCE_FILE_NAME_DEFAULT)
     prevent_select = pl.get_boolean_attrib(element, 'prevent-select', PREVENT_SELECT_DEFAULT)
     highlight_lines = pl.get_string_attrib(element, 'highlight-lines', HIGHLIGHT_LINES_DEFAULT)
+    highlight_lines_color = pl.get_string_attrib(element, 'highlight-lines-color', HIGHLIGHT_LINES_COLOR_DEFAULT)
 
     if source_file_name is not None:
         base_path = data['options']['question_path']
