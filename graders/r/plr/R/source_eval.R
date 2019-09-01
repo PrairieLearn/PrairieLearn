3e07400ebdd43aee1bc6183c35f160332229d956
Dirk Eddelbuettel <edd@debian.org>
2019-09-01 10:01:27 -0500

    release 0.0.2 with new helper and NULL as default for uid

--- graders/r/plr/R/source_eval.R
+++ graders/r/plr/R/source_eval.R
@@ -6,27 +6,35 @@
 #' removing group and others the rights to read (or write or execute). We therefore
 #' also \code{chmod} the supplied file back to mode \dQuote{0644} ensuring that
 #' the file can be read so that the expression can be evaluated---but nothing else
-#' should be in reach,
+#' should be in reach.
+#'
+#' The \code{eval_safe_as} convenience function fetches the (numeric) user id
+#' before calling \code{unix::eval_safe}; it is equivalent to \code{source_and_eval_safe}
+#' but does not involve a file.
+#'
+#' Note that you must run these functions as the \sQuote{root} user in order to set the
+#' uid.
 #'
 #' @param file A filename with an R file to be source, typically containing
-#' the student code to be evaluated safely
+#' the student code to be evaluated safely.
 #' @param expr An expression to be evaluate by \code{\link[unix]{eval_safe}},
 #' typically the name of the sane of the function containing the student code
-#' plus the argument supplied from the test runner
-#' @param uid Numeric or character user id identifying the user id with (presumably
-#' ower #' privileges) as which the code is running; the numeric uid is obtained
-#' via \code{\link[unix]{user_info}} is a character is supplied
+#' plus the argument supplied from the test runner.
+#' @param uid Optional numeric or character user id identifying the user id with
+#' (presumably lower) privileges as which the code is running; the numeric uid
+#' is obtained via \code{\link[unix]{user_info}} is a character is supplied.
+#' Note that using this argument requires being the \sQuote{root} user.
 #'
 #' @return A value of the \code{expr} sourced from \code{file} and evaluated by
-#' \code{uid}, or NULL in case of error
+#' \code{uid}, or NULL in case of error.
 #'
 #' @examples
 #' \dontrun{
 #' n <- sample(3:20, 1)         # random payload
 #' res <- source_and_eval_safe("code/fib.R", fib(n), "ag")
 #' }
-source_and_eval_safe <- function(file, expr, uid) {
-    if (class(uid) == "character") uid <- user_info(uid)$uid
+source_and_eval_safe <- function(file, expr, uid=NULL) {
+    if (!is.null(uid) && class(uid) == "character") uid <- user_info(uid)$uid
 
     if (!file.exists(file)) return(invisible(NULL))
 
@@ -34,8 +42,14 @@ source_and_eval_safe <- function(file, expr, uid) {
     Sys.chmod(file, mode="0664")
     source(file)
 
-    res <- eval_safe(expr, uid)
+    res <- eval_safe(expr, uid=uid)
     Sys.chmod(file, mode=oldmode)
 
     res
+}
+
+#' @rdname source_and_eval_safe
+eval_safe_as <- function(expr, uid=NULL) {
+     if (!is.null(uid) && class(uid) == "character") uid <- user_info(uid)$uid
+    res <- eval_safe(expr, uid=uid)
 }
--- graders/r/plr/inst/tinytest/test_eval.R
+++ graders/r/plr/inst/tinytest/test_eval.R
@@ -1,8 +1,11 @@
 library(plr)
 
-filewithpath <- system.file("tinytest", "files", "fib.R", package="plr")
-#res <- source_and_eval_safe(filewithpath, fib(10), "nobody")
-## TODO Apparently we cannot test a file with 'source'
+## note that we do not set a uid here -- only root can use that argument
+## but we cannot assume the test is running as root (not should it)
 
-res <- 55
+filewithpath <- system.file("tinytest", "files", "fib.R", package="plr")
+res <- source_and_eval_safe(filewithpath, fib(10))
 expect_equal(res, 55)
+
+res <- eval_safe_as(21 + 21)
+expect_equal(res, 42)
