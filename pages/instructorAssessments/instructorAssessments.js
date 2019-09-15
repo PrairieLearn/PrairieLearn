2da21e0a4ab7d88e56f57dc567e88c25baa9c946
Timothy Bretl <tbretl@illinois.edu>
2019-09-14 23:08:11 -0500

    enable add assessment

--- pages/instructorAssessments/instructorAssessments.js
+++ pages/instructorAssessments/instructorAssessments.js
@@ -10,6 +10,15 @@ const sanitizeName = require('../../lib/sanitize-name');
 const sqldb = require('@prairielearn/prairielib/sql-db');
 const sqlLoader = require('@prairielearn/prairielib/sql-loader');
 
+const async = require('async');
+const error = require('@prairielearn/prairielib/error');
+const debug = require('debug')('prairielearn:instructorAssessments');
+const fs = require('fs-extra');
+const path = require('path');
+const uuidv4 = require('uuid/v4');
+const logger = require('../../lib/logger');
+const editHelpers = require('../shared/editHelpers');
+
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
 const csvFilename = (locals) => {
@@ -109,5 +118,110 @@ router.get('/:filename', function(req, res, next) {
         next(new Error('Unknown filename: ' + req.params.filename));
     }
 });
+
+router.post('/', (req, res, next) => {
+    debug(`Responding to post with action ${req.body.__action}`);
+    if (req.body.__action == 'add_assessment') {
+        debug(`Responding to action add_assessment`);
+
+        if (!res.locals.authz_data.has_course_permission_edit) return next(new Error('Access denied'));
+
+        // Do not allow users to edit the exampleCourse
+        if (res.locals.course.options.isExampleCourse) {
+            return next(error.make(400, `attempting to edit example course`, {
+                locals: res.locals,
+                body: req.body,
+            }));
+        }
+
+        let edit = {
+            userID: res.locals.user.user_id,
+            courseID: res.locals.course.id,
+            coursePath: res.locals.course.path,
+            courseInstanceID: res.locals.course_instance.id,
+            courseInstancePath: path.join(res.locals.course.path, 'courseInstances', res.locals.course_instance.short_name),
+            uid: res.locals.user.uid,
+            user_name: res.locals.user.name,
+        };
+
+        edit.description = 'Add assessment in browser and sync';
+        edit.write = add_write;
+        editHelpers.doEdit(edit, res.locals, (err, job_sequence_id) => {
+            if (ERR(err, (e) => logger.error(e))) {
+                res.redirect(res.locals.urlPrefix + '/edit_error/' + job_sequence_id);
+            } else {
+                debug(`Get assessment_id from tid=${edit.tid} with course_instance_id=${edit.courseInstanceID}`);
+                sqldb.queryOneRow(sql.select_assessment_id_from_tid, {tid: edit.tid, course_instance_id: edit.courseInstanceID}, (err, result) => {
+                    if (ERR(err, next)) return;
+                    res.redirect(res.locals.urlPrefix + '/assessment/' + result.rows[0].assessment_id);
+                });
+            }
+        });
+    } else {
+        next(error.make(400, 'unknown __action: ' + req.body.__action, {
+            locals: res.locals,
+            body: req.body,
+        }));
+    }
+});
+
+function add_write(edit, callback) {
+    const assessmentsPath = path.join(edit.courseInstancePath, 'assessments');
+    async.series([
+        (callback) => {
+            debug(`Generate unique TID in ${assessmentsPath}`);
+            fs.readdir(assessmentsPath, (err, filenames) => {
+                if (ERR(err, callback)) return;
+
+                let number = 1;
+                filenames.forEach((filename) => {
+                    let found = filename.match(/^HW([0-9]+)$/);
+                    if (found) {
+                        const foundNumber = parseInt(found[1]);
+                        if (foundNumber >= number) {
+                            number = foundNumber + 1;
+                        }
+                    }
+                });
+
+                edit.tid = `HW${number}`;
+                edit.assessmentNumber = number,
+                edit.assessmentPath = path.join(assessmentsPath, edit.tid);
+                edit.pathsToAdd = [
+                    edit.assessmentPath,
+                ];
+                edit.commitMessage = `in-browser edit: add assessment ${edit.tid}`;
+                callback(null);
+            });
+        },
+        (callback) => {
+            debug(`Write infoAssessment.json`);
+
+            // "number" may not be unique - that's ok, the user can change it later -
+            // what's important is that "tid" is unique (see above), because that's a
+            // directory name
+            infoJson = {
+                uuid: uuidv4(),
+                type: 'Homework',
+                title: 'Replace this title',
+                set: 'Homework',
+                number: `${edit.assessmentNumber}`,
+                allowAccess: [],
+                zones: [],
+            };
+
+            // We use outputJson to create the directory edit.assessmentsPath if it
+            // does not exist (which it shouldn't). We use the file system flag 'wx'
+            // to throw an error if edit.assessmentPath already exists.
+            fs.outputJson(path.join(edit.assessmentPath, 'infoAssessment.json'), infoJson, {spaces: 4, flag: 'wx'}, (err) => {
+                if (ERR(err, callback)) return;
+                callback(null);
+            });
+        },
+    ], (err) => {
+        if (ERR(err, callback)) return;
+        callback(null);
+    });
+}
 
 module.exports = router;
