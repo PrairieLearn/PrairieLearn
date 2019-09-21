b90dccc75f5811cf4fc42dfe7eedf669ad630147
Nicolas Nytko <nicknytko@gmail.com>
2019-09-20 21:59:56 -0500

    make the linter happy

--- elements/pl-code-output/pl-code-output.py
+++ elements/pl-code-output/pl-code-output.py
@@ -1,30 +1,24 @@
 import prairielearn as pl
 import lxml.html
-import chevron
-import json
-import numpy as np
-import numpy.linalg as la
 import pandas as pd
-import math
-import os
-import itertools
 
 
 def prepare(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     pl.check_attribs(element, required_attribs=['variable-name'], optional_attribs=['variable-type', 'no-highlight', 'variable-prefix', 'variable-suffix'])
 
+
 def render(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     vartype = pl.get_string_attrib(element, 'variable-type', "text")
     varname = pl.get_string_attrib(element, 'variable-name')
 
-    if not varname in data['params']:
+    if varname not in data['params']:
         raise Exception("Could not find {} in params!".format(varname))
 
     varout = data['params'][varname]
     html = ""
-    
+
     # render the output variable
     if vartype == "dataframe":
         varout = pd.read_json(varout)
@@ -39,5 +33,5 @@ def render(element_html, data):
         html += "<pl-code language='python' no-highlight='{}'>{}</pl-code>".format(no_highlight, text)
     else:
         raise Exception("{} is not a valid variable type!".format(vartype))
-        
+
     return html
