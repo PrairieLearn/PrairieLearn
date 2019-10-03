8d0198f02942f7066d56f6d3cde403e952d21992
James J Balamuta <balamut2@illinois.edu>
2019-10-03 09:44:58 -0500

    Add question on adding two numbers with R

---
+++ exampleCourse/questions/examplesRAddTwoNumbers/server.py
@@ -0,0 +1,26 @@
+# Access R using rpy2
+import rpy2.robjects as robjects
+
+def generate(data):
+
+    # Sample two random integers between 5 and 10 (inclusive)
+    values = robjects.r(
+      '''
+       x = runif(2, 5, 10)
+       x = floor(x)
+       x
+      '''
+    )
+    # Cast to R
+    a = values[0] + 0
+    b = values[1] + 0
+
+    # Put these two integers into data['params']
+    data['params']['a'] = a
+    data['params']['b'] = b
+
+    # Compute the sum of these two integers
+    c = a + b
+
+    # Put the sum into data['correct_answers']
+    data['correct_answers']['c'] = c
