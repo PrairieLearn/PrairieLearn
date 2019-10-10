8312a43eb2b5b704e32833432749cf62cd63b20b
Matthew West <mwest@illinois.edu>
2019-10-10 08:09:36 -0500

    drop/add constraint in a single transaction

--- migrations/151_assessments__assessment_set_id_fkey__alter.sql
+++ migrations/151_assessments__assessment_set_id_fkey__alter.sql
@@ -1,2 +1,3 @@
-ALTER TABLE assessments DROP CONSTRAINT assessments_assessment_set_id_fkey;
-ALTER TABLE assessments ADD CONSTRAINT assessments_assessment_set_id_fkey FOREIGN KEY (assessment_set_id) REFERENCES assessment_sets(id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE assessments
+    DROP CONSTRAINT assessments_assessment_set_id_fkey,
+    ADD CONSTRAINT assessments_assessment_set_id_fkey FOREIGN KEY (assessment_set_id) REFERENCES assessment_sets(id) ON UPDATE CASCADE ON DELETE CASCADE;
