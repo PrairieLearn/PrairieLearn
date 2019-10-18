4f600ca8d5938fb469ffbb8f33921e21c23ce43b
James J Balamuta <balamut2@illinois.edu>
2019-10-18 11:28:45 -0500

    Build examples into the overarching pl-element doc example question

--- exampleCourse/questions/examplesElementCodeDocumentation/server.py
+++ exampleCourse/questions/examplesElementCodeDocumentation/server.py
@@ -6,6 +6,9 @@ import sympy
 # Required for matrix input
 import numpy as np
 
+# Requried for python variable
+import pandas as pd
+
 def generate(data):
 
     # Fill in the Blank Inputs
@@ -29,6 +32,18 @@ def generate(data):
     # Output elements
     data['params']['matrixC'] = pl.to_json(np.matrix('5 6; 7 8'))
     data['params']['matrixD'] = pl.to_json(np.matrix('-1 4; 3 2'))
-
-
     
+    # Display python variable contents
+    data_dictionary = { 'a': 1, 'b': 2, 'c': 3 }
+    data['params']['data_dictionary'] = pl.to_json(data_dictionary)
+
+    # Display a pandas data frame
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    df = pd.DataFrame(data=d)
+    data['params']['df'] = pl.to_json(df)
+
+    # Display a dynamically generated graph
+    mat = np.random.random((3, 3))
+    mat = mat / np.linalg.norm(mat, 1, axis=0)
+    data['params']['labels'] = pl.to_json(['A', 'B', 'C'])
+    data['params']['matrix'] = pl.to_json(mat)
