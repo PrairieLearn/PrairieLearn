9153314985bb1b68674bf8d52167c869a85dd6f6
Timothy Bretl <tbretl@illinois.edu>
2019-12-13 09:24:22 -0600

    selectCourses checks has_course_permission_edit

--- middlewares/selectCourses.sql
+++ middlewares/selectCourses.sql
@@ -1,23 +1,11 @@
 -- BLOCK select_courses
-WITH
-    course_permissions_for_user AS (
-        SELECT
-            *
-        FROM
-            course_permissions AS cp
-        WHERE
-            cp.user_id = $user_id
-    )
 SELECT
     c.*
 FROM
     pl_courses AS c
-    LEFT JOIN course_permissions_for_user AS cp ON (cp.course_id = c.id)
+    JOIN authz_course($user_id, c.id, $is_administrator) AS permissions_course ON TRUE
 WHERE
     c.deleted_at IS NULL
-    AND (
-        $is_administrator
-        OR (cp.id IS NOT NULL)
-    )
+    AND (permissions_course->>'has_course_permission_edit')::BOOLEAN IS TRUE
 ORDER BY
     c.short_name, c.title, c.id;
