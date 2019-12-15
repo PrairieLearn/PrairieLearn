6ee89730f935a65b471ce6a1dc624858a0abc76c
Timothy Bretl <tbretl@illinois.edu>
2019-12-14 22:16:49 -0600

    style

--- pages/instructorAssessmentOverview/instructorAssessmentOverview.js
+++ pages/instructorAssessmentOverview/instructorAssessmentOverview.js
@@ -12,7 +12,7 @@ const sql = sqlLoader.loadSqlEquiv(__filename);
 
 const error = require('@prairielearn/prairielib/error');
 const logger = require('../../lib/logger');
-const {AssessmentCopyEditor, AssessmentRenameEditor, AssessmentDeleteEditor} = require('../shared/editHelpers');
+const { AssessmentCopyEditor, AssessmentRenameEditor, AssessmentDeleteEditor } = require('../shared/editHelpers');
 
 router.get('/', function(req, res, next) {
     debug('GET /');
