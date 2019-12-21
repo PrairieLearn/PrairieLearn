428494f9041a083c8375e1ba9b87febc3cd2a2f7
Timothy Bretl <tbretl@illinois.edu>
2019-12-20 21:30:22 -0600

    get course instance by uuid and not short_name

--- pages/instructorInstanceAdminSettings/instructorInstanceAdminSettings.js
+++ pages/instructorInstanceAdminSettings/instructorInstanceAdminSettings.js
@@ -57,8 +57,11 @@ router.post('/', function(req, res, next) {
                 if (ERR(err, (e) => logger.error(e))) {
                     res.redirect(res.locals.urlPrefix + '/edit_error/' + job_sequence_id);
                 } else {
-                    debug(`Get course_instance_id from ciid=${editor.short_name} with course_id=${res.locals.course.id}`);
-                    sqldb.queryOneRow(sql.select_course_instance_id_from_short_name, {short_name: editor.short_name, course_id: res.locals.course.id}, (err, result) => {
+                    debug(`Get course_instance_id from uuid=${editor.uuid} with course_id=${res.locals.course.id}`);
+                    // Normally, it would not be safe to get an id from a string in a SQL query. The reason it is safe
+                    // in this context is that the uuid was created by doEdit and is known to be valid. Otherwise we would
+                    // be leaving ourselves open to SQL injection.
+                    sqldb.queryOneRow(sql.select_course_instance_id_from_uuid, {uuid: editor.uuid, course_id: res.locals.course.id}, (err, result) => {
                         if (ERR(err, next)) return;
                         res.redirect(res.locals.plainUrlPrefix + '/course_instance/' + result.rows[0].course_instance_id + '/instructor/instance_admin/settings');
                     });
--- pages/instructorInstanceAdminSettings/instructorInstanceAdminSettings.sql
+++ pages/instructorInstanceAdminSettings/instructorInstanceAdminSettings.sql
@@ -16,12 +16,13 @@ WHERE
     ci.course_id = $course_id
     AND ci.deleted_at IS NULL;
 
--- BLOCK select_course_instance_id_from_short_name
+-- BLOCK select_course_instance_id_from_uuid
 SELECT
     ci.id AS course_instance_id
 FROM
     course_instances AS ci
 WHERE
-    ci.short_name = $short_name
+    /* vulnerable to injection attack unless we know the uuid is valid */
+    ci.uuid = $uuid
     AND ci.course_id = $course_id
     AND ci.deleted_at IS NULL;
