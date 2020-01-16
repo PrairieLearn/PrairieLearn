f642257cff33daf18dbf71f320d0ca47a01de990
Nicolas Nytko <nicknytko@gmail.com>
2020-01-16 12:04:45 -0600

    add basic demo question

---
+++ exampleCourse/questions/demoAutograderSquare/server.py
@@ -0,0 +1,20 @@
+import numpy as np
+import prairielearn as pl
+import json
+
+
+def generate(data):
+
+    # Define the variables here
+    names_for_user = [
+        {"name": "x", "description": "Description of the variable", "type": "float"}
+    ]
+    names_from_user = [
+        {"name": "x_sq",
+            "description": "The square of $x$", "type": "float"}
+    ]
+
+    data["params"]["names_for_user"] = names_for_user
+    data["params"]["names_from_user"] = names_from_user
+
+    return data
---
+++ exampleCourse/questions/demoAutograderSquare/tests/test.py
@@ -0,0 +1,16 @@
+from pl_helpers import name, points, not_repeated
+from pl_unit_test import PrairieLearnTestCaseWithPlot, PrairieLearnTestCase
+from code_feedback import Feedback as feedback
+from functools import wraps
+
+
+class Test(PrairieLearnTestCaseWithPlot):
+
+
+    @points(10)
+    @name("x_sq")
+    def test_0(self):
+        points = 0
+        if feedback.check_scalar('x_sq', self.ref.x_sq, self.st.x_sq, accuracy_critical=False):
+            points += 1
+        feedback.set_points(points)
