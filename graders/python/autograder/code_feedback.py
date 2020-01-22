b774429a6d0a7dea614acf94e18e0f462f5433e3
Nicolas Nytko <nicknytko@gmail.com>
2020-01-21 20:56:30 -0600

    start docs

--- graders/python/autograder/code_feedback.py
+++ graders/python/autograder/code_feedback.py
@@ -57,6 +57,11 @@ class Feedback:
 
     @classmethod
     def add_feedback(cls, text):
+        """
+        Feedback.add_feedback(text)
+
+        Adds some text to the feedback output for the current test.
+        """
         with open("/grade/run/feedback_" + cls.test_name + ".txt", 'a+',
                   encoding='utf-8') as f:
             f.write(cls.buffer + text)
@@ -65,11 +70,28 @@ class Feedback:
 
     @classmethod
     def finish(cls, fb_text):
+        """
+        Feedback.finish(fb_text)
+        
+        Complete grading immediately, additionally outputting the message in fb_text.
+        """
         cls.add_feedback(fb_text)
         raise GradingComplete()
 
     @classmethod
     def check_numpy_array_sanity(cls, name, num_axes, data):
+        """
+        Feedback.check_numpy_array_sanity(name, num_axes, data)
+
+        Perform a sanity check on a NumPy array, making sure that it is
+        in fact defined and has the correct dimensionality.  If the checks
+        fail then grading will automatically stop.
+
+        - name: Name of the array that is being checked.  This will be used to give feedback.
+        - num_axes: Number of axes that the array should have.
+        - data: NumPy array to check.
+        """
+        
         import numpy as np
         if data is None:
             cls.finish("'%s' is None or not defined" % name)
@@ -96,6 +118,19 @@ class Feedback:
     def check_numpy_array_features(cls, name, ref, data,
                                    accuracy_critical=True,
                                    report_failure=True):
+        """
+        Feedback.check_numpy_array_features(name, ref, data)
+
+        Check that a student NumPy array has the same shape and datatype as a 
+        reference solution NumPy array.
+
+        - name: Name of the array that is being checked.  This will be used to give feedback.
+        - ref: Reference NumPy array.
+        - data: Student NumPy array to be checked.  Do not mix this up with the previous array!
+          This argument is subject to more strict type checking.
+        - accuracy_critical: If true, grading will halt on failure.
+        - report_failure: If true, feedback will be given on failure.
+        """
         import numpy as np
 
         def bad(msg):
@@ -136,6 +171,24 @@ class Feedback:
                                    accuracy_critical=True, rtol=1e-05,
                                    atol=1e-08, report_success=True,
                                    report_failure=True):
+        """
+        Feedback.check_numpy_allclose(name, ref, data)
+
+        Check that a student NumPy array has similar values to a reference NumPy array.
+        Note that this checks value according to the numpy.allclose function, which goes 
+        by the following check:
+          absolute(a - b) <= (atol + rtol * absolute(b))
+
+        - name: Name of the array that is being checked.  This will be used to give feedback.
+        - ref: Reference NumPy array.
+        - data: Student NumPy array to be checked.  Do not mix this up with the previous array!
+          This argument is subject to more strict type checking.
+        - rtol: Maximum relative tolerance between values.
+        - atol: Maximum absolute tolerance between values.
+        - accuracy_critical: If true, grading will halt on failure.
+        - report_failure: If true, feedback will be given on failure.
+        """
+        
         import numpy as np
 
         if not cls.check_numpy_array_features(name, ref, data,
@@ -158,9 +211,25 @@ class Feedback:
         return good
 
     @classmethod
-    def check_list_features(cls, name, ref, data, entry_type=None,
+    def check_list(cls, name, ref, data, entry_type=None,
                             accuracy_critical=True,
                             report_failure=True):
+        """
+        Feedback.check_list(name, ref, data)
+
+        Check that a student list has correct length with respect to a reference
+        list.  Can also check for a homogeneous data type for the list.
+
+        - name: Name of the list that is being checked.  This will be used to give feedback.
+        - ref: Reference list.
+        - data: Student list to be checked.  Do not mix this up with the previous list!
+          This argument is subject to more strict type checking.
+        - entry_type: If not None, requires that each element in the student solution be
+          of this type.
+        - accuracy_critical: If true, grading will halt on failure.
+        - report_failure: If true, feedback will be given on failure.
+        """
+        
         def bad(msg):
             if report_failure:
                 cls.add_feedback(msg)
@@ -187,27 +256,23 @@ class Feedback:
         return True
 
     @classmethod
-    def check_list(cls, name, ref, data, entry_type=None):
-
-        if data is None:
-            cls.finish("'%s' is None or not defined" % name)
-
-        if not isinstance(data, list):
-            cls.finish("'%s' is not a list" % name)
-
-        if len(ref) != len(data):
-            cls.finish("'%s' has the wrong length--expected %d, got %d"
-                       % (name, len(ref), len(data)))
-
-        if entry_type is not None:
-            for i, entry in enumerate(data):
-                if not isinstance(entry, entry_type):
-                    cls.finish("'%s[%d]' has the wrong type" % (name, i))
-
-    @classmethod
     def check_tuple(cls, name, ref, data, accuracy_critical=True,
                     report_failure=True, report_success=True):
+        """
+        Feedback.check_tuple(name, ref, data)
 
+        Check that a student tuple has correct length with respect to a reference
+        tuple, and same values. 
+
+        - name: Name of the tuple that is being checked.  This will be used to give feedback.
+        - ref: Reference tuple.
+        - data: Student tuple to be checked.  Do not mix this up with the previous tuple!
+          This argument is subject to more strict type checking.
+        - accuracy_critical: If true, grading will halt on failure.
+        - report_failure: If true, feedback will be given on failure.
+        - report_success: If true, feedback will be given on success.
+        """
+        
         def bad(msg):
             if report_failure:
                 cls.add_feedback(msg)
@@ -249,6 +314,29 @@ class Feedback:
     def check_scalar(cls, name, ref, data, accuracy_critical=True,
                      rtol=1e-5, atol=1e-8, report_success=True,
                      report_failure=True):
+        """
+        Feedback.check_scalar(name, ref, data)
+
+        Check that a student scalar has correct value with respect to a reference scalar.
+        This will mark a value as correct if it passes any of the following checks:
+
+        abs(ref - data) < ref(ref) * rtol
+        abs(ref - data) < atol
+
+        One of rtol or atol can be omitted (set to None) if that check is unwanted. 
+        Or both, but then nothing would be graded :)
+
+        - name: Name of the scalar that is being checked.  This will be used to give feedback.
+        - ref: Reference scalar.
+        - data: Student scalar to be checked.  Do not mix this up with the previous value!
+          This argument is subject to more strict type checking.
+        - accuracy_critical: If true, grading will halt on failure.
+        - rtol: Maximum relative tolerance.
+        - atol: Maximum absolute tolerance.
+        - report_failure: If true, feedback will be given on failure.
+        - report_success: If true, feedback will be given on success.
+        """
+        
         import numpy as np
 
         def bad(msg):
@@ -290,6 +378,16 @@ class Feedback:
 
     @classmethod
     def call_user(cls, f, *args, **kwargs):
+        """
+        Feedback.call_user(f)
+
+        Attempts to call a student defined function, with any arbitrary arguments specified
+        in *args and **kwargs.  If the student code raises an exception, this will be caught
+        and user feedback will be given.
+
+        If the function call succeeds, the user return value will be returned from this function. 
+        """
+        
         try:
             return f(*args, **kwargs)
         except Exception:
@@ -320,17 +418,43 @@ class Feedback:
             raise GradingComplete()
 
     @classmethod
-    def check_plot(cls, name, ref, plot, check_axes_scale=None):
+    def check_plot(cls, name, ref, plot, check_axes_scale=None,
+                   accuracy_critical=False, report_failure=True,
+                   report_success=True):
+        """
+        Feedback.check_plot(name, ref, plot, check_axes_scale)
+
+        Checks that a student plot has the same lines as a reference plot solution.
+        Can optionally check the axis scales to ensure they are the same as the reference.
+
+        - name: Name of plot scalar that is being checked.  This will be used to give feedback.
+        - ref: Reference plot.
+        - data: Student plot to be checked.  Do not mix this up with the previous value!
+          This argument is subject to more strict type checking.
+        - check_axes_scale: One of None, 'x', 'y', or 'xy'
+          Signals which axis scale should be checked against the reference solution.
+        - accuracy_critical: If true, grading will halt on failure.
+        - report_failure: If true, feedback will be given on failure.
+        - report_success: If true, feedback will be given on success.
+        """
+        
         import numpy as np
         import matplotlib.axes
 
+        def bad(msg):
+            if report_failure:
+                cls.add_feedback(msg)
+
+            if accuracy_critical:
+                cls.finish('')
+            else:
+                return False
+        
         if plot is None:
-            cls.add_feedback("'%s' is None or not defined" % name)
-            return False
+            return bad("'%s' is None or not defined" % name)
 
         if not isinstance(plot, matplotlib.axes.Axes):
-            cls.add_feedback("'%s' is not an object of matplotlib axes" % name)
-            return False
+            return bad("'%s' is not an object of matplotlib axes" % name)
 
         # check_axes_scale can be None, 'x', 'y', or 'xy'
         if check_axes_scale:
@@ -349,22 +473,18 @@ class Feedback:
                     scales_match = False
 
             if not scales_match:
-                cls.add_feedback(("'%s' does not have the correct"
-                                  " scale for its axes" % name))
-                return False
+                return bad("'%s' does not have the correct scale for its axes" % name)
 
         user_lines = plot.get_lines()
         ref_lines = ref.get_lines()
 
         if user_lines is None:
-            cls.add_feedback("No lines were plotted in '%s'" % name)
-            return False
+            return bad("No lines were plotted in '%s'" % name)
 
         if len(user_lines) != len(ref_lines):
-            cls.add_feedback("%d lines were plotted in '%s' but %d lines were "
-                             "expected"
-                             % (len(user_lines), name, len(ref_lines)))
-            return False
+            return bad("%d lines were plotted in '%s' but %d lines were "
+                       "expected"
+                       % (len(user_lines), name, len(ref_lines)))
 
         ref_datas = {}
         for i, ref_line in enumerate(ref_lines):
@@ -385,40 +505,44 @@ class Feedback:
                     break
 
         if num_correct == len(ref_lines):
-            cls.add_feedback("'%s' looks good" % name)
+            if report_success:
+                cls.add_feedback("'%s' looks good" % name)
             return True
         else:
-            cls.add_feedback("'%s' is inaccurate" % name)
-            return False
+            return bad("'%s' is inaccurate" % name)
 
-    # `check_dataframe`
-    # Checks and adds feedback regarding the correctness of
-    # a pandas `DataFrame`.
-    # @author: Wade Fagen-Ulmschneider (waf)
-    #
-    # By default, checks if the student DataFrame `data` contains the
-    # same contents as the reference DataFrame `ref` by using
-    # `pandas.util.testing.assert_frame_equal` after basic sanity checks.
-    #
-    # @params:
-    #   `name`, String: The human-readable name of the DataFrame being checked
-    #   `ref`, DataFrame: The reference (correct) DataFrame
-    #   `data`, DataFrame: The student DataFrame
-    #   `subset_columns` = [], Array of Strings:
-    #    - If `subset_columns` is an empty array, all columns are
-    #      used in the check.
-    #    - Otherwise, only columns named in `subset_columns` are used
-    #      in the check and other columns are dropped.
-    #   `check_values` = True, Boolean: Check the values of each cell,
-    #    in addition to the dimensions of the DataFrame
-    #   `allow_order_variance` = True, Boolean: Allow rows to appear in any
-    #    order (so long as the dimensions and values are correct)
-    #   `display_input` = False, Boolean: Display the student's answer in
-    #    the feedback area.
+
     @classmethod
     def check_dataframe(cls, name, ref, data, subset_columns=[],
                         check_values=True, allow_order_variance=True,
                         display_input=False):
+        """
+         `check_dataframe`
+         Checks and adds feedback regarding the correctness of
+         a pandas `DataFrame`.
+         @author: Wade Fagen-Ulmschneider (waf)
+        
+         By default, checks if the student DataFrame `data` contains the
+         same contents as the reference DataFrame `ref` by using
+         `pandas.util.testing.assert_frame_equal` after basic sanity checks.
+        
+         @params:
+           `name`, String: The human-readable name of the DataFrame being checked
+           `ref`, DataFrame: The reference (correct) DataFrame
+           `data`, DataFrame: The student DataFrame
+           `subset_columns` = [], Array of Strings:
+            - If `subset_columns` is an empty array, all columns are
+              used in the check.
+            - Otherwise, only columns named in `subset_columns` are used
+              in the check and other columns are dropped.
+           `check_values` = True, Boolean: Check the values of each cell,
+            in addition to the dimensions of the DataFrame
+           `allow_order_variance` = True, Boolean: Allow rows to appear in any
+            order (so long as the dimensions and values are correct)
+           `display_input` = False, Boolean: Display the student's answer in
+            the feedback area.
+        """
+        
         import pandas as pd
 
         def bad(msg):
