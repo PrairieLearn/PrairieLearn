3600b0a06934cc99f791625d19940be59015b33f
Timothy Bretl <tbretl@illinois.edu>
2020-01-23 15:02:47 -0600

    create course_instance_permissions table

---
+++ migrations/165_course_instance_permissions__create.sql
@@ -0,0 +1,9 @@
+CREATE TYPE enum_course_instance_role AS ENUM ('None', 'Student Data Viewer', 'Student Data Editor');
+
+CREATE TABLE course_instance_permissions (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users ON DELETE CASCADE ON UPDATE CASCADE,
+    course_instance_id BIGINT NOT NULL REFERENCES pl_courses ON DELETE CASCADE ON UPDATE CASCADE,
+    course_instance_role enum_course_instance_role,
+    UNIQUE (user_id, course_instance_id)
+);
