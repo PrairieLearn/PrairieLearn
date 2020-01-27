06dc9bf128ce24e716554d4809bc1d593a572c44
Timothy Bretl <tbretl@illinois.edu>
2020-01-27 16:31:16 -0600

    fix the test for real this time

--- tests/testAccessAsStudent.js
+++ tests/testAccessAsStudent.js
@@ -6,20 +6,25 @@ const baseUrl = siteUrl + '/pl';
 const courseInstanceUrl = baseUrl + '/course_instance/1';
 const assessmentsUrl = courseInstanceUrl + '/assessments';
 
+const storedConfig = {};
+
 describe('Test access with authn as student', function() {
     this.timeout(20000);
 
     before('set authenticated user', function(callback) {
+        storedConfig.authUid = config.authUid;
+        storedConfig.authName = config.authName;
+        storedConfig.authUin = config.authUin;
         config.authUid = 'student@illinois.edu';
         config.authName = 'Student User';
         config.authUin = '00000001';
+        callback(null);
     });
     before('set up testing server', helperServer.before());
     after('shut down testing server', helperServer.after);
     after('unset authenticated user', function(callback) {
-        delete config.authUid;
-        delete config.authName;
-        delete config.authUin;
+        Object.assign(config, storedConfig);
+        callback(null);
     });
 
     describe('The student user', function() {
