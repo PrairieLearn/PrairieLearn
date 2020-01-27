d418b86b7ff238450b6654485f087689db5388e4
Timothy Bretl <tbretl@illinois.edu>
2020-01-27 16:11:44 -0600

    fix test

--- tests/testAccessAsStudent.js
+++ tests/testAccessAsStudent.js
@@ -6,15 +6,21 @@ const baseUrl = siteUrl + '/pl';
 const courseInstanceUrl = baseUrl + '/course_instance/1';
 const assessmentsUrl = courseInstanceUrl + '/assessments';
 
-config.authUid = 'student@illinois.edu';
-config.authName = 'Student User';
-config.authUin = '00000001';
-
 describe('Test access with authn as student', function() {
     this.timeout(20000);
 
+    before('set authenticated user', function(callback) {
+        config.authUid = 'student@illinois.edu';
+        config.authName = 'Student User';
+        config.authUin = '00000001';
+    });
     before('set up testing server', helperServer.before());
     after('shut down testing server', helperServer.after);
+    after('unset authenticated user', function(callback) {
+        delete config.authUid;
+        delete config.authName;
+        delete config.authUin;
+    });
 
     describe('The student user', function() {
         it('should not have access to the assessments page', function(callback) {
