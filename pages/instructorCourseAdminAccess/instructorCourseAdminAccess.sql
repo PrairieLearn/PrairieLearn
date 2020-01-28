a127cd1df7f0f25f4dca3881a0b810e444397478
Timothy Bretl <tbretl@illinois.edu>
2020-01-28 12:41:06 -0600

    fix select all users with role on admin access page

--- pages/instructorCourseAdminAccess/instructorCourseAdminAccess.sql
+++ pages/instructorCourseAdminAccess/instructorCourseAdminAccess.sql
@@ -13,7 +13,7 @@ SELECT
 FROM
     course_permissions AS cp
     FULL JOIN course_instance_permissions AS cip ON (cp.user_id = cip.user_id)
-    LEFT JOIN course_instances AS ci ON (ci.id = cip.course_instance_id AND ci.course_id = $course_id)
+    JOIN course_instances AS ci ON ((ci.id = cip.course_instance_id OR cip.course_instance_role IS NULL) AND ci.course_id = $course_id)
     JOIN users AS u ON (u.user_id = cp.user_id OR u.user_id = cip.user_id),
     LATERAL (SELECT min(ar.start_date) AS start_date, max(ar.end_date) AS end_date FROM course_instance_access_rules AS ar WHERE ar.course_instance_id = ci.id) AS d
 WHERE
