a74aad79fefb9bc8d1d35f2c0f8e9eaacd5728d9
Timothy Bretl <tbretl@illinois.edu>
2020-01-29 22:03:46 -0600

    rewrite select_course_users with cip to cp fkey

--- pages/instructorCourseAdminAccess/instructorCourseAdminAccess.sql
+++ pages/instructorCourseAdminAccess/instructorCourseAdminAccess.sql
@@ -12,12 +12,11 @@ SELECT
     ) ORDER BY d.start_date DESC NULLS LAST, d.end_date DESC NULLS LAST, ci.id DESC) FILTER (WHERE cip.course_instance_role IS NOT NULL) AS course_instance_roles
 FROM
     course_permissions AS cp
-    FULL JOIN course_instance_permissions AS cip ON (cp.user_id = cip.user_id)
-    JOIN course_instances AS ci ON ((ci.id = cip.course_instance_id OR cip.course_instance_role IS NULL) AND ci.course_id = $course_id)
-    JOIN users AS u ON (u.user_id = cp.user_id OR u.user_id = cip.user_id),
+    JOIN users AS u ON (u.user_id = cp.user_id)
+    LEFT JOIN course_instance_permissions AS cip ON (cip.course_permission_id = cp.id)
+    LEFT JOIN course_instances AS ci ON (ci.id = cip.course_instance_id AND ci.course_id = $course_id),
     LATERAL (SELECT min(ar.start_date) AS start_date, max(ar.end_date) AS end_date FROM course_instance_access_rules AS ar WHERE ar.course_instance_id = ci.id) AS d
 WHERE
     cp.course_id = $course_id
-    OR cip.course_instance_role IS NOT NULL
 GROUP BY
     u.user_id, cp.course_role;
