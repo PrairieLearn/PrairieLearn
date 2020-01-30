9159de733e56a06de777bbefd7047cb1b9d216ae
Timothy Bretl <tbretl@illinois.edu>
2020-01-29 21:50:32 -0600

    add fkey that links cip to cp

--- migrations/165_course_instance_permissions__create.sql
+++ migrations/165_course_instance_permissions__create.sql
@@ -2,10 +2,10 @@ CREATE TYPE enum_course_instance_role AS ENUM ('None', 'Student Data Viewer', 'S
 
 CREATE TABLE course_instance_permissions (
     id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES users ON DELETE CASCADE ON UPDATE CASCADE,
     course_instance_id BIGINT NOT NULL REFERENCES course_instances ON DELETE CASCADE ON UPDATE CASCADE,
     course_instance_role enum_course_instance_role,
-    UNIQUE (user_id, course_instance_id)
+    course_permission_id BIGINT NOT NULL REFERENCES course_permissions ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE (course_permission_id, course_instance_id)
 );
 
 CREATE INDEX course_instance_permissions_course_instance_id_idx ON course_instance_permissions (course_instance_id);
