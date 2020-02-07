ffa39bb5d408590365308e411ab2237638b4418e
Matthew West <mwest@illinois.edu>
2020-02-07 08:45:06 -0600

    change to explicit RETURN syntax

--- sprocs/jsonb_array_to_double_precision_array.sql
+++ sprocs/jsonb_array_to_double_precision_array.sql
@@ -2,26 +2,24 @@ DROP FUNCTION IF EXISTS jsonb_array_to_double_precision_array(jsonb, double prec
 
 CREATE OR REPLACE FUNCTION
     jsonb_array_to_double_precision_array(
-        IN jsonb_array jsonb,
-        OUT double_precision_array double precision[]
-    )
+        IN jsonb_array jsonb
+    ) RETURNS double precision[]
 AS $$
 DECLARE
     len integer;
 BEGIN
     IF jsonb_array = 'null'::jsonb THEN
-        double_precision_array := NULL;
-        RETURN;
+        RETURN NULL;
     END IF;
 
     len := jsonb_array_length(jsonb_array);
 
     IF len IS NULL THEN
-        double_precision_array := NULL;
+        RETURN NULL;
     ELSIF len = 0 THEN
-        double_precision_array := array[]::double precision[];
+        RETURN array[]::double precision[];
     ELSE
-        double_precision_array := (SELECT array_agg(v::double precision) FROM jsonb_array_elements_text(jsonb_array) AS v);
+        RETURN (SELECT array_agg(v::double precision) FROM jsonb_array_elements_text(jsonb_array) AS v);
     END IF;
 END;
 $$ LANGUAGE plpgsql VOLATILE;
--- sprocs/jsonb_array_to_text_array.sql
+++ sprocs/jsonb_array_to_text_array.sql
@@ -2,26 +2,24 @@ DROP FUNCTION IF EXISTS jsonb_array_to_text_array(jsonb, text[]);
 
 CREATE OR REPLACE FUNCTION
     jsonb_array_to_text_array(
-        IN jsonb_array jsonb,
-        OUT text_array text[]
-    )
+        IN jsonb_array jsonb
+    ) RETURNS text[]
 AS $$
 DECLARE
     len integer;
 BEGIN
     IF jsonb_array = 'null'::jsonb THEN
-        text_array := NULL;
-        RETURN;
+        RETURN NULL;
     END IF;
 
     len := jsonb_array_length(jsonb_array);
 
     IF len IS NULL THEN
-        text_array := NULL;
+        RETURN NULL;
     ELSIF len = 0 THEN
-        text_array := array[]::text[];
+        RETURN array[]::text[];
     ELSE
-        text_array := (SELECT array_agg(v) FROM jsonb_array_elements_text(jsonb_array) AS v);
+        RETURN (SELECT array_agg(v) FROM jsonb_array_elements_text(jsonb_array) AS v);
     END IF;
 END;
 $$ LANGUAGE plpgsql VOLATILE;
