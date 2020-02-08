22f3df5aa6f87499eeacfc1b2a2b2ac9134c945a
Timothy Bretl <tbretl@illinois.edu>
2020-02-08 14:46:53 -0600

    WIP: nearly-complete permissions UI

--- pages/instructorCourseAdminAccess/instructorCourseAdminAccess.js
+++ pages/instructorCourseAdminAccess/instructorCourseAdminAccess.js
@@ -17,6 +17,9 @@ router.get('/', function(req, res, next) {
     sqldb.query(sql.select_course_users, {course_id: res.locals.course.id}, function(err, result) {
         if (ERR(err, next)) return;
         debug(result.rows);
+        result.rows.forEach((row) => {
+            debug(row.other_course_instances);
+        });
         res.locals.course_users = result.rows;
         res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
     });
@@ -58,29 +61,106 @@ router.post('/', function(req, res, next) {
         });
     } else if (req.body.__action == 'change_course_content_access') {
         // FIXME: check authz
+        // - remove all course instance access roles if user changed to owner?
 
         if (req.body.user_id == res.locals.user.user_id) {
             return next(new Error('Owners cannot change their own permissions'));
         }
 
+        // Before proceeding, we *could* make some effort to verify that the user
+        // is still a member of the course staff. The reason we might want to do so
+        // is that sql.update_course_permissions will throw an "incorrect row count"
+        // error if the user has been removed from the course staff, and we might
+        // want to throw a more informative error beforehand.
+        //
+        // We are making the design choice *not* to do this verification, because
+        // it is unlikely that a course will have many owners all making changes to
+        // permissions simultaneously, and so we are choosing to prioritize speed
+        // in responding to the POST request.
+
         async.series([
-            (callback) => {
-                sqldb.query(sql.select_course_users, {course_id: res.locals.course.id}, function(err, result) {
-                    if (ERR(err, callback)) return;
-                    const course_users = result.rows;
-                    if (course_users.every((course_user) => course_user.user_id != req.body.user_id)) {
-                        return callback(new Error('Not a course user'));
-                    }
-                    callback(null);
-                });
-            },
             (callback) => {
                 const params = {
                     user_id: req.body.user_id,
-                    course_role: req.body.course_role,
                     course_id: res.locals.course.id,
+                    course_role: req.body.course_role,
                 };
-                sqldb.query(sql.update_course_permissions, params, (err, result) => {
+                sqldb.queryOneRow(sql.update_course_permissions, params, (err, _result) => {
+                    if (ERR(err, callback)) return;
+                    callback(null)
+                });
+            }
+        ], (err) => {
+            if (ERR(err, next)) return;
+            res.redirect(req.originalUrl);
+        });
+    } else if (req.body.__action == 'change_student_data_access') {
+        // FIXME: check authz
+        // - check if user_id is owner (owners have full access to all)
+
+        // Again, we could make some effort to verify that the user is still a
+        // member of the course staff and that they still have student data access
+        // in the given course instance. We choose not to do this for the same
+        // reason as above (see handler for change_course_content_access).
+
+        async.series([
+            (callback) => {
+                const params = {
+                    user_id: req.body.user_id,
+                    course_id: res.locals.course.id,
+                    course_instance_id: req.body.course_instance_id,
+                    course_instance_role: req.body.course_instance_role,
+                };
+                sqldb.queryOneRow(sql.update_course_instance_permissions, params, (err, _result) => {
+                    if (ERR(err, callback)) return;
+                    callback(null)
+                });
+            }
+        ], (err) => {
+            if (ERR(err, next)) return;
+            res.redirect(req.originalUrl);
+        });
+    } else if (req.body.__action == 'add_student_data_access') {
+        // FIXME: check authz
+        // - check if user_id is owner (owners have full access to all)
+
+        // Again, we could make some effort to verify that the user is still a
+        // member of the course staff. We choose not to do this for the same
+        // reason as above (see handler for change_course_content_access).
+
+        async.series([
+            (callback) => {
+                const params = {
+                    user_id: req.body.user_id,
+                    course_id: res.locals.course.id,
+                    course_instance_id: req.body.course_instance_id,
+                };
+                sqldb.queryOneRow(sql.insert_course_instance_permissions, params, (err, _result) => {
+                    if (ERR(err, callback)) return;
+                    callback(null)
+                });
+            }
+        ], (err) => {
+            if (ERR(err, next)) return;
+            res.redirect(req.originalUrl);
+        });
+    } else if (req.body.__action == 'remove_student_data_access') {
+        // FIXME: check authz
+        // - check if user_id is owner (owners have full access to all)
+
+        // Again, we could make some effort to verify that the user is still a
+        // member of the course staff and that they still have student data access
+        // in the given course instance. We choose not to do this for the same
+        // reason as above (see handler for change_course_content_access).
+
+        async.series([
+            (callback) => {
+                const params = {
+                    user_id: req.body.user_id,
+                    course_id: res.locals.course.id,
+                    course_instance_id: req.body.course_instance_id,
+                };
+                sqldb.queryOneRow(sql.delete_course_instance_permissions, params, (err, _result) => {
                     if (ERR(err, callback)) return;
                     callback(null)
                 });
--- pages/instructorCourseAdminAccess/instructorCourseAdminAccess.sql
+++ pages/instructorCourseAdminAccess/instructorCourseAdminAccess.sql
@@ -8,7 +8,8 @@ SELECT
         'id', ci.id,
         'short_name', ci.short_name,
         'course_instance_permission_id', cip.id,
-        'course_instance_role', cip.course_instance_role
+        'course_instance_role', cip.course_instance_role,
+        'course_instance_role_formatted', CASE WHEN cip.course_instance_role = 'Student Data Viewer'::enum_course_instance_role THEN 'Viewer' WHEN cip.course_instance_role = 'Student Data Editor'::enum_course_instance_role THEN 'Editor' END
     ) ORDER BY d.start_date DESC NULLS LAST, d.end_date DESC NULLS LAST, ci.id DESC) FILTER (WHERE cip.course_instance_role IS NOT NULL) AS course_instance_roles,
     jsonb_agg(jsonb_build_object(
         'id', ci.id,
@@ -28,7 +29,39 @@ GROUP BY
 -- BLOCK update_course_permissions
 UPDATE course_permissions AS cp
 SET
-    course_role = $course_role
+    course_role = $course_role::enum_course_role
 WHERE
     cp.user_id = $user_id
+    AND cp.course_id = $course_id;
+
+-- BLOCK update_course_instance_permissions
+UPDATE course_instance_permissions AS cip
+SET
+    course_instance_role = $course_instance_role::enum_course_instance_role
+FROM
+    course_permissions AS cp
+WHERE
+    cip.course_instance_id = $course_instance_id
+    AND cp.id = cip.course_permission_id
+    AND cp.user_id = $user_id
+    AND cp.course_id = $course_id;
+
+-- BLOCK insert_course_instance_permissions
+INSERT INTO course_instance_permissions
+    (course_instance_id, course_instance_role, course_permission_id)
+SELECT
+    $course_instance_id, 'Student Data Viewer', cp.id
+FROM
+    course_permissions AS cp
+WHERE
+    cp.user_id = $user_id
+    AND cp.course_id = $course_id;
+
+-- BLOCK delete_course_instance_permissions
+DELETE FROM course_instance_permissions AS cip
+USING course_permissions AS cp
+WHERE
+    cip.course_instance_id = $course_instance_id
+    AND cp.id = cip.course_permission_id
+    AND cp.user_id = $user_id
     AND cp.course_id = $course_id;
