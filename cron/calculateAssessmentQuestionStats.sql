2f82c62ea1baf0dfce242699207a4c304e61672f
Matthew West <mwest@illinois.edu>
2020-02-23 10:56:46 -0600

    only recalc question stats for assessments that need it

--- cron/calculateAssessmentQuestionStats.sql
+++ cron/calculateAssessmentQuestionStats.sql
@@ -1,3 +1,8 @@
 -- BLOCK select_assessments
 SELECT *
-FROM assessments;
+FROM assessments AS a
+WHERE EXISTS (
+    SELECT *
+    FROM assessment_instances AS ai
+    WHERE ai.modified_at > a.stats_last_updated
+);
