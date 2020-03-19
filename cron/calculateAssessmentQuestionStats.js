ac3632be5b1475ee7bfe558d223e7ae864a41c6f
Matthew West <mwest@illinois.edu>
2020-03-19 18:14:09 -0500

    Add logging for question stats calculation

--- cron/calculateAssessmentQuestionStats.js
+++ cron/calculateAssessmentQuestionStats.js
@@ -1,5 +1,6 @@
 const util = require('util');
 
+const logger = require('../lib/logger');
 const sqldb = require('@prairielearn/prairielib/sql-db');
 const sqlLoader = require('@prairielearn/prairielib/sql-loader');
 
@@ -12,6 +13,7 @@ module.exports.run = function(callback) {
         const result = await sqldb.queryAsync(sql.select_assessments, {});
         const assessments = result.rows;
         for (const assessment of assessments) {
+            logger.verbose(`calculateAssessmentQuestionStats: processing assessment_id = ${assessment.id}`);
             await sqldb.callAsync('assessment_questions_calculate_stats_for_assessment', [assessment.id]);
         }
     })(callback);
