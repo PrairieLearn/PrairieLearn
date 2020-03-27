10ac0566701afc044c8c632edb5f203cd74fdce1
Kevin <kevin88tu@gmail.com>
2020-03-27 01:10:57 -0500

    removed console logs

--- pages/questionThumbnail/questionThumbnail.js
+++ pages/questionThumbnail/questionThumbnail.js
@@ -4,12 +4,10 @@ var router = express.Router();
 
 router.get('/*', function(req, res, _next) {
     var filename = req.params[0];
-    console.log(filename);
     var clientFilesDir = path.join(
         res.locals.course.path,
         'questions',
     );
-    console.log(clientFilesDir);
     res.sendFile(filename, {root: clientFilesDir});
 });
 
