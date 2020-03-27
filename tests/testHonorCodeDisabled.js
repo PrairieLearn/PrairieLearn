8c349bb171a2be5e1768be56eb8669ec8c2dec8d
Tim Yang <timothy.d.yang@gmail.com>
2020-03-27 14:00:43 -0500

    Add tests

---
+++ tests/testHonorCodeDisabled.js
@@ -0,0 +1,101 @@
+const util = require('util');
+const assert = require('chai').assert;
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const { step } = require('mocha-steps');
+
+const config = require('../lib/config');
+const sqldb = require('@prairielearn/prairielib/sql-db');
+const sqlLoader = require('@prairielearn/prairielib/sql-loader');
+const sql = sqlLoader.loadSqlEquiv(__filename);
+
+const helperServer = require('./helperServer');
+
+/**
+ * A wrapper around node-fetch that provides a few features:
+ * * Automatic parsing with cheerio
+ * * A `form` option akin to that from the `request` library
+ */
+const fetchCheerio = async (url, options = {}) => {
+  if (options.form) {
+    options.body = JSON.stringify(options.form);
+    options.headers = {
+      'Content-Type': 'application/json',
+      ...options.headers,
+    };
+    delete options.form;
+  }
+  const response = await fetch(url, options);
+  const text = await response.text();
+  response.$ = cheerio.load(text);
+  // response.text() can only be called once, which we already did.
+  // patch this so consumers can use it as normal.
+  response.text = () => text;
+  return response;
+};
+
+/**
+ * This test validates the exam landing page with `requireHonorCode` disabled.
+ * It is deliberately written separately from `testExam.js` as a start to
+ * breaking tests out of that monolith. It does not reuse existing conventions
+ * from `testExam.js` in favor of experimenting with a different way of writing
+ * PrairieLearn tests.
+ */
+describe('Exam assessment with `requireHonorCode` disabled', function() {
+  this.timeout(60000);
+
+  const context = {};
+  context.siteUrl = `http://localhost:${config.serverPort}`;
+  context.baseUrl = `${context.siteUrl}/pl`;
+  context.courseInstanceBaseUrl = `${context.baseUrl}/course_instance/1`;
+
+  /**
+   * Utility function that extracts a CSRF token from a `__csrf_token` input
+   * that is a descendent of the `parentSelector`, if one is specified.
+   * The token will also be persisted to `context.__csrf_token`.
+   */
+  const extractAndSaveCSRFToken = ($, parentSelector = '') => {
+    const csrfTokenInput = $(`${parentSelector} input[name="__csrf_token"]`);
+    assert.lengthOf(csrfTokenInput, 1);
+    const csrfToken = csrfTokenInput.val();
+    assert.isString(csrfToken);
+    context.__csrf_token = csrfToken;
+    return csrfToken;
+  };
+
+  after('set up testing server', helperServer.before);
+
+  after('shut down testing server', helperServer.after);
+
+  step('get default exam info', async () => {
+    const results = await sqldb.queryOneRowAsync(sql.select_exam1, []);
+  });
+
+  step('visit default exam landing page', async () => {
+    const response = await fetchCheerio(context.assessmentUrl);
+    assert.isTrue(response.ok);
+
+    assert.equal(response.$('#start-assessment').text(), 'Start assessment');
+
+    // We should see the honor code div by default
+    assert.lengthOf(response.$('div.test-suite-honor-code'), 1);
+
+    extractAndSaveCSRFToken(response.$, 'form');
+  });
+
+  step('get `"requireHonorCode": false` exam info', async () => {
+    const results = await sqldb.queryOneRowAsync(sql.select_exam2, []);
+  });
+
+  step('visit `"requireHonorCode": false` exam landing page', async () => {
+    const response = await fetchCheerio(context.assessmentUrl);
+    assert.isTrue(response.ok);
+
+    assert.equal(response.$('#start-assessment').text(), 'Start assessment');
+
+    // We should not see the honor code div anymore
+    assert.lengthOf(response.$('div.test-suite-honor-code'), 0);
+
+    extractAndSaveCSRFToken(response.$, 'form');
+  });
+});
---
+++ tests/testHonorCodeDisabled.sql
@@ -0,0 +1,21 @@
+-- BLOCK select_exam1
+SELECT
+    a.id
+FROM
+    assessments AS a
+    JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
+WHERE
+    a.course_instance_id = 1
+    AND aset.abbreviation = 'E'
+    AND a.number = '1';
+
+-- BLOCK select_exam2
+SELECT
+    a.id
+FROM
+    assessments AS a
+    JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
+WHERE
+    a.course_instance_id = 1
+    AND aset.abbreviation = 'E'
+    AND a.number = '2';
