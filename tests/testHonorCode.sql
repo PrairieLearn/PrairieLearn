47d2f7b53dd41e566aa19c612d528ecd34b45ba8
Tim Yang <tdy@users.noreply.github.com>
2020-03-27 21:57:27 -0500

    Parameterize query
    Co-Authored-By: Matthew West <mwest@illinois.edu>

--- tests/testHonorCode.sql
+++ tests/testHonorCode.sql
@@ -8,14 +8,3 @@ WHERE
     a.course_instance_id = 1
     AND aset.abbreviation = 'E'
     AND a.number = $number;
-
--- BLOCK select_exam2
-SELECT
-    a.id
-FROM
-    assessments AS a
-    JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
-WHERE
-    a.course_instance_id = 1
-    AND aset.abbreviation = 'E'
-    AND a.number = '2';
