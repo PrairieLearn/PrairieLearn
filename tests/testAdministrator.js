64d3d9ce476c0abaa3d4b65358820ddb2d80781a
David Mitchell <david@astrosticks.com>
2020-04-14 16:34:27 -0500

    Updating and expanding unit tests for admin pages

--- tests/testAdministrator.js
+++ tests/testAdministrator.js
@@ -10,6 +10,10 @@ const locals = {};
 locals.siteUrl = 'http://localhost:' + config.serverPort;
 locals.baseUrl = locals.siteUrl + '/pl';
 
+locals.administratorOverviewUrl = locals.baseUrl + '/administrator/overview';
+locals.administratorInstitutionsUrl = locals.baseUrl + '/administrator/institutions';
+locals.administratorCoursesUrl = locals.baseUrl + '/administrator/courses';
+
 describe('Administrator pages', function() {
     this.timeout(20000);
 
@@ -20,7 +24,6 @@ describe('Administrator pages', function() {
 
     describe('1. view administrator overview page', function() {
         it('should load successfully', function(callback) {
-            locals.administratorOverviewUrl = locals.baseUrl + '/administrator/overview';
             request(locals.administratorOverviewUrl, function (error, response, body) {
                 if (error) {
                     return callback(error);
@@ -35,7 +38,51 @@ describe('Administrator pages', function() {
         it('should parse', function() {
             locals.$ = cheerio.load(page);
         });
-        it('should contain QA 101', function() {
+        it('should contain button labeled Add administrator', function() {
+            elemList = locals.$('#administratorInsertButton span:contains("Add administrator")');
+            assert.lengthOf(elemList, 1);
+        });
+    });
+
+    describe('2. view administrator institutions page', function() {
+        it('should load successfully', function(callback) {
+            request(locals.administratorInstitutionsUrl, function (error, response, body) {
+                if (error) {
+                    return callback(error);
+                }
+                if (response.statusCode != 200) {
+                    return callback(new Error('bad status: ' + response.statusCode));
+                }
+                page = body;
+                callback(null);
+            });
+        });
+        it('should parse', function() {
+            locals.$ = cheerio.load(page);
+        });
+        it('should contain Default institution', function() {
+            elemList = locals.$('td:contains("Default")');
+            assert.lengthOf(elemList, 2);
+        });
+    });
+
+    describe('3. view administrator courses page', function() {
+        it('should load successfully', function(callback) {
+            request(locals.administratorCoursesUrl, function (error, response, body) {
+                if (error) {
+                    return callback(error);
+                }
+                if (response.statusCode != 200) {
+                    return callback(new Error('bad status: ' + response.statusCode));
+                }
+                page = body;
+                callback(null);
+            });
+        });
+        it('should parse', function() {
+            locals.$ = cheerio.load(page);
+        });
+        it('should contain course QA 101', function() {
             elemList = locals.$('td:contains("QA 101")');
             assert.lengthOf(elemList, 1);
         });
