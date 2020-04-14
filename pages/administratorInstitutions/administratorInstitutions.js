52639ce9ed49449caedb4629f1940705004ff548
David Mitchell <david@astrosticks.com>
2020-04-14 14:47:06 -0500

    Adding 'institutions' admin page

---
+++ pages/administratorInstitutions/administratorInstitutions.js
@@ -0,0 +1,21 @@
+const ERR = require('async-stacktrace');
+const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
+
+const error = require('@prairielearn/prairielib/error');
+const { sqlDb, sqlLoader } = require('@prairielearn/prairielib');
+
+const cache = require('../../lib/cache');
+
+const sql = sqlLoader.loadSqlEquiv(__filename);
+
+router.get('/', (req, res, next) => {
+    sqlDb.queryOneRow(sql.select, [], (err, result) => {
+        if (ERR(err, next)) return;
+        _.assign(res.locals, result.rows[0]);
+        res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+    });
+});
+
+module.exports = router;
