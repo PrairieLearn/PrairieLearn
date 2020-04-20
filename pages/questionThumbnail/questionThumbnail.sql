1e619422ab517ea8f8fbb49dca49c428be51beb5
Kevin <kevin88tu@gmail.com>
2020-04-20 14:22:19 -0500

    work on async

--- pages/questionThumbnail/questionThumbnail.sql
+++ pages/questionThumbnail/questionThumbnail.sql
@@ -3,7 +3,5 @@ SELECT
     q.thumbnail, q.qid, q.id
 FROM
     questions as q
-    LEFT JOIN course_instances AS ci ON (ci.id = $course_instance_id)
-    LEFT JOIN pl_courses AS c ON (c.id = $course_id)
 WHERE
     q.id = $question_id
