0f505e418a9b1c35dc4e8cbe722fbef88c0edf14
frankieyuan <frankie.yuan@yahoo.com>
2020-04-20 20:20:23 -0500

    sql migration for group ER diagram

---
+++ migrations/171_group_change_assessment_inst.sql
@@ -0,0 +1,20 @@
+ALTER TABLE assessments ADD COLUMN groupwork boolean DEFAULT FALSE;
+ALTER TABLE assessment_instances ADD COLUMN group_id BIGINT;
+ALTER TABLE assessment_instances ADD CONSTRAINT user_group_XOR CHECK ((user_id IS NOT NULL AND group_id is NULL) OR (group_id IS NOT NULL AND user_id is NULL));
+
+CREATE TABLE IF NOT EXISTS group_type(
+    id BIGSERIAL PRIMARY KEY,
+    assessment_id bigint REFERENCES assessments(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    grouptype           INT    NOT NULL,
+    max           INT,
+    min           INT    NOT NULL
+);
+CREATE TABLE IF NOT EXISTS groups(
+    id BIGSERIAL PRIMARY KEY     NOT NULL,
+    group_type bigint REFERENCES group_type(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS group_user (
+  id BIGSERIAL PRIMARY KEY     NOT NULL,
+  members TEXT[]
+);
