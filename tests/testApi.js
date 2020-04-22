945d61a7612fabe977fabc4cdb94a09c721dac35
mepstein-gh <mepstein@illinois.edu>
2020-04-22 08:56:59 -0500

    Add API call to get assessment_access_rules (#2389)
    * Added api call to get assessment_access_rules
    
    * Added test for new API call
    
    * Fixed incorrect URL in new test
    
    * Fixed typo in new test
    
    * Adjusted the query for assessment_access_rules
    
    * Made some further adjustments to the assessment_access_rules query
    
    * Made some further adjustments to the assessment_access_rules query
    
    * A trivial change to try to get github to run the CI build/tests -- it seems to be stuck.
    
    * That worked -- now undoing trivial change before someone complains about extra blank line :-)

--- tests/testApi.js
+++ tests/testApi.js
@@ -436,4 +436,32 @@ describe('API', function() {
             assert.lengthOf(locals.json, 7);
         });
     });
+
+    describe('13. GET to API for Exam 1 access rules', function() {
+        it('should load successfully', function(callback) {
+            locals.apiAssessmentAccessRulesUrl = locals.apiCourseInstanceUrl + `/assessments/${locals.assessment_id}/assessment_access_rules`;
+            const options = {
+                url: locals.apiAssessmentAccessRulesUrl,
+                headers: {
+                    'Private-Token': locals.api_token,
+                },
+            };
+            request(options, function (error, response, body) {
+                if (error) {
+                    return callback(error);
+                }
+                if (response.statusCode != 200) {
+                    return callback(new Error('bad status: ' + response.statusCode));
+                }
+                page = body;
+                callback(null);
+            });
+        });
+        it('should parse as JSON', function() {
+            locals.json = JSON.parse(page);
+        });
+        it('should have two access rules', function() {
+            assert.lengthOf(locals.json, 2);
+        });
+    });
 });
