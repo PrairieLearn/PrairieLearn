1bbb369998397f5af383788fe5b0a52bf3c466b5
frankieyuan <frankie.yuan@yahoo.com>
2020-04-27 23:46:59 -0500

    minor bug fix for group_delete_all

--- sprocs/assessment_groups_delete_all.sql
+++ sprocs/assessment_groups_delete_all.sql
@@ -5,16 +5,16 @@ CREATE OR REPLACE FUNCTION
     ) returns void
 AS $$
 BEGIN
-    INSERT INTO temp
-    SELECT gr.id
-    FROM group_type AS gt
-    JOIN groups AS gr ON gr.group_type_id = gt.id
-    WHERE gt.assessment_id = assessment_groups_delete_all.assessment_id;
-
     DELETE FROM group_user gu
-    WHERE gu.group_id IN (SELECT id FROM temp);
+    WHERE gu.group_id IN (SELECT gr.id
+                        FROM group_type AS gt
+                        JOIN groups AS gr ON gr.group_type_id = gt.id
+                        WHERE gt.assessment_id = assessment_groups_delete_all.assessment_id);
 
     DELETE FROM groups g
-    WHERE g.id IN (SELECT id FROM temp);
+    WHERE g.id IN (SELECT gr.id
+                FROM group_type AS gt
+                JOIN groups AS gr ON gr.group_type_id = gt.id
+                WHERE gt.assessment_id = assessment_groups_delete_all.assessment_id);
 END;
 $$ LANGUAGE plpgsql VOLATILE;
