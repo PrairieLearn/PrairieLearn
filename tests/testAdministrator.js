b98c128ad027ecae5ec5d52b5fccc3d9ea51fbc1
David Mitchell <david@astrosticks.com>
2020-04-30 15:08:32 -0500

    Adding simple GET tests for new admin pages

--- tests/testAdministrator.js
+++ tests/testAdministrator.js
@@ -10,10 +10,6 @@ const locals = {};
 locals.siteUrl = 'http://localhost:' + config.serverPort;
 locals.baseUrl = locals.siteUrl + '/pl';
 
-locals.administratorOverviewUrl = locals.baseUrl + '/administrator/overview';
-locals.administratorInstitutionsUrl = locals.baseUrl + '/administrator/institutions';
-locals.administratorCoursesUrl = locals.baseUrl + '/administrator/courses';
-
 describe('Administrator pages', function() {
     this.timeout(20000);
 
@@ -22,9 +18,10 @@ describe('Administrator pages', function() {
 
     var page, elemList;
 
-    describe('1. view administrator overview page', function() {
+    describe('1. view administrator admins list page', function() {
+        locals.administratorAdminsUrl = locals.baseUrl + '/administrator/admins';
         it('should load successfully', function(callback) {
-            request(locals.administratorOverviewUrl, function (error, response, body) {
+            request(locals.administratorAdminsUrl, function (error, response, body) {
                 if (error) {
                     return callback(error);
                 }
@@ -44,9 +41,10 @@ describe('Administrator pages', function() {
         });
     });
 
-    describe('2. view administrator institutions page', function() {
+    describe('2. view administrator server page', function() {
+        locals.administratorServerUrl = locals.baseUrl + '/administrator/server';
         it('should load successfully', function(callback) {
-            request(locals.administratorInstitutionsUrl, function (error, response, body) {
+            request(locals.administratorServerUrl, function (error, response, body) {
                 if (error) {
                     return callback(error);
                 }
@@ -60,13 +58,10 @@ describe('Administrator pages', function() {
         it('should parse', function() {
             locals.$ = cheerio.load(page);
         });
-        it('should contain Default institution', function() {
-            elemList = locals.$('td:contains("Default")');
-            assert.lengthOf(elemList, 2);
-        });
     });
 
     describe('3. view administrator courses page', function() {
+        locals.administratorCoursesUrl = locals.baseUrl + '/administrator/courses';
         it('should load successfully', function(callback) {
             request(locals.administratorCoursesUrl, function (error, response, body) {
                 if (error) {
@@ -82,9 +77,33 @@ describe('Administrator pages', function() {
         it('should parse', function() {
             locals.$ = cheerio.load(page);
         });
+        it('should contain default institution', function() {
+            elemList = locals.$('td:contains("Default")');
+            assert.lengthOf(elemList, 1);
+        });
         it('should contain course QA 101', function() {
             elemList = locals.$('td:contains("QA 101")');
             assert.lengthOf(elemList, 1);
         });
     });
+
+    describe('4. view administrator exam-mode networks page', function() {
+        locals.administratorNetworksUrl = locals.baseUrl + '/administrator/networks';
+        it('should load successfully', function(callback) {
+            request(locals.administratorNetworksUrl, function (error, response, body) {
+                if (error) {
+                    return callback(error);
+                }
+                if (response.statusCode != 200) {
+                    return callback(new Error('bad status: ' + response.statusCode));
+                }
+                page = body;
+                callback(null);
+            });
+        });
+        it('should parse', function() {
+            locals.$ = cheerio.load(page);
+        });
+    });
+
 });
