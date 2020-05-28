bf237540062d57afe2cd448dfdd6e55b8283bdb3
frankieyuan <frankie.yuan@yahoo.com>
2020-05-28 16:35:04 -0500

    Change JOIN to LEFT JOIN in gradebook

--- pages/studentGradebook/studentGradebook.sql
+++ pages/studentGradebook/studentGradebook.sql
@@ -27,8 +27,8 @@ FROM
     JOIN assessments AS a ON (a.id = ai.assessment_id)
     JOIN course_instances AS ci ON (ci.id = a.course_instance_id)
     JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
-    JOIN group_users AS gu ON ($user_id = gu.user_id)
-    JOIN groups AS g ON (gu.group_id = g.id)
+    LEFT JOIN group_users AS gu ON ($user_id = gu.user_id)
+    LEFT JOIN groups AS g ON (gu.group_id = g.id)
 
 WHERE
     ci.id = $course_instance_id
