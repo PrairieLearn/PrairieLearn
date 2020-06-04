0762894e3267e4d44dab83df782fe3c37f1ac085
Andrew Stec <andrewstec@gmail.com>
2020-06-04 10:52:08 -0700

    Adding demo question

---
+++ exampleCourse/questions/demoDropdownQuestion/server.py
@@ -0,0 +1,8 @@
+import random, copy
+
+def generate(data):
+
+    # Correct Answers
+    data['correct_answers']['hume'] = 'wise'
+    data['correct_answers']['aristotle'] = 'whole'
+    data['correct_answers']['socrates'] = 'unexamined'
