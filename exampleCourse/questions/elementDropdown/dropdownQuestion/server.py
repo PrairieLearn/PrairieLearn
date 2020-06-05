4cf750d6998544b829b4c5ad4cb6420d3e370e0d
Andrew Stec <andrewstec@gmail.com>
2020-06-05 14:18:16 -0700

    Removing duplicate files accidentally created

--- exampleCourse/questions/elementDropdown/dropdownQuestion/server.py
+++
@@ -1,8 +0,0 @@
-import random, copy
-
-def generate(data):
-
-    # Correct Answers
-    data['correct_answers']['hume'] = 'wise'
-    data['correct_answers']['aristotle'] = 'whole'
-    data['correct_answers']['socrates'] = 'unexamined'
