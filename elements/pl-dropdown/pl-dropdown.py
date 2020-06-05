14e84af2711dbd9ac2020d5ea0b18453d22c9f65
Andrew Stec <andrewstec@gmail.com>
2020-06-05 09:19:11 -0700

    Fixed python linting errors

--- elements/pl-dropdown/pl-dropdown.py
+++ elements/pl-dropdown/pl-dropdown.py
@@ -3,15 +3,16 @@ import lxml.html
 import json
 import random
 import chevron
-import os
 from enum import Enum
 
 WEIGHT_DEFAULT = 1
 
-class SortTypes(Enum): 
+
+class SortTypes(Enum):
     RANDOM = 'random'
     ASCEND = 'ascend'
     DESCEND = 'descend'
+
 
 def prepare(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
@@ -19,12 +20,13 @@ def prepare(element_html, data):
     answer_key = pl.get_string_attrib(element, 'answer-key')
     correct_answer = pl.get_string_attrib(element, 'answer', None)
 
-    ## Set correct answer from HTML attrib if given
-    if correct_answer != None:
+    # Set correct answer from HTML attrib if given
+    if correct_answer is not None:
         data['correct_answers'][answer_key] = correct_answer
 
-    if data['correct_answers'][answer_key] == None:
+    if data['correct_answers'][answer_key] is None:
         raise Exception('Correct answer not defined for answer-key: %s' % answer_key)
+
 
 def render(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
@@ -40,7 +42,7 @@ def render(element_html, data):
             dropdown_options.sort(reverse=True)
         elif sort == SortTypes.ASCEND.name:
             dropdown_options.sort(reverse=False)
-        elif sort == SortTypes.RANDOM.name: 
+        elif sort == SortTypes.RANDOM.name:
             random.shuffle(dropdown_options)
 
         html_params = {
@@ -71,6 +73,7 @@ def render(element_html, data):
         html = chevron.render(f, html_params).strip()
     return html
 
+
 def parse(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     answer_key = pl.get_string_attrib(element, 'answer-key')
@@ -80,33 +83,34 @@ def parse(element_html, data):
 
     if answer is None:
         data['format_errors'][answer_key] = 'No answer was submitted.'
-    
+
     if answer not in valid_options:
         data['format_errors'][answer_key] = 'Invalid answer submitted.'
-    
+
 
 def grade(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     name = pl.get_string_attrib(element, 'answer-key')
     weight = pl.get_integer_attrib(element, 'weight', WEIGHT_DEFAULT)
     submitted_answer = data['submitted_answers'].get(name, None)
-    
+
     if data['correct_answers'][name] == submitted_answer:
         data['partial_scores'][name] = {'score': 1, 'weight': weight}
     else:
         data['partial_scores'][name] = {'score': 0, 'weight': weight}
+
 
 def test(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     answer_key = pl.get_string_attrib(element, 'answer-key')
     weight = pl.get_integer_attrib(element, 'weight', WEIGHT_DEFAULT)
 
-    ## correct_answer is what the answer should be
+    # correct_answer is what the answer should be
     correct_answer = data['correct_answers'][answer_key]
-    
-    ## incorrect and correct answer test cases
+
+    # incorrect and correct answer test cases
     if data['test_type'] == 'correct':
-        data['raw_submitted_answers'][answer_key] = data['correct_answers'][answer_key]
+        data['raw_submitted_answers'][answer_key] = correct_answer
         data['partial_scores'][answer_key] = {'score': 1, 'weight': weight}
     elif data['test_type'] == 'incorrect':
         dropdown_options = json.loads(pl.get_string_attrib(element, 'options'))
@@ -119,7 +123,7 @@ def test(element_html, data):
         data['raw_submitted_answers'][answer_key] = incorrect_ans
         data['partial_scores'][answer_key] = {'score': 0, 'weight': weight}
     elif data['test_type'] == 'invalid':
-        ## Test for invalid drop-down options in case injection on front-end
+        # Test for invalid drop-down options in case injection on front-end
         data['raw_submitted_answers'][answer_key] = 'INVALID STRING'
         data['format_errors'][answer_key] = 'format error message'
     else:
