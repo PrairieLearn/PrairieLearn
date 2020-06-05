b5fb82f2841e57779e25b63f0575e9c6904db375
Andrew Stec <andrewstec@gmail.com>
2020-06-05 14:27:45 -0700

    Removed unnecessary server.py file

--- exampleCourse/questions/demoOverlayDropdown/server.py
+++
@@ -1,8 +0,0 @@
-import random, copy
-
-def generate(data):
-
-    # Correct Answers
-    data['correct_answers']['northwest1'] = 'idaho'
-    data['correct_answers']['northwest2'] = 'washington'
-    data['correct_answers']['northwest3'] = 'portland'
