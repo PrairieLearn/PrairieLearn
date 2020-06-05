1ee9e8de7c3212f447c1e2093cbd90eae86117d7
Andrew Stec <andrewstec@gmail.com>
2020-06-05 13:51:44 -0700

    Moving pl-dropdown example questions into exampleCourse

---
+++ exampleCourse/questions/geographyMapDropdown/server.py
@@ -0,0 +1,8 @@
+import random, copy
+
+def generate(data):
+
+    # Correct Answers
+    data['correct_answers']['northwest1'] = 'idaho'
+    data['correct_answers']['northwest2'] = 'washington'
+    data['correct_answers']['northwest3'] = 'portland'
