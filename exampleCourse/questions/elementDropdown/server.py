ce8eff47b9056205996d83f12eed6b2e91db52a6
Andrew Stec <andrewstec@gmail.com>
2020-06-08 13:52:27 -0700

    Changed elementDropdown example to use pl-answer

--- exampleCourse/questions/elementDropdown/server.py
+++ exampleCourse/questions/elementDropdown/server.py
@@ -2,7 +2,13 @@ import random, copy
 
 def generate(data):
 
-    # Correct Answers
-    data['correct_answers']['hume'] = 'wise'
-    data['correct_answers']['aristotle'] = 'whole'
-    data['correct_answers']['socrates'] = 'unexamined'
+	# Can override pl-answer solution 
+	data['correct_answers']['aristotle'] = 'whole'
+	data['correct_answers']['hume'] = 'wise'
+	data['correct_answers']['socrates'] = 'unexamined'
+
+	# Can override pl-answer options as parameters
+	data['params']['aristotle'] = ['whole', 'part', 'inverse']
+	data['params']['socrates'] = ['insatiable', 'unexamined', 'examined']
+	data['params']['hume'] = ['wise', 'clumsy', 'reckless']
+
