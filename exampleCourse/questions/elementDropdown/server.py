a6a58d5c23b5e321d0be4d6c4189c7e76d2d8fe8
Andrew Stec <andrewstec@gmail.com>
2020-06-12 13:23:40 -0700

    Re-wrote question to demonstrate parameterization of questions and answers

--- exampleCourse/questions/elementDropdown/server.py
+++ exampleCourse/questions/elementDropdown/server.py
@@ -2,13 +2,27 @@ import random, copy
 
 def generate(data):
 
-	# Can override pl-answer solution 
-	data['correct_answers']['aristotle'] = 'whole'
-	data['correct_answers']['hume'] = 'wise'
-	data['correct_answers']['socrates'] = 'unexamined'
+	QUESTION1 = 'aristotle'
+	QUESTION2 = 'hume'
+	QUESTION3 = 'socrates'
 
 	# Can override pl-answer options as parameters
-	data['params']['aristotle'] = ['whole', 'part', 'inverse']
-	data['params']['socrates'] = ['insatiable', 'unexamined', 'examined']
-	data['params']['hume'] = ['wise', 'clumsy', 'reckless']
+	data['params'][QUESTION1] = { 
+		'tag1': 'true', 'ans1': 'whole', 
+		'tag2': 'false', 'ans2': 'part',
+		'tag3': 'false', 'ans3': 'inverse' 
+	}
 
+	data['params'][QUESTION2] = {
+		'tag1': 'true', 'ans1': 'insatiable',
+		'tag2': 'false', 'ans2': 'unexamined',
+		'tag3': 'false', 'ans3': 'examined' 
+	}
+
+	data['params'][QUESTION3] = {
+		'tag1': 'true', 'ans1': 'wise',
+		'tag2': 'false', 'ans2': 'clumsy',
+		'tag3': 'false', 'ans3': 'reckless'
+	}
+
+	return data
