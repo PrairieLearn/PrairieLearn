b64e4c537b87c1ce37b1efbd91bf31d8108aa4dd
Timothy Bretl <tbretl@illinois.edu>
2020-06-22 21:52:00 -0500

    fix typo in clearCookies

--- middlewares/clearCookies.js
+++ middlewares/clearCookies.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 
-const cookies_to_ignore = ['pl_authn', 'pl_assessmentpw', 'pl_requested_max_access_level'];
+const cookies_to_ignore = ['pl_authn', 'pl_assessmentpw', 'pl_requested_login_type'];
 
 module.exports = function(req, res, next) {
     _(req.cookies).each(function(value, key) {
