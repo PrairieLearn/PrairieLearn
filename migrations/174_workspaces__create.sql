ed3ccb2a0b86e333b0ca6c5d2ac96c9f66692135
Tim Yang <timothy.d.yang@gmail.com>
2020-06-24 10:09:48 -0500

    Fix DEFAULT syntax

--- migrations/174_workspaces__create.sql
+++ migrations/174_workspaces__create.sql
@@ -1,10 +1,10 @@
-CREATE TYPE enum_workspace_state AS ENUM ('launching', 'running', 'stopped', 'uninitialized') DEFAULT 'uninitialized';
+CREATE TYPE enum_workspace_state AS ENUM ('launching', 'running', 'stopped', 'uninitialized');
 
 CREATE TABLE IF NOT EXISTS workspaces (
     id bigserial PRIMARY KEY,
     s3_bucket text,
     s3_root_key text,
-    state enum_workspace_state,
+    state enum_workspace_state DEFAULT 'uninitialized',
     variant_id bigint REFERENCES variants(id) ON DELETE CASCADE ON UPDATE CASCADE,
     workspace_host_id bigint REFERENCES workspace_hosts(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
