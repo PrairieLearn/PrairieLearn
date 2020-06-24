f51700f8d1c09babfb7e12ceb3ea98acb0934d13
Tim Yang <timothy.d.yang@gmail.com>
2020-06-24 10:02:52 -0500

    Change log level to ENUM

--- migrations/175_workspace_logs__create.sql
+++ migrations/175_workspace_logs__create.sql
@@ -1,7 +1,9 @@
+CREATE TYPE enum_workspace_log_log_level AS ENUM ('debug', 'error', 'info', 'warn');
+
 CREATE TABLE IF NOT EXISTS workspace_logs (
     id bigserial PRIMARY KEY,
     date timestamptz DEFAULT now(),
-    level smallint,
+    log_level enum_workspace_log_log_level,
     message text,
     workspace_id bigint NOT NULL REFERENCES workspaces(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
