b3fece215703c865fcafa00d6ce8aa3f073d6b8f
Tim Yang <timothy.d.yang@gmail.com>
2020-06-24 10:03:31 -0500

    Change state machine to ENUM

--- migrations/174_workspaces__create.sql
+++ migrations/174_workspaces__create.sql
@@ -1,8 +1,10 @@
+CREATE TYPE enum_workspace_state AS ENUM ('launching', 'running', 'stopped', 'uninitialized') DEFAULT 'uninitialized';
+
 CREATE TABLE IF NOT EXISTS workspaces (
     id bigserial PRIMARY KEY,
     s3_bucket text,
     s3_root_key text,
-    state text,
+    state enum_workspace_state,
     variant_id bigint REFERENCES variants(id) ON DELETE CASCADE ON UPDATE CASCADE,
     workspace_host_id bigint REFERENCES workspace_hosts(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
