afeb3ef92b1063b662d33c00fd25213b8490fb56
David Mitchell <david@astrosticks.com>
2020-06-25 12:00:18 -0500

    Aggregate multipleInstance scores in student gradebook (#2068)

--- pages/studentGradebook/studentGradebook.sql
+++ pages/studentGradebook/studentGradebook.sql
@@ -1,27 +1,22 @@
 -- BLOCK select_assessment_instances
 SELECT
-    a.id AS assessment_id,
     a.number AS assessment_number,
     a.order_by AS assessment_order_by,
     CASE
-        WHEN a.multiple_instance THEN a.title || ' instance #' || ai.number
+        WHEN a.multiple_instance THEN a.title || ' (' || COUNT(ai.id) || ' instances)'
         ELSE a.title
     END AS title,
-    aset.id AS assessment_set_id,
     aset.abbreviation AS assessment_set_abbreviation,
     aset.name AS assessment_set_name,
     aset.heading AS assessment_set_heading,
     aset.color AS assessment_set_color,
     aset.number AS assessment_set_number,
     CASE
-        WHEN a.multiple_instance THEN aset.abbreviation || a.number || '#' || ai.number
+        WHEN a.multiple_instance THEN aset.abbreviation || a.number
         ELSE aset.abbreviation || a.number
     END AS label,
-    ai.id AS assessment_instance_id,
-    ai.number AS assessment_instance_number,
-    ai.score_perc AS assessment_instance_score_perc,
-    ai.open AS assessment_instance_open,
-    (lag(assessment_set_id) OVER (PARTITION BY aset.id ORDER BY a.order_by, a.id, ai.number) IS NULL) AS start_new_set
+    MAX(ai.score_perc) AS assessment_instance_score_perc,
+    (lag(assessment_set_id) OVER (PARTITION BY aset.id ORDER BY a.order_by, a.id) IS NULL) AS start_new_set
 FROM
     assessment_instances AS ai
     JOIN assessments AS a ON (a.id = ai.assessment_id)
@@ -31,5 +26,7 @@ WHERE
     ci.id = $course_instance_id
     AND ai.user_id = $user_id
     AND a.deleted_at IS NULL
+GROUP BY
+    aset.id, aset.abbreviation, aset.name, aset.heading, aset.color, aset.number, a.id
 ORDER BY
-    aset.number, a.order_by, a.id, ai.number;
+    aset.number, a.order_by, a.id;
