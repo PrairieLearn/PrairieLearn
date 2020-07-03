0a98be2cde016962106b048e1ec1d681e82efc13
Tim Yang <tdy@users.noreply.github.com>
2020-07-03 13:51:29 -0500

    Update elements/pl-workspace/pl-workspace.py

--- elements/pl-workspace/pl-workspace.py
+++ elements/pl-workspace/pl-workspace.py
@@ -3,6 +3,7 @@ import chevron
 
 def render(element_html, data):
     # Get workspace url
+    # TODO: Improve UX if key undefined: https://github.com/PrairieLearn/PrairieLearn/pull/2665/files#r449319839
     workspace_url = data['options']['workspace_url']
 
     # Create and return html
