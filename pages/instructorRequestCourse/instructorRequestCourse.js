82e89c5e49dcd2680083aa0dc2fbfeef42a0b7e5
Nicolas Nytko <nicknytko@gmail.com>
2020-07-05 19:30:12 -0500

    move status badge to partial

--- pages/instructorRequestCourse/instructorRequestCourse.js
+++ pages/instructorRequestCourse/instructorRequestCourse.js
@@ -7,18 +7,6 @@ const sqldb = require('@prairielearn/prairielib/sql-db');
 const sqlLoader = require('@prairielearn/prairielib/sql-loader');
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
-function approval_status_get_icon(status) {
-    if (status == 'pending') {
-        return 'fa-sync-alt';
-    } else if (status == 'approved') {
-        return 'fa-check';
-    } else if (status == 'denied') {
-        return 'fa-times';
-    } else {
-        return '';
-    }
-}
-
 router.get('/', function(req, res, next) {
     sqldb.queryOneRow(sql.get_requests, {user_id: res.locals.authn_user.user_id}, (err, result) => {
         if (ERR(err, next)) return;
