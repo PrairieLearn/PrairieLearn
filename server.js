5389e659ceb7a417bc370713385eb50953e4fd92
Tim Yang <timothy.d.yang@gmail.com>
2020-07-05 19:01:28 -0500

    Add landing page draft

--- server.js
+++ server.js
@@ -249,6 +249,7 @@ app.use(/^(\/?)$|^(\/pl\/?)$/, require('./middlewares/clearCookies'));
 // some pages don't need authorization
 app.use('/', require('./pages/home/home'));
 app.use('/pl', require('./pages/home/home'));
+app.use('/pl/landing', require('./pages/landing/landing'));
 app.use('/pl/settings', require('./pages/userSettings/userSettings'));
 app.use('/pl/enroll', require('./pages/enroll/enroll'));
 app.use('/pl/logout', require('./pages/authLogout/authLogout'));
