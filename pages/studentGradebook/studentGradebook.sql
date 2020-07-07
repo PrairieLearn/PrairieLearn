42ff3143dfdd5c1ed40b5d2acf71879a33fe985a
David Mitchell <david@astrosticks.com>
2020-07-07 17:10:09 -0500

    Switch to aggregate headers in student gradebook

--- pages/studentGradebook/studentGradebook.sql
+++ pages/studentGradebook/studentGradebook.sql
@@ -1,27 +1,31 @@
 -- BLOCK select_assessment_instances
+WITH individual_instances AS (
 SELECT
+    a.id AS assessment_id,
     a.number AS assessment_number,
     a.order_by AS assessment_order_by,
-    a.title AS title,
+    a.multiple_instance AS assessment_multiple_instance,
+    a.title AS assessment_title,
+    CASE
+        WHEN a.multiple_instance THEN a.title || ' instance #' || ai.number
+        ELSE a.title
+    END AS title,
+    aset.id AS assessment_set_id,
     aset.abbreviation AS assessment_set_abbreviation,
     aset.name AS assessment_set_name,
     aset.heading AS assessment_set_heading,
     aset.color AS assessment_set_color,
     aset.number AS assessment_set_number,
     CASE
-        WHEN a.multiple_instance THEN aset.abbreviation || a.number
+        WHEN a.multiple_instance THEN aset.abbreviation || a.number || '#' || ai.number
         ELSE aset.abbreviation || a.number
     END AS label,
-    CASE
-        WHEN a.multiple_instance THEN
-            CASE
-              WHEN (COUNT(ai.id) > 1) THEN aset.abbreviation || a.number || '#1-' || MAX(ai.number)
-              ELSE aset.abbreviation || a.number || '#1'
-            END
-        ELSE aset.abbreviation || a.number
-    END AS label,
-    MAX(ai.score_perc) AS assessment_instance_score_perc,
-    (lag(assessment_set_id) OVER (PARTITION BY aset.id ORDER BY a.order_by, a.id) IS NULL) AS start_new_set
+    ai.id AS assessment_instance_id,
+    ai.number AS assessment_instance_number,
+    ai.score_perc AS display_score_perc,
+    a.multiple_instance AS collapse,
+    false AS collapse_control,
+    assessment_id AS collapse_id
 FROM
     assessment_instances AS ai
     JOIN assessments AS a ON (a.id = ai.assessment_id)
@@ -31,7 +35,41 @@ WHERE
     ci.id = $course_instance_id
     AND ai.user_id = $user_id
     AND a.deleted_at IS NULL
-GROUP BY
-    aset.id, aset.abbreviation, aset.name, aset.heading, aset.color, aset.number, a.id
+),
+combined_instance_headers AS (
+    SELECT
+        ii.assessment_id AS assessment_id,
+        MIN(ii.assessment_number) AS assessment_number,
+        MIN(ii.assessment_order_by) AS assessment_order_by,
+        true AS assessment_multiple_instance,
+        MIN(ii.assessment_title) AS assessment_title,
+        MIN(ii.assessment_title) AS title,
+        MIN(ii.assessment_set_id) AS assessment_set_id,
+        MIN(ii.assessment_set_abbreviation) AS assessment_set_abbreviation,
+        MIN(ii.assessment_set_name) AS assessment_set_name,
+        MIN(ii.assessment_set_heading) AS assessment_set_heading,
+        MIN(ii.assessment_set_color) AS assessment_set_color,
+        MIN(ii.assessment_set_number) AS assessment_set_number,
+        MIN(ii.assessment_set_abbreviation) || MIN(ii.assessment_number) || '#' || MIN(ii.assessment_instance_number) || '-' || MAX(ii.assessment_instance_number) AS label,
+        0 AS assessment_instance_id,
+        0 AS assessment_instance_number,
+        MAX(ii.display_score_perc) AS display_score_perc,
+        false AS collapse,
+        true AS collapse_control,
+        ii.assessment_id AS collapse_id
+
+    FROM individual_instances ii
+    WHERE ii.assessment_multiple_instance = true
+    GROUP BY
+        ii.assessment_id
+)
+SELECT
+    *,
+    (lag(assessment_set_id) OVER (PARTITION BY assessment_set_id ORDER BY assessment_order_by, assessment_id, assessment_instance_number) IS NULL) AS start_new_set
+FROM (
+    SELECT * FROM individual_instances
+    UNION ALL
+    SELECT * FROM combined_instance_headers
+) AS combined_rows
 ORDER BY
-    aset.number, a.order_by, a.id;
+    assessment_set_number, assessment_order_by, assessment_id, assessment_instance_number;
