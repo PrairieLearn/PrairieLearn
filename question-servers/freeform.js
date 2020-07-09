0e79bfa1bb3f61e5bef9dce3367a8c0d6302d889
Nicolas Nytko <nicknytko@gmail.com>
2020-07-08 22:24:35 -0500

    Add data['options'] for other Freeform stages (#2689)
    * port options from #2507
    
    * remove extension stuff
    
    * Update freeform.js

--- question-servers/freeform.js
+++ question-servers/freeform.js
@@ -164,20 +164,30 @@ module.exports = {
         return path.join(element.directory, element.controller);
     },
 
+    /**
+     * Add clientFiles urls for elements.
+     * Returns a copy of data with the new urls inserted.
+     */
+    getElementClientFiles: function(data, elementName, _context) {
+        let dataCopy = _.cloneDeep(data);
+        /* The options field wont contain URLs unless in the 'render' stage, so check
+           if it is populated before adding the element url */
+        if ('base_url' in data.options) {
+            /* Join the URL using Posix join to avoid generating a path with backslashes,
+               as would be the case when running on Windows */
+            dataCopy.options.client_files_element_url = path.posix.join(data.options.base_url, 'elements', elementName, 'clientFilesElement');
+            /* This will add extension urls once that is merged (and will use the context, I promise!) */
+        }
+        return dataCopy;
+    },
+
     elementFunction: async function(pc, fcn, elementName, elementHtml, data, context) {
         return new Promise((resolve, reject) => {
             const resolvedElement = module.exports.resolveElement(elementName, context);
             const cwd = resolvedElement.directory;
             const controller = resolvedElement.controller;
+            const dataCopy = module.exports.getElementClientFiles(data, elementName, context);
 
-            let dataCopy = _.cloneDeep(data);
-            /* The options field will be empty unless in the 'render' stage, so check
-               if it is populated before adding the element url */
-            if ('base_url' in data.options) {
-                /* Join the URL using Posix join to avoid generating a path with backslashes,
-                   as would be the case when running on Windows */
-                dataCopy.options.client_files_element_url = path.posix.join(data.options.base_url, 'elements', elementName, 'clientFilesElement');
-            }
             const pythonArgs = [elementHtml, dataCopy];
             const pythonFile = controller.replace(/\.[pP][yY]$/, '');
             const opts = {
@@ -205,11 +215,12 @@ module.exports = {
 
         const cwd = resolvedElement.directory;
         const controller = resolvedElement.controller;
+        const dataCopy = module.exports.getElementClientFiles(data, elementName, context);
 
         if (_.isString(controller)) {
             // python module
             const elementHtml = $(element).clone().wrap('<container/>').parent().html();
-            const pythonArgs = [elementHtml, data];
+            const pythonArgs = [elementHtml, dataCopy];
             const pythonFile = controller.replace(/\.[pP][yY]$/, '');
             const opts = {
                 cwd,
@@ -220,7 +231,7 @@ module.exports = {
                 if (err instanceof codeCaller.FunctionMissingError) {
                     // function wasn't present in server
                     debug(`elementFunction(): function not present`);
-                    return callback(null, module.exports.defaultElementFunctionRet(fcn, data), '');
+                    return callback(null, module.exports.defaultElementFunctionRet(fcn, dataCopy), '');
                 }
                 if (ERR(err, callback)) return;
                 debug(`elementFunction(): completed`);
@@ -228,7 +239,7 @@ module.exports = {
             });
         } else {
             // JS module (FIXME: delete this block of code in future)
-            const jsArgs = [$, element, null, data];
+            const jsArgs = [$, element, null, dataCopy];
             controller[fcn](...jsArgs, (err, ret) => {
                 if (ERR(err, callback)) return;
                 callback(null, ret, '');
@@ -705,6 +716,20 @@ module.exports = {
         }
     },
 
+    /**
+     * Gets any options that are available in any freeform phase.
+     * These include file paths that are relevant for questions and elements.
+     * URLs are not included here because those are only applicable during 'render'.
+     */
+    getContextOptions: function(context) {
+        /* These options are always available in any phase. */
+
+        let options = {};
+        options.question_path = context.question_dir;
+        options.client_files_question_path = path.join(context.question_dir, 'clientFilesQuestion');
+        return options;
+    },
+
     generate: function(question, course, variant_seed, callback) {
         debug('generate()');
         module.exports.getContext(question, course, (err, context) => {
@@ -717,6 +742,7 @@ module.exports = {
                 variant_seed: parseInt(variant_seed, 36),
                 options: _.defaults({}, course.options, question.options),
             };
+            _.extend(data.options, module.exports.getContextOptions(context));
             workers.getPythonCaller((err, pc) => {
                 if (ERR(err, callback)) return;
                 module.exports.processQuestion('generate', pc, data, context, (err, courseIssues, data, _html, _fileData, _renderedElementNames) => {
@@ -750,6 +776,7 @@ module.exports = {
                 variant_seed: parseInt(variant.variant_seed, 36),
                 options: _.get(variant, 'options', {}),
             };
+            _.extend(data.options, module.exports.getContextOptions(context));
             workers.getPythonCaller((err, pc) => {
                 if (ERR(err, callback)) return;
                 module.exports.processQuestion('prepare', pc, data, context, (err, courseIssues, data, _html, _fileData, _renderedElementNames) => {
@@ -813,8 +840,7 @@ module.exports = {
             data.options.workspace_url = locals.workspaceUrl || null;
 
             // Put key paths in data.options
-            data.options.question_path = context.question_dir;
-            data.options.client_files_question_path = path.join(context.question_dir, 'clientFilesQuestion');
+            _.extend(data.options, module.exports.getContextOptions(context));
 
             module.exports.getCachedDataOrCompute(
                 course,
@@ -1109,6 +1135,7 @@ module.exports = {
                 raw_submitted_answers: _.get(submission, 'raw_submitted_answer', {}),
                 gradable: _.get(submission, 'gradable', true),
             };
+            _.extend(data.options, module.exports.getContextOptions(context));
             workers.getPythonCaller((err, pc) => {
                 if (ERR(err, callback)) return;
                 module.exports.processQuestion('parse', pc, data, context, (err, courseIssues, data, _html, _fileData) => {
@@ -1154,6 +1181,7 @@ module.exports = {
                 raw_submitted_answers: submission.raw_submitted_answer,
                 gradable: submission.gradable,
             };
+            _.extend(data.options, module.exports.getContextOptions(context));
             workers.getPythonCaller((err, pc) => {
                 if (ERR(err, callback)) return;
                 module.exports.processQuestion('grade', pc, data, context, (err, courseIssues, data, _html, _fileData) => {
@@ -1201,6 +1229,7 @@ module.exports = {
                 gradable: true,
                 test_type: test_type,
             };
+            _.extend(data.options, module.exports.getContextOptions(context));
             workers.getPythonCaller((err, pc) => {
                 if (ERR(err, callback)) return;
                 module.exports.processQuestion('test', pc, data, context, (err, courseIssues, data, _html, _fileData) => {
