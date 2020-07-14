a161bd281feee2aafbafbb0044d382be016e8060
Yipeng <yipenghan98@gmail.com>
2020-07-14 01:26:22 -0500

    Refactor the code

--- workspace_host/interface.js
+++ workspace_host/interface.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express()
 const port = 8081;
-const { spawn, exec } = require("child_process");
+const { spawn, exec } = require("child_process");   // use dockerode and aws-sdk instead
+const Docker = require('dockerode');
+const fs = require("fs");
+const async = require('async');
+
 const bodyParser = require('body-parser');
 const osu = require('node-os-utils');
 const cpu = osu.cpu;
@@ -11,107 +15,194 @@ app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
     cpu.usage().then(v => {
+        // let them know that the workspace host is online
+        // also send something useful back
         res.status(200).send({cpu_usage: v});
     });
 });
 
 app.post('/', function(req, res) {
-    async function syncContainer(workspace_id, direction="push") {
-        if (direction == "push") {
-            var cmd = "aws s3 sync ./workspace-0 s3://arn:aws:s3:us-east-2:484505946769:accesspoint/access-point-bb/workspace-0";
-        } else if (direction == "pull") {
-            var cmd = "aws s3 sync s3://arn:aws:s3:us-east-2:484505946769:accesspoint/access-point-bb/question-0 ./workspace-0 && echo success";
-        } else {
-            console.log("Unknown command");
-            return false;
-        }
-        var obj = exec(cmd, function (error, stdout, stderr) {
-            if (stdout) { 
-                console.log("Sync successfully");         
-                return true;
+
+    async function _reinitializeS3(workspace_id, callback) {
+        var cmd = "aws s3 cp s3://pl-workspace/question-0 s3://pl-workspace/workspace-0 --recursive";
+
+        var _ = exec(cmd, function (error, stdout, stderr) {
+            if (stdout) {   
+                callback(null, workspace_id);
             } else if (stderr) {
-                console.log(stderr);
-                return false;
+                callback(stderr);
             } else if (error) {
-                console.log(error);
-                return false;
+                callback(error);
             };
         });
     };
 
-    async function initContainer(workspace_id) {
-        // Do what you gotta do to create the container
-
-        // remove --rm for production 
-        var ret = await syncContainer(workspace_id, "pull");
-        if (ret == false) {
-            console.log("Fail to load the origin question from S3");
-            res.status(500).send("Fail to load the origin question from S3");
+    async function _syncPullContainer(workspace_id, callback) {
+        if (fs.existsSync("./workspace-0")) {
+            // we already had a local copy of the latest code, no need to sync
+            callback(null, workspace_id);
             return;
         }
+        var cmd = "aws s3 sync s3://pl-workspace/workspace-0 ./workspace-0 && echo success";
+        
+        var _ = exec(cmd, function (error, stdout, stderr) {
+            if (stdout) {       
+                callback(null, workspace_id);
+            } else if (stderr) {
+                callback(stderr);
+            } else if (error) {
+                callback(error);
+            };
+        });
+    };
+
+    async function _syncPushContainer(workspace_id, callback) {
+        var cmd = "aws s3 sync ./workspace-0 s3://pl-workspace/workspace-0";
+        
+        var _ = exec(cmd, function (error, stdout, stderr) {
+            if (stdout) {       
+                callback(null, workspace_id);
+            } else if (stderr) {
+                callback(stderr);
+            } else if (error) {
+                callback(error);
+            };
+        });
+    };
+
+    async function _createContainer(workspace_id, callback) {
+        // remove --rm for production 
         var cmd = 'docker create -it --name "workspace-0" -p 13746:8080 -v "$PWD/workspace-0:/home/coder/project" -u "$(id -u):$(id -g)" codercom/code-server:latest --auth none';
-        var obj = exec(cmd, function (error, stdout, stderr) {
+
+        var _ = exec(cmd, function (error, stdout, stderr) {
             if (stdout) {
-                startContainer(workspace_id);
+                callback(null, workspace_id);
             } else if (stderr) {
                 if (stderr.includes("is already in use")) {
-                    startContainer(workspace_id);
+                    callback(null, workspace_id);
                 } else {
-                    res.status(500).send(stderr);
+                    callback(stderr);
                 }
             } else if (error) {
-                res.status(500).send(error);
+                callback(error);
             }
         });
     };
 
-    async function delContainer(workspace_id) {
+    async function _delContainer(workspace_id, callback) {
         // Do what you gotta do to shutdown the container
         var cmd = `docker rm workspace-${workspace_id}`;
-        var obj = exec(cmd, function (error, stdout, stderr) {
+        var _ = exec(cmd, function (error, stdout, stderr) {
             if (stdout) {          
-                res.status(200).send(`Container for workspace ${workspace_id} deleted.`);
+                console.log(`Container for workspace ${workspace_id} deleted.`);
+                callback(null, workspace_id);
             } else if (stderr) {
-                res.status(500).send(stderr);
+                callback(stderr);
             } else if (error) {
-                res.status(500).send(error);
+                callback(error);
             };
         });
     };
 
-    async function startContainer(workspace_id) {
+    async function _startContainer(workspace_id, callback) {
         // Do what you gotta do to start the container
         var cmd = `docker start workspace-${workspace_id} && sleep 2`;
-        var obj = exec(cmd, function (error, stdout, stderr) {
+        var _ = exec(cmd, function (error, stdout, stderr) {
             if (stdout) {          
-                res.status(200).send(`Container for workspace ${workspace_id} is now running.`);
+                console.log(`Container for workspace ${workspace_id} is now running.`);
+                callback(null, workspace_id);
             } else if (stderr) {
-                res.status(500).send(stderr);
+                callback(stderr);
             } else if (error) {
-                res.status(500).send(error);
+                callback(error);
             };
         });
     };
 
-    async function stopContainer(workspace_id) {
-        // Do what you gotta do to stop the container
-
-
-        var ret = await syncContainer(workspace_id, "push");
-        if (ret == false) {
-            console.log("Fail to push the code to S3");
-            res.status(500).send("Fail to push the code to S3");
-            return;
-        }
+    async function _stopContainer(workspace_id, callback) {
         var cmd = `docker stop workspace-${workspace_id}`;
-
-        var obj = exec(cmd, function (error, stdout, stderr) {
+        var _ = exec(cmd, function (error, stdout, stderr) {
             if (stdout) {          
-                res.status(200).send(`Container for workspace ${workspace_id} is now stopped.`);
+                console.log(`Container for workspace ${workspace_id} is now stopped.`);
+                callback(null, workspace_id);
             } else if (stderr) {
-                res.status(500).send(stderr);
+                callback(stderr);
             } else if (error) {
-                res.status(500).send(error);
+                callback(error);
+            };
+        });
+    };
+
+    // Called by the main server the first time a workspace is used by a user
+    function initSequence(workspace_id, res) {
+        async.waterfall([
+            (callback) => {_reinitializeS3(workspace_id, callback)},
+            _syncPullContainer,
+            _createContainer,
+            _startContainer,
+        ], function(err) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).send(`Container for workspace ${workspace_id} initialized.`);
+            };
+        });
+    };
+
+    // Called by the main server when the user comes back after leaving
+    // Workspace host will NOT check if the S3 resources is allocated correctly, the main server need to do the checking
+    function reloadSequence(workspace_id, res) {
+        async.waterfall([
+            (callback) => {_syncPullContainer(workspace_id, callback)},
+            _createContainer,
+            _startContainer,
+        ], function(err) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).send(`Container for workspace ${workspace_id} reloaded.`);
+            };
+        });
+    };
+
+    // Called by the main server when the user need to save the file to S3
+    // Will be trigger by the user's heartbeats and when user has lost connection with the websocket
+    function syncSequence(workspace_id, res) {
+        async.waterfall([
+            (callback) => {_syncPushContainer(workspace_id, callback)},
+        ], function(err) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).send(`Code of workspace ${workspace_id} pushed.`);
+            };
+        });
+    };
+
+    // Called by the main server when the user lose connection with the websocket
+    function stopSequence(workspace_id, res) {
+        async.waterfall([
+            (callback) => {_stopContainer(workspace_id, callback)},
+            _syncPushContainer,
+        ], function(err) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).send(`Container for workspace ${workspace_id} stopped.`);
+            };
+        });
+    };
+
+    // Usage unclear, maybe should be called automatically by the workspace host?
+    // Maybe should also remove the local copy of the code as well?
+    function destroySequence(workspace_id, res) {
+        async.waterfall([
+            (callback) => {_delContainer(workspace_id, callback)},
+        ], function(err) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).send(`Container for workspace ${workspace_id} destroyed.`);
             };
         });
     };
@@ -122,8 +213,8 @@ app.post('/', function(req, res) {
         res.status(500).send("Missing workspace_id");
     } else if (action == undefined) {
         res.status(500).send("Missing action");
-    } else if (["init", "del", "start", "stop", "sync"].includes(action)) {
-        eval(action + "Container")(workspace_id);
+    } else if (["init", "reload", "sync", "stop", "destroy"].includes(action)) {
+        eval(action + "Sequence")(workspace_id, res);
     } else if (action == "status") {
         // check the status
         res.status(200).send("Running");
