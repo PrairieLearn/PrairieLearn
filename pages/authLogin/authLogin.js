12e6fc74ff83eb26cda8647eb6dcffaa5fb77613
Matthew West <mwest@illinois.edu>
2020-07-17 11:45:41 -0500

    Fix startup error due to removed SQL file

--- pages/authLogin/authLogin.js
+++ pages/authLogin/authLogin.js
@@ -1,13 +1,5 @@
-var ERR = require('async-stacktrace');
 var express = require('express');
 var router = express.Router();
-
-var sqldb = require('@prairielearn/prairielib').sqlDb;
-var sqlLoader = require('@prairielearn/prairielib').sqlLoader;
-
-var config = require('../../lib/config');
-
-var sql = sqlLoader.loadSqlEquiv(__filename);
 
 router.get('/', function(req, res, next) {
     // We could set res.locals.config.hasOauth = false (or
