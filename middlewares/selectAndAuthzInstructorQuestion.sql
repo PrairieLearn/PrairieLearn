017456397a442baed34bf2d997060eea0cb21dee
Tim Yang <timothy.d.yang@gmail.com>
2020-07-17 19:57:10 -0500

    Change to adding all users as XC 101 viewers

--- middlewares/selectAndAuthzInstructorQuestion.sql
+++ middlewares/selectAndAuthzInstructorQuestion.sql
@@ -1,25 +1,14 @@
 -- BLOCK insert_xc101_viewer
 WITH
-course_permissions_for_user AS (
-    SELECT
-        *
-    FROM
-        course_permissions AS cp
-    WHERE
-        cp.user_id = $user_id
-),
 example_course AS (
     SELECT * FROM pl_courses WHERE (options->'isExampleCourse')::boolean IS TRUE
 )
-INSERT INTO course_permissions (user_id, course_id, course_role)
-    SELECT
-        cp.user_id, xc.id, 'Viewer'
-    FROM
-        course_permissions_for_user AS cp
-    JOIN example_course AS xc ON (xc.id != cp.course_id)
-    WHERE
-        cp.course_role IN ('Owner', 'Editor')
-    LIMIT 1
+INSERT INTO course_permissions
+    (user_id, course_id, course_role)
+SELECT
+    $user_id, xc.id, 'Viewer'
+FROM
+    example_course as xc
 ON CONFLICT DO NOTHING
 
 -- BLOCK select_and_auth
