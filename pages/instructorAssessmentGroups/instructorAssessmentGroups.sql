14f222b433bb7794f302f80a3044a0b744c44e84
frankieyuan <frankie.yuan@yahoo.com>
2020-07-20 23:27:59 -0500

    Optimize sql in instructorGroup to speed up the displaying (used to employ async javascript for loop)

--- pages/instructorAssessmentGroups/instructorAssessmentGroups.sql
+++ pages/instructorAssessmentGroups/instructorAssessmentGroups.sql
@@ -18,21 +18,34 @@ WHERE
 ORDER BY tid;
 
 -- BLOCK select_group_users
-SELECT DISTINCT
-    u.uid,
-    gr.name,
-    gu.group_id AS GID,
-    e.role
+SELECT
+    gr.id AS gid,
+    gr.name AS name,
+    COUNT(u.uid) AS size,
+    array_agg(u.uid) AS uid_list
 FROM
     groups AS gr
     JOIN group_users AS gu ON gu.group_id = gr.id AND gr.deleted_at IS NULL
-    RIGHT JOIN enrollments AS e ON e.user_id = gu.user_id
-    JOIN users AS u ON u.user_id = e.user_id
+    JOIN users AS u ON u.user_id = gu.user_id
 WHERE
     gr.group_config_id = $group_config_id
-    OR gr.group_config_id IS NULL
-    AND e.course_instance_id = $course_instance_id
-ORDER BY GID
+GROUP BY
+    gr.id
+ORDER BY
+    gr.id;
+
+-- BLOCK select_not_in_group
+SELECT
+    u.uid
+FROM
+    groups AS gr
+    JOIN group_users AS gu ON gr.group_config_id = $group_config_id AND gu.group_id = gr.id AND gr.deleted_at IS NULL
+    RIGHT JOIN enrollments AS e ON e.user_id = gu.user_id AND e.course_instance_id = $course_instance_id
+    JOIN users AS u ON u.user_id = e.user_id
+WHERE
+    gr.id IS NULL
+    AND e.role = 'Student'
+ORDER BY u.uid;
 
 --BLOCK config_group
 UPDATE 
