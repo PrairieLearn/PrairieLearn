831b8ab1f46283e58eafd5dcfad703c3c6410a72
ZhiningQiu <aaronpapa@ZhiningQius-MacBook-Pro.local>
2020-07-21 22:59:10 +0800

    final update on variants_insert for group

--- sprocs/variants_insert.sql
+++ sprocs/variants_insert.sql
@@ -47,7 +47,7 @@ BEGIN
             JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
             JOIN assessments AS a ON (a.id = ai.assessment_id)
             JOIN course_instances AS ci ON (ci.id = a.course_instance_id)
-            LEFT OUTER JOIN groups AS g ON (g.id = ai.group_id)
+            LEFT OUTER JOIN groups AS g ON (g.id = ai.group_id AND g.deleted_at IS NULL)
             LEFT OUTER JOIN users AS u ON (u.user_id = ai.user_id)
         WHERE
             iq.id = instance_question_id;
