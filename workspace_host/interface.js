f91e04d09c347271366c31ac61a1c5321435343a
polohan <32109208+polohan@users.noreply.github.com>
2020-07-25 17:30:38 -0500

    Add workspace server/host support for development (#2723)
    * Add support to automatic initlize the workspace host on load
    
    * Add support for auto saving
    
    * Add code for workspace host
    
    * Refactor the code
    
    * Change the functions from async to normal function
    
    * Replace exec() call to control docker to dockerode functions
    
    * Remove some experimental functionality
    
    * Add code to delete all local code in _delContainer()
    
    * Replace exec() call to AWS SDK calls
    
    * Add code to import the config so that interface.js can be run locally
    
    * Change the hardcoded path and fix some minor error handling issue
    
    * README update
    
    * minor README fix
    
    * must run interface from main PL dir
    
    * Fix CI
    
    * Update socket.io path
    
    Co-authored-by: Nicolas Nytko <nicknytko@gmail.com>
    
    * Replace the localhost with host.docker.internal
    
    * Using config.json to import the workspace bucket name
    
    * Fully support creating new workspace container on demand with auto-port-assigning
    
    * add config.workspaceLocalhost parameter
    
    * add xterm container
    
    * add error reporting to workspace socket
    
    * Add workspace init for S3/DB
    
    * Fix callback
    
    * Update workspace.js
    
    * Remove example-files dir
    
    * Fix typo
    
    * Update lib/workspaceSocket.js
    
    * Add support for running `interface.js` inside container
    
    * Fix push/pull paths when running natively
    
    * Update readme for containerized `interface.js`
    
    * Update readme to mention external grading flags
    
    * change route order, update url rewriting, add jupyter container
    
    * Add Jupyter Lab instructions
    
    Co-authored-by: Tim Yang <timothy.d.yang@gmail.com>
    Co-authored-by: Matthew West <mwest@illinois.edu>
    Co-authored-by: Tim Yang <tdy@users.noreply.github.com>
    Co-authored-by: Nicolas Nytko <nicknytko@gmail.com>

---
+++ workspace_host/interface.js
@@ -0,0 +1,420 @@
+const express = require('express');
+const app = express()
+const port = 8081;
+const { spawn, exec } = require("child_process");   // use dockerode and aws-sdk instead
+const path = require("path");
+const AWS = require('aws-sdk');
+const Docker = require('dockerode');
+const fs = require("fs");
+const async = require('async');
+const logger = require('../lib/logger');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+var net = require('net');
+
+const aws = require('../lib/aws.js');
+aws.init((err) => {
+    if (err) logger.debug(err);
+});
+
+const config = require('../lib/config.js');
+config.loadConfig("config.json");
+
+const workspaceBucketName = config.workspaceS3Bucket;
+if (workspaceBucketName == '') {
+    logger.warn("Workspace bucket is not configed correctly. Check config.json.");
+} else {
+    logger.info("Workspace bucket is configed to: " + workspaceBucketName);
+}
+
+const bodyParser = require('body-parser');
+const docker = new Docker();
+
+var id_port_mapper = {};
+var port_id_mapper = {};
+const workspaceProxyOptions = {
+    target: 'invalid',
+    ws: true,
+    pathRewrite: (path) => {
+        var match = path.match('/workspace/[0-9]+/container/*');
+        if (match) {
+            var substring = match[0];
+            return path.replace(substring, '');
+        } else {
+            return path;
+        }
+    },
+    logProvider: _provider => logger,
+    router: (req) => {
+        var url = req.url;
+        var workspace_id = parseInt(url.replace("/workspace/", ""));
+        if (workspace_id in id_port_mapper) {
+            url = `http://${config.workspaceNativeLocalhost}:${id_port_mapper[workspace_id]}/`;
+            return url;
+        } else {
+            return "";
+        };
+    },
+};
+const workspaceProxy = createProxyMiddleware(workspaceProxyOptions);
+app.use('/workspace/([0-9])+/*', workspaceProxy);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+// For development
+app.put('/', function(req, res) {
+    var workspace_id = req.body.workspace_id;
+    var port = req.body.port;
+    id_port_mapper[workspace_id] = port;
+    port_id_mapper[port] = workspace_id;
+    res.status(200).send("OK");
+});
+
+app.post('/', function(req, res) {
+    var workspace_id = req.body.workspace_id;
+    var action = req.body.action;
+    if (workspace_id == undefined) {
+        res.status(500).send("Missing workspace_id");
+    } else if (action == undefined) {
+        res.status(500).send("Missing action");
+    } else if (["init", "sync", "reset", "destroy"].includes(action)) {
+        eval(action + "Sequence")(workspace_id, res);
+    } else if (action == "status") {
+        res.status(200).send("Running");
+    } else {
+        res.status(500).send(`Action "${action}" undefined`);
+    };
+});
+
+app.listen(port, () => console.log(`Listening on http://${config.workspaceNativeLocalhost}:${port}/`));
+
+function _getAvailablePort(workspace_id, curPort, callback) {
+    if (curPort > 65535) {
+        callback("No available port at this time.");
+        return;
+    };
+    var server = net.createServer()
+    server.listen(curPort, function (err) {
+        server.once('close', function () {
+            callback(null, workspace_id, curPort);
+        });
+        server.close();
+    });
+    server.on('error', function (err) {
+        _getAvailablePort(workspace_id, curPort + 1, callback);
+        return;
+    });
+};
+
+// DEPRECATED
+function _resetS3(workspace_id, callback) {
+    var cmd = "aws s3 cp s3://pl-workspace/question-0 s3://pl-workspace/workspace-0 --recursive";
+
+    var _ = exec(cmd, function (error, stdout, stderr) {
+        if (stdout) {
+            callback(null, workspace_id);
+        } else if (stderr) {
+            callback(stderr);
+        } else if (error) {
+            callback(error);
+        };
+    });
+};
+
+function _uploadToS3(filePath, S3FilePath, callback) {
+    s3 = new AWS.S3();
+    var uploadParams = {
+        Bucket: workspaceBucketName,
+        Key: S3FilePath,
+        Body: fs.readFileSync(filePath),
+    };
+    s3.upload(uploadParams, function(err, data) {
+        if (err) {
+            callback(null, [filePath, S3FilePath, err]);
+            return;
+        };
+        callback(null, "OK");
+    });
+};
+
+function _downloadFromS3(filePath, S3FilePath, callback) {
+    if (!fs.existsSync(path.dirname(filePath))){
+        fs.mkdirSync(path.dirname(filePath), { recursive: true });
+    };
+    s3 = new AWS.S3();
+    var downloadParams = {
+        Bucket: workspaceBucketName,
+        Key: S3FilePath
+    };
+    var fileStream = fs.createWriteStream(filePath);
+    var s3Stream = s3.getObject(downloadParams).createReadStream();
+
+    s3Stream.on('error', function(err) {
+        // This is for errors like no such file on S3, etc
+        callback(null, [filePath, S3FilePath, err]);
+        return;
+    });
+
+    s3Stream.pipe(fileStream).on('error', function(err) {
+        // This is for errors like the connection is lost, etc
+        callback(null, [filePath, S3FilePath, err]);
+    }).on('close', function() {
+        callback(null, "OK");
+    });
+};
+
+function _recursiveUploadJobManager(curDirPath, S3curDirPath) {
+    var ret = [];
+    fs.readdirSync(curDirPath).forEach(function (name) {
+        var filePath = path.join(curDirPath, name);
+        var S3filePath = path.join(S3curDirPath, name);
+        var stat = fs.statSync(filePath);
+        if (stat.isFile()) {
+            ret.push([filePath, S3filePath]);
+        } else if (stat.isDirectory()) {
+            ret = ret.concat(_recursiveUploadJobManager(filePath, S3filePath));
+        };
+    });
+    return ret;
+};
+
+function _recursiveDownloadJobManager(curDirPath, S3curDirPath, callback) {
+    s3 = new AWS.S3();
+    var listingParams = {
+        Bucket: workspaceBucketName,
+        Prefix: S3curDirPath
+    };
+
+    s3.listObjectsV2(listingParams, (err, data) => {
+        if (err) {
+            callback(err);
+            return;
+        };
+        var contents = data["Contents"];
+        var ret = [];
+        contents.forEach(dict => {
+          if ("Key" in dict) {
+              var filePath = path.join(curDirPath, dict["Key"].slice(S3curDirPath.length));
+              var S3filePath = dict["Key"];
+              ret.push([filePath, S3filePath]);
+          };
+      });
+      callback(null, ret);
+    });
+};
+
+function _syncPullContainer(workspace_id, callback) {
+    const workspacePrefix = process.env.HOST_JOBS_DIR ? '/jobs' : process.cwd();
+    const workspaceName= `workspace-${workspace_id}`;
+    _recursiveDownloadJobManager(`${workspacePrefix}/${workspaceName}`, workspaceName, (err, jobs_params) => {
+        if (err) {
+            callback(err);
+            return;
+        };
+        var jobs = [];
+        jobs_params.forEach(([filePath, S3filePath]) => {
+            jobs.push( ((mockCallback) => {
+                _downloadFromS3(filePath, S3filePath, mockCallback);
+            }));
+        });
+
+        var status = [];
+        async.parallel(jobs, function(_, results) {
+            results.forEach((res) => {
+                if (res != "OK") {
+                    res[2].fileLocalPath = res[0];
+                    res[2].fileS3Path = res[0];
+                    status.push(res[2]);
+                };
+            });
+            if (status.length != 0) {
+                callback(status);
+            } else {
+                callback(null, workspace_id);
+            }
+        });
+    });
+};
+
+async function _syncPushContainer(workspace_id, callback) {
+    const workspacePrefix = process.env.HOST_JOBS_DIR ? '/jobs' : process.cwd();
+    const workspaceName= `workspace-${workspace_id}`;
+    if (!fs.existsSync(workspaceName)) {
+        // we didn't a local copy of the code, DO NOT sync
+        callback(null, workspace_id);
+        return;
+    }
+    var jobs_params = _recursiveUploadJobManager(`${workspacePrefix}/${workspaceName}`, workspaceName);
+    var jobs = [];
+    jobs_params.forEach(([filePath, S3filePath]) => {
+        jobs.push( ((mockCallback) => {
+            _uploadToS3(filePath, S3filePath, mockCallback);
+        }));
+    });
+
+    var status = [];
+    async.parallel(jobs, function(_, results) {
+        results.forEach((res) => {
+            if (res != "OK") {
+                res[2].fileLocalPath = res[0];
+                res[2].fileS3Path = res[0];
+                status.push(res[2]);
+            };
+        });
+        if (status.length != 0) {
+            callback(status);
+        } else {
+            callback(null, workspace_id);
+        };
+    });
+};
+
+function _getContainer(workspace_id, callback) {
+    var container = docker.getContainer("workspace-" + workspace_id);
+    callback(null, container);
+};
+
+function _createContainer(workspace_id, port, callback) {
+    const workspaceName = 'workspace-' + workspace_id;
+    const workspaceDir = process.env.HOST_JOBS_DIR || process.cwd();
+    const workspacePath = path.join(workspaceDir, workspaceName);
+    const containerPath = '/home/coder/project';
+    logger.info("Workspace path is configed to: " + workspacePath);
+    docker.createContainer({
+        Image: 'codercom/code-server',
+        ExposedPorts: {
+            "8080/tcp": {},
+        },
+        Env: [
+            `WORKSPACE_BASE_URL=/workspace/${workspace_id}/container/`,
+        ],
+        HostConfig: {
+            PortBindings: {'8080/tcp': [{"HostPort": port.toString()}]},
+            Binds: [`${workspacePath}:${containerPath}`],
+            // Copied directly from externalGraderLocal.js
+            Memory: 1 << 30, // 1 GiB
+            MemorySwap: 1 << 30, // same as Memory, so no access to swap
+            KernelMemory: 1 << 29, // 512 MiB
+            DiskQuota: 1 << 30, // 1 GiB
+            IpcMode: 'private',
+            CpuPeriod: 100000, // microseconds
+            CpuQuota: 90000, // portion of the CpuPeriod for this container
+            PidsLimit: 1024,
+        },
+        Cmd: ['--auth', 'none'],
+        name: workspaceName,
+        Volumes: {
+            [containerPath]: {}
+          },
+    }, (err, container) => {
+        if (err) {
+            if (err.toString().includes("is already in use")) {
+                _getContainer(workspace_id, callback);
+            } else {
+                callback(err);
+            };
+        } else {
+            id_port_mapper[workspace_id] = port;
+            port_id_mapper[port] = workspace_id;
+            callback(null, container);
+        };
+    });
+};
+
+function _delContainer(container, callback) {
+    var workspaceName= container.id;
+    // Require Node.js 12.10.0 or later otherwise it will complain that the folder isn't empty
+    fs.rmdirSync(workspaceName, { recursive: true });
+    container.remove((err) => {
+        if (err) {
+            callback(err);
+        } else {
+            callback(null, container);
+        };
+    });
+};
+
+function _startContainer(container, callback) {
+    container.start((err) => {
+        if (err) {
+            if (err.toString().includes("already started")) {
+                callback(null, container);
+            } else {
+                callback(err);
+            };
+        } else {
+            callback(null, container);
+        };
+    });
+};
+
+function _stopContainer(container, callback) {
+    container.stop((err) => {
+        if (err) {
+            callback(err);
+        } else {
+            callback(null, container);
+        };
+    });
+};
+
+// Called by the main server the first time a workspace is used by a user
+function initSequence(workspace_id, res) {
+    async.waterfall([
+        (callback) => {_syncPullContainer(workspace_id, callback)},
+        (workspace_id, callback) => {_getAvailablePort(workspace_id, 1024, callback)},
+        _createContainer,
+        _syncPullContainer,
+        _startContainer,
+    ], function(err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(`Container for workspace ${workspace_id} initialized.`);
+        };
+    });
+};
+
+// Called by the main server when the user need to save the file to S3
+// Will be trigger by the user's heartbeats and when user has lost connection with the websocket
+function syncSequence(workspace_id, res) {
+    async.waterfall([
+        (callback) => {_syncPushContainer(workspace_id, callback)},
+    ], function(err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(`Code of workspace ${workspace_id} pushed.`);
+        };
+    });
+};
+
+// Called by the main server when the user want to reset the file to default
+function resetSequence(workspace_id, res) {
+    async.waterfall([
+        (callback) => {_syncPullContainer(workspace_id, callback)},
+    ], function(err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(`Code of workspace ${workspace_id} reset.`);
+        };
+    });
+};
+
+// Usage unclear, maybe should be called automatically by the workspace host?
+// Maybe should also remove the local copy of the code as well?
+function destroySequence(workspace_id, res) {
+    async.waterfall([
+        (callback) => {_syncPushContainer(workspace_id, callback)},
+        _getContainer,
+        _stopContainer,
+        _delContainer
+    ], function(err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(`Container for workspace ${workspace_id} destroyed.`);
+        };
+    });
+};
