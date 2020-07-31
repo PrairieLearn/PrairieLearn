9e454d288f070dbf3ac6ee521c783f861200d199
Matthew West <mwest@illinois.edu>
2020-07-31 08:52:53 -0500

    Add dev mode script to kill leftover workspaces (#2792)

--- docker/start_workspace.sh
+++ docker/start_workspace.sh
@@ -3,6 +3,14 @@
 TMUX_CONF=/PrairieLearn/.tmux.conf
 [[ -f $TMUX_CONF ]] && args=(-f $TMUX_CONF) || args=()
 
+# kill any containers with a name like workspace-*
+CONTAINERS=$(docker ps -aq --filter "name=workspace-")
+if [[ ! -z "$CONTAINERS" ]] ; then
+   echo Killing existing workspace containers: $CONTAINERS
+   docker kill $CONTAINERS
+   docker rm $CONTAINERS
+fi
+
 cd /PrairieLearn
 tmux "${args[@]}" new-session \; \
   send-keys "docker/init.sh" C-m \; \
