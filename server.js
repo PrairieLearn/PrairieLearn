8eab4dcaca9ac532fcdabc52f5a1394167e237bb
Tim Yang <tdy@users.noreply.github.com>
2020-07-31 23:45:33 -0500

    Fix workspace state machine (#2779)
    * start fixing state machine
    
    * change sqldb queries to calls
    
    * add 'running' -> 'stopped' transition
    
    * refactor state machine updater into lib/workspace
    
    * combine lib/workspace and lib/workspaceSocket
    
    * lint unused lib
    
    * add logging event for state transitions
    
    * WIP
    
    * unify launch sequence and convert to async/await
    
    Co-authored-by: Matthew West <mwest@illinois.edu>

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const aws = require('./lib/aws.js');
 const externalGrader = require('./lib/externalGrader');
 const externalGraderResults = require('./lib/externalGraderResults');
 const externalGradingSocket = require('./lib/externalGradingSocket');
-const workspaceSocket = require('./lib/workspaceSocket');
+const workspace = require('./lib/workspace');
 const assessment = require('./lib/assessment');
 const { sqldb, migrations } = require('@prairielearn/prairielib');
 const sprocs = require('./sprocs');
@@ -1049,10 +1049,8 @@ if (config.startServer) {
             });
         },
         function(callback) {
-            workspaceSocket.init(function(err) {
-                if (ERR(err, callback)) return;
-                callback(null);
-            });
+            workspace.init();
+            callback(null);
         },
         function(callback) {
             serverJobs.init(function(err) {
