eafbb7b4a26aa788748a6ef28cd4cebfb0021ee9
Tim Yang <timothy.d.yang@gmail.com>
2020-08-04 23:45:53 -0500

    change to local redis default

--- docker/init.sh
+++ docker/init.sh
@@ -15,9 +15,7 @@ else
     docker/start_s3rver.sh
     docker/start_postgres.sh
     docker/gen_ssl.sh
-
-    # Uncomment to start redis to test message passing
-    # docker/start_redis.sh
+    docker/start_redis.sh
 
     if [[ $NODEMON == "true" ]]; then
         npm run start-nodemon
