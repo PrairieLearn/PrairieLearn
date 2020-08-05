f66682fb3780585e1119480c0257d581b24f3b97
Tim Yang <timothy.d.yang@gmail.com>
2020-08-04 22:40:22 -0500

    add missing comment marker

--- docker/init.sh
+++ docker/init.sh
@@ -17,7 +17,7 @@ else
     docker/gen_ssl.sh
 
     # Uncomment to start redis to test message passing
-    docker/start_redis.sh
+    # docker/start_redis.sh
 
     if [[ $NODEMON == "true" ]]; then
         npm run start-nodemon
