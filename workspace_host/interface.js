27129734d24dd251ce3c602cac8baadda71ad2b5
Yipeng <yipenghan98@gmail.com>
2020-08-06 00:10:57 -0500

    Modify the code to skip the not exist graded files

--- workspace_host/interface.js
+++ workspace_host/interface.js
@@ -15,7 +15,6 @@ const { v4: uuidv4 } = require('uuid');
 const argv = require('yargs-parser') (process.argv.slice(2));
 const tar = require('tar');
 
-
 const aws = require('../lib/aws.js');
 aws.init((err) => {
     if (err) logger.debug(err);
@@ -116,8 +115,6 @@ app.post('/', function(req, res) {
         res.status(500).send(`Action '${action}' undefined`);
     }
 });
-
-
 
 app.listen(config.workspaceDevHostPort, () => console.log(`Listening on port ${config.workspaceDevHostPort}`));
 
@@ -728,19 +725,20 @@ function destroySequence(workspace_id, res) {
 
 function gradeSequence(workspace_id, res) {
     var workspace_dir = `${workspacePrefix}/${id_workspace_mapper[workspace_id].localName}/project`;
-    var graded_file_list = id_workspace_mapper[workspace_id].settings.workspace_graded_files.map((file) => {return path.join(workspace_dir, file);});
-    graded_file_list = ['starter_code.c', 'starter_code.h'].map((file) => {return path.join(workspace_dir, file);});    // since in the DB the workspace_graded_file col is empty, hardcode file list
-    console.log(graded_file_list);
+    var graded_file_list = id_workspace_mapper[workspace_id].settings.workspace_graded_files;
     var timestamp = new Date().toISOString().replace(/T/, '*').replace(/\..+/, '');
     var zip_file_name = `workspace-${workspace_id}-${timestamp}.tgz`;
+    var exist_graded_file_list = [];
 
     async.series([
         async () => {
-            for (var file_path in graded_file_list) {
+            for (const file of graded_file_list) {
                 try {
+                    const file_path = path.join(workspace_dir, file);
                     await fsPromises.lstat(file_path);
+                    exist_graded_file_list.push(file);
                 } catch(err) {
-                    return(`Required graded file ${path.basename(file_path)} does not exist.`);
+                    continue;
                 }
             }
             return null;
@@ -748,11 +746,13 @@ function gradeSequence(workspace_id, res) {
         (callback) => {
             tar.create(
                 {
-                  gzip: true,
-                  file: zip_file_name,
-                  sync: false,
+                    gzip: true,
+                    file: zip_file_name,
+                    sync: false,
+                    cwd: workspace_dir,
+                    noDirRecurse: true,
                 },
-                graded_file_list,
+                exist_graded_file_list,
                 (err) => {
                     if (err) {
                         res.status(500).send(err);
