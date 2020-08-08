375881e2873a7f3f381c48d5ccd2e7dd802d23c8
Nathan Walters <nwalters512@gmail.com>
2020-08-07 18:54:28 -0700

    Fix lint issues

--- tests/testChunks.js
+++ tests/testChunks.js
@@ -5,32 +5,32 @@ const chunksLib = require('../lib/chunks');
 const COURSE = {
     questionDB: {
         'simple-question': {},
-        'complex/question': {}
+        'complex/question': {},
     },
     courseInstanceDB: {
         'simple-course-instance': {
             assessmentDB: {
                 'simple-assessment': {},
                 'complex/assessment': {},
-            }
+            },
         },
         'complex/course/instance': {
             assessmentDB: {
                 'simple-assessment': {},
                 'complex/assessment': {},
-            }
-        }
-    }
-}
+            },
+        },
+    },
+};
 
 describe('chunks', () => {
     describe('identifyChunksFromChangedFiles', () => {
         it('should identify simple question', () => {
             const chunks = chunksLib.identifyChunksFromChangedFiles(
                 ['questions/simple-question/tests/test.py'],
-                COURSE
+                COURSE,
             );
             assert.isOk(chunks.questionChunks.has('simple-question'));
-        })
-    })
+        });
+    });
 });
