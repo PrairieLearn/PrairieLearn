bdf696d62ff670484da9cedddbcb513d2d30e9b9
Mariana Silva <mfsilva@illinois.edu>
2020-08-11 12:10:12 -0500

    fixed code simple-example

--- exampleCourse/questions/demo/autograder/python/parson-simple-example/server.py
+++ exampleCourse/questions/demo/autograder/python/parson-simple-example/server.py
@@ -1,6 +1,8 @@
 
 def generate(data):
-    data["params"]["names_for_user"] = []
+    data["params"]["names_for_user"] = [
+        {"name": "a", "description": "The input array", "type": "array"}
+    ]
     data["params"]["names_from_user"] = [
-        {"name": "make_array_b", "description": "The new created array", "type": "array"}
+        {"name": "make_array_b", "description": "The user-defined function", "type": "function"}
     ]
--- exampleCourse/questions/demo/autograder/python/parson-simple-example/tests/trailing_code.py
+++ exampleCourse/questions/demo/autograder/python/parson-simple-example/tests/trailing_code.py
@@ -1,1 +1,6 @@
-    print (make_array_b(a))
+
+print("Given a =")
+print(a)
+print("Your function returns b = ")
+b = make_array_b(a)
+print(b)
