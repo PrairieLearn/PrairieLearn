a37815ecf57c56b66528ca0fc1714b9c70e530f4
Tim Yang <timothy.d.yang@gmail.com>
2020-08-13 15:49:03 -0500

    Fix pg mismatch

--- migrations/197_workspaces__created_at__add.sql
+++ migrations/197_workspaces__created_at__add.sql
@@ -1,1 +1,1 @@
-ALTER TABLE workspaces ADD COLUMN IF NOT EXISTS created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE workspaces ADD COLUMN IF NOT EXISTS created_at timestamp with time zone NOT NULL DEFAULT current_timestamp;
