1db6af1e04efbee5aa64a90df9f5cf51ee5e08fa
Tim Yang <tdy@users.noreply.github.com>
2020-08-14 15:40:20 -0500

    Update workspace_host/interface.js
    Co-authored-by: Matthew West <mwest@illinois.edu>

--- workspace_host/interface.js
+++ workspace_host/interface.js
@@ -693,7 +693,7 @@ function _recursiveDownloadJobManager(curDirPath, S3curDirPath, callback) {
 }
 
 async function _syncInitialZipAsync(workspace) {
-    workspaceHelper.updateMessage(workspace.id, 'Preparing initial files');
+    workspaceHelper.updateMessage(workspace.id, 'Loading initial files');
     const localName = workspace.local_name;
     const s3Name = workspace.s3_name;
     const localPath = `${workspacePrefix}/${localName}`;
