49ececbb74df686016a4c732db62f9ec316a49fb
Tim Yang <timothy.d.yang@gmail.com>
2020-08-17 15:30:27 -0500

    Add workspace versioning to support resets

--- pages/workspace/workspace.js
+++ pages/workspace/workspace.js
@@ -7,6 +7,10 @@ const logger = require('../../lib/logger');
 const workspaceHelper = require('../../lib/workspace');
 
 const error = require('@prairielearn/prairielib/error');
+const sqldb = require('@prairielearn/prairielib/sql-db');
+const sqlLoader = require('@prairielearn/prairielib/sql-loader');
+
+const sql = sqlLoader.loadSqlEquiv(__filename);
 
 router.get('/:workspace_id', (req, res, _next) => {
     res.locals.workspace_id = req.params.workspace_id;
@@ -21,6 +25,11 @@ router.get('/:workspace_id/:action', asyncHandler(async (req, res, next) => {
     if (action === 'reboot') {
         logger.info(`Rebooting workspace ${workspace_id}.`);
         await workspaceHelper.updateState(workspace_id, 'stopped', 'Rebooting container');
+        res.redirect(`/pl/workspace/${workspace_id}`);
+    } else if (action === 'reset') {
+        logger.info(`Resetting workspace ${workspace_id}.`);
+        await workspaceHelper.updateState(workspace_id, 'uninitialized', 'Resetting container');
+        await sqldb.queryAsync(sql.increment_workspace_version, {workspace_id});
         res.redirect(`/pl/workspace/${workspace_id}`);
     } else {
         return next(error.make(400, 'unknown action', {locals: res.locals, body: req.body}));
