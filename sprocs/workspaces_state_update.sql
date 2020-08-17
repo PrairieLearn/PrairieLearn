817f8e4e4768b749edb805f67c22d24b861ddcb9
Tim Yang <timothy.d.yang@gmail.com>
2020-08-17 16:17:54 -0500

    Add workspace version to logging

--- sprocs/workspaces_state_update.sql
+++ sprocs/workspaces_state_update.sql
@@ -5,6 +5,8 @@ CREATE OR REPLACE FUNCTION
         workspace_message text
     ) returns void
 AS $$
+DECLARE
+    workspace_version bigint;
 BEGIN
     UPDATE
         workspaces as w
@@ -14,10 +16,12 @@ BEGIN
         message = workspace_message,
         message_updated_at = now()
     WHERE
-        w.id = workspace_id;
+        w.id = workspace_id
+    RETURNING
+        w.version INTO workspace_version;
 
     INSERT INTO workspace_logs
-        (date, level, message, workspace_id)
+        (date, level, message, workspace_id, workspace_version)
     VALUES
         (now(),
          'info'::enum_log_level,
@@ -26,6 +30,7 @@ BEGIN
              THEN 'state:' || workspace_state
              ELSE 'state:' || workspace_state || ', message:' || workspace_message
          END,
-         workspace_id);
+         workspace_id,
+         workspace_version);
 END;
 $$ LANGUAGE plpgsql VOLATILE;
