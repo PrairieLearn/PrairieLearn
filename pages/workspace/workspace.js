6aef3b29a796304ba588fc4e0f9520369475a1fe
Tim Yang <timothy.d.yang@gmail.com>
2020-08-18 20:58:33 -0500

    Change logger.info -> debug

--- pages/workspace/workspace.js
+++ pages/workspace/workspace.js
@@ -1,11 +1,12 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
 
 const config = require('../../lib/config');
-const logger = require('../../lib/logger');
 const workspaceHelper = require('../../lib/workspace');
 
+const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
 const error = require('@prairielearn/prairielib/error');
 const sqldb = require('@prairielearn/prairielib/sql-db');
 const sqlLoader = require('@prairielearn/prairielib/sql-loader');
@@ -23,11 +24,11 @@ router.get('/:workspace_id/:action', asyncHandler(async (req, res, next) => {
     const action = req.params.action;
 
     if (action === 'reboot') {
-        logger.info(`Rebooting workspace ${workspace_id}.`);
+        debug(`Rebooting workspace ${workspace_id}`);
         await workspaceHelper.updateState(workspace_id, 'stopped', 'Rebooting container');
         res.redirect(`/pl/workspace/${workspace_id}`);
     } else if (action === 'reset') {
-        logger.info(`Resetting workspace ${workspace_id}.`);
+        debug(`Resetting workspace ${workspace_id}`);
         await workspaceHelper.updateState(workspace_id, 'uninitialized', 'Resetting container');
         await sqldb.queryAsync(sql.increment_workspace_version, {workspace_id});
         res.redirect(`/pl/workspace/${workspace_id}`);
