f7650c5ad1643e7f8cebccac13d94a047634d67f
frankieyuan <frankie.yuan@yahoo.com>
2020-08-23 00:04:13 -0500

    basic functionality of group exam mode

--- pages/studentAssessmentExam/studentAssessmentExam.js
+++ pages/studentAssessmentExam/studentAssessmentExam.js
@@ -21,7 +21,30 @@ router.get('/', function(req, res, next) {
         sqldb.query(sql.select_single_assessment_instance, params, function(err, result) {
             if (ERR(err, next)) return;
             if (result.rowCount == 0) {
-                res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+                if (res.locals.assessment.group_work) {
+                    sqldb.query(sql.get_config_info, params, function(err, result) {
+                        if (ERR(err, next)) return;
+                        res.locals.permissions = result.rows[0];
+                        res.locals.minsize = result.rows[0].minimum || 0;
+                        res.locals.maxsize = result.rows[0].maximum || 999;
+                        sqldb.query(sql.get_group_info, params, function(err, result) {
+                            if (ERR(err, next)) return;
+                            res.locals.groupsize = result.rowCount;
+                            res.locals.needsize = res.locals.minsize - res.locals.groupsize;
+                            if (res.locals.groupsize > 0) {
+                                res.locals.groupinfo = result.rows;
+                                res.locals.joincode = res.locals.groupinfo[0].name + '-' + res.locals.groupinfo[0].join_code;
+                                res.locals.start = false;
+                                if (res.locals.needsize <= 0) {
+                                    res.locals.start = true;
+                                }
+                            }
+                            res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+                        });
+                    });
+                } else {
+                    res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+                }
             } else {
                 res.redirect(res.locals.urlPrefix + '/assessment_instance/' + result.rows[0].id);
             }
@@ -33,10 +56,30 @@ router.post('/', function(req, res, next) {
     if (res.locals.assessment.type !== 'Exam') return next();
     if (!res.locals.authz_result.authorized_edit) return next(error.make(403, 'Not authorized', res.locals));
     if (req.body.__action == 'newInstance') {
-        assessment.makeAssessmentInstance(res.locals.assessment.id, res.locals.user.user_id, res.locals.assessment.group_work, res.locals.authn_user.user_id, res.locals.authz_data.mode, res.locals.authz_result.time_limit_min, res.locals.req_date, (err, assessment_instance_id) => {
-            if (ERR(err, next)) return;
-            res.redirect(res.locals.urlPrefix + '/assessment_instance/' + assessment_instance_id);
-        });
+        //make sure there is no new instance created by student's teammate simultaneously
+        if (res.locals.assessment.group_work) {
+            var params = {
+                assessment_id: res.locals.assessment.id,
+                user_id: res.locals.user.user_id,
+            };
+            sqldb.query(sql.select_single_assessment_instance, params, function(err, result) {
+                if (ERR(err, next)) return;
+                if (result.rowCount == 0) {
+                    assessment.makeAssessmentInstance(res.locals.assessment.id, res.locals.user.user_id, res.locals.assessment.group_work, res.locals.authn_user.user_id, res.locals.authz_data.mode, res.locals.authz_result.time_limit_min, res.locals.authz_data.date, (err, assessment_instance_id) => {
+                        if (ERR(err, next)) return;
+                        res.redirect(res.locals.urlPrefix + '/assessment_instance/' + assessment_instance_id);
+                    });
+                } else {
+                    //one of teammate already created a new instance -> redirect
+                    res.redirect(res.locals.urlPrefix + '/assessment_instance/' + result.rows[0].id);
+                }
+            });
+        } else {
+            assessment.makeAssessmentInstance(res.locals.assessment.id, res.locals.user.user_id, res.locals.assessment.group_work, res.locals.authn_user.user_id, res.locals.authz_data.mode, res.locals.authz_result.time_limit_min, res.locals.req_date, (err, assessment_instance_id) => {
+                if (ERR(err, next)) return;
+                res.redirect(res.locals.urlPrefix + '/assessment_instance/' + assessment_instance_id);
+            });
+        }
     } else {
         return next(error.make(400, 'unknown __action', {locals: res.locals, body: req.body}));
     }
--- pages/studentAssessmentInstanceExam/studentAssessmentInstanceExam.sql
+++ pages/studentAssessmentInstanceExam/studentAssessmentInstanceExam.sql
@@ -26,3 +26,39 @@ WHERE
 WINDOW
     w AS (ORDER BY qo.row_order)
 ORDER BY qo.row_order;
+
+-- BLOCK get_group_info
+SELECT 
+    gu.group_id, gr.name, gr.join_code, us.uid, gc.student_authz_join, gc.student_authz_create, gc.student_authz_leave
+FROM
+    assessment_instances ai
+    JOIN group_configs gc ON ai.assessment_id = gc.assessment_id
+    JOIN groups gr ON gr.group_config_id = gc.id
+    JOIN group_users gu ON gu.group_id = gr.id
+    JOIN group_users gu2 ON gu2.group_id = gu.group_id
+    JOIN users us ON us.user_id = gu2.user_id
+WHERE 
+    ai.id = $assessment_instance_id 
+    AND gu.user_id = $user_id 
+    AND gr.deleted_at IS NULL 
+    AND gc.deleted_at IS NULL;
+
+-- BLOCK leave_group
+WITH log AS (
+    DELETE FROM 
+        group_users
+    WHERE 
+        user_id = $user_id 
+        AND group_id IN (SELECT gr.id
+                        FROM assessment_instances ai
+                        JOIN group_configs gc ON gc.assessment_id = ai.assessment_id
+                        JOIN groups gr ON gr.group_config_id = gc.id
+                        WHERE ai.id = $assessment_instance_id
+                        AND gr.deleted_at IS NULL
+                        AND gc.deleted_at IS NULL)
+    RETURNING group_id
+) 
+INSERT INTO group_logs 
+    (authn_user_id, user_id, group_id, action)
+SELECT $user_id, $user_id, group_id, 'leave'
+FROM log;
