dad8f2c823d88e740e930c5ac19ec1239f958707
Matthew West <mwest@illinois.edu>
2020-08-23 22:10:58 -0500

    Fix the file editor when there are soft-deleted rows (#2923)

--- lib/editorUtil.sql
+++ lib/editorUtil.sql
@@ -8,14 +8,16 @@ SELECT sync_errors, sync_warnings
 FROM questions
 WHERE
     course_id = $course_id
-    AND qid = $qid;
+    AND qid = $qid
+    AND deleted_at IS NULL;
 
 -- BLOCK select_errors_and_warnings_for_course_instance
 SELECT sync_errors, sync_warnings
 FROM course_instances
 WHERE
     course_id = $course_id
-    AND short_name = $ciid;
+    AND short_name = $ciid
+    AND deleted_at IS NULL;
 
 -- BLOCK select_errors_and_warnings_for_assessment
 SELECT a.sync_errors, a.sync_warnings
@@ -23,5 +25,7 @@ FROM assessments AS a, course_instances AS ci
 WHERE
     ci.course_id = $course_id
     AND ci.short_name = $ciid
+    AND ci.deleted_at IS NULL
     AND a.course_instance_id = ci.id
-    AND a.tid = $aid;
+    AND a.tid = $aid
+    AND a.deleted_at IS NULL;
