1545d7cff83fcb5b9c2b78526cb6f14ab447f8c0
Timothy Bretl <tbretl@illinois.edu>
2020-08-25 15:29:34 -0500

    more informative error message

--- pages/instructorFileDownload/instructorFileDownload.js
+++ pages/instructorFileDownload/instructorFileDownload.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { decodePath } = require('../../lib/uri-util');
 
 router.get('/*', function(req, res, next) {
-    if (!res.locals.authz_data.has_course_permission_view) return next(new Error('Insufficient permissions'));
+    if (!res.locals.authz_data.has_course_permission_view) return next(error.make(403, 'Access denied (must be course viewer)'));
     if (req.query.type) res.type(req.query.type);
     if (req.query.attachment) res.attachment(req.query.attachment);
     res.sendFile(decodePath(req.params[0]), {root: res.locals.course.path});
