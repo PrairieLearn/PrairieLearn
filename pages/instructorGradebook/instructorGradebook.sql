392b412d5a1a219186a83789e42f14730f0307ea
ZhiningQiu <aaronpapa@ZhiningQius-MacBook-Pro.local>
2020-08-29 00:14:14 +0800

    fix issue with duplicate entries in gradebook

--- pages/instructorGradebook/instructorGradebook.sql
+++ pages/instructorGradebook/instructorGradebook.sql
@@ -1,26 +1,38 @@
 -- BLOCK course_assessments
 SELECT
-    a.id AS assessment_id,a.number AS assessment_number,
+    a.id AS assessment_id,
+    a.number AS assessment_number,
     aset.number AS assessment_set_number,aset.color,
     (aset.abbreviation || a.number) AS label
-FROM assessments AS a
-JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
-WHERE a.deleted_at IS NULL
-AND a.course_instance_id = $course_instance_id
+FROM 
+    assessments AS a
+    JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
+WHERE 
+    a.deleted_at IS NULL
+    AND a.course_instance_id = $course_instance_id
 ORDER BY (aset.number, a.order_by, a.id);
 
 -- BLOCK user_scores
 WITH
 course_assessments AS (
-    SELECT a.id,a.order_by AS assessment_order_by,aset.number AS assessment_set_number
-    FROM assessments AS a
-    JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
-    WHERE a.deleted_at IS NULL
-    AND a.course_instance_id = $course_instance_id
+    SELECT 
+        a.id,
+        a.order_by AS assessment_order_by,
+        aset.number AS assessment_set_number
+    FROM 
+        assessments AS a
+        JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
+    WHERE 
+        a.deleted_at IS NULL 
+        AND a.course_instance_id = $course_instance_id
 ),
 course_scores AS (
     SELECT DISTINCT ON (ai.user_id, ai.group_id, a.id)
-        ai.user_id, a.id AS assessment_id, ai.score_perc, ai.id AS assessment_instance_id, ai.group_id
+        ai.user_id, 
+        a.id AS assessment_id, 
+        ai.score_perc, 
+        ai.id AS assessment_instance_id, 
+        ai.group_id
     FROM
         assessment_instances AS ai
         JOIN assessments AS a ON (a.id = ai.assessment_id)
@@ -49,7 +61,10 @@ scores AS (
         course_users AS u
         CROSS JOIN course_assessments AS a
         LEFT JOIN group_users AS gu ON (u.user_id = gu.user_id)
-        LEFT JOIN course_scores AS s ON (((s.user_id = u.user_id)OR(s.group_id = gu.group_id)) AND s.assessment_id = a.id)
+        LEFT JOIN groups AS gr ON (gr.id = gu.group_id)
+        LEFT JOIN course_scores AS s ON (((s.user_id = u.user_id) OR (s.group_id = gu.group_id)) AND s.assessment_id = a.id)
+     WHERE 
+         gr.deleted_at IS NULL
 )
 SELECT user_id,uid,uin,user_name,role,
     ARRAY_AGG(
