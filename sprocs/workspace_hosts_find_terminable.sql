d7ca2fc41d8e5fd397acdc57cb2c9847ce8be7d2
Tim Yang <timothy.d.yang@gmail.com>
2020-08-29 01:19:48 -0500

    Remove unneeded join/groupby

--- sprocs/workspace_hosts_find_terminable.sql
+++ sprocs/workspace_hosts_find_terminable.sql
@@ -15,10 +15,6 @@ BEGIN
             wh.*
         FROM
             workspace_hosts AS wh
-        JOIN
-            workspaces AS w ON ((w.workspace_host_id = wh.id) AND (w.state = 'launching' OR w.state = 'running'))
-        GROUP BY
-            wh.id
         HAVING
             (((wh.state = 'draining' OR wh.state = 'unhealthy') AND wh.load_count = 0) OR
             (wh.state = 'unhealthy' AND (now() - wh.unhealthy_at) > make_interval(secs => unhealthy_timeout_sec)) OR
