bfc33966430940434331d62a94599343d012818e
Nicolas Nytko <nicknytko@gmail.com>
2020-08-31 16:54:39 -0500

    add matt's sproc

---
+++ migrations/215_chunks__add_unique_columns.sql
@@ -0,0 +1,1 @@
+CREATE UNIQUE INDEX chunk_unique_values_idx ON chunks (type, course_id, question_id, course_instance_id, assessment_id);
--- sprocs/chunks_insert.sql
+++ sprocs/chunks_insert.sql
@@ -1,58 +1,28 @@
 CREATE OR REPLACE FUNCTION
     chunks_insert(
-        IN course_id bigint,
+        IN in_course_id bigint,
         IN generated_chunks json[]
     ) RETURNS void
 AS $$
-DECLARE
-    chunk json;
-    found_chunk_id bigint;
 BEGIN
-    FOR chunk IN SELECT * FROM unnest(generated_chunks) LOOP
-        found_chunk_id := NULL;
-
-        IF chunk->>'type' = 'question' THEN
-            SELECT c.id
-            INTO found_chunk_id
-            FROM chunks AS c
-            JOIN questions AS q ON q.uuid = (chunk->>'questionUuid')::uuid AND
-                 q.qid = (chunk->>'questionName')
-            WHERE c.question_id = q.id AND
-                  c.type = (chunk->>'type')::enum_chunk_type AND
-                  q.course_id = chunks_insert.course_id
-            LIMIT 1;
-
-            IF found_chunk_id IS NOT NULL THEN
-                UPDATE chunks
-                SET uuid = (chunk->>'uuid')::uuid
-                WHERE id = found_chunk_id;
-            ELSE
-                INSERT INTO chunks (question_id, uuid, type, course_id)
-                     (SELECT
-                          q.id,
-                          (chunk->>'uuid')::uuid,
-                          (chunk->>'type')::enum_chunk_type,
-                          q.course_id
-                      FROM
-                          questions AS q
-                      WHERE
-                          q.uuid = (chunk->>'questionUuid')::uuid AND
-                          q.qid = chunk->>'questionName' AND
-                          q.course_id = chunks_insert.course_id);
-            END IF;
-        ELSIF chunk->>'type' = 'elements' THEN
-            RAISE warning '%: %', chunk->>'type', chunk::TEXT;
-        ELSIF chunk->>'type' = 'clientFilesCourse' THEN
-            RAISE warning '%: %', chunk->>'type', chunk::TEXT;
-        ELSIF chunk->>'type' = 'serverFilesCourse' THEN
-            RAISE warning '%: %', chunk->>'type', chunk::TEXT;
-        ELSIF chunk->>'type' = 'clientFilesCourseInstance' THEN
-            RAISE warning '%: %', chunk->>'type', chunk::TEXT;
-        ELSIF chunk->>'type' = 'clientFilesAssessment' THEN
-            RAISE warning '%: %', chunk->>'type', chunk::TEXT;
-        ELSE
-            RAISE exception 'Unknown chunk type %s', chunk->>type;
-        END IF;
-    END LOOP;
+    INSERT INTO chunks (type, course_id, question_id, course_instance_id, assessment_id, uuid)
+    SELECT
+        (chunk->>'type')::enum_chunk_type,
+        in_course_id,
+        q.id,
+        ci.id,
+        a.id,
+        (chunk->>'uuid')::uuid
+    FROM unnest(generated_chunks) AS chunk(json)
+    LEFT JOIN questions AS q ON (q.uuid = (chunk->>'questionUuid')::uuid AND q.qid = (chunk->>'questionName'))
+    LEFT JOIN assessments AS a ON (a.uuid = (chunk->>'assessmentUuid')::uuid AND a.tid = (chunk->>'assessmentName'))
+    LEFT JOIN course_instances AS ci ON (ci.uuid = (chunk->>'courseInstanceUuid')::uuid AND ci.short_name = (chunk->>'courseInstanceName'))
+    ON CONFLICT (type, course_id, question_id, course_instance_id, assessment_id) DO UPDATE
+    SET
+        type = EXCLUDED.type,
+        question_id = EXCLUDED.question_id,
+        course_instance_id = EXCLUDED.course_instance_id,
+        assessment_id = EXCLUDED.assessment_id,
+        uuid = EXCLUDED.uuid;
 END;
 $$ LANGUAGE plpgsql VOLATILE;
