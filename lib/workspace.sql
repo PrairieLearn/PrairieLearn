8c6d375215e9a2e1b7ada2d9c83508e42a956a7b
Matthew West <mwest@illinois.edu>
2020-08-31 00:09:13 -0500

    Add strict load limits for workspace hosts

--- lib/workspace.sql
+++ lib/workspace.sql
@@ -1,16 +1,3 @@
--- BLOCK select_ready_workspace_hosts
-SELECT *
-FROM workspace_hosts AS wh
-WHERE wh.state = 'ready';
-
--- BLOCK update_workspaces_workspace_host_id
-UPDATE
-    workspaces as w
-SET
-    workspace_host_id = $workspace_host_id
-WHERE
-    w.id = $workspace_id;
-
 -- BLOCK select_workspace_host
 SELECT wh.*
 FROM
