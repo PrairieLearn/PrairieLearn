4fb7d29e128645deb66247ec8de0766115bccab4
Andrew Stec <andrewstec@gmail.com>
2020-09-01 11:02:02 -0700

    Changing API get return type to stream, modifying file fetch on front-end

--- pages/studentAssessmentInstanceFile/studentAssessmentInstanceFile.js
+++ pages/studentAssessmentInstanceFile/studentAssessmentInstanceFile.js
@@ -9,27 +9,27 @@ const sqlLoader = require('@prairielearn/prairielib/sql-loader');
 
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
-router.get('/:file_id/:display_filename', function(req, res, next) {
-    let params = {
+router.get('/:file_id/:display_filename', async function(req, res, next) {
+    let options = {
         assessment_instance_id: res.locals.assessment_instance.id,
         file_id: req.params.file_id,
         display_filename: req.params.display_filename,
     };
-    sqldb.queryZeroOrOneRow(sql.select_file, params, function(err, result) {
-        if (ERR(err, next)) return;
 
-        if (result.rows.length < 1) {
-            return next(new Error('No such file: ' + req.params.display_filename));
-        }
+    // NOTE: Might want to remove SQL file
 
-        if (result.rows[0].storage_type === fileStore.storageTypes.S3) {
-            const stream = fileStore.get(result.rows[0].storage_filename, fileStore.storageTypes.S3);
-            res.sendFile(stream);
-        } else {
-            res.sendFile(result.rows[0].storage_filename, {root: config.filesRoot});
-        }
+    const stream = await fileStore.get(options.assessment_instance_id, options.file_id, options.display_filename);
 
-    });
+    // Explore error handling?
+    stream.pipe(res);
+        // if (result.rows[0].storage_type === fileStore.storageTypes.S3) {
+        //     const stream = await fileStore.get(result.rows[0].storage_filename, fileStore.storageTypes.S3);
+        //     res.sendFile(result.rows[0].storage_filename, stream);
+        //     // stream.pipe(res);
+        // } else {
+        //     res.sendFile(result.rows[0].storage_filename, {root: config.filesRoot});
+        // }
+
 });
 
 module.exports = router;
