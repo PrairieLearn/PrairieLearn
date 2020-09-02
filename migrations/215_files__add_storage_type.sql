d2279afc6d53e24588d29e3220dcf51bfd57a998
Andrew Stec <andrewstec@gmail.com>
2020-09-02 12:21:05 -0700

    Altering migration to store question_id foreign key in Files table

--- migrations/215_files__add_storage_type.sql
+++ migrations/215_files__add_storage_type.sql
@@ -1,2 +1,7 @@
-ALTER TABLE files ADD COLUMN storage_type text DEFAULT 'fileSystem';
+ALTER TABLE files 
+    ADD COLUMN storage_type text DEFAULT 'fileSystem',
+    ADD COLUMN question_id INT;
+    
+ALTER TABLE files
+    ADD CONSTRAINT questions_question_id_fkey FOREIGN KEY (question_id) REFERENCES questions(id) ON UPDATE CASCADE ON DELETE SET NULL
 
