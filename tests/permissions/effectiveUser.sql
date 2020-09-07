60e288097aa9be976d688ae59e649a32ebf2423d
Timothy Bretl <tbretl@illinois.edu>
2020-09-07 13:55:36 -0500

    remove unused queries

--- tests/permissions/effectiveUser.sql
+++ tests/permissions/effectiveUser.sql
@@ -1,13 +1,3 @@
--- BLOCK select_permissions
-SELECT
-    u.uid,
-    cp.course_role,
-    cip.course_instance_role
-FROM
-    users AS u
-    JOIN course_permissions AS cp ON cp.user_id = u.user_id AND cp.course_id = $course_id
-    LEFT JOIN course_instance_permissions AS cip ON cip.course_permission_id = cp.id AND cip.course_instance_id = $course_instance_id;
-
 -- BLOCK insert_enrollment
 INSERT INTO enrollments (user_id, course_instance_id) VALUES ($user_id, $course_instance_id);
 
@@ -22,6 +12,3 @@ WHERE
 
 -- BLOCK insert_administrator
 INSERT INTO administrators (user_id) VALUES ($user_id);
-
--- BLOCK temp_users
-SELECT * FROM users;
