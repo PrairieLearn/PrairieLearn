8bb1ba87e7ad9562a318f6b05cb6a472ef12050f
Andrew Stec <andrewstec@gmail.com>
2020-09-08 11:48:44 -0700

    Removed unnecessary relationship and field

--- migrations/215_files__add_storage_type.sql
+++ migrations/215_files__add_storage_type.sql
@@ -1,7 +1,2 @@
 ALTER TABLE files 
     ADD COLUMN storage_type text DEFAULT 'fileSystem',
-    ADD COLUMN question_id INT;
-    
-ALTER TABLE files
-    ADD CONSTRAINT questions_question_id_fkey FOREIGN KEY (question_id) REFERENCES questions(id) ON UPDATE CASCADE;
-
