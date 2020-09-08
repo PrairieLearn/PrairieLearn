982b20c9a7a4ec36bce53c4526c90760a0778051
Timothy Bretl <tbretl@illinois.edu>
2020-09-07 21:25:10 -0500

    consolidate enum_role drop migration

--- migrations/217_enum_role__drop.sql
+++ migrations/217_enum_role__drop.sql
@@ -1,3 +1,5 @@
+ALTER TABLE assessment_access_rules DROP COLUMN IF EXISTS role;
+ALTER TABLE course_instance_access_rules DROP COLUMN IF EXISTS role;
 ALTER TABLE enrollments DROP COLUMN IF EXISTS role;
 ALTER TABLE access_logs DROP COLUMN IF EXISTS auth_role;
 ALTER TABLE access_logs DROP COLUMN IF EXISTS user_role;
