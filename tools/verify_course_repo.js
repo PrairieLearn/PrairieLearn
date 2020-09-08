680252434e79a6fa41e615942346780b9b8e8ed6
Your Name <you@example.com>
2020-09-08 00:02:44 -0700

    fix linter errors

--- tools/verify_course_repo.js
+++ tools/verify_course_repo.js
@@ -1,8 +1,8 @@
 const courseDB = require('../sync/course-db');
 (async () => {
-    const courseData = await courseDB.loadFullCourseNew("/course");
+    const courseData = await courseDB.loadFullCourseNew('/course');
     const errors = [];
-    courseDB.writeErrorsAndWarningsForCourseData("fakeCourseId", courseData, line => line ? errors.push(line) : null);
+    courseDB.writeErrorsAndWarningsForCourseData('fakeCourseId', courseData, line => line ? errors.push(line) : null);
     if(errors.length !== 0){
         errors.forEach(line => console.error(line));
         process.exit(1);
