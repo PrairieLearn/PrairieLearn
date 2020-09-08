d9501cdb75dbd92da207eb0e373dea0628bda2fa
Timothy Bretl <tbretl@illinois.edu>
2020-09-07 22:00:06 -0500

    fix migration to convert enrollments to cp and cip

--- migrations/216_course_instance_permissions__create.sql
+++ migrations/216_course_instance_permissions__create.sql
@@ -9,3 +9,46 @@ CREATE TABLE course_instance_permissions (
 );
 
 CREATE INDEX course_instance_permissions_course_instance_id_idx ON course_instance_permissions (course_instance_id);
+
+/*
+    Give course_permissions with course_role 'None' to all users with
+    enrollments of role TA or Instructor.
+*/
+INSERT INTO course_permissions
+    (user_id, course_id, course_role)
+SELECT
+    e.user_id,
+    ci.course_id,
+    'None' AS course_role
+FROM
+    enrollments AS e
+    JOIN course_instances AS ci ON (ci.id = e.course_instance_id) AND (ci.deleted_at IS NULL)
+    JOIN pl_courses AS c ON (c.id = ci.course_id) AND (c.deleted_at IS NULL)
+WHERE
+    e.role >= 'TA'
+GROUP BY
+    e.user_id, ci.course_id
+ON CONFLICT DO NOTHING;
+
+/*
+    Give course_instance_permissions with course_instance_role 'Student Data Editor'
+    to all users with enrollments of role Instructor, and 'Student Data Viewer' to all
+    users with enrollments of role TA.
+*/
+INSERT INTO course_instance_permissions
+    (course_instance_id, course_instance_role, course_permission_id)
+SELECT
+    e.course_instance_id,
+    CASE
+        WHEN e.role = 'Instructor' THEN 'Student Data Editor'::enum_course_instance_role
+        ELSE 'Student Data Viewer'::enum_course_instance_role
+    END AS course_instance_role,
+    cp.id AS course_permission_id
+FROM
+    enrollments AS e
+    JOIN course_instances AS ci ON (ci.id = e.course_instance_id) AND (ci.deleted_at IS NULL)
+    JOIN pl_courses AS c ON (c.id = ci.course_id) AND (c.deleted_at IS NULL)
+    JOIN course_permissions AS cp ON (cp.user_id = e.user_id) AND (cp.course_id = c.id)
+WHERE
+    e.role >= 'TA'
+ON CONFLICT DO NOTHING;
