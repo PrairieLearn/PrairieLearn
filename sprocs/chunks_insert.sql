6d1a833fc0bac16df3219a2b3c23f414d5a47569
Nicolas Nytko <nicknytko@gmail.com>
2020-09-12 23:59:09 -0500

    fix sproc

--- sprocs/chunks_insert.sql
+++ sprocs/chunks_insert.sql
@@ -14,9 +14,9 @@ BEGIN
         a.id,
         (chunk->>'uuid')::uuid
     FROM unnest(generated_chunks) AS chunk(json)
-    LEFT JOIN questions AS q ON (q.uuid = (chunk->>'questionUuid')::uuid AND q.qid = (chunk->>'questionName'))
-    LEFT JOIN course_instances AS ci ON (ci.uuid = (chunk->>'courseInstanceUuid')::uuid AND ci.short_name = (chunk->>'courseInstanceName'))
-    LEFT JOIN assessments AS a ON (a.uuid = (chunk->>'assessmentUuid')::uuid AND a.tid = (chunk->>'assessmentName'))
+    LEFT JOIN questions AS q ON (q.uuid = (chunk->>'questionUuid')::uuid AND q.qid = (chunk->>'questionName') AND q.course_id = in_course_id)
+    LEFT JOIN course_instances AS ci ON (ci.uuid = (chunk->>'courseInstanceUuid')::uuid AND ci.short_name = (chunk->>'courseInstanceName') AND ci.course_id = in_course_id)
+    LEFT JOIN assessments AS a ON (a.uuid = (chunk->>'assessmentUuid')::uuid AND a.tid = (chunk->>'assessmentName') AND a.course_instance_id = ci.id)
     ON CONFLICT (type, course_id, coalesce(question_id, -1), coalesce(course_instance_id, -1), coalesce(assessment_id, -1)) DO UPDATE
     SET
         type = EXCLUDED.type,
