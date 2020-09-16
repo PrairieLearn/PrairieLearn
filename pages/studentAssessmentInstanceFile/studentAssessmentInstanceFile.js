155af97dad84a71a99a42be3113cd9a4a6c7558c
Andrew Stec <andrewstec@gmail.com>
2020-09-16 08:32:24 -0700

    Fixing linting

--- pages/studentAssessmentInstanceFile/studentAssessmentInstanceFile.js
+++ pages/studentAssessmentInstanceFile/studentAssessmentInstanceFile.js
@@ -1,9 +1,9 @@
-const ERR = require('async-stacktrace');
+// const ERR = require('async-stacktrace');
 const express = require('express');
 const router = express.Router();
 const fileStore = require('../../lib/file-store');
 
-router.get('/:file_id/:display_filename', async function(req, res, next) {
+router.get('/:file_id/:display_filename', async function(req, res) {
     const options = {
         assessment_instance_id: res.locals.assessment_instance.id,
         file_id: req.params.file_id,
@@ -13,9 +13,6 @@ router.get('/:file_id/:display_filename', async function(req, res, next) {
     const buffer = await fileStore.get(options.file_id);
     res.set('Content-Type', 'application/octet-stream');
     res.send(buffer);
-    // stream
-    //     .on('error', (err) => {return ERR(err, next);})
-    //     .pipe(res);
 });
 
 module.exports = router;
