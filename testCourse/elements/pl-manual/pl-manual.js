a0543b1c62f12ec7f6b48ec6bcd1a62f1516abfa
mebird <mbe_pro@outlook.com>
2020-09-24 10:24:09 -0700

    broken manual grading w/ input mock up

--- testCourse/elements/pl-manual/pl-manual.js
+++ testCourse/elements/pl-manual/pl-manual.js
@@ -1,15 +1,39 @@
-(function () {
-  /*****************************************************************************/
+function ManualGradingElement(uuid) {
+  $(`#pl-manual-${uuid}`)
+    .closest('form')
+    .on('submit', () => {
+      // This should be a sum out of 100 (percentage)
+      let points = 0;
+      let feedback = [];
 
-  /** Creates a PrairieManualGrading object.
+      // Iterate over children and grab out appropriate values
+      // TODO: Augment so we can persist the state of certain children instead of only
+      //        passing on a single point/feedback value
+      // SPIKE: Figure out what anchors to give to children and validate uniqueness in pl-manual.py
+      $(`#pl-manual-${uuid}`)
+        .children('input')
+        .toArray()
+        .forEach((child) => {
+          const val = child.getAttribute('value');
+          const field = child.getAttribute('name');
+          switch (field) {
+            case 'points':
+              points += parseInt(val);
+            case 'feedback':
+              feedback.push(val);
+              break;
+            default:
+              throw new Error(
+                `Unable to attribute field "${field}" to a score.`
+              );
+          }
+        });
 
-        @constructor
-        @this {PrairieManualGrading}
-        @param {string} id The ID of the wrapping div of the manual grading element.
-    */
-  function PrairieManualGrading(id) {
-    // TODO: Add a handler to create a slider for partials
-    // TODO: Handle
-    console.log(id);
-  }
-});
+      // FIXME: Remove demo logging
+      console.log(uuid, points, feedback);
+
+      // These are the actual fields that the form submission will take a look at
+      $(`#pl-manual-points-${uuid}`).attr('value', points / 100);
+      $(`#pl-manual-feedback-${uuid}`).attr('value', JSON.stringify(feedback));
+    });
+}
--- testCourse/elements/pl-manual/pl-manual.py
+++ testCourse/elements/pl-manual/pl-manual.py
@@ -5,6 +5,11 @@ import chevron
 
 def prepare(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
+
+    required_attribs = ['answers-name']
+    optional_attribs = ['show-default']
+    pl.check_attribs(element, required_attribs, optional_attribs)
+
     answers_name = pl.get_string_attrib(element, 'answers-name', None)
     partial_child = list(filter(lambda child: not isinstance(child, lxml.html.HtmlComment)
                                 and pl.get_string_attrib(child, 'answers-name', '') == answers_name, element))
@@ -18,25 +23,64 @@ def prepare(element_html, data):
 
 
 def render(element_html, data):
-    element = lxml.html.fragment_fromstring(element_html)
-    answers_name = pl.get_string_attrib(element, 'answers-name', None)
+    # TODO: vacously return the rendered html if not in right view
 
-    # TODO: Add checks for the correct views (only show in view question mode)
-    # TODO: We should be able to pre-load partials & scores at some higher level
-    #       so we need to add a query at a higher level to populate all of this
-    #       for now, we're just gating behind the instructor view
-    # TODO: Render popover as nested HTML, or create a "form" element that we can wrap and pass on
-    #       and create a seperate rubric toggle
-    #       this will require looking into the PL form submission logic--do we create a workflow to "cough things up"?
-    #       manual grading should control scores, so we should look for elements with a certian toggle or data id
-    # TODO: Can we wrap the manual grading mode in a form that requires instructor ID to submit, and then have form fields
-    #       that cough up all the info we need for manual grading, and then hit the endoint
+    element = lxml.html.fragment_fromstring(element_html)
+    # FIXME: get rid of debug line here
+    answers_name = pl.get_string_attrib(element, 'answers-name', None) or 'ans'
+    show_default = pl.get_string_attrib(element, 'show-default', True)
+    uuid = pl.get_uuid()
+
+    # TODO: Check for correct view & load with nonstatic data
+    # 'partial_feedback': data['partial_scores'][answers_name]['feedback']
+    # 'partial_score': data['partial_scores'][answers_name]['score']
+    partial_score = 0
+    partial_feedback = 'this assignment was really great!'
+    finished_grading = 'graded'
+
+    popover_body = []
+    html = []
+    print(show_default)
+    if show_default:
+        params = {
+            'partial_score': partial_score,
+            'partial_feedback': partial_feedback
+        }
+        with open('pl-manual-default.mustache', 'r', encoding='utf-8') as f:
+            obj = {
+                'html': chevron.render(f, params).strip()
+            }
+            popover_body.append(obj)
+
+    # Move anything with the pl-manual-* prefix into the popover body
+    #       Append "score" and "feedback" values to the outer element
+    # TODO: Unsure how to build this super extensibly, look @js file for how we build scores rn
+    for child in element:
+        if child.tag and child.tag.startswith('pl-manual-'):
+            child.set('score', partial_score)
+            child.set('feedback', partial_feedback)
+            child.set('index', len(popover_body))
+            popover_body.append(child)
+        else:
+            html.append({'html': pl.inner_html(child)})
+
+    close_button = None
+    with open('pl-manual-close.mustache', 'r', encoding='utf-8') as f:
+        params = {
+            'uuid': uuid
+        }
+        close_button = chevron.render(f, params).strip()
+
     html_params = {
-        'answers-name': answers_name,
-        'html': pl.inner_html(element),
-        'uuid': pl.get_uuid(),
-        'partial_score': 0
-        # 'partial_score': data['partial_scores'][answers_name]
+        'answers_name': answers_name,
+        'html': html,
+        'popover_body': popover_body,
+        'close_button': close_button,
+        'uuid': uuid,
+        'partial_score': partial_score,
+        'partial_feedback': partial_feedback,
+        'finished_grading': finished_grading,
+        'show_grading': True
     }
 
     with open('pl-manual.mustache', 'r', encoding='utf-8') as f:
@@ -48,4 +92,6 @@ def render(element_html, data):
 def grade(element_html, data):
     # TODO: do we even have a grade step? we should be relying upon the form submission for this element,
     #       and the rendering should be purely structural
+    #       if we stick to the form, and allow an instructor ID or a student ID through, then we could even
+    #       enable peer grading... (manual grading is student/instructor view of a pre-loaded variant)
     return
