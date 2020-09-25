a05cff607a47fd9929cfb189171f69b3920486e8
Andrew <andrewstec@gmail.com>
2020-09-25 08:54:52 -0700

    Update docker/start_s3rver.sh
    Co-authored-by: Matthew West <mwest@illinois.edu>

--- docker/start_s3rver.sh
+++ docker/start_s3rver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p /s3rver
-npx s3rver --silent --directory /s3rver --port 5000 --configure-bucket workspaces --configure-bucket prairielearn-dev &
+npx s3rver --silent --directory /s3rver --port 5000 --configure-bucket workspaces --configure-bucket prairielearn.dev.file-store &
