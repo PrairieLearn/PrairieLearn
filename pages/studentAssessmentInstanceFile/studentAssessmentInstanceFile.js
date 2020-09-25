38e5b733fdc5ee9db6f48e1e5b7fa9fbec8c9580
Andrew Stec <andrewstec@gmail.com>
2020-09-25 12:59:04 -0700

    Implementing stream for reading files

--- pages/studentAssessmentInstanceFile/studentAssessmentInstanceFile.js
+++ pages/studentAssessmentInstanceFile/studentAssessmentInstanceFile.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const fileStore = require('../../lib/file-store');
 
-router.get('/:file_id/:display_filename', async function(req, res) {
+router.get('/:file_id/:display_filename', async function(req, res, next) {
     const options = {
         assessment_instance_id: res.locals.assessment_instance.id,
         file_id: req.params.file_id,
         display_filename: req.params.display_filename,
     };
 
-    const file = await fileStore.get(options.file_id);
-    res.send(file.contents);
+    const stream = await fileStore.getStream(options.file_id);
+    stream.on('error', next).pipe(res);
 });
 
 module.exports = router;
