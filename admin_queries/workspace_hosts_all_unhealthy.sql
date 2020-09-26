9606b8d25c9bf284899c8e9598882bddbab2c1a1
Tim Yang <tdy@users.noreply.github.com>
2020-09-26 12:43:31 -0500

    Add wh.unhealthy_reason when workspace hosts go unhealthy (#3180)
    * Add wh.unhealthy_reason when workspace hosts go unhealthy
    
    * Update admin queries
    
    * Bump migration

--- admin_queries/workspace_hosts_all_unhealthy.sql
+++ admin_queries/workspace_hosts_all_unhealthy.sql
@@ -3,7 +3,8 @@ UPDATE
 SET
     state = 'unhealthy',
     state_changed_at = NOW(),
-    unhealthy_at = NOW()
+    unhealthy_at = NOW(),
+    unhealthy_reason = $unhealthy_reason
 WHERE
     $confirm = 'yes'
     AND wh.state IN ('launching', 'ready', 'draining')
@@ -11,4 +12,5 @@ RETURNING
     wh.id AS workspace_host_id,
     wh.instance_id,
     wh.state,
-    wh.unhealthy_at;
+    wh.unhealthy_at,
+    wh.unhealthy_reason;
