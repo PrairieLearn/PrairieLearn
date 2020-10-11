dea65fe338931b4b663258a6ce63e73f3ac0b742
Jonatan Schroeder <jonatan@cs.ubc.ca>
2020-10-11 10:38:41 -0700

    Review of C grader demo question in example course.

--- exampleCourse/questions/demo/autograder/c/square/tests/main.c
+++ exampleCourse/questions/demo/autograder/c/square/tests/main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 extern int square(int x);
 
 int main(int argc, char *argv[]) {
+  
+  int n;
 
-  for (int i = 1; i < argc; i++) {
-    printf("%+d\n", square(atoi(argv[i])));
-  }
+  /* Saves stdout in a new file descriptor */
+  int realStdoutNo = dup(STDOUT_FILENO);
+  FILE *realStdout = fdopen(realStdoutNo, "w");
+
+  /* Switches stdout/stderr to point to different file descriptor to
+   * avoid students passing code by printing the correct result. */
+  int devNull = open("/dev/null", O_WRONLY);
+  dup2(devNull, STDOUT_FILENO);
+  dup2(devNull, STDERR_FILENO);
+  
+  scanf("%d", &n);
+  fprintf(realStdout, "[%d]\n", square(n));
+  
   return 0;
 }
--- exampleCourse/questions/demo/autograder/c/square/tests/test.py
+++ exampleCourse/questions/demo/autograder/c/square/tests/test.py
@@ -11,9 +11,7 @@ class DemoGrader(cgrader.CGrader):
 
         for inval in [0, 1, 2, 3, 5, 10, -20, 100, 512, -23, -4]:
             outval = inval ** 2
-            self.test_send_in_check_out('./main',
-                                        exp_output = '%+d' % outval,
-                                        args = [str(inval)])
+            self.test_run('./main', f'{inval}\n', f'[{outval}]')
 
 g = DemoGrader()
 g.start()
