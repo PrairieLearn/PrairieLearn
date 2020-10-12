38449c106aa86e9d8db196e7877892664033cb4a
Nicolas Nytko <nicknytko@gmail.com>
2020-10-11 19:09:02 -0500

    sneak some other changes in too

--- pages/editError/editError.js
+++ pages/editError/editError.js
@@ -8,7 +8,7 @@ const syncHelpers = require('../shared/syncHelpers');
 router.get('/:job_sequence_id', function(req, res, next) {
     const job_sequence_id = req.params.job_sequence_id;
     const course_id = res.locals.course ? res.locals.course.id : null;
-    serverJobs.getJobSequence(job_sequence_id, course_id, (err, job_sequence) => {
+    serverJobs.getJobSequenceWithFormattedOutput(job_sequence_id, course_id, (err, job_sequence) => {
         if (ERR(err, next)) return;
 
         // All edits wait for the corresponding job sequence to finish before
