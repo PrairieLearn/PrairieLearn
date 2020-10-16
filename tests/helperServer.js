8b0a9cc2d302d46f3d0ff9bd2781ff807f636dd3
Matthew West <mwest@illinois.edu>
2020-10-16 13:15:20 -0500

    Disable spurious linter warning

--- tests/helperServer.js
+++ tests/helperServer.js
@@ -219,7 +219,7 @@ module.exports.getLastJobSequenceId = util.callbackify(module.exports.getLastJob
 
 module.exports.waitForJobSequenceAsync = async (job_sequence_id) => {
     let job_sequence;
-    while (true) {
+    while (true) { // eslint-disable-line no-constant-condition
         const result = await sqldb.queryOneRowAsync(sql.select_job_sequence, { job_sequence_id });
         job_sequence = result.rows[0];
         if (job_sequence.status != 'Running') break;
