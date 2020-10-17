445c73da0f86ec9feed0050134f4eb37be65580b
Jonatan Schroeder <jonatan@cs.ubc.ca>
2020-10-17 10:19:43 -0700

    C Grader: better formatting of default message if expected string has line breaks.

--- graders/c/cgrader/cgrader.py
+++ graders/c/cgrader/cgrader.py
@@ -140,8 +140,8 @@ class CGrader:
         if reject_output is not None and not isinstance(reject_output, list):
             reject_output = [reject_output]
         if msg is None and exp_output is not None:
-            msg = 'Expected "%s"' % ('" AND "' if must_match_all_outputs \
-                                     else '" OR "').join([str(t) for t in exp_output]) + \
+            msg = 'Expected: %s' % (' AND ' if must_match_all_outputs \
+                                     else ' OR ').join([f'\n{t}\n' if '\n' in t else f'"{t}"' for t in exp_output]) + \
                   (' but not "%s"' % '"/"'.join([str(t) for t in reject_output]) if reject_output else '')
 
         out = self.run_command(command if args is None else ([command] + args),
