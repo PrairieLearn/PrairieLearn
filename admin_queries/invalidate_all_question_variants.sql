4f33cd4859e0679acef749e8ef1fd0a040c23001
David Gene <david@astrosticks.com>
2020-10-29 14:09:05 -0500

    WIP break variants query

---
+++ admin_queries/invalidate_all_question_variants.sql
@@ -0,0 +1,18 @@
+/* UPDATE variants AS v
+SET broken = true
+FROM 
+    assessment_questions aq
+    JOIN instance_questions iq ON iq.assessment_question_id = aq.id
+WHERE
+    v.instance_question_id = iq.id
+    AND aq.id = $assessment_question_id
+RETURNING v.id AS updated_variant_id; */
+
+SELECT 
+    v.id AS variant_id,
+    v.broken AS variant_broken
+FROM
+    variants v
+    JOIN instance_questions iq ON v.instance_question_id = iq.id
+    JOIN assessment_questions aq ON iq.assessment_question_id = aq.id
+WHERE aq.id = $assessment_question_id;
