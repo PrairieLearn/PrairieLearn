01d8e4cb055a455912c3f94be07eb3c6f162a78c
Jonatan Schroeder <jonatan@cs.ubc.ca>
2020-11-12 16:24:39 -0800

    Removed unnecessary modules from time_left page.

--- pages/studentAssessmentInstanceTimeLeft/studentAssessmentInstanceTimeLeft.js
+++ pages/studentAssessmentInstanceTimeLeft/studentAssessmentInstanceTimeLeft.js
@@ -1,12 +1,5 @@
-const util = require('util');
-const ERR = require('async-stacktrace');
 const express = require('express');
 const router = express.Router();
-const _ = require('lodash');
-
-const error = require('@prairielearn/prairielib/error');
-const assessment = require('../../lib/assessment');
-const studentAssessmentInstance = require('../shared/studentAssessmentInstance');
 
 router.get('/', function(req, res, next) {
     if (res.locals.assessment.type !== 'Exam') return next();
