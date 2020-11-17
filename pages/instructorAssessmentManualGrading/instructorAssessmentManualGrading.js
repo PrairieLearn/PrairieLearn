2eacd990fb0e435fa42a1eac2b3350a696899a6d
mebird <mbe_pro@outlook.com>
2020-11-16 19:53:55 -0800

    tearing out old backend logic

--- pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.js
+++ pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.js
@@ -5,21 +5,16 @@ const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'
 
 const error = require('@prairielearn/prairielib/error');
 
+// TODO:
 // eslint-disable-next-line no-unused-vars
 router.get('/', function(req, res, next) {
+    if (!res.locals.authz_data.has_instructor_edit) return next();
     debug('GET /');
-    // TODO: Implement logic to:
-    //       - Display all questions from the assignment, and list percentage graded
-    //       - Hotlink to question statistics (so it's easy to bounce over and see current scores)
-    //       - Hotlink to the "first question to grade" from each assessment (can be random)
-    //       - Grab anything else we may need for a useful UI (assigning TAs to mark a given question?)
 });
 
+// TODO:
 router.post('/', function(req, res, next) {
     if (!res.locals.authz_data.has_instructor_edit) return next();
-    // TODO: Do manual grading to force mark all questions as finished grading
-    // TODO: Implement action to force mark a single question as finished grading
-    // TODO: Ideate if we need any other endpoints on this page.
     return next(error.make(400, 'unknown __action', {locals: res.locals, body: req.body}));
 });
 module.exports = router;
--- pages/instructorQuestionManualGrading/instructorQuestionManualGrading.js
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGrading.js
@@ -1,91 +1,26 @@
 const ERR = require('async-stacktrace');
-const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 const async = require('async');
 const error = require('@prairielearn/prairielib/error');
 const question = require('../../lib/question');
 const path = require('path');
-const debug = require('debug')(
-  'prairielearn:' + path.basename(__filename, '.js'),
-);
+
 const logPageView = require('../../middlewares/logPageView')(
   path.basename(__filename, '.js'),
 );
 
-/**
- * Submits the regraded partials to the regrading workflow.
- * Returns the variant ID of the current question.
- */
-function submitPartials(req, res, callback) {
-  const variant_id = req.body.__variant_id;
-  const submitted_partials = _.omit(req.body, [
-    '__action',
-    '__csrf_token',
-    '__variant_id',
-  ]);
-
-  const status = 'graded';
-  const partials = {};
-  for (const key in submitted_partials) {
-    _.set(partials, key, submitted_partials[key]);
-  }
-  for (const key in partials) {
-    partials[key].status = status;
-  }
-
-  // TODO: Send these partials off to the backend
-  console.log(partials);
-  callback(null, variant_id);
-}
-
-/**
- * Grabs the next question from the current assignment to grade.
- */
-function getNextToGrade(req, res, callback) {
-  // TODO: Implement.
-  const variant_id = req.body.__variant_id;
-  callback(error.make(501, 'Not implemented.'), variant_id);
-}
 
 router.post('/', function (req, res, next) {
-  if (res.locals.question.type == 'Freeform') {
-    const redirectToVariant = function (err, variant_id) {
-      if (ERR(err, next)) return;
-      res.redirect(
-        `${res.locals.urlPrefix}/question/${res.locals.question.id}/manual_grading/?variant_id=${variant_id}`,
-      );
-    };
-
-    switch (req.body.__action) {
-      case 'grade':
-        submitPartials(req, res, redirectToVariant);
-        break;
-      case 'next':
-        getNextToGrade(req, res, redirectToVariant);
-        break;
-      default:
-        next(
-          error.make(400, 'unknown __action: ' + req.body.__action, {
-            locals: res.locals,
-            body: req.body,
-          }),
-        );
-    }
-  } else {
-    return next(
-      error.make(
-        400,
-        'Manual grading is only supported for freeform (V3) questions',
-        { locals: res.locals, body: req.body },
-      ),
-    );
-  }
+  // TODO:
+  if (!res.locals.authz_data.has_instructor_edit) return next();
 });
 
 router.get('/', function (req, res, next) {
+  // TODO:
+  if (!res.locals.authz_data.has_instructor_edit) return next();
+  
   var variant_id = req.query.variant_id;
-  debug(`manually grading variant_id ${variant_id}`);
 
   if (variant_id) {
     res.locals.overlayGradingInterface = true;
