8aa67327e876c8dc93a272e565ebb935b4651e62
Andrew Stec <andrewstec@gmail.com>
2020-11-26 10:19:52 -0800

    Removing submission from query, as student may have not submitted anything, but we need to grade based on last variant regardless

--- pages/instructorQuestionManualGrading/instructorQuestionManualGrading.sql
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGrading.sql
@@ -3,7 +3,6 @@ SELECT v.*
 FROM
     variants as v
     JOIN instance_questions as iq ON (v.instance_question_id = iq.id)
-    JOIN submissions as s ON (v.id = s.variant_id)
 WHERE instance_question_id = $instance_question_id
 ORDER BY v.id DESC
 LIMIT 1;
