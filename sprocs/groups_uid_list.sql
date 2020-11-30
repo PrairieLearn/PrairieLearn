05f20a5721c042c3cd108d8060b8946319265ca0
Jonatan Schroeder <jonatan@cs.ubc.ca>
2020-11-30 15:50:26 -0800

    More elegant solution.

--- sprocs/groups_uid_list.sql
+++ sprocs/groups_uid_list.sql
@@ -7,13 +7,11 @@ CREATE OR REPLACE FUNCTION
     )
 AS $$
 BEGIN
-    SELECT array_agg(x.uid)
+    SELECT array_agg(u.uid ORDER BY u.uid)
     INTO uid_list
-    FROM (SELECT u.uid
-          FROM
-              group_users AS gu
-              JOIN users AS u ON (u.user_id = gu.user_id)
-          WHERE gu.group_id = groups_uid_list.group_id
-          ORDER BY u.uid) AS x;
+    FROM
+        group_users AS gu
+        JOIN users AS u ON (u.user_id = gu.user_id)
+    WHERE gu.group_id = groups_uid_list.group_id;
 END
 $$ LANGUAGE plpgsql STABLE;
