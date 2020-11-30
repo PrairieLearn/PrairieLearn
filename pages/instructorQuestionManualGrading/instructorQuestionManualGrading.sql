5fc4ed62afb542aed5ea9dbf11e3734832a665fc
Andrew Stec <andrewstec@gmail.com>
2020-11-30 10:30:45 -0800

    Fixing query

--- pages/instructorQuestionManualGrading/instructorQuestionManualGrading.sql
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGrading.sql
@@ -10,10 +10,7 @@ LIMIT 1;
 -- BLOCK instance_question_select_question
 SELECT q.*
 FROM
-    variants as v
-    JOIN instance_questions as iq ON (v.instance_question_id = iq.id)
-    JOIN submissions as s ON (v.id = s.variant_id)
-    JOIN assessment_questions as aq ON (iq.assessment_question_id = aq.id)
-    JOIN questions as q ON (aq.question_id = q.id)
-WHERE instance_question_id = $instance_question_id
-ORDER BY v.id DESC;
+    questions as q
+    JOIN assessment_questions AS aq ON (q.id = aq.question_id)
+    JOIN instance_questions AS iq ON (aq.id = iq.assessment_question_id)
+WHERE iq.id = $instance_question_id;
