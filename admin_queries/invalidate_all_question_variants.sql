cba01a053088b87e11a8c68ee1129522bb8b4d56
David Gene <david@astrosticks.com>
2020-12-03 17:11:08 -0600

    Working SQL for breaking variants

--- admin_queries/invalidate_all_question_variants.sql
+++ admin_queries/invalidate_all_question_variants.sql
@@ -1,30 +1,16 @@
-/* UPDATE variants AS v
-SET broken = true
-FROM 
-    assessment_questions aq
-    JOIN instance_questions iq ON iq.assessment_question_id = aq.id
-WHERE
-    v.instance_question_id = iq.id
-    AND aq.id = $assessment_question_id
-RETURNING v.id AS updated_variant_id; */
-
-/*
-SELECT 
-    v.id AS variant_id,
-    v.broken AS variant_broken,
-    v.open AS variant_open
-FROM
-    variants v
-    JOIN instance_questions iq ON v.instance_question_id = iq.id
-    JOIN assessment_questions aq ON iq.assessment_question_id = aq.id
-WHERE aq.id = $assessment_question_id; 
-*/
-
 UPDATE variants AS v
-SET broken = true
-FROM
-    assessment_questions aq 
-    JOIN instance_questions iq ON iq.assessment_question_id=aq.id
-WHERE
-    v.instance_question_id = iq.id
+SET
+    broken = true,
+    open = true
+WHERE id IN (
+    SELECT v.id
+    FROM
+        variants v
+        JOIN instance_questions iq ON v.instance_question_id = iq.id
+        JOIN assessment_questions aq ON iq.assessment_question_id = aq.id
+    WHERE
+        v.open = true
+        AND v.broken = false
+        AND aq.id = $assessment_question_id
+)
 RETURNING v.id AS updated_variant_id;
