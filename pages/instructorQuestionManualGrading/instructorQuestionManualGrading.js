3655532f019f1541fc10b5cc4beebdd08bce6eda
mebird <mebird@users.noreply.github.com>
2020-12-21 12:54:39 -0800

    blind programming of first bit of UI work; time to build

--- pages/instructorQuestionManualGrading/instructorQuestionManualGrading.js
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGrading.js
@@ -2,7 +2,10 @@ const ERR = require('async-stacktrace');
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const async = require('async');
 const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
+const question = require('../../lib/question');
+const logPageView = require('../../middlewares/logPageView')(path.basename(__filename, '.js'));
 const { error, sqlDb, sqlLoader} = require('@prairielearn/prairielib');
 
 const sql = sqlLoader.loadSqlEquiv(__filename);
@@ -19,36 +22,64 @@ class NoSubmissionError extends Error {
 router.get('/', (req, res, next) => {
     console.log(res.locals);
     const params = {instance_question_id: res.locals.instance_question_id};
-    sqlDb.query(sql.instance_question_select_last_variant_with_submission, params, (err, result) => {
-        if (ERR(err, next)) return;
-        if (result.rowCount == 0) {
-            // perhaps student loaded view to create variant, but did not submit anything
-            res.locals['no_submission_found'] = true;
-            return;
-        }
+    
+    async.series([
+        // First, grab the most recent variant associated with this instance question for this assessment
+        (callback) => 
+            sqlDb.query(sql.instance_question_select_last_variant_with_submission, params, (err, result) => {
+                if (ERR(err, next)) return;
+                if (result.rowCount == 0) {
+                    // perhaps student loaded view to create variant, but did not submit anything
+                    res.locals['no_submission_found'] = true;
+                    return;
+                }
 
-        const variant_id = result.rows[0].id;
-
-        sqlDb.queryOneRow(sql.instance_question_select_question, params, (err, result) => {
-            if (ERR(err, next)) return;
-            if (result.rowCount == 0) throw new Error('Question not found');
-
-            res.locals.question = result.rows[0];
-
+                // Set the variant ID to the most recent submission
+                params.variant_id = result.rows[0].id;
+                callback(null);
+            }),
+        // Select the higher level question data given the instance question
+        (callback) => 
+            sqlDb.queryOneRow(sql.instance_question_select_question, params, (err, result) => { 
+                if (ERR(err, next)) return;
+                if (result.rowCount == 0) throw new Error('Question not found');
+                res.locals.question = result.rows[0];
+                callback(null);
+            }),
+        // Check for the variant to be graded
+        (callback) => {
             const params = [
-                variant_id,
+                params.variant_id,
                 null,
                 true, // select graded submissions
             ];
-
             sqlDb.callZeroOrOneRow('variants_select_submission_for_grading', params, (err, result) => {
                 if (ERR(err, next)) return;
                 if (result.rowCount == 0) return new NoSubmissionError();
-                const submission = result.rows[0];
-                res.locals['submission'] = submission;
-                res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+                res.locals['submission'] = result.rows[0];
+                callback(null);
             });
-        });
+        },
+        // Grab the question data so we can render the question body
+        (callback) => {
+            res.locals.overlayGradingInterface = true;
+            question.getAndRenderVariant(params.variant_id, null, res.locals, function (
+              err,
+            ) {
+              if (ERR(err, callback)) return;
+              callback(null);
+            });
+        },
+        // Log the page view
+        (callback) => {
+            logPageView(req, res, (err) => {
+                if (ERR(err, next)) return;
+                callback(null);
+            });
+        },
+    ], (err) => {
+        if (ERR(err, next)) return;
+        res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
     });
 
     debug('GET /');
