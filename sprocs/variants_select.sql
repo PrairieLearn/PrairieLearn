0808c0075bf797f111c272fdbaacc81188839205
Jonatan Schroeder <jonatan@cs.ubc.ca>
2020-12-21 11:06:28 -0800

    Unified queries in variants_select.

--- sprocs/variants_select.sql
+++ sprocs/variants_select.sql
@@ -9,12 +9,17 @@ DECLARE
     variant_with_id record;
     course_instance_display_timezone text;
     course_display_timezone text;
-    assessment_record record;
-    assessment_instance_record record;
 BEGIN
-    SELECT v.*, NULL::jsonb AS assessment, NULL::jsonb AS assessment_instance
+    SELECT
+        v.*,
+        to_jsonb(a.*) AS assessment,
+        to_jsonb(ai.*) AS assessment_instance,
+        ai.date AS assessment_instance_date
     INTO variant_with_id
     FROM variants as v
+    LEFT JOIN instance_questions AS iq ON (iq.id = v.instance_question_id)
+    LEFT JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
+    LEFT JOIN assessments AS a ON (a.id = ai.assessment_id)
     WHERE v.id = variants_select.variant_id;
 
     IF NOT FOUND THEN RAISE EXCEPTION 'no such variant_id: %', variant_id; END IF;
@@ -37,20 +42,8 @@ BEGIN
         q.id = variant_with_id.question_id;
 
     IF variant_with_id.instance_question_id IS NOT NULL THEN
-        SELECT ai.*
-        INTO assessment_instance_record
-        FROM instance_questions AS iq
-        JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
-        WHERE iq.id = variant_with_id.instance_question_id;
-
-        SELECT a.*
-        INTO assessment_record
-        FROM assessments AS a
-        WHERE a.id = assessment_instance_record.assessment_id;
-
-        variant_with_id.assessment := to_jsonb(assessment_record.*);
-        variant_with_id.assessment_instance := jsonb_set(to_jsonb(assessment_instance_record.*), '{formatted_date}',
-                                         to_jsonb(format_date_full_compact(assessment_instance_record.date, COALESCE(course_instance_display_timezone, course_display_timezone))));
+        variant_with_id.assessment_instance := jsonb_set(variant_with_id.assessment_instance,
+            '{formatted_date}', to_jsonb(format_date_full_compact(variant_with_id.assessment_instance_date, COALESCE(course_instance_display_timezone, course_display_timezone))));
     END IF;
 
     variant := jsonb_set(to_jsonb(variant_with_id.*), '{formatted_date}',
