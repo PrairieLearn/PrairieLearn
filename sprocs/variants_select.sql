04a5e7330c48f43da7c34e98df6c587090e16591
Jonatan Schroeder <jonatan@cs.ubc.ca>
2020-12-20 17:29:13 -0800

    Link assessment instance from question preview when showing variant linked to instance question

--- sprocs/variants_select.sql
+++ sprocs/variants_select.sql
@@ -2,13 +2,17 @@ DROP FUNCTION IF EXISTS variants_select(bigint);
 CREATE OR REPLACE FUNCTION
     variants_select (
         IN variant_id bigint,
-        OUT variant jsonb
+        OUT variant jsonb,
+        OUT assessment jsonb,
+        OUT assessment_instance jsonb
     )
 AS $$
 DECLARE
     variant_with_id record;
     course_instance_display_timezone text;
     course_display_timezone text;
+    assessment_record record;
+    assessment_instance_record record;
 BEGIN
     SELECT v.*
     INTO variant_with_id
@@ -33,6 +37,23 @@ BEGIN
         JOIN pl_courses AS c ON (c.id = q.course_id)
     WHERE
         q.id = variant_with_id.question_id;
+
+    IF variant_with_id.instance_question_id IS NOT NULL THEN
+       SELECT ai.*
+       INTO assessment_instance_record
+       FROM instance_questions AS iq
+       JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
+       WHERE iq.id = variant_with_id.instance_question_id;
+
+       SELECT a.*
+       INTO assessment_record
+       FROM assessments AS a
+       WHERE a.id = assessment_instance_record.assessment_id;
+
+       assessment := to_jsonb(assessment_record.*);
+       assessment_instance := jsonb_set(to_jsonb(assessment_instance_record.*), '{formatted_date}',
+                                        to_jsonb(format_date_full_compact(assessment_instance_record.date, COALESCE(course_instance_display_timezone, course_display_timezone))));
+    END IF;
 
     variant := jsonb_set(to_jsonb(variant_with_id.*), '{formatted_date}',
                          to_jsonb(format_date_full_compact(variant_with_id.date, COALESCE(course_instance_display_timezone, course_display_timezone))));
