a9216d0d9facd19b399574fff4f5882b4d21c04a
Jonatan Schroeder <jonatan@cs.ubc.ca>
2020-12-22 10:57:27 -0800

    Limit submission rate for a question - DB and schemas

---
+++ migrations/224_limit_submission_rate__columns__add.sql
@@ -0,0 +1,1 @@
+ALTER TABLE assessment_questions ADD COLUMN submission_rate_limit_min integer default 0;
--- sync/fromDisk/assessments.js
+++ sync/fromDisk/assessments.js
@@ -100,9 +100,11 @@ function getParamsForAssessment(assessmentInfoFile, questionIds) {
     let alternativeGroupNumber = 0;
     let assessmentQuestionNumber = 0;
     assessmentParams.alternativeGroups = zones.map((zone) => {
+        let zoneSubmissionRateLimitMin = _.has(zone, 'submissionRateLimitMin') ? zone.submissionRateLimitMin : (assessment.submissionRateLimitMin || 0);
         return zone.questions.map((question) => {
             /** @type {{ qid: string, maxPoints: number | number[], points: number | number[], forceMaxPoints: boolean, triesPerVariant: number }[]} */
             let alternatives;
+            let questionSubmissionRateLimitMin = _.has(question, 'submissionRateLimitMin') ? question.submissionRateLimitMin : zoneSubmissionRateLimitMin;
             if (_(question).has('alternatives')) {
                 alternatives = _.map(question.alternatives, function(alternative) {
                     return {
@@ -112,6 +114,7 @@ function getParamsForAssessment(assessmentInfoFile, questionIds) {
                         forceMaxPoints: _.has(alternative, 'forceMaxPoints') ? alternative.forceMaxPoints
                             : (_.has(question, 'forceMaxPoints') ? question.forceMaxPoints : false),
                         triesPerVariant: _.has(alternative, 'triesPerVariant') ? alternative.triesPerVariant : (_.has(question, 'triesPerVariant') ? question.triesPerVariant : 1),
+                        submissionRateLimitMin: _.has(alternative, 'submissionRateLimitMin') ? alternative.submissionRateLimitMin : questionSubmissionRateLimitMin,
                     };
                 });
             } else if (_(question).has('id')) {
@@ -121,6 +124,7 @@ function getParamsForAssessment(assessmentInfoFile, questionIds) {
                     points: question.points,
                     forceMaxPoints: question.forceMaxPoints || false,
                     triesPerVariant: question.triesPerVariant || 1,
+                    submissionRateLimitMin: questionSubmissionRateLimitMin,
                 }];
             }
 
@@ -166,6 +170,7 @@ function getParamsForAssessment(assessmentInfoFile, questionIds) {
                     init_points: alternative.initPoints,
                     force_max_points: alternative.forceMaxPoints,
                     tries_per_variant: alternative.triesPerVariant,
+                    submission_rate_limit_min: alternative.submissionRateLimitMin,
                     question_id: questionId,
                     number_in_alternative_group: alternativeIndex + 1,
                 };
