7800cf0fb75b2fb9efc6c94da73eb8a904c9d010
mfsilva22 <mfsilva@illinois.edu>
2020-12-26 16:48:44 -0800

    Modified python graded example

--- exampleCourse/questions/demo/autograder/python/dragDropCode_simple/server.py
+++
@@ -1,5 +0,0 @@
-def generate(data):
-    data['params']['names_for_user'] = []
-    data['params']['names_from_user'] = [
-        {'name': 'return_even', 'description': 'a function that returns a hard-coded even number', 'type': 'python function'}
-    ]
