1d7848b633b73526cb649cfe282d1bdee1171dee
Jonatan Schroeder <jonatan@cs.ubc.ca>
2021-01-02 13:56:27 -0800

    Use of interval instead of date in exam overview

--- sprocs/instance_questions_next_allowed_grade.sql
+++ sprocs/instance_questions_next_allowed_grade.sql
@@ -3,23 +3,14 @@ CREATE OR REPLACE FUNCTION
     instance_questions_next_allowed_grade (
         IN iq_id BIGINT,
         OUT allow_grade_date TIMESTAMPTZ,
-        OUT allow_grade_formatted_date TEXT,
-        OUT allow_grade_left_ms BIGINT
+        OUT allow_grade_left_ms BIGINT,
+        OUT allow_grade_interval TEXT
     )
 AS $$
-DECLARE
-    question_id BIGINT;
-    course_instance_id BIGINT;
-    course_instance_display_timezone TEXT;
-    course_display_timezone TEXT;
 BEGIN
     SELECT
-        aq.question_id,
-        v.course_instance_id,
         MAX(gj.date + aq.grade_rate_minutes * INTERVAL '1 min')
     INTO
-        question_id,
-        course_instance_id,
         allow_grade_date
     FROM
         instance_questions iq
@@ -30,34 +21,36 @@ BEGIN
     WHERE
         iq.id = iq_id
         AND aq.grade_rate_minutes IS NOT NULL
-        AND gj.gradable
-    GROUP BY
-        aq.question_id,
-        v.course_instance_id;
+        AND gj.gradable;
 
     IF NOT FOUND THEN
         allow_grade_date := NULL;
-        allow_grade_formatted_date := NULL;
         allow_grade_left_ms := 0;
-        RETURN;
+        allow_grade_interval := 'now';
+    ELSE
+        allow_grade_left_ms := GREATEST(0, floor(extract(epoch from (allow_grade_date - CURRENT_TIMESTAMP)) * 1000));
+        
+        WITH parts AS (
+            SELECT
+                div(allow_grade_left_ms, 1000 * 60 * 60 * 24)     AS days,
+                mod(div(allow_grade_left_ms, 1000 * 60 * 60), 24) AS hours,
+                mod(div(allow_grade_left_ms, 1000 * 60), 60)      AS mins,
+                mod(div(allow_grade_left_ms, 1000), 60)           AS secs
+        )
+        SELECT
+            CASE
+                WHEN days > 1 THEN 'in ' || days::text || ' days'
+                WHEN days > 0 THEN 'in ' || days::text || ' day'
+                WHEN hours > 1 THEN 'in ' || hours::text || ' hours'
+                WHEN hours > 0 THEN 'in ' || hours::text || ' hour'
+                WHEN mins > 1 THEN 'in ' || mins::text || ' minutes'
+                WHEN mins > 0 THEN 'in ' || mins::text || ' minute'
+                WHEN secs > 1 THEN 'in ' || secs::text || ' seconds'
+                ELSE 'soon'
+            END
+        INTO allow_grade_interval
+        FROM parts;
     END IF;
-
-    IF course_instance_id IS NOT NULL THEN
-        SELECT ci.display_timezone
-        INTO course_instance_display_timezone
-        FROM course_instances AS ci
-        WHERE ci.id = course_instance_id;
-    END IF;
-
-    SELECT c.display_timezone
-    INTO course_display_timezone
-    FROM 
-        questions AS q
-        JOIN pl_courses AS c ON (c.id = q.course_id)
-    WHERE q.id = question_id;
-
-    allow_grade_formatted_date := format_date_full_compact(allow_grade_date, COALESCE(course_instance_display_timezone, course_display_timezone));
-    allow_grade_left_ms := GREATEST(0, floor(extract(epoch from (allow_grade_date - CURRENT_TIMESTAMP)) * 1000));
 
 END;
 $$ LANGUAGE PLPGSQL IMMUTABLE;
