7c34ae48f280062b11c903d1ad5cdf772e9a3dd4
Jonatan Schroeder <jonatan@cs.ubc.ca>
2021-01-03 20:05:45 -0800

    Assessibility and programming conventions

--- pages/instructorAssessmentInstances/instructorAssessmentInstances.js
+++ pages/instructorAssessmentInstances/instructorAssessmentInstances.js
@@ -28,24 +28,28 @@ router.get('/', function(req, res, next) {
         let has_open_instance = false;
         let has_closed_instance = false;
         result.rows.forEach(function(row) {
-            if (!row.open)
+            if (!row.open) {
                 has_closed_instance = true;
-            else if (row.time_remaining_sec === null)
+            } else if (row.time_remaining_sec === null) {
                 has_open_instance = true;
-            else {
-                if (!(row.total_time_sec in time_limit_list))
+            } else {
+                if (!(row.total_time_sec in time_limit_list)) {
                     time_limit_list[row.total_time_sec] = row.total_time;
+                }
                 if (remaining_time_min === null ||
-                    remaining_time_min > row.time_remaining_sec)
+                    remaining_time_min > row.time_remaining_sec) {
                     remaining_time_min = row.time_remaining_sec;
+                }
                 if (remaining_time_max === null ||
-                    remaining_time_max < row.time_remaining_sec)
+                    remaining_time_max < row.time_remaining_sec) {
                     remaining_time_max = row.time_remaining_sec;
+                }
             }
         });
         time_limit_list = Object.values(time_limit_list);
-        if (time_limit_list.length > 5)
+        if (time_limit_list.length > 5) {
             time_limit_list.splice(3, time_limit_list.length - 4, '...');
+        }
 
         res.locals.time_limit_totals = {
             total_time: time_limit_list.length > 0 ? time_limit_list.join(', ') : 'No time limits',
@@ -94,7 +98,7 @@ router.post('/', function(req, res, next) {
                 assessment_instance_id,
                 res.locals.authn_user.user_id,
             ];
-            sqldb.call('assessment_instances_delete', params, function(err, _result) {
+            sqldb.call('assessment_instances_delete', params, function(err) {
                 if (ERR(err, next)) return;
                 res.redirect(req.originalUrl);
             });
@@ -104,7 +108,7 @@ router.post('/', function(req, res, next) {
             res.locals.assessment.id,
             res.locals.authn_user.user_id,
         ];
-        sqldb.call('assessment_instances_delete_all', params, function(err, _result) {
+        sqldb.call('assessment_instances_delete_all', params, function(err) {
             if (ERR(err, next)) return;
             res.redirect(req.originalUrl);
         });
@@ -128,19 +132,20 @@ router.post('/', function(req, res, next) {
             base_time: 'date_limit',
             authn_user_id: res.locals.authz_data.authn_user.user_id,
         };
-        if (req.body.plus_minus == 'unlimited')
+        if (req.body.plus_minus == 'unlimited') {
             params.base_time = 'null';
-        else if (req.body.plus_minus == 'expire') {
+        } else if (req.body.plus_minus == 'expire') {
             params.base_time = 'current_date';
             params.time_add = 0;
             params.time_ref = 'minutes';
-        } else if (req.body.plus_minus == 'set_total')
+        } else if (req.body.plus_minus == 'set_total') {
             params.base_time = 'start_date';
-        else if (req.body.plus_minus == 'set_rem')
+        } else if (req.body.plus_minus == 'set_rem') {
             params.base_time = 'current_date';
-        else
+        } else {
             params.time_add *= req.body.plus_minus;
-        sqldb.query(sql.set_time_limit, params, function(err, _result) {
+        }
+        sqldb.query(sql.set_time_limit, params, function(err) {
             if (ERR(err, next)) return;
             res.redirect(req.originalUrl);
         });
@@ -153,19 +158,20 @@ router.post('/', function(req, res, next) {
             reopen_closed: !!req.body.reopen_closed,
             authn_user_id: res.locals.authz_data.authn_user.user_id,
         };
-        if (req.body.plus_minus == 'unlimited')
+        if (req.body.plus_minus == 'unlimited') {
             params.base_time = 'null';
-        else if (req.body.plus_minus == 'expire') {
+        } else if (req.body.plus_minus == 'expire') {
             params.base_time = 'current_date';
             params.time_add = 0;
             params.time_ref = 'minutes';
-        } else if (req.body.plus_minus == 'set_total')
+        } else if (req.body.plus_minus == 'set_total') {
             params.base_time = 'start_date';
-        else if (req.body.plus_minus == 'set_rem')
+        } else if (req.body.plus_minus == 'set_rem') {
             params.base_time = 'current_date';
-        else
+        } else {
             params.time_add *= req.body.plus_minus;
-        sqldb.query(sql.set_time_limit_all, params, function(err, _result) {
+        }
+        sqldb.query(sql.set_time_limit_all, params, function(err) {
             if (ERR(err, next)) return;
             res.redirect(req.originalUrl);
         });
