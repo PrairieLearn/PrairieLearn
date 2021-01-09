d269a21f3e8432b49d6c364ccec4d50aeeb70b4b
Matthew West <mwest@illinois.edu>
2021-01-09 11:16:01 -0600

    Pass course directory as null
    Co-authored-by: Nathan Walters <nwalters512@gmail.com>

--- tools/verify_course_repo.js
+++ tools/verify_course_repo.js
@@ -2,7 +2,7 @@ const courseDB = require('../sync/course-db');
 (async () => {
     const courseData = await courseDB.loadFullCourseNew('/course');
     const errors = [];
-    courseDB.writeErrorsAndWarningsForCourseData('fakeCourseId', courseData, line => line ? errors.push(line) : null);
+    courseDB.writeErrorsAndWarningsForCourseData(null, courseData, line => line ? errors.push(line) : null);
     if (errors.length !== 0) {
         errors.forEach(line => console.error(line));
         process.exit(1);
