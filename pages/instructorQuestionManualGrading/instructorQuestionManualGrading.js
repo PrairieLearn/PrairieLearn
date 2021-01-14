4f525d8b335fb4c5b9b17a8b07dce35cf2a4b873
mebird <mebird@users.noreply.github.com>
2021-01-13 23:39:43 -0800

    cleaning up & adding comments for debug

--- pages/instructorQuestionManualGrading/instructorQuestionManualGrading.js
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGrading.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const path = require('path');
 const async = require('async');
 const question = require('../../lib/question');
-const logPageView = require('../../middlewares/logPageView')(path.basename(__filename, '.js'));
 const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
 const { error, sqlDb} = require('@prairielearn/prairielib');
 
@@ -21,6 +20,13 @@ router.get('/', (req, res, next) => {
             });
         },
        (callback) => {
+            // FYI, Maja:
+            // Use case 1: Student never loaded question (variant and submission is null)
+            // Use case 2: Student loaded question but did not submit anything (submission is null)
+            // Use case 1/2: "An answer has not been submitted for this question. Grading has been disabled."
+            //              Should we render the question in this case?
+            // Use case 3: Student has answered question (question, variant, submission are NOT null)
+            // Other cases to figure out later: grading in progress, question is broken...
             console.log(res.locals);
             res.locals.overlayGradingInterface = true;
             question.getAndRenderVariant(res.locals.variant.id, null, res.locals, function (
