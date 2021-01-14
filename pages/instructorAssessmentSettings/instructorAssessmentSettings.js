e9a4b6687a0603ccb459ac4968ac6cdc497eb4a6
Jonatan Schroeder <jonatan@cs.ubc.ca>
2021-01-13 16:43:21 -0800

    Add student link to assessment settings page

--- pages/instructorAssessmentSettings/instructorAssessmentSettings.js
+++ pages/instructorAssessmentSettings/instructorAssessmentSettings.js
@@ -29,6 +29,7 @@ router.get('/', function(req, res, next) {
     ], function(err) {
         if (ERR(err, next)) return;
         debug('render page');
+        res.locals.urlHost = req.headers.host;
         res.locals.infoAssessmentPath = encodePath(path.join('courseInstances', res.locals.course_instance.short_name, 'assessments', res.locals.assessment.tid, 'infoAssessment.json'));
         res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
     });
