b3e4c740bc06733e92c50abf02b619443ffca9ed
Nathan Walters <nwalters512@gmail.com>
2021-01-16 17:07:20 -0800

    Drop one-off executor files

--- images/executor/src/@types/async-stacktrace.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "async-stacktrace" {
-  function ERR(
-    err: Error | null | undefined,
-    callback: ((err: Error | null | undefined, ...args: any[]) => void) | null
-  ): boolean;
-
-  export = ERR;
-}
