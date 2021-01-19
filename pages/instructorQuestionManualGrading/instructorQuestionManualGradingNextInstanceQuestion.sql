e1f30908c01011c85a2af09bb7b21117a8c4ad49
Andrew Stec <andrewstec@gmail.com>
2021-01-19 13:34:51 -0800

    Changing query to select instance_questions where no submissions have been submitted by student for manual grading

--- pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
@@ -2,6 +2,7 @@
 SELECT iq.* FROM instance_questions AS iq
     LEFT JOIN variants AS v ON (v.instance_question_id = iq.id)
     LEFT JOIN submissions AS s ON (s.variant_id = v.id)
-WHERE iq.assessment_question_id = $assessment_question_id AND s.graded_at IS NULL;
-
+    -- Note that we want WHERE s.graded_at OR where no submission exists so we can manually grade 
+    -- non-submissions as 0 or where submissions have been submitted likely as non-0 scores.
+WHERE iq.assessment_question_id = $assessment_question_id AND (s.graded_at IS NULL OR s IS NULL);
     
