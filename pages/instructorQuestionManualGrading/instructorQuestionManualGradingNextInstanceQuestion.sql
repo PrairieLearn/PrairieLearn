8865f83adb49de2a21fc3a1e8412d06f0cc11ff3
Andrew Stec <andrewstec@gmail.com>
2021-01-20 14:09:28 -0800

    Removing hardcoded query

--- pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
@@ -8,7 +8,7 @@ SELECT s.*
         ORDER BY s.date DESC, s.id DESC
         LIMIT 1
     ) s ON (s.variant_id = v.id)
-    -- ... and ONLY IF the last submission 'graded_at' is null (has not been graded)
-WHERE iq.assessment_question_id = 53 AND s.graded_at IS NULL
+    -- ... but ONLY IF the last submission 'graded_at' is null (has not been graded)
+WHERE iq.assessment_question_id = $assessment_question_id AND s.graded_at IS NULL
 ORDER BY s.date DESC, s.id DESC
 LIMIT 1;
