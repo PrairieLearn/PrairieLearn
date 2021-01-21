c6c356f70455c33758b114b4948a3227756cead4
mebird <mebird@users.noreply.github.com>
2021-01-21 05:53:03 -0800

    basic query fixes and monkey patching other items

--- pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.js
+++ pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.js
@@ -8,6 +8,10 @@ const sqlLoader = require('@prairielearn/prairielib/sql-loader');
 
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
+function isGradable(instanceQuestion) {
+    return !instanceQuestion.graded_at;
+}
+
 router.get('/', function(req, res, next) {
     debug('GET /');
     var params = {
@@ -15,7 +19,8 @@ router.get('/', function(req, res, next) {
     };
     sqldb.query(sql.select_submissions_manual_grading, params, function(err, result) {
         if (ERR(err, next)) return;
-        res.locals.instance_questions = result.rows;
+        res.locals.instance_questions_to_grade = result.rows.filter(isGradable);
+        res.locals.instance_questions_graded = result.rows.filter(iq => !isGradable(iq));
         console.log(result.rows);
         debug('render page');
         res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
--- pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.sql
+++ pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.sql
@@ -1,9 +1,14 @@
 -- BLOCK select_submissions_manual_grading
 SELECT
-    s.*
+    iq.*,
+    s.graded_at,
+    q.id AS question_id,
+    aq.max_points
 FROM
     instance_questions AS iq
     JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
+    JOIN assessment_questions AS aq ON (aq.id = iq.assessment_question_id)
+    JOIN questions AS q ON (q.id = aq.question_id)
     JOIN variants AS v ON (v.instance_question_id = iq.id)
     JOIN (
         -- We only want the last submission...
@@ -12,7 +17,7 @@ FROM
         LIMIT 1
     ) s ON (s.variant_id = v.id)
 WHERE
-    -- Front end can filter out by the manual grading date
     ai.assessment_id = $assessment_id
+    AND s.gradable IS TRUE
 ORDER BY iq.id DESC, s.date DESC, s.id DESC
 LIMIT 1;
