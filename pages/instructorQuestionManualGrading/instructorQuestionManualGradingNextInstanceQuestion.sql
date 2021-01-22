089214ce2cd61ba76bff408dea1956027b5da200
Andrew Stec <andrewstec@gmail.com>
2021-01-22 14:46:38 -0800

    Fixing logic to find a unique submission per user

--- pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
@@ -4,11 +4,9 @@ SELECT iq.*
     JOIN variants AS v ON (v.instance_question_id = iq.id)
     JOIN (
         -- We only want the last submission...
-        SELECT * FROM submissions AS s
-        ORDER BY s.date DESC, s.id DESC
-        LIMIT 1
+        SELECT DISTINCT ON (s.auth_user_id) * FROM submissions AS s
+        ORDER BY s.auth_user_id, s.date DESC, s.id DESC
     ) s ON (s.variant_id = v.id)
     -- ... but ONLY IF the last submission 'graded_at' is null (has not been graded)
-WHERE iq.assessment_question_id = $assessment_question_id AND s.graded_at IS NULL
-ORDER BY s.date DESC, s.id DESC
+WHERE iq.assessment_question_id = $assessment_question_id AND s.graded_at IS NULL;
 LIMIT 1;
