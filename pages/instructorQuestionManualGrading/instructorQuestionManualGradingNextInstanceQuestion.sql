a5efec300bd7a5e839cad16ecc90f1a8d4b38992
Andrew Stec <andrewstec@gmail.com>
2021-01-22 14:47:23 -0800

    Updating comment

--- pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
@@ -3,10 +3,9 @@ SELECT iq.*
     FROM instance_questions AS iq
     JOIN variants AS v ON (v.instance_question_id = iq.id)
     JOIN (
-        -- We only want the last submission...
+        -- We only want the LAST submission that is created by each user
         SELECT DISTINCT ON (s.auth_user_id) * FROM submissions AS s
         ORDER BY s.auth_user_id, s.date DESC, s.id DESC
     ) s ON (s.variant_id = v.id)
-    -- ... but ONLY IF the last submission 'graded_at' is null (has not been graded)
 WHERE iq.assessment_question_id = $assessment_question_id AND s.graded_at IS NULL;
 LIMIT 1;
