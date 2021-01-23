af8b4b8c9659fce34194764aa5793b68a8735f65
David Gene <david@astrosticks.com>
2021-01-23 15:34:23 -0600

    Update sequential tests to account for cascade unlocking questions

--- tests/testSequentialQuestions.js
+++ tests/testSequentialQuestions.js
@@ -31,7 +31,7 @@ describe('Assessment that forces students to complete questions in-order', funct
     const response = await helperClient.fetchCheerio(context.instructorAssessmentQuestionsUrl, { method: 'GET' });
     assert.isTrue(response.ok);
 
-    const expectedPercentages = [0, 60, 75, 100];
+    const expectedPercentages = [0, 60, 75, 0, 100, 100];
     const computedPercentages = response.$('.pl-sequence-prev-unlock-score').map((i, elem) => {
       // turn string "25%" -> number 25
       return Number(response.$(elem).text().trim().slice(0,-1));
@@ -39,7 +39,20 @@ describe('Assessment that forces students to complete questions in-order', funct
     assert.deepEqual(expectedPercentages, computedPercentages);
   });
 
+  /**
+   * Updates context.instanceQuestions to the current state of the assessment instance
+   */
+  async function refreshContextQuestions() {
+    const results = await sqldb.callAsync('question_order', [context.assessmentInstanceId]);
+    context.instanceQuestions = results.rows.map(e => {return {
+      id: Number(e.instance_question_id),
+      locked: Boolean(e.sequence_locked),
+      url: `${context.courseInstanceBaseUrl}/instance_question/${e.instance_question_id}/`
+    };});
+  }
+
   step('Questions are locked/unlocked properly on student assessment page', async function() {
+    // Generate assessment instance
     const assessmentCreateResponse = await helperClient.fetchCheerio(context.assessmentUrl);
     helperClient.extractAndSaveCSRFToken(context, assessmentCreateResponse.$, 'form');
     const form = {
@@ -54,21 +67,30 @@ describe('Assessment that forces students to complete questions in-order', funct
     assert.include(assessmentInstanceUrl, '/assessment_instance/');
     context.assessmentInstanceUrl = assessmentInstanceUrl;
 
-    const expectedLocks = [false, false, true, true];
+    const urlParts = assessmentInstanceUrl.split('/');
+    context.assessmentInstanceId = urlParts[urlParts.length-1];
+    await refreshContextQuestions();
+
+    initialExpectedLocks = [false, false, true, true, true, true];
+
+    // Locks in database match assessment configuration
+    assert.deepEqual(
+      context.instanceQuestions.map(e=>{
+        return e.locked;
+      }),
+      initialExpectedLocks
+    );
+
+    // Locks in UI match database
     const computedLocks = response.$('table#assessment-questions tbody tr').map((i, elem) => {
       return response.$(elem).hasClass('pl-sequence-locked');
     }).get();
-    assert.deepEqual(expectedLocks, computedLocks);
-
-    // Get URL for Question 2
-    context.questionUrl = context.siteUrl + response.$('a:contains("Question 2")').attr('href');
+    assert.deepEqual(computedLocks, initialExpectedLocks);
   });
 
   step('Accessing Question 3 returns a 403', async function() {
-    const results = await sqldb.queryOneRowAsync(sql.select_locked_question, []);
-    context.lockedQuestionId = results.rows[0].id;
-    context.lockedQuestionUrl = `${context.courseInstanceBaseUrl}/instance_question/${context.lockedQuestionId}/`;
-    const response = await helperClient.fetchCheerio(context.lockedQuestionUrl);
+    context.lockedQuestion = context.instanceQuestions[2];
+    const response = await helperClient.fetchCheerio(context.lockedQuestion.url);
     assert.isTrue(!response.ok);
     assert.equal(response.status, 403);
   });
@@ -76,18 +98,19 @@ describe('Assessment that forces students to complete questions in-order', funct
   step('Question 3 URL is not exposed from student assessment page', async function() {
     const response = await helperClient.fetchCheerio(context.assessmentUrl);
     assert.isTrue(response.ok);
-    assert.equal(response.$(`a[href*="instance_question/${context.lockedQuestionId}"]`).length, 0);
+    assert.equal(response.$(`a[href*="instance_question/${context.lockedQuestion.id}"]`).length, 0);
   });
 
   step('Question 2 "next question" link is locked before any submissions', async function() {
-    const response = await helperClient.fetchCheerio(context.questionUrl);
+    context.firstUnlockedQuestion = context.instanceQuestions[1];
+    const response = await helperClient.fetchCheerio(context.firstUnlockedQuestion.url);
     assert.isTrue(response.ok);
 
     assert.isTrue(response.$('#question-nav-next').hasClass('pl-sequence-locked'));
   });
 
-  async function submitQuestion(score, questionUrl) {
-    const preSubmissionResponse = await helperClient.fetchCheerio(questionUrl);
+  async function submitQuestion(score, question) {
+    const preSubmissionResponse = await helperClient.fetchCheerio(question.url);
     assert.isTrue(preSubmissionResponse.ok);
     helperClient.extractAndSaveCSRFToken(context, preSubmissionResponse.$, '.question-form');
     context.__variant_id = preSubmissionResponse.$('.question-form input[name="__variant_id"]').attr('value');
@@ -98,36 +121,45 @@ describe('Assessment that forces students to complete questions in-order', funct
       '__csrf_token': context.__csrf_token,
     };
 
-    const response = await helperClient.fetchCheerio(questionUrl, { method: 'POST', form });
+    const response = await helperClient.fetchCheerio(question.url, { method: 'POST', form });
     assert.isTrue(response.ok);
 
     return response;
   }
 
   step('Submitting 50% on Question 2 does not unlock Question 3', async function() {
-    const response = await submitQuestion(50, context.questionUrl);
+    const response = await submitQuestion(50, context.firstUnlockedQuestion);
     assert.isTrue(response.$('#question-nav-next').hasClass('pl-sequence-locked'));
   });
 
   step('Submitting 75% on Question 2 unlocks Question 3', async function() {
-    const response = await submitQuestion(75, context.questionUrl);
+    const response = await submitQuestion(75, context.firstUnlockedQuestion);
     assert.isFalse(response.$('#question-nav-next').hasClass('pl-sequence-locked'));
   });
 
   step('Submitting 0% on Question 2 leaves Question 3 unlocked', async function() {
-    const response = await submitQuestion(0, context.questionUrl);
+    const response = await submitQuestion(0, context.firstUnlockedQuestion);
     assert.isFalse(response.$('#question-nav-next').hasClass('pl-sequence-locked'));
   });
 
   step('Accessing Question 3 no longer returns a 403 and Question 4 is locked', async function() {
-    const response = await helperClient.fetchCheerio(context.lockedQuestionUrl);
+    const response = await helperClient.fetchCheerio(context.lockedQuestion.url);
     assert.isTrue(response.ok);
     assert.isTrue(response.$('#question-nav-next').hasClass('pl-sequence-locked'));
   });
 
   step('Submitting 0% on Question 3 unlocks Question 4 (run out of attempts)', async function() {
-    const response = await submitQuestion(0, context.lockedQuestionUrl);
+    const response = await submitQuestion(0, context.lockedQuestion);
     assert.isTrue(response.ok);
     assert.isFalse(response.$('#question-nav-next').hasClass('pl-sequence-locked'));
+  });
+
+  step('Unlocking question 4 cascades to question 5', async function() {
+    await refreshContextQuestions();
+    assert.isFalse(context.instanceQuestions[4].locked);
+  });
+
+  step('Unlocking question 4 does NOT cascade to question 6', async function() {
+    assert.isTrue(context.instanceQuestions[5].locked);
   });
 });
--- tests/testSequentialQuestions.sql
+++ tests/testSequentialQuestions.sql
@@ -6,13 +6,3 @@ FROM
 WHERE 
     uuid = 'd7bcc376-4f23-41d6-9f71-87dd1d23991b'
 ;
-
--- BLOCK select_locked_question
-SELECT
-    iq.id
-FROM
-    instance_questions iq
-    JOIN assessment_questions aq ON (aq.id = iq.assessment_question_id)
-    JOIN questions q ON (q.id = aq.question_id)
-WHERE
-    q.qid = 'partialCredit2';
