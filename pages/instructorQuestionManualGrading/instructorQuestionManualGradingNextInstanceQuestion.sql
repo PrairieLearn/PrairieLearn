ab501c41b2d340f45f03342861a88ee9402ca4eb
Andrew Stec <andrewstec@gmail.com>
2021-01-25 12:20:46 -0800

    Fixing issue with graded_at in wrong part of query

--- pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
@@ -3,8 +3,11 @@ SELECT iq.*
     FROM instance_questions AS iq
     JOIN variants AS v ON (v.instance_question_id = iq.id)
     JOIN (
-        -- We only want the LAST submission that is created by each user
+        -- We only want the LAST submissions that are created by each user to join on all
+        -- instance questions under assessment question
         SELECT DISTINCT ON (s.auth_user_id) * FROM submissions AS s
+        WHERE s.graded_at IS NULL
         ORDER BY s.auth_user_id, s.date DESC, s.id DESC
     ) s ON (s.variant_id = v.id)
-WHERE iq.assessment_question_id = $assessment_question_id AND s.graded_at IS NULL;
+WHERE iq.assessment_question_id = 50
+LIMIT 1;
