d76b1046ce91b06be4ae177613c563a2cbbf69d2
Jonatan Schroeder <jonatan@cs.ubc.ca>
2021-01-26 17:00:54 -0800

    Fixes in general

--- pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.sql
+++ pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.sql
@@ -1,5 +1,5 @@
 -- BLOCK select_instance_questions_manual_grading
-SELECT
+SELECT DISTINCT ON (iq.id)
     iq.*,
     s.graded_at,
     q.id AS question_id,
@@ -13,13 +13,7 @@ FROM
     JOIN questions AS q ON (q.id = aq.question_id)
     JOIN question_order(ai.id) AS qo ON (qo.instance_question_id = iq.id)
     JOIN variants AS v ON (v.instance_question_id = iq.id)
-    JOIN (
-        -- We only want the last submission...
-        SELECT * FROM submissions AS s
-        ORDER BY s.date DESC, s.id DESC
-        LIMIT 1
-    ) s ON (s.variant_id = v.id)
+    JOIN submissions s ON (s.variant_id = v.id)
 WHERE
     ai.assessment_id = $assessment_id
-ORDER BY iq.id DESC, s.date DESC, s.id DESC
-LIMIT 1;
+ORDER BY iq.id DESC, s.date DESC, s.id DESC;
