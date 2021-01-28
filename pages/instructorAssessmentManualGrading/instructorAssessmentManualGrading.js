48d3767d2c4ae2de9a326d07676073184761043a
Jonatan Schroeder <jonatan@cs.ubc.ca>
2021-01-28 11:05:49 -0800

    Lint

--- pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.js
+++ pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
-const { _error, sqlDb, sqlLoader} = require('@prairielearn/prairielib');
+const { sqlDb, sqlLoader} = require('@prairielearn/prairielib');
 
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
