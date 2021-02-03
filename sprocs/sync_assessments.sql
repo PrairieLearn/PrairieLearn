32612f15a979d812243bd22eb9d02d527f59ece5
Dev User <dev@illinois.edu>
2021-02-03 17:20:51 +0800

    change unique index

--- sprocs/sync_assessments.sql
+++ sprocs/sync_assessments.sql
@@ -183,14 +183,14 @@ BEGIN
                 (valid_assessment.data->>'student_group_create')::boolean,
                 (valid_assessment.data->>'student_group_join')::boolean,
                 (valid_assessment.data->>'student_group_leave')::boolean
-            ) ON CONFLICT (assessment_id)
+            ) ON CONFLICT (assessment_id) WHERE deleted_at IS NULL
             DO UPDATE
             SET 
-                maximum = (valid_assessment.data->>'group_max_size')::bigint,
-                minimum = (valid_assessment.data->>'group_min_size')::bigint,
-                student_authz_create = (valid_assessment.data->>'student_group_create')::boolean,
-                student_authz_join = (valid_assessment.data->>'student_group_join')::boolean,
-                student_authz_leave = (valid_assessment.data->>'student_group_leave')::boolean;
+                maximum = EXCLUDED.maximum,
+                minimum = EXCLUDED.minimum,
+                student_authz_create = EXCLUDED.student_authz_create,
+                student_authz_join = EXCLUDED.student_authz_join,
+                student_authz_leave = EXCLUDED.student_authz_leave;
         END IF;
 
         -- Now process all access rules for this assessment
