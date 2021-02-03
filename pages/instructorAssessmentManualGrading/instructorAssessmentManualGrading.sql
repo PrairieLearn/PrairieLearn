18bdfa094525f1140505c4ace17f49a4cb153633
Andrew Stec <andrewstec@gmail.com>
2021-02-03 13:59:17 -0800

    Elevating user names to Grading By column by people who manually grade assessment question

--- pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.sql
+++ pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.sql
@@ -30,11 +30,12 @@ SELECT
      WHERE
          iq.assessment_question_id = aq.id
          AND s.manual_grading_user IS NOT NULL) AS num_locked_submissions,
-    (SELECT array_agg(DISTINCT s.manual_grading_user)
+    (SELECT array_agg(DISTINCT u.uid)
      FROM
          instance_questions AS iq
          JOIN variants AS v ON (v.instance_question_id = iq.id)
          JOIN submissions AS s ON (s.variant_id = v.id)
+         JOIN users AS u ON (u.user_id = s.manual_grading_user)
      WHERE
          iq.assessment_question_id = aq.id) AS manual_grading_users
 FROM
