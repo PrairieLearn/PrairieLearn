7709fcffd936f4e94f75d63212b8b23ed21501cf
Andrew Stec <andrewstec@gmail.com>
2021-02-09 12:40:23 -0800

    Correcting query, as would not get last submission without FULL JOIN on user table

--- sprocs/instance_questions_select_manual_grading_objects.sql
+++ sprocs/instance_questions_select_manual_grading_objects.sql
@@ -9,7 +9,8 @@ CREATE OR REPLACE FUNCTION
         IN user_id bigint,
         OUT question jsonb,
         OUT variant jsonb,
-        OUT submission jsonb
+        OUT submission jsonb,
+        OUT _user jsonb
     )
 AS $$
 BEGIN
@@ -22,24 +23,27 @@ BEGIN
         JOIN questions AS q ON (q.id = aq.question_id)
         JOIN variants AS v ON (v.instance_question_id = iq.id)
         JOIN submissions AS s ON (s.variant_id = v.id)
-    WHERE iq.id = 3
+    WHERE iq.id = iq_id
     ORDER BY s.date DESC, s.id DESC
     LIMIT 1
     FOR UPDATE;
 
-    UPDATE submissions
-    SET manual_grading_user = user_id
-    WHERE id = (SELECT id FROM submission);
+    IF submission IS NOT NULL AND submission->>'manual_grading_user' IS NOT NULL THEN
+        UPDATE submissions
+        SET manual_grading_user = user_id
+        WHERE id = (SELECT id FROM submission);
+    END IF;
 
-    SELECT to_jsonb(q.*), to_jsonb(v.*), to_jsonb(s.*)
-    INTO question, variant, submission
+    SELECT to_jsonb(q.*), to_jsonb(v.*), to_jsonb(s.*), to_jsonb(u.*)
+    INTO question, variant, submission, _user
     FROM
         instance_questions AS iq
         JOIN assessment_questions AS aq ON (aq.id = iq.assessment_question_id)
         JOIN questions AS q ON (q.id = aq.question_id)
         LEFT JOIN variants AS v ON (v.instance_question_id = iq.id)
         LEFT JOIN submissions AS s ON (s.variant_id = v.id)
-    WHERE iq.id = 3
+        FULL JOIN users as u ON (s.manual_grading_user = u.user_id)
+    WHERE iq.id = iq_id
     ORDER BY s.date DESC, s.id DESC
     LIMIT 1;
 
