2d51a59e5883eac74b2ac39c48bf412354dd73af
Matthew West <mwest@illinois.edu>
2021-02-08 21:44:54 -0600

    Make migration idempotent

--- migrations/228_workspaces__home_dir__add.sql
+++ migrations/228_workspaces__home_dir__add.sql
@@ -1,6 +1,7 @@
 -- Create homedir location with no default, and retroactively set all existing workspaces to be using S3
 ALTER TABLE workspaces
-ADD COLUMN homedir_location enum_file_storage_type;
+ADD COLUMN IF NOT EXISTS homedir_location enum_file_storage_type;
 
 UPDATE workspaces
-SET homedir_location = 'S3';
+SET homedir_location = 'S3'
+WHERE homedir_location IS NULL;
