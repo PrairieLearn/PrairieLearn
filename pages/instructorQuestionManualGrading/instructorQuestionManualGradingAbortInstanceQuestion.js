30a97893be5c6e18e84898b95b6b59b74cfe1a08
Andrew Stec <andrewstec@gmail.com>
2021-02-16 11:42:58 -0800

    Fixing linting

--- pages/instructorQuestionManualGrading/instructorQuestionManualGradingAbortInstanceQuestion.js
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGradingAbortInstanceQuestion.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
-
+const error = require('@prairielearn/prairielib/error');
 const sqldb = require('@prairielearn/prairielib/sql-db');
 const sqlLoader = require('@prairielearn/prairielib/sql-loader');
 
@@ -17,8 +17,8 @@ router.get('/', function(req, res, next) {
     const url = `${res.locals.urlPrefix}/course_instance/${res.locals.course_instance_id}/instructor/assessment/${res.locals.assessment_id}/manual_grading`;
 
     sqldb.queryOneRow(sql.instance_question_abort_manual_grading, params, function(err, result) {
-        if (ERR(err, next)) return callback(error.makeWithData(`Cannot find instance question: ${res.locals.instance_question_id}`));
-        if (result.rowCount > 0) { res.redirect(redirectUrl)}
+        if (ERR(err, next)) return next(error.make(500, `Cannot find instance question: ${res.locals.instance_question_id}`));
+        if (result.rowCount > 0) { res.redirect(url); }
     });
 });
 
