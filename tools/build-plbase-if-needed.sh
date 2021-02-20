ad8f4405b2e5fdef7f97941757c746d149401e86
Nathan Walters <nwalters512@gmail.com>
2021-02-20 14:16:54 -0800

    Update plbase build script

--- tools/build-plbase-if-needed.sh
+++ tools/build-plbase-if-needed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if git diff --exit-code remotes/origin/master...HEAD -- images/plbase; then
+if git diff --exit-code origin/master...HEAD -- images/plbase; then
   echo "prairielearn/plbase files not modified; no rebuild required"
 else
   echo "prairielearn/plbase requires a rebuild"
