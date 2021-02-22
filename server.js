b414454cef46cdb4f1dba704c2e55b16ed56546c
Andrew Stec <andrewstec@gmail.com>
2021-02-22 13:11:21 -0800

    Removing endpoint, as now internal to instructorQuestionManualGrading namespace

--- server.js
+++ server.js
@@ -718,16 +718,6 @@ module.exports.initExpress = function() {
         function(req, res, next) {res.locals.assessment_question_id = req.params.assessment_question_id; next();},
         require('./pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion'),
     ]);
-    app.use('/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/instance_question/:instance_question_id/abort_grading', [
-        function(req, res, next) {
-            res.locals.instance_question_id = req.params.instance_question_id;
-            res.locals.course_instance_id = req.params.course_instance_id;
-            res.locals.assessment_id = req.params.assessment_id;
-            next();
-        },
-        require('./middlewares/selectAndAuthzAssessment'),
-        require('./pages/instructorQuestionManualGrading/instructorQuestionManualGradingAbortInstanceQuestion'),
-    ]);
     app.use('/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/assessment_question/:assessment_question_id/manual_grading', [
         function(req, res, next) {res.locals.navSubPage = 'manual_grading'; next();},
         function(req, res, next) {res.locals.assessment_question_id = req.params.assessment_question_id; next();},
