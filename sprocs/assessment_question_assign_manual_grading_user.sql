ec8431bb50d672a375398ee0f285da2ec57e0920
Andrew Stec <andrewstec@gmail.com>
2021-02-26 12:59:36 -0800

    Fixing bug where all assessment questions were manual grade user reset

--- sprocs/assessment_question_assign_manual_grading_user.sql
+++ sprocs/assessment_question_assign_manual_grading_user.sql
@@ -19,22 +19,20 @@ BEGIN
 
     -- Reset manual_grading_user field for any abandoned/ungraded iqs for current user
     WITH instance_questions_graded_at AS (
-        SELECT DISTINCT ON (iq.id) iq.id, s.graded_at
+        SELECT DISTINCT ON (iq.id) iq.*, s.graded_at
         FROM instance_questions AS iq
             JOIN variants AS v ON (v.instance_question_id = iq.id)
             JOIN submissions AS s ON (s.variant_id = v.id)
-        WHERE
-            iq.assessment_question_id = arg_assessment_question_id
-            AND iq.manual_grading_user = arg_user_id
-            AND iq.id != arg_instance_question_id
         ORDER BY iq.id ASC, s.date DESC, s.id DESC
     )
     UPDATE instance_questions AS iq
     SET manual_grading_user = NULL
-    FROM instance_questions_graded_at
+    FROM instance_questions_graded_at as iqga
     WHERE
-        iq.id = instance_questions_graded_at.id
-        AND instance_questions_graded_at.graded_at IS NULL;
+        iqga.manual_grading_user = arg_user_id
+        AND iqga.id != arg_instance_question_id
+        AND iqga.assessment_question_id = arg_assessment_question_id
+        AND iqga.graded_at IS NULL;
 
 END;
 $$ LANGUAGE plpgsql VOLATILE;
