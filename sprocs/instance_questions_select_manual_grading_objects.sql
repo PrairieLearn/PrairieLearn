3215bdf9010ebe20d8317ab84a0276b5e3d80418
Andrew Stec <andrewstec@gmail.com>
2021-03-01 08:38:13 -0800

    Fixing bug on retrieving submission

--- sprocs/instance_questions_select_manual_grading_objects.sql
+++ sprocs/instance_questions_select_manual_grading_objects.sql
@@ -42,8 +42,8 @@ BEGIN
         instance_questions AS iq
         JOIN assessment_questions AS aq ON (aq.id = iq.assessment_question_id)
         JOIN questions AS q ON (q.id = aq.question_id)
-        LEFT JOIN variants AS v ON (v.instance_question_id = iq.id)
-        LEFT JOIN submissions AS s ON (s.variant_id = v.id)
+        JOIN variants AS v ON (v.instance_question_id = iq.id)
+        JOIN submissions AS s ON (s.variant_id = v.id)
         JOIN users AS u ON (u.user_id = iq.manual_grading_user)
     WHERE iq.id = arg_instance_question_id
     ORDER BY s.date DESC, s.id DESC
