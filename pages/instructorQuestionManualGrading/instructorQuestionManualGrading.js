ec03ff7f31ff96fa3486195b7544faf71cf5d2b2
Andrew Stec <andrewstec@gmail.com>
2021-03-09 10:55:50 -0800

    CAdding backend logic for accepting submission and loading view on diff, integrating with front-end

--- pages/instructorQuestionManualGrading/instructorQuestionManualGrading.js
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGrading.js
@@ -58,44 +58,61 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', function(req, res, next) {
+
+    const note = req.body.submission_note;
+    const score = req.body.submission_score;
+    const modifiedAt = req.body.instance_question_modified_at;
+    const params = [
+        res.locals.instance_question.id,
+        res.locals.authn_user.user_id,
+        score / 100,
+        modifiedAt,
+        {manual: note},
+    ];
+
     if (req.body.__action == 'add_manual_grade') {
-        const note = req.body.submission_note;
-        const score = req.body.submission_score;
-        const modifiedAt = req.body.instance_question_modified_at;
-        const params = [
-            res.locals.instance_question.id,
-            res.locals.authn_user.user_id,
-            score / 100,
-            modifiedAt,
-            {manual: note},
-        ];
-        sqlDb.callZeroOrOneRow('instance_questions_manually_grade_submission', params, (err, result) => {
-            if (ERR(err, next)) return;
-            const {instance_question, submission, grading_user, instance_question_modified} = result.rows[0];
+        async.series([
+            (callback) => {
+                sqlDb.callZeroOrOneRow('instance_questions_manually_grade_submission', params, (err, result) => {
+                    if (ERR(err, next)) return;
+                    const {instance_question, submission, grading_user, instance_question_modified, variant} = result.rows[0];
+                    if (!instance_question || !submission || !grading_user || !variant) return next(error.make('500', 'Manual grading dependencies missing'));
+                    Object.assign(res.locals, {instance_question, submission, grading_user, instance_question_modified, variant});
+                    
+                if (instance_question_modified == true) {
+                    // someone has beat user to submitting manual grade
+    
+                    Object.assign(res.locals, {
+                        diff: {
+                            current: {
+                                graded_by: `${res.locals.grading_user.name}: (${res.locals.grading_user.uid})`,
+                                feedback: res.locals.submission.feedback,
+                                score: res.locals.submission.score,
+                            },
+                            incoming: {
+                                graded_by: `${res.locals.authn_user.name}: (${res.locals.authn_user.uid})`,
+                                feedback: {manual: note},
+                                score,
+                            },
+                        },
+                    });
+                }
 
-            if (instance_question_modified == true) {
-            // someone has beat user to submitting manual grade
-
-                if (!instance_question || !submission || !grading_user) return next(error.make('500', 'Manual grading dependencies missing'));
-
-                const diff = JSON.stringify({
-                    current: {
-                        graded_by: `${grading_user.name}: (${grading_user.uid})`,
-                        feedback: submission.feedback,
-                        score: submission.score,
-                    },
-                    incoming: {
-                        graded_by: `${res.locals.authn_user.name}: (${res.locals.authn_user.uid})`,
-                        feedback: {manual: note},
-                        score,
-                    },
+                callback(null);
                 });
-
-                return res.status(409).json(diff);
-                // no way to redirect with a payload and want to avoid storing this information in new database table
-                // return res.redirect(302, `${res.locals.urlPrefix}/instance_question/${instance_question.id}/manual_grading/select_submission?diff=${encodeURIComponent(diff)}`);
+            },
+           (callback) => {
+                res.locals.overlayGradingInterface = true;
+                question.getAndRenderVariant(res.locals.variant.id, null, res.locals, function (err) {
+                  if (ERR(err, next)) return;
+                  callback(null);
+                });
+            },
+        ], (err) => {
+            if (ERR(err, next)) return;   
+            if (res.locals.instance_question_modified == true) {
+                return res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
             }
-            res.redirect(`${res.locals.urlPrefix}/assessment/${req.body.assessment_id}/assessment_question/${req.body.assessment_question_id}/next_ungraded`);
         });
     } else if (req.body.__action == 'abort_manual_grading') {
         const params = {
@@ -106,6 +123,11 @@ router.post('/', function(req, res, next) {
         sqlDb.queryOneRow(sql.instance_question_abort_manual_grading, params, function(err, result) {
             if (ERR(err, next)) return next(error.make(500, `Cannot find instance question: ${res.locals.instance_question_id}`));
             if (result.rowCount > 0) { res.redirect(url); }
+        });
+    } else if (req.body.__action == 'accept_manual_grade_conflict') {
+        sqlDb.callZeroOrOneRow('instance_questions_manually_grade_submission', params, (err) => {
+            if (ERR(err, next)) return;
+            res.redirect(`${res.locals.urlPrefix}/assessment/${req.body.assessment_id}/assessment_question/${req.body.assessment_question_id}/next_ungraded`);
         });
     } else {
         return next(error.make(400, 'unknown __action', {locals: res.locals, body: req.body}));
