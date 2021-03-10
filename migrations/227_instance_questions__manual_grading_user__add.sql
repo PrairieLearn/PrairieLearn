cadbc27d897c14966aeb52111e1063c1b5c60935
Andrew Stec <andrewstec@gmail.com>
2021-03-10 09:12:41 -0800

    Adding new value for manual grading conflicts

--- migrations/227_instance_questions__manual_grading_user__add.sql
+++ migrations/227_instance_questions__manual_grading_user__add.sql
@@ -1,2 +1,2 @@
-ALTER TABLE instance_questions
-    ADD COLUMN manual_grading_user bigint;
+ALTER TABLE instance_questions ADD COLUMN IF NOT EXISTS manual_grading_user bigint;
+ALTER TABLE instance_questions ADD COLUMN IF NOT EXISTS manual_grading_conflict boolean DEFAULT FALSE;
