b71d678d65c8803aceed3e376c8af4c66b2fad3e
Andrew Stec <andrewstec@gmail.com>
2021-03-11 09:00:14 -0800

    Splitting migration logic

--- migrations/227_instance_questions__manual_grading_user__add.sql
+++ migrations/227_instance_questions__manual_grading_user__add.sql
@@ -1,3 +1,2 @@
 ALTER TABLE instance_questions ADD COLUMN IF NOT EXISTS manual_grading_user bigint;
 ALTER TABLE instance_questions ADD COLUMN IF NOT EXISTS manual_grading_conflict boolean DEFAULT FALSE;
-ALTER TYPE enum_grading_method ADD VALUE IF NOT EXISTS 'ManualBeta';
---
+++ migrations/228_enum_grading_method__manual_beta__add.sql
@@ -0,0 +1,1 @@
+ALTER TYPE enum_grading_method ADD VALUE IF NOT EXISTS 'ManualBeta';
