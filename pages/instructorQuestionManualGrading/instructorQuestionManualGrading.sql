7df49c2383c2b720a5b1d7c3dd7a4200549381d6
Andrew Stec <andrewstec@gmail.com>
2021-03-11 12:59:15 -0800

    Should only remove grading user if ungraded on abort

--- pages/instructorQuestionManualGrading/instructorQuestionManualGrading.sql
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGrading.sql
@@ -1,5 +1,7 @@
 -- BLOCK instance_question_abort_manual_grading
 UPDATE instance_questions
     SET manual_grading_user = NULL
-    WHERE id = $instance_question_id
+    WHERE 
+        id = $instance_question_id
+        AND graded_at IS NULL
 RETURNING *;
