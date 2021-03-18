2c18caa026d5e70383a79726f8c97963e83e9030
Mariana <mfsilva@illinois.edu>
2021-03-17 19:02:21 -0500

    updated migration

--- migrations/231_assessments__assessment_topic__add.sql
+++ migrations/231_assessments__assessment_topic__add.sql
@@ -1,3 +1,4 @@
 ALTER TABLE assessments ADD COLUMN assessment_topic_id BIGINT;
+ALTER TABLE assessments ADD FOREIGN KEY (assessment_topic_id) REFERENCES assessment_topics(id) ON UPDATE CASCADE ON DELETE SET NULL;
 
 ALTER TABLE topics ADD COLUMN heading TEXT;
