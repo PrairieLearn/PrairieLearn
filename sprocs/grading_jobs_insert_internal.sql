c0da79b5149aa3012d6e009c2b60687624e13dfd
bevansm <bevansm@users.noreply.github.com>
2021-03-23 11:32:01 -0700

    broken internal insert proc

--- sprocs/grading_jobs_insert_internal.sql
+++ sprocs/grading_jobs_insert_internal.sql
@@ -90,12 +90,12 @@ BEGIN
     INSERT INTO grading_jobs AS gj
         (submission_id,     score,     v2_score, correct,     feedback,
             partial_scores, auth_user_id,  
-            grading_method_internal, grading_method_external, grading_method_manual,
+            'Internal',
             gradable)
     VALUES
         (submission_id, new_score, new_v2_score, new_correct, new_feedback,
         new_partial_scores, authn_user_id, 
-        grading_method_internal, grading_method_external, grading_method_manual,
+        grading_method,
         new_gradable)
     RETURNING gj.*
     INTO grading_job;
