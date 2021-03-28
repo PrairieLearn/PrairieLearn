0676ceb0651bcfc4fdbd963e91480d00c5e0953b
Matthew West <mwest@illinois.edu>
2021-03-28 09:51:41 -0500

    Add builds for all images in CI

--- tools/build-plbase-if-needed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if git diff --exit-code remotes/origin/master...HEAD -- images/plbase; then
-  echo "prairielearn/plbase files not modified; no rebuild required"
-else
-  echo "prairielearn/plbase requires a rebuild"
-  docker build ./images/plbase -t prairielearn/plbase
-fi
