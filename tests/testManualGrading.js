817b04de45a9ec432683e664d3f73688e39bb6e9
Andrew Stec <andrewstec@gmail.com>
2021-03-30 11:02:23 -0700

    Submissions can be seen by instructor in tests

--- tests/testManualGrading.js
+++ tests/testManualGrading.js
@@ -18,12 +18,19 @@ const hm1ManualGradingUrl = baseUrl + '/course_instance/1/';
 
 let storedConfig = null;
 let ciBody = null;
-
+let iciBody = null;
 let hm1Body = null;
+let manualGradingBody = null;
+
 let hm1AutomaticTestSuiteUrl = null;
 let hm1AddTwoNumbersUrl = null;
 let hm1AddVectorsCartesianUrl = null;
 let hm1AdvantagesFossilFuelsUrl = null;
+
+let manualGradingUrl = null;
+let manualHm1AddTwoNumbersUrl = null;
+let manualHm1AddVectorsCartesianUrl = null;
+let manualHm1AdvantagesFossilFuelsUrl = null;
 
 const mockStudents = [
     {authUid: 'student1', authName: 'Student User 1', authUin: '00000001'},
@@ -32,23 +39,29 @@ const mockStudents = [
 ];
 
 const setInstructor = () => {
-    config.authUid = storedConfig.authUid;
-    config.authName = storedConfig.authName;
-    config.authUin = storedConfig.authUin;
+    if (storedConfig) {
+        config.authUid = storedConfig.authUid;
+        config.authName = storedConfig.authName;
+        config.authUin = storedConfig.authUin;
+    }
 };
 
 const setStudent = (student) => {
-    if(!storedConfig) {
-        storedConfig = {authUid: config.authUid, authName: config.authName, authUin: config.authUin};
+    if (!storedConfig) {
+        storedConfig = {
+            authUid: config.authUid,
+            authName: config.authName, 
+            authUin: config.authUin,
+        };
     }
     config.authUid = student.authUid;
     config.authName = student.authName;
     config.authUin = student.authUin;
 };
 
-const getQuestionRow = ($, question, className) => {
-    const rows = $('#instance-question-grading-table > tbody > tr');
-    const questionRow = rows.filter(() => $(this).find('.question-title-value').text().includes('question'))[0];
+const getQuestionRow = ($page, question, className) => {
+    const rows = $page('#instance-question-grading-table > tbody > tr');
+    const questionRow = rows.filter(() => $page(this).find('.question-title-value').text().includes('question'))[0];
     if (!questionRow) {
         throw new Error('Question row not found');
     }
@@ -106,15 +119,15 @@ describe('Manual grading', function() {
     describe('Students should find hw1 assessments in QA 101, Sp15 course instance', () => {
         it('should have access to the assessments page', async () => {
             const res = await fetch(courseInstanceUrl);
-            assert(res.ok);
+            assert.equal(res.ok, true);
 
             ciBody = await res.text();
             assert.isString(ciBody);
             assert.include(ciBody, 'Homework for automatic test suite');
         });
         it('should find all hw1 "Homework for automatic test suite" testing links', async () => {
-            const $ciBody = cheerio.load(ciBody);
-            hm1AutomaticTestSuiteUrl = siteUrl + $ciBody('#content > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > a[href]').attr('href');
+            const $ciPage = cheerio.load(ciBody);
+            hm1AutomaticTestSuiteUrl = siteUrl + $ciPage('a:contains("Homework for automatic test suite")').attr('href');
             
             const res = await fetch(hm1AutomaticTestSuiteUrl);
             assert(res.ok);
@@ -170,10 +183,32 @@ describe('Manual grading', function() {
             const context = await sqldb.queryAsync(sql.get_all_submissions, []);
             assert.equal(context.rowCount, 9);
         });
-        it('Instructors should be able to see saved student submissions for manual grading', () => {
+        it('Instructors should be able to see the 9 ungraded student submissions for manual grading', async () => {
             setInstructor();
-            
+            iciBody = await (await fetch(instructorCourseInstanceUrl)).text();
+            assert.isString(iciBody);
 
+            manualGradingUrl = siteUrl + cheerio.load(iciBody)('a:contains("Homework for automatic test suite")').attr('href') + 'manual_grading';
+            manualGradingBody = await (await fetch(manualGradingUrl)).text();
+            const $manualGradingPage = cheerio.load(manualGradingBody);
+
+            // get manual grading table row for each question
+            const $addNumbersRow = cheerio.load(
+                $manualGradingPage('.qid-value:contains("addNumbers")').parent().html(),
+            );
+            const $addVectorsRow = cheerio.load(
+                $manualGradingPage('.qid-value:contains("addVectors")').parent().html(),
+            );
+            const $fossilFuelsRow = cheerio.load(
+                $manualGradingPage('.qid-value:contains("fossilFuelsRadio")').parent().html(),
+            );
+            console.log('ungradedAddNumbersRow', $addNumbersRow.html());
+            assert.equal($addNumbersRow('.ungraded-value').text(), 3);
+            assert.equal($addVectorsRow('.ungraded-value').text(), 3);
+            assert.equal($fossilFuelsRow('.ungraded-value').text(), 3);
+            assert.equal($addNumbersRow('.graded-value').text(), 0);
+            assert.equal($addVectorsRow('.graded-value').text(), 0);
+            assert.equal($fossilFuelsRow('.graded-value').text(), 0);
         });
     });
 });
