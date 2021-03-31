e2e5ddcd27dcdba3f25ab205ee4f6f3be6c1ad39
Jonatan Schroeder <jonatan@cs.ubc.ca>
2021-03-31 13:37:22 -0700

    Replace filtering via row_number with SELECT DISTINCT ON

--- pages/instructorAssessmentDownloads/instructorAssessmentDownloads.sql
+++ pages/instructorAssessmentDownloads/instructorAssessmentDownloads.sql
@@ -1,36 +1,46 @@
 -- BLOCK select_assessment_instances
-SELECT
-    (aset.name || ' ' || a.number) AS assessment_label,
-    u.user_id, u.uid, u.uin, u.name, coalesce(e.role, 'None'::enum_role) AS role,
-    substring(u.uid from '^[^@]+') AS username,
-    ai.score_perc, ai.points, ai.max_points,
-    ai.number,ai.id AS assessment_instance_id,ai.open,
-    CASE
-        WHEN ai.open AND ai.date_limit IS NOT NULL
-            THEN greatest(0, floor(extract(epoch from (ai.date_limit - current_timestamp)) / (60 * 1000)))::text || ' min'
-        WHEN ai.open THEN 'Open'
-        ELSE 'Closed'
-    END AS time_remaining,
-    format_date_iso8601(ai.date, ci.display_timezone) AS date_formatted,
-    format_interval(ai.duration) AS duration,
-    EXTRACT(EPOCH FROM ai.duration) AS duration_secs,
-    EXTRACT(EPOCH FROM ai.duration) / 60 AS duration_mins,
-    (row_number() OVER (PARTITION BY u.user_id ORDER BY score_perc DESC, ai.number DESC, ai.id DESC)) = 1 AS highest_score,
-    g.name AS group_name,
-    groups_uid_list(g.id) AS uid_list
-FROM
-    assessments AS a
-    JOIN course_instances AS ci ON (ci.id = a.course_instance_id)
-    JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
-    JOIN assessment_instances AS ai ON (ai.assessment_id = a.id)
-    LEFT JOIN group_configs AS gc ON (gc.assessment_id = a.id)
-    LEFT JOIN groups AS g ON (g.id = ai.group_id AND g.group_config_id = gc.id)
-    LEFT JOIN users AS u ON (u.user_id = ai.user_id)
-    LEFT JOIN enrollments AS e ON (e.user_id = u.user_id AND e.course_instance_id = a.course_instance_id)
-WHERE
-    a.id = $assessment_id
+WITH filtered_assessment_instances AS (
+    SELECT DISTINCT ON (CASE WHEN $group_work THEN ai.group_id ELSE u.user_id END,
+                        CASE WHEN $highest_score THEN NULL ELSE ai.id END)
+        (aset.name || ' ' || a.number) AS assessment_label,
+        u.user_id, u.uid, u.uin, u.name, coalesce(e.role, 'None'::enum_role) AS role,
+        substring(u.uid from '^[^@]+') AS username,
+        ai.score_perc, ai.points, ai.max_points,
+        ai.number,ai.id AS assessment_instance_id,ai.open,
+        CASE
+            WHEN ai.open AND ai.date_limit IS NOT NULL
+                THEN greatest(0, floor(extract(epoch from (ai.date_limit - current_timestamp)) / (60 * 1000)))::text || ' min'
+            WHEN ai.open THEN 'Open'
+            ELSE 'Closed'
+        END AS time_remaining,
+        format_date_iso8601(ai.date, ci.display_timezone) AS date_formatted,
+        format_interval(ai.duration) AS duration,
+        EXTRACT(EPOCH FROM ai.duration) AS duration_secs,
+        EXTRACT(EPOCH FROM ai.duration) / 60 AS duration_mins,
+        g.name AS group_name,
+        groups_uid_list(g.id) AS uid_list
+    FROM
+        assessments AS a
+        JOIN course_instances AS ci ON (ci.id = a.course_instance_id)
+        JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
+        JOIN assessment_instances AS ai ON (ai.assessment_id = a.id)
+        LEFT JOIN group_configs AS gc ON (gc.assessment_id = a.id)
+        LEFT JOIN groups AS g ON (g.id = ai.group_id AND g.group_config_id = gc.id)
+        LEFT JOIN group_users AS gu ON (gu.group_id = g.id)
+        JOIN users AS u ON (u.user_id = ai.user_id OR u.user_id = gu.user_id)
+        LEFT JOIN enrollments AS e ON (e.user_id = u.user_id AND e.course_instance_id = a.course_instance_id)
+    WHERE
+        a.id = $assessment_id
+    ORDER BY
+        CASE WHEN $group_work THEN ai.group_id ELSE u.user_id END,
+        CASE WHEN $highest_score THEN NULL ELSE ai.id END,
+        ai.score_perc DESC,
+        ai.number DESC
+)
+SELECT *
+FROM filtered_assessment_instances
 ORDER BY
-    e.role DESC, u.uid, group_name, u.uin, u.user_id, ai.number, ai.id;
+    role DESC, uid, group_name, uin, user_id, number, assessment_instance_id;
 
 
 -- BLOCK select_instance_questions
