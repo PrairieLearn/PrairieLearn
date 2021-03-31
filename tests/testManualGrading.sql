91017e1e5809e66b44bb35a8235178124e39d90c
Andrew Stec <andrewstec@gmail.com>
2021-03-31 11:39:21 -0700

    Removing unused query

--- tests/testManualGrading.sql
+++ tests/testManualGrading.sql
@@ -1,9 +1,6 @@
 -- BLOCK get_all_submissions
 SELECT * FROM submissions;
 
--- BLOCK remove_all_submissions
-DELETE FROM submissions;
-
 -- BLOCK get_instance_question
 SELECT *
 FROM instance_questions
