e78b28dc91320034f75061de0471c55ec537fb65
Andrew Stec <andrewstec@gmail.com>
2021-04-01 06:47:59 -0700

    Updating modified_at to fix spin-lock view issue

--- sprocs/instance_questions_manually_grade.sql
+++ sprocs/instance_questions_manually_grade.sql
@@ -23,11 +23,14 @@ BEGIN
     UPDATE instance_questions
     SET
         points = arg_manual_grade_score * aq.max_points,
-        score_perc = arg_manual_grade_score * 100
+        score_perc = arg_manual_grade_score * 100,
         -- TO DO: Integrate with update in grading (points_in_grading) logic once booleans merged and logic clarified
         -- status = 'grading',
         -- points_in_grading = arg_manual_grade_score * aq.max_points,
         -- score_perc_in_grading = arg_manual_grade_score * 100
+        modified_at = now()
+        -- Note: Other grade updates may not be updating modified_at, which may become problematic
+        -- when other processes are grading simataneously and manual grading is taking place.
     FROM
         instance_questions AS iq
         JOIN assessment_questions AS aq ON (aq.id = iq.assessment_question_id)
