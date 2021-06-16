3e4263e2f79cee4c75c303607195aed850f8cf09
David Gene <david@astrosticks.com>
2021-06-16 13:55:35 -0500

    WIP add units sync file

---
+++ sync/fromDisk/units.js
@@ -0,0 +1,7 @@
+// @ts-check
+const sqldb = require('@prairielearn/prairielib/sql-db');
+const infofile = require('../infofile');
+
+module.exports.sync = async function(courseId, courseData, assessmentIds) {
+    
+}
