6804f2d5f18bcfa4dc38b18ca44c737a8067bd5e
Kieran Weaver <weaver2@student.ubc.ca>
2021-06-26 19:12:46 -0700

    Clean up update sprocs

--- sprocs/assessment_instances_update_score_perc.sql
+++ sprocs/assessment_instances_update_score_perc.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE FUNCTION
         IN new_score_perc double precision,
         IN authn_user_id bigint,
         IN assessment_id bigint,
-        IN userid varchar,
+        IN user_uid varchar,
         IN date timestamptz,
         IN score_only boolean
     ) RETURNS void
@@ -14,32 +14,33 @@ AS $$
 DECLARE
     max_points double precision;
     new_points double precision;
-    new_instance_id bigint := assessment_instance_id;
-    tmp_user_id bigint;
-    tmp_group_work boolean;
-    tmp_mode enum_mode;
-    time_limit_min integer := NULL;
+    instance_id bigint := assessment_instance_id;
+    user_id bigint;
+    group_work boolean;
+    mode enum_mode;
+    time_limit_min integer := 0;
 BEGIN
     SELECT ai.max_points INTO max_points FROM assessment_instances AS ai WHERE ai.id = assessment_instance_id;
 
     IF NOT FOUND THEN
 		IF score_only THEN
-			SELECT user_id INTO tmp_user_id FROM users WHERE users.uid = userid;
-			IF NOT FOUND THEN RAISE EXCEPTION 'no such user: %', userid; END IF;
+			SELECT u.user_id INTO user_id FROM users AS u WHERE u.uid = user_uid;
 
 			SELECT 
+				a.group_work,
+				a.mode
+			INTO
 				group_work,
 				mode
-			INTO
-				tmp_group_work,
-				tmp_mode
 			FROM assessments AS a WHERE a.id = assessment_id;
-
-			IF NOT FOUND THEN RAISE EXCEPTION 'no such assessment: %', assessment_id; END IF;
 			
-			SELECT tmp.assessment_instance_id INTO new_instance_id FROM assessment_instances_insert(assessment_id, tmp_user_id, tmp_group_work, authn_user_id, tmp_mode, time_limit_min, date) AS tmp;
+			SELECT
+				tmp.assessment_instance_id
+			INTO
+				instance_id
+			FROM assessment_instances_insert(assessment_id, user_id, group_work, authn_user_id, mode, time_limit_min, date) AS tmp;
 
-			SELECT ai.max_points INTO max_points FROM assessment_instances AS ai WHERE ai.id = new_instance_id;
+			SELECT ai.max_points INTO max_points FROM assessment_instances AS ai WHERE ai.id = instance_id;
 		ELSE
 			RAISE EXCEPTION 'no such assessment_instance_id: %', assessment_instance_id;
 		END IF;
@@ -55,7 +56,7 @@ BEGIN
             score_perc = new_score_perc,
             score_perc_in_grading = 0,
             modified_at = now()
-        WHERE ai.id = new_instance_id
+        WHERE ai.id = instance_id
         RETURNING ai.*
     )
     INSERT INTO assessment_score_logs
