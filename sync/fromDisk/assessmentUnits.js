44799912b4c6e282cc6b76a91e60aee891f5f01d
David Gene <david@astrosticks.com>
2021-06-28 10:16:07 -0500

    WIP unit sync js

---
+++ sync/fromDisk/assessmentUnits.js
@@ -0,0 +1,51 @@
+// @ts-check
+const sqldb = require('@prairielearn/prairielib/sql-db');
+const infofile = require('../infofile');
+
+const perf = require('../performance')('assessmentUnits');
+
+/**
+ * @param {any} courseId
+ * @param {import('../course-db').CourseData} courseData
+ */
+module.exports.sync = async function(courseId, courseData) {
+    // We can only safely remove unused assessment units if both `infoCourse.json`
+    // and all `infoAssessment.json` files are valid.
+    const isInfoCourseValid = !infofile.hasErrors(courseData.course);
+    const areAllInfoAssessmentsValid = Object.values(courseData.courseInstances).every(ci => {
+        return Object.values(ci.assessments).every(a => !infofile.hasErrors(a));
+    });
+    const deleteUnused = isInfoCourseValid && areAllInfoAssessmentsValid;
+
+    /** @type {string[]} */
+    let courseAssessmentUnits = [];
+    if (!infofile.hasErrors(courseData.course)) {
+        courseAssessmentUnits = courseData.course.data.assessmentUnits.map(u => JSON.stringify([
+            u.name,
+            u.heading,
+        ]));
+    }
+
+    /** @type Set<string> */
+    const knownAssessmentUnitNames = new Set();
+    Object.values(courseData.courseInstances).forEach(ci => {
+        Object.values(ci.assessments).forEach(a => {
+            if (!infofile.hasErrors(a)) {
+                knownAssessmentUnitNames.add(a.data.unit);
+            }
+        });
+    });
+    const assessmentUnitNames = [...knownAssessmentUnitNames];
+
+    const params = [
+        isInfoCourseValid,
+        deleteUnused,
+        courseAssessmentUnits,
+        assessmentUnitNames,
+        courseId,
+    ];
+
+    perf.start('sproc:sync_assessment_units');
+    await sqldb.callOneRowAsync('sync_assessment_units', params);
+    perf.end('sproc:sync_assessment_units');
+}
--- sync/fromDisk/units.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-const sqldb = require('@prairielearn/prairielib/sql-db');
-const infofile = require('../infofile');
-
-module.exports.sync = async function(courseId, courseData, assessmentIds) {
-    
-}
