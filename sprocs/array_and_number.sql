c697be2b15c6fbec13a42e379ae309ff1a01ae1c
Matthew West <mwest@illinois.edu>
2021-06-29 14:31:21 -0500

    Fix sprocs/array_and_number.sql to not check EXISTS

--- sprocs/array_and_number.sql
+++ sprocs/array_and_number.sql
@@ -1,7 +1,6 @@
---create types
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'array_and_number') THEN
-        CREATE TYPE array_and_number AS (arr DOUBLE PRECISION[], number INTEGER);
-    END IF;
-END$$;
+-- We do not test whether the type already exists.
+-- This is because we will be creating it within the current default schema, which will be empty.
+-- We could test whether the type exists in any schema by querying pg_type like in migrations/000_initial_state.sql.
+-- But that would test for the type in any schema, and we only care about schema on the search path.
+-- It's hard to restrict the "does this type exist" query to just the current search path.
+CREATE TYPE array_and_number AS (arr DOUBLE PRECISION[], number INTEGER);
