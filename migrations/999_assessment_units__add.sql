48eb44e403a9b177fca5b3a1c369337cef4d5eda
David Gene <david@astrosticks.com>
2021-07-10 12:02:09 -0500

    WIP assessment unit migration

--- migrations/999_assessment_units__add.sql
+++ migrations/999_assessment_units__add.sql
@@ -1,10 +1,12 @@
 CREATE TABLE IF NOT EXISTS assessment_units (
-    course_id BIGINT NOT NULL REFERENCES pl_courses(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    name TEXT NOT NULL,
-    heading TEXT,
-    number INTEGER,
-    PRIMARY KEY (course_id, name)
+    id bigserial PRIMARY KEY,
+    course_instance_id BIGINT NOT NULL REFERENCES course_instances(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    name TEXT NOT NULL DEFAULT 'Default',
+    heading TEXT DEFAULT 'Default unit',
+    number INTEGER
 );
+
+ALTER TABLE assessments ADD COLUMN unit_id BIGINT REFERENCES assessment_units(id);
 
 CREATE TYPE assessment_grouping AS ENUM ('set', 'unit');
 ALTER TABLE pl_courses ADD COLUMN assessments_group_by assessment_grouping DEFAULT 'set' NOT NULL;
