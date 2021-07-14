d564975de586ca09a15f1d3e4930cdf627fbbe4b
David Gene <david@astrosticks.com>
2021-07-13 22:21:04 -0500

    enum naming consistency, update pg files

--- migrations/999_assessment_units__add.sql
+++ migrations/999_assessment_units__add.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS assessment_units (
     id bigserial PRIMARY KEY,
-    course_id BIGINT NOT NULL REFERENCES pl_courses(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    course_id BIGINT NOT NULL REFERENCES pl_courses(id) ON UPDATE CASCADE ON DELETE CASCADE,
     name TEXT NOT NULL DEFAULT 'Default',
     heading TEXT DEFAULT 'Default unit',
     number INTEGER,
     UNIQUE(course_id, name)
 );
 
-ALTER TABLE assessments ADD COLUMN assessment_unit_id BIGINT REFERENCES assessment_units(id);
+ALTER TABLE assessments ADD COLUMN assessment_unit_id BIGINT REFERENCES assessment_units(id) ON UPDATE CASCADE ON DELETE SET NULL;
 
-CREATE TYPE assessment_grouping AS ENUM ('set', 'unit');
-ALTER TABLE pl_courses ADD COLUMN assessments_group_by assessment_grouping DEFAULT 'set' NOT NULL;
+CREATE TYPE enum_assessment_grouping AS ENUM ('set', 'unit');
+ALTER TABLE pl_courses ADD COLUMN assessments_group_by enum_assessment_grouping DEFAULT 'set' NOT NULL;
