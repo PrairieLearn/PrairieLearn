37de580e18b6fc770b0bc6aa4515ced7a8b318ef
Matthew West <matt@prairielearn.com>
2021-07-19 08:59:46 -0500

    Update @prairielearn/prairielib to direct path

--- tests/testSequentialQuestions.js
+++ tests/testSequentialQuestions.js
@@ -3,8 +3,8 @@ const assert = require('chai').assert;
 const { step } = require('mocha-steps');
 
 const config = require('../lib/config');
-const sqldb = require('@prairielearn/prairielib/sql-db');
-const sqlLoader = require('@prairielearn/prairielib/sql-loader');
+const sqldb = require('../prairielib/sql-db');
+const sqlLoader = require('../prairielib/sql-loader');
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
 const helperServer = require('./helperServer');
