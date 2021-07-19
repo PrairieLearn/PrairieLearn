e4eda54dae5b3b26e53f99682bd12817fa454b99
echau01 <echau01@students.cs.ubc.ca>
2021-07-19 11:18:17 -0700

    Update prairielib DB requires

--- tests/testSubmittable.js
+++ tests/testSubmittable.js
@@ -2,8 +2,8 @@ const util = require('util');
 const config = require('../lib/config');
 const assert = require('chai').assert;
 
-const sqldb = require('@prairielearn/prairielib/sql-db');
-const sqlLoader = require('@prairielearn/prairielib/sql-loader');
+const sqldb = require('../prairielib/lib/sql-db');
+const sqlLoader = require('../prairielib/lib/sql-loader');
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
 const helperServer = require('./helperServer');
