a91f54d7b556a9ee0ade3b1c47cc12ac7a8b5ab9
Matthew West <matt@prairielearn.com>
2021-07-19 19:01:36 -0500

    Change migration to use IF NOT EXISTS

--- migrations/233_force_sequential_questions__add.sql
+++ migrations/233_force_sequential_questions__add.sql
@@ -1,6 +1,6 @@
-ALTER TABLE alternative_groups ADD COLUMN advance_score_perc DOUBLE PRECISION;
-ALTER TABLE assessments ADD COLUMN advance_score_perc DOUBLE PRECISION;
-ALTER TABLE zones ADD COLUMN advance_score_perc DOUBLE PRECISION;
+ALTER TABLE alternative_groups ADD COLUMN IF NOT EXISTS advance_score_perc DOUBLE PRECISION;
+ALTER TABLE assessments ADD COLUMN IF NOT EXISTS advance_score_perc DOUBLE PRECISION;
+ALTER TABLE zones ADD COLUMN IF NOT EXISTS advance_score_perc DOUBLE PRECISION;
 
-ALTER TABLE assessment_questions ADD COLUMN advance_score_perc DOUBLE PRECISION;
-ALTER TABLE assessment_questions ADD COLUMN effective_advance_score_perc DOUBLE PRECISION;
+ALTER TABLE assessment_questions ADD COLUMN IF NOT EXISTS advance_score_perc DOUBLE PRECISION;
+ALTER TABLE assessment_questions ADD COLUMN IF NOT EXISTS effective_advance_score_perc DOUBLE PRECISION;
