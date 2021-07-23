a26c7552cc335917efbcb0233e265fe71fd94c6b
Eugene Chau <echau01@students.cs.ubc.ca>
2021-07-23 07:39:33 -0700

    Add "active" access restriction (#3993)
    * Implement viewOnly access rule
    
    * Rename "viewOnly" to "submittable", and invert all old "viewOnly" logic
    
    * Remove unneeded start_date and end_date computation
    
    * Wording improvements
    
    * Intermediate commit: seeing if I am doing unit tests correctly
    
    * Make tests run (tests did not run in previous commit)
    
    * Fix typo
    
    * Fix failing test
    
    * Add testShowClosedAssessment to tests/index.js
    
    * Another attempt at fixing a unit test
    
    * Implement first draft of unit tests
    
    * Diagnosing cause of test failure
    
    * Another diagnosis attempt
    
    * Diagnosis attempt 3
    
    * Diagnosis attempt 4
    
    * Diagnosis attempt 5
    
    * Perhaps the issue is with timezones -- diagnosis attempt 6
    
    * Hopefully it works now
    
    * Seeing if my tests in particular are passing
    
    * Try to fix failing test
    
    * This should fix the failing test
    
    * Another attempt at fixing tests
    
    * Yet another attempt
    
    * Add unit tests for homeworks with submittable rule
    
    * Attempt at fixing unit test
    
    * Debugging
    
    * Debugging 2
    
    * Fix unit test
    
    * Fix typo; fix unit test
    
    * Uncomment all other tests
    
    * Show credit available in next submittable period if assessment will become submittable
    
    * Remove unnecessary ORDER BY key
    
    * Display next time that an assessment can be submitted if student clicks into a non-submittable assessment
    
    * Comment out all tests except submittable tests
    
    * Change endDate so that unit test passes
    
    * Uncomment all tests
    
    * Add unit test for when assessment will never be submittable again
    
    * Fix unit test
    
    * Uncomment all tests
    
    * Add documentation
    
    * Change startDate in test assessment
    
    * Commit the correct file this time
    
    * Add tests for edge cases; comment out all tests other than submittable tests
    
    * Add missing semicolons; rewrote a comment
    
    * Fix SQL query execution; increase timeout length
    
    * Fix status codes in unit tests
    
    * Remove erroneous extractAndSaveVariantId calls
    
    * Uncomment all tests
    
    * Update documentation, guard against a corner case
    
    * Add test for corner case handled in previous commit
    
    * Set submittable to false if no access rules are satisfied
    
    * Make a lot of suggested changes
    
    * Fix indentation
    
    * Fix unit test
    
    * Fix unit test; convert tabs to spaces
    
    * Fix unit test again
    
    * Implement links to assessments correctly; improve detection of nonzero credit in non-submittable access rule
    
    * Uncomment all tests
    
    * Improve clarity in unit test
    
    * Make SQL query more efficient
    
    * Add missing semicolon
    
    * Make suggested changes to documentation
    
    * Renumber migration
    
    * Add migration file
    
    * Increment test homework number
    
    * Formatting
    
    * Change "CRSF" to "CSRF"
    
    * Update prairielib DB requires
    
    * Add example assessment that uses submittable rule
    
    * Change "Access Rule" to "Access Restriction"
    
    * News item
    
    * Improve documentation and fix infoAssessment.json
    
    * Rename submittable to active
    
    * Wording changes
    
    * Rewrite news item to separate before and after cases
    
    Co-authored-by: Jonatan Schroeder <jonatan@cs.ubc.ca>
    Co-authored-by: Matthew West <matt@prairielearn.com>

--- sprocs/authz_assessment_instance.sql
+++ sprocs/authz_assessment_instance.sql
@@ -16,6 +16,8 @@ CREATE FUNCTION
         OUT seb_config JSONB,
         OUT show_closed_assessment boolean, -- If students can view the assessment after it is closed.
         OUT show_closed_assessment_score boolean, -- If students can view their grade after the assessment is closed
+        OUT active boolean,         -- If the assessment is visible but not active
+        OUT next_active_time text,  -- The next time the assessment becomes active. This is non-null only if the assessment is not currently active but will be later.
         OUT access_rules JSONB       -- For display to the user. The currently active rule is marked by 'active' = TRUE.
     )
 AS $$
@@ -42,6 +44,8 @@ BEGIN
     seb_config := assessment_result.seb_config;
     show_closed_assessment := assessment_result.show_closed_assessment;
     show_closed_assessment_score := assessment_result.show_closed_assessment_score;
+    active := assessment_result.active;
+    next_active_time := assessment_result.next_active_time;
 
     time_limit_expired := FALSE;
     IF assessment_instance.date_limit IS NOT NULL AND assessment_instance.date_limit < req_date THEN
