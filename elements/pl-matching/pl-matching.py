7fdd5f25e682977f13620763023bffb81075671d
Neal Terrell <neal.terrell@csulb.edu>
2021-08-09 11:38:52 -0700

    Rename "pl-answer" to "pl-statement". Rework random selection of statements and options.

--- elements/pl-matching/pl-matching.py
+++ elements/pl-matching/pl-matching.py
@@ -3,9 +3,11 @@ import lxml.html
 import random
 import math
 import chevron
+import itertools
 
 WEIGHT_DEFAULT = 1
-FIXED_ANSWER_ORDER_DEFAULT = False
+FIXED_STATEMENTS_ORDER_DEFAULT = False
+FIXED_OPTIONS_ORDER_DEFAULT = False
 INLINE_DEFAULT = False
 PARTIAL_CREDIT_DEFAULT = True
 HIDE_ANSWER_PANEL_DEFAULT = False
@@ -23,12 +25,23 @@ def get_form_name(answers_name, index):
     return f'{answers_name}-dropdown-{index}'
 
 
+def alpha_counter(i, letter_a):
+    """Converts a base-10 integer to base 26, using letter_a as the "0" digit."""
+    result = ""
+    while i > 0:
+        i -= 1
+        result = chr(ord(letter_a) + i % 26) + result
+        i //= 26
+
+    return result
+
+
 def get_counter(i, counter_type):
     """Converts an integer counter to the specified CSS counter type"""
     if counter_type == 'lower-alpha':
-        return chr(ord('a') + i - 1)
+        return alpha_counter(i, 'a')
     elif counter_type == 'upper-alpha':
-        return chr(ord('A') + i - 1)
+        return alpha_counter(i, 'A')
     elif counter_type == 'decimal':
         return str(i)
     else:
@@ -52,6 +65,17 @@ def get_select_options(options_list, selected_value):
         'value': opt,
         'selected': 'selected' if opt == selected_value else ''
     } for opt in options_list]
+
+
+def partition(data, pred):
+    """Implements a partition function, splitting the data into two lists based on the predicate."""
+    yes, no = [], []
+    for d in data:
+        if pred(d):
+            yes.append(d)
+        else:
+            no.append(d)
+    return (yes, no)
 
 
 def categorize_matches(element, data):
@@ -97,38 +121,63 @@ def prepare(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
 
     required_attribs = ['answers-name']
-    optional_attribs = ['fixed-order', 'number-answers', 'number-options', 'none-of-the-above', 'blank', 'counter-type']
+    optional_attribs = ['fixed-statements-order', 'number-statements', 'number-options', 'none-of-the-above', 'blank', 'counter-type']
     pl.check_attribs(element, required_attribs, optional_attribs)
     name = pl.get_string_attrib(element, 'answers-name')
-    options, answers = categorize_matches(element, data)
+    options, statements = categorize_matches(element, data)
 
-    # Choose and randomize the options and answers.
-    # Options are always randomized; answers can be a fixed order.
-    fixed_answer_order = pl.get_boolean_attrib(element, 'fixed-answer-order', FIXED_ANSWER_ORDER_DEFAULT)
-    number_answers = pl.get_integer_attrib(element, 'number-answers', len(answers))
+    # Choose and randomize the options and statements. Each can be in a fixed order.
+    fixed_statements_order = pl.get_boolean_attrib(element, 'fixed-statements-order', FIXED_STATEMENTS_ORDER_DEFAULT)
+    number_statements = pl.get_integer_attrib(element, 'number-statements', len(statements))
     number_options = pl.get_integer_attrib(element, 'number-options', len(options))
     nota = pl.get_boolean_attrib(element, 'none-of-the-above', NOTA_DEFAULT)
 
-    # Shuffle or sample the options.
-    if number_options == len(options):
-        random.shuffle(options)
+    # Organize the list of statements to use.
+    if fixed_statements_order:
+        if number_statements < len(statements):
+            # Take a random sampling, but maintain the original order of the statements.
+            indices = random.sample(range(len(statements)), number_statements)
+            statements = [statements[i] for i in sorted(indices)]
+        # Otherwise, just use all the statements as-is.
     else:
-        options = random.sample(options, number_options)
-        # If we aren't using all options, there is a chance that one answer will be missing its option,
-        # so we force none-of-the-above.
+        # Shuffle or sample the statements.
+        if number_statements < len(statements):
+            statements = random.sample(statements, number_statements)
+        else:
+            random.shuffle(statements)
+
+    # Organize the list of options to use.
+    # First, select all the options associated with the chosen statements.
+    needed_options_keys = set((s[0] for s in statements))
+    needed_options, distractors = partition(options, lambda opt: opt[1] in needed_options_keys)
+
+    if len(needed_options) < number_options:
+        # The limit is set above the # of options needed to match the chosen statements.
+        # Add distractor options; and None of the Above if needed.
+        more_needed = number_options - len(needed_options)
+        if more_needed >= len(distractors):
+            # Add all distractors.
+            needed_options.extend(distractors)
+            # Add NOTA if that's still not enough.
+            if more_needed > len(distractors):
+                nota = True
+        else:
+            # Add a sample of the distractors.
+            needed_options.extend(random.sample(distractors, more_needed))
+        options = needed_options
+
+    elif len(needed_options) > number_options:
+        # The limit is set below the # of options needed.
+        # Add None of the Above to compensate.
+        options = random.sample(needed_options, number_options)
         nota = True
-    # Then append None of the above if requested.
+    else:
+        # The number of needed options matches the total options.
+        options = needed_options
+        random.shuffle(options)
+
     if nota:
         options.append((len(options), '__nota__', 'None of the above'))
-
-    # Answers are always sampled/shuffled unless a fixed order is required.
-    if number_answers < len(answers):
-        if not fixed_answer_order:
-            answers = random.sample(answers, number_answers)
-        else:
-            answers = answers[:number_answers]  # Does this configuration even make sense?
-    elif not fixed_answer_order:
-        random.shuffle(answers)
 
     # Build the options to display to the student.
     chosen_option_names = []
@@ -138,10 +187,10 @@ def prepare(element_html, data):
         display_options.append(keyed_option)
         chosen_option_names.append(option_name)
 
-    # Build the answers to display to the student.
-    display_answers = []
+    # Build the statements to display to the student.
+    display_statements = []
     correct_matches = []
-    for (i, (match_name, html)) in enumerate(answers):
+    for (i, (match_name, html)) in enumerate(statements):
         # Check if the matched option was removed from the display_options to make room for
         # none-of-the-above option.
         if nota and match_name not in chosen_option_names:
@@ -150,21 +199,21 @@ def prepare(element_html, data):
             match_index = chosen_option_names.index(match_name)
 
         keyed_answer = {'key': str(i), 'html': html, 'match': match_name}
-        display_answers.append(keyed_answer)
+        display_statements.append(keyed_answer)
         correct_matches.append(match_index + 1)
 
-    data['params'][name] = (display_answers, display_options)
+    data['params'][name] = (display_statements, display_options)
     data['correct_answers'][name] = correct_matches
 
 
 def parse(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     name = pl.get_string_attrib(element, 'answers-name')
-    display_answers, display_options = data['params'].get(name)
+    display_statements, display_options = data['params'].get(name)
     submitted_answers = data['submitted_answers']
     counter_type = pl.get_string_attrib(element, 'counter-type', COUNTER_TYPE_DEFAULT)
 
-    for i in range(len(display_answers)):
+    for i in range(len(display_statements)):
         expected_html_name = get_form_name(name, i)
         student_answer = submitted_answers.get(expected_html_name, None)
 
@@ -184,7 +233,7 @@ def parse(element_html, data):
 def render(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     name = pl.get_string_attrib(element, 'answers-name')
-    display_answers, display_options = data['params'].get(name)
+    display_statements, display_options = data['params'].get(name)
     submitted_answers = data['submitted_answers']
     counter_type = pl.get_string_attrib(element, 'counter-type', COUNTER_TYPE_DEFAULT)
     hide_score_badge = pl.get_boolean_attrib(element, 'hide-score-badge', HIDE_SCORE_BADGE_DEFAULT)
@@ -202,35 +251,35 @@ def render(element_html, data):
         score = partial_score.get('score', None)
         display_score_badge = score is not None and show_answer_feedback
 
-        answerset = []
-        for i, answer in enumerate(display_answers):
+        statement_set = []
+        for i, answer in enumerate(display_statements):
             form_name = get_form_name(name, answer['key'])
             student_answer = submitted_answers.get(form_name, None)
             correct_answer = get_counter(data['correct_answers'].get(name)[i], counter_type)
 
-            answer_html = {
+            statement_html = {
                 'html': answer['html'].strip(),
                 'options': get_select_options(dropdown_options, submitted_answers.get(form_name, None)),
                 'name': form_name,
                 'display_score_badge': display_score_badge,
                 'correct': display_score_badge and student_answer == correct_answer
             }
-            answerset.append(answer_html)
+            statement_set.append(statement_html)
 
-        optionset = []
+        option_set = []
         for option in display_options:
             option_html = {
                 'key': option['key'],
                 'html': option['html'].strip()
             }
-            optionset.append(option_html)
+            option_set.append(option_html)
 
         html_params = {
             'question': True,
             'name': name,
             'uuid': pl.get_uuid(),
-            'answers': answerset,
-            'options': optionset,
+            'statements': statement_set,
+            'options': option_set,
             'counter_type': counter_type,
         }
 
@@ -253,15 +302,15 @@ def render(element_html, data):
         if parse_error is None:
             partial_score = data['partial_scores'].get(name, {'score': None})
             score = partial_score.get('score', None)
-            answerset = []
-            for i, answer in enumerate(display_answers):
+            statement_set = []
+            for i, answer in enumerate(display_statements):
                 form_name = get_form_name(name, answer['key'])
                 student_answer = submitted_answers.get(form_name, None)
                 correct_answer = get_counter(data['correct_answers'].get(name)[i], counter_type)
 
                 parse_error = data['format_errors'].get(form_name, None)
                 display_score_badge = parse_error is None and score is not None and show_answer_feedback
-                answer_html = {
+                statement_html = {
                     'html': answer['html'].strip(),
                     'disabled': 'disabled',
                     'options': get_select_options(dropdown_options, submitted_answers.get(form_name, None)),
@@ -269,20 +318,20 @@ def render(element_html, data):
                     'correct': display_score_badge and student_answer == correct_answer,
                     'parse_error': parse_error
                 }
-                answerset.append(answer_html)
+                statement_set.append(statement_html)
 
-            optionset = []
+            option_set = []
             for option in display_options:
                 option_html = {
                     'key': option['key'],
                     'html': option['html'].strip()
                 }
-                optionset.append(option_html)
+                option_set.append(option_html)
 
             html_params = {
                 'submission': True,
-                'answers': answerset,
-                'options': optionset,
+                'statements': statement_set,
+                'options': option_set,
                 'display_score_badge': score is not None,
                 'hide_letter_keys': pl.get_boolean_attrib(element, 'hide-letter-keys', HIDE_LETTER_KEYS_DEFAULT),
                 'counter_type': counter_type,
@@ -306,29 +355,29 @@ def render(element_html, data):
         if not pl.get_boolean_attrib(element, 'hide-answer-panel', HIDE_ANSWER_PANEL_DEFAULT):
             correct_answer_list = data['correct_answers'].get(name, [])
 
-            answerset = []
-            for i, answer in enumerate(display_answers):
+            statement_set = []
+            for i, answer in enumerate(display_statements):
                 form_name = get_form_name(name, answer['key'])
                 correct_answer = correct_answer_list[i]
 
-                answer_html = {
+                statement_html = {
                     'html': answer['html'].strip(),
                     'options': [{'value': get_counter(correct_answer, counter_type)}],
                 }
-                answerset.append(answer_html)
+                statement_set.append(statement_html)
 
-            optionset = []
+            option_set = []
             for option in display_options:
                 option_html = {
                     'key': option['key'],
                     'html': option['html'].strip()
                 }
-                optionset.append(option_html)
+                option_set.append(option_html)
 
             html_params = {
                 'answer': True,
-                'answers': answerset,
-                'options': optionset,
+                'statements': statement_set,
+                'options': option_set,
                 'hide_letter_keys': pl.get_boolean_attrib(element, 'hide-letter-keys', HIDE_LETTER_KEYS_DEFAULT),
                 'counter_type': counter_type,
             }
@@ -344,15 +393,15 @@ def grade(element_html, data):
     weight = pl.get_integer_attrib(element, 'weight', WEIGHT_DEFAULT)
     partial_credit = pl.get_boolean_attrib(element, 'partial-credit', PARTIAL_CREDIT_DEFAULT)
     counter_type = pl.get_string_attrib(element, 'counter-type', COUNTER_TYPE_DEFAULT)
-    display_answers, _ = data['params'][name]
-    number_answers = len(display_answers)
+    display_statements, _ = data['params'][name]
+    number_statements = len(display_statements)
 
     submitted_answers = data['submitted_answers']
     correct_answers = data['correct_answers'].get(name, [])
 
     # Count the number of answers that are correct.
     num_correct = 0
-    for i in range(number_answers):
+    for i in range(number_statements):
         expected_html_name = get_form_name(name, i)
         student_answer = submitted_answers.get(expected_html_name, None)
         correct_answer = get_counter(correct_answers[i], counter_type)
@@ -360,12 +409,12 @@ def grade(element_html, data):
             num_correct += 1
 
     score = 0
-    if not partial_credit and num_correct == number_answers:
+    if not partial_credit and num_correct == number_statements:
         score = 1
     elif partial_credit:
         # EDC grading
-        number_wrong = number_answers - num_correct
-        score = 1 - 1.0 * number_wrong / number_answers
+        number_wrong = number_statements - num_correct
+        score = 1 - 1.0 * number_wrong / number_statements
     data['partial_scores'][name] = {'score': score, 'weight': weight}
 
 
