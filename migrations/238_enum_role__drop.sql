6f35d5818557fa28e54a2ecc1d1e840fb24467f4
Matthew West <matt@prairielearn.com>
2021-08-10 21:51:28 -0500

    Delete access rules with "role > Student" in migration

--- migrations/238_enum_role__drop.sql
+++ migrations/238_enum_role__drop.sql
@@ -1,3 +1,6 @@
+DELETE FROM assessment_access_rules WHERE role > 'Student';
+DELETE FROM course_instance_access_rules WHERE role > 'Student';
+
 ALTER TABLE assessment_access_rules DROP COLUMN IF EXISTS role;
 ALTER TABLE course_instance_access_rules DROP COLUMN IF EXISTS role;
 ALTER TABLE enrollments DROP COLUMN IF EXISTS role;
