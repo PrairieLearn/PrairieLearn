ca77cc3c2ccb0442307cacf94264d1506ef34d70
Jonatan Schroeder <jonatan@cs.ubc.ca>
2021-08-16 16:53:44 -0700

    Unify class directory, restrict user access, create user directory

--- graders/java/autograder.sh
+++ graders/java/autograder.sh
@@ -3,7 +3,7 @@
 STUDENT_FILES=$(find /grade/student -iname '*.java')
 TEST_FILES=$(find /grade/tests/junit -iname '*.java')
 RESULTS_FILE="/grade/results/results.json"
-JAVA_COMPILATION_FLAGS="-Xlint -Xlint:-path -d /grade/student"
+JAVA_COMPILATION_FLAGS="-Xlint -Xlint:-path -d /grade/classpath"
 
 exception() {
     jq -n --arg msg "$1" '{gradable: false, message: $msg}' > $RESULTS_FILE
@@ -11,6 +11,11 @@ exception() {
 }
 
 mkdir -p /grade/results
+mkdir -p /grade/classpath
+
+cp -r /javagrader/*.class /javagrader/org /javagrader/libs/* /grade/classpath
+cp -r /grade/tests/libs/*                                    /grade/classpath 2> /dev/null
+cp -r /grade/serverFilesCourse/java/libs/*                   /grade/classpath 2> /dev/null
 
 STUDENT_COMPILE_OUT=$(javac $JAVA_COMPILATION_FLAGS $STUDENT_FILES 2>&1)
 if [ "$?" -ne 0 ] ; then
@@ -28,11 +33,15 @@ fi
 RESULTS_TEMP_DIR=$(mktemp -d -p /grade/results)
 RESULTS_TEMP_FILE="$RESULTS_TEMP_DIR/$RANDOM.json"
 
+chmod 700 /javagrader
+chmod 711 /grade
+chmod 700 /grade/*
 chmod 711 /grade/results
+chmod -R a+rX /grade/classpath
 chmod 777 $RESULTS_TEMP_DIR
 
-su sbuser <<EOF
-java JUnitAutograder "$RESULTS_TEMP_FILE" "$TEST_FILES" "$STUDENT_COMPILE_OUT"
+su - sbuser <<EOF
+java -cp "/grade/classpath:/grade/classpath/*" JUnitAutograder "$RESULTS_TEMP_FILE" "$TEST_FILES" "$STUDENT_COMPILE_OUT"
 EOF
 
 if [ -f $RESULTS_TEMP_FILE ] ; then
