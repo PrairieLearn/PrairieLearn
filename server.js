0cac70248fb05311669e5e67a1d49a1af6ac5cc7
Andrew Stec <andrewstec@gmail.com>
2021-08-18 12:00:28 -0700

    Returning server instead of app

--- server.js
+++ server.js
@@ -1012,7 +1012,7 @@ module.exports.startServerAsync = async () => {
         throw new Error('unknown serverType: ' + config.serverType);
     }
 
-    return app;
+    return server;
 };
 module.exports.startServer = util.callbackify(module.exports.startServerAsync);
 
