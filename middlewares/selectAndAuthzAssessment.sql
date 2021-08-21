ef84679b3764b21852b7dc1cb7c4d53f220fc388
David Gene <david@astrosticks.com>
2021-08-21 09:44:50 -0400

    Add assessment_unit to assessment settings page

--- middlewares/selectAndAuthzAssessment.sql
+++ middlewares/selectAndAuthzAssessment.sql
@@ -2,11 +2,13 @@
 SELECT
     to_jsonb(a) AS assessment,
     to_jsonb(aset) AS assessment_set,
+    to_jsonb(au) AS assessment_unit,
     to_jsonb(aa) AS authz_result,
     assessment_label(a, aset) AS assessment_label
 FROM
     assessments AS a
     JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
+    JOIN assessment_units AS au ON (au.id = a.assessment_unit_id)
     JOIN course_instances AS ci ON (ci.id = a.course_instance_id)
     JOIN LATERAL authz_assessment(a.id, $authz_data, $req_date, ci.display_timezone) AS aa ON TRUE
 WHERE
