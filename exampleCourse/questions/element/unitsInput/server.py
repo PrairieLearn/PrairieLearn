c73657c1300073ee804aa8963299081ec41f5a66
Cadenze <borisowenli@gmail.com>
2021-09-04 13:28:54 -0700

    feat: demo units question
    added question that demonstrates the capabilities and different grading/comparison methods of the units input element
    
    Co-Authored-By: Morgan Arnold <62062835+CrashAndSideburns@users.noreply.github.com>

---
+++ exampleCourse/questions/element/unitsInput/server.py
@@ -0,0 +1,5 @@
+def generate(data):
+    data['correct_answers']['capacitance'] = "1 F"
+    data['correct_answers']['length1'] = "1 m"
+    data['correct_answers']['length2'] = "1 m"
+    data['correct_answers']['length3'] = "1 m"
