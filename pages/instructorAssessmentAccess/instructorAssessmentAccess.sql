5af8c02d8913c1fcfcc1e35b3942a84d1b5843a2
Matthew West <matt@prairielearn.com>
2021-09-14 23:08:47 -0500

    Fix pt_reservations linking (#4815)
    * Fix pt_reservations linking
    
    * Also add index on assessment_access_rules.exam_uuid for PT lookups
    
    * Don't display examUuid linking error in devMode
    
    * Downgrade examUuid mismatch to warning (was error)
    
    * Upgrade sync error message to red and display warning
    
    * Fix database/ to reflect new index
    
    * Fix port for local ptHost
    
    Co-authored-by: Jonatan Schroeder <jonatan@cs.ubc.ca>
    
    Co-authored-by: Jonatan Schroeder <jonatan@cs.ubc.ca>

--- pages/instructorAssessmentAccess/instructorAssessmentAccess.sql
+++ pages/instructorAssessmentAccess/instructorAssessmentAccess.sql
@@ -34,18 +34,27 @@ SELECT
     END AS password,
     CASE
         WHEN aar.exam_uuid IS NULL THEN '—'
+        WHEN pt_x.id IS NOT NULL THEN '—'
         WHEN e.exam_id IS NULL THEN 'Exam not found: ' || aar.exam_uuid
         WHEN NOT $link_exam_id THEN ps_c.rubric || ': ' || e.exam_string
         ELSE '<a href="https://cbtf.engr.illinois.edu/sched/course/'
             || ps_c.course_id || '/exam/' || e.exam_id || '">'
             || ps_c.rubric || ': ' || e.exam_string || '</a>'
-    END AS exam
+    END AS exam,
+    aar.exam_uuid,
+    e.exam_id AS ps_exam_id,
+    pt_c.id AS pt_course_id,
+    pt_c.name AS pt_course_name,
+    pt_x.id AS pt_exam_id,
+    pt_x.name AS pt_exam_name
 FROM
     assessment_access_rules AS aar
     JOIN assessments AS a ON (a.id = aar.assessment_id)
     JOIN course_instances AS ci ON (ci.id = a.course_instance_id)
     LEFT JOIN exams AS e ON (e.uuid = aar.exam_uuid)
     LEFT JOIN courses AS ps_c ON (ps_c.course_id = e.course_id)
+    LEFT JOIN pt_exams AS pt_x ON (pt_x.uuid = aar.exam_uuid)
+    LEFT JOIN pt_courses AS pt_c ON (pt_c.id = pt_x.course_id)
 WHERE
     a.id = $assessment_id
 ORDER BY
