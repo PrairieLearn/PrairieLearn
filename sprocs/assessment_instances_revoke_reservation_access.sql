7718b46f34cb1e57a9a4ecf27fd4430a43919b74
Jonatan Schroeder <jonatan@cs.ubc.ca>
2021-09-27 13:21:09 -0700

    Ensure each reservation id is only updated once

--- sprocs/assessment_instances_revoke_reservation_access.sql
+++ sprocs/assessment_instances_revoke_reservation_access.sql
@@ -7,19 +7,22 @@ AS $$
 BEGIN
     PERFORM 1
     FROM
-        assessment_instances AS ai
-        JOIN assessment_access_rules AS aar ON (aar.assessment_id = ai.assessment_id AND exam_uuid IS NOT NULL)
-        JOIN pt_exams AS x ON (x.uuid = aar.exam_uuid)
-        LEFT JOIN groups AS g ON (g.id = ai.group_id)
-        LEFT JOIN group_users AS gu ON (gu.group_id = g.id)
-        JOIN users AS u ON (u.user_id = ai.user_id OR u.user_id = gu.user_id)
-        JOIN pt_enrollments AS e ON (e.user_id = u.user_id)
-        JOIN pt_reservations AS r ON (r.enrollment_id = e.id AND r.exam_id = x.id)
-        CROSS JOIN LATERAL reservations_student_end_exam(r.id, arg_authn_user_id)
-    WHERE
-        ai.id = arg_assessment_instance_id
-        AND g.deleted_at IS NULL
-        AND (now() BETWEEN r.access_start AND r.access_end);
+       (SELECT DISTINCT r.id
+        FROM
+            assessment_instances AS ai
+            JOIN assessment_access_rules AS aar ON (aar.assessment_id = ai.assessment_id AND exam_uuid IS NOT NULL)
+            JOIN pt_exams AS x ON (x.uuid = aar.exam_uuid)
+            LEFT JOIN groups AS g ON (g.id = ai.group_id)
+            LEFT JOIN group_users AS gu ON (gu.group_id = g.id)
+            JOIN users AS u ON (u.user_id = ai.user_id OR u.user_id = gu.user_id)
+            JOIN pt_enrollments AS e ON (e.user_id = u.user_id)
+            JOIN pt_reservations AS r ON (r.enrollment_id = e.id AND r.exam_id = x.id)
+        WHERE
+            ai.id = arg_assessment_instance_id
+            AND g.deleted_at IS NULL
+            AND (now() BETWEEN r.access_start AND r.access_end)
+        ) AS res
+        CROSS JOIN LATERAL reservations_student_end_exam(res.id, arg_authn_user_id);
 EXCEPTION
 -- The function reservations_student_end_exam is only expected to exist if PT integration is in place, if it is not then nothing should be done
 WHEN undefined_function THEN RETURN;
