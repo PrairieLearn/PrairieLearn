7c91efb182729ef0e67fe8cd3e80af064fd38dfd
Matthew West <matt@prairielearn.com>
2021-10-05 18:07:40 -0500

    Remove reservation-based exam mode setting

--- sprocs/ip_to_mode.sql
+++ sprocs/ip_to_mode.sql
@@ -18,33 +18,5 @@ BEGIN
     ELSE
         mode := 'Public';
     END IF;
-
-    -- Do we have an active CBTF reservation, if so force mode to 'Exam'
-    PERFORM *
-    FROM reservations AS r
-    WHERE
-        r.user_id = ip_to_mode.authn_user_id
-        AND r.delete_date IS NULL
-        AND r.checked_in IS NOT NULL
-        AND r.access_start IS NOT NULL
-        AND date BETWEEN r.access_start AND r.access_end;
-
-    IF FOUND THEN
-        mode := 'Exam';
-    END IF;
-
-    -- Do we have an active PT reservation? If so, force mode to 'Exam'
-    PERFORM 1
-    FROM
-        pt_reservations AS r
-        JOIN pt_enrollments AS e ON (e.id = r.enrollment_id)
-    WHERE
-        e.user_id = ip_to_mode.authn_user_id
-        AND (date BETWEEN r.access_start AND r.access_end);
-
-    IF FOUND THEN
-        mode := 'Exam';
-    END IF;
-
 END;
 $$ LANGUAGE plpgsql VOLATILE;
