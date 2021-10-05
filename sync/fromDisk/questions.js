d3350401128ee8215e84f458af17b04e95ed4eac
Tim Yang <tdy@users.noreply.github.com>
2021-10-05 12:42:35 -0500

    Apply suggestions from code review
    Co-authored-by: Nathan Walters <nathan@prairielearn.com>

--- sync/fromDisk/questions.js
+++ sync/fromDisk/questions.js
@@ -36,7 +36,7 @@ function getParamsForQuestion(q) {
         external_grading_entrypoint: (q.externalGradingOptions && q.externalGradingOptions.entrypoint),
         external_grading_timeout: (q.externalGradingOptions && q.externalGradingOptions.timeout),
         external_grading_enable_networking: (q.externalGradingOptions && q.externalGradingOptions.enableNetworking),
-        external_grading_environment: (q.externalGradingOptions && q.externalGradingOptions.environment) || {},
+        external_grading_environment: q.externalGradingOptions?.environment ?? {},
         dependencies: q.dependencies || {},
         workspace_image: (q.workspaceOptions && q.workspaceOptions.image),
         workspace_port: (q.workspaceOptions && q.workspaceOptions.port),
@@ -46,7 +46,7 @@ function getParamsForQuestion(q) {
         workspace_sync_ignore: (q.workspaceOptions && q.workspaceOptions.syncIgnore),
         workspace_url_rewrite: (q.workspaceOptions && q.workspaceOptions.rewriteUrl),
         workspace_enable_networking: (q.workspaceOptions && q.workspaceOptions.enableNetworking),
-        workspace_environment: (q.workspaceOptions && q.workspaceOptions.environment) || {},
+        workspace_environment: q.workspaceOptions?.environment ?? {},
     };
 }
 
