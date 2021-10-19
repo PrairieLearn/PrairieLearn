14f832e8a5332ff6b44ff4dcfc5c51f0fa1c18a1
Nathan Walters <nathan@prairielearn.com>
2021-10-19 11:21:35 -0700

    Update configs for Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:import/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
   parserOptions: {
     ecmaVersion: 12,
   },
