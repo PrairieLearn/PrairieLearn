5c63faa933d400fed5da2ca63551e77ab66ab5c4
Nathan Walters <nathan@prairielearn.com>
2021-10-19 12:28:04 -0700

    Move eslint-disable-next-line comments to previous line

--- tests/helperServer.js
+++ tests/helperServer.js
@@ -246,8 +246,8 @@ module.exports.getLastJobSequenceId = util.callbackify(
 
 module.exports.waitForJobSequenceAsync = async (job_sequence_id) => {
   let job_sequence;
+  // eslint-disable-next-line no-constant-condition
   while (true) {
-    // eslint-disable-line no-constant-condition
     const result = await sqldb.queryOneRowAsync(sql.select_job_sequence, {
       job_sequence_id,
     });
