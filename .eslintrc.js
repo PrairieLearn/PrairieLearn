500c2047d5d0b4907f9b709f98b779a3d67b5276
Nathan Walters <nwalters512@gmail.com>
2021-10-20 12:24:50 -0700

    Add Prettier infra

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,20 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:import/recommended'],
+  extends: ['eslint:recommended', 'plugin:import/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    'comma-dangle': ['error', 'always-multiline'],
-    'eqeqeq': ['error', 'smart'],
+    eqeqeq: ['error', 'smart'],
     'handle-callback-err': 'error',
-    'no-tabs': 'error',
     'no-unused-vars': [
       'error',
-      { args: 'after-used', argsIgnorePattern: '^_' },
+      {
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
     ],
-    quotes: [
-      'error',
-      'single',
-      { avoidEscape: true, allowTemplateLiterals: true },
-    ],
-    semi: ['error', 'always'],
 
     // By default, eslint-plugin-import only validates ESM syntax. We're still
     // using CommonJS, so we need to explicitly enable support for that.
