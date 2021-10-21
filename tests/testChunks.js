55592d9ceb071871fa527386d5a5be710bf6db0c
Nathan Walters <nathan@prairielearn.com>
2021-10-20 18:21:00 -0700

    Snapshot work in progress

--- tests/testChunks.js
+++ tests/testChunks.js
@@ -1,7 +1,17 @@
 // @ts-check
 const assert = require('chai').assert;
+const util = require('util');
+const tmp = require('tmp-promise');
+const fs = require('fs-extra');
+const path = require('path');
 
 const chunksLib = require('../lib/chunks');
+const config = require('../lib/config');
+const sqldb = require('../prairielib/lib/sql-db');
+const sqlLoader = require('../prairielib/lib/sql-loader');
+const sql = sqlLoader.loadSqlEquiv(__filename);
+
+const helperServer = require('./helperServer');
 
 const COURSE = {
   course: {},
@@ -131,6 +141,48 @@ describe('chunks', () => {
         COURSE
       );
       assert.isOk(chunks.courseInstances['complex/course/instance'].clientFilesCourseInstance);
+    });
+  });
+
+  describe('deletes chunks that are no longer needed', function () {
+    this.timeout(60000);
+
+    /** @type {tmp.DirectoryResult} */
+    let tempTestCourseDir;
+    let courseId;
+
+    before('set up testing server', async () => {
+      // We need to modify the test course - create a copy that we can
+      // safely manipulate.
+      tempTestCourseDir = await tmp.dir({ unsafeCleanup: true });
+      await fs.copy(path.resolve(__dirname, '..', 'testCourse'), tempTestCourseDir.path, {
+        overwrite: true,
+      });
+
+      config.chunksConsumer = true;
+
+      await util.promisify(helperServer.before(tempTestCourseDir.path).bind(this))();
+
+      // Find the ID of this course
+      const results = await sqldb.queryOneRowAsync(sql.select_course_by_path, {
+        course_path: tempTestCourseDir.path,
+      });
+      courseId = results.rows[0].id;
+    });
+
+    after('shut down testing server', async () => {
+      await tempTestCourseDir.cleanup();
+      await util.promisify(helperServer.after.bind(this))();
+
+      config.chunksConsumer = false;
+    });
+
+    it('deletes chunks that are no longer needed', async () => {
+      const course_ids = [courseId];
+      const authn_user_id = 1;
+
+      // Generate chunks for the example course
+      await chunksLib.generateAllChunksForCourseList(course_ids, authn_user_id);
     });
   });
 });
