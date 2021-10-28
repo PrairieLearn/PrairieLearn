d3b038f1dbc1636630fd557a2b73486d025aba52
Nathan Walters <nathan@prairielearn.com>
2021-10-28 14:45:38 -0700

    Apply suggestions from code review

--- lib/chunks.sql
+++ lib/chunks.sql
@@ -72,10 +72,11 @@ SELECT
     ci.id,
     a.id,
     (chunk->>'uuid')::uuid
-FROM JSON_ARRAY_ELEMENTS($chunks) AS chunk(json)
-LEFT JOIN questions AS q ON (q.qid = (chunk->>'questionName') AND q.deleted_at IS NULL AND q.course_id = $course_id)
-LEFT JOIN course_instances AS ci ON (ci.short_name = (chunk->>'courseInstanceName') AND ci.deleted_at IS NULL AND ci.course_id = $course_id)
-LEFT JOIN assessments AS a ON (a.tid = (chunk->>'assessmentName') AND a.deleted_at IS NULL AND a.course_instance_id = ci.id)
+FROM
+    JSON_ARRAY_ELEMENTS($chunks) AS chunk(json)
+    LEFT JOIN questions AS q ON (q.qid = (chunk->>'questionName') AND q.deleted_at IS NULL AND q.course_id = $course_id)
+    LEFT JOIN course_instances AS ci ON (ci.short_name = (chunk->>'courseInstanceName') AND ci.deleted_at IS NULL AND ci.course_id = $course_id)
+    LEFT JOIN assessments AS a ON (a.tid = (chunk->>'assessmentName') AND a.deleted_at IS NULL AND a.course_instance_id = ci.id)
 ON CONFLICT (type, course_id, coalesce(question_id, -1), coalesce(course_instance_id, -1), coalesce(assessment_id, -1)) DO UPDATE
 SET
     type = EXCLUDED.type,
@@ -87,26 +88,26 @@ SET
 -- BLOCK delete_chunks
 WITH chunks_metadata AS (
     SELECT
-        (chunk->>'type')::enum_chunk_type AS type,
+        (cm->>'type')::enum_chunk_type AS type,
         q.id AS question_id,
         ci.id AS course_instance_id,
         a.id AS assessment_id,
-        (chunk->>'uuid')::uuid AS uuid
-    FROM JSON_ARRAY_ELEMENTS($chunks) AS chunk(json)
-    LEFT JOIN questions AS q ON (q.qid = (chunk->>'questionName') AND q.deleted_at IS NULL AND q.course_id = $course_id)
-    LEFT JOIN course_instances AS ci ON (ci.short_name = (chunk->>'courseInstanceName') AND ci.deleted_at IS NULL AND ci.course_id = $course_id)
-    LEFT JOIN assessments AS a ON (a.tid = (chunk->>'assessmentName') AND a.deleted_at IS NULL AND a.course_instance_id = ci.id)
+        (cm->>'uuid')::uuid AS uuid
+    FROM JSON_ARRAY_ELEMENTS($chunks) AS cm
+    LEFT JOIN questions AS q ON (q.qid = (cm->>'questionName') AND q.deleted_at IS NULL AND q.course_id = $course_id)
+    LEFT JOIN course_instances AS ci ON (ci.short_name = (cm->>'courseInstanceName') AND ci.deleted_at IS NULL AND ci.course_id = $course_id)
+    LEFT JOIN assessments AS a ON (a.tid = (cm->>'assessmentName') AND a.deleted_at IS NULL AND a.course_instance_id = ci.id)
 ),
 chunks_to_delete AS (
     SELECT id FROM chunks
-    INNER JOIN chunks_metadata AS cm ON (
+    JOIN chunks_metadata AS cm ON (
         chunks.course_id = $course_id
         AND chunks.type = cm.type
         AND CASE
             WHEN (cm.type = 'elements' OR cm.type = 'elementExtensions' OR cm.type = 'clientFilesCourse' OR cm.type = 'serverFilesCourse') THEN TRUE
-            WHEN (cm.type = 'clientFilesCourseInstance') THEN cm.course_instance_id = cm.course_instance_id
-            WHEN (cm.type = 'clientFilesAssessment') THEN cm.assessment_id = cm.assessment_id
-            WHEN (cm.type = 'question') THEN cm.question_id = cm.question_id
+            WHEN (cm.type = 'clientFilesCourseInstance') THEN cm.course_instance_id = chunks.course_instance_id
+            WHEN (cm.type = 'clientFilesAssessment') THEN cm.assessment_id = chunks.assessment_id
+            WHEN (cm.type = 'question') THEN cm.question_id = chunks.question_id
             ELSE FALSE
         END
     )
