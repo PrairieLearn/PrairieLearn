8621f9e7fb5b8799ae437f22fc36b2139c5c3fb9
Seth Poulsen <poulsenseth@yahoo.com>
2021-11-01 11:52:08 -0500

    pl-order-blocks: Refactor DAG grading code
    Code is now much shorter and simpler. This refactor also helps prepare
    for implementing #4960.
    
    Tested on over 9000 submissions from prior courses to ensure the
    refactor didn't change any behavior.

--- elements/pl-order-blocks/dag_checker.py
+++ elements/pl-order-blocks/dag_checker.py
@@ -1,72 +1,45 @@
+from collections import Counter
+import networkx as nx
 
-class AnswerState:
-    def __init__(self):
-        self.valid = set()  # a block is valid if all blocks that it depends on have appeared before it
-        self.current_group = None
-        self.current_group_len = 0
+def check_topological_sorting(order, graph):
+    seen = set()
+    for i, node in enumerate(order):
+        if node is None or not all([u in seen for (u,_) in graph.in_edges(node)]):
+            return i, i
+        seen.add(node)
+    return len(order), -1
 
-    def begin_group(self, group_id):
-        self.current_group = group_id
-
-    def end_group(self):
-        self.current_group = None
-        self.current_group_len = 0
-
-    def set_valid(self, stmt_id):
-        self.valid.add(stmt_id)
-        if self.current_group is not None:
-            self.current_group_len += 1
-
-    def is_valid(self, stmt_id):
-        return stmt_id in self.valid
-
-    def get_current_group_blocks(self):
-        return self.current_group_len
-
+def check_grouping(order, group_belonging):
+    group_sizes = Counter(group_belonging.values())
+    cur_group = None
+    cur_group_size = None
+    for i, node in enumerate(order):
+        group_id = group_belonging.get(node)
+        if group_id is not None and cur_group is None:
+            cur_group = group_id
+            cur_group_size = 1
+        elif group_id is None and cur_group is not None:
+            return i, i
+        elif group_id is not None and cur_group is not None:
+            if group_id == cur_group:
+                cur_group_size += 1
+                if cur_group_size == group_sizes[cur_group]:
+                    cur_group = None
+                    cur_group_size = None
+            else:
+                return i, i
+    return len(order), -1
 
 def grade_dag(order, depends_graph, group_belonging):
-    first_wrong = -1
-    answer_state = AnswerState()
+    graph = nx.DiGraph()
+    for node in depends_graph:
+        for node2 in depends_graph[node]:
+            graph.add_edge(node2, node)
 
-    group_sizes = {}
-    for stmt_id in group_belonging:
-        group = group_belonging.get(stmt_id)
-        if group is None:
-            continue
-        if group_sizes.get(group) is None:
-            group_sizes[group] = 0
-        group_sizes[group] += 1
+    top_sort_correctness, top_sort_first_wrong = check_topological_sorting(order, graph)
+    grouping_correctness, grouping_first_wrong = check_grouping(order, group_belonging)
 
-    for i, block in enumerate(order):
-        block_valid = True
-        depends = depends_graph.get(block)
-        if depends is None:
-            block_valid = False
-        else:
-            for depend in depends:
-                if not answer_state.is_valid(depend):
-                    block_valid = False
-
-        if block_valid:
-            group_id = group_belonging.get(block)
-            if group_id is None and answer_state.current_group is None:
-                answer_state.set_valid(block)
-            elif group_id is not None and answer_state.current_group is None:
-                answer_state.begin_group(group_id)
-                answer_state.set_valid(block)
-            elif group_id is None and answer_state.current_group is not None:
-                first_wrong = i
-                break
-            elif group_id is not None and answer_state.current_group is not None:
-                if group_id == answer_state.current_group:
-                    answer_state.set_valid(block)
-                    if answer_state.get_current_group_blocks() == group_sizes.get(group_id):
-                        answer_state.end_group()
-                else:
-                    first_wrong = i
-                    break
-        else:
-            first_wrong = i
-            break
-
-    return len(answer_state.valid), first_wrong
+    if top_sort_correctness < grouping_correctness:
+        return top_sort_correctness, top_sort_first_wrong
+    else:
+        return grouping_correctness, grouping_first_wrong
