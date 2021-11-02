8aa84858b0692673e67014aae7b64d77c35e5517
Andrew Stec <andrewstec@gmail.com>
2021-11-02 08:52:00 -0700

    Seems like best option so far, even if it is out of date. Consider child process

--- pages/instructorScanArtifacts/instructorScanArtifacts.js
+++ pages/instructorScanArtifacts/instructorScanArtifacts.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const logger = require('../../lib/logger');
 const config = require('../../lib/config.js');
 const quagga = require('quagga').default;
+const imagemagick = require('imagemagick');
 const fs = require('fs');
 const {fromPath} = require('pdf2pic');
 
@@ -12,36 +13,75 @@ const {fromPath} = require('pdf2pic');
 // const sql = sqlLoader.loadSqlEquiv(__filename);
 
 router.get('/', (req, res, next) => {
-    const file = './test.pdf';
+    imagemagick.identify('./test.pdf', (err, output) => {
+        if (err) { console.log(err); }
+        console.log(output);
+    });
+    imagemagick.convert(['test.pdf[0]', '-flatten', '-quality', '100', '-resize', '150%', 'kittens-small.jpg'], (err, output) => {
+        if (err) { console.log(err); }
+        console.log('the output', output);
 
-    const out = './';
-        
-    const baseOptions = {
-      width: 2550,
-      height: 3300,
-      density: 330,
-      savePath: out,
-    };
-    
-    const convert = fromPath(file, baseOptions);
-    convert.setGMClass(true);
-    convert(1);
 
-    // quagga.decodeSingle({
-    //     src: './test.jpeg',
-    //     numOfWorkers: 0, // 0 in node
-    //     inputStream: {
-    //         size: 640, // pixel width
-    //         area: { // defines rectangle of the detection/localization area
-    //             top: '0%',    // top offset
-    //             right: '0%',  // right offset
-    //             left: '0%',   // left offset
-    //             bottom: '0%',  // bottom offset
-    //           },
-    //     },
-    //     locate: true,
-    // }, (data) => {
-    //     console.log(data);
+        const javascriptBarcodeReader = require('javascript-barcode-reader');
+        // const jpeg = require('jpeg-js');
+        // const jpegData = fs.readFileSync();
+        // const clampedArrayImage = jpeg.decode(jpegData);
+
+        // javascriptBarcodeReader({
+        //   /* Image file Path || {data: Uint8ClampedArray, width, height} || HTML5 Canvas ImageData */
+        //   image: './kittens-small.jpg',
+        //   barcode: 'code-128',
+        //   // barcodeType: 'industrial',
+        //   options: {    
+        //     useAdaptiveThreshold: true, // for images with sahded portions
+        //     // singlePass: true
+        //   },
+        // })
+        //   .then(code => {
+        //     console.log(code);
+        //   })
+        //   .catch(err => {
+        //     console.log(err);
+        //   });
+
+
+
+        quagga.decodeSingle({
+            src: './kittens-small.jpg',
+            numOfWorkers: 0, // 0 in node
+            locate: true,
+            decoder: {
+                readers: ["code_128_reader", "ean_reader"] // List of active readers
+            },
+            locator: {
+                patchSize: "x-large", // x-small, small, medium, large, x-large
+            },
+        }, (data) => {
+            console.log(data);
+        });
+    });
+    console.log('FINALLY DONE');
+
+    // const convert = fromPath('./test1.pdf', {
+    //     format: 'png',
+    //     background: 'white',
+    //     saveFilename: 'test',
+    //     savePath: '.',
+    //   });
+    // convert.setGMClass(true);
+    // convert(1).then((resolve) => {
+    //     const baseOptions = {
+    //         format: 'jpeg',
+    //         saveFilename: 'test',
+    //         savePath: '.',
+    //         background: 'white',
+    //       };
+    //     const convert = fromPath('./test.1.png', baseOptions);
+    //     convert.setGMClass(true);
+    //     return convert(1);
+    // })
+    // .then((resolve) => {
+    //     console.log(resolve);
 
     //     res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
     // });
