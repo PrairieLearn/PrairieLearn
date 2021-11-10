39c5ea647841c3cc4d6abd62ee04f3d01e090b70
Andrew Stec <andrewstec@gmail.com>
2021-11-10 12:58:11 -0800

    Removing endpoint logic, as file upload payload through view instead

--- api/v1/endpoints/courseInstanceAssessmentInstances/index.js
+++ api/v1/endpoints/courseInstanceAssessmentInstances/index.js
@@ -4,23 +4,11 @@ const express = require('express');
 const router = express.Router({
   mergeParams: true,
 });
-const multer = require('multer');
-const config = require('../../../../lib/config');
 
-const fileStore = require('../../../../lib/file-store');
 const sqldb = require('../../../../prairielib/lib/sql-db');
 const sqlLoader = require('../../../../prairielib/lib/sql-loader');
 
 const sql = sqlLoader.load(path.join(__dirname, '..', 'queries.sql'));
-
-const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: {
-    fieldSize: config.fileUploadMaxBytes,
-    fileSize: config.fileUploadMaxBytes,
-    parts: config.fileUploadMaxParts,
-  },
-});
 
 router.get('/:assessment_instance_id', (req, res, next) => {
   const params = {
@@ -64,49 +52,6 @@ router.get('/:assessment_instance_id/submissions', (req, res, next) => {
     res.status(200).send(result.rows[0].item);
   });
 });
-
-router.put(
-  '/:assessment_instance_id/submission/:submission_id/file',
-  upload.single('file'),
-  (req, res, next) => {
-    const { assessment_instance_id, course_instance_id, submission_id } = req.params;
-
-    // # TO DO - split out error to user through API if these errors are hit.
-    if (!assessment_instance_id || !course_instance_id || !submission_id) {
-      ERR(
-        Error(
-          'Required params for artifact upload: course_instance_id, assessment_instance_id, and submission_id'
-        )
-      );
-    }
-    if (!req.file) {
-      ERR(Error('Missing artifact file data'), next);
-      return;
-    }
-    if (!/([a-zA-Z0-9\s_\\.\-:])+(.pdf)$/.test(req.file.originalname)) {
-      ERR(Error('Valid pdf required.'));
-      return;
-    }
-
-    fileStore
-      .upload(
-        req.file.originalname,
-        req.file.buffer,
-        'artifact_upload',
-        assessment_instance_id,
-        course_instance_id,
-        submission_id,
-        res.locals.user.user_id,
-        res.locals.authn_user.user_id
-      )
-      .then(() => {
-        res.status(200).send('Artifact uploaded');
-      })
-      .catch((err) => {
-        if (ERR(err, next)) return;
-      });
-  }
-);
 
 router.get('/:assessment_instance_id/log', (req, res, next) => {
   const params = [req.params.assessment_instance_id, true];
