2d95e0112830a61086cd2a0e733c701c235556c9
Andrew Stec <andrewstec@gmail.com>
2021-11-10 11:53:34 -0800

    Running formatter

--- pages/instructorScanPaper/instructorScanPaper.js
+++ pages/instructorScanPaper/instructorScanPaper.js
@@ -15,105 +15,106 @@ const fs = require('fs');
 // const sql = sqlLoader.loadSqlEquiv(__filename);
 
 router.get('/', (req, res, next) => {
-
   let file = req.body.file;
   file = './test.pdf';
 
-    imagemagick.identify(file, (err, output) => {
-        if (err) { console.log(err); }
-        console.log(output);
-    });
-    imagemagick.convert(['test.pdf[0]', '-flatten', '-quality', '100', '-resize', '150%', 'kittens-small.jpg'], (err, output) => {
-        if (err) { console.log(err); }
-        console.log('the output', output);
-        
-        quagga.decodeSingle({
-            src: fs.readFileSync('./kittens-small.jpg'),
-            numOfWorkers: 0,
-            inputStream: {
-                mime: "image/jpeg",
-                size: 800,
-                area: {
-                    top: "70%",
-                    right: "25%",
-                    left: "25%",
-                    bottom: "20%"
-                },
+  imagemagick.identify(file, (err, output) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(output);
+  });
+  imagemagick.convert(
+    ['test.pdf[0]', '-flatten', '-quality', '100', '-resize', '150%', 'kittens-small.jpg'],
+    (err, output) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log('the output', output);
+
+      quagga.decodeSingle(
+        {
+          src: fs.readFileSync('./kittens-small.jpg'),
+          numOfWorkers: 0,
+          inputStream: {
+            mime: 'image/jpeg',
+            size: 800,
+            area: {
+              top: '70%',
+              right: '25%',
+              left: '25%',
+              bottom: '20%',
             },
-            locate: true,
-        }, (result) => {
-            console.log("result", result);
-        });
-    });
+          },
+          locate: true,
+        },
+        (result) => {
+          console.log('result', result);
+        }
+      );
+    }
+  );
 
-    res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
-    next();
+  res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+  next();
 
+  // const javascriptBarcodeReader = require('javascript-barcode-reader');
+  // const jpeg = require('jpeg-js');
+  // const jpegData = fs.readFileSync();
+  // const clampedArrayImage = jpeg.decode(jpegData);
 
+  // javascriptBarcodeReader({
+  //   /* Image file Path || {data: Uint8ClampedArray, width, height} || HTML5 Canvas ImageData */
+  //   image: './kittens-small.jpg',
+  //   barcode: 'code-128',
+  //   // barcodeType: 'industrial',
+  //   options: {
+  //     useAdaptiveThreshold: true, // for images with sahded portions
+  //     // singlePass: true
+  //   },
+  // })
+  //   .then(code => {
+  //     console.log(code);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
 
-        // const javascriptBarcodeReader = require('javascript-barcode-reader');
-        // const jpeg = require('jpeg-js');
-        // const jpegData = fs.readFileSync();
-        // const clampedArrayImage = jpeg.decode(jpegData);
+  // const convert = fromPath('./test1.pdf', {
+  //     format: 'png',
+  //     background: 'white',
+  //     saveFilename: 'test',
+  //     savePath: '.',
+  //   });
+  // convert.setGMClass(true);
+  // convert(1).then((resolve) => {
+  //     const baseOptions = {
+  //         format: 'jpeg',
+  //         saveFilename: 'test',
+  //         savePath: '.',
+  //         background: 'white',
+  //       };
+  //     const convert = fromPath('./test.1.png', baseOptions);
+  //     convert.setGMClass(true);
+  //     return convert(1);
+  // })
+  // .then((resolve) => {
+  //     console.log(resolve);
 
-        // javascriptBarcodeReader({
-        //   /* Image file Path || {data: Uint8ClampedArray, width, height} || HTML5 Canvas ImageData */
-        //   image: './kittens-small.jpg',
-        //   barcode: 'code-128',
-        //   // barcodeType: 'industrial',
-        //   options: {    
-        //     useAdaptiveThreshold: true, // for images with sahded portions
-        //     // singlePass: true
-        //   },
-        // })
-        //   .then(code => {
-        //     console.log(code);
-        //   })
-        //   .catch(err => {
-        //     console.log(err);
-        //   });
-
-
-
-
-
-    // const convert = fromPath('./test1.pdf', {
-    //     format: 'png',
-    //     background: 'white',
-    //     saveFilename: 'test',
-    //     savePath: '.',
-    //   });
-    // convert.setGMClass(true);
-    // convert(1).then((resolve) => {
-    //     const baseOptions = {
-    //         format: 'jpeg',
-    //         saveFilename: 'test',
-    //         savePath: '.',
-    //         background: 'white',
-    //       };
-    //     const convert = fromPath('./test.1.png', baseOptions);
-    //     convert.setGMClass(true);
-    //     return convert(1);
-    // })
-    // .then((resolve) => {
-    //     console.log(resolve);
-
-    //     res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
-    // });
-
+  //     res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+  // });
 });
 
-router.post('/', function(req, res, next) {
-
+router.post('/', function (req, res, next) {
   console.log(req.body);
-    // upper bound pdf size limit = 25mb
+  // upper bound pdf size limit = 25mb
 
-    // each page size conversion to 300kb
+  // each page size conversion to 300kb
 
-    // barcode reader fails (send back page image)
+  // barcode reader fails (send back page image)
 
-    res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
-    next();
+  res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+  next();
 });
 
 module.exports = router;
--- pages/instructorScrapPaper/instructorScrapPaper.js
+++ pages/instructorScrapPaper/instructorScrapPaper.js
@@ -14,92 +14,95 @@ const sharp = require('sharp');
 const jsCrc = require('js-crc');
 
 const generateBarcodes = async (numBarcodes) => {
-    const barcodes = [];
+  const barcodes = [];
 
-    // Barcodes should be error-detecting <x char hexatridecimal produced by base36 encoding><4 char crc16 hash>
-    // Sequential counter to produce x char base36 string should be base36 encode input
-    // Approx lower and upper bound: 80000000000 - 999999999999; 80 billion to 100 billion
+  // Barcodes should be error-detecting <x char hexatridecimal produced by base36 encoding><4 char crc16 hash>
+  // Sequential counter to produce x char base36 string should be base36 encode input
+  // Approx lower and upper bound: 80000000000 - 999999999999; 80 billion to 100 billion
 
-    if (!numBarcodes || numBarcodes > 1000) {
-        throw new Error('Cannot produce more than 1000 or less than 1 barcoded sheets');
+  if (!numBarcodes || numBarcodes > 1000) {
+    throw new Error('Cannot produce more than 1000 or less than 1 barcoded sheets');
+  }
+
+  const client = await sqldb.beginTransactionAsync();
+  try {
+    const queryCount = await sqldb.queryWithClientAsync(client, sql.get_barcodes_count, {});
+
+    // Want to create new barcodes based on sequential count in database
+    let barcodesCount = Number(queryCount[1].rows[0].count);
+    for (let i = 0; i < numBarcodes; i++) {
+      barcodesCount += 1;
+      const base36 = BigInt(barcodesCount).toString(36);
+      const crc16 = jsCrc.crc16(base36);
+      const barcode = `${base36}${crc16}`; // concat as string in chance integer combo
+      barcodes.push(barcode);
     }
 
-    const client = await sqldb.beginTransactionAsync();
-    try {
-      const queryCount = await sqldb.queryWithClientAsync(client, sql.get_barcodes_count, {});
-
-      // Want to create new barcodes based on sequential count in database
-      let barcodesCount = Number(queryCount[1].rows[0].count);
-      for (let i = 0; i < numBarcodes; i++) {
-          barcodesCount+=1;
-          const base36 = BigInt(barcodesCount).toString(36);
-          const crc16 = jsCrc.crc16(base36);
-          const barcode = `${base36}${crc16}`;  // concat as string in chance integer combo
-          barcodes.push(barcode);
-      }
-
-      const insert_barcodes = sql.insert_barcodes.replace('$barcodes', barcodes.map(barcode => "('" + barcode + "')").join(','));
-      const insertedBarcodes = await sqldb.queryWithClientAsync(client, insert_barcodes, {});
-      if (insertedBarcodes.rows.length != numBarcodes) {
-          throw Error('Wrong number of barcodes created. Aborting');
-      }
-    } catch (err) {
-        // rolls back if error
-        await sqldb.endTransactionAsync(client, err);
-        throw err;
+    const insert_barcodes = sql.insert_barcodes.replace(
+      '$barcodes',
+      barcodes.map((barcode) => "('" + barcode + "')").join(',')
+    );
+    const insertedBarcodes = await sqldb.queryWithClientAsync(client, insert_barcodes, {});
+    if (insertedBarcodes.rows.length != numBarcodes) {
+      throw Error('Wrong number of barcodes created. Aborting');
     }
-    await sqldb.endTransactionAsync(client, null);
-    return barcodes;
+  } catch (err) {
+    // rolls back if error
+    await sqldb.endTransactionAsync(client, err);
+    throw err;
+  }
+  await sqldb.endTransactionAsync(client, null);
+  return barcodes;
 };
 
 const createBarcodeSVGs = async (barcodes) => {
-    const svgs = [];
-    for (let i = 0; i < barcodes.length; i++) {
-        // TO DO: Substitute bitgener with library that supports label formatting with spaces 'sxcxc xvcxcv vvcxvs'
-        const result = await bitgener({
-            data: barcodes[i],
-            type: 'code128',
-            output: 'buffer',
-            encoding: 'utf8',
-            original1DSize: true,
-            addQuietZone: true,
-            color: '#000',
-            bgColor: '#FFF',
-            hri: {
-                show: true,
-                fontFamily: 'Futura',
-                marginTop: 5,
-            },
-        });
+  const svgs = [];
+  for (let i = 0; i < barcodes.length; i++) {
+    // TO DO: Substitute bitgener with library that supports label formatting with spaces 'sxcxc xvcxcv vvcxvs'
+    const result = await bitgener({
+      data: barcodes[i],
+      type: 'code128',
+      output: 'buffer',
+      encoding: 'utf8',
+      original1DSize: true,
+      addQuietZone: true,
+      color: '#000',
+      bgColor: '#FFF',
+      hri: {
+        show: true,
+        fontFamily: 'Futura',
+        marginTop: 5,
+      },
+    });
 
-        svgs.push(result.svg);
-    }
-    return svgs;
+    svgs.push(result.svg);
+  }
+  return svgs;
 };
 const convertImage = async (image) => {
-    return sharp(image)
-        .png({quality: 100})  // TO DO: lookup defaults
-        .toBuffer({resolveWithObject: true});
+  return sharp(image)
+    .png({ quality: 100 }) // TO DO: lookup defaults
+    .toBuffer({ resolveWithObject: true });
 };
 const svgsToPdf = async (title, svgs) => {
-    let doc = null;
+  let doc = null;
 
-    for (let i = 0; i < svgs.length; i++) {
-        if (i === 0) {
-            doc = new pdfkit({size: 'A3'});
-        } else {
-            doc.addPage({size: 'A3'});
-        }
-
-        doc.fontSize(32);
-        doc.text(title, {align: 'center'})
-
-        const {data, info} = await convertImage(svgs[i]);
-
-        //                        center image             give bottom margin with 30 padding
-        doc.image(data, (doc.page.width - info.width) / 2, doc.page.height - info.height - 30);
+  for (let i = 0; i < svgs.length; i++) {
+    if (i === 0) {
+      doc = new pdfkit({ size: 'A3' });
+    } else {
+      doc.addPage({ size: 'A3' });
     }
-    return doc;
+
+    doc.fontSize(32);
+    doc.text(title, { align: 'center' });
+
+    const { data, info } = await convertImage(svgs[i]);
+
+    //                        center image             give bottom margin with 30 padding
+    doc.image(data, (doc.page.width - info.width) / 2, doc.page.height - info.height - 30);
+  }
+  return doc;
 };
 
 router.get('/', (req, res, next) => {
@@ -107,26 +110,26 @@ router.get('/', (req, res, next) => {
   next();
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   if (req.body.__action == 'make_scrap_paper') {
     const numPages = req.body.num_pages;
     const pageLabel = req.body.page_label;
 
     if (!numPages || numPages < 0 || numPages > 500) {
-      throw Error('Must be more than 1 page but not more than 500 pages')
+      throw Error('Must be more than 1 page but not more than 500 pages');
     }
     if (typeof pageLabel !== 'string' || pageLabel.length > 45) {
-      throw Error('Page label must be valid string less than 45 characters')
+      throw Error('Page label must be valid string less than 45 characters');
     }
 
     generateBarcodes(numPages)
-      .then(barcodes => {
+      .then((barcodes) => {
         return createBarcodeSVGs(barcodes);
       })
-      .then(barcodeSVGs => {
+      .then((barcodeSVGs) => {
         return svgsToPdf(pageLabel, barcodeSVGs);
       })
-      .then(pdf => {
+      .then((pdf) => {
         const chunks = [];
         return new Promise((resolve, reject) => {
           pdf.on('data', (chunk) => {
@@ -137,16 +140,16 @@ router.post('/', function(req, res, next) {
           });
           pdf.on('error', (error) => {
             reject(error);
-          })
+          });
           pdf.end();
         });
       })
-      .then(pdfBuffer => {
+      .then((pdfBuffer) => {
         res.locals['pdf'] = pdfBuffer.toString('base64');
         res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
         next();
       })
-      .catch(err => {
+      .catch((err) => {
         if (ERR(err, next)) return;
       });
   } else {
