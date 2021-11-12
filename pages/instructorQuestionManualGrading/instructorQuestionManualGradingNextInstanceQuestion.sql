0eac7b27b5b2bdd0b83f8a9b3f3beeb0da575d7d
Seth Poulsen <poulsenseth@yahoo.com>
2021-11-12 15:01:02 -0600

    Fix problem where no questions showed up but they should have

--- pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
@@ -1,6 +1,6 @@
 -- BLOCK get_next_unmarked_instance_question
 SELECT iq.*
-FROM 
+FROM
     instance_questions AS iq
         JOIN assessment_questions AS aq ON (aq.id = iq.assessment_question_id)
         JOIN assessments AS a ON (a.id = aq.assessment_id)
@@ -8,11 +8,11 @@ FROM
         JOIN (
             -- We only want the LAST submissions that are created by each user to join on all
             -- instance questions under assessment question
-            SELECT DISTINCT ON (s.auth_user_id) * FROM submissions AS s
+            SELECT * FROM submissions AS s
             WHERE s.graded_at IS NULL
             ORDER BY s.auth_user_id, s.date DESC, s.id DESC
         ) s ON (s.variant_id = v.id)
-WHERE 
+WHERE
     iq.assessment_question_id = $assessment_question_id
     AND a.id = $assessment_id
 LIMIT 1;
