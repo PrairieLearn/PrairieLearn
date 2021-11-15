eb8fe4dd33229f1cd854d916912c2c3b19c73b66
Seth Poulsen <poulsenseth@yahoo.com>
2021-11-15 10:58:48 -0600

    Bug Fixes for basic Manual Grading Pages

--- pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.sql
+++ pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.sql
@@ -4,7 +4,7 @@ SELECT
     q.qid,
     q.title,
     q.id AS question_id,
-    admin_assessment_question_number(aq.id) as number,
+    -- admin_assessment_question_number(aq.id) as number,
     ag.number AS alternative_group_number,
     (count(*) OVER (PARTITION BY ag.number)) AS alternative_group_size,
     (SELECT COUNT(DISTINCT iq.id)
@@ -13,15 +13,20 @@ SELECT
          JOIN variants AS v ON (v.instance_question_id = iq.id)
          JOIN submissions AS s ON (s.variant_id = v.id)
      WHERE
-         iq.assessment_question_id = aq.id) AS num_submissions,
-    (SELECT COUNT(DISTINCT iq.id)
-     FROM
-         instance_questions AS iq
-         JOIN variants AS v ON (v.instance_question_id = iq.id)
-         JOIN submissions AS s ON (s.variant_id = v.id)
-     WHERE
          iq.assessment_question_id = aq.id
-         AND s.graded_at IS NULL) AS num_ungraded_submissions
+    ) AS num_submissions,
+    (SELECT COUNT(*) FROM
+        (SELECT DISTINCT ON (iq.id) iq.id, s.graded_at
+         FROM
+             instance_questions AS iq
+             JOIN variants AS v ON (v.instance_question_id = iq.id)
+             JOIN submissions AS s ON (s.variant_id = v.id)
+         WHERE
+             iq.assessment_question_id = aq.id
+         GROUP BY iq.id, s.date, s.graded_at
+         ORDER BY iq.id DESC, s.date DESC
+         ) AS submission_info
+     WHERE submission_info.graded_at IS NULL) AS num_ungraded_submissions
 FROM
     assessment_questions AS aq
     JOIN questions AS q ON (q.id = aq.question_id)
