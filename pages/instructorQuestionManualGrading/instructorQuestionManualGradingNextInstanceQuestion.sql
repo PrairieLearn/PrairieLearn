240273975f69ed1ce7429688bfddfb979477c6cf
Seth Poulsen <poulsenseth@yahoo.com>
2021-11-19 11:45:45 -0600

    Order manual grading pages by group or user name

--- pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
+++ pages/instructorQuestionManualGrading/instructorQuestionManualGradingNextInstanceQuestion.sql
@@ -1,16 +1,19 @@
 -- BLOCK get_next_ungraded_instance_question
 WITH to_grade AS (
-    SELECT *, (lag(id) OVER (ORDER BY id DESC) IS NOT DISTINCT FROM $prior_instance_question_id) AS is_next
+    SELECT *, (lag(id) OVER (ORDER BY user_or_group_name DESC) IS NOT DISTINCT FROM $prior_instance_question_id) AS is_next
     FROM (
         SELECT DISTINCT ON (iq.id)
             iq.*,
             s.graded_at,
-            ai.id AS assessment_instance_id
+            ai.id AS assessment_instance_id,
+            COALESCE(g.name, u.name) AS user_or_group_name
         FROM
             instance_questions AS iq
             JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
             JOIN variants AS v ON (v.instance_question_id = iq.id)
             JOIN submissions AS s ON (s.variant_id = v.id)
+            LEFT JOIN users u ON (u.user_id = ai.user_id)
+            LEFT JOIN groups g ON (g.id = ai.group_id)
         WHERE
             ai.assessment_id = $assessment_id
             AND iq.assessment_question_id = $assessment_question_id
