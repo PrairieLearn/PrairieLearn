f51486e64c2acaf0840335a7c2b2a64610c20411
Seth Poulsen <poulsenseth@yahoo.com>
2021-11-22 18:56:19 -0600

    Add unit tests

--- elements/pl-order-blocks/dag_checker_test.py
+++ elements/pl-order-blocks/dag_checker_test.py
@@ -1,5 +1,5 @@
 import unittest
-from dag_checker import grade_dag
+from dag_checker import grade_dag, lcs_partial_credit
 
 problem_1_dag = {'1': [], '2': ['1'], '3': ['1'], '4': ['2', '3'], '5': ['1'], '6': ['2'], '7': ['4', '5', '6'], '8': [], '9': ['7', '8'], '10': ['9']}
 problem_1_groups = {str(i): None for i in range(1, 11)}
@@ -15,7 +15,12 @@ problem_1_expected = [
     7,
     6,
 ]
-
+problem_1_expected_ed = [
+    0,
+    0,
+    2,
+    4,
+]
 
 problem_2_dag = {'1': [], '2': ['1'], '3': ['2'], '4': [], '5': ['4'], '6': ['5'], '7': ['3', '6']}
 problem_2_groups = {'1': 1, '2': 1, '3': 1, '4': 2, '5': 2, '6': 2, '7': None}
@@ -38,8 +43,9 @@ problem_2_expected = [
 class TestDAGChecker(unittest.TestCase):
 
     def test_grade_dag(self):
-        for submission, expected in zip(problem_1_submissions, problem_1_expected):
+        for submission, expected, expected_ed in zip(problem_1_submissions, problem_1_expected, problem_1_expected_ed):
             self.assertEqual(grade_dag(submission, problem_1_dag, problem_1_groups), expected)
+            self.assertEqual(lcs_partial_credit(submission, problem_1_dag), expected_ed)
 
         for submission, expected in zip(problem_2_submissions, problem_2_expected):
             self.assertEqual(grade_dag(submission, problem_2_dag, problem_2_groups), expected)
