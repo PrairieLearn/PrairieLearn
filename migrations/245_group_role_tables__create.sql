7c64e0f3ec8a4747f62403542accb86b8c63352f
Renzo <renzol2@illinois.edu>
2021-11-23 22:08:26 -0600

    Finish initial migration

--- migrations/245_group_role_tables__create.sql
+++ migrations/245_group_role_tables__create.sql
@@ -1,3 +1,4 @@
+-- Create table and index for group roles
 CREATE TABLE IF NOT EXISTS group_roles (
     id BIGSERIAL PRIMARY KEY,
     role_name TEXT NOT NULL,
@@ -12,8 +13,9 @@ CREATE TABLE IF NOT EXISTS group_roles (
             REFERENCES assessments(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX group_roles_assessment_id_number_key ON group_roles (assessment_id);
+CREATE UNIQUE INDEX assessment_question_role_permissions_assessment_question_id_key ON group_roles (assessment_id);
 
+-- Create relational table and associated indexes between roles and assessment questions
 CREATE TABLE IF NOT EXISTS assessment_question_role_permissions (
     assessment_question_id: BIGINT NOT NULL,
     group_role_id: BIGINT NOT NULL,
@@ -27,3 +29,10 @@ CREATE TABLE IF NOT EXISTS assessment_question_role_permissions (
         FOREIGN KEY (group_role_id)
             REFERENCES group_roles(id) ON UPDATE CASCADE ON DELETE CASCADE,
 )
+
+CREATE UNIQUE INDEX assessment_question_role_permissions_assessment_question_id_key ON assessment_question_role_permissions (assessment_id);
+CREATE UNIQUE INDEX assessment_question_role_permissions_group_role_id_key ON assessment_question_role_permissions (group_role_id);
+
+-- Alter the group user table to include a role
+ALTER TABLE group_users ADD COLUMN IF NOT EXISTS group_role_id BIGINT;
+ALTER TABLE group_users ADD FOREIGN KEY (group_role_id) REFERENCES group_roles(id) ON DELETE CASCADE ON UPDATE CASCADE;
