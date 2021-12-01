6a7dea81cf808b1315f9864e86b6c94fcbac85c6
Andrew Stec <andrewstec@gmail.com>
2021-12-01 11:39:26 -0800

    Fixed linting

--- elements/pl-artifact-scan/pl-artifact-scan.py
+++ elements/pl-artifact-scan/pl-artifact-scan.py
@@ -7,12 +7,13 @@ BLANK_ANSWER = ''
 REQUIRED = 'required'
 REQUIRED_DEFAULT = True
 
+
 def crc16arc(msg):
-    '''
+    """
     CRC-16/ARC
     This algorithm has to match the 'js-crc' npm algorithm, which was assumed to be the same as this.
     https://stackoverflow.com/questions/63167168/trying-to-find-the-input-for-crc16-ibm
-    '''
+    """
     crc = 0
     for b in msg:
         crc ^= b
@@ -20,25 +21,27 @@ def crc16arc(msg):
             crc = (crc >> 1) ^ 0xa001 if crc & 1 else crc >> 1
     return crc
 
+
 def is_invalid_barcode(barcode):
     barcode = barcode.lower()
-    sha16 = barcode[len(barcode)-4:len(barcode)]
+    sha16 = barcode[len(barcode) - 4:len(barcode)]
     base32 = barcode.replace(sha16, '')
     recomputed_sha16 = hex(crc16arc(bytes(base32, encoding='utf-8')))
     if sha16 in recomputed_sha16:
         return False
-    else: 
+    else:
         return True
+
 
 def prepare(element_html, data):
     lxml.html.fragment_fromstring(element_html)
 
+
 def render(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
-    submitted_barcode = data['submitted_answers'].get(BARCODE, '').strip()
     required = pl.get_boolean_attrib(element, REQUIRED, REQUIRED_DEFAULT)
     uuid = pl.get_uuid()
-    
+
     if data['panel'] == 'question':
         html_params = {
             'question': True,
@@ -65,11 +68,10 @@ def parse(element_html, data):
 
     # TO DO: Figure out logic for if we need more barcodes per instance question submission.
     # ie. Student submits two pieces of paper to be scanned.
-    # See line 12 of pl-artifact-scan.mustache
     del data['submitted_answers'][BARCODE]
 
     data['submitted_answers'][BARCODE] = submitted_barcode
-    
+
     if isinstance(submitted_barcode, str) is not True:
         data['format_errors'][BARCODE] = 'Barcode is not a valid string'
         return
