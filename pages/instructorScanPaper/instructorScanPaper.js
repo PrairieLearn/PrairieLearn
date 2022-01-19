8c4a9885ec5c05c61a9b0d8f6bb0b37065ce2189
Nathan Walters <nathan@prairielearn.com>
2022-01-19 15:24:41 -0800

    Address more feedback from review

--- pages/instructorScanPaper/instructorScanPaper.js
+++ pages/instructorScanPaper/instructorScanPaper.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
+const asyncHandler = require('express-async-handler');
 
-const { processScrapPaperPdf } = require('../../lib/scrapPaperReader');
+const { processScrapPaperPdf } = require('../../lib/scrap-paper/read');
 const serverJobs = require('../../lib/server-jobs');
 
 const error = require('../../prairielib/lib/error');
@@ -13,48 +12,54 @@ router.get('/', (req, res, next) => {
   res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
 });
 
-router.post('/', async (req, res, next) => {
-  if (!res.locals.authz_data.has_course_instance_permission_edit) return next();
-  if (req.body.__action === 'scan_scrap_paper') {
-    if (!req.file) {
-      return next(error.make(400, 'Missing PDF file'));
+router.post(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    if (!res.locals.authz_data.has_course_instance_permission_edit) return next();
+    if (req.body.__action === 'scan_scrap_paper') {
+      if (!req.file) {
+        return next(error.make(400, 'Missing PDF file'));
+      }
+      if (!res.locals || !res.locals.authn_user || !res.locals.authn_user.user_id) {
+        return next(error.make(400, 'File-store requires "authn_user" to save data'));
+      }
+
+      const options = {
+        course_id: res.locals.course ? res.locals.course.id : null,
+        type: 'decoding_pdf_barcoded_collection',
+        description: 'Load data from local disk',
+      };
+      const jobSequenceId = await serverJobs.createJobSequenceAsync(options);
+
+      const jobOptions = {
+        course_id: res.locals.course ? res.locals.course.id : null,
+        type: 'decode_pdf_collection',
+        description:
+          'Decodes each barcode on each page in the pdf collection used as scrap paper by students.',
+        job_sequence_id: jobSequenceId,
+        last_in_sequence: false,
+      };
+
+      // Kick off the job in the background - don't wait for it to complete
+      // before redirecting the user.
+      const job = await serverJobs.createJobAsync(jobOptions);
+      processScrapPaperPdf(
+        req.file.buffer,
+        req.file.originalname,
+        res.locals.authn_user.user_id,
+        job
+      );
+
+      res.redirect(res.locals.urlPrefix + '/jobSequence/' + jobSequenceId);
+    } else {
+      return next(
+        error.make(400, 'unknown __action', {
+          locals: res.locals,
+          body: req.body,
+        })
+      );
     }
-    if (!res.locals || !res.locals.authn_user || !res.locals.authn_user.user_id) {
-      return next(error.make(400, 'File-store requires "authn_user" to save data'));
-    }
-
-    const options = {
-      course_id: res.locals.course ? res.locals.course.id : null,
-      type: 'decoding_pdf_barcoded_collection',
-      description: 'Load data from local disk',
-    };
-    const jobSequenceId = await serverJobs.createJobSequenceAsync(options);
-
-    const jobOptions = {
-      course_id: res.locals.course ? res.locals.course.id : null,
-      type: 'decode_pdf_collection',
-      description:
-        'Decodes each barcode on each page in the pdf collection used as scrap paper by students.',
-      job_sequence_id: jobSequenceId,
-      last_in_sequence: false,
-    };
-
-    const job = await serverJobs.createJobAsync(jobOptions);
-    res.redirect(res.locals.urlPrefix + '/jobSequence/' + jobSequenceId);
-    return processScrapPaperPdf(
-      req.file.buffer,
-      req.file.originalname,
-      res.locals.authn_user.user_id,
-      job
-    );
-  } else {
-    return next(
-      error.make(400, 'unknown __action', {
-        locals: res.locals,
-        body: req.body,
-      })
-    );
-  }
-});
+  })
+);
 
 module.exports = router;
--- pages/instructorScrapPaper/instructorScrapPaper.js
+++ pages/instructorScrapPaper/instructorScrapPaper.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const asyncHandler = require('express-async-handler');
+
 const { createBarcodedPdf } = require('../../lib/scrap-paper/generate');
 const error = require('../../prairielib/lib/error');
 
@@ -12,37 +14,42 @@ router.get('/', (req, res) => {
   res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
 });
 
-router.post('/', async (req, res, next) => {
-  if (!res.locals.authz_data.has_course_instance_permission_edit) return next();
-  if (req.body.__action === 'print_scrap_paper') {
-    const numPages = Number.parseInt(req.body.num_pages, 10);
-    const pageLabel = req.body.page_label;
+router.post(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    if (!res.locals.authz_data.has_course_instance_permission_edit) return next();
+    if (req.body.__action === 'print_scrap_paper') {
+      const numPages = Number.parseInt(req.body.num_pages, 10);
+      const pageLabel = req.body.page_label;
 
-    if (!numPages || numPages < 1 || numPages > pageLimit) {
-      return next(error.make(400, `Must be more than 1 page but not more than ${pageLimit} pages`));
-    }
-    if (typeof pageLabel !== 'string' || pageLabel.length > charLimit) {
+      if (!numPages || numPages < 1 || numPages > pageLimit) {
+        return next(
+          error.make(400, `Must be more than 1 page but not more than ${pageLimit} pages`)
+        );
+      }
+      if (typeof pageLabel !== 'string' || pageLabel.length > charLimit) {
+        return next(
+          error.make(400, `Page label must be valid string less than ${charLimit} characters`)
+        );
+      }
+
+      const pdf = await createBarcodedPdf(numPages, pageLabel);
+
+      res.header(
+        'Content-Disposition',
+        `attachment; filename=Barcoded scrap paper - ${new Date().toISOString()}.pdf`
+      );
+      pdf.pipe(res);
+      pdf.end();
+    } else {
       return next(
-        error.make(400, `Page label must be valid string less than ${charLimit} characters`)
+        error.make(400, 'unknown __action', {
+          locals: res.locals,
+          body: req.body,
+        })
       );
     }
-
-    const pdf = await createBarcodedPdf(numPages, pageLabel);
-
-    res.header(
-      'Content-Disposition',
-      `attachment; filename=Barcoded scrap paper - ${new Date().toISOString()}.pdf`
-    );
-    pdf.pipe(res);
-    pdf.end();
-  } else {
-    return next(
-      error.make(400, 'unknown __action', {
-        locals: res.locals,
-        body: req.body,
-      })
-    );
-  }
-});
+  })
+);
 
 module.exports = router;
