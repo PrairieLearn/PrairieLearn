2f40a7dda78146c897de53ba19c861a7c5af412d
Nathan Walters <nwalters512@gmail.com>
2022-01-27 13:52:01 -0800

    Enhance tool

--- tools/validate-links.mjs
+++ tools/validate-links.mjs
@@ -4,6 +4,15 @@ import glob from 'fast-glob';
 const URL_REGEX =
   /github\.com\/PrairieLearn\/PrairieLearn\/(?:tree|blob)\/master\/([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
 
+function filePathFromUrl(url) {
+  let path = url.match(URL_REGEX)[1];
+
+  // Some URLs include line numbers at the end - strip them off
+  path = path.replace(/#L\d+(-L\d+)?$/, '');
+
+  return path;
+}
+
 const files = await glob('**/*.{md,html}', { ignore: ['node_modules/**'] });
 
 let error = false;
@@ -18,12 +27,8 @@ for (const file of files) {
     // console.log(file);
     // console.log(matches);
     for (const match of matches) {
-      // console.log(`- ${match}`);
       // Extract the file path portion of the URL
-      // let pathMatch = URL_REGEX.exec(match);
-      // console.log(pathMatch);
-      const filePath = match.match(URL_REGEX)[1];
-      // console.log(`path: ${filePath}`);
+      const filePath = filePathFromUrl(match);
 
       // Check that the file exists in the repo
       if (!(await fs.pathExists(filePath))) {
@@ -31,9 +36,6 @@ for (const file of files) {
         console.error(`${file}: ${filePath} does not exist in the repo`);
       }
     }
-    // console.log(file);
-    // console.log(matches);
-    // console.log(matches.groups);
   }
 }
 
