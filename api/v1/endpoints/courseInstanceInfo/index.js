4560f13dc9c7f2243a94e0c6642cac02624b26be
Nathan Walters <nwalters512@gmail.com>
2022-01-28 15:11:11 -0800

    Reformat with Prettier

--- api/v1/endpoints/courseInstanceInfo/index.js
+++ api/v1/endpoints/courseInstanceInfo/index.js
@@ -2,7 +2,7 @@ const ERR = require('async-stacktrace');
 const path = require('path');
 const express = require('express');
 const router = express.Router({
-    mergeParams: true,
+  mergeParams: true,
 });
 
 const sqldb = require('../../../../prairielib/lib/sql-db');
@@ -11,13 +11,13 @@ const sqlLoader = require('../../../../prairielib/lib/sql-loader');
 const sql = sqlLoader.load(path.join(__dirname, '..', 'queries.sql'));
 
 router.get('/', (req, res, next) => {
-    const params = {
-        course_instance_id: res.locals.course_instance.id,
-    };
-    sqldb.queryOneRow(sql.select_course_instance_info, params, (err, result) => {
-        if (ERR(err, next)) return;
-        res.status(200).send(result.rows[0].item);
-    });
+  const params = {
+    course_instance_id: res.locals.course_instance.id,
+  };
+  sqldb.queryOneRow(sql.select_course_instance_info, params, (err, result) => {
+    if (ERR(err, next)) return;
+    res.status(200).send(result.rows[0].item);
+  });
 });
 
 module.exports = router;
