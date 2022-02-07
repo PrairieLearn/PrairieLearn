934b9d44cb5cea374e224aa7d1dd2f149b9255de
Jonatan Schroeder <jonatan@cs.ubc.ca>
2022-02-07 14:56:17 -0500

    Formatting changes
    Co-authored-by: Nathan Walters <nathan@prairielearn.com>

--- sprocs/variants_select.sql
+++ sprocs/variants_select.sql
@@ -22,11 +22,14 @@ BEGIN
         LEFT JOIN instance_questions AS iq ON (iq.id = v.instance_question_id)
         LEFT JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
         LEFT JOIN assessments AS a ON (a.id = ai.assessment_id)
-    WHERE v.id = variants_select.variant_id
-          AND v.question_id = variants_select.question_id
-          -- instance_question_id is null for question preview, so allow any variant of the question
-          AND (variants_select.instance_question_id IS NULL
-               OR v.instance_question_id = variants_select.instance_question_id);
+    WHERE
+        v.id = variants_select.variant_id
+        AND v.question_id = variants_select.question_id
+        -- instance_question_id is null for question preview, so allow any variant of the question
+        AND (
+            variants_select.instance_question_id IS NULL
+            OR v.instance_question_id = variants_select.instance_question_id
+        );
 
     IF NOT FOUND THEN RAISE EXCEPTION 'no such variant_id for this question: %', variant_id; END IF;
 
