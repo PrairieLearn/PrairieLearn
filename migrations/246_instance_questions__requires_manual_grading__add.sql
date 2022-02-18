374506e47559dc57502cad266b6864f43b03ef50
Jonatan Schroeder <jonatan@yorku.ca>
2022-02-18 17:37:07 -0500

    Foreign key for assigned grader

--- migrations/246_instance_questions__requires_manual_grading__add.sql
+++ migrations/246_instance_questions__requires_manual_grading__add.sql
@@ -1,5 +1,6 @@
 ALTER TABLE instance_questions ADD COLUMN IF NOT EXISTS requires_manual_grading BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE instance_questions ADD COLUMN IF NOT EXISTS assigned_grader BIGINT;
+ALTER TABLE instance_questions ADD FOREIGN KEY (assigned_grader) REFERENCES users ON DELETE SET NULL ON UPDATE CASCADE;
 
 WITH ranked_submissions AS (
     SELECT
