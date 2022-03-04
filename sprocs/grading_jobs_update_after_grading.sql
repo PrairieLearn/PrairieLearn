1b0c39ce595c14edbec114689687961931deeb16
Nathan Walters <nwalters512@gmail.com>
2022-03-04 12:41:11 -0800

    Check for newer submissions

--- sprocs/grading_jobs_update_after_grading.sql
+++ sprocs/grading_jobs_update_after_grading.sql
@@ -21,6 +21,8 @@ AS $$
 DECLARE
     grading_job grading_jobs%rowtype;
     credit integer;
+    submission_id bigint;
+    submission_date timestamptz;
     variant_id bigint;
     instance_question_id bigint;
     assessment_instance_id bigint;
@@ -38,12 +40,16 @@ BEGIN
     -- we must have a variant, but we might not have an assessment_instance
     SELECT
         s.credit,
+        s.id,
+        s.date,
         v.id,
         q.grading_method,
         iq.id,
         ai.id
     INTO
         credit,
+        submission_id,
+        submission_date,
         variant_id,
         grading_method,
         instance_question_id,
@@ -70,6 +76,25 @@ BEGIN
     -- example. This is not involved in re-grading because we will
     -- make a separate grading_job for re-grades.
     IF grading_job.graded_at IS NOT NULL THEN RETURN; END IF;
+
+    -- Bail out if there's a newer submission that we performed any grading on.
+    -- This only applies to student questions - that is, where there's an
+    -- associated instance question. This prevents a race condition where we
+    -- grade submissions in a different order than how they were saved.
+    -- This does not impact instructors since there's no notion of an assessment
+    -- to grade.
+    SELECT 1
+    FROM
+        instance_questions AS iq
+        JOIN variants AS v ON (v.instance_question_id = iq.id)
+        JOIN submissions AS s ON (s.variant_id = v.id)
+    WHERE
+        iq.id = instance_question_id
+        AND s.id != submission_id
+        AND s.date > submission_date
+        AND s.grading_requested_at IS NOT NULL;
+
+    IF FOUND THEN RETURN; END IF;
 
     -- ######################################################################
     -- store the grading information
@@ -119,7 +144,7 @@ BEGIN
     INTO grading_job;
 
     IF new_gradable = FALSE THEN
-        IF assessment_instance_id IS NOT NULL THEN
+        IF instance_question_id IS NOT NULL THEN
             UPDATE instance_questions
             SET status = 'invalid'::enum_instance_question_status
             WHERE id = instance_question_id;
@@ -129,7 +154,7 @@ BEGIN
         -- update all parent objects
 
         PERFORM variants_update_after_grading(variant_id, grading_job.correct);
-        IF assessment_instance_id IS NOT NULL THEN
+        IF instance_question_id IS NOT NULL THEN
            PERFORM instance_questions_grade(instance_question_id, grading_job.score, grading_job.id, grading_job.auth_user_id);
            PERFORM assessment_instances_grade(assessment_instance_id, grading_job.auth_user_id, credit);
         END IF;
