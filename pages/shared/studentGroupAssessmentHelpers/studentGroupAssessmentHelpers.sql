619d10cfd45d9ca81b616f67ecf6a465fbe74d9b
liiabutler <liiabiia@yahoo.com>
2022-03-06 16:03:44 -0600

    remove unused sql blocks

--- pages/shared/studentGroupAssessmentHelpers/studentGroupAssessmentHelpers.sql
+++ pages/shared/studentGroupAssessmentHelpers/studentGroupAssessmentHelpers.sql
@@ -1,27 +1,3 @@
--- BLOCK select_single_assessment_instance
-SELECT
-    ai.*
-FROM
-    assessment_instances AS ai
-    LEFT JOIN groups AS g ON (g.id = ai.group_id AND g.deleted_at IS NULL)
-    LEFT JOIN group_users AS gu ON (gu.group_id = g.id)
-WHERE
-    ai.assessment_id = $assessment_id
-    AND ai.number = 1
-    AND ((gu.user_id = $user_id) OR (ai.user_id = $user_id));
-
--- BLOCK find_assessment_instance
-SELECT
-    ai.*
-FROM
-    assessment_instances AS ai
-    LEFT JOIN groups AS g ON (g.id = ai.group_id AND g.deleted_at IS NULL)
-    LEFT JOIN group_users AS gu ON (gu.group_id = g.id)
-WHERE
-    ai.assessment_id = $assessment_id
-    AND ai.number = (SELECT max(number) FROM assessment_instances)
-    AND ((gu.user_id = $user_id) OR (ai.user_id = $user_id))
-
 -- BLOCK get_config_info
 SELECT
     gc.*
