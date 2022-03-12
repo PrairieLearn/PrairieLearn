4e1bacab1ca53e0ee94f9dfdb38402468826f7ef
Nathan Walters <nwalters512@gmail.com>
2022-03-11 16:56:35 -0800

    Implement grading retry logic

--- lib/assessment.sql
+++ lib/assessment.sql
@@ -50,3 +50,9 @@ WHERE
     a.id = $assessment_id
     AND ai.open;
 
+-- BLOCK unset_grading_needed
+UPDATE assessment_instances AS ai
+SET
+    grading_needed = TRUE
+WHERE
+    ai.id = $assessment_instance_id;
