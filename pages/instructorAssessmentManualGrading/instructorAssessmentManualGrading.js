1c4764999eeae3302bd3c8548396c5ea2143cd07
Jonatan Schroeder <jonatan@yorku.ca>
2022-03-16 15:22:08 -0400

    Promisify assessment manual grading pages

--- pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.js
+++ pages/instructorAssessmentManualGrading/instructorAssessmentManualGrading.js
@@ -1,6 +1,7 @@
 const ERR = require('async-stacktrace');
 const express = require('express');
 const router = express.Router();
+const asyncHandler = require('express-async-handler');
 const path = require('path');
 const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
 const sqlDb = require('../../prairielib/lib/sql-db');
@@ -8,19 +9,20 @@ const sqlLoader = require('../../prairielib/lib/sql-loader');
 
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
-router.get('/', function (req, res, next) {
-  debug('GET /');
-  var params = {
-    assessment_id: res.locals.assessment.id,
-    user_id: res.locals.authz_data.user.user_id,
-  };
-  sqlDb.query(sql.select_questions_manual_grading, params, function (err, result) {
-    if (ERR(err, next)) return;
+router.get(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    debug('GET /');
+    var params = {
+      assessment_id: res.locals.assessment.id,
+      user_id: res.locals.authz_data.user.user_id,
+    };
+    const result = await sqlDb.queryAsync(sql.select_questions_manual_grading, params);
     res.locals.questions = result.rows;
     res.locals.num_open_instances = result.rows[0]?.num_open_instances || 0;
     debug('render page');
     res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
-  });
-});
+  })
+);
 
 module.exports = router;
--- pages/instructorAssessmentQuestionManualGrading/instructorAssessmentQuestionManualGrading.js
+++ pages/instructorAssessmentQuestionManualGrading/instructorAssessmentQuestionManualGrading.js
@@ -1,6 +1,8 @@
 const ERR = require('async-stacktrace');
 const express = require('express');
 const router = express.Router();
+const asyncHandler = require('express-async-handler');
+const util = require('util');
 const path = require('path');
 const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
 const error = require('../../prairielib/lib/error');
@@ -11,109 +13,103 @@ const ltiOutcomes = require('../../lib/ltiOutcomes');
 
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
-router.get('/', function (req, res, next) {
-  debug('GET /');
-  var params = {
-    assessment_id: res.locals.assessment.id,
-    assessment_question_id: res.locals.assessment_question_id,
-  };
-  sqlDb.queryOneRow(sql.select_question, params, (err, result) => {
-    if (ERR(err, next)) return;
-
+router.get(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    debug('GET /');
+    var params = {
+      assessment_id: res.locals.assessment.id,
+      assessment_question_id: res.locals.assessment_question_id,
+    };
+    const result = await sqlDb.queryOneRowAsync(sql.select_question, params);
     Object.assign(res.locals, result.rows[0]);
 
     res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
-  });
-});
+  })
+);
 
-router.get('/instances.json', function (req, res, next) {
-  debug('GET /instances.json');
+router.get(
+  '/instances.json',
+  asyncHandler(async (req, res, next) => {
+    debug('GET /instances.json');
 
-  var params = {
-    assessment_id: res.locals.assessment.id,
-    assessment_question_id: res.locals.assessment_question_id,
-  };
+    var params = {
+      assessment_id: res.locals.assessment.id,
+      assessment_question_id: res.locals.assessment_question_id,
+    };
 
-  sqlDb.query(sql.select_instance_questions_manual_grading, params, function (err, result) {
-    if (ERR(err, next)) return;
+    const result = await sqlDb.queryAsync(sql.select_instance_questions_manual_grading, params);
     result.rows.forEach((row) => {
       // bootstrap-table does not like nulls as filter targets, set to 0 instead
       Object.assign(row, { assigned_grader: row.assigned_grader || 0 });
     });
     res.send({ instance_questions: result.rows });
-  });
-});
+  })
+);
 
-router.post('/', (req, res, next) => {
-  if (!res.locals.authz_data.has_course_instance_permission_edit) return next();
-  if (req.body.__action === 'batch_action') {
-    const action_data = JSON.parse(req.body.batch_action_data) || {};
-    const instance_question_ids = Array.isArray(req.body.instance_question_id)
-      ? req.body.instance_question_id
-      : [req.body.instance_question_id];
-    const params = {
-      assessment_id: res.locals.assessment.id,
-      assessment_question_id: res.locals.assessment_question_id,
-      instance_question_ids,
-      requires_manual_grading: !!action_data?.requires_manual_grading,
-      assigned_grader: action_data?.assigned_grader,
-    };
-    sqlDb.query(sql.update_instance_questions, params, function (err, _result) {
-      if (ERR(err, next)) return;
+router.post(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    if (!res.locals.authz_data.has_course_instance_permission_edit) return next();
+    if (req.body.__action === 'batch_action') {
+      const action_data = JSON.parse(req.body.batch_action_data) || {};
+      const instance_question_ids = Array.isArray(req.body.instance_question_id)
+        ? req.body.instance_question_id
+        : [req.body.instance_question_id];
+      const params = {
+        assessment_id: res.locals.assessment.id,
+        assessment_question_id: res.locals.assessment_question_id,
+        instance_question_ids,
+        requires_manual_grading: !!action_data?.requires_manual_grading,
+        assigned_grader: action_data?.assigned_grader,
+      };
+      await sqlDb.queryAsync(sql.update_instance_questions, params);
       res.send({});
-    });
-  } else if (req.body.__action === 'edit_question_points') {
-    const params = [
-      res.locals.assessment_id,
-      req.body.assessment_instance_id,
-      null, // submission_id
-      req.body.instance_question_id,
-      null, // uid
-      null, // assessment_instance_number
-      null, // qid
-      null, // score_perc
-      req.body.points,
-      null, // feedback
-      null, // partial_scores
-      res.locals.authn_user.user_id,
-    ];
-    sqlDb.call('instance_questions_update_score', params, (err, _result) => {
-      if (ERR(err, next)) return;
-      ltiOutcomes.updateScore(req.body.assessment_instance_id, (err) => {
-        if (ERR(err, next)) return;
-        res.send({});
-      });
-    });
-  } else if (req.body.__action === 'edit_question_score_perc') {
-    const params = [
-      res.locals.assessment_id,
-      req.body.assessment_instance_id,
-      null, // submission_id
-      req.body.instance_question_id,
-      null, // uid
-      null, // assessment_instance_number
-      null, // qid
-      req.body.score_perc,
-      null, // points
-      null, // feedback
-      null, // partial_scores
-      res.locals.authn_user.user_id,
-    ];
-    sqlDb.call('instance_questions_update_score', params, (err, _result) => {
-      if (ERR(err, next)) return;
-      ltiOutcomes.updateScore(req.body.assessment_instance_id, (err) => {
-        if (ERR(err, next)) return;
-        res.send({});
-      });
-    });
-  } else {
-    return next(
-      error.make(400, 'unknown __action', {
-        locals: res.locals,
-        body: req.body,
-      })
-    );
-  }
-});
+    } else if (req.body.__action === 'edit_question_points') {
+      const params = [
+        res.locals.assessment_id,
+        req.body.assessment_instance_id,
+        null, // submission_id
+        req.body.instance_question_id,
+        null, // uid
+        null, // assessment_instance_number
+        null, // qid
+        null, // score_perc
+        req.body.points,
+        null, // feedback
+        null, // partial_scores
+        res.locals.authn_user.user_id,
+      ];
+      await sqlDb.callAsync('instance_questions_update_score', params);
+      await util.promisify(ltiOutcomes.updateScore)(req.body.assessment_instance_id);
+      res.send({});
+    } else if (req.body.__action === 'edit_question_score_perc') {
+      const params = [
+        res.locals.assessment_id,
+        req.body.assessment_instance_id,
+        null, // submission_id
+        req.body.instance_question_id,
+        null, // uid
+        null, // assessment_instance_number
+        null, // qid
+        req.body.score_perc,
+        null, // points
+        null, // feedback
+        null, // partial_scores
+        res.locals.authn_user.user_id,
+      ];
+      await sqlDb.callAsync('instance_questions_update_score', params);
+      await util.promisify(ltiOutcomes.updateScore)(req.body.assessment_instance_id);
+      res.send({});
+    } else {
+      return next(
+        error.make(400, 'unknown __action', {
+          locals: res.locals,
+          body: req.body,
+        })
+      );
+    }
+  })
+);
 
 module.exports = router;
