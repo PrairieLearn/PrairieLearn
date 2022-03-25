001c11dcfe61d07c3eac95f5be25bd841c18f9f3
Jonatan Schroeder <jonatan@yorku.ca>
2022-03-25 17:30:45 -0400

    Update migration to account for second run on prod servers

--- migrations/252_instance_questions__requires_manual_grading__update.sql
+++ migrations/252_instance_questions__requires_manual_grading__update.sql
@@ -1,3 +1,16 @@
+-- This migration should ideally be run as close as possible to when
+-- the code is deployed in production.
+
+-- Any submissions for manually graded questions submitted after this
+-- migration runs and before the code is updated (particularly sproc
+-- submissions_insert) will not be tagged for manual grading when it
+-- should.
+
+-- Any submissions graded after this migration runs and before the
+-- code is updated (particularly sproc
+-- instance_questions_update_score) will still be tagged for manual
+-- grading when it shouldn't.
+
 WITH ranked_submissions AS (
     SELECT
         v.instance_question_id,
@@ -9,7 +22,7 @@ WITH ranked_submissions AS (
 )
 UPDATE instance_questions AS iq
 SET
-    requires_manual_grading = TRUE
+    requires_manual_grading = (rs.graded_at IS NULL)
 FROM
     assessment_questions AS aq
     JOIN questions AS q ON (q.id = aq.question_id)
@@ -19,4 +32,4 @@ WHERE
     AND q.grading_method = 'Manual'
     AND rs.instance_question_id = iq.id
     AND rs.submission_row = 1
-    AND rs.graded_at IS NULL;
+    AND (iq.requires_manual_grading OR rs.graded_at IS NULL);
