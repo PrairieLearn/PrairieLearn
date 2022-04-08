ad110247f46e5d0ef55ea787c188ac6744aa429f
Jonatan Schroeder <jonatan@yorku.ca>
2022-04-08 17:21:02 -0400

    Camel case in function and parameter names

--- pages/instructorAssessmentAccess/instructorAssessmentAccess.js
+++ pages/instructorAssessmentAccess/instructorAssessmentAccess.js
@@ -11,14 +11,14 @@ const sqlLoader = require('../../prairielib/lib/sql-loader');
 
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
-let compare_date = function (old_date, new_date, old_is_null, new_is_null, both_are_null) {
-  if (old_date === null) return new_date === null ? both_are_null : old_is_null;
-  else if (new_date === null) return new_is_null;
-  else return old_date - new_date;
+let compareDate = function (oldDate, newDate, oldIsNull, newIsNull, bothAreNull) {
+  if (oldDate === null) return newDate === null ? bothAreNull : oldIsNull;
+  else if (newDate === null) return newIsNull;
+  else return oldDate - newDate;
 };
 
-let apply_rule = function (list, formal_rule) {
-  let new_rule = Object.assign({}, formal_rule);
+let applyRule = function (list, originalRule) {
+  let new_rule = { ...originalRule };
   let valid = true;
   new_rule.valid_now_public = new_rule.valid_now && (new_rule.mode_raw ?? 'Public') === 'Public';
   new_rule.valid_now_exam = new_rule.valid_now && (new_rule.mode_raw ?? 'Exam') === 'Exam';
@@ -28,14 +28,14 @@ let apply_rule = function (list, formal_rule) {
     if (old_rule.mode_raw !== null && old_rule.mode_raw !== new_rule.mode_raw) {
       if (new_rule.mode_raw === null) {
         // New rule is NULL, old rule is not NULL, so split new rule.
-        let new_rule_public = Object.assign({}, new_rule);
+        let new_rule_public = { ...new_rule };
         new_rule_public.mode_raw = 'Public';
         new_rule_public.mode = 'Public';
-        apply_rule(list, new_rule_public);
-        let new_rule_exam = Object.assign({}, new_rule);
+        applyRule(list, new_rule_public);
+        let new_rule_exam = { ...new_rule };
         new_rule_exam.mode_raw = 'Exam';
         new_rule_exam.mode = 'Exam';
-        apply_rule(list, new_rule_exam);
+        applyRule(list, new_rule_exam);
         valid = false;
       }
       return;
@@ -54,30 +54,30 @@ let apply_rule = function (list, formal_rule) {
     if (old_rule.exam_uuid !== new_rule.exam_uuid) return;
 
     if (
-      compare_date(new_rule.start_date_raw, old_rule.start_date_raw, -1, +1, 0) >= 0 &&
-      compare_date(new_rule.end_date_raw, old_rule.end_date_raw, +1, -1, 0) <= 0
+      compareDate(new_rule.start_date_raw, old_rule.start_date_raw, -1, +1, 0) >= 0 &&
+      compareDate(new_rule.end_date_raw, old_rule.end_date_raw, +1, -1, 0) <= 0
     ) {
       valid = false;
     } else if (
-      compare_date(new_rule.start_date_raw, old_rule.start_date_raw, -1, +1, 0) >= 0 &&
-      compare_date(new_rule.start_date_raw, old_rule.end_date_raw, -1, -1, -1) <= 0
+      compareDate(new_rule.start_date_raw, old_rule.start_date_raw, -1, +1, 0) >= 0 &&
+      compareDate(new_rule.start_date_raw, old_rule.end_date_raw, -1, -1, -1) <= 0
     ) {
       new_rule.start_date_raw = old_rule.end_date_plus_one_raw;
       new_rule.start_date = old_rule.end_date_plus_one;
     } else if (
-      compare_date(new_rule.end_date_raw, old_rule.start_date_raw, +1, +1, +1) >= 0 &&
-      compare_date(new_rule.end_date_raw, old_rule.end_date_raw, +1, -1, 0) <= 0
+      compareDate(new_rule.end_date_raw, old_rule.start_date_raw, +1, +1, +1) >= 0 &&
+      compareDate(new_rule.end_date_raw, old_rule.end_date_raw, +1, -1, 0) <= 0
     ) {
       new_rule.end_date_raw = old_rule.start_date_minus_one_raw;
       new_rule.end_date = old_rule.start_date_minus_one;
     } else if (
-      compare_date(new_rule.start_date_raw, old_rule.start_date_raw, -1, +1, 0) < 0 &&
-      compare_date(new_rule.end_date_raw, old_rule.end_date_raw, +1, -1, 0) > 0
+      compareDate(new_rule.start_date_raw, old_rule.start_date_raw, -1, +1, 0) < 0 &&
+      compareDate(new_rule.end_date_raw, old_rule.end_date_raw, +1, -1, 0) > 0
     ) {
-      let rule_before = Object.assign({}, new_rule);
+      let rule_before = { ...new_rule };
       rule_before.end_date_raw = old_rule.start_date_minus_one_raw;
       rule_before.end_date = old_rule.start_date_minus_one;
-      apply_rule(list, rule_before);
+      applyRule(list, rule_before);
 
       new_rule.start_date_raw = old_rule.end_date_plus_one_raw;
       new_rule.start_date = old_rule.end_date_plus_one;
@@ -129,7 +129,7 @@ router.get(
 
     res.locals.access_rules.forEach((formal) => {
       if (formal.uids_raw === null) {
-        apply_rule(student_rules, formal);
+        applyRule(student_rules, formal);
       }
 
       user_spec_rules.forEach((set) => {
@@ -137,7 +137,7 @@ router.get(
           formal.uids_raw === null ||
           set.uids.filter((uid) => formal.uids_raw.includes(uid)).length
         ) {
-          apply_rule(set.rules, formal);
+          applyRule(set.rules, formal);
         }
       });
     });
