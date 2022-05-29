0a44846e92d2309ecaa42dedd01155857f0c3926
Matthew West <mwest@illinois.edu>
2022-05-28 20:56:09 -0500

    Split out new index to separate migration

--- migrations/255_workspaces__durations__add.sql
+++ migrations/255_workspaces__durations__add.sql
@@ -1,3 +1,2 @@
 ALTER TABLE workspaces ADD COLUMN launching_duration INTERVAL DEFAULT '0 seconds';
 ALTER TABLE workspaces ADD COLUMN running_duration INTERVAL DEFAULT '0 seconds';
-CREATE INDEX workspaces_created_at_key ON workspaces (created_at);
