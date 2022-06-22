c89d96a014362ddc96b17ea763e73a5f12ba2bd5
Jonatan Schroeder <jonatan@yorku.ca>
2022-06-22 08:55:42 -0400

    Consistency: keep workspace-host last
    Co-authored-by: Nathan Walters <nathan@prairielearn.com>

--- docker/init.sh
+++ docker/init.sh
@@ -5,5 +5,5 @@ if [[ $NODEMON == "true" ]]; then
     # start-nodemon is listed first so it can use standard input
     make -s -C /PrairieLearn -j 2 start-nodemon start-workspace-host
 else
-    make -s -C /PrairieLearn -j 2 start-workspace-host start
+    make -s -C /PrairieLearn -j 2 start start-workspace-host
 fi
