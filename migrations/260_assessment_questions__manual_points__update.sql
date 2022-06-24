5ba09189101d486badf0f180503836bcabb64fdb
Jonatan Schroeder <jonatan@yorku.ca>
2022-06-24 09:28:12 -0400

    Ensure points_list contains at least one value for exams (may be overturned)

--- migrations/260_assessment_questions__manual_points__update.sql
+++ migrations/260_assessment_questions__manual_points__update.sql
@@ -2,12 +2,14 @@
 
 UPDATE assessment_questions AS aq
 SET
-    manual_points = CASE WHEN q.grading_method = 'Manual' THEN max_points ELSE 0 END,
-    max_auto_points = CASE WHEN q.grading_method = 'Manual' THEN 0 ELSE max_points END,
-    init_points = CASE WHEN q.grading_method = 'Manual' THEN 0 ELSE init_points END,
-    points_list = CASE WHEN q.grading_method = 'Manual' THEN NULL ELSE points_list END
+    manual_points = CASE WHEN q.grading_method = 'Manual' THEN aq.max_points ELSE 0 END,
+    max_auto_points = CASE WHEN q.grading_method = 'Manual' THEN 0 ELSE aq.max_points END,
+    init_points = CASE WHEN q.grading_method = 'Manual' THEN 0 ELSE aq.init_points END,
+    points_list = CASE WHEN q.grading_method = 'Manual' AND a.type = 'Exam' THEN '{0}'::double precision[] ELSE aq.points_list END
 FROM
-    questions AS q
+    questions AS q,
+    assessments AS a
 WHERE
     aq.manual_points IS NULL
+    AND a.id = aq.assessment_id
     AND q.id = aq.question_id;
