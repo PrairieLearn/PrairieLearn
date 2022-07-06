42337c28b5984453a8b105f7aca5803aa9dc81ee
Nathan Walters <nwalters512@gmail.com>
2022-07-06 16:56:46 -0700

    Emit Node metrics to Cloudwatch

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ const { LocalCache } = require('./lib/local-cache');
 const workers = require('./lib/workers');
 const assets = require('./lib/assets');
 const namedLocks = require('./lib/named-locks');
+const nodeMetrics = require('./lib/node-metrics');
 
 process.on('warning', (e) => console.warn(e));
 
@@ -1917,6 +1918,10 @@ if (config.startServer) {
           if (ERR(err, callback)) return;
           callback(null);
         });
+      },
+      function (callback) {
+        nodeMetrics.init();
+        callback(null);
       },
       function (callback) {
         freeformServer.init(function (err) {
