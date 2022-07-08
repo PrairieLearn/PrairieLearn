1e6c78726b637cee3eb4225f305df8c0e68f4e05
Nathan Walters <nwalters512@gmail.com>
2022-07-08 14:57:34 -0700

    Wire up native bindings

--- packages/bind-mount/index.js
+++ packages/bind-mount/index.js
@@ -1,4 +1,35 @@
-const addon = require('bindings')('addon');
+try {
+  const addon = require('bindings')('addon');
 
-module.exports.mount = addon.Mount;
-module.exports.unmount = addon.Unmount;
+  module.exports.mount = (source, target) => {
+    return new Promise((resolve, reject) => {
+      addon.Mount(source, target, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  };
+
+  module.exports.unmount = (target) => {
+    return new Promise((resolve, reject) => {
+      addon.Unmount(target, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  };
+} catch (err) {
+  module.exports.mount = () => {
+    throw new Error('Failed to load native bindings');
+  };
+
+  module.exports.mount = () => {
+    throw new Error('Failed to load native bindings');
+  };
+}
