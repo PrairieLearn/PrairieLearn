bddd30794ba7bc2977bb39d85ba239025fbea8aa
Nathan Walters <nwalters512@gmail.com>
2022-07-08 14:32:38 -0700

    Reformat addon

--- packages/bind-mount/addon.cc
+++ packages/bind-mount/addon.cc
@@ -15,60 +15,55 @@ struct MountContext {
 };
 
 class BindMountWorker : public Nan::AsyncWorker {
-  public:
-    BindMountWorker(Nan::Callback *callback, MountContext *command)
-      : Nan::AsyncWorker(callback, "PrairieLearn:BindMountWorker"), command(command) {}
-    ~BindMountWorker() {}
+public:
+  BindMountWorker(Nan::Callback *callback, MountContext *command): Nan::AsyncWorker(callback, "PrairieLearn:BindMountWorker"), command(command) {}
+  ~BindMountWorker() {}
 
-    void Execute() {
-      switch (command->command) {
-        case Command::MountCommand: {
-          this->command->error = mount(command->source.c_str(), command->target.c_str(), NULL, MS_BIND, NULL);
-          break;
-        }
-        case Command::UnmountCommand: {
-          this->command->error = umount(this->command->target.c_str());
-          break;
-        }
-      }
+  void Execute() {
+    switch (command->command) {
+    case Command::MountCommand:
+      this->command->error = mount(command->source.c_str(), command->target.c_str(), NULL, MS_BIND, NULL);
+      break;
+    case Command::UnmountCommand:
+      this->command->error = umount(this->command->target.c_str());
+      break;
+    }
+  }
+
+  void HandleOKCallback() {
+    Nan::HandleScope scope;
+
+    v8::Local<v8::Value> argv[] = {Nan::Null()};
+
+    if (this->command->error > 0) {
+      const char *syscall = this->command->command == Command::MountCommand ? "mount" : "umount";
+      argv[0] = Nan::ErrnoException(this->command->error, syscall, "", this->command->target.c_str());
     }
 
-    void HandleOKCallback() {
-      Nan::HandleScope scope;
+    this->callback->Call(2, argv, this->async_resource);
 
-      v8::Local<v8::Value> argv[] = {
-        Nan::Null()
-      };
+    delete this->command;
+  }
 
-      if (this->command->error > 0) {
-        const char *syscall = this->command->command == Command::MountCommand ? "mount" : "umount";
-        argv[0] = Nan::ErrnoException(this->command->error, syscall, "", this->command->target.c_str());
-      }
-
-      this->callback->Call(2, argv, this->async_resource);
-
-      delete this->command;
-    }
-
-  private:
-    MountContext *command;
+private:
+  MountContext *command;
 };
 
-void Mount(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  #ifndef __linux__
+void Mount(const Nan::FunctionCallbackInfo<v8::Value> &info) {
+#ifndef __linux__
 
   Nan::ThrowError("bind-mount is only supported on Linux");
   return;
 
-  #else
+#else
 
   if (info.Length() != 3) {
     Nan::ThrowError("Requires three arguments");
     return;
   }
 
-  const char* source = *Nan::Utf8String(info[0]);
-  const char* target = *Nan::Utf8String(info[1]);
+  const char *source = *Nan::Utf8String(info[0]);
+  const char *target = *Nan::Utf8String(info[1]);
 
   MountContext *command = new MountContext();
   command->command = Command::MountCommand;
@@ -78,16 +73,16 @@ void Mount(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   Nan::Callback *callback = new Nan::Callback(Nan::To<v8::Function>(info[2]).ToLocalChecked());
   Nan::AsyncQueueWorker(new BindMountWorker(callback, command));
 
-  #endif
+#endif
 }
 
-void Unmount(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  #ifndef __linux__
+void Unmount(const Nan::FunctionCallbackInfo<v8::Value> &info) {
+#ifndef __linux__
 
   Nan::ThrowError("bind-mount is only supported on Linux");
   return;
 
-  #else
+#else
 
   if (info.Length() != 1) {
     Nan::ThrowError("Requires one argument");
@@ -106,7 +101,7 @@ void Unmount(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   Nan::Callback *callback = new Nan::Callback(Nan::To<v8::Function>(info[1]).ToLocalChecked());
   Nan::AsyncQueueWorker(new BindMountWorker(callback, command));
 
-  #endif
+#endif
 }
 
 NAN_MODULE_INIT(Initialize) {
