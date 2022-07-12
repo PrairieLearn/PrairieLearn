920acaf19a48150c9af62ae6d994854588114b49
Matthew West <matt@prairielearn.com>
2022-07-12 17:07:47 -0500

    Upgrade to NodeJS v16 (from v14) (#5994)
    * Upgrade to NodeJS v16 (from v14)
    
    * Add "engines" spec in package.json
    
    * Load new image into local docker images list

--- images/plbase/plbase-install.sh
+++ images/plbase/plbase-install.sh
@@ -43,7 +43,7 @@ cd /nvm
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 source /nvm/nvm.sh
 export NVM_SYMLINK_CURRENT=true
-nvm install 14
+nvm install 16
 # PrairieLearn doesn't currently use `npm` itself, but we can't be sure that
 # someone else isn't using our base image and relying on `npm`, so we'll
 # continue to install it to avoid breaking things.
