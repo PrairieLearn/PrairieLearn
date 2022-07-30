d0c8f162ed47479efca541e1d9bf9fc4e74bd580
Renzo <renzol2@illinois.edu>
2022-07-29 19:46:35 -0500

    Prevent breaking of group assessments without roles

--- migrations/260_group_configs__using_group_roles__add.sql
+++ migrations/260_group_configs__using_group_roles__add.sql
@@ -2,3 +2,9 @@ ALTER TABLE group_configs ADD COLUMN using_group_roles boolean DEFAULT false;
 
 ALTER TABLE group_users DROP CONSTRAINT group_users_pkey;
 ALTER TABLE group_users ADD PRIMARY KEY (group_id, user_id, group_role_id);
+
+-- Insert role for group assessments that don't use group roles
+INSERT INTO group_roles 
+    (role_name, maximum)
+VALUES
+    ('No group roles', 99);
