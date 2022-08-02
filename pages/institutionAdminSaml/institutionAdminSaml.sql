dd8a2288ecaa9ba10ec5666028c86b454cc24d94
Nathan Walters <nwalters512@gmail.com>
2022-08-02 14:57:30 -0700

    Fix keypair behavior on conflicts

--- pages/institutionAdminSaml/institutionAdminSaml.sql
+++ pages/institutionAdminSaml/institutionAdminSaml.sql
@@ -24,7 +24,7 @@ SET
   sso_login_url = EXCLUDED.sso_login_url,
   issuer = EXCLUDED.issuer,
   certificate = EXCLUDED.certificate,
-  public_key = COALESCE(EXCLUDED.public_key, saml_providers.private_key),
+  public_key = COALESCE(EXCLUDED.public_key, saml_providers.public_key),
   private_key = COALESCE(EXCLUDED.private_key, saml_providers.private_key);
 
 -- BLOCK delete_institution_saml_provider
