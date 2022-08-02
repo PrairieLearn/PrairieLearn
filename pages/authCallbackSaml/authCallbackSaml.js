b8da2b0429805128da5541c7a5ec9fa652d1769c
Nathan Walters <nwalters512@gmail.com>
2022-08-01 17:03:21 -0700

    Get everything working

--- pages/authCallbackSaml/authCallbackSaml.js
+++ pages/authCallbackSaml/authCallbackSaml.js
@@ -1,17 +1,28 @@
 const asyncHandler = require('express-async-handler');
 const express = require('express');
-const router = express.Router();
+const passport = require('passport');
 
-router.get(
+const router = express.Router({ mergeParams: true });
+
+router.post(
   '/',
   asyncHandler(async (req, res) => {
-    console.log(req, res);
-    let redirUrl = res.locals.homeUrl;
-    if ('preAuthUrl' in req.cookies) {
-      redirUrl = req.cookies.preAuthUrl;
-      res.clearCookie('preAuthUrl');
-    }
-    res.redirect(redirUrl);
+    console.log('AUTH CALLBACK SAML');
+    const authData = {
+      response: res,
+      failureRedirect: '/pl',
+      session: false,
+    };
+    passport.authenticate('saml', authData)(req, res, (...args) => {
+      console.log(args);
+
+      let redirUrl = res.locals.homeUrl;
+      if ('preAuthUrl' in req.cookies) {
+        redirUrl = req.cookies.preAuthUrl;
+        res.clearCookie('preAuthUrl');
+      }
+      res.redirect(redirUrl);
+    });
   })
 );
 
