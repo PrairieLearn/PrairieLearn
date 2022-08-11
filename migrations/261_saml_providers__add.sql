a57b069b7f3f1878c7840a03638ba8c10587db0e
Nathan Walters <nwalters512@gmail.com>
2022-08-11 15:30:22 -0700

    Make saml_providers.institution_id not null

--- migrations/261_saml_providers__add.sql
+++ migrations/261_saml_providers__add.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS saml_providers (
   id BIGSERIAL PRIMARY KEY,
-  institution_id BIGINT REFERENCES institutions(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  institution_id BIGINT NOT NULL REFERENCES institutions(id) ON UPDATE CASCADE ON DELETE CASCADE,
   sso_login_url TEXT NOT NULL,
   issuer TEXT NOT NULL,
   certificate TEXT NOT NULL,
