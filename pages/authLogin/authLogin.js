92590a87c1adee0a92e184bf19037320581c8551
Nathan Walters <nwalters512@gmail.com>
2022-08-15 14:04:00 -0700

    Remove outdated comment

--- pages/authLogin/authLogin.js
+++ pages/authLogin/authLogin.js
@@ -51,11 +51,6 @@ router.get(
         .filter(Boolean);
     }
 
-    // We could set res.locals.config.hasOauth = false (or
-    // hasAzure) to not display those options inside the CBTF, but
-    // this will also need to depend on which institution we have
-    // detected (e.g., UIUC doesn't want Azure during exams, but
-    // ZJUI does want it).
     res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
   })
 );
