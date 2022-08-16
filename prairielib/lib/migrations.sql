b2112cfad4d66c8241ccff80941252f1ff048a68
Nathan Walters <nathan@prairielearn.com>
2022-08-16 14:10:41 -0700

    Rework migrations to use timestamps instead of indexes (#6129)
    * Start work on migration rewriting script
    
    * Rename all migrations
    
    * More progress on migrations
    
    * Work on migration logic
    
    * Work on tests
    
    * Add tests
    
    * Make everything work
    
    * Fix up tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Matthew West <matt@prairielearn.com>
    
    * Use O(n) algorithm to determine which migrations to execute
    
    * Remove unused import
    
    * Add README.md to migrations directory
    
    * Fix TypeScript build
    
    * Ensure all migrations have timestamps
    
    * Update prairielib/lib/migrations.js
    
    * Use correct sqldb functions
    
    * Format with Prettier
    
    * Update database description
    
    * Fix property access
    
    * Fix tests
    
    Co-authored-by: Matthew West <matt@prairielearn.com>

--- prairielib/lib/migrations.sql
+++ prairielib/lib/migrations.sql
@@ -8,11 +8,25 @@ CREATE TABLE IF NOT EXISTS migrations (
   UNIQUE(project, index)
 );
 
--- BLOCK alter_migrations_table
+-- BLOCK add_projects_column
 ALTER TABLE migrations ADD COLUMN IF NOT EXISTS project TEXT DEFAULT 'prairielearn';
 CREATE UNIQUE INDEX IF NOT EXISTS migrations_index_project_key ON migrations (index, project);
 ALTER TABLE migrations DROP CONSTRAINT migrations_index_key;
 DROP INDEX IF EXISTS migrations_index_key;
+
+-- BLOCK add_timestamp_column
+ALTER TABLE migrations ADD COLUMN IF NOT EXISTS timestamp TEXT;
+CREATE UNIQUE INDEX IF NOT EXISTS migrations_timestamp_project_key ON migrations (timestamp, project);
+
+-- BLOCK get_migrations
+SELECT id, filename, index, timestamp FROM migrations WHERE project = $project;
+
+-- BLOCK update_migration
+UPDATE migrations
+SET
+  filename = $filename,
+  timestamp = $timestamp
+WHERE id = $id;
 
 -- BLOCK get_last_migration
 SELECT MAX(index) AS last_migration FROM migrations WHERE project = $project;
