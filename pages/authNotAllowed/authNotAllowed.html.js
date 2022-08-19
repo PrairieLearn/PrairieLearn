82f971a3f47e1d9a1c61aa17a2e979e897b3a6bb
Nathan Walters <nwalters512@gmail.com>
2022-08-19 11:26:35 -0700

    Work on data fetching

--- pages/authNotAllowed/authNotAllowed.html.js
+++ pages/authNotAllowed/authNotAllowed.html.js
@@ -1,7 +1,26 @@
 const { html } = require('@prairielearn/html');
+const { renderEjs } = require('@prairielearn/html-ejs');
 
-function AuthNotAllowed({ resLocals }) {
-  return html`Hello!`.toString();
+function AuthNotAllowed({ institutionAuthnProviders, resLocals }) {
+  console.log(institutionAuthnProviders);
+  return html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        ${renderEjs(__filename, "<%- include('../partials/head') %>", resLocals)}
+      </head>
+      <body>
+        ${renderEjs(__filename, "<%- include('../partials/navbar') %>", {
+          ...resLocals,
+          navbarType: 'unauthenticated',
+        })}
+        <div class="container">
+          <h1>Unsupported authentication method</h1>
+          <p>The authentication method you tried to use is not supported by your institution.</p>
+        </div>
+      </body>
+    </html>
+  `.toString();
 }
 
 module.exports.AuthNotAllowed = AuthNotAllowed;
--- pages/authNotAllowed/authNotAllowed.js
+++ pages/authNotAllowed/authNotAllowed.js
@@ -1,14 +1,28 @@
 const { Router } = require('express');
 const asyncHandler = require('express-async-handler');
 
+const sqldb = require('../../prairielib/sql-db');
+const sqlLoader = require('../../prairielib/lib/sql-loader');
 const { AuthNotAllowed } = require('./authNotAllowed.html');
 
+const sql = sqlLoader.loadSqlEquiv(__filename);
 const router = Router({ mergeParams: true });
 
 router.get(
   '/',
   asyncHandler(async (req, res) => {
-    res.send(AuthNotAllowed({ resLocals: res.locals }));
+    const institutionAuthnProviders = await sqldb.queryAsync(
+      sql.select_institution_authn_providers,
+      {
+        institution_id: req.params.institution_id,
+      }
+    );
+    res.send(
+      AuthNotAllowed({
+        institutionAuthnProviders: institutionAuthnProviders.rows,
+        resLocals: res.locals,
+      })
+    );
   })
 );
 
