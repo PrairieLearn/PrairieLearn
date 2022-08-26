d946fa0167ebf2ced82a92205bbbb1e88124a96e
Nathan Walters <nwalters512@gmail.com>
2022-08-25 17:26:15 -0700

    Make migration more robust

--- migrations/20220825210520_265_users__uin_unique_per_institution.sql
+++ migrations/20220825210520_265_users__uin_unique_per_institution.sql
@@ -1,2 +1,2 @@
-ALTER TABLE users ADD CONSTRAINT users_uin_institution_id_key UNIQUE (uin, institution_id);
+CREATE UNIQUE INDEX IF NOT EXISTS users_uin_institution_id_key ON users (uin, institution_id);
 ALTER TABLE users DROP CONSTRAINT IF EXISTS users_uin_key;
