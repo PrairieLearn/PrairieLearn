f6f81050b640765504c74a9c00fe75d60ac28df9
Eliot Robson <eliot.robson24@gmail.com>
2022-08-27 12:48:52 -0500

    Moved over files

--- pages/instructorAssessmentInstance/instructorAssessmentInstance.js
+++ pages/instructorAssessmentInstance/instructorAssessmentInstance.js
@@ -7,6 +7,9 @@ const error = require('../../prairielib/lib/error');
 const sqldb = require('../../prairielib/lib/sql-db');
 const sqlLoader = require('../../prairielib/lib/sql-loader');
 
+var path = require('path');
+var debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
+
 const sanitizeName = require('../../lib/sanitize-name');
 const ltiOutcomes = require('../../lib/ltiOutcomes');
 
@@ -197,6 +200,17 @@ router.post('/', (req, res, next) => {
         if (ERR(err, next)) return;
         res.redirect(req.originalUrl);
       });
+    });
+  } else if (req.body.__action == 'break_variant') {
+    var params = {
+        'instance_question_id': req.body.__instance_question_id,
+        'authn_user_id': res.locals.authn_user.user_id,
+    };
+    debug(res.locals);
+    debug('break_variant', {params});
+    sqlDb.query(sql.mark_variant_broken, params, (err, _result) => {
+        if (ERR(err, next)) return;
+        res.redirect(req.originalUrl);
     });
   } else {
     return next(
