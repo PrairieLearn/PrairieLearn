35584b2f0b6ecb027ecb4532c2b04ececb98eb24
Jonatan Schroeder <jonatan@yorku.ca>
2022-09-06 16:34:07 -0400

    Separate entity for rubric grading (for logs/jobs)

--- migrations/20220905173159__rubrics__create.sql
+++ migrations/20220905173159__rubrics__create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS rubrics (
     id BIGSERIAL PRIMARY KEY,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     starting_points DOUBLE PRECISION NOT NULL,
     max_points DOUBLE PRECISION NOT NULL,
     min_points DOUBLE PRECISION NOT NULL,
@@ -21,22 +21,33 @@ CREATE TABLE IF NOT EXISTS rubric_items (
 
 CREATE INDEX IF NOT EXISTS rubric_items_rubric_id ON rubric_items(rubric_id);
 
-CREATE TABLE IF NOT EXISTS instance_question_rubric_items (
+CREATE TABLE IF NOT EXISTS rubric_gradings (
     id BIGSERIAL PRIMARY KEY,
-    instance_question_id BIGINT NOT NULL REFERENCES instance_questions(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    manual BOOLEAN NOT NULL,
+    rubric_id BIGINT NOT NULL REFERENCES rubrics(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    computed_points DOUBLE PRECISION NOT NULL,
+    adjust_points DOUBLE PRECISION NOT NULL,
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE IF NOT EXISTS rubric_grading_items (
+    id BIGSERIAL PRIMARY KEY,
+    rubric_grading_id BIGINT NOT NULL REFERENCES rubric_gradings(id) ON DELETE CASCADE ON UPDATE CASCADE,
     rubric_item_id BIGINT NOT NULL REFERENCES rubric_items(id) ON DELETE CASCADE ON UPDATE CASCADE,
     points DOUBLE PRECISION NOT NULL,
     note TEXT,
     deleted_at TIMESTAMP WITH TIME ZONE
 );
 
-CREATE INDEX IF NOT EXISTS instance_question_rubric_items_rubric_item_id ON instance_question_rubric_items(rubric_item_id);
+-- TODO Consider other indices
 
-ALTER TABLE instance_question_rubric_items ADD CONSTRAINT instance_question_rubric_items_instance_question_id_rubric_item_id_key UNIQUE (instance_question_id, rubric_item_id);
-
-ALTER TABLE instance_questions ADD COLUMN IF NOT EXISTS manual_adjust_points DOUBLE PRECISION;
+ALTER TABLE rubric_grading_items ADD CONSTRAINT rubric_grading_items_rubric_grading_id_rubric_item_id_key UNIQUE (rubric_grading_id, rubric_item_id);
 
 ALTER TABLE assessment_questions ADD COLUMN IF NOT EXISTS manual_rubric_id BIGINT REFERENCES rubrics(id) ON DELETE SET NULL ON UPDATE CASCADE;
 ALTER TABLE assessment_questions ADD COLUMN IF NOT EXISTS auto_rubric_id BIGINT REFERENCES rubrics(id) ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE instance_questions ADD COLUMN IF NOT EXISTS manual_rubric_grading_id BIGINT REFERENCES rubric_gradings(id) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE instance_questions ADD COLUMN IF NOT EXISTS auto_rubric_grading_id BIGINT REFERENCES rubric_gradings(id) ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE grading_jobs ADD COLUMN IF NOT EXISTS manual_rubric_grading_id BIGINT REFERENCES rubric_gradings(id) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE grading_jobs ADD COLUMN IF NOT EXISTS auto_rubric_grading_id BIGINT REFERENCES rubric_gradings(id) ON DELETE SET NULL ON UPDATE CASCADE;
 
