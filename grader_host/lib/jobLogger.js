d937871a89bb3dfab57c6b1ad3371742379fb8ac
Nathan Walters <nwalters512@gmail.com>
2022-09-16 16:08:13 -0700

    Tweak error handling of S3 stream logging

--- grader_host/lib/jobLogger.js
+++ grader_host/lib/jobLogger.js
@@ -7,23 +7,21 @@ const config = require('./config').config;
 module.exports = function (options) {
   const { bucket, rootKey } = options;
 
-  const s3LoggerStream = new S3StreamLogger({
-    bucket,
-    folder: rootKey,
-    name_format: 'output.log', // No need to rotate, all logs go to same file
-    upload_every: 1000, // Most jobs are short-lived, so push every second
-    buffer_size: 100 * 1000, // 100kB - this is increased from the default 10kB
+  const s3StreamLoggerTransport = new winston.transports.Stream({
+    stream: new S3StreamLogger({
+      bucket,
+      folder: rootKey,
+      name_format: 'output.log', // No need to rotate, all logs go to same file
+      upload_every: 1000, // Most jobs are short-lived, so push every second
+      buffer_size: 100 * 1000, // 100kB - this is increased from the default 10kB
+    }),
   });
 
-  s3LoggerStream.on('error', (err) => {
+  s3StreamLoggerTransport.on('error', (err) => {
     globalLogger.error(`Error writing to S3`, err);
   });
 
-  const transports = [
-    new winston.transports.Stream({
-      stream: s3LoggerStream,
-    }),
-  ];
+  const transports = [s3StreamLoggerTransport];
 
   if (config.useConsoleLoggingForJobs) {
     transports.push(new winston.transports.Console({ timestamp: true, colorize: true }));
