51bacf0c028ca6ae62475ffac63a3897ae254fc3
Nathan Walters <nwalters512@gmail.com>
2022-09-21 17:24:07 -0700

    Lift revision code into shared module

--- middlewares/addRevisionHeader.js
+++ middlewares/addRevisionHeader.js
@@ -1,22 +1,11 @@
-const { createCache } = require('async-cache-dedupe');
-const execa = require('execa');
 const asyncHandler = require('express-async-handler');
 
-// We use `async-cache-dedupe` so that we don't end up with a bunch of concurrent
-// Git subprocesses.
-const cache = createCache({
-  ttl: 60, // seconds
-  storage: { type: 'memory' },
-});
-
-cache.define('getCurrentRevision', async () => {
-  return (await execa('git', ['rev-parse', 'HEAD'])).stdout.trim();
-});
+const { getCurrentRevision } = require('../lib/revision');
 
 module.exports = asyncHandler(async (req, res, next) => {
   try {
-    const revision = await cache.getCurrentRevision();
-    res.set('X-PrairieLearn-Revision', revision);
+    const revision = await getCurrentRevision();
+    res.set('X-PrairieLearn-Revision', revision ?? 'unknown');
   } catch {
     res.set('X-PrairieLearn-Revision', 'unknown');
   }
