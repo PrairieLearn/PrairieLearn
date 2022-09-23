f0af70c1efe8082686a84465b90399b4ec499315
Nathan Walters <nwalters512@gmail.com>
2022-09-23 11:16:09 -0700

    Replace dependency with custom memoize function

--- middlewares/addRevisionHeader.js
+++ middlewares/addRevisionHeader.js
@@ -3,12 +3,8 @@ const asyncHandler = require('express-async-handler');
 const { getCurrentRevision } = require('../lib/revision');
 
 module.exports = asyncHandler(async (req, res, next) => {
-  try {
-    const revision = await getCurrentRevision();
-    res.set('X-PrairieLearn-Revision', revision ?? 'unknown');
-  } catch {
-    res.set('X-PrairieLearn-Revision', 'unknown');
-  }
+  const revision = await getCurrentRevision();
+  res.set('X-PrairieLearn-Revision', revision ?? 'unknown');
 
   next();
 });
