f061455a60cafe7c2b51f9c2e0d4d811834e82ac
Seth Poulsen <poulsenseth@yahoo.com>
2022-10-13 15:54:20 -0500

    Add db migration for sharing sets

---
+++ migrations/20221101200859_sharing_sets__create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS sharing_sets (
+    id BIGSERIAL PRIMARY KEY,
+    course_id BIGINT NOT NULL REFERENCES pl_courses ON DELETE CASCADE ON UPDATE CASCADE,
+    description text,
+    name text,
+    number INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS question_sharing_sets (
+    id BIGSERIAL PRIMARY KEY,
+    question_id BIGINT not null,
+    sharing_set_id BIGINT not null
+);
