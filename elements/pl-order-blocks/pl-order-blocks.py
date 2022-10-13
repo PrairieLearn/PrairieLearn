0b295ca47bd4ed742457a5e5aab5600307883aa4
Seth Poulsen <poulsenseth@yahoo.com>
2022-10-13 13:30:45 -0500

    extra TODO note

--- elements/pl-order-blocks/pl-order-blocks.py
+++ elements/pl-order-blocks/pl-order-blocks.py
@@ -388,6 +388,7 @@ def parse(element_html, data):
             answer['ranking'] = search['ranking'] if search is not None else None
             answer['tag'] = search['tag'] if search is not None else None
             # TODO special code below in DAG condition should be in other conditions too, so they can also use paired distractors
+            # TODO also make sure we are safe to include the tag, instead of using `None` to denote distractors in all grading conditions
     elif grading_mode == 'dag':
         for answer in student_answer:
             search = next((item for item in correct_answers if item['inner_html'] == answer['inner_html']), None)
