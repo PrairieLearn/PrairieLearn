212ed2b9052ceaabba425f4f4decbcb3b4724b34
Seth Poulsen <poulsenseth@yahoo.com>
2022-10-18 12:11:21 -0500

    Add table to declare which courses can access which shared sets

--- migrations/20221101200859_sharing_sets__create.sql
+++ migrations/20221101200859_sharing_sets__create.sql
@@ -8,11 +8,19 @@ CREATE TABLE IF NOT EXISTS sharing_sets (
 
 CREATE TABLE IF NOT EXISTS question_sharing_sets (
     id BIGSERIAL PRIMARY KEY,
-    question_id BIGINT not null,
-    sharing_set_id BIGINT not null
+    question_id BIGINT NOT NULL,
+    sharing_set_id BIGINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS course_sharing_sets (
+    id BIGSERIAL PRIMARY KEY,
+    course_id BIGINT NOT NULL REFERENCES pl_courses ON DELETE CASCADE ON UPDATE CASCADE,
+    sharing_set_id BIGINT NOT NULL REFERENCES sharing_sets ON DELETE CASCADE ON UPDATE CASCADE,
 );
 
 ALTER TABLE pl_courses ADD COLUMN IF NOT EXISTS sharing_name text;
 
+
+-- Need to run (or re-run) after the test course is synced
 UPDATE pl_courses SET sharing_name = 'test-course' WHERE title = 'Test Course';
 
