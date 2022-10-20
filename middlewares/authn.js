ca49a3ab36c569f5d623f2eb396da54fc4ea57ea
Nathan Walters <nwalters512@gmail.com>
2022-10-20 14:46:29 -0700

    Simplify UUID regexp

--- middlewares/authn.js
+++ middlewares/authn.js
@@ -8,8 +8,7 @@ var sqlLoader = require('../prairielib/lib/sql-loader');
 
 var sql = sqlLoader.loadSqlEquiv(__filename);
 
-const UUID_REGEXP =
-  /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
+const UUID_REGEXP = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
 
 module.exports = function (req, res, next) {
   res.locals.is_administrator = false;
