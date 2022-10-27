aa9a1160b21f6a95efd711eb4ccce8e1b6aa46a5
Seth Poulsen <poulsenseth@yahoo.com>
2022-10-27 16:00:12 -0500

    Template out Sharing admin page

--- pages/instructorCourseAdminSharing/instructorCourseAdminSharing.html.js
+++ pages/instructorCourseAdminSharing/instructorCourseAdminSharing.html.js
@@ -3,32 +3,75 @@ const { renderEjs } = require('@prairielearn/html-ejs');
 
 
 const InstructorSharing =  ({
+  sharing_name,
+  sharing_id,
+  sharing_sets,
   resLocals,
 }) => {
   return html`
     <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          ${renderEjs(__filename, "<%- include('../../pages/partials/head') %>", resLocals)}
-          <style>
-            .continue-card-container {
-              width: 100%;
-              max-width: 400px;
-            }
-          </style>
-        </head>
-        <body>
-          <script>
-              $(function() {
-                  $('[data-toggle="popover"]').popover({
-                      sanitize: false
-                  })
-              });
-          </script>
+    <html lang="en">
+      <head>
+        ${renderEjs(__filename, "<%- include('../../pages/partials/head') %>", resLocals)}
+        <style>
+          .continue-card-container {
+            width: 100%;
+            max-width: 400px;
+          }
+        </style>
+      </head>
+      <body>
+        <script>
+            $(function() {
+                $('[data-toggle="popover"]').popover({
+                    sanitize: false
+                })
+            });
+        </script>
         ${renderEjs(__filename, "<%- include('../partials/navbar'); %>", resLocals)}
         <div id="content" class="container-fluid">
+          <div class="card mb-4">
+            <div class="card-header bg-primary text-white d-flex">
+                Course Sharing Info
+            </div>
+            <table class="table table-sm table-hover two-column-description">
+              <tbody>
+                  <tr><th>Sharing Name</th><td>${sharing_name}</td></tr>
+                  <tr><th>Sharing ID</th><td><${sharing_id}></td></tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div class="card mb-4">
+          <div class="card-header bg-primary">
+            <div class="row align-items-center justify-content-between">
+              <div class="col-auto">
+                <span class="text-white">Sharing Sets</span>
+              </div>
+              <div class="col-auto">
+                <button type="button" class="btn btn-light btn-sm ml-auto" id="courseSharingSetAddButton" tabindex="0"
+                  data-toggle="popover" data-container="body" data-html="true" data-placement="auto" title="Add users"
+                  data-content="<%= include('coursePermissionsInsertForm', {id: 'coursePermissionsInsertButton'}) %>"
+                  data-trigger="manual" onclick="$(this).popover('show')"
+                >
+                  <i class="fas fa-plus" aria-hidden="true"></i>
+                  <span class="d-none d-sm-inline">Add users</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <table class="table table-sm table-hover table-striped">
+            <thead>
+              <th>Sharing Set Name</th>
+              <th>Shared With</th>
+            </thead>
+            <tbody>
+              ${sharing_sets.map((sharing_set) => html`
+                <tr><td>${sharing_set.name}</td><td>${sharing_set.shared_to.join(', ')}</td></tr>
+              `)}
+            </tbody>
         </div>
-        <body>
+      </body>
     </html>
   `.toString();
 
--- pages/instructorCourseAdminSharing/instructorCourseAdminSharing.js
+++ pages/instructorCourseAdminSharing/instructorCourseAdminSharing.js
@@ -6,7 +6,6 @@ const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'
 const fs = require('fs-extra');
 const async = require('async');
 const ERR = require('async-stacktrace');
-const { CourseInfoEditor } = require('../../lib/editors');
 const logger = require('../../lib/logger');
 const error = require('../../prairielib/lib/error');
 const { InstructorSharing } = require('./instructorCourseAdminSharing.html')
@@ -14,75 +13,36 @@ const { InstructorSharing } = require('./instructorCourseAdminSharing.html')
 router.get('/', function (req, res, next) {
   debug('GET /');
 
-  // res.render()
-  res.send(InstructorSharing({resLocals: res.locals}));
+  console.log(res.locals);
 
-//   async.series(
-//     [
-//       (callback) => {
-//         fs.access(res.locals.course.path, (err) => {
-//           if (err) {
-//             if (err.code === 'ENOENT') {
-//               res.locals.needToSync = true;
-//             } else {
-//               return ERR(err, callback);
-//             }
-//           }
-//           callback(null);
-//         });
-//       },
-//       (callback) => {
-//         if (res.locals.needToSync) return callback(null);
-//         fs.access(path.join(res.locals.course.path, 'infoCourse.json'), (err) => {
-//           if (err) {
-//             if (err.code === 'ENOENT') {
-//               res.locals.noInfo = true;
-//             } else {
-//               return ERR(err, callback);
-//             }
-//           }
-//           callback(null);
-//         });
-//       },
-//     ],
-//     (err) => {
-//       if (ERR(err, next)) return;
-      // res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
-//     }
-//   );
+  // TODO get stuff from query
+  sharing_sets = [
+    {
+      name: 'derivatives',
+      shared_to: [
+        'calculus 1',
+        'calculus 2'
+      ]
+    },
+    {
+      name: 'proofs',
+      shared_to: [
+      ]
+    }
+  ]
+
+  res.send(InstructorSharing({
+    sharing_name: 'test-course',
+    sharing_id:  "qp4oumpo4wmrpq",
+    sharing_sets: sharing_sets,
+    resLocals: res.locals
+  }));
 });
 
 router.post('/', (req, res, next) => {
-  if (!res.locals.authz_data.has_course_permission_edit || res.locals.course.example_course) {
-    return next(
-      error.make(403, 'Access denied (must be course editor and must not be example course)')
-    );
+  if (!res.locals.authz_data.has_course_permission_own) {
+    return next(error.make(403, 'Access denied (must be course owner)'));
   }
-
-  debug(`Responding to post with action ${req.body.__action}`);
-//   if (req.body.__action === 'add_configuration') {
-//     debug(`Responding to action add_configuration`);
-//     const editor = new CourseInfoEditor({
-//       locals: res.locals,
-//     });
-//     editor.canEdit((err) => {
-//       if (ERR(err, next)) return;
-//       editor.doEdit((err, job_sequence_id) => {
-//         if (ERR(err, (e) => logger.error('Error in doEdit()', e))) {
-//           res.redirect(res.locals.urlPrefix + '/edit_error/' + job_sequence_id);
-//         } else {
-//           res.redirect(req.originalUrl);
-//         }
-//       });
-//     });
-//   } else {
-//     next(
-//       error.make(400, 'unknown __action: ' + req.body.__action, {
-//         locals: res.locals,
-//         body: req.body,
-//       })
-//     );
-//   }
 });
 
 module.exports = router;
