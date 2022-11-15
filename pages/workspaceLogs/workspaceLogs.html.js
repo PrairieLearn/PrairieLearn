142a5aad05a013a37fc20f11338d6cac07f8f21c
Nathan Walters <nwalters512@gmail.com>
2022-11-15 13:33:35 -0600

    Improve logs UX

--- pages/workspaceLogs/workspaceLogs.html.js
+++ pages/workspaceLogs/workspaceLogs.html.js
@@ -85,11 +85,19 @@ const WorkspaceVersionLogs = ({ version, workspaceLogs, resLocals }) => {
 
           <div
             id="js-container-logs"
-            class="bg-dark p-3"
+            class="bg-dark p-3 mb-3 rounded"
             data-logs-endpoint="${resLocals.urlPrefix}/workspace/${resLocals.workspace_id}/logs/version/${version}/container_logs"
           >
-            <pre class="text-white rounded mb-3"><code></code></pre>
-            <button type="button" class="btn btn-primary js-load-more-logs">Load more logs</button>
+            <pre class="text-white mb-3"><code></code></pre>
+            <button type="button" class="btn btn-primary js-load-more-logs">
+              <span
+                class="spinner-border spinner-border-sm js-loading-spinner"
+                role="status"
+                aria-hidden="true"
+                hidden
+              ></span>
+              <span class="js-button-message">Load more logs</span>
+            </button>
           </div>
 
           ${WorkspaceLogsTable({ workspaceLogs })}
@@ -97,31 +105,58 @@ const WorkspaceVersionLogs = ({ version, workspaceLogs, resLocals }) => {
 
         <script>
           $(() => {
+            const LOAD_MORE_LOGS_MESSAGE = 'Load more logs';
+            const LOADING_LOGS_MESSAGE = 'Loading logs...';
+
             const containerLogs = document.querySelector('#js-container-logs');
             const containerLogsOutput = containerLogs.querySelector('code');
             const loadMoreLogsButton = containerLogs.querySelector('.js-load-more-logs');
+            const loadingSpinner = loadMoreLogsButton.querySelector('.js-loading-spinner');
+            const buttonMessage = loadMoreLogsButton.querySelector('.js-button-message');
 
             const logsEndpoint = containerLogs.getAttribute('data-logs-endpoint');
             let startAfter = null;
 
             async function fetchNextLogs() {
-              let url = logsEndpoint;
-              if (startAfter) {
-                url += '?' + new URLSearchParams({ start_after: startAfter });
-              }
-              const res = await fetch(url);
-              if (!res.ok) {
-                // TODO: better error handling
-                console.error('Error fetching logs', await res.text());
-              } else {
-                const logs = await res.text();
-                console.log(logs);
-                containerLogsOutput.appendChild(document.createTextNode(logs));
+              // Disable the button so that the user can't try to load more
+              // than once chunk at a time.
+              loadMoreLogsButton.disabled = true;
+              buttonMessage.textContent = LOADING_LOGS_MESSAGE;
+              loadingSpinner.hidden = false;
 
-                if (res.headers.has('x-next-start-after')) {
-                  startAfter = res.headers.get('x-next-start-after');
-                  console.log('starting after', startAfter);
+              const start = Date.now();
+              try {
+                let url = logsEndpoint;
+                if (startAfter) {
+                  url += '?' + new URLSearchParams({ start_after: startAfter });
                 }
+                const res = await fetch(url);
+                if (!res.ok) {
+                  // TODO: better error handling
+                  console.error('Error fetching logs', await res.text());
+                } else {
+                  const logs = await res.text();
+                  console.log(logs);
+                  containerLogsOutput.appendChild(document.createTextNode(logs));
+
+                  if (res.headers.has('x-next-start-after')) {
+                    startAfter = res.headers.get('x-next-start-after');
+                    console.log('starting after', startAfter);
+                  }
+                }
+              } catch (err) {
+              } finally {
+                // Add artificial delay so that the spinner is visible for at
+                // least a second. This will also keep people from spamming the
+                // button as fast as possible.
+                const end = Date.now();
+                const delta = Date.now() - start;
+                const delay = Math.max(0, 1000 - delta);
+                setTimeout(() => {
+                  loadMoreLogsButton.disabled = false;
+                  buttonMessage.textContent = LOAD_MORE_LOGS_MESSAGE;
+                  loadingSpinner.hidden = true;
+                }, delay);
               }
             }
 
@@ -129,13 +164,7 @@ const WorkspaceVersionLogs = ({ version, workspaceLogs, resLocals }) => {
             fetchNextLogs();
 
             // When the user clicks the "Load more logs" button, load the next chunk of logs.
-            loadMoreLogsButton.addEventListener('click', () => {
-              // Disable the button so that we can't load more than one chunk at a time.
-              loadMoreLogsButton.disabled = true;
-              fetchNextLogs().finally(() => {
-                loadMoreLogsButton.disabled = false;
-              });
-            });
+            loadMoreLogsButton.addEventListener('click', () => fetchNextLogs());
           });
         </script>
       </body>
