b4f103db059f063a1fe5404b7b9dde5877a9f8f5
Eliot Robson <eliot.robson24@gmail.com>
2022-11-17 11:55:31 -0600

    Adding scoring infrastructure

--- python/prairielearn.py
+++ python/prairielearn.py
@@ -15,8 +15,8 @@ import importlib
 import importlib.util
 import os
 import collections
-from typing import Dict, Any, TypedDict, Literal, Optional
-from typing_extensions import NotRequired
+from typing import Dict, Any, TypedDict, Literal, Optional, Callable, Tuple, Union
+from typing_extensions import assert_never, NotRequired
 
 class PartialScore(TypedDict):
     "A class with type signatures for the partial scores dict"
@@ -51,6 +51,76 @@ class QuestionData(TypedDict):
 
 class ElementTestData(QuestionData):
     test_type: Literal['correct', 'incorrect', 'invalid']
+
+def grade_question_parameterized(data: QuestionData,
+                                 question_name: str,
+                                 grade_function: Callable[[Any], Tuple[Union[bool, float], Optional[str]]],
+                                 weight: int = 1,
+                                 feedback_field_name: Optional[str] = None) -> None:
+    '''
+    Grade question question_name, marked correct if grade_function(student_answer) returns True in
+    its first argument. grade_function should take in a single parameter (which will be the submitted
+    answer) and return a 2-tuple.
+        - The first element of the 2-tuple should either be:
+            - a boolean indicating whether the question should be marked correct
+            - a partial score between 0 and 1, inclusive
+        - The second element of the 2-tuple should either be:
+            - a string containing feedback
+            - None, if there is no feedback (usually this should only occur if the answer is correct)
+    Note: if the feedback_field_name is the same as the question name,
+    then the feedback_field_name does not need to be specified.
+    '''
+
+    # Create the data dictionary at first
+    data['partial_scores'][question_name] = {
+            'score': 0.0,
+            'weight': weight
+    }
+
+    try:
+        submitted_answer = data['submitted_answers'][question_name]
+    except KeyError:
+        # Catch error if no answer submitted
+        data["format_errors"][question_name] = 'No answer was submitted'
+        return
+
+    # Try to grade, exiting if there's an exception
+    try:
+        result, feedback_content = grade_function(submitted_answer)
+
+        # Check _must_ be done in this order. Int check is to deal with subclass issues
+        if isinstance(result, bool):
+            partial_score = 1.0 if result else 0.0
+        elif isinstance(result, (float, int)):
+            assert 0.0 <= result <= 1.0
+            partial_score = result
+        else:
+            assert_never(result)
+
+    except ValueError as err:
+        # Exit if there's a format error
+        data["format_errors"][question_name] = html.escape(str(err))
+        return
+
+
+    # Set question score if grading succeeded
+    data['partial_scores'][question_name]['score'] = partial_score
+
+    # Put all feedback here
+    if feedback_content:
+        # Check for unescaped bad stuff in feedback string
+        if isinstance(submitted_answer, str):
+            contains_bad_chars = all(x in submitted_answer for x in {'<', '>'})
+            if contains_bad_chars and submitted_answer in feedback_content:
+                raise ValueError(f'Unescaped student input should not be present in the feedback for {question_name}.')
+
+        data['partial_scores'][question_name]['feedback'] = feedback_content
+
+        if not feedback_field_name:
+            feedback_field_name = question_name
+
+        data['feedback'][feedback_field_name] = feedback_content
+
 
 def to_json(v):
     """to_json(v)
