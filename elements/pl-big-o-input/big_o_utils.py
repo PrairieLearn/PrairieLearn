a5771eaec65e4c3167792b814a2993cd0e0ab815
Eliot Robson <eliot.robson24@gmail.com>
2022-11-17 12:17:53 -0600

    Formatting

--- elements/pl-big-o-input/big_o_utils.py
+++ elements/pl-big-o-input/big_o_utils.py
@@ -8,21 +8,23 @@ ASTWhitelist = Tuple[Any, ...]
 
 # See https://github.com/PrairieLearn/PrairieLearn/blob/d10be38fec52929386efb67cce645cd615d589a5/python/python_helper_sympy.py
 # This is a stripped down version that adds factorial but remotes trig and complex numbers.
+
+
 @dataclass
 class _Constants:
     helpers: SympyMap = field(
-        default_factory = lambda: {
+        default_factory=lambda: {
             '_Integer': sympy.Integer,
         }
     )
     variables: SympyMap = field(
-        default_factory = lambda: {
+        default_factory=lambda: {
             'pi': sympy.pi,
             'e': sympy.E,
         }
     )
     functions: SympyMap = field(
-        default_factory = lambda: {
+        default_factory=lambda: {
             'exp': sympy.exp,
             'log': sympy.log,
             'sqrt': sympy.sqrt,
@@ -30,36 +32,44 @@ class _Constants:
         }
     )
 
+
 @dataclass
 class HasFloatError(Exception):
     offset: int
     n: float
 
+
 @dataclass
 class HasInvalidExpressionError(Exception):
     offset: int
+
 
 @dataclass
 class HasInvalidFunctionError(Exception):
     offset: int
     text: str
 
+
 @dataclass
 class HasInvalidVariableError(Exception):
     offset: int
     text: str
 
+
 @dataclass
 class HasParseError(Exception):
     offset: int
+
 
 @dataclass
 class HasEscapeError(Exception):
     offset: int
 
+
 @dataclass
 class HasCommentError(Exception):
     offset: int
+
 
 class CheckNumbers(ast.NodeTransformer):
     def visit_Constant(self, node: ast.Constant):
@@ -68,6 +78,7 @@ class CheckNumbers(ast.NodeTransformer):
         elif isinstance(node.n, float):
             raise HasFloatError(node.col_offset, node.n)
         return node
+
 
 class CheckWhiteList(ast.NodeVisitor):
     def __init__(self, whitelist: ASTWhitelist):
@@ -79,9 +90,11 @@ class CheckWhiteList(ast.NodeVisitor):
             raise HasInvalidExpressionError(node.col_offset)
         return super().visit(node)
 
+
 class CheckFunctions(ast.NodeVisitor):
     def __init__(self, functions: Dict[Any, Any]):
         self.functions = functions
+
     def visit_Call(self, node: Any):
         if isinstance(node.func, ast.Name):
             if node.func.id not in self.functions:
@@ -89,9 +102,11 @@ class CheckFunctions(ast.NodeVisitor):
                 raise HasInvalidFunctionError(node.col_offset, node.func.id)
         self.generic_visit(node)
 
+
 class CheckVariables(ast.NodeVisitor):
     def __init__(self, variables: SympyMap):
         self.variables: SympyMap = variables
+
     def visit_Name(self, node: Any):
         if isinstance(node.ctx, ast.Load):
             if not is_name_of_function(node):
@@ -100,14 +115,17 @@ class CheckVariables(ast.NodeVisitor):
                     raise HasInvalidVariableError(node.col_offset, node.id)
         self.generic_visit(node)
 
+
 def is_name_of_function(node: Any) -> bool:
     return isinstance(node, ast.Name) and isinstance(node.parent, ast.Call) and (node not in node.parent.args)  # type: ignore
+
 
 def get_parent_with_location(node: ast.AST) -> ast.AST:
     if hasattr(node, 'col_offset'):
         return node
     else:
         return get_parent_with_location(node.parent)  # type: ignore
+
 
 def evaluate(expr: str, locals_for_eval: Dict[str, Any] = {}) -> Any:
 
@@ -140,8 +158,9 @@ def evaluate(expr: str, locals_for_eval: Dict[str, Any] = {}) -> Any:
 
     ast.fix_missing_locations(root)
 
-    locals: Dict[Any, Any] = {k:locals_for_eval[key][k] for key in locals_for_eval for k in locals_for_eval[key].keys()}
+    locals: Dict[Any, Any] = {k: locals_for_eval[key][k] for key in locals_for_eval for k in locals_for_eval[key].keys()}
     return eval(compile(root, '<ast>', 'eval'), {'__builtins__': None}, locals)
+
 
 def convert_string_to_sympy(expr: str, variables: List[str]) -> Any:
     const = _Constants()
@@ -168,21 +187,22 @@ def grade_bigo_expression(a_true: str, a_sub: str, variables: List[str]) -> Tupl
     if sym_true.equals(sym_sub):
         return (1, 'Correct! Note that your expression may be unnecessarily complex.')
 
-    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < 0):
+    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < sympy.sympify(0)):
         return (0, 'Your expression is negative.')
 
     L = sympy.limit(sym_true / sym_sub, sympy.Symbol(variables[0]), sympy.oo)
 
-    if L < 0:
+    if L < sympy.sympify(0):
         return (0, 'Your expression is negative.')
     elif L == sympy.oo:
         return (0, "Your answer is incorrect.")
-    elif L == 0:
+    elif L == sympy.sympify(0):
         return (.25, "Your answer is correct, but too loose.")
-    elif L == 1:
+    elif L == sympy.sympify(1):
         return (.5, "Your answer is correct, but you have unnecessary lower order terms.")
     else:
         return (.5, "Your answer is correct but has unncessary constant factors.")
+
 
 def grade_theta_expression(a_true: str, a_sub: str, variables: List[str]) -> Tuple[float, str]:
     if a_true == a_sub:
@@ -194,17 +214,17 @@ def grade_theta_expression(a_true: str, a_sub: str, variables: List[str]) -> Tup
     if sym_true.equals(sym_sub):
         return (1, 'Correct! Note that your expression may be unnecessarily complex.')
 
-    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < 0):
+    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < sympy.sympify(0)):
         return (0, 'Your expression is negative.')
 
     omega_L = sympy.limit(sym_sub / sym_true, sympy.Symbol(variables[0]), sympy.oo)
     bigo_L = sympy.limit(sym_true / sym_sub, sympy.Symbol(variables[0]), sympy.oo)
 
-    if omega_L < 0 or bigo_L < 0:
+    if omega_L < sympy.sympify(0) or bigo_L < sympy.sympify(0):
         return (0, 'Your expression is negative.')
     elif omega_L == sympy.oo or bigo_L == sympy.oo:
         return (0, "Your answer is incorrect.")
-    elif omega_L == 1 and bigo_L == 1:
+    elif omega_L == sympy.sympify(1) and bigo_L == sympy.sympify(1):
         return (0.25, "Incorrect, your answer has unnecessary lower order terms.")
     else:
         return (0.25, "Incorrect, your answer has unnecessary constant factors.")
@@ -220,21 +240,22 @@ def grade_omega_expression(a_true: str, a_sub: str, variables: List[str]) -> Tup
     if sym_true.equals(sym_sub):
         return (1, 'Correct! Note that your expression may be unnecessarily complex.')
 
-    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < 0):
+    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < sympy.sympify(0)):
         return (0, 'Your expression is negative.')
 
     L = sympy.limit(sym_true / sym_sub, sympy.Symbol(variables[0]), sympy.oo)
 
-    if L < 0:
+    if L < sympy.sympify(0):
         return (0, 'Your expression is negative.')
     elif L == sympy.oo:
         return (0.25, "Your answer is correct, but too loose.")
-    elif L == 0:
+    elif L == sympy.sympify(0):
         return (0, "Your answer is incorrect.")
-    elif L == 1:
+    elif L == sympy.sympify(1):
         return (.5, "Your answer is correct, but you have unnecessary lower order terms.")
     else:
         return (.5, "Your answer is correct but has unncessary constant factors.")
+
 
 def grade_little_o_expression(a_true: str, a_sub: str, variables: List[str]) -> Tuple[float, str]:
     if a_true == a_sub:
@@ -246,21 +267,22 @@ def grade_little_o_expression(a_true: str, a_sub: str, variables: List[str]) -> 
     if sym_true.equals(sym_sub):
         return (1, 'Correct! Note that your expression may be unnecessarily complex.')
 
-    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < 0):
+    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < sympy.sympify(0)):
         return (0, 'Your expression is negative.')
 
     L = sympy.limit(sym_true / sym_sub, sympy.Symbol(variables[0]), sympy.oo)
 
-    if L < 0:
+    if L < sympy.sympify(0):
         return (0, 'Your expression is negative.')
     elif L == sympy.oo:
         return (0, "Your answer is incorrect.")
-    elif L == 0:
+    elif L == sympy.sympify(0):
         return (.25, "Your answer is correct, but too loose.")
-    elif L == 1:
+    elif L == sympy.sympify(1):
         return (.5, "Your answer is correct, but you have unnecessary lower order terms.")
     else:
         return (.5, "Your answer is correct but has unncessary constant factors.")
+
 
 def grade_little_omega_expression(a_true: str, a_sub: str, variables: List[str]) -> Tuple[float, str]:
     if a_true == a_sub:
@@ -272,24 +294,25 @@ def grade_little_omega_expression(a_true: str, a_sub: str, variables: List[str])
     if sym_true.equals(sym_sub):
         return (1, 'Correct! Note that your expression may be unnecessarily complex.')
 
-    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < 0):
+    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < sympy.sympify(0)):
         return (0, 'Your expression is negative.')
 
     L = sympy.limit(sym_true / sym_sub, sympy.Symbol(variables[0]), sympy.oo)
 
-    if L < 0:
+    if L < sympy.sympify(0):
         return (0, 'Your expression is negative.')
     elif L == sympy.oo:
         return (0.25, "Your answer is correct, but too loose.")
-    elif L == 0:
+    elif L == sympy.sympify(0):
         return (0, "Your answer is incorrect.")
-    elif L == 1:
+    elif L == sympy.sympify():
         return (.5, "Your answer is correct, but you have unnecessary lower order terms.")
     else:
         return (.5, "Your answer is correct but has unncessary constant factors.")
 
+
 def point_to_error(s: str, ind: int, w: int = 5) -> str:
-    w_left: str = ' ' * (ind - max(0, ind-w))
-    w_right: str = ' ' * (min(ind+w, len(s)) - ind)
-    initial: str = s[ind-len(w_left):ind+len(w_right)]
-    return  f"{initial}\n{w_left}^{w_right}"
+    w_left: str = ' ' * (ind - max(0, ind - w))
+    w_right: str = ' ' * (min(ind + w, len(s)) - ind)
+    initial: str = s[ind - len(w_left):ind + len(w_right)]
+    return f"{initial}\n{w_left}^{w_right}"
