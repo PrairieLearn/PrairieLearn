dc1ea1d03a8e6f323ad55ee203ac61ffc5a516c4
Eliot Robson <eliot.robson24@gmail.com>
2022-11-17 12:04:56 -0600

    Added first draft of bigo element

---
+++ elements/pl-big-o-input/big_o_utils.py
@@ -0,0 +1,295 @@
+from typing import List, Tuple, Dict, Any
+import sympy
+import ast
+from dataclasses import dataclass, field
+
+SympyMap = Dict[str, Any]
+ASTWhitelist = Tuple[Any, ...]
+
+# See https://github.com/PrairieLearn/PrairieLearn/blob/d10be38fec52929386efb67cce645cd615d589a5/python/python_helper_sympy.py
+# This is a stripped down version that adds factorial but remotes trig and complex numbers.
+@dataclass
+class _Constants:
+    helpers: SympyMap = field(
+        default_factory = lambda: {
+            '_Integer': sympy.Integer,
+        }
+    )
+    variables: SympyMap = field(
+        default_factory = lambda: {
+            'pi': sympy.pi,
+            'e': sympy.E,
+        }
+    )
+    functions: SympyMap = field(
+        default_factory = lambda: {
+            'exp': sympy.exp,
+            'log': sympy.log,
+            'sqrt': sympy.sqrt,
+            'factorial': sympy.factorial
+        }
+    )
+
+@dataclass
+class HasFloatError(Exception):
+    offset: int
+    n: float
+
+@dataclass
+class HasInvalidExpressionError(Exception):
+    offset: int
+
+@dataclass
+class HasInvalidFunctionError(Exception):
+    offset: int
+    text: str
+
+@dataclass
+class HasInvalidVariableError(Exception):
+    offset: int
+    text: str
+
+@dataclass
+class HasParseError(Exception):
+    offset: int
+
+@dataclass
+class HasEscapeError(Exception):
+    offset: int
+
+@dataclass
+class HasCommentError(Exception):
+    offset: int
+
+class CheckNumbers(ast.NodeTransformer):
+    def visit_Constant(self, node: ast.Constant):
+        if isinstance(node.n, int):
+            return ast.Call(func=ast.Name(id='_Integer', ctx=ast.Load()), args=[node], keywords=[])
+        elif isinstance(node.n, float):
+            raise HasFloatError(node.col_offset, node.n)
+        return node
+
+class CheckWhiteList(ast.NodeVisitor):
+    def __init__(self, whitelist: ASTWhitelist):
+        self.whitelist: ASTWhitelist = whitelist
+
+    def visit(self, node: Any):
+        if not isinstance(node, self.whitelist):
+            node = get_parent_with_location(node)
+            raise HasInvalidExpressionError(node.col_offset)
+        return super().visit(node)
+
+class CheckFunctions(ast.NodeVisitor):
+    def __init__(self, functions: Dict[Any, Any]):
+        self.functions = functions
+    def visit_Call(self, node: Any):
+        if isinstance(node.func, ast.Name):
+            if node.func.id not in self.functions:
+                node = get_parent_with_location(node)
+                raise HasInvalidFunctionError(node.col_offset, node.func.id)
+        self.generic_visit(node)
+
+class CheckVariables(ast.NodeVisitor):
+    def __init__(self, variables: SympyMap):
+        self.variables: SympyMap = variables
+    def visit_Name(self, node: Any):
+        if isinstance(node.ctx, ast.Load):
+            if not is_name_of_function(node):
+                if node.id not in self.variables:
+                    node = get_parent_with_location(node)
+                    raise HasInvalidVariableError(node.col_offset, node.id)
+        self.generic_visit(node)
+
+def is_name_of_function(node: Any) -> bool:
+    return isinstance(node, ast.Name) and isinstance(node.parent, ast.Call) and (node not in node.parent.args)  # type: ignore
+
+def get_parent_with_location(node: ast.AST) -> ast.AST:
+    if hasattr(node, 'col_offset'):
+        return node
+    else:
+        return get_parent_with_location(node.parent)  # type: ignore
+
+def evaluate(expr: str, locals_for_eval: Dict[str, Any] = {}) -> Any:
+
+    # Disallow escape character
+    ind = expr.find('\\')
+    if ind != -1:
+        raise HasEscapeError(ind)
+
+    # Disallow comment character
+    ind = expr.find('#')
+    if ind != -1:
+        raise HasCommentError(ind)
+
+    try:
+        root = ast.parse(expr, mode='eval')
+    except Exception as err:
+        raise HasParseError(err.offset)  # type: ignore
+
+    for node in ast.walk(root):
+        for child in ast.iter_child_nodes(node):
+            child.parent = node  # type: ignore
+
+    CheckFunctions(locals_for_eval['functions']).visit(root)
+    CheckVariables(locals_for_eval['variables']).visit(root)
+
+    whitelist: ASTWhitelist = (ast.Module, ast.Expr, ast.Load, ast.Expression, ast.Call, ast.Name, ast.Constant, ast.UnaryOp, ast.UAdd, ast.USub, ast.BinOp, ast.Add, ast.Sub, ast.Mult, ast.Div, ast.Mod, ast.Pow)
+    CheckWhiteList(whitelist).visit(root)
+
+    root = CheckNumbers().visit(root)
+
+    ast.fix_missing_locations(root)
+
+    locals: Dict[Any, Any] = {k:locals_for_eval[key][k] for key in locals_for_eval for k in locals_for_eval[key].keys()}
+    return eval(compile(root, '<ast>', 'eval'), {'__builtins__': None}, locals)
+
+def convert_string_to_sympy(expr: str, variables: List[str]) -> Any:
+    const = _Constants()
+
+    locals_for_eval = {
+        'functions': const.functions,
+        'variables': const.variables,
+        'helpers': const.helpers
+    }
+
+    if variables is not None:
+        for variable in variables:
+            locals_for_eval['variables'][variable] = sympy.Symbol(variable)
+    return evaluate(expr, locals_for_eval)
+
+
+def grade_bigo_expression(a_true: str, a_sub: str, variables: List[str]) -> Tuple[float, str]:
+    if a_true == a_sub:
+        return (1, "Correct!")
+
+    sym_true = convert_string_to_sympy(a_true, variables)
+    sym_sub = convert_string_to_sympy(a_sub, variables)
+
+    if sym_true.equals(sym_sub):
+        return (1, 'Correct! Note that your expression may be unnecessarily complex.')
+
+    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < 0):
+        return (0, 'Your expression is negative.')
+
+    L = sympy.limit(sym_true / sym_sub, sympy.Symbol(variables[0]), sympy.oo)
+
+    if L < 0:
+        return (0, 'Your expression is negative.')
+    elif L == sympy.oo:
+        return (0, "Your answer is incorrect.")
+    elif L == 0:
+        return (.25, "Your answer is correct, but too loose.")
+    elif L == 1:
+        return (.5, "Your answer is correct, but you have unnecessary lower order terms.")
+    else:
+        return (.5, "Your answer is correct but has unncessary constant factors.")
+
+def grade_theta_expression(a_true: str, a_sub: str, variables: List[str]) -> Tuple[float, str]:
+    if a_true == a_sub:
+        return (1, "Correct!")
+
+    sym_true = convert_string_to_sympy(a_true, variables)
+    sym_sub = convert_string_to_sympy(a_sub, variables)
+
+    if sym_true.equals(sym_sub):
+        return (1, 'Correct! Note that your expression may be unnecessarily complex.')
+
+    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < 0):
+        return (0, 'Your expression is negative.')
+
+    omega_L = sympy.limit(sym_sub / sym_true, sympy.Symbol(variables[0]), sympy.oo)
+    bigo_L = sympy.limit(sym_true / sym_sub, sympy.Symbol(variables[0]), sympy.oo)
+
+    if omega_L < 0 or bigo_L < 0:
+        return (0, 'Your expression is negative.')
+    elif omega_L == sympy.oo or bigo_L == sympy.oo:
+        return (0, "Your answer is incorrect.")
+    elif omega_L == 1 and bigo_L == 1:
+        return (0.25, "Incorrect, your answer has unnecessary lower order terms.")
+    else:
+        return (0.25, "Incorrect, your answer has unnecessary constant factors.")
+
+
+def grade_omega_expression(a_true: str, a_sub: str, variables: List[str]) -> Tuple[float, str]:
+    if a_true == a_sub:
+        return (1, "Correct!")
+
+    sym_true = convert_string_to_sympy(a_true, variables)
+    sym_sub = convert_string_to_sympy(a_sub, variables)
+
+    if sym_true.equals(sym_sub):
+        return (1, 'Correct! Note that your expression may be unnecessarily complex.')
+
+    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < 0):
+        return (0, 'Your expression is negative.')
+
+    L = sympy.limit(sym_true / sym_sub, sympy.Symbol(variables[0]), sympy.oo)
+
+    if L < 0:
+        return (0, 'Your expression is negative.')
+    elif L == sympy.oo:
+        return (0.25, "Your answer is correct, but too loose.")
+    elif L == 0:
+        return (0, "Your answer is incorrect.")
+    elif L == 1:
+        return (.5, "Your answer is correct, but you have unnecessary lower order terms.")
+    else:
+        return (.5, "Your answer is correct but has unncessary constant factors.")
+
+def grade_little_o_expression(a_true: str, a_sub: str, variables: List[str]) -> Tuple[float, str]:
+    if a_true == a_sub:
+        return (1, "Correct!")
+
+    sym_true = convert_string_to_sympy(a_true, variables)
+    sym_sub = convert_string_to_sympy(a_sub, variables)
+
+    if sym_true.equals(sym_sub):
+        return (1, 'Correct! Note that your expression may be unnecessarily complex.')
+
+    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < 0):
+        return (0, 'Your expression is negative.')
+
+    L = sympy.limit(sym_true / sym_sub, sympy.Symbol(variables[0]), sympy.oo)
+
+    if L < 0:
+        return (0, 'Your expression is negative.')
+    elif L == sympy.oo:
+        return (0, "Your answer is incorrect.")
+    elif L == 0:
+        return (.25, "Your answer is correct, but too loose.")
+    elif L == 1:
+        return (.5, "Your answer is correct, but you have unnecessary lower order terms.")
+    else:
+        return (.5, "Your answer is correct but has unncessary constant factors.")
+
+def grade_little_omega_expression(a_true: str, a_sub: str, variables: List[str]) -> Tuple[float, str]:
+    if a_true == a_sub:
+        return (1, "Correct!")
+
+    sym_true = convert_string_to_sympy(a_true, variables)
+    sym_sub = convert_string_to_sympy(a_sub, variables)
+
+    if sym_true.equals(sym_sub):
+        return (1, 'Correct! Note that your expression may be unnecessarily complex.')
+
+    if (sympy.limit(sym_sub, sympy.Symbol(variables[0]), sympy.oo) < 0):
+        return (0, 'Your expression is negative.')
+
+    L = sympy.limit(sym_true / sym_sub, sympy.Symbol(variables[0]), sympy.oo)
+
+    if L < 0:
+        return (0, 'Your expression is negative.')
+    elif L == sympy.oo:
+        return (0.25, "Your answer is correct, but too loose.")
+    elif L == 0:
+        return (0, "Your answer is incorrect.")
+    elif L == 1:
+        return (.5, "Your answer is correct, but you have unnecessary lower order terms.")
+    else:
+        return (.5, "Your answer is correct but has unncessary constant factors.")
+
+def point_to_error(s: str, ind: int, w: int = 5) -> str:
+    w_left: str = ' ' * (ind - max(0, ind-w))
+    w_right: str = ' ' * (min(ind+w, len(s)) - ind)
+    initial: str = s[ind-len(w_left):ind+len(w_right)]
+    return  f"{initial}\n{w_left}^{w_right}"
---
+++ elements/pl-big-o-input/pl-big-o-input.py
@@ -0,0 +1,269 @@
+from enum import Enum
+from typing import List, Optional, Tuple
+from typing_extensions import assert_never
+import prairielearn as pl
+import lxml.html
+from html import escape
+import sympy
+import chevron
+import math
+import big_o_utils as bou
+
+VARIABLES_DEFAULT = ''
+DISPLAY_DEFAULT = 'inline'
+SIZE_DEFAULT = 35
+PLACEHOLDER_TEXT_THRESHOLD = 20
+SHOW_HELP_TEXT_DEFAULT = True
+WEIGHT_DEFAULT = 1
+
+class BigOType(Enum):
+    BIGO = r'O'
+    THETA = r'\Theta'
+    OMEGA = r'\Omega'
+    LITTLE_O = r'o'
+    LITTLE_OMEGA = r'\omega'
+
+class DisplayType(Enum):
+    INLINE = 'inline'
+    BLOCK = 'block'
+
+def prepare(element_html: str, data: pl.QuestionData) -> None:
+    element = lxml.html.fragment_fromstring(element_html)
+    required_attribs = ['answers-name']
+    optional_attribs = ['weight', 'correct-answer', 'variables', 'size', 'display', 'show-help-text', 'type']
+    pl.check_attribs(element, required_attribs, optional_attribs)
+    name = pl.get_string_attrib(element, 'answers-name')
+
+    if pl.has_attrib(element, 'correct-answer'):
+        if name in data['correct_answers']:
+            raise ValueError(f"duplicate correct_answers variable name: {name}")
+        data['correct_answers'][name] = pl.get_string_attrib(element, 'correct-answer')
+
+
+def render(element_html: str, data: pl.QuestionData) -> str:
+    element = lxml.html.fragment_fromstring(element_html)
+    name = pl.get_string_attrib(element, 'answers-name')
+    variables = get_variables_list(pl.get_string_attrib(element, 'variables', VARIABLES_DEFAULT))
+    if len(variables) > 1:
+        raise ValueError("Only one variable is supported")
+    display = DisplayType(pl.get_string_attrib(element, 'display', DISPLAY_DEFAULT))
+    size = pl.get_integer_attrib(element, 'size', SIZE_DEFAULT)
+
+    bigo_type_name = pl.get_string_attrib(element, 'type', BigOType.BIGO.name).upper()
+    bigo_type = BigOType[bigo_type_name].value
+
+    operators: List[str] = ['exp', 'log', 'sqrt', 'factorial', '( )', '+', '-', '*', '/', '^', '**']
+    constants: List[str] = ['pi', 'e']
+
+    info_params = {
+        'format': True,
+        'variables': variables,
+        'operators': operators,
+        'constants': constants
+    }
+
+    PARTIAL_SCORE_DEFAULT: pl.PartialScore = {'score': None}
+    score = data['partial_scores'].get(name, PARTIAL_SCORE_DEFAULT).get('score')
+
+    if data['panel'] == 'question':
+        editable = data['editable']
+        raw_submitted_answer = data['raw_submitted_answers'].get(name)
+
+        with open('pl-big-o-input.mustache', 'r', encoding='utf-8') as f:
+            info = chevron.render(f, info_params).strip()
+
+        if raw_submitted_answer is not None:
+            raw_submitted_answer = escape(raw_submitted_answer)
+
+        score_type, score_value = determine_score_params(score)
+
+        html_params = {
+            'question': True,
+            'name': name,
+            'editable': editable,
+            'info': info,
+            'size': size,
+            'show_info': pl.get_boolean_attrib(element, 'show-help-text', SHOW_HELP_TEXT_DEFAULT),
+            'uuid': pl.get_uuid(),
+            display.value: True,
+            'show_placeholder': size >= PLACEHOLDER_TEXT_THRESHOLD,
+            'raw_submitted_answer': raw_submitted_answer,
+            'type': bigo_type,
+            score_type: True,
+            'score_value': score_value
+        }
+
+        with open('pl-big-o-input.mustache', 'r', encoding='utf-8') as f:
+            return chevron.render(f, html_params).strip()
+
+    elif data['panel'] == 'submission':
+        parse_error: Optional[str] = data['format_errors'].get(name)
+        missing_input = False
+        feedback = None
+        a_sub = None
+        raw_submitted_answer = None
+
+        if parse_error is None and name in data['submitted_answers']:
+            a_sub = sympy.latex(sympy.sympify(data['submitted_answers'][name], evaluate=False))
+            if name in data['partial_scores']:
+                feedback = data['partial_scores'][name].get('feedback')
+        elif name not in data['submitted_answers']:
+            missing_input = True
+            parse_error = None
+        else:
+
+            # Use the existing format text in the invalid popup.
+            with open('pl-big-o-input.mustache', 'r', encoding='utf-8') as f:
+                info = chevron.render(f, info_params).strip()
+
+
+            # Render invalid popup
+            raw_submitted_answer = data['raw_submitted_answers'].get(name)
+            if isinstance(parse_error, str):
+                with open('pl-big-o-input.mustache', 'r', encoding='utf-8') as f:
+                    parse_error += chevron.render(f, {'format_error': True, 'format_string': info}).strip()
+            if raw_submitted_answer is not None:
+                raw_submitted_answer = pl.escape_unicode_string(raw_submitted_answer)
+
+        score_type, score_value = determine_score_params(score)
+
+        html_params = {
+            'submission': True,
+            'type': bigo_type,
+            'parse_error': parse_error,
+            'uuid': pl.get_uuid(),
+            display.value: True,
+            'error': parse_error or missing_input,
+            'a_sub': a_sub,
+            'feedback': feedback,
+            'raw_submitted_answer': raw_submitted_answer,
+            score_type: True,
+            'score_value': score_value
+        }
+        with open('pl-big-o-input.mustache', 'r', encoding='utf-8') as f:
+            return chevron.render(f, html_params).strip()
+
+    # Display the correct answer.
+    elif data['panel'] == 'answer':
+        a_tru = data['correct_answers'].get(name)
+        if a_tru is not None:
+            a_tru = sympy.sympify(a_tru)
+            html_params = {
+                'answer': True,
+                'a_tru': sympy.latex(a_tru),
+                'type': bigo_type
+            }
+            with open('pl-big-o-input.mustache', 'r', encoding='utf-8') as f:
+                return chevron.render(f, html_params).strip()
+        return ''
+
+    assert_never(data['panel'])
+
+def parse(element_html: str, data: pl.QuestionData) -> None:
+    element = lxml.html.fragment_fromstring(element_html)
+    name = pl.get_string_attrib(element, 'answers-name')
+    variables = get_variables_list(pl.get_string_attrib(element, 'variables', VARIABLES_DEFAULT))
+
+    a_sub = data['submitted_answers'].get(name)
+    if not a_sub:
+        data['format_errors'][name] = 'No submitted answer.'
+        data['submitted_answers'][name] = None
+        return
+
+    # Replace '^' with '**' wherever it appears.
+    a_sub = a_sub.replace('^', '**')
+
+    # Replace unicode minus with hyphen minus wherever it occurs
+    a_sub = a_sub.replace(u'\u2212', '-')
+
+    # Strip whitespace
+    a_sub = a_sub.strip()
+    data['submitted_answers'][name] = a_sub
+
+    s = None
+    try:
+        bou.convert_string_to_sympy(a_sub, variables)
+    except bou.HasFloatError as err:
+        s = f'Your answer contains the floating-point number {err.n}. ' \
+            f'All numbers must be expressed as integers (or ratios of integers)' \
+            f'<br><br><pre>{bou.point_to_error(a_sub, err.offset)}</pre>'
+    except bou.HasInvalidExpressionError as err:
+        s = f'Your answer has an invalid expression. '\
+            f'<br><br><pre>{bou.point_to_error(a_sub, err.offset)}</pre>'
+    except bou.HasInvalidFunctionError as err:
+        s = f'Your answer calls an invalid function "{err.text}". ' \
+            f'<br><br><pre>{bou.point_to_error(a_sub, err.offset)}</pre>'
+    except bou.HasInvalidVariableError as err:
+        s = f'Your answer refers to an invalid variable "{err.text}". ' \
+            f'<br><br><pre>{bou.point_to_error(a_sub, err.offset)}</pre>'
+    except bou.HasParseError as err:
+        s = f'Your answer has a syntax error. ' \
+            f'<br><br><pre>{bou.point_to_error(a_sub, err.offset)}</pre>'
+    except bou.HasEscapeError as err:
+        s = f'Your answer must not contain the character "\\". ' \
+            f'<br><br><pre>{bou.point_to_error(a_sub, err.offset)}</pre>'
+    except bou.HasCommentError as err:
+        s = f'Your answer must not contain the character "#". ' \
+            f'<br><br><pre>{bou.point_to_error(a_sub, err.offset)}</pre>'
+    except Exception:
+        s = 'Invalid format.'
+    finally:
+        if (s is not None):
+            data['format_errors'][name] = s
+            data['submitted_answers'][name] = None
+
+def grade(element_html: str, data: pl.QuestionData) -> None:
+    element = lxml.html.fragment_fromstring(element_html)
+    name = pl.get_string_attrib(element, 'answers-name')
+    variables = get_variables_list(pl.get_string_attrib(element, 'variables', VARIABLES_DEFAULT))
+    weight = pl.get_integer_attrib(element, 'weight', WEIGHT_DEFAULT)
+    a_tru: str = data['correct_answers'].get(name, '')
+
+    def grade_bigo(a_sub: str) -> Tuple[float, str]:
+        return bou.grade_bigo_expression(a_tru, a_sub, variables)
+    def grade_theta(a_sub: str) -> Tuple[float, str]:
+        return bou.grade_theta_expression(a_tru, a_sub, variables)
+    def grade_omega(a_sub: str) -> Tuple[float, str]:
+        return bou.grade_omega_expression(a_tru, a_sub, variables)
+    def grade_little_o(a_sub: str) -> Tuple[float, str]:
+        return bou.grade_little_o_expression(a_tru, a_sub, variables)
+    def grade_little_omega(a_sub: str) -> Tuple[float, str]:
+        return bou.grade_little_omega_expression(a_tru, a_sub, variables)
+
+    bigo_type_name = pl.get_string_attrib(element, 'type', BigOType.BIGO.name).upper()
+    bigo_type =  BigOType[bigo_type_name]
+
+    if bigo_type is BigOType.BIGO:
+        pl.grade_question_parameterized(data, name, grade_bigo, weight=weight)
+    elif bigo_type is BigOType.THETA:
+        pl.grade_question_parameterized(data, name, grade_theta, weight=weight)
+    elif bigo_type is BigOType.OMEGA:
+        pl.grade_question_parameterized(data, name, grade_omega, weight=weight)
+    elif bigo_type is BigOType.LITTLE_O:
+        pl.grade_question_parameterized(data, name, grade_little_o, weight=weight)
+    elif bigo_type is BigOType.LITTLE_OMEGA:
+        pl.grade_question_parameterized(data, name, grade_little_omega, weight=weight)
+    else:
+        assert_never(bigo_type)
+
+
+def get_variables_list(variables_string: str) -> List[str]:
+    variables_list = [variable.strip() for variable in variables_string.split(',')]
+    if variables_list == ['']:
+        return []
+    return variables_list
+
+def determine_score_params(score: Optional[float]) -> Tuple[str, float]:
+    if score is not None:
+        try:
+            score_val: float = float(score)
+            if score_val >= 1:
+                return ('correct', 1.0)
+            elif score_val > 0:
+                return ('partial', math.floor(score_val*100))
+            else:
+                return ('incorrect', 0.0)
+        except Exception:
+            raise ValueError(f"invalid score {score}")
+    else:
+        return '', 0.0
