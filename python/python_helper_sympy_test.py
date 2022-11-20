cbb79f0ba5b7dfac581b7bb6e6ee5541135d2189
Eliot Robson <eliot.robson24@gmail.com>
2022-11-19 20:39:32 -0600

    Complex number tests

--- python/python_helper_sympy_test.py
+++ python/python_helper_sympy_test.py
@@ -54,6 +54,14 @@ class TestSympy:
     def test_string_conversion(self, a_sub: str, sympy_ref: sympy.Expr) -> None:
         assert sympy_ref == phs.convert_string_to_sympy(a_sub, ['n', 'm'], allow_complex=True)
 
+    @pytest.mark.parametrize('a_sub, sympy_ref', [
+        ('i', sympy.I),
+        ('j', sympy.I),
+        ('i*i', -1),
+        ('j*j', -1)
+    ])
+    def test_string_conversion_no_complex(self, a_sub: str, sympy_ref: sympy.Expr) -> None:
+        assert sympy_ref != phs.convert_string_to_sympy(a_sub, ['i', 'j'], allow_complex=False)
 
     @pytest.mark.parametrize('a_sub', EXPR_STRINGS)
     def test_json_conversion(self, a_sub: str) -> None:
@@ -71,6 +79,11 @@ class TestSympy:
 
 class TestExceptions:
     VARIABLES = ['n']
+
+    @pytest.mark.parametrize('a_sub', ['i', '5 * i', 'j'])
+    def test_not_allowed_complex(self, a_sub: str) -> None:
+        with pytest.raises(phs.HasInvalidVariableError):
+            phs.convert_string_to_sympy(a_sub, self.VARIABLES, allow_complex=False)
 
     @pytest.mark.parametrize('a_sub', ['i', '5 * i', 'j'])
     def test_reserved_variables(self, a_sub: str) -> None:
