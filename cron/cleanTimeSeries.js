d03a0d9ef0247937fa779bde2f0e9151881f337c
Nathan Walters <nwalters512@gmail.com>
2022-11-21 14:30:16 -0800

    Rewrite with async/await

--- cron/cleanTimeSeries.js
+++ cron/cleanTimeSeries.js
@@ -1,4 +1,4 @@
-const ERR = require('async-stacktrace');
+const { callbackify } = require('util');
 
 const logger = require('../lib/logger');
 const sqldb = require('../prairielib/lib/sql-db');
@@ -6,10 +6,7 @@ const sqlLoader = require('../prairielib/lib/sql-loader');
 
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
-module.exports.run = (callback) => {
-  sqldb.query(sql.clean_time_series, {}, (err, result) => {
-    if (ERR(err, callback)) return;
-    logger.verbose(`Deleted ${result.rowCount} old rows from time_series table`);
-    callback(null);
-  });
-};
+module.exports.run = callbackify(async () => {
+  const result = await sqldb.queryAsync(sql.clean_time_series);
+  logger.verbose(`Deleted ${result.rowCount} old rows from time_series table`);
+});
