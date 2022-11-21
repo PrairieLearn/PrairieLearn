3a33bc098e4fb546936a2cce5f570b095ceb3c08
Nathan Walters <nathan@prairielearn.com>
2022-11-21 14:52:16 -0800

    Update cron/cleanTimeSeries.js

--- cron/cleanTimeSeries.js
+++ cron/cleanTimeSeries.js
@@ -8,6 +8,6 @@ const sqlLoader = require('../prairielib/lib/sql-loader');
 const sql = sqlLoader.loadSqlEquiv(__filename);
 
 module.exports.run = callbackify(async () => {
-  const result = await sqldb.queryAsync(sql.clean_time_series);
+  const result = await sqldb.queryAsync(sql.clean_time_series, {});
   logger.verbose(`Deleted ${result.rowCount} old rows from time_series table`);
 });
