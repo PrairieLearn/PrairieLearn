2e1619ec0c85d13479691267f5d6efd90cfebb79
Eliot Robson <eliot.robson24@gmail.com>
2022-12-03 13:29:16 -0600

    Ran black

--- elements/pl-external-grader-variables/pl-external-grader-variables.py
+++ elements/pl-external-grader-variables/pl-external-grader-variables.py
@@ -5,22 +5,22 @@ import chevron
 
 def render(element_html: str, data: pl.QuestionData) -> str:
     element = lxml.html.fragment_fromstring(element_html)
-    pl.check_attribs(element, [], ['params-name'])
+    pl.check_attribs(element, [], ["params-name"])
 
     # Get frontend variables
     html_variables = []
 
     for child in element:
-        if child.tag == 'pl-variable':
-            pl.check_attribs(child, ['name', 'type'], ['description'])
+        if child.tag == "pl-variable":
+            pl.check_attribs(child, ["name", "type"], ["description"])
 
             var_dict = {
-                'name': pl.get_string_attrib(child, 'name'),
-                'type': pl.get_string_attrib(child, 'type')
+                "name": pl.get_string_attrib(child, "name"),
+                "type": pl.get_string_attrib(child, "type"),
             }
 
-            if pl.has_attrib(child, 'description'):
-                var_dict['description'] = pl.get_string_attrib(child, 'description')
+            if pl.has_attrib(child, "description"):
+                var_dict["description"] = pl.get_string_attrib(child, "description")
 
             html_variables.append(var_dict)
 
@@ -28,31 +28,35 @@ def render(element_html: str, data: pl.QuestionData) -> str:
             continue
 
         else:
-            raise ValueError(f"Tags inside of pl-external-grader-variables must be pl-variable, not '{child.tag}'.")
+            raise ValueError(
+                f"Tags inside of pl-external-grader-variables must be pl-variable, not '{child.tag}'."
+            )
 
-    has_backend_variables = pl.has_attrib(element, 'params-name')
+    has_backend_variables = pl.has_attrib(element, "params-name")
 
     if has_backend_variables and len(html_variables) > 0:
-        raise ValueError('Cannot define variables from both backend and frontend')
+        raise ValueError("Cannot define variables from both backend and frontend")
 
     # Get backend variables
     backend_variables = []
 
     if has_backend_variables:
-        params_name = pl.get_string_attrib(element, 'params-name')
-        backend_variables = data['params'][params_name]
+        params_name = pl.get_string_attrib(element, "params-name")
+        backend_variables = data["params"][params_name]
 
     # Make final variables list
     names_user_description = backend_variables + html_variables
     has_names_user_description = len(names_user_description) > 0
 
     # Show descriptions if any variable has them set
-    has_descriptions = any('description' in var_description for var_description in names_user_description)
+    has_descriptions = any(
+        "description" in var_description for var_description in names_user_description
+    )
 
     html_params = {
-        'has_descriptions': has_descriptions,
-        'names_user_description': names_user_description,
-        'has_names_user_description': has_names_user_description,
+        "has_descriptions": has_descriptions,
+        "names_user_description": names_user_description,
+        "has_names_user_description": has_names_user_description,
     }
-    with open('pl-external-grader-variables.mustache', 'r', encoding='utf-8') as f:
+    with open("pl-external-grader-variables.mustache", "r", encoding="utf-8") as f:
         return chevron.render(f, html_params).strip()
--- exampleCourse/questions/demo/autograder/python/pandas/server.py
+++ exampleCourse/questions/demo/autograder/python/pandas/server.py
@@ -1,4 +1,3 @@
-import numpy as np
 import prairielearn as pl
 import pandas as pd
 
