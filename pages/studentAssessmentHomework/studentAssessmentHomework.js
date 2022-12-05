acd4d2a27e40662d3699ffb747e0cbb71201c658
caleqw2 <caleqw2@illinois.edu>
2022-12-05 15:37:26 -0600

    Rewrite updateGroupRoles as async

--- pages/studentAssessmentHomework/studentAssessmentHomework.js
+++ pages/studentAssessmentHomework/studentAssessmentHomework.js
@@ -13,7 +13,7 @@ var groupAssessmentHelper = require('../../lib/groups');
 
 var sql = sqlLoader.loadSqlEquiv(__filename);
 
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
   debug('GET');
   if (res.locals.assessment.type !== 'Homework') return next();
   debug('is Homework');
@@ -31,72 +31,69 @@ router.get('/', function (req, res, next) {
     user_id: res.locals.user.user_id,
   };
 
-  // TODO: Make this not an async function
-  sqldb.query(sql.find_single_assessment_instance, params, async function (err, result) {
-    if (ERR(err, next)) return;
-    if (result.rowCount === 0) {
-      debug('no assessment instance');
+  const result = await sqldb.queryAsync(sql.find_single_assessment_instance, params);
+  if (result.rowCount === 0) {
+    debug('no assessment instance');
 
-      // No, you do not need to verify authz_result.authorized_edit (indeed, this flag exists
-      // only for an assessment instance, not an assessment).
-      //
-      // The assessment that is created here will be owned by the effective user. The only
-      // reason to worry, therefore, is if the effective user has a different UID than the
-      // authn user. This is only allowed, however, if the authn user has permission to edit
-      // student data in the course instance (which has already been checked), exactly the
-      // permission required to create an assessment for the effective user.
+    // No, you do not need to verify authz_result.authorized_edit (indeed, this flag exists
+    // only for an assessment instance, not an assessment).
+    //
+    // The assessment that is created here will be owned by the effective user. The only
+    // reason to worry, therefore, is if the effective user has a different UID than the
+    // authn user. This is only allowed, however, if the authn user has permission to edit
+    // student data in the course instance (which has already been checked), exactly the
+    // permission required to create an assessment for the effective user.
 
-      // If this assessment is group work and there is no existing instance,
-      // show the group info page.
-      if (res.locals.assessment.group_work) {
-        const groupInfo = await groupAssessmentHelper.getGroupInfo(res.locals.assessment.id, res.locals.user.user_id);
-        res.locals.permissions = groupInfo.permissions;
-        res.locals.hasRoles = groupInfo.hasRoles; // TODO: Wondering if this is redundant from 'rolesInfo'
-        res.locals.minSize = groupInfo.minSize;
-        res.locals.groupSize = groupInfo.groupSize;
-        res.locals.groupInfo = groupInfo.groupInfo; // TODO: Could rename to "groupMembers" because each row is a distinct group member?
-        res.locals.joinCode = groupInfo.joinCode;
-        res.locals.start = groupInfo.start;
-        res.locals.rolesInfo = groupInfo.rolesInfo;
-        res.locals.used_join_code = groupInfo.usedJoinCode; // TODO: Maybe rewrite
+    // If this assessment is group work and there is no existing instance,
+    // show the group info page.
+    if (res.locals.assessment.group_work) {
+      const groupInfo = await groupAssessmentHelper.getGroupInfo(res.locals.assessment.id, res.locals.user.user_id);
+      res.locals.permissions = groupInfo.permissions;
+      res.locals.hasRoles = groupInfo.hasRoles; // TODO: Wondering if this is redundant from 'rolesInfo'
+      res.locals.minSize = groupInfo.minSize;
+      res.locals.groupSize = groupInfo.groupSize;
+      res.locals.groupInfo = groupInfo.groupInfo; // TODO: Could rename to "groupMembers" because each row is a distinct group member?
+      res.locals.joinCode = groupInfo.joinCode;
+      res.locals.start = groupInfo.start;
+      res.locals.rolesInfo = groupInfo.rolesInfo;
+      res.locals.used_join_code = groupInfo.usedJoinCode; // TODO: Maybe rewrite
 
-        if (groupInfo.isGroupMember && groupInfo.hasRoles) {
-          const result = await groupAssessmentHelper.getAssessmentLevelPermissions(res.locals.assessment.id, res.locals.user.user_id);
-          res.locals.canViewRoleTable = result.can_assign_roles_at_start;
-        }
-
-        res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
-      } else {
-        // Before allowing the user to create a new assessment instance, we need
-        // to check if the current access rules require a password. If they do,
-        // we'll ensure that the password has already been entered before allowing
-        // students to create and start a new assessment instance.
-        if (!checkPasswordOrRedirect(req, res)) return;
-
-        const time_limit_min = null;
-        assessment.makeAssessmentInstance(
-          res.locals.assessment.id,
-          res.locals.user.user_id,
-          res.locals.assessment.group_work,
-          res.locals.authn_user.user_id,
-          res.locals.authz_data.mode,
-          time_limit_min,
-          res.locals.authz_data.date,
-          (err, assessment_instance_id) => {
-            if (ERR(err, next)) return;
-            debug('redirecting');
-            res.redirect(res.locals.urlPrefix + '/assessment_instance/' + assessment_instance_id);
-          }
-        );
+      if (groupInfo.isGroupMember && groupInfo.hasRoles) {
+        const result = await groupAssessmentHelper.getAssessmentLevelPermissions(res.locals.assessment.id, res.locals.user.user_id);
+        res.locals.canViewRoleTable = result.can_assign_roles_at_start;
       }
+
+      res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
     } else {
-      debug('redirecting');
-      res.redirect(res.locals.urlPrefix + '/assessment_instance/' + result.rows[0].id);
+      // Before allowing the user to create a new assessment instance, we need
+      // to check if the current access rules require a password. If they do,
+      // we'll ensure that the password has already been entered before allowing
+      // students to create and start a new assessment instance.
+      if (!checkPasswordOrRedirect(req, res)) return;
+
+      const time_limit_min = null;
+      assessment.makeAssessmentInstance(
+        res.locals.assessment.id,
+        res.locals.user.user_id,
+        res.locals.assessment.group_work,
+        res.locals.authn_user.user_id,
+        res.locals.authz_data.mode,
+        time_limit_min,
+        res.locals.authz_data.date,
+        (err, assessment_instance_id) => {
+          if (ERR(err, next)) return;
+          debug('redirecting');
+          res.redirect(res.locals.urlPrefix + '/assessment_instance/' + assessment_instance_id);
+        }
+      );
     }
-  });
+  } else {
+    debug('redirecting');
+    res.redirect(res.locals.urlPrefix + '/assessment_instance/' + result.rows[0].id);
+  }
 });
 
-router.post('/', function (req, res, next) {
+router.post('/', async function (req, res, next) {
   if (res.locals.assessment.type !== 'Homework') return next();
   if (req.body.__action === 'new_instance') {
     var params = {
@@ -173,16 +170,13 @@ router.post('/', function (req, res, next) {
       }
     );
   } else if (req.body.__action === 'update_group_roles') {
-    groupAssessmentHelper.updateGroupRoles(
+    await groupAssessmentHelper.updateGroupRoles(
       req.body,
       res.locals.assessment.id,
       res.locals.user.user_id,
-      res.locals.authn_user.user_id,
-      function (err) {
-        if (ERR(err, next)) return;
-        res.redirect(req.originalUrl);
-      }
+      res.locals.authn_user.user_id
     );
+    res.redirect(req.originalUrl);
   } else if (req.body.__action === 'leave_group') {
     groupAssessmentHelper.leaveGroup(
       res.locals.assessment.id,
