f995f912aaba672b9f6a9a932a8f4ee7221082a6
Eliot Robson <eliot.robson24@gmail.com>
2022-12-04 19:06:39 -0600

    Added error code

--- exampleCourse/questions/element/pythonVariable/server.py
+++ exampleCourse/questions/element/pythonVariable/server.py
@@ -8,6 +8,10 @@ import pandas as pd
 
 def generate(data):
     df = pd.io.parsers.read_csv("breast-cancer-train.dat", header=None)
+
+    encoded_json_df = df.to_json(orient = "table", date_format = "iso")
+    pd.read_json(encoded_json_df, orient="table")
+
     data['params']['df'] = pl.to_json(df.head(15))
     data['params']['matrix'] = pl.to_json(np.random.random((3, 3)))
     data['params']['my_dictionary'] = {'a': 1, 'b': 2, 'c': 3}
