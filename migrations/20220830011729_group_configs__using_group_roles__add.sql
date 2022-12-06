8eb74375b319a266ae0dfad20d312d85b0cea4fa
Cale Wolf <calewolf@gmail.com>
2022-12-06 12:14:13 -0600

    Update migrations/20220830011729_group_configs__using_group_roles__add.sql
    Co-authored-by: Nathan Walters <nathan@prairielearn.com>

--- migrations/20220830011729_group_configs__using_group_roles__add.sql
+++ migrations/20220830011729_group_configs__using_group_roles__add.sql
@@ -1,7 +1,8 @@
 ALTER TABLE group_configs ADD COLUMN has_roles boolean DEFAULT false;
 
-ALTER TABLE group_users DROP CONSTRAINT group_users_pkey;
-ALTER TABLE group_users ADD PRIMARY KEY (group_id, user_id, group_role_id);
+ALTER TABLE group_users
+DROP CONSTRAINT group_users_pkey,
+ADD PRIMARY KEY (group_id, user_id, group_role_id);
 
 -- Insert role for group assessments that don't use group roles
 INSERT INTO group_roles 
