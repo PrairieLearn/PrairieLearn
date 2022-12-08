5d777c1a6798acf4b06b7d87754073487c5b7788
Eliot Robson <eliot.robson24@gmail.com>
2022-12-08 13:03:06 -0600

    Added message

--- python/conftest.py
+++ python/conftest.py
@@ -1,22 +1,4 @@
-import pytest
-from prairielearn import QuestionData
-
-
-@pytest.fixture
-def question_data() -> QuestionData:
-    return {
-        "params": dict(),
-        "correct_answers": dict(),
-        "submitted_answers": dict(),
-        "format_errors": dict(),
-        "partial_scores": dict(),
-        "score": 0.0,
-        "feedback": dict(),
-        "variant_seed": 0,
-        "options": dict(),
-        "raw_submitted_answers": dict(),
-        "editable": False,
-        "panel": "question",
-        "extensions": dict(),
-        "num_valid_submissions": 0,
-    }
+"""
+Test file containing shared test fixtures. Needed for now for test runner to pick up this folder.
+Will add fixtures as needed
+"""
