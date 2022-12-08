5ed13338d658cb59816b98b76b54f2aa4075d4f6
Jonatan Schroeder <jonatan@yorku.ca>
2022-12-08 14:23:40 -0500

    Add question_score_logs entry (draft)

--- sprocs/assessment_questions_update_rubric.sql
+++ sprocs/assessment_questions_update_rubric.sql
@@ -13,12 +13,10 @@ AS $$
 DECLARE
     next_number BIGINT := 0;
     rubric_item RECORD;
+    assessment_question assessment_questions;
 BEGIN
 
-    SELECT
-        CASE WHEN rubric_type = 'auto' THEN auto_rubric_id ELSE manual_rubric_id END
-    INTO
-        arg_rubric_id
+    SELECT * INTO assessment_question
     FROM assessment_questions
     WHERE id = arg_assessment_question_id
     FOR UPDATE;
@@ -26,6 +24,8 @@ BEGIN
     IF NOT FOUND THEN
         RAISE EXCEPTION 'No such assessment question %', arg_assessment_question_id;
     END IF;
+
+    arg_rubric_id := CASE WHEN rubric_type = 'auto' THEN assessment_question.auto_rubric_id ELSE assessment_question.manual_rubric_id END;
 
     IF NOT use_rubrics AND arg_rubric_id IS NULL THEN RETURN; END IF;
 
@@ -101,20 +101,35 @@ BEGIN
 
     -- TODO Do this only for instance questions that have rubric items
     -- that have been affected by a change, depending on options
-    UPDATE instance_questions iq
-    SET
-        manual_points = CASE WHEN rubric_type = 'auto' THEN manual_points ELSE rgi.arg_computed_points END,
-        auto_points = CASE WHEN rubric_type = 'auto' THEN rgi.arg_computed_points ELSE auto_points END,
-        manual_rubric_grading_id = CASE WHEN rubric_type = 'auto' THEN manual_rubric_grading_id ELSE rgi.rubric_grading_id END,
-        auto_rubric_grading_id = CASE WHEN rubric_type = 'auto' THEN rgi.rubric_grading_id ELSE auto_rubric_grading_id END
+    WITH updated_instance_questions AS (
+        UPDATE instance_questions iq
+        SET
+            manual_points = CASE WHEN rubric_type = 'auto' THEN manual_points ELSE rgi.arg_computed_points END,
+            auto_points = CASE WHEN rubric_type = 'auto' THEN rgi.arg_computed_points ELSE auto_points END,
+            manual_rubric_grading_id = CASE WHEN rubric_type = 'auto' THEN manual_rubric_grading_id ELSE rgi.rubric_grading_id END,
+            auto_rubric_grading_id = CASE WHEN rubric_type = 'auto' THEN rgi.rubric_grading_id ELSE auto_rubric_grading_id END,
+            points = CASE WHEN rubric_type = 'auto' THEN manual_points ELSE auto_points END + rgi.arg_computed_points
+            -- TODO score_perc, highest_submission_score (maybe use instance_questions_update_score?)
+        FROM
+            rubric_gradings AS rg
+            JOIN rubric_gradings_insert(arg_rubric_id, rg.id, NULL, NULL, NULL) AS rgi ON TRUE
+        WHERE
+            iq.assessment_question_id = arg_assessment_question_id
+            AND rg.id = CASE WHEN rubric_type = 'auto' THEN iq.auto_rubric_grading_id ELSE iq.manual_rubric_grading_id END
+        RETURNING iq.*
+    )
+    INSERT INTO question_score_logs
+        (instance_question_id, auth_user_id,
+        max_points, max_manual_points, max_auto_points,
+        points, score_perc, auto_points, manual_points)
+    SELECT
+        iq.id, 1, -- TODO Add auth_user_id as argument
+        assessment_question.max_points, assessment_question.max_manual_points, assessment_question.max_auto_points,
+        iq.points, iq.score_perc, iq.auto_points, iq.manual_points
     FROM
-        rubric_gradings AS rg
-        JOIN rubric_gradings_insert(arg_rubric_id, rg.id, NULL, NULL, NULL) AS rgi ON TRUE
-    WHERE
-        iq.assessment_question_id = arg_assessment_question_id
-        AND rg.id = CASE WHEN rubric_type = 'auto' THEN iq.auto_rubric_grading_id ELSE iq.manual_rubric_grading_id END;
+        updated_instance_questions iq;
 
-    -- TODO Recalculate existing instance questions if necessary
+    -- TODO Add grading job?
     -- TODO Handle issues arising from conflicting changes
 END;
 $$ LANGUAGE plpgsql VOLATILE;
