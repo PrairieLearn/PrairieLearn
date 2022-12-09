18be23323e4f370909aecae2d119a8ad98f8f1a2
Nathan Walters <nathan@prairielearn.com>
2022-12-09 12:22:20 -0800

    Remove legacy login redirects (#6762)

--- pages/authCallbackShib/authCallbackShib.js
+++ pages/authCallbackShib/authCallbackShib.js
@@ -6,7 +6,7 @@ var config = require('../../lib/config');
 var csrf = require('../../lib/csrf');
 var sqldb = require('../../prairielib/lib/sql-db');
 
-router.get('/:action?/:target(*)?', function (req, res, next) {
+router.get('/', function (req, res, next) {
   if (!config.hasShib) return next(new Error('Shibboleth login is not enabled'));
   var authUid = null;
   var authName = null;
@@ -34,7 +34,6 @@ router.get('/:action?/:target(*)?', function (req, res, next) {
       httpOnly: true,
       secure: true,
     });
-    if (req.params.action === 'redirect') return res.redirect('/' + req.params.target);
     var redirUrl = res.locals.homeUrl;
     if ('preAuthUrl' in req.cookies) {
       redirUrl = req.cookies.preAuthUrl;
