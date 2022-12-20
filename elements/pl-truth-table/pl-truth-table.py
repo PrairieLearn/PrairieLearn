1bf193dff7356a14f01d0dac479146b304a4e8eb
Eliot Robson <eliot.robson24@gmail.com>
2022-12-20 00:36:11 -0500

    Black compatibility and formatting

--- elements/pl-truth-table/pl-truth-table.py
+++ elements/pl-truth-table/pl-truth-table.py
@@ -5,13 +5,15 @@ import lxml.html
 import lxml.etree
 import prairielearn as pl
 
-from typing import List, Tuple, NamedTuple, Dict
+from typing import List, Tuple, NamedTuple
 from typing_extensions import assert_never
 from itertools import product, count
+
 
 class AnswerCol(NamedTuple):
     text: str
     expression: str
+
 
 WEIGHT_DEFAULT = 1
 FIXED_VARIABLES_ORDER_DEFAULT = False
@@ -22,10 +24,14 @@ HIDE_ANSWER_PANEL_DEFAULT = False
 HIDE_SCORE_BADGE_DEFAULT = False
 BLANK_DEFAULT = True
 
+
 def get_form_name(answers_name: str, index: int) -> str:
     return f"{answers_name}-dropdown-{index}"
 
-def get_question_information(element: lxml.html.HtmlElement) -> Tuple[List[str], List[str], List[AnswerCol]]:
+
+def get_question_information(
+    element: lxml.html.HtmlElement,
+) -> Tuple[List[str], List[str], List[AnswerCol]]:
     variable_names = []
     rows = []
     cols = []
@@ -44,9 +50,8 @@ def get_question_information(element: lxml.html.HtmlElement) -> Tuple[List[str],
             expression = pl.get_string_attrib(child, "expression", None)
             if expression is None:
                 expression = lxml.html.document_fromstring(
-                        pl.inner_html(child)
-                    ).text_content()
-
+                    pl.inner_html(child)
+                ).text_content()
 
             cols.append(AnswerCol(pl.inner_html(child), expression))
 
@@ -54,7 +59,9 @@ def get_question_information(element: lxml.html.HtmlElement) -> Tuple[List[str],
             continue
 
         else:
-            raise ValueError(f"Tags inside of pl-truth-table must be one of pl-variable, pl-row, or pl-answer-column, not '{child.tag}'.")
+            raise ValueError(
+                f"Tags inside of pl-truth-table must be one of pl-variable, pl-row, or pl-answer-column, not '{child.tag}'."
+            )
 
     return variable_names, rows, cols
 
@@ -69,17 +76,14 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
 
     variable_names, custom_rows, answer_columns = get_question_information(element)
 
-    expressions = [
-        answer_col.expression
-        for answer_col in answer_columns
-    ]
+    expressions = [answer_col.expression for answer_col in answer_columns]
 
     num_vars = len(variable_names)
 
     fixed_order = pl.get_boolean_attrib(
         element, "fixed-order", FIXED_VARIABLES_ORDER_DEFAULT
     )
-    num_rows = pl.get_integer_attrib(element, "num-rows", None)
+    # num_rows = pl.get_integer_attrib(element, "num-rows", None)
 
     if len(custom_rows) > 0:
         random.shuffle(custom_rows)
@@ -88,8 +92,7 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
         random.shuffle(variable_names)
 
     display_variables = [
-        {"key": str(i), "html": variable}
-        for i, variable in enumerate(variable_names)
+        {"key": str(i), "html": variable} for i, variable in enumerate(variable_names)
     ]
 
     display_ans_columns = [
@@ -104,7 +107,7 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
     display_rows = []
     correct_answers = []
 
-    #TODO redo logic involving custom rows
+    # TODO redo logic involving custom rows
     all_rows = True
     row_counter = count(0)
 
@@ -112,21 +115,22 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
         for value_assignment in product([True, False], repeat=num_vars):
             var_vals = dict(zip(variable_names, value_assignment))
 
-            display_rows.append({
-                "key": str(next(row_counter)),
-                "values": list(map(str, value_assignment))
-            })
+            display_rows.append(
+                {
+                    "key": str(next(row_counter)),
+                    "values": list(map(str, value_assignment)),
+                }
+            )
 
             for expr in expressions:
-                #TODO get rid of this eval call!!!
+                # TODO get rid of this eval call!!!
                 # Explanation: This eval call is similar to that made in the symbolic input question,
                 # there's an open refactor of the backend of that element, so this is going to need to wait
                 # until that is in until refactoring here and merging
                 val = eval(expr, var_vals)
                 correct_answers.append(str(val))
 
-
-    '''
+    """
     if num_rows is not None and (len(custom_rows) > num_rows or default_num_rows > num_rows):
         if len(custom_rows) > 0:
             custom_rows = random.sample(custom_rows, num_rows)
@@ -216,12 +220,12 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
             display_rows.append(keyed_row)
             for j in range(len(answer_columns)):
                 correct_answers.append(str(bool(eval(expressions[j]))))
-    '''
+    """
 
     data["params"][name] = {
         "display_variables": display_variables,
         "display_rows": display_rows,
-        "display_ans_columns": display_ans_columns
+        "display_ans_columns": display_ans_columns,
     }
     data["correct_answers"][name] = correct_answers
 
@@ -238,7 +242,9 @@ def parse(element_html: str, data: pl.QuestionData) -> None:
     submitted_answers = data["submitted_answers"].get(name, [])
 
     if expected_num_answers != len(submitted_answers):
-        data["format_errors"][name] = "Number of submitted answers doesn't match number of rows."
+        data["format_errors"][
+            name
+        ] = "Number of submitted answers doesn't match number of rows."
         return
 
     for i in range(expected_num_answers):
@@ -252,7 +258,6 @@ def render(element_html: str, data: pl.QuestionData) -> str:
     element = lxml.html.fragment_fromstring(element_html)
     name = pl.get_string_attrib(element, "answers-name")
 
-
     display_variables = data["params"][name]["display_variables"]
     display_rows = data["params"][name]["display_rows"]
     display_ans_columns = data["params"][name]["display_ans_columns"]
@@ -263,7 +268,7 @@ def render(element_html: str, data: pl.QuestionData) -> str:
     submitted_answers = data["submitted_answers"].get(name, [])
 
     # TODO I think these lines can be deleted???
-    #if submitted_answers == "True" or submitted_answers == "False":
+    # if submitted_answers == "True" or submitted_answers == "False":
     #    submitted_answers = [submitted_answers]
 
     hide_score_badge = pl.get_boolean_attrib(
@@ -271,8 +276,8 @@ def render(element_html: str, data: pl.QuestionData) -> str:
     )
     true_label = pl.get_string_attrib(element, "true-label", TRUE_LABEL_DEFAULT)
     false_label = pl.get_string_attrib(element, "false-label", FALSE_LABEL_DEFAULT)
-    #TODO start dropdowns as blank??
-    blank_start = pl.get_boolean_attrib(element, "blank", BLANK_DEFAULT)
+    # TODO start dropdowns as blank??
+    # blank_start = pl.get_boolean_attrib(element, "blank", BLANK_DEFAULT)
     show_answer_feedback = not hide_score_badge
 
     if data["panel"] == "question":
@@ -281,13 +286,15 @@ def render(element_html: str, data: pl.QuestionData) -> str:
         display_score_badge = score is not None and show_answer_feedback
 
         variable_set = [
-            {"html": variable["html"].strip(), "name": get_form_name(name, variable["key"])}
+            {
+                "html": variable["html"].strip(),
+                "name": get_form_name(name, variable["key"]),
+            }
             for variable in display_variables
         ]
 
         ans_column_set = [
-            {"html": ans_column["html"].strip()}
-            for ans_column in display_ans_columns
+            {"html": ans_column["html"].strip()} for ans_column in display_ans_columns
         ]
 
         row_set = []
@@ -308,19 +315,24 @@ def render(element_html: str, data: pl.QuestionData) -> str:
                     student_answer = submitted_answers[answer_index]
                 correct_answer = correct_answers[answer_index]
 
-                ans_column_row_set.append({
-                    "html": ans_column["html"].strip(),
-                    "correct": display_score_badge and student_answer == correct_answer,
-                    "submitted_true": student_answer == "True",
-                    "submitted_false": student_answer == "False",
-                })
+                ans_column_row_set.append(
+                    {
+                        "html": ans_column["html"].strip(),
+                        "correct": display_score_badge
+                        and student_answer == correct_answer,
+                        "submitted_true": student_answer == "True",
+                        "submitted_false": student_answer == "False",
+                    }
+                )
 
-            row_set.append({
-                "key": row["key"],
-                "values": value_set,
-                "answer_columns": ans_column_row_set,
-                "display_score_badge": display_score_badge,
-            })
+            row_set.append(
+                {
+                    "key": row["key"],
+                    "values": value_set,
+                    "answer_columns": ans_column_row_set,
+                    "display_score_badge": display_score_badge,
+                }
+            )
 
         html_params = {
             "question": True,
@@ -356,13 +368,11 @@ def render(element_html: str, data: pl.QuestionData) -> str:
         display_score_badge = score is not None and show_answer_feedback
 
         variable_set = [
-            {"html": variable["html"].strip()}
-            for variable in display_variables
+            {"html": variable["html"].strip()} for variable in display_variables
         ]
 
         ans_column_set = [
-            {"html": ans_column["html"].strip()}
-            for ans_column in display_ans_columns
+            {"html": ans_column["html"].strip()} for ans_column in display_ans_columns
         ]
 
         row_set = []
@@ -378,31 +388,35 @@ def render(element_html: str, data: pl.QuestionData) -> str:
             for j in range(num_ans_columns):
                 answer_index = (i * num_ans_columns) + j
 
-                format_error = data["format_errors"].get(get_form_name(name, answer_index))
+                format_error = data["format_errors"].get(
+                    get_form_name(name, answer_index)
+                )
 
                 if format_error is not None:
-                    student_answer_set.append({
-                        "parse_error": format_error
-                    })
+                    student_answer_set.append({"parse_error": format_error})
 
                     continue
-
 
                 student_answer = submitted_answers[answer_index]
                 correct_answer = correct_answers[answer_index]
 
-                student_answer_set.append({
-                    "html": true_label if student_answer == "True" else false_label,
-                    "correct_answer": correct_answer,
-                    "correct": display_score_badge and student_answer == correct_answer,
-                })
+                student_answer_set.append(
+                    {
+                        "html": true_label if student_answer == "True" else false_label,
+                        "correct_answer": correct_answer,
+                        "correct": display_score_badge
+                        and student_answer == correct_answer,
+                    }
+                )
 
-            row_set.append({
-                "key": row["key"],
-                "values": value_set,
-                "student_answers": student_answer_set,
-                "display_score_badge": display_score_badge,
-            })
+            row_set.append(
+                {
+                    "key": row["key"],
+                    "values": value_set,
+                    "student_answers": student_answer_set,
+                    "display_score_badge": display_score_badge,
+                }
+            )
 
         html_params = {
             "submission": True,
@@ -421,17 +435,15 @@ def render(element_html: str, data: pl.QuestionData) -> str:
             return chevron.render(f, html_params).strip()
 
     elif data["panel"] == "answer":
-        if pl.get_boolean_attrib(element, "hide-answer-panel", HIDE_ANSWER_PANEL_DEFAULT):
+        if pl.get_boolean_attrib(
+            element, "hide-answer-panel", HIDE_ANSWER_PANEL_DEFAULT
+        ):
             return ""
 
-        variable_set = [
-            {"html": variable["html"]}
-            for variable in display_variables
-        ]
+        variable_set = [{"html": variable["html"]} for variable in display_variables]
 
         ans_column_set = [
-            {"html": ans_column["html"].strip()}
-            for ans_column in display_ans_columns
+            {"html": ans_column["html"].strip()} for ans_column in display_ans_columns
         ]
 
         row_set = []
@@ -449,15 +461,17 @@ def render(element_html: str, data: pl.QuestionData) -> str:
             for j in range(num_ans_columns):
                 correct_answer = correct_answers[(i * num_ans_columns) + j]
 
-                correct_answer_set.append({
-                    "html": true_label if correct_answer == "True" else false_label
-                })
+                correct_answer_set.append(
+                    {"html": true_label if correct_answer == "True" else false_label}
+                )
 
-            row_set.append({
-                "key": row["key"],
-                "values": value_set,
-                "correct_answers": correct_answer_set,
-            })
+            row_set.append(
+                {
+                    "key": row["key"],
+                    "values": value_set,
+                    "correct_answers": correct_answer_set,
+                }
+            )
 
         html_params = {
             "answer": True,
@@ -486,13 +500,17 @@ def grade(element_html: str, data: pl.QuestionData) -> None:
     def edc_grade_fn(submitted_answers: List[str]) -> Tuple[float, None]:
         num_correct = sum(
             1 if submitted_answer == correct_answer else 0
-            for (submitted_answer, correct_answer) in zip(submitted_answers, correct_answers)
+            for (submitted_answer, correct_answer) in zip(
+                submitted_answers, correct_answers
+            )
         )
 
         return num_correct / len(correct_answers), None
 
     def no_partial_credit_grade_fn(submitted_answers: List[str]) -> Tuple[bool, None]:
-        for (submitted_answer, correct_answer) in zip(submitted_answers, correct_answers):
+        for (submitted_answer, correct_answer) in zip(
+            submitted_answers, correct_answers
+        ):
             if submitted_answer != correct_answer:
                 return False, None
 
@@ -530,20 +548,24 @@ def test(element_html: str, data: pl.ElementTestData) -> None:
         data["partial_scores"][name] = {"score": 0, "weight": weight}
 
         if test_case == 1:
-            #TODO this is a global parse error, add a test case with a local one
+            # TODO this is a global parse error, add a test case with a local one
             data["raw_submitted_answers"][name] = correct_answers + [None]
-            data["format_errors"][name] = "Number of submitted answers doesn't match number of rows."
+            data["format_errors"][
+                name
+            ] = "Number of submitted answers doesn't match number of rows."
 
         elif test_case == 2:
             # First, choose a random submission to change to None
-            random_index = random.randint(0, len(correct_answers)-1)
+            random_index = random.randint(0, len(correct_answers) - 1)
             submitted_answers = correct_answers.copy()
             submitted_answers[random_index] = None
 
             data["raw_submitted_answers"][name] = submitted_answers
 
             # Then check that this gives the desired parse error in only one index
-            data["format_errors"][get_form_name(name, random_index)] = "No answer was submitted."
+            data["format_errors"][
+                get_form_name(name, random_index)
+            ] = "No answer was submitted."
 
         else:
             # TODO change this later to assert never
