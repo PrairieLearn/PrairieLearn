51e37df5c0a8562a07449336312de0da51690315
Jonatan Schroeder <jonatan@yorku.ca>
2022-12-21 12:54:55 -0500

    Move rubric selection from sproc to lib

--- lib/manualGrading.sql
+++ lib/manualGrading.sql
@@ -36,3 +36,45 @@ ORDER BY
     user_or_group_name,
     iq.id
 LIMIT 1;
+
+-- BLOCK select_rubric_data
+WITH submission_count_per_rubric_item AS (
+    SELECT
+        rgi.rubric_item_id,
+        COUNT(1) AS num_submissions
+    FROM
+        instance_questions iq
+        JOIN rubric_gradings rg ON (rg.id IN (iq.manual_rubric_grading_id, iq.auto_rubric_grading_id))
+        JOIN rubric_grading_items rgi ON (rgi.rubric_grading_id = rg.id)
+    WHERE
+        iq.assessment_question_id = $assessment_question_id
+        AND rg.rubric_id = $rubric_id
+    GROUP BY rgi.rubric_item_id
+), rubric_items_data AS (
+    SELECT
+        JSONB_AGG(
+            JSONB_BUILD_OBJECT(
+                'rubric_item', TO_JSONB(ri),
+                'grading_item', TO_JSONB(rgi),
+                'num_submissions', COALESCE(scpri.num_submissions, 0))
+            ORDER BY ri.number, ri.id) AS items_data
+    FROM
+        rubric_items AS ri
+        LEFT JOIN submission_count_per_rubric_item AS scpri ON (scpri.rubric_item_id = ri.id)
+        LEFT JOIN rubric_grading_items AS rgi ON (rgi.rubric_item_id = ri.id
+                                                  AND rgi.rubric_grading_id = $rubric_grading_id)
+    WHERE
+        ri.rubric_id = $rubric_id
+        AND ri.deleted_at IS NULL
+)
+SELECT
+    TO_JSONB(r) AS rubric_data,
+    TO_JSONB(rg) AS rubric_grading_data,
+    rid.items_data AS rubric_items
+FROM
+    rubrics r
+    LEFT JOIN rubric_gradings rg ON (rg.id = $rubric_grading_id)
+    LEFT JOIN rubric_items_data rid ON (TRUE)
+WHERE
+    r.id = $rubric_id
+    AND r.deleted_at IS NULL;
--- sprocs/index.js
+++ sprocs/index.js
@@ -166,7 +166,6 @@ module.exports.init = function (callback) {
       'group_users_insert.sql',
       'sync_assessment_modules.sql',
       'assessment_questions_update_rubric.sql',
-      'rubric_gradings_select_data.sql',
       'rubric_gradings_insert.sql',
       'rubric_gradings_recompute.sql',
     ],
