aa89a73facbc34345e630c051785613aa0e42a0a
Jonatan Schroeder <jonatan@yorku.ca>
2023-01-04 11:23:09 -0500

    Move populate and rendering of locals to manualGrading

--- pages/instructorAssessmentManualGrading/instanceQuestion/instanceQuestion.js
+++ pages/instructorAssessmentManualGrading/instanceQuestion/instanceQuestion.js
@@ -48,14 +48,7 @@ async function prepareLocalsForRender(req, res) {
     res.locals.conflict_grading_job = (
       await sqldb.queryZeroOrOneRowAsync(sql.select_grading_job_data, params)
     ).rows[0];
-    res.locals.conflict_grading_job.rubric_grading_manual =
-      await manualGrading.selectRubricGradingData(
-        res.locals.conflict_grading_job.manual_rubric_grading_id
-      );
-    res.locals.conflict_grading_job.rubric_grading_auto =
-      await manualGrading.selectRubricGradingData(
-        res.locals.conflict_grading_job.auto_rubric_grading_id
-      );
+    await manualGrading.populateManualGradingData(res.locals.conflict_grading_job);
   }
 
   const graders_result = await sqldb.queryZeroOrOneRowAsync(sql.select_graders, {
