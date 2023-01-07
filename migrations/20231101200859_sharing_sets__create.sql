9afec6c5fa1652294095136eb63c1cf2eea78ef2
Seth Poulsen <poulsenseth@yahoo.com>
2023-01-06 21:12:04 -0600

    Set up foreign key constraints and fix database tests

--- migrations/20231101200859_sharing_sets__create.sql
+++ migrations/20231101200859_sharing_sets__create.sql
@@ -12,6 +12,15 @@ CREATE TABLE IF NOT EXISTS question_sharing_sets (
     sharing_set_id BIGINT NOT NULL
 );
 
+ALTER TABLE question_sharing_sets 
+ADD CONSTRAINT question_sharing_sets_question_id_fkey 
+FOREIGN KEY (question_id) REFERENCES questions(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE question_sharing_sets 
+ADD CONSTRAINT question_sharing_sets_sharing_set_id_fkey
+FOREIGN KEY (sharing_set_id) REFERENCES sharing_sets(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
 -- TODO: table name here might be confusing. It should denote
 -- that these are the courses that are allowed to access a given sharing set
 CREATE TABLE IF NOT EXISTS course_sharing_sets (
@@ -28,7 +37,6 @@ ALTER TABLE pl_courses ADD COLUMN IF NOT EXISTS question_sharing_enabled boolean
 -- Need to run (or re-run) after the test course is synced
 -- Run with command:
 -- docker exec -it mypl psql postgres -f PrairieLearn/migrations/20231101200859_sharing_sets__create.sql
-
 UPDATE pl_courses SET sharing_name = 'test-course' WHERE title = 'Test Course';
 UPDATE pl_courses SET sharing_id = '390bd8c3-7461-4b05-b5f8-dd5c821109d8' WHERE title = 'Test Course';
 
