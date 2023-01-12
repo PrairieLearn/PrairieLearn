2c705d5c85798d1a023bdf7c969e0809d65e4c33
Nathan Walters <nwalters512@gmail.com>
2023-01-12 10:50:08 -0800

    Silence ESLint warning

--- tests/instructorAssessmentDownloads.test.js
+++ tests/instructorAssessmentDownloads.test.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const assert = require('chai').assert;
 const request = require('request');
 const cheerio = require('cheerio');
+// eslint-disable-next-line import/no-unresolved -- https://github.com/import-js/eslint-plugin-import/issues/2132
 const { parse: csvParse } = require('csv-parse/sync');
 
 const helperServer = require('./helperServer');
