03661cb837b36c4895d79477f35a7bb6764ef3ce
Nathan Walters <nwalters512@gmail.com>
2023-01-26 10:36:41 -0800

    Enable type checking for elements

--- elements/pl-file-preview/pl-file-preview.py
+++ elements/pl-file-preview/pl-file-preview.py
@@ -1,16 +1,8 @@
 import chevron
-import lxml.html
 import prairielearn as pl
 
 
-def prepare(element_html, data):
-    element = lxml.html.fragment_fromstring(element_html)
-    required_attribs = []
-    optional_attribs = []
-    pl.check_attribs(element, required_attribs, optional_attribs)
-
-
-def render(element_html, data):
+def render(element_html: str, data: pl.QuestionData):
     if data["panel"] != "submission":
         return ""
 
@@ -30,19 +22,14 @@ def render(element_html, data):
     html_params["errors"] = data["format_errors"].get("_files", [])
 
     # Reshape files into a useful form
-    if len(submitted_files) > 0:
-        files = []
-        for idx, file in enumerate(submitted_files):
-            files.append(
-                {
-                    "name": file["name"],
-                    "index": idx,
-                }
-            )
-        html_params["has_files"] = True
-        html_params["files"] = files
-    else:
-        html_params["has_files"] = False
+    html_params["has_files"] = len(submitted_files) > 0
+    html_params["files"] = [
+        {
+            "name": file["name"],
+            "index": idx,
+        }
+        for idx, file in enumerate(submitted_files)
+    ]
 
     with open("pl-file-preview.mustache", "r", encoding="utf-8") as f:
         return chevron.render(f, html_params).strip()
