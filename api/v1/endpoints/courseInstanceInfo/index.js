44c65fd98ee56a735841233a4ab20ca45d075905
Nathan Walters <nwalters512@gmail.com>
2023-01-27 14:07:18 -0800

    Rewrite all API handlers with async/await

--- api/v1/endpoints/courseInstanceInfo/index.js
+++ api/v1/endpoints/courseInstanceInfo/index.js
@@ -1,4 +1,5 @@
-const ERR = require('async-stacktrace');
+// @ts-check
+const asyncHandler = require('express-async-handler');
 const path = require('path');
 const express = require('express');
 const router = express.Router({
@@ -10,14 +11,14 @@ const sqlLoader = require('../../../../prairielib/lib/sql-loader');
 
 const sql = sqlLoader.load(path.join(__dirname, '..', 'queries.sql'));
 
-router.get('/', (req, res, next) => {
-  const params = {
-    course_instance_id: res.locals.course_instance.id,
-  };
-  sqldb.queryOneRow(sql.select_course_instance_info, params, (err, result) => {
-    if (ERR(err, next)) return;
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const result = await sqldb.queryOneRowAsync(sql.select_course_instance_info, {
+      course_instance_id: res.locals.course_instance.id,
+    });
     res.status(200).send(result.rows[0].item);
-  });
-});
+  })
+);
 
 module.exports = router;
