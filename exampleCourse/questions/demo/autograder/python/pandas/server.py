66ede444fa64d1e832570f6d3898a49cbbc6d604
Nathan Walters <nwalters512@gmail.com>
2023-01-27 14:30:18 -0800

    Initial work on linting exampleCourse and testCourse Python

--- exampleCourse/questions/demo/autograder/python/pandas/server.py
+++ exampleCourse/questions/demo/autograder/python/pandas/server.py
@@ -1,11 +1,15 @@
 import numpy as np
-import prairielearn as pl
 import pandas as pd
+import prairielearn as pl
 
 
 def generate(data):
     data["params"]["names_for_user"] = []
     data["params"]["names_from_user"] = [
-        {"name": "df", "description": "dataframe as described above.", "type": "Pandas DataFrame"}
+        {
+            "name": "df",
+            "description": "dataframe as described above.",
+            "type": "Pandas DataFrame",
+        }
     ]
-    data['params']['df'] = pl.to_json(pd.DataFrame(data={'a': [1, 2], 'b': [3, 4]}))
+    data["params"]["df"] = pl.to_json(pd.DataFrame(data={"a": [1, 2], "b": [3, 4]}))
