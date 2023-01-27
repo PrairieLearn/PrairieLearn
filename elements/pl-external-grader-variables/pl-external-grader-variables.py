430ba66de58ae1bd9d45ac2cf7143cde625fd2e0
Eliot Robson <eliot.robson24@gmail.com>
2023-01-27 15:44:43 -0600

    format

--- elements/pl-external-grader-variables/pl-external-grader-variables.py
+++ elements/pl-external-grader-variables/pl-external-grader-variables.py
@@ -21,8 +21,8 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
             var_dict = {
                 "name": pl.get_string_attrib(child, "name"),
                 "type": pl.get_string_attrib(child, "type"),
-                # Handle empty string, inner_html doesn't return None
-                "description": pl.inner_html(child) or None,
+                # Use that empty string is returned, won't trigger the description column
+                "description": pl.inner_html(child),
             }
 
             html_variables.append(var_dict)
@@ -32,7 +32,7 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
 
         else:
             raise ValueError(
-                f"Tags inside of pl-external-grader-variables must be pl-variable, not '{child.tag}'."
+                f'Tags inside of pl-external-grader-variables must be pl-variable, not "{child.tag}".'
             )
 
     declared_empty = pl.get_boolean_attrib(element, "empty", EMPTY_DEFAULT)
@@ -40,18 +40,18 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
     if declared_empty:
         if html_variables:
             raise ValueError(
-                f"Variable name '{params_name}' was declared empty, but has variables defined in 'question.html'."
+                f'Variable name "{params_name}" was declared empty, but has variables defined in "question.html".'
             )
         elif params_name in data["params"]:
             raise ValueError(
-                f"Variable name '{params_name}' was declared empty, but has variables defined in 'server.py'."
+                f'Variable name "{params_name}" was declared empty, but has variables defined in "server.py".'
             )
 
         data["params"][params_name] = []
     elif params_name not in data["params"]:
         if not html_variables:
             raise ValueError(
-                f"Variable name '{params_name}' has no variables defined in 'question.html' or 'server.py'. "
+                f'Variable name "{params_name}" has no variables defined in "question.html" or "server.py". '
                 "Did you mean to set it to be empty?"
             )
 
@@ -59,7 +59,7 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
     else:
         if html_variables:
             raise ValueError(
-                f"Cannot define variables from both 'question.html' and 'server.py' for variable name '{params_name}'."
+                f'Cannot define variables from both "question.html" and "server.py" for variable name "{params_name}".'
             )
 
 
@@ -72,7 +72,7 @@ def render(element_html: str, data: pl.QuestionData) -> str:
     names_user_description = data["params"][params_name]
     has_names_user_description = len(names_user_description) > 0
 
-    # Show descriptions if any variable has them set
+    # Show descriptions if any variable has them set (non-empty and not None)
     has_descriptions = any(d.get("description", None) for d in names_user_description)
 
     html_params = {
