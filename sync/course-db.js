9d50fa31779df9bfbd61dc64b706148d33dac56c
Nathan Walters <nathan@prairielearn.com>
2023-02-01 05:10:26 -0800

    Remove workspaces S3 code (#6983)
    * Remove some S3 code from workspaces
    
    * Remove homedir_location references
    
    * Remove workspaceHomeDirLocation config option
    
    * Unzip into temp directory
    
    * Remove workspaceMainZipsDirectory from config
    
    * Remove workspaceHostZipsDirectory from config
    
    * Cleanup unused variables
    
    * Remove syncIgnore from docs and code
    
    * Fix schema
    
    * Improve schema tests
    
    ---------
    
    Co-authored-by: Jonatan Schroeder <jonatan@yorku.ca>

--- sync/course-db.js
+++ sync/course-db.js
@@ -378,7 +378,6 @@ const FILE_UUID_REGEX =
  * @property {string} home
  * @property {string} args
  * @property {string[]} gradedFiles
- * @property {string[]} syncIgnore
  * @property {string} rewriteUrl
  * @property {boolean} enableNetworking
  * @property {Record<string, string | null>} environment
