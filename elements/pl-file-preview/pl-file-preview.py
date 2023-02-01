fa7326a57dfb6b0291143d8b0951183d3a36102e
Nathan Walters <nwalters512@gmail.com>
2023-02-01 14:16:09 -0800

    Assert no attributes on pl-file-preview

--- elements/pl-file-preview/pl-file-preview.py
+++ elements/pl-file-preview/pl-file-preview.py
@@ -1,5 +1,13 @@
 import chevron
+import lxml.html
 import prairielearn as pl
+
+
+def prepare(element_html, data):
+    element = lxml.html.fragment_fromstring(element_html)
+    required_attribs = []
+    optional_attribs = []
+    pl.check_attribs(element, required_attribs, optional_attribs)
 
 
 def render(element_html: str, data: pl.QuestionData) -> str:
