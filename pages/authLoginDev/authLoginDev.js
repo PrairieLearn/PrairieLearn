67169bc10215b9e1ce52708e31a69a7391a674ea
Dave Mussulman <mussulma@gmail.com>
2023-02-02 13:51:03 -0600

    make authLoginDev for making a cookie based on devMode permissions

---
+++ pages/authLoginDev/authLoginDev.js
@@ -0,0 +1,58 @@
+const ERR = require('async-stacktrace');
+const express = require('express');
+
+const config = require('../../lib/config');
+const sqldb = require('../../prairielib/lib/sql-db');
+const sqlLoader = require('../../prairielib/lib/sql-loader');
+const csrf = require('../../lib/csrf');
+
+const sql = sqlLoader.loadSqlEquiv(__filename);
+
+var router = express.Router();
+
+router.get('/', (req, res, next) => {
+  if (!config.devMode) {
+    return next(new Error("devMode logins are not enabled"));
+  }
+
+  var authUid = config.authUid;
+  var authName = config.authName;
+  var authUin = config.authUin;
+
+  // We allow unit tests to override the user. Unit tests may also override the req_date
+  // (middlewares/date.js) and the req_mode (middlewares/authzCourseOrInstance.js).
+
+  if (req.cookies.pl_test_user === 'test_student') {
+    authUid = 'student@illinois.edu';
+    authName = 'Student User';
+    authUin = '000000001';
+  } else if (req.cookies.pl_test_user === 'test_instructor') {
+    authUid = 'instructor@illinois.edu';
+    authName = 'Instructor User';
+    authUin = '100000000';
+  }
+
+  let params = [authUid, authName, authUin, 'dev'];
+  sqldb.call('users_select_or_insert', params, (err, result) => {
+    if (ERR(err, next)) return;
+
+    var tokenData = {
+      user_id: result.rows[0].user_id,
+      authn_provider_name: 'devMode',
+    };
+    var pl_authn = csrf.generateToken(tokenData, config.secretKey);
+    res.cookie('pl_authn', pl_authn, {
+      maxAge: config.authnCookieMaxAgeMilliseconds,
+      httpOnly: true,
+      secure: true,
+    });
+    var redirUrl = res.locals.homeUrl;
+    if ('preAuthUrl' in req.cookies) {
+      redirUrl = req.cookies.preAuthUrl;
+      res.clearCookie('preAuthUrl');
+    }
+    res.redirect(redirUrl);
+  });
+});
+
+module.exports = router;
