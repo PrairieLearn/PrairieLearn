f422433987ad21e0ac615b49ab5680d230b1aee5
Dave Mussulman <mussulma@gmail.com>
2023-02-02 14:42:56 -0600

    init.sh migrate before start

--- docker/init.sh
+++ docker/init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo 'Starting PrairieLearn...'
+
+make -s -C /PrairieLearn start-support
+cd /PrairieLearn
+node server.js --migrate-and-exit >/dev/null
+
 if [[ $NODEMON == "true" ]]; then
     # start-nodemon is listed first so it can use standard input
     make -s -C /PrairieLearn -j 2 start-nodemon start-workspace-host-nodemon
