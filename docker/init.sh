bef7c191f5da6a593a7cd60ed159065525787090
Dave Mussulman <mussulma@gmail.com>
2023-02-03 13:17:25 -0600

    refactor init.sh after cd /PrairieLearn

--- docker/init.sh
+++ docker/init.sh
@@ -2,13 +2,13 @@
 
 echo 'Starting PrairieLearn...'
 
-make -s -C /PrairieLearn start-support
 cd /PrairieLearn
+make -s start-support
 node server.js --migrate-and-exit >/dev/null
 
 if [[ $NODEMON == "true" ]]; then
     # start-nodemon is listed first so it can use standard input
-    make -s -C /PrairieLearn -j 2 start-nodemon start-workspace-host-nodemon
+    make -s -j 2 start-nodemon start-workspace-host-nodemon
 else
-    make -s -C /PrairieLearn -j 2 start start-workspace-host
+    make -s -j 2 start start-workspace-host
 fi
