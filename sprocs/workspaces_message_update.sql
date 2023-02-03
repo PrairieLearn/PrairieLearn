be7f435118fbab665a3fe6a2033de91dddfbb3b3
Nathan Walters <nwalters512@gmail.com>
2023-02-03 09:42:14 -0800

    Rewrite sproc to use single CTE

--- sprocs/workspaces_message_update.sql
+++ sprocs/workspaces_message_update.sql
@@ -4,22 +4,21 @@ CREATE FUNCTION
         workspace_message text
     ) returns void
 AS $$
-DECLARE
-    workspace_version bigint;
 BEGIN
-    UPDATE
-        workspaces as w
-    SET
-        message = workspace_message,
-        message_updated_at = now()
-    WHERE
-        w.id = workspace_id
-    RETURNING
-        w.version INTO workspace_version;
-
+    WITH workspace AS (
+        UPDATE
+            workspaces as w
+        SET
+            message = workspace_message,
+            message_updated_at = now()
+        WHERE
+            w.id = workspace_id
+        RETURNING
+            w.version
+    )
     INSERT INTO workspace_logs
         (date, workspace_id, version, message)
     VALUES
-        (now(), workspace_id, workspace_version, workspace_message);
+        (now(), workspace_id, (SELECT version FROM workspace), workspace_message);
 END;
 $$ LANGUAGE plpgsql VOLATILE;
