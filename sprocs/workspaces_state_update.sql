91f31cfc5a97e36dff14c23b8128c5da912d66b4
Nathan Walters <nwalters512@gmail.com>
2023-02-03 09:30:50 -0800

    Rewrite sproc as single query

--- sprocs/workspaces_state_update.sql
+++ sprocs/workspaces_state_update.sql
@@ -5,46 +5,35 @@ CREATE FUNCTION
         workspace_message text
     ) returns void
 AS $$
-DECLARE
-    old_workspace workspaces;
-    launching_delta interval = '0 seconds';
-    running_delta interval = '0 seconds';
 BEGIN
-    -- Compute the duration deltas to add
-
-    SELECT * INTO old_workspace FROM workspaces WHERE id = workspace_id;
-
-    IF old_workspace.state = 'launching' THEN
-        launching_delta = now() - old_workspace.state_updated_at;
-    END IF;
-
-    IF old_workspace.state = 'running' THEN
-        running_delta = now() - old_workspace.state_updated_at;
-    END IF;
-
-    -- Update the workspace itself
-
-    UPDATE
-        workspaces as w
-    SET
-        state = workspace_state,
-        state_updated_at = now(),
-        message = workspace_message,
-        message_updated_at = now(),
-        launched_at = CASE WHEN workspace_state = 'launching' THEN now() ELSE launched_at END,
-        heartbeat_at = CASE WHEN workspace_state = 'running' THEN now() ELSE heartbeat_at END,
-        running_at = CASE WHEN workspace_state = 'running' THEN now() ELSE running_at END,
-        stopped_at = CASE WHEN workspace_state = 'stopped' THEN now() ELSE stopped_at END,
-        launching_duration = launching_duration + launching_delta,
-        running_duration = running_duration + running_delta
-    WHERE
-        w.id = workspace_id;
-
-    -- Log it
-
+    WITH old_workspace AS (
+        SELECT * FROM workspaces WHERE id = workspace_id
+    ), deltas AS (
+        SELECT
+            CASE WHEN old_workspace.state = 'launching' THEN now() - old_workspace.state_updated_at ELSE '0 seconds'::interval END AS launching_delta,
+            CASE WHEN old_workspace.state = 'running' THEN now() - old_workspace.state_updated_at ELSE '0 seconds'::interval END AS running_delta
+        FROM old_workspace
+    ), updated_workspace AS (
+        UPDATE
+            workspaces as w
+        SET
+            state = workspace_state,
+            state_updated_at = now(),
+            message = workspace_message,
+            message_updated_at = now(),
+            launched_at = CASE WHEN workspace_state = 'launching' THEN now() ELSE launched_at END,
+            heartbeat_at = CASE WHEN workspace_state = 'running' THEN now() ELSE heartbeat_at END,
+            running_at = CASE WHEN workspace_state = 'running' THEN now() ELSE running_at END,
+            stopped_at = CASE WHEN workspace_state = 'stopped' THEN now() ELSE stopped_at END,
+            launching_duration = launching_duration + (SELECT launching_delta FROM deltas),
+            running_duration = running_duration + (SELECT running_delta FROM deltas)
+        WHERE
+            w.id = workspace_id
+        RETURNING *
+    )
     INSERT INTO workspace_logs
         (date, workspace_id, version, state, message)
     VALUES
-        (now(), workspace_id, old_workspace.version, workspace_state, workspace_message);
+        (now(), workspace_id, (SELECT version FROM old_workspace), workspace_state, workspace_message);
 END;
 $$ LANGUAGE plpgsql VOLATILE;
