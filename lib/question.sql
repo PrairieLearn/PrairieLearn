2a7d2942d307cc6dd514ad6fb0f5dd3ea533ff01
Nathan Walters <nwalters512@gmail.com>
2023-02-03 16:43:58 -0800

    Fix question test issue checking

--- lib/question.sql
+++ lib/question.sql
@@ -101,8 +101,8 @@ WHERE
 ORDER BY
    s.date DESC;
 
--- BLOCK select_issues_for_variant
-SELECT COUNT(*)
+-- BLOCK select_issue_count_for_variant
+SELECT COUNT(*)::int
 FROM issues AS i
 WHERE i.variant_id = $variant_id;
 
