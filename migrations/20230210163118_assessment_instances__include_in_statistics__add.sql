ec80e3baf401c6d208776a005326eaa14dd3f853
Matthew West <mwest@illinois.edu>
2023-02-10 11:31:19 -0600

    Fix migrations

--- migrations/20230210163118_assessment_instances__include_in_statistics__add.sql
+++ migrations/20230210163118_assessment_instances__include_in_statistics__add.sql
@@ -1,1 +1,1 @@
-ALTER TABLE assessment_instances ADD COLUMN IF EXISTS NOT EXISTS include_in_statistics boolean NOT NULL DEFAULT TRUE;
+ALTER TABLE assessment_instances ADD COLUMN IF NOT EXISTS include_in_statistics boolean NOT NULL DEFAULT TRUE;
--- migrations/20230210165254_assessment_instances__include_in_statistics__update.sql
+++ migrations/20230210165254_assessment_instances__include_in_statistics__update.sql
@@ -1,6 +1,6 @@
 UPDATE assessment_instances AS ai
-SET include_in_statistics = NOT users_is_instructor_in_course_instance(ai.user_id, a.course_instance_id);
+SET include_in_statistics = NOT users_is_instructor_in_course_instance(ai.user_id, a.course_instance_id)
 FROM assessments AS a
 WHERE
     ai.assessment_id = a.id
-    AND a.date > now() - interval '4 months';
+    AND ai.date > now() - interval '4 months';
