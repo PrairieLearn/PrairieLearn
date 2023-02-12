d830fe5e4cc3f4b2e605ad01d570ed59b1414a0b
Dave Mussulman <mussulma@gmail.com>
2023-02-11 19:20:12 -0600

    fixes based on master changes

--- pages/authCallbackOAuth2/authCallbackOAuth2.js
+++ pages/authCallbackOAuth2/authCallbackOAuth2.js
@@ -16,7 +16,7 @@ router.get(
   '/',
   asyncHandler(async (req, res, _next) => {
     if (!config.hasOauth) throw new Error('Google login is not enabled');
-    const code = req.query.code;
+    const code = String(req.query.code);
     if (code == null) {
       throw new Error('No "code" query parameter for authCallbackOAuth2');
     }
@@ -29,7 +29,7 @@ router.get(
     );
 
     logger.verbose('Got Google auth with code: ' + code);
-    const token_response = await oauth2Client.getToken(code).catch((err) => {
+    const getTokenRes = await oauth2Client.getToken(code).catch((err) => {
       if (err?.response) {
         // This is probably a detailed error from the Google API client. We'll
         // pick off the useful bits and attach them to the Sentry scope so that
@@ -44,18 +44,12 @@ router.get(
       throw err;
     });
 
-    oauth2Client.setCredentials(token_response);
-    const tokenInfo = await oauth2Client.getTokenInfo(
-      oauth2Client.credentials.access_token
-    );
-    console.log(tokenInfo);
-    logger.verbose('Got Google auth tokens: ' + JSON.stringify(tokens));
-    // SHOULD RE REMOVED oauth2Client.credentials = tokens;
+    logger.verbose('Got Google auth tokens: ' + JSON.stringify(getTokenRes.tokens));
     // tokens.id_token is a JWT (JSON Web Token)
     // http://openid.net/specs/draft-jones-json-web-token-07.html
     // A JWT has the form HEADER.PAYLOAD.SIGNATURE
     // We get the PAYLOAD, un-base64, parse to JSON:
-    const parts = tokens.id_token.split('.');
+    const parts = getTokenRes.tokens.id_token.split('.');
     identity = JSON.parse(Buffer.from(parts[1], 'base64').toString('utf-8'));
     logger.verbose('Got Google auth identity: ' + JSON.stringify(identity));
     assert(identity.email);
