805b41cbe06126c0d2f1d4a343cb49b5cddfc4cf
Jonatan Schroeder <jonatan@yorku.ca>
2023-02-13 15:53:31 -0500

    Reformat SQL files

--- lib/score-upload.sql
+++ lib/score-upload.sql
@@ -1,61 +1,69 @@
 -- BLOCK select_assessment_info
 SELECT
-    assessment_label(a, aset),
-    ci.id AS course_instance_id,
-    c.id AS course_id
+  assessment_label (a, aset),
+  ci.id AS course_instance_id,
+  c.id AS course_id
 FROM
-    assessments AS a
-    JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
-    JOIN course_instances AS ci ON (ci.id = a.course_instance_id)
-    JOIN pl_courses AS c ON (c.id = ci.course_id)
+  assessments AS a
+  JOIN assessment_sets AS aset ON (aset.id = a.assessment_set_id)
+  JOIN course_instances AS ci ON (ci.id = a.course_instance_id)
+  JOIN pl_courses AS c ON (c.id = ci.course_id)
 WHERE
-    a.id = $assessment_id;
+  a.id = $assessment_id;
 
 -- BLOCK select_assessment_instance_uid
 SELECT
-    ai.id AS assessment_instance_id
+  ai.id AS assessment_instance_id
 FROM
-    assessment_instances AS ai
-    JOIN users AS u ON (u.user_id = ai.user_id)
+  assessment_instances AS ai
+  JOIN users AS u ON (u.user_id = ai.user_id)
 WHERE
-    ai.assessment_id = $assessment_id
-    AND ai.number = $instance_number
-    AND u.uid = $uid;
+  ai.assessment_id = $assessment_id
+  AND ai.number = $instance_number
+  AND u.uid = $uid;
 
 -- BLOCK select_assessment_instance_group
 SELECT
-    ai.id AS assessment_instance_id
+  ai.id AS assessment_instance_id
 FROM
-    assessment_instances AS ai
-    JOIN groups AS g ON (g.id = ai.group_id)
+  assessment_instances AS ai
+  JOIN groups AS g ON (g.id = ai.group_id)
 WHERE
-    ai.assessment_id = $assessment_id
-    AND ai.number = $instance_number
-    AND g.name = $group_name;
+  ai.assessment_id = $assessment_id
+  AND ai.number = $instance_number
+  AND g.name = $group_name;
 
 -- BLOCK select_submission_to_update
 SELECT
-    s.id AS submission_id,
-    iq.id AS instance_question_id,
-    COALESCE(g.name, u.uid) AS uid_or_group,
-    q.qid
+  s.id AS submission_id,
+  iq.id AS instance_question_id,
+  COALESCE(g.name, u.uid) AS uid_or_group,
+  q.qid
 FROM
-    instance_questions AS iq
-    JOIN assessment_questions AS aq ON (aq.id = iq.assessment_question_id)
-    JOIN questions AS q on (q.id = aq.question_id)
-    JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
-    LEFT JOIN groups AS g ON (g.id = ai.group_id AND g.deleted_at IS NULL)
-    LEFT JOIN users AS u ON (u.user_id = ai.user_id)
-    LEFT JOIN variants AS v ON (v.instance_question_id = iq.id)
-    LEFT JOIN submissions AS s ON (s.variant_id = v.id)
+  instance_questions AS iq
+  JOIN assessment_questions AS aq ON (aq.id = iq.assessment_question_id)
+  JOIN questions AS q on (q.id = aq.question_id)
+  JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
+  LEFT JOIN groups AS g ON (
+    g.id = ai.group_id
+    AND g.deleted_at IS NULL
+  )
+  LEFT JOIN users AS u ON (u.user_id = ai.user_id)
+  LEFT JOIN variants AS v ON (v.instance_question_id = iq.id)
+  LEFT JOIN submissions AS s ON (s.variant_id = v.id)
 WHERE
-    ai.assessment_id = $assessment_id
-    AND (
-        s.id = $submission_id
-        OR ($submission_id IS NULL
-            AND COALESCE(g.name, u.uid) = $uid_or_group
-            AND ai.number = $ai_number
-            AND q.qid = $qid)
+  ai.assessment_id = $assessment_id
+  AND (
+    s.id = $submission_id
+    OR (
+      $submission_id IS NULL
+      AND COALESCE(g.name, u.uid) = $uid_or_group
+      AND ai.number = $ai_number
+      AND q.qid = $qid
     )
-ORDER BY s.date DESC, ai.number DESC
-LIMIT 1;
+  )
+ORDER BY
+  s.date DESC,
+  ai.number DESC
+LIMIT
+  1;
--- migrations/20220905173159__rubrics__create.sql
+++ migrations/20220905173159__rubrics__create.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS rubrics (
+CREATE TABLE IF NOT EXISTS
+  rubrics (
     id BIGSERIAL PRIMARY KEY,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     starting_points DOUBLE PRECISION NOT NULL,
@@ -6,11 +7,12 @@ CREATE TABLE IF NOT EXISTS rubrics (
     min_points DOUBLE PRECISION NOT NULL,
     modified_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP WITH TIME ZONE
-);
+  );
 
-CREATE TABLE IF NOT EXISTS rubric_items (
+CREATE TABLE IF NOT EXISTS
+  rubric_items (
     id BIGSERIAL PRIMARY KEY,
-    rubric_id BIGINT NOT NULL REFERENCES rubrics(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    rubric_id BIGINT NOT NULL REFERENCES rubrics (id) ON DELETE CASCADE ON UPDATE CASCADE,
     number BIGINT NOT NULL,
     points DOUBLE PRECISION NOT NULL,
     short_text TEXT NOT NULL,
@@ -18,38 +20,49 @@ CREATE TABLE IF NOT EXISTS rubric_items (
     staff_instructions TEXT,
     key_binding TEXT,
     deleted_at TIMESTAMP WITH TIME ZONE
-);
+  );
 
-CREATE INDEX IF NOT EXISTS rubric_items_rubric_id ON rubric_items(rubric_id);
+CREATE INDEX IF NOT EXISTS rubric_items_rubric_id ON rubric_items (rubric_id);
 
-CREATE TABLE IF NOT EXISTS rubric_gradings (
+CREATE TABLE IF NOT EXISTS
+  rubric_gradings (
     id BIGSERIAL PRIMARY KEY,
-    rubric_id BIGINT NOT NULL REFERENCES rubrics(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    rubric_id BIGINT NOT NULL REFERENCES rubrics (id) ON DELETE CASCADE ON UPDATE CASCADE,
     computed_points DOUBLE PRECISION NOT NULL,
     adjust_points DOUBLE PRECISION NOT NULL,
     starting_points DOUBLE PRECISION NOT NULL,
     max_points DOUBLE PRECISION NOT NULL,
     min_points DOUBLE PRECISION NOT NULL
-);
+  );
 
-CREATE TABLE IF NOT EXISTS rubric_grading_items (
+CREATE TABLE IF NOT EXISTS
+  rubric_grading_items (
     id BIGSERIAL PRIMARY KEY,
-    rubric_grading_id BIGINT NOT NULL REFERENCES rubric_gradings(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    rubric_item_id BIGINT NOT NULL REFERENCES rubric_items(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    rubric_grading_id BIGINT NOT NULL REFERENCES rubric_gradings (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    rubric_item_id BIGINT NOT NULL REFERENCES rubric_items (id) ON DELETE CASCADE ON UPDATE CASCADE,
     score DOUBLE PRECISION NOT NULL DEFAULT 1,
     points DOUBLE PRECISION NOT NULL,
     short_text TEXT NOT NULL,
     note TEXT
-);
+  );
 
-ALTER TABLE rubric_grading_items ADD CONSTRAINT rubric_grading_items_rubric_grading_id_rubric_item_id_key UNIQUE (rubric_grading_id, rubric_item_id);
+ALTER TABLE rubric_grading_items
+ADD CONSTRAINT rubric_grading_items_rubric_grading_id_rubric_item_id_key UNIQUE (rubric_grading_id, rubric_item_id);
 
-ALTER TABLE assessment_questions ADD COLUMN IF NOT EXISTS manual_rubric_id BIGINT REFERENCES rubrics(id) ON DELETE SET NULL ON UPDATE CASCADE;
-ALTER TABLE assessment_questions ADD COLUMN IF NOT EXISTS auto_rubric_id BIGINT REFERENCES rubrics(id) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE assessment_questions
+ADD COLUMN IF NOT EXISTS manual_rubric_id BIGINT REFERENCES rubrics (id) ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE submissions ADD COLUMN IF NOT EXISTS manual_rubric_grading_id BIGINT REFERENCES rubric_gradings(id) ON DELETE SET NULL ON UPDATE CASCADE;
-ALTER TABLE submissions ADD COLUMN IF NOT EXISTS auto_rubric_grading_id BIGINT REFERENCES rubric_gradings(id) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE assessment_questions
+ADD COLUMN IF NOT EXISTS auto_rubric_id BIGINT REFERENCES rubrics (id) ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE grading_jobs ADD COLUMN IF NOT EXISTS manual_rubric_grading_id BIGINT REFERENCES rubric_gradings(id) ON DELETE SET NULL ON UPDATE CASCADE;
-ALTER TABLE grading_jobs ADD COLUMN IF NOT EXISTS auto_rubric_grading_id BIGINT REFERENCES rubric_gradings(id) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE submissions
+ADD COLUMN IF NOT EXISTS manual_rubric_grading_id BIGINT REFERENCES rubric_gradings (id) ON DELETE SET NULL ON UPDATE CASCADE;
 
+ALTER TABLE submissions
+ADD COLUMN IF NOT EXISTS auto_rubric_grading_id BIGINT REFERENCES rubric_gradings (id) ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE grading_jobs
+ADD COLUMN IF NOT EXISTS manual_rubric_grading_id BIGINT REFERENCES rubric_gradings (id) ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE grading_jobs
+ADD COLUMN IF NOT EXISTS auto_rubric_grading_id BIGINT REFERENCES rubric_gradings (id) ON DELETE SET NULL ON UPDATE CASCADE;
