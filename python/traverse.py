0a731602fd2b035e517e9f3429a8fe9184b04d9c
Nathan Walters <nwalters512@gmail.com>
2023-02-13 11:50:22 -0800

    Add iterative implementation from Eliot

--- python/traverse.py
+++ python/traverse.py
@@ -1,4 +1,6 @@
-from typing import Callable, List, Optional, Union
+from collections import deque
+from typing import Callable, Deque, List, Optional, Tuple, TypedDict, Union
+import timeit
 
 import lxml.html
 
@@ -17,12 +19,12 @@ def append_text(element: lxml.html.HtmlElement, text: str) -> None:
     stripped_text = text.lstrip()
 
     if element.text:
-        print("appending element text", text)
+        # print("appending element text", text)
         stripped_element_text = element.text.rstrip()
         sep = " " if stripped_element_text and stripped_text else ""
         element.text = stripped_element_text + sep + stripped_text
     else:
-        print("setting element text", text)
+        # print("setting element text", text)
         element.text = stripped_text
 
 
@@ -30,12 +32,12 @@ def append_tail(element: lxml.html.HtmlElement, text: str) -> None:
     stripped_text = text.lstrip()
 
     if element.tail:
-        print("appending element tail", text)
+        # print("appending element tail", text)
         stripped_element_tail = element.tail.rstrip()
         sep = " " if stripped_element_tail and stripped_text else ""
         element.tail = stripped_element_tail + sep + stripped_text
     else:
-        print("setting element tail", text)
+        # print("setting element tail", text)
         element.tail = " " + stripped_text
 
 
@@ -57,18 +59,18 @@ def traverse_and_execute(
 
     root_parent = elements[0].getparent()
     children = root_parent.getchildren()
-    print("traversing into element", root_parent)
+    # print("traversing into element", root_parent)
     for element in children:
         traverse_and_execute_impl(element, fn)
 
-    print("root_parent", root_parent, lxml.html.tostring(root_parent))
-    print("root_parent.getchildren()", root_parent.getchildren())
+    # print("root_parent", root_parent, lxml.html.tostring(root_parent))
+    # print("root_parent.getchildren()", root_parent.getchildren())
 
     result_elements = root_parent.getchildren()
     if root_parent.text:
         result_elements.insert(0, root_parent.text)
 
-    print("result_elements", result_elements)
+    # print("result_elements", result_elements)
     return result_elements
 
 
@@ -77,7 +79,7 @@ def traverse_and_execute_impl(
     fn: Callable[[lxml.html.HtmlElement], Union[None, List[lxml.html.HtmlElement]]],
 ) -> None:
     new_element = fn(element)
-    print("new_element", new_element)
+    # print("new_element", new_element)
     has_new_element = new_element is not element and (
         new_element is not None
         and len(new_element) > 0
@@ -136,24 +138,24 @@ def traverse_and_replace(
                 parent.remove(element)
                 return None
 
-            print("-------")
-            print("new_elements", new_elements)
-            print(
-                "element",
-                element,
-                lxml.html.tostring(element),
-                element.text,
-                element.tail,
-            )
-            print("parent", lxml.html.tostring(parent), parent.text, parent.tail)
+            # print("-------")
+            # print("new_elements", new_elements)
+            # print(
+            #     "element",
+            #     element,
+            #     lxml.html.tostring(element),
+            #     element.text,
+            #     element.tail,
+            # )
+            # print("parent", lxml.html.tostring(parent), parent.text, parent.tail)
 
             # Special case: the first new element is just a string.
             if isinstance(new_elements[0], str):
                 if self_index == 0:
                     # We need to attach this string to the `text` of the parent.
-                    print("attaching to parent")
+                    # print("attaching to parent")
                     append_text(parent, new_elements[0])
-                    print("parent", lxml.html.tostring(parent))
+                    # print("parent", lxml.html.tostring(parent))
                 else:
                     # We need to attach this string to the `tail` of the previous element.
                     print("attaching to previous child")
@@ -161,7 +163,7 @@ def traverse_and_replace(
                     print("parent", lxml.html.tostring(parent))
                 del new_elements[0]
 
-            print("len(parent)", len(parent))
+            # print("len(parent)", len(parent))
 
             # Special case: the element we're replacing has trailing text.
             if len(parent) > 1 and element.tail is not None:
@@ -169,15 +171,15 @@ def traverse_and_replace(
                 if self_index == 0:
                     if last_new_element is not None:
                         # We need to attach this string to the `text` of the parent.
-                        print("attaching tail to last new element")
+                        # print("attaching tail to last new element")
                         append_tail(last_new_element, element.tail)
-                        print("last_new_element", lxml.html.tostring(last_new_element))
+                        # print("last_new_element", lxml.html.tostring(last_new_element))
                     else:
-                        print("attaching tail to parent text")
+                        # print("attaching tail to parent text")
                         append_text(parent, element.tail)
                 else:
                     # We need to attach this string to the `tail` of the previous element.
-                    print("attaching tail to previous child (multi)")
+                    # print("attaching tail to previous child (multi)")
                     append_tail(previous_element, element.tail)
 
             # Special case: our parent has only a single child (us) and the
@@ -186,32 +188,203 @@ def traverse_and_replace(
             # parent's text.
             if len(parent) == 1 and element.tail is not None:
                 if len(new_elements) > 0:
-                    print("attaching tail")
-                    print(
-                        "new_elements[-1]",
-                        lxml.html.tostring(new_elements[-1]),
-                        new_elements[-1].tail,
-                    )
-                    print("element", lxml.html.tostring(element), element.tail)
+                    # print("attaching tail")
+                    # print(
+                    #     "new_elements[-1]",
+                    #     lxml.html.tostring(new_elements[-1]),
+                    #     new_elements[-1].tail,
+                    # )
+                    # print("element", lxml.html.tostring(element), element.tail)
                     append_tail(new_elements[-1], element.tail)
                 else:
-                    print("attaching tail to parent")
+                    # print("attaching tail to parent")
                     append_text(parent, element.tail)
-                print("parent", lxml.html.tostring(parent))
+                # print("parent", lxml.html.tostring(parent))
 
             parent.remove(element)
-            print("parent after removal", lxml.html.tostring(parent))
-            print("iterating over elements", new_elements, type(new_elements))
+            # print("parent after removal", lxml.html.tostring(parent))
+            # print("iterating over elements", new_elements, type(new_elements))
             for index, new_element in enumerate(new_elements):
-                print("iteration", index, new_element)
+                # print("iteration", index, new_element)
                 parent.insert(self_index + index, new_element)
-            print("parent after insertion", lxml.html.tostring(parent))
+            # print("parent after insertion", lxml.html.tostring(parent))
 
             return new_elements
 
     elements = traverse_and_execute(html, handle_element)
 
     return "".join(serialize_str_or_element(e) for e in elements)
+
+
+def get_source_definition(element: lxml.html.HtmlElement) -> str:
+    return (
+        "<"
+        + " ".join((element.tag, *(f'{k}="{v}"' for k, v in element.attrib.items())))
+        + ">"
+    )
+
+
+def traverse_and_replace_new(
+    html: str, replace: Callable[[lxml.html.HtmlElement], ElementReplacement]
+) -> str:
+    # Initialize result and work data structures
+    result: Deque[Union[str, lxml.html.HtmlElement]] = deque()
+
+    initial_list = lxml.html.fragments_fromstring(html)
+    count_stack: Deque[int] = deque([len(initial_list)])
+    work_stack: Deque[Union[str, lxml.html.HtmlElement]] = deque(reversed(initial_list))
+    tail_stack: Deque[Tuple[str, Optional[str]]] = deque()
+
+    while work_stack:
+        element = work_stack.pop()
+
+        # For just a string, append to final result
+        if isinstance(element, str):
+            result.append(element)
+
+        else:
+            new_elements = replace(element)
+
+            # Turn new_elements into a list containing we can process
+            if new_elements is None:
+                new_elements = []
+            elif isinstance(new_elements, str):
+                fragments = lxml.html.fragments_fromstring(new_elements)
+                new_elements = fragments if fragments is not None else []
+
+            if isinstance(new_elements, list):
+                # Modify count stack for new elements and decrement for element that was replaced
+                count_stack[-1] += len(new_elements) - 1
+
+                # Add element tail before processing replaced element
+                if element.tail is not None:
+                    count_stack[-1] += 1
+                    work_stack.append(element.tail)
+
+                # Extend and go to the next iteration
+                if new_elements:
+                    work_stack.extend(reversed(new_elements))
+
+                continue
+
+            # Add opening tag and text
+            result.append(get_source_definition(new_elements))
+
+            if new_elements.text is not None:
+                result.append(new_elements.text)
+
+            # Add all children to the work stack
+            children = list(new_elements)
+
+            if children:
+                work_stack.extend(reversed(children))
+
+            count_stack.append(len(children))
+            tail_stack.append((new_elements.tag, element.tail))
+
+        # Close all closable tags
+        while count_stack[-1] == 0:
+            count_stack.pop()
+            tail_tag, tail_text = tail_stack.pop()
+
+            result.append(f"</{tail_tag}>")
+            if tail_text is not None:
+                result.append(tail_text)
+        else:
+            count_stack[-1] -= 1
+
+    # No need to empty tail stack, should be empty from above
+    # can add asserts that the tail stack and count stack are
+    # empty here to debug
+
+    return "".join(result)
+
+
+class ElementWithChildren(TypedDict):
+    element: lxml.html.HtmlElement
+
+
+class Node:
+    def __init__(self, element: Union[lxml.html.Element, str]):
+        pass
+
+
+
+def traverse_and_replace_exp(
+    html: str, replace: Callable[[lxml.html.HtmlElement], ElementReplacement]
+) -> str:
+
+    # Initialize result and work data structures
+    result: Deque[Union[str, lxml.html.HtmlElement]] = deque()
+
+    initial_list = lxml.html.fragments_fromstring(html)
+    count_stack: Deque[int] = deque([len(initial_list)])
+    work_stack: Deque[Union[str, lxml.html.HtmlElement]] = deque(reversed(initial_list))
+    tail_stack: Deque[Tuple[str, Optional[str]]] = deque()
+
+    while work_stack:
+        element = work_stack.pop()
+
+        # For just a string, append to final result
+        if isinstance(element, str):
+            result.append(element)
+
+        else:
+            new_elements = replace(element)
+
+            # Turn new_elements into a list containing we can process
+            if new_elements is None:
+                new_elements = []
+            elif isinstance(new_elements, str):
+                fragments = lxml.html.fragments_fromstring(new_elements)
+                new_elements = fragments if fragments is not None else []
+
+            if isinstance(new_elements, list):
+                # Modify count stack for new elements and decrement for element that was replaced
+                count_stack[-1] += len(new_elements) - 1
+
+                # Add element tail before processing replaced element
+                if element.tail is not None:
+                    count_stack[-1] += 1
+                    work_stack.append(element.tail)
+
+                # Extend and go to the next iteration
+                if new_elements:
+                    work_stack.extend(reversed(new_elements))
+
+                continue
+
+            # Add opening tag and text
+            result.append(get_source_definition(new_elements))
+
+            if new_elements.text is not None:
+                result.append(new_elements.text)
+
+            # Add all children to the work stack
+            children = list(new_elements)
+
+            if children:
+                work_stack.extend(reversed(children))
+
+            count_stack.append(len(children))
+            tail_stack.append((new_elements.tag, element.tail))
+
+        # Close all closable tags
+        while count_stack[-1] == 0:
+            count_stack.pop()
+            tail_tag, tail_text = tail_stack.pop()
+
+            result.append(f"</{tail_tag}>")
+            if tail_text is not None:
+                result.append(tail_text)
+        else:
+            count_stack[-1] -= 1
+
+    # No need to empty tail stack, should be empty from above
+    # can add asserts that the tail stack and count stack are
+    # empty here to debug
+
+    return "".join(result)
 
 
 if __name__ == "__main__":
@@ -222,6 +395,26 @@ if __name__ == "__main__":
     </pl-question-panel>
 
     <pl-integer-input answers-name="c" label="$c=$"></pl-integer-input>
+
+    <pl-answer-panel>
+      <pl-code-tabs filename="foo.py"></pl-code-tabs>
+    </pl-answer-panel>
     """
 
-    print(traverse_and_replace(html, lambda e: e))
+    def replace(e: lxml.html.Element) -> str:
+        if e.tag == "<pl-integer-input>":
+            return "<pl-number-input></pl-number-input>"
+        if e.tag == "pl-number-input":
+            return '<input type="number" />'
+        if e.tag == "pl-code-tabs":
+            return "<div><pl-code></pl-code><pl-code></pl-code></div>"
+        if e.tag == "pl-code":
+            return "<pre><code>foo</code></pre>"
+
+    original = timeit.timeit(lambda: traverse_and_replace(html, replace), number=1000)
+
+    print("original", original)
+
+    new = timeit.timeit(lambda: traverse_and_replace_new(html, replace), number=1000)
+
+    print("new", new)
