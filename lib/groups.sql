49723764dd61f4d50dc127771e0d2cd4be5b67cc
caleqw2 <caleqw2@illinois.edu>
2023-02-14 00:35:14 -0600

    Run prettier

--- lib/groups.sql
+++ lib/groups.sql
@@ -19,45 +19,80 @@ WITH
         FROM
           group_configs AS gc
         WHERE
-            gc.assessment_id = $assessment_id
-            AND gc.deleted_at IS NULL
-    )
-    RETURNING id
-),
-create_log AS (
-    INSERT INTO group_logs
-        (authn_user_id, user_id, group_id, action)
-    SELECT $authn_user_id, $user_id, cg.id, 'create' FROM create_group AS cg
-),
-get_role AS (
+          gc.assessment_id = $assessment_id
+          AND gc.deleted_at IS NULL
+      )
+    RETURNING
+      id
+  ),
+  create_log AS (
+    INSERT INTO
+      group_logs (authn_user_id, user_id, group_id, action)
     SELECT
-        gr.id AS role_id
+      $authn_user_id,
+      $user_id,
+      cg.id,
+      'create'
     FROM
-        group_roles AS gr
+      create_group AS cg
+  ),
+  get_role AS (
+    SELECT
+      gr.id AS role_id
+    FROM
+      group_roles AS gr
     WHERE
-        (gr.assessment_id = $assessment_id AND gr.can_assign_roles_at_start)
-    ORDER BY gr.maximum
-    LIMIT 1
-),
-join_group AS (
-    INSERT INTO group_users
-        (user_id, group_id)
-    SELECT $user_id, cg.id 
-    FROM create_group AS cg
-),
-assign_role AS (
-    INSERT INTO group_user_roles
-        (user_id, group_role_id, group_id)
-    SELECT $user_id, gr.role_id, cg.id 
-    FROM create_group AS cg, (SELECT * FROM get_role) as gr
-)
-INSERT INTO group_logs
-    (authn_user_id, user_id, group_id, action)
-SELECT $authn_user_id, $user_id, cg.id, 'join' FROM create_group AS cg;
+      (
+        gr.assessment_id = $assessment_id
+        AND gr.can_assign_roles_at_start
+      )
+    ORDER BY
+      gr.maximum
+    LIMIT
+      1
+  ),
+  join_group AS (
+    INSERT INTO
+      group_users (user_id, group_id)
+    SELECT
+      $user_id,
+      cg.id
+    FROM
+      create_group AS cg
+  ),
+  assign_role AS (
+    INSERT INTO
+      group_user_roles (user_id, group_role_id, group_id)
+    SELECT
+      $user_id,
+      gr.role_id,
+      cg.id
+    FROM
+      create_group AS cg,
+      (
+        SELECT
+          *
+        FROM
+          get_role
+      ) as gr
+  )
+INSERT INTO
+  group_logs (authn_user_id, user_id, group_id, action)
+SELECT
+  $authn_user_id,
+  $user_id,
+  cg.id,
+  'join'
+FROM
+  create_group AS cg;
 
 -- BLOCK get_group_members
-SELECT
-    DISTINCT u.uid, u.user_id, gu.group_id, g.name AS group_name, g.join_code
+SELECT DISTINCT
+  u.uid,
+  u.user_id,
+  gu.group_id,
+  g.name AS group_name,
+  g.join_code
 FROM
   assessments AS a
   JOIN group_configs AS gc ON gc.assessment_id = a.id
@@ -66,34 +101,59 @@ FROM
   JOIN group_users AS gu2 ON gu2.group_id = gu.group_id
   JOIN users AS u ON u.user_id = gu2.user_id
 WHERE
-    a.id = $assessment_id
-    AND gu.user_id = $user_id
-    AND g.deleted_at IS NULL
-    AND gc.deleted_at IS NULL
-
--- BLOCK get_group_roles
-SELECT gr.id::INTEGER, gr.role_name, COUNT(gu.user_id)::INTEGER AS count, gr.maximum, gr.minimum
-FROM (SELECT * FROM group_roles WHERE assessment_id = $assessment_id) AS gr 
-LEFT JOIN (
-    SELECT * FROM group_user_roles
-    WHERE group_id = $group_id
-    ) AS gu ON gu.group_role_id = gr.id
-GROUP BY gr.id, maximum, minimum, role_name;
+  a.id = $assessment_id
+  AND gu.user_id = $user_id
+  AND g.deleted_at IS NULL
+  AND gc.deleted_at IS NULL
+  -- BLOCK get_group_roles
+SELECT
+  gr.id::INTEGER,
+  gr.role_name,
+  COUNT(gu.user_id)::INTEGER AS count,
+  gr.maximum,
+  gr.minimum
+FROM
+  (
+    SELECT
+      *
+    FROM
+      group_roles
+    WHERE
+      assessment_id = $assessment_id
+  ) AS gr
+  LEFT JOIN (
+    SELECT
+      *
+    FROM
+      group_user_roles
+    WHERE
+      group_id = $group_id
+  ) AS gu ON gu.group_role_id = gr.id
+GROUP BY
+  gr.id,
+  maximum,
+  minimum,
+  role_name;
 
 -- BLOCK get_assessment_level_permissions
-SELECT 
-    bool_or(gr.can_assign_roles_at_start) AS can_assign_roles_at_start, 
-    bool_or(gr.can_assign_roles_during_assessment) AS can_assign_roles_during_assessment
+SELECT
+  bool_or(gr.can_assign_roles_at_start) AS can_assign_roles_at_start,
+  bool_or(gr.can_assign_roles_during_assessment) AS can_assign_roles_during_assessment
 FROM
-    group_roles as gr JOIN group_user_roles as gu ON gr.id = gu.group_role_id
+  group_roles as gr
+  JOIN group_user_roles as gu ON gr.id = gu.group_role_id
 WHERE
-    gr.assessment_id = $assessment_id AND gu.user_id = $user_id;
+  gr.assessment_id = $assessment_id
+  AND gu.user_id = $user_id;
 
 -- BLOCK get_role_assignments
-SELECT 
-    gu.user_id, u.uid, gr.role_name
-FROM 
-    users u JOIN group_user_roles gu ON u.user_id = gu.user_id
-    JOIN group_roles gr ON gu.group_role_id = gr.id
+SELECT
+  gu.user_id,
+  u.uid,
+  gr.role_name
+FROM
+  users u
+  JOIN group_user_roles gu ON u.user_id = gu.user_id
+  JOIN group_roles gr ON gu.group_role_id = gr.id
 WHERE
-    gu.group_id = $group_id;
+  gu.group_id = $group_id;
--- migrations/20220830011729_group_configs__using_group_roles__add.sql
+++ migrations/20220830011729_group_configs__using_group_roles__add.sql
@@ -1,14 +1,20 @@
-ALTER TABLE group_configs ADD COLUMN has_roles boolean DEFAULT false;
-ALTER TABLE group_users DROP COLUMN IF EXISTS group_role_id;
+ALTER TABLE group_configs
+ADD COLUMN has_roles boolean DEFAULT false;
+
+ALTER TABLE group_users
+DROP COLUMN IF EXISTS group_role_id;
 
 -- Create new table for group user-role relation
-CREATE TABLE IF NOT EXISTS group_user_roles (
-    group_id BIGINT REFERENCES groups(id),
-    user_id BIGINT REFERENCES users(user_id),
-    group_role_id BIGINT REFERENCES group_roles(id),
+CREATE TABLE IF NOT EXISTS
+  group_user_roles (
+    group_id BIGINT REFERENCES groups (id),
+    user_id BIGINT REFERENCES users (user_id),
+    group_role_id BIGINT REFERENCES group_roles (id),
     PRIMARY KEY (group_id, user_id, group_role_id)
-);
+  );
 
 CREATE INDEX group_user_roles_group_id_key ON group_user_roles (group_id);
+
 CREATE INDEX group_user_roles_user_id_key ON group_user_roles (user_id);
+
 CREATE INDEX group_user_roles_group_role_id_key ON group_user_roles (group_role_id);
