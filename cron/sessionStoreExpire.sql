6cee6d22776e8058aa6f36ac7f5b5bd28e0190ff
Nathan Walters <nwalters512@gmail.com>
2023-02-23 10:15:30 -0800

    Reformat

--- cron/sessionStoreExpire.sql
+++ cron/sessionStoreExpire.sql
@@ -1,4 +1,8 @@
 -- BLOCK expire
 DELETE FROM pl_sessions
-WHERE EXTRACT(EPOCH FROM (now() - updated_at)) >= $expirationInSeconds
-;
+WHERE
+  EXTRACT(
+    EPOCH
+    FROM
+      (now() - updated_at)
+  ) >= $expirationInSeconds;
