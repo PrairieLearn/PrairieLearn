7f534d871c3f678cfe492c9033c178c4d7f71a79
Nathan Walters <nwalters512@gmail.com>
2023-02-27 13:16:16 -0800

    WIP

--- packages/workspace-utils/src/index.ts
+++ packages/workspace-utils/src/index.ts
@@ -1,53 +1,61 @@
 import { loadSqlEquiv, queryAsync } from '@prairielearn/postgres';
-import type { Namespace } from 'socket.io';
+import type { Server as SocketIOServer } from 'socket.io';
 
 const sql = loadSqlEquiv(__filename);
 
-export interface WorkspaceUpdater {
-  /**
-   * Updates a workspace's current message.
-   *
-   * @param id The workspace's id.
-   * @param message The workspace's new message.
-   * @param toDatabase Whether to write the message to the database.
-   */
-  updateMessage(
-    workspace_id: string | number,
-    message: string,
-    toDatabase?: boolean
-  ): Promise<void>;
+let socketIoServer: SocketIOServer | null = null;
 
-  /**
-   * Updates a workspace's current state and message.
-   *
-   * @param id The workspace's id.
-   * @param state The workspace's new state.
-   * @param message The workspace's new message.
-   */
-  updateState(workspace_id: string | number, state: string, message?: string): Promise<void>;
+export function init(io: SocketIOServer) {
+  socketIoServer = io;
 }
 
-export function createUpdater(namespace: Namespace): WorkspaceUpdater {
-  function emitMessageForWorkspace(workspaceId: string | number, event: string, ...args: any[]) {
-    namespace.to(`workspace-${workspaceId}`).emit(event, ...args);
-  }
+function emitMessageForWorkspace(workspaceId: string | number, event: string, ...args: any[]) {
+  getSocketNamespace()
+    .to(`workspace-${workspaceId}`)
+    .emit(event, ...args);
+}
 
-  return {
-    async updateMessage(workspace_id, message, toDatabase = true) {
-      if (toDatabase) await queryAsync(sql.update_workspace_message, { workspace_id, message });
-      emitMessageForWorkspace(workspace_id, 'change:message', {
-        workspace_id,
-        message,
-      });
-    },
-    async updateState(workspace_id, state, message = '') {
-      // TODO: add locking
-      await queryAsync(sql.update_workspace_state, { workspace_id, state, message });
-      emitMessageForWorkspace(workspace_id, 'change:state', {
-        workspace_id,
-        state,
-        message,
-      });
-    },
-  };
+export function getSocketNamespace() {
+  if (!socketIoServer) throw new Error('socket.io server not initialized');
+  return socketIoServer.of('/workspace');
+}
+
+/**
+ * Updates a workspace's current message.
+ *
+ * @param id The workspace's id.
+ * @param message The workspace's new message.
+ * @param toDatabase Whether to write the message to the database.
+ */
+export async function updateMessage(
+  workspace_id: string | number,
+  message: string,
+  toDatabase: boolean = true
+): Promise<void> {
+  if (toDatabase) await queryAsync(sql.update_workspace_message, { workspace_id, message });
+  emitMessageForWorkspace(workspace_id, 'change:message', {
+    workspace_id,
+    message,
+  });
+}
+
+/**
+ * Updates a workspace's current state and message.
+ *
+ * @param id The workspace's id.
+ * @param state The workspace's new state.
+ * @param message The workspace's new message.
+ */
+export async function updateState(
+  workspace_id: string | number,
+  state: string,
+  message: string = ''
+): Promise<void> {
+  // TODO: add locking
+  await queryAsync(sql.update_workspace_state, { workspace_id, state, message });
+  emitMessageForWorkspace(workspace_id, 'change:state', {
+    workspace_id,
+    state,
+    message,
+  });
 }
