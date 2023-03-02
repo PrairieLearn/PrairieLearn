b6b5ab5f8707ede277b146c5c88227e339d97cab
Seth Poulsen <poulsenseth@yahoo.com>
2023-03-02 10:59:50 -0600

    Delcare variants.course_id not null

---
+++ migrations/20240310165254_variants__course_id__nonnull.sql
@@ -0,0 +1,14 @@
+-- declare the new course_id column of the variants table to be not null
+-- using the approach suggested by a postgres dev here: https://dba.stackexchange.com/questions/267947/how-can-i-set-a-column-to-not-null-without-locking-the-table-during-a-table-scan/268128#268128
+-- to avoid a long table lock
+ALTER TABLE variants
+ADD CONSTRAINT variants_course_id_not_null CHECK (course_id IS NOT NULL) NOT VALID;
+
+ALTER TABLE variants VALIDATE CONSTRAINT variants_course_id_not_null;
+
+ALTER TABLE variants
+ALTER COLUMN course_id
+SET NOT NULL;
+
+ALTER TABLE variants
+DROP CONSTRAINT variants_course_id_not_null;
