2aef281b6177cb843343d303e68b103ad95e588d
Nathan Walters <nwalters512@gmail.com>
2023-03-02 15:10:40 -0800

    Adjust how passport.authenticate is used

--- ee/auth/azure/callback.js
+++ ee/auth/azure/callback.js
@@ -2,30 +2,39 @@
 const passport = require('passport');
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const util = require('util');
 
 const authnLib = require('../../../lib/authn');
 
 const router = express.Router();
 
+function authenticate(req, res) {
+  return new Promise((resolve, reject) => {
+    passport.authenticate(
+      'azuread-openidconnect',
+      {
+        failureRedirect: '/pl',
+        session: false,
+      },
+      (err, user) => {
+        if (err) {
+          reject(err);
+        } else if (!user) {
+          reject(new Error('Login failed'));
+        } else {
+          resolve(user);
+        }
+      }
+    )(req, res);
+  });
+}
+
 router.post(
   '/',
   asyncHandler(async (req, res) => {
-    // This will write the resolved user to `req.user`.
-    await util.promisify(
-      passport.authenticate('azuread-openidconnect', {
-        failureRedirect: '/pl',
-        session: false,
-      })
-    )(req, res);
-
-    const user = req.user;
-    if (!user) throw new Error('Login failed');
+    const user = await authenticate(req, res);
 
     const authnParams = {
-      // @ts-expect-error `upn` is not defined on the type.
       uid: user.upn,
-      // @ts-expect-error `displayName` is not defined on the type.
       name: user.displayName,
       uin: null,
       provider: 'Azure',
