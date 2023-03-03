43919fa5ecd5882220d6a803012cf1b11ea19b17
Nathan Walters <nwalters512@gmail.com>
2023-03-03 12:55:57 -0800

    Format migration

--- packages/migrations/schema-migrations/20230303193423_batched_migrations__create.sql
+++ packages/migrations/schema-migrations/20230303193423_batched_migrations__create.sql
@@ -1,12 +1,13 @@
-CREATE TABLE IF NOT EXISTS batched_migrations (
-  current BIGINT NOT NULL,
-  finished_at TIMESTAMP WITH TIME ZONE,
-  id BIGSERIAL PRIMARY KEY,
-  max BIGINT NOT NULL,
-  min BIGINT NOT NULL,
-  name TEXT,
-  project TEXT DEFAULT 'prairielearn',
-  started_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-  timestamp TEXT,
-  UNIQUE (project, timestamp)
-)
+CREATE TABLE IF NOT EXISTS
+  batched_migrations (
+    current BIGINT NOT NULL,
+    finished_at TIMESTAMP WITH TIME ZONE,
+    id BIGSERIAL PRIMARY KEY,
+    max BIGINT NOT NULL,
+    min BIGINT NOT NULL,
+    name TEXT,
+    project TEXT DEFAULT 'prairielearn',
+    started_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    timestamp TEXT,
+    UNIQUE (project, timestamp)
+  );
