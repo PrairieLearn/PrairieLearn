09d883409d9fe907b1c21c3232d184f9e5a0f21c
Eliot Robson <eliot.robson24@gmail.com>
2023-03-09 11:38:45 -0600

    Apply suggestions from code review
    Co-authored-by: Nathan Walters <nwalters512@gmail.com>
    Co-authored-by: Jonatan Schroeder <jonatan@yorku.ca>

--- exampleCourse/questions/demo/randomFakeData/server.py
+++ exampleCourse/questions/demo/randomFakeData/server.py
@@ -8,7 +8,7 @@ def formatDate(date):
     return date.strftime("%B %d, %Y")
 
 
-def appendEmployee(data, fake, alias, birthdays):
+def append_employee(data, fake, alias, birthdays):
     name = fake.name()
     birthday = formatDate(birthdays.pop(0))
     data["params"][alias] = name
@@ -32,9 +32,9 @@ def generate(data):
     birthdays = sorted(birthdays)
 
     data["params"]["employees"] = []
-    appendEmployee(data, fake, "youngest", birthdays)
-    appendEmployee(data, fake, "employee1", birthdays)
-    appendEmployee(data, fake, "employee2", birthdays)
-    appendEmployee(data, fake, "employee3", birthdays)
+    for alias, birthday in zip(["youngest", "employee1", "employee2", "employee3"], birthdays):
+        name = fake.name()
+        data["params"][alias] = name
+        data["params"]["employees"].append({"name": name, "birthday": formatDate(birthday)})```
     random.shuffle(data["params"]["employees"])
     data["params"]["company"] = fake.company()
