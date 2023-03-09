445a2f93d7a2ee26427a4c3a2fa95753f5202b9a
Eliot Robson <eliot.robson24@gmail.com>
2023-03-09 14:46:34 -0600

    base working

--- elements/pl-graph/pl-graph.py
+++ elements/pl-graph/pl-graph.py
@@ -23,13 +23,11 @@ DIRECTED_DEFAULT = True
 def graphviz_from_networkx(
     element: lxml.html.HtmlElement, data: pl.QuestionData
 ) -> str:
-    engine = pl.get_string_attrib(element, "engine", ENGINE_DEFAULT)
     input_param_name = pl.get_string_attrib(element, "params-name")
 
     networkx_graph = pl.from_json(data["params"][input_param_name])
 
     G = nx.nx_agraph.to_agraph(networkx_graph)
-    G.layout(engine)
 
     return G.string()
 
@@ -37,9 +35,6 @@ def graphviz_from_networkx(
 def graphviz_from_adj_matrix(
     element: lxml.html.HtmlElement, data: pl.QuestionData
 ) -> str:
-    # Get matrix attributes
-
-    engine = pl.get_string_attrib(element, "engine", ENGINE_DEFAULT)
 
     # Legacy input with passthrough
     input_param_matrix = pl.get_string_attrib(
@@ -124,7 +119,6 @@ def graphviz_from_adj_matrix(
             else:
                 G.add_edge(out_node, in_node)
 
-    G.layout(engine)
     return G.string()
 
 
@@ -182,20 +176,25 @@ def render(element_html: str, data: pl.QuestionData) -> str:
 
     if input_param_name is not None:
         if input_type in matrix_backends:
-            graphviz_data = json.dumps(matrix_backends[input_type](element, data))
+            graphviz_data = matrix_backends[input_type](element, data)
         else:
             raise ValueError(f'Unknown graph type "{input_type}".')
     else:
         # Read the contents of this element as the data to render
         # we dump the string to json to ensure that newlines are
         # properly encoded
-        graphviz_data = json.dumps(str(element.text))
+        graphviz_data = str(element.text)
+
+
+    translated_dotcode = pygraphviz.AGraph(string=graphviz_data)
+    svg = translated_dotcode.draw(format="svg", prog=engine).decode("utf-8", "strict")
 
     html_params = {
         "uuid": pl.get_uuid(),
         "workerURL": "/node_modules/viz.js/full.render.js",
         "data": graphviz_data,
         "engine": engine,
+        "svg": svg
     }
 
     with open("pl-graph.mustache") as f:
