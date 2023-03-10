e1af7a9f56ff8301eb232301a01defea7c3ae786
Nathan Walters <nwalters512@gmail.com>
2023-03-10 09:50:20 -0800

    Make date column non-nullable

--- migrations/20230310174245_workspace_host_logs__create.sql
+++ migrations/20230310174245_workspace_host_logs__create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS
   workspace_host_logs (
     id bigserial PRIMARY KEY,
-    date timestamptz DEFAULT now(),
+    date timestamptz NOT NULL DEFAULT now(),
     message text,
     state enum_workspace_host_state,
     workspace_host_id bigint NOT NULL REFERENCES workspace_hosts (id) ON DELETE CASCADE ON UPDATE CASCADE
