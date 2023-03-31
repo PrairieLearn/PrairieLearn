0a4f781bb1a76a93706244826d4751b4079d66dd
Nathan Walters <nwalters512@gmail.com>
2023-03-30 17:14:42 -0700

    Handle more routes

--- tests/accessibility/index.test.js
+++ tests/accessibility/index.test.js
@@ -7,7 +7,9 @@ const fetch = require('node-fetch').default;
 const { A11yError } = require('@sa11y/format');
 const util = require('util');
 const expressListEndpoints = require('express-list-endpoints');
+const sqldb = require('@prairielearn/postgres');
 
+const news_items = require('../../news_items');
 const server = require('../../server');
 const config = require('../../lib/config');
 const helperServer = require('../helperServer');
@@ -72,22 +74,11 @@ async function checkPage(url) {
   A11yError.checkAndThrow(results.violations);
 }
 
-// These are trivially known because there will only be one course and course
-// instance in the database after syncing the example course.
 const STATIC_ROUTE_PARAMS = {
+  // These are trivially known because there will only be one course and course
+  // instance in the database after syncing the example course.
   course_id: 1,
   course_instance_id: 1,
-  // We're cheating a bit with this one, as this job (the initial sync) won't
-  // actually be renderable on every single job sequence page in production.
-  // However, it will be in tests, since we're using an admin role.
-  job_sequence_id: 1,
-};
-
-const OTHER_ROUTE_PARAMS = {
-  // TODO: use specific fixed assessment.
-  assessment_id: 1,
-  // TODO: use a specific fixed question.
-  question_id: 1,
 };
 
 function getRouteParams(url) {
@@ -121,38 +112,61 @@ const SKIP_ROUTES = [
   // This is not a real page.
   '/*',
 
+  // Special-case: `express-list-endpoints` doesn't handle regexp routes well.
+  // This matches the serialized path for regexp routes.
+  '/ RegExp(/\\/pl\\/shibcallback/)',
+
   // This page is not user-visible.
   '/pl/webhooks/ping',
 
   // These routes just render JSON.
   /^\/pl\/api\/v1\//,
+  '/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/instances/raw_data.json',
   '/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/manual_grading/assessment_question/:assessment_question_id/instances.json',
 
   // Static assets.
   '/pl/static/elements/*',
   '/pl/static/cacheableElements/:cachebuster/*',
-  '/pl/course/:course_id/cacheableElementExtensions/:cachebuster/*',
-  '/pl/course/:course_id/cacheableElements/:cachebuster/*',
-  '/pl/course_instance/:course_instance_id/cacheableElementExtensions/:cachebuster/*',
-  '/pl/course_instance/:course_instance_id/cacheableElements/:cachebuster/*',
-  '/pl/course_instance/:course_instance_id/instructor/cacheableElementExtensions/:cachebuster/*',
-  '/pl/course_instance/:course_instance_id/instructor/cacheableElements/:cachebuster/*',
+  '/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/instances/client.js',
 
   // File downloads.
   '/pl/course_instance/:course_instance_id/assessment_instance/:assessment_instance_id/file/:unsafe_file_id/:unsafe_display_filename',
+  '/pl/course_instance/:course_instance_id/assessment/:assessment_id/clientFilesAssessment/*',
+  '/pl/course_instance/:course_instance_id/cacheableElementExtensions/:cachebuster/*',
+  '/pl/course_instance/:course_instance_id/cacheableElements/:cachebuster/*',
+  '/pl/course_instance/:course_instance_id/clientFilesCourse/*',
+  '/pl/course_instance/:course_instance_id/clientFilesCourseInstance/*',
+  '/pl/course_instance/:course_instance_id/elementExtensions/*',
+  '/pl/course_instance/:course_instance_id/elements/*',
   '/pl/course_instance/:course_instance_id/instance_question/:instance_question_id/file/:filename',
   '/pl/course_instance/:course_instance_id/instance_question/:instance_question_id/clientFilesQuestion/*',
   '/pl/course_instance/:course_instance_id/instance_question/:instance_question_id/generatedFilesQuestion/variant/:variant_id/*',
   '/pl/course_instance/:course_instance_id/instance_question/:instance_question_id/submission/:submission_id/file/*',
   '/pl/course_instance/:course_instance_id/instance_question/:instance_question_id/text/:filename',
+  '/pl/course_instance/:course_instance_id/instructor/assessment_instance/:assessment_instance_id/:filename',
+  '/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/assessment_statistics/:filename',
+  '/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/clientFilesAssessment/*',
   '/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/downloads/:filename',
+  '/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/file_download/*',
+  '/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/question_statistics/:filename',
+  '/pl/course_instance/:course_instance_id/instructor/cacheableElementExtensions/:cachebuster/*',
+  '/pl/course_instance/:course_instance_id/instructor/cacheableElements/:cachebuster/*',
+  '/pl/course_instance/:course_instance_id/instructor/clientFilesCourse/*',
+  '/pl/course_instance/:course_instance_id/instructor/clientFilesCourseInstance/*',
+  '/pl/course_instance/:course_instance_id/instructor/course_admin/file_download/*',
+  '/pl/course_instance/:course_instance_id/instructor/elements/*',
+  '/pl/course_instance/:course_instance_id/instructor/elementExtensions/*',
   '/pl/course_instance/:course_instance_id/instructor/grading_job/:job_id/file/:file',
   '/pl/course_instance/:course_instance_id/instructor/instance_admin/assessments/file/:filename',
+  '/pl/course_instance/:course_instance_id/instructor/instance_admin/file_download/*',
   '/pl/course_instance/:course_instance_id/instructor/instance_admin/gradebook/:filename',
+  '/pl/course_instance/:course_instance_id/instructor/instance_admin/gradebook/raw_data.json',
   '/pl/course_instance/:course_instance_id/instructor/instance_question/:instance_question_id/clientFilesQuestion/*',
   '/pl/course_instance/:course_instance_id/instructor/instance_question/:instance_question_id/generatedFilesQuestion/variant/:variant_id/*',
   '/pl/course_instance/:course_instance_id/instructor/instance_question/:instance_question_id/submission/:submission_id/file/*',
   '/pl/course_instance/:course_instance_id/instructor/news_item/:news_item_id/*',
+  '/pl/course_instance/:course_instance_id/instructor/question/:question_id/clientFilesQuestion/*',
+  '/pl/course_instance/:course_instance_id/instructor/question/:question_id/generatedFilesQuestion/variant/:variant_id/*',
   '/pl/course_instance/:course_instance_id/instructor/question/:question_id/file_download/*',
   '/pl/course_instance/:course_instance_id/instructor/question/:question_id/file/:filename',
   '/pl/course_instance/:course_instance_id/instructor/question/:question_id/preview/file/:filename',
@@ -161,29 +175,55 @@ const SKIP_ROUTES = [
   '/pl/course_instance/:course_instance_id/instructor/question/:question_id/submission/:submission_id/file/*',
   '/pl/course_instance/:course_instance_id/instructor/question/:question_id/text/:filename',
   '/pl/course_instance/:course_instance_id/news_item/:news_item_id/*',
+  '/pl/course/:course_id/cacheableElementExtensions/:cachebuster/*',
+  '/pl/course/:course_id/cacheableElements/:cachebuster/*',
   '/pl/course/:course_id/clientFilesCourse/*',
   '/pl/course/:course_id/course_admin/file_download/*',
   '/pl/course/:course_id/news_item/:news_item_id/*',
+  '/pl/course/:course_id/question/:question_id/clientFilesQuestion/*',
+  '/pl/course/:course_id/elements/*',
+  '/pl/course/:course_id/elementExtensions/*',
+  '/pl/course/:course_id/question/:question_id/file_download/*',
+  '/pl/course/:course_id/question/:question_id/file/:filename',
+  '/pl/course/:course_id/question/:question_id/generatedFilesQuestion/variant/:variant_id/*',
+  '/pl/course/:course_id/question/:question_id/preview/file/:filename',
   '/pl/course/:course_id/question/:question_id/preview/text/:filename',
   '/pl/course/:course_id/question/:question_id/statistics/:filename',
   '/pl/course/:course_id/question/:question_id/submission/:submission_id/file/*',
-  '/pl/course/:course_id/question/:question_id/file/:filename',
   '/pl/course/:course_id/question/:question_id/text/:filename',
+  '/pl/news_item/:news_item_id/*',
 
   // Renders partial HTML documents, not a full page.
   '/pl/course_instance/:course_instance_id/instance_question/:instance_question_id/variant/:variant_id/submission/:submission_id',
+  '/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/manual_grading/instance_question/:instance_question_id/variant/:variant_id/submission/:submission_id',
+  '/pl/course_instance/:course_instance_id/instructor/instance_admin/assessments/stats/:assessment_id',
   '/pl/course_instance/:course_instance_id/instructor/question/:question_id/preview/variant/:variant_id/submission/:submission_id',
   '/pl/course_instance/:course_instance_id/assessment_instance/:assessment_instance_id/time_remaining',
+  '/pl/course/:course_id/question/:question_id/preview/variant/:variant_id/submission/:submission_id',
 
   // These pages just redirect to other pages and thus don't have to be tested.
-  '/pl/loadFromDisk',
-  '/pl/oauth2login',
-  '/pl/oauth2callback',
   '/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/manual_grading/assessment_question/:assessment_question_id/next_ungraded',
+  '/pl/course_instance/:course_instance_id/instructor/loadFromDisk',
+  '/pl/course_instance/:course_instance_id/loadFromDisk',
+  '/pl/course/:course_id/loadFromDisk',
+  '/pl/loadFromDisk',
+  '/pl/oauth2callback',
+  '/pl/oauth2login',
 
   // TODO: enable once the following PR is merged:
   // https://github.com/PrairieLearn/PrairieLearn/pull/7382
   '/pl/course/:course_id/effectiveUser',
+
+  // TODO: add tests for file editing/viewing.
+  /\/file_edit\/\*$/,
+  /\/file_view\/\*$/,
+
+  // TODO: add tests for job sequence pages.
+  /\/:job_sequence_id$/,
+
+  // TODO: add tests for workspace pages. These will require us to open a question
+  // in order to create a workspace.
+  /^\/pl\/workspace\//,
 ];
 
 // const ONLY_ROUTES = ['/pl/course_instance/:course_instance_id/effectiveUser'];
@@ -207,13 +247,38 @@ describe('accessibility', () => {
   before('set up testing server', async function () {
     await util.promisify(helperServer.before(EXAMPLE_COURSE_DIR).bind(this))();
 
+    // We want to test a news item page, so we need to "init" them.
+    const notify_with_new_server = true;
+    await news_items.initAsync(notify_with_new_server);
+
     const app = server.initExpress();
     endpoints = expressListEndpoints(app);
     endpoints.sort((a, b) => a.path.localeCompare(b.path));
 
+    const firstNewsItemResult = await sqldb.queryOneRowAsync(
+      'SELECT id FROM news_items ORDER BY id ASC LIMIT 1',
+      {}
+    );
+
+    const questionGalleryAssessmentResult = await sqldb.queryOneRowAsync(
+      'SELECT id FROM assessments WHERE tid = $tid',
+      {
+        tid: 'gallery/elements',
+      }
+    );
+
+    const calculationQuestionResult = await sqldb.queryOneRowAsync(
+      'SELECT id FROM questions WHERE qid = $qid',
+      {
+        qid: 'demo/calculation',
+      }
+    );
+
     routeParams = {
       ...STATIC_ROUTE_PARAMS,
-      ...OTHER_ROUTE_PARAMS,
+      news_item_id: firstNewsItemResult.rows[0].id,
+      assessment_id: questionGalleryAssessmentResult.rows[0].id,
+      question_id: calculationQuestionResult.rows[0].id,
     };
   });
   after('shut down testing server', helperServer.after);
@@ -221,19 +286,11 @@ describe('accessibility', () => {
   test('All pages pass accessibility checks', async function () {
     this.timeout(120_000);
 
-    const invalidEndpoints = [];
     const missingParamsEndpoints = [];
     const failingEndpoints = [];
 
     for (const endpoint of endpoints) {
       console.log(endpoint);
-
-      if (endpoint.path.startsWith('/ RegExp(')) {
-        // `express-list-endpoints` doesn't handle regex routes well.
-        // We'll just ignore them for now.
-        invalidEndpoints.push(endpoint);
-        continue;
-      }
 
       if (shouldSkipPath(endpoint.path)) {
         continue;
@@ -270,11 +327,6 @@ describe('accessibility', () => {
       shouldFail = true;
     }
 
-    if (invalidEndpoints.length > 0) {
-      console.log('The following endpoints are invalid:');
-      invalidEndpoints.forEach((e) => console.log(`  ${e.path}`));
-    }
-
     if (failingEndpoints.length > 0) {
       console.log('The following endpoints failed accessibility checks:\n');
       failingEndpoints.forEach(([e, err]) => {
@@ -282,6 +334,7 @@ describe('accessibility', () => {
         console.log(err.message);
         console.log('\n');
       });
+      shouldFail = true;
     }
 
     if (shouldFail) {
