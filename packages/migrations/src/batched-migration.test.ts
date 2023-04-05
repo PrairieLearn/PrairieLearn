815cb3e83ed06edd97df6085393fb0fae77bacbc
Nathan Walters <nwalters512@gmail.com>
2023-04-05 15:12:38 -0700

    Use postgres test-utils to simplify tests

--- packages/migrations/src/batched-migration.test.ts
+++ packages/migrations/src/batched-migration.test.ts
@@ -1,64 +1,13 @@
 import { assert } from 'chai';
-import * as sqldb from '@prairielearn/postgres';
-import pg from 'pg';
-import fs from 'fs';
-import path from 'path';
+import { makePostgresTestUtils, queryOneRowAsync } from '@prairielearn/postgres';
+import * as namedLocks from '@prairielearn/named-locks';
 
 import { BatchedMigration, BatchedMigrationExecutor } from './batched-migration';
-import { SCHEMA_MIGRATIONS_PATH } from './index';
+import { SCHEMA_MIGRATIONS_PATH, init } from './index';
 
-const WORKER_ID = process.env.MOCHA_WORKER_ID || '1';
-const POSTGRES_DATABASE = `prairielearn_migrations_${WORKER_ID}`;
-const POSTGRES_INIT_CONNECTION_STRING = 'postgres://postgres@localhost/postgres';
-
-// This was borrowed from `tests/helperDb.js` in the main database. When that's
-// moved to a shared package, we should be able to reuse that functionality.
-async function makeTestDatabase() {
-  const POSTGRES_USER = 'postgres';
-  const POSTGRES_HOST = 'localhost';
-
-  const client = new pg.Client(POSTGRES_INIT_CONNECTION_STRING);
-  await client.connect();
-  await client.query(`DROP DATABASE IF EXISTS ${POSTGRES_DATABASE};`);
-  await client.query(`CREATE DATABASE ${POSTGRES_DATABASE};`);
-  await client.end();
-
-  await sqldb.initAsync(
-    {
-      user: POSTGRES_USER,
-      database: POSTGRES_DATABASE,
-      host: POSTGRES_HOST,
-      max: 10,
-      idleTimeoutMillis: 30000,
-    },
-    (err) => {
-      throw err;
-    }
-  );
-
-  // Run local migrations to set up the database. Once the real migration-running
-  // code is lifted into this package, we can use that instead.
-  const migrations = fs.readdirSync(SCHEMA_MIGRATIONS_PATH);
-  const sortedMigrations = migrations.sort((a, b) => {
-    const aTimestamp = parseInt(a.split('_')[0]);
-    const bTimestamp = parseInt(b.split('_')[0]);
-    return aTimestamp - bTimestamp;
-  });
-  for (const migrationName of sortedMigrations) {
-    const migrationPath = path.join(SCHEMA_MIGRATIONS_PATH, migrationName);
-    const migration = fs.readFileSync(migrationPath, 'utf8');
-    await sqldb.queryAsync(migration, {});
-  }
-}
-
-async function destroyTestDatabase() {
-  await sqldb.closeAsync();
-
-  const client = new pg.Client(POSTGRES_INIT_CONNECTION_STRING);
-  await client.connect();
-  await client.query(`DROP DATABASE IF EXISTS ${POSTGRES_DATABASE}`);
-  await client.end();
-}
+const postgresTestUtils = makePostgresTestUtils({
+  database: 'prairielearn_migrations',
+});
 
 class TestBatchMigration extends BatchedMigration {
   async getMin() {
@@ -73,16 +22,24 @@ class TestBatchMigration extends BatchedMigration {
 }
 
 async function getBatchedMigrationState() {
-  const migration = await sqldb.queryOneRowAsync(
-    'SELECT * FROM batched_migrations WHERE name = $name;',
-    { name: 'test_batch_migration' }
-  );
+  const migration = await queryOneRowAsync('SELECT * FROM batched_migrations WHERE name = $name;', {
+    name: 'test_batch_migration',
+  });
   return migration.rows[0];
 }
 
 describe('BatchedMigrationExecutor', () => {
-  beforeEach(async () => makeTestDatabase());
-  afterEach(async () => destroyTestDatabase());
+  beforeEach(async () => {
+    await postgresTestUtils.createDatabase();
+    await namedLocks.init(postgresTestUtils.getPoolConfig(), (err) => {
+      throw err;
+    });
+    await init([SCHEMA_MIGRATIONS_PATH], 'prairielearn_migrations');
+  });
+  afterEach(async () => {
+    await namedLocks.close();
+    await postgresTestUtils.dropDatabase();
+  });
 
   it('runs one iteration of a batched migration', async () => {
     assert.equal(true, true);
