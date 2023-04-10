316df6715ca651c317e630562189ec6cef4d2ca7
Nathan Walters <nwalters512@gmail.com>
2023-04-10 16:36:33 -0700

    Add comment to remind us to delete

--- migrations/20230410205430_batched_migration__test.js
+++ migrations/20230410205430_batched_migration__test.js
@@ -1,5 +1,6 @@
 const { enqueueBatchedMigration } = require('@prairielearn/migrations');
 
+// TODO: delete this file before merging.
 module.exports = async function () {
   await enqueueBatchedMigration('20230407165410_test');
 };
