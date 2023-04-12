3719949feaf99672a5b0ccab5134a6b64c98597f
Nathan Walters <nwalters512@gmail.com>
2023-04-12 09:49:33 -0700

    Remove test migration

--- migrations/20230410205430_batched_migration__test.js
+++
@@ -1,6 +0,0 @@
-const { enqueueBatchedMigration } = require('@prairielearn/migrations');
-
-// TODO: delete this file before merging.
-module.exports = async function () {
-  await enqueueBatchedMigration('20230407165410_test');
-};
