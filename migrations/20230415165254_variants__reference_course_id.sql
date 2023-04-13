a50ceadd2742771f6dbff4aa2829932e2962186b
Seth Poulsen <poulsenseth@yahoo.com>
2023-04-13 11:14:10 -0500

    don't backfill course_id column

--- migrations/20230415165254_variants__reference_course_id.sql
+++ migrations/20230415165254_variants__reference_course_id.sql
@@ -3,11 +3,3 @@ ADD COLUMN IF NOT EXISTS course_id bigint;
 
 ALTER TABLE variants
 ADD CONSTRAINT variants_course_id_fkey FOREIGN KEY (course_id) REFERENCES pl_courses (id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-UPDATE variants AS v
-SET
-  course_id = q.course_id
-FROM
-  questions AS q
-WHERE
-  v.question_id = q.id;
