3bc83a9af535a034a608b0099987933b58bbf664
Seth Poulsen <poulsenseth@yahoo.com>
2023-04-13 11:47:17 -0500

    add column and constraints in one expression

--- migrations/20230415165254_variants__reference_course_id.sql
+++ migrations/20230415165254_variants__reference_course_id.sql
@@ -1,5 +1,2 @@
 ALTER TABLE variants
-ADD COLUMN IF NOT EXISTS course_id bigint;
-
-ALTER TABLE variants
-ADD CONSTRAINT variants_course_id_fkey FOREIGN KEY (course_id) REFERENCES pl_courses (id) ON UPDATE CASCADE ON DELETE CASCADE;
+ADD COLUMN IF NOT EXISTS course_id bigint NOT NULL REFERENCES pl_courses (id) ON UPDATE CASCADE ON DELETE CASCADE;
