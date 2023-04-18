0c7d818854492162ffce521a5cd2c245b8a11e76
caleqw2 <caleqw2@illinois.edu>
2023-04-18 11:13:43 -0500

    Remove non-unique indexes from group_user_roles

--- migrations/20220830011729_group_configs__using_group_roles__add.sql
+++ migrations/20220830011729_group_configs__using_group_roles__add.sql
@@ -14,9 +14,3 @@ CREATE TABLE IF NOT EXISTS
   );
 
 CREATE UNIQUE INDEX group_user_roles_group_id_user_id_group_role_id_key ON group_user_roles (group_id, user_id, group_role_id);
-
-CREATE INDEX group_user_roles_group_id_key ON group_user_roles (group_id);
-
-CREATE INDEX group_user_roles_user_id_key ON group_user_roles (user_id);
-
-CREATE INDEX group_user_roles_group_role_id_key ON group_user_roles (group_role_id);
