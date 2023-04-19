84dcd2ec048fc50b04ed8422b4b000b8b3803e41
caleqw2 <caleqw2@illinois.edu>
2023-04-18 23:28:48 -0500

    Fix config import in groupRole.test.js

--- tests/groupRole.test.js
+++ tests/groupRole.test.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch').default;
 const path = require('path');
 const { step } = require('mocha-steps');
 
-const config = require('../lib/config');
+const { config } = require('../lib/config');
 const sqldb = require('@prairielearn/postgres');
 const sql = sqldb.loadSqlEquiv(__filename);
 
