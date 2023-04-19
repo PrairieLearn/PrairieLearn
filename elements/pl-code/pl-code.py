277589f0a81a69d8eb566358875ed999ca1be752
Jonatan Schroeder <jonatan@yorku.ca>
2023-04-19 19:39:34 -0400

    Just use a link

--- elements/pl-code/pl-code.py
+++ elements/pl-code/pl-code.py
@@ -128,11 +128,8 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
     if language is not None:
         lexer = get_lexer_by_name(language)
         if lexer is None:
-            allowed_languages = (
-                l[1][0] for l in pygments.lexers.get_all_lexers() if l[1]
-            )
             raise KeyError(
-                f'Unknown language: "{language}". Must be one of: {", ".join(allowed_languages)}'
+                f'Unknown language: "{language}". Must be one of the alias listed in https://pygments.org/languages/.'
             )
 
     style = pl.get_string_attrib(element, "style", STYLE_DEFAULT)
