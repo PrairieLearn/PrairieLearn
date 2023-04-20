924d27f76e3d53f909ccc046125f6892b8c5bb41
Nathan Walters <nwalters512@gmail.com>
2023-04-20 16:30:32 -0700

    Fix error

--- middlewares/csrfToken.js
+++ middlewares/csrfToken.js
@@ -1,9 +1,10 @@
+// @ts-check
 const path = require('path');
 const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
 
 const { config } = require('../lib/config');
 const error = require('@prairielearn/error');
-const { checkSignedToken } = require('@prairielearn/signed-token');
+const { generateSignedToken, checkSignedToken } = require('@prairielearn/signed-token');
 
 module.exports = function (req, res, next) {
   var tokenData = {
@@ -12,7 +13,7 @@ module.exports = function (req, res, next) {
   if (res.locals.authn_user && res.locals.authn_user.user_id) {
     tokenData.authn_user_id = res.locals.authn_user.user_id;
   }
-  res.locals.__csrf_token = csrf.generateToken(tokenData, config.secretKey);
+  res.locals.__csrf_token = generateSignedToken(tokenData, config.secretKey);
 
   if (req.method === 'POST') {
     // NOTE: If you are trying to debug a "CSRF Fail" in a form with file
