de70d066bb6d8f3d1723ed960f3b3d4ef2d98ffa
Nathan Walters <nwalters512@gmail.com>
2023-04-27 09:14:38 -0700

    Support and document DEV environment variable

--- docker/init.sh
+++ docker/init.sh
@@ -6,7 +6,7 @@ cd /PrairieLearn
 make -s start-support
 node server.js --migrate-and-exit >/dev/null
 
-if [[ $NODEMON == "true" ]]; then
+if [[ $NODEMON == "true" || DEV == "true" ]]; then
     # `dev` is listed first so it can use standard input
     make -s -j 2 dev dev-workspace-host
 else
