22e81b1853c4410ba40a91b5d2afd32dd66ec680
Seth Poulsen <poulsenseth@yahoo.com>
2023-04-27 13:47:31 -0500

    get both variant course and question course when grading assessment

--- sprocs/variants_select_for_assessment_instance_grading.sql
+++ sprocs/variants_select_for_assessment_instance_grading.sql
@@ -12,13 +12,15 @@ BEGIN
     SELECT DISTINCT ON (iq.id)
         to_jsonb(v.*) AS variant,
         to_jsonb(q.*) AS question,
-        to_jsonb(c.*) AS course
+        to_jsonb(qc.*) AS question_course,
+        to_jsonb(vc.*) AS variant_course
     FROM
         variants AS v
         JOIN instance_questions AS iq ON (iq.id = v.instance_question_id)
         JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
         JOIN questions AS q ON (q.id = v.question_id)
-        JOIN pl_courses AS c ON (c.id = v.course_id)
+        JOIN pl_courses AS qc ON (c.id = q.course_id)
+        JOIN pl_courses AS vc ON (c.id = v.course_id)
     WHERE ai.id = variants_select_for_assessment_instance_grading.assessment_instance_id
     ORDER BY iq.id, v.date DESC;
 END;
