bec781feef18baa07b0e9f550385186ebb3fa45d
Seth Poulsen <poulsenseth@yahoo.com>
2023-04-28 21:39:44 -0500

    working validation of question access using sharing sets

--- middlewares/selectAndAuthzInstructorQuestion.js
+++ middlewares/selectAndAuthzInstructorQuestion.js
@@ -10,7 +10,7 @@ module.exports = function (req, res, next) {
   if (res.locals.course_instance) {
     const params = {
       question_id: req.params.question_id,
-      // course_instance_id: res.locals.course_instance.id,
+      course_instance_id: res.locals.course_instance.id,
     };
     sqldb.queryZeroOrOneRow(
       sql.select_and_auth_with_course_instance,
@@ -18,6 +18,9 @@ module.exports = function (req, res, next) {
       function (err, result) {
         if (ERR(err, next)) return;
         if (result.rowCount === 0) return next(error.make(403, 'Access denied'));
+        let row = result.rows[0];
+        if (row.question.id !== row.course_id && !row.shared_with_course) return next(error.make(403, 'Access denied'));
+
         _.assign(res.locals, result.rows[0]);
         next();
       }
--- middlewares/selectAndAuthzInstructorQuestion.sql
+++ middlewares/selectAndAuthzInstructorQuestion.sql
@@ -26,28 +26,52 @@ WHERE
 
 -- BLOCK select_and_auth_with_course_instance
 WITH
+  course AS (
+    SELECT
+      c.id AS id
+    FROM
+      course_instances AS ci
+      JOIN pl_courses AS c on ci.course_id = c.id
+    WHERE
+      ci.id = $course_instance_id
+  ),
   issue_count AS (
     SELECT
       count(*) AS open_issue_count
     FROM
       issues AS i
+      JOIN course on i.course_id = course.id
     WHERE
       i.question_id = $question_id
       AND i.course_caused
       AND i.open
+  ),
+  sharing_info AS (
+    SELECT
+      count(*) > 0 AS shared_with_course
+    FROM
+      sharing_sets AS ss
+      JOIN sharing_set_questions AS ssq on ssq.sharing_set_id = ss.id
+      JOIN sharing_set_courses AS ssc on ssc.sharing_set_id = ss.id
+      JOIN course ON ssc.course_id = course.id
+    WHERE
+      ssq.question_id = $question_id
   )
 SELECT
   to_json(q) AS question,
   to_json(top) AS topic,
   tags_for_question (q.id) AS tags,
   assessments_format_for_question (q.id, ci.id) AS assessments,
-  issue_count.open_issue_count
+  issue_count.open_issue_count,
+  sharing_info.shared_with_course
 FROM
   questions as q
   JOIN topics as top ON (top.id = q.topic_id),
   course_instances AS ci,
-  issue_count
+  issue_count,
+  sharing_info
 WHERE
   q.id = $question_id
-  AND q.course_id = ci.course_id
+  AND ci.id = $course_instance_id
+  -- AND q.course_id = ci.course_id
   AND q.deleted_at IS NULL;
