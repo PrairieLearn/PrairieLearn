67a5189bbd08a6f8bad9783aee444d34ea94f551
Nathan Walters <nwalters512@gmail.com>
2023-05-01 16:47:50 -0700

    Fix broken tests

--- apps/prairielearn/src/tests/helperCourse.js
+++ apps/prairielearn/src/tests/helperCourse.js
@@ -1,12 +1,9 @@
 // @ts-check
-const path = require('node:path');
-
+const { TEST_COURSE_PATH } = require('../lib/paths');
 const syncFromDisk = require('../sync/syncFromDisk');
 const { makeMockLogger } = require('./mockLogger');
 
-const courseDirDefault = path.join(__dirname, '..', 'testCourse');
-
-async function syncCourse(courseDir = courseDirDefault) {
+async function syncCourse(courseDir = TEST_COURSE_PATH) {
   const { logger, getOutput } = makeMockLogger();
   const result = await syncFromDisk.syncOrCreateDiskToSqlAsync(courseDir, logger);
   if (!result || result.hadJsonErrorsOrWarnings) {
