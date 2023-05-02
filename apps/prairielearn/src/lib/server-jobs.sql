8fce1521ec664f48b00543b18aff5dc725784c25
Nathan Walters <nathan@prairielearn.com>
2023-05-02 16:14:35 -0700

    Don't store server job stdout/stderr separately (#7696)
    * Don't store server job stdout/stderr separately
    
    * Remove more column writes

--- apps/prairielearn/src/lib/server-jobs.sql
+++ apps/prairielearn/src/lib/server-jobs.sql
@@ -124,8 +124,6 @@ WITH
         WHEN $exit_code = 0 THEN 'Success'::enum_job_status
         ELSE 'Error'::enum_job_status
       END,
-      stderr = $stderr,
-      stdout = $stdout,
       output = $output,
       exit_code = $exit_code,
       exit_signal = $exit_signal
@@ -171,8 +169,6 @@ WITH
     SET
       finish_date = CURRENT_TIMESTAMP,
       status = 'Error'::enum_job_status,
-      stderr = $stderr,
-      stdout = $stdout,
       output = $output,
       error_message = $error_message
     WHERE
