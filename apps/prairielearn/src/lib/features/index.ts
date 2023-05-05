5eac45c7705209395fdddc1da490be62a9b4f881
Nathan Walters <nwalters512@gmail.com>
2023-05-05 12:29:04 -0700

    Remove placeholder feature

--- apps/prairielearn/src/lib/features/index.ts
+++ apps/prairielearn/src/lib/features/index.ts
@@ -1,5 +1,5 @@
 import { FeatureManager, FeatureGrantType } from './manager';
 
-const features = new FeatureManager(['course:manual-grading']);
+const features = new FeatureManager([]);
 
 export { features, FeatureGrantType };
