ddbf5a1a50c7600d8d160b27e22c9d7160ea87ae
Nathan Walters <nwalters512@gmail.com>
2023-05-10 09:55:06 -0700

    Fix Mocha setup

--- apps/prairielearn/.mocharc.js
+++ apps/prairielearn/.mocharc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  require: ['ts-node/register', './src/tests/mocha-hooks.mjs'],
+  require: ['./src/tests/mocha-env.mjs', 'ts-node/register', './src/tests/mocha-hooks.mjs'],
   timeout: '30000', // in milliseconds
   'watch-files': ['.'],
 };
