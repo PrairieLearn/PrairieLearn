4e9dbaf8fb7733dc8a8527946432e9494ac634f4
Seth Poulsen <poulsenseth@yahoo.com>
2023-05-11 21:05:44 -0500

    change migration to typescript

--- apps/prairielearn/src/migrations/20240310165253_variants__course_id__backfill__finalize.js
+++
@@ -1,5 +0,0 @@
-const { finalizeBatchedMigration } = require('@prairielearn/migrations');
-
-module.exports = async function () {
-  await finalizeBatchedMigration('20230418190511_variants_course_id');
-}
