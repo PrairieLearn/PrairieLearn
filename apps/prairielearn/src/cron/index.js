f86215e551b08f6c34cc9fb42b85199781a4577d
Nathan Walters <nwalters512@gmail.com>
2023-05-15 15:20:27 -0700

    Report cron errors to Sentry

--- apps/prairielearn/src/cron/index.js
+++ apps/prairielearn/src/cron/index.js
@@ -5,6 +5,7 @@ const _ = require('lodash');
 const debug = require('debug')('prairielearn:cron');
 const { v4: uuidv4 } = require('uuid');
 const { trace, context, suppressTracing, SpanStatusCode } = require('@prairielearn/opentelemetry');
+const Sentry = require('@prairielearn/sentry');
 
 const { config } = require('../lib/config');
 const { isEnterprise } = require('../lib/license');
@@ -270,6 +271,13 @@ module.exports = {
                     stack: err.stack,
                     data: JSON.stringify(err.data),
                     cronUuid,
+                  });
+
+                  Sentry.captureException(err, {
+                    tags: {
+                      'cron.name': job.name,
+                      'cron.uuid': cronUuid,
+                    },
                   });
 
                   span.recordException(err);
