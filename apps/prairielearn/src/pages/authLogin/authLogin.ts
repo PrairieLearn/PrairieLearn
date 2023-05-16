0b113246b883591af110615818a43f3e71fe111b
Nathan Walters <nwalters512@gmail.com>
2023-05-16 11:13:35 -0700

    More tweaks

--- apps/prairielearn/src/pages/authLogin/authLogin.ts
+++ apps/prairielearn/src/pages/authLogin/authLogin.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import asyncHandler = require('express-async-handler');
 import { z } from 'zod';
-import sqldb = require('@prairielearn/postgres');
+import { loadSqlEquiv, queryRows } from '@prairielearn/postgres';
 
 import { config } from '../../lib/config';
-import { isEnterprise } from '../../lib/license';
 
 import {
   AuthLogin,
@@ -12,7 +11,7 @@ import {
   type InstitutionAuthnProvider,
 } from './authLogin.html';
 
-const sql = sqldb.loadSqlEquiv(__filename);
+const sql = loadSqlEquiv(__filename);
 const router = Router();
 
 const InstitutionAuthnProviderSchema = z.object({
@@ -20,6 +19,10 @@ const InstitutionAuthnProviderSchema = z.object({
   long_name: z.string(),
   short_name: z.string(),
   default_authn_provider_name: z.string(),
+});
+const InstitutionSupportedProvidersSchema = z.object({
+  name: z.string(),
+  is_default: z.boolean(),
 });
 const ServiceSchema = z.string().nullable();
 const InstitutionIdSchema = z.string().nullable();
@@ -40,17 +43,18 @@ router.get(
       }
 
       // Look up the supported providers for this institution.
-      const supportedProvidersRes = await sqldb.queryAsync(
+      const supportedProviders = await queryRows(
         sql.select_supported_providers_for_institution,
         {
           institution_id: institutionId,
-        }
+        },
+        InstitutionSupportedProvidersSchema
       );
 
       res.send(
         AuthLoginUnsupportedProvider({
           institutionId,
-          supportedProviders: supportedProvidersRes.rows,
+          supportedProviders,
           service,
           resLocals: res.locals,
         })
@@ -58,45 +62,42 @@ router.get(
       return;
     }
 
-    let institutionAuthnProviders: InstitutionAuthnProvider[] | null = null;
-    if (isEnterprise()) {
-      const institutionAuthnProvidersRes = await sqldb.queryRows(
-        sql.select_institution_authn_providers,
-        InstitutionAuthnProviderSchema
-      );
-      institutionAuthnProviders = institutionAuthnProvidersRes
-        .map((provider) => {
-          // Special case: omit the default institution in production.
-          if (provider.id === '1' && config.devMode === false) return null;
+    const institutionAuthnProvidersRes = await queryRows(
+      sql.select_institution_authn_providers,
+      InstitutionAuthnProviderSchema
+    );
+    const institutionAuthnProviders = institutionAuthnProvidersRes
+      .map((provider) => {
+        // Special case: omit the default institution in production.
+        if (provider.id === '1' && config.devMode === false) return null;
 
-          let url: string | null = null;
-          switch (provider.default_authn_provider_name) {
-            case 'SAML':
-              url = `/pl/auth/institution/${provider.id}/saml/login`;
-              break;
-            case 'Google':
-              url = '/pl/oauth2login';
-              break;
-            case 'Azure':
-              url = '/pl/azure_login';
-              break;
-            case 'Shibboleth':
-              url = '/pl/shibcallback';
-              break;
-            default:
-              return null;
-          }
+        let url: string | null = null;
+        switch (provider.default_authn_provider_name) {
+          case 'SAML':
+            url = `/pl/auth/institution/${provider.id}/saml/login`;
+            break;
+          case 'Google':
+            url = '/pl/oauth2login';
+            break;
+          case 'Azure':
+            url = '/pl/azure_login';
+            break;
+          case 'Shibboleth':
+            url = '/pl/shibcallback';
+            break;
+          default:
+            return null;
+        }
 
-          return {
-            name:
-              provider.long_name !== provider.short_name
-                ? `${provider.long_name} (${provider.short_name})`
-                : provider.long_name,
-            url,
-          };
-        })
-        .filter((provider): provider is InstitutionAuthnProvider => provider !== null);
-    }
+        return {
+          name:
+            provider.long_name !== provider.short_name
+              ? `${provider.long_name} (${provider.short_name})`
+              : provider.long_name,
+          url,
+        };
+      })
+      .filter((provider): provider is InstitutionAuthnProvider => provider !== null);
 
     res.send(AuthLogin({ service, institutionAuthnProviders, resLocals: res.locals }));
   })
