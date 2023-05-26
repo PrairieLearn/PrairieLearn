08436209a1632167f2fcfd4733d807efb6e36d2f
Nathan Walters <nathan@prairielearn.com>
2023-05-26 12:46:12 -0700

    Add admin pages for institutions/courses/instances (#7783)
    * Add new administratorInstitutions page
    
    * Add nav tab and page title
    
    * Remove institutions from courses page
    
    * Add general settings page for institution
    
    * Add courses and course admin pages
    
    * Add course instance admin page
    
    * Use new types
    
    * Rename page HTML functions
    
    * Fix existing type errors
    
    * Update administratorInstitutions page
    
    * Apply suggestions from code review
    
    * Revamp administrator tests
    
    * Arrow functions
    
    * Update apps/prairielearn/src/ee/auth/saml/router.ts

---
+++ apps/prairielearn/src/ee/institution/admin/courseInstance.html.ts
@@ -0,0 +1,53 @@
+import { html } from '@prairielearn/html';
+import { renderEjs } from '@prairielearn/html-ejs';
+import { Course, type CourseInstance, type Institution } from '../../../lib/db-types';
+
+export function InstitutionAdminCourseInstance({
+  institution,
+  course,
+  course_instance,
+  resLocals,
+}: {
+  institution: Institution;
+  course: Course;
+  course_instance: CourseInstance;
+  resLocals: Record<string, any>;
+}) {
+  return html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        ${renderEjs(__filename, "<%- include('../../../pages/partials/head')%>", {
+          ...resLocals,
+          navPage: 'institution_admin',
+          pageTitle: 'Courses',
+        })}
+      </head>
+      <body>
+        ${renderEjs(__filename, "<%- include('../../../pages/partials/navbar') %>", {
+          ...resLocals,
+          institution,
+          navbarType: 'institution',
+          navPage: 'institution_admin',
+          navSubPage: 'courses',
+        })}
+        <nav class="container" aria-label="Breadcrumbs">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="/pl/institution/${institution.id}/admin/courses">Courses</a>
+            </li>
+            <li class="breadcrumb-item">
+              <a href="/pl/institution/${institution.id}/admin/course/${course.id}">
+                ${course.title} (${course.short_name})
+              </a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+              ${course_instance.long_name} (${course_instance.short_name})
+            </li>
+          </ol>
+        </nav>
+        <main class="container mb-4"></main>
+      </body>
+    </html>
+  `.toString();
+}
