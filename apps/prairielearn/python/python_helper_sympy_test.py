20e1eec61c376c6c104b0c5d79d0d3fc85bc4b4c
Eliot Robson <eliot.robson24@gmail.com>
2023-05-27 15:57:28 -0500

    Added another test case

--- apps/prairielearn/python/python_helper_sympy_test.py
+++ apps/prairielearn/python/python_helper_sympy_test.py
@@ -66,6 +66,7 @@ class TestSympy:
         ("n/-m", -N / M),
         ("n / 2", N / 2),
         ("3^m + 4", 3**M + 4),
+        ("nm^(-1)", N / M),
         ("factorial(3m)", sympy.factorial(3 * M)),
         ("m! + n", sympy.factorial(M) + N),
         ("5mn sin m + arccos 3n", 5 * M * N * sympy.sin(M) + sympy.acos(3 * N)),
