53a65c8ad67fd28a1f24e771fca64a068b54164f
Nathan Walters <nwalters512@gmail.com>
2023-05-30 13:39:43 -0700

    Drop old indexes

--- apps/prairielearn/src/migrations/20230530202846_feature_grants__add_indexes_without_type.sql
+++ apps/prairielearn/src/migrations/20230530202846_feature_grants__add_indexes_without_type.sql
@@ -11,7 +11,7 @@ ALTER INDEX feature_grants_name_user_id_type_idx
 RENAME TO feature_grants_name_user_id_type_idx_old;
 
 ALTER TABLE feature_grants
-ADD CONSTRAINT feature_grants_by_name UNIQUE (
+ADD CONSTRAINT feature_grants_by_name_idx UNIQUE (
   name,
   institution_id,
   course_id,
@@ -30,3 +30,12 @@ CREATE INDEX IF NOT EXISTS feature_grants_by_entity_idx ON feature_grants (
 CREATE INDEX IF NOT EXISTS feature_grants_user_id_name_idx ON feature_grants (user_id, name);
 
 CREATE INDEX IF NOT EXISTS feature_grants_name_user_id_idx ON feature_grants (name, user_id);
+
+ALTER TABLE feature_grants
+DROP CONSTRAINT feature_grants_by_name_idx_old;
+
+DROP INDEX feature_grants_by_entity_idx_old;
+
+DROP INDEX feature_grants_user_id_name_type_idx_old;
+
+DROP INDEX feature_grants_name_user_id_type_idx_old;
