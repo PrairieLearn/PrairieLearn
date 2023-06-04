33c56cde8e8312cd15e6f2b0dfb9a865a8ee591c
Eliot Robson <eliot.robson24@gmail.com>
2023-06-04 11:40:10 -0500

    Fixed file generation

--- exampleCourse/questions/demo/randomZippedFile/server.py
+++ exampleCourse/questions/demo/randomZippedFile/server.py
@@ -15,31 +15,28 @@ def generate(data):
     params_dict["text_file_name"] = f"{fake.unique.word()}.txt"
     params_dict["python_file_name"] = f"{fake.unique.word()}.py"
 
-    # First, generate python file contents. We have to do this because paths
-    # aren't given in the `file()` function.
-    template_path = os.path.join(
-        data["options"]["question_path"],
-        "fileTemplates",
-        "python_file_template.mustache",
-    )
-    with open(template_path, "r") as template_file:
-        params_dict["python_file_contents"] = chevron.render(
-            template_file, {"function_name": params_dict["function_name"]}
-        )
-
 
 def file(data):
     if data["filename"] == "generated_files.zip":
         params_dict = data["params"]
+
+        # First, generate python file contents from template
+        template_path = os.path.join(
+            data["options"]["question_path"],
+            "fileTemplates",
+            "python_file_template.mustache",
+        )
+        with open(template_path, "r") as template_file:
+            python_file_contents = chevron.render(
+                template_file, {"function_name": params_dict["function_name"]}
+            )
 
         # Create buffer for zipping
         zip_buffer = io.BytesIO()
 
         # Fill zip file
         with zipfile.ZipFile(zip_buffer, "a") as zip_file:
-            zip_file.writestr(
-                params_dict["python_file_name"], params_dict["python_file_contents"]
-            )
+            zip_file.writestr(params_dict["python_file_name"], python_file_contents)
             zip_file.writestr(
                 params_dict["text_file_name"], params_dict["words_for_text"]
             )
