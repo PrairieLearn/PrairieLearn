535b92456db412be817aaa2f001d177b02e4453e
Nathan Walters <nwalters512@gmail.com>
2023-06-05 09:28:35 -0700

    Parse authenticated user with zod

--- apps/prairielearn/src/lib/db-types.ts
+++ apps/prairielearn/src/lib/db-types.ts
@@ -72,3 +72,16 @@ export const AuthnProviderSchema = z.object({
   name: z.string().nullable(),
 });
 export type AuthnProvider = z.infer<typeof AuthnProviderSchema>;
+
+export const UserSchema = z.object({
+  deleted_at: z.date().nullable(),
+  institution_id: IdSchema,
+  lti_context_id: z.string().nullable(),
+  lti_course_instance_id: IdSchema.nullable(),
+  lti_user_id: z.string().nullable(),
+  name: z.string().nullable(),
+  uid: z.string(),
+  uin: z.string().nullable(),
+  user_id: IdSchema,
+});
+export type User = z.infer<typeof UserSchema>;
