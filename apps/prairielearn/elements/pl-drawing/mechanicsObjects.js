c204cc4c6571d57b8a2e26199983daf5612cf258
Mariana Silva <mfsilva@illinois.edu>
2023-06-10 11:55:05 -0300

    added pl-battery

--- apps/prairielearn/elements/pl-drawing/mechanicsObjects.js
+++ apps/prairielearn/elements/pl-drawing/mechanicsObjects.js
@@ -3705,6 +3705,104 @@ mechanicsObjects.byType['pl-capacitor'] = class extends PLDrawingBaseElement {
   }
 };
 
+mechanicsObjects.byType['pl-battery'] = class extends PLDrawingBaseElement {
+  static generate(canvas, options) {
+
+    var gap = options.interval
+    var theta = Math.atan2(options.y2-options.y1, options.x2 - options.x1);
+    var d = Math.sqrt( Math.pow(options.y2-options.y1,2) + Math.pow(options.x2 - options.x1,2) );
+    
+    // Battery "legs"
+    var xm1 = options.x1 + ((d-gap)/2)*Math.cos(theta);
+    var ym1 = options.y1 + ((d-gap)/2)*Math.sin(theta); 
+    var xm2 = options.x1 + ((d+gap)/2)*Math.cos(theta); 
+    var ym2 = options.y1 + ((d+gap)/2)*Math.sin(theta); 
+
+    let obj1 = new fabric.Line([options.x1, options.y1, xm1, ym1], {
+      stroke: options.stroke,
+      strokeWidth: options.strokeWidth,
+      selectable: false,
+      evented: false,
+      originX: 'center',
+      originY: 'center',
+    });
+    if (!('id' in obj1)) {
+      obj1.id = window.PLDrawingApi.generateID();
+    }
+    let obj2 = new fabric.Line([xm2, ym2, options.x2, options.y2], {
+      stroke: options.stroke,
+      strokeWidth: options.strokeWidth,
+      selectable: false,
+      evented: false,
+      originX: 'center',
+      originY: 'center',
+    });
+    if (!('id' in obj2)) {
+      obj2.id = window.PLDrawingApi.generateID();
+    }
+
+    // Battery lines
+    var cline1 = options.height/2
+    var cline2 = options.height
+
+    var c1x1 = xm1 - cline1*Math.sin(theta);
+    var c1y1 = ym1 + cline1*Math.cos(theta); 
+    var c1x2 = xm1 + cline1*Math.sin(theta);
+    var c1y2 = ym1 - cline1*Math.cos(theta);      
+    var c2x1 = xm2 - cline2*Math.sin(theta);
+    var c2y1 = ym2 + cline2*Math.cos(theta); 
+    var c2x2 = xm2 + cline2*Math.sin(theta);
+    var c2y2 = ym2 - cline2*Math.cos(theta);  
+
+    let obj3 = new fabric.Line([c1x1, c1y1, c1x2, c1y2], {
+      stroke: options.stroke,
+      strokeWidth: options.strokeWidth,
+      selectable: false,
+      evented: false,
+      originX: 'center',
+      originY: 'center',
+    });
+    if (!('id' in obj3)) {
+      obj3.id = window.PLDrawingApi.generateID();
+    }
+    let obj4 = new fabric.Line([c2x1, c2y1, c2x2, c2y2], {
+      stroke: options.stroke,
+      strokeWidth: options.strokeWidth,
+      selectable: false,
+      evented: false,
+      originX: 'center',
+      originY: 'center',
+    });
+    if (!('id' in obj4)) {
+      obj4.id = window.PLDrawingApi.generateID();
+    }
+
+    canvas.add(obj1, obj2, obj3, obj4);
+
+    if (options.label) {
+      var c1x1 = xm1 - 3*cline1*Math.sin(theta);
+      var c1y1 = ym1 + 3*cline1*Math.cos(theta); 
+      let textObj = new mechanicsObjects.LatexText(options.label, {
+        left: c1x1 + options.offsetx,
+        top:  c1y1 + options.offsety, 
+        textAlign: 'left',
+        fontSize: options.fontSize,
+        selectable: false,
+        originX: 'center',
+        originY: 'center',
+      })
+      canvas.add(textObj);
+    }
+
+    return;
+  }
+
+  static get_button_tooltip() {
+    return 'Add battery';
+  }
+};
+
+
 mechanicsObjects.attachHandlersNoClone = function (
   subObj,
   reference,
