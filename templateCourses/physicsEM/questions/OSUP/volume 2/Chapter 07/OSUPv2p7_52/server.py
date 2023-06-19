5ec2094c8e7d6dbf83f3dc36dd9deaba8a84f617
Mariana Silva <mfsilva@illinois.edu>
2023-06-19 12:50:53 -0500

    modified chapter 7 questions

--- templateCourses/physicsEM/questions/OSUP/volume 2/Chapter 07/OSUPv2p7_52/server.py
+++ templateCourses/physicsEM/questions/OSUP/volume 2/Chapter 07/OSUPv2p7_52/server.py
@@ -1,26 +1,9 @@
-import copy
-import math
+
 import random
 
-import ext
 import numpy as np
 
-# Tolerance for pl-number-input
-rtol = 0.03
-
-# For error checking...
-errorCheck = "true"
-
-# For the attribution...
-displayAttribution = "true"
-source = "OSUP"
-volume = 2
-chapter = 7
-
-
 def generate(data):
-    # Pass the rtol value to {{params.rtol}}.
-    data["params"]["rtol"] = str(rtol)
 
     # Sample random numbers
     point = random.choice(["$P_1$", "$P_2$", "$P_3$", "$P_4$"])
@@ -45,49 +28,4 @@ def generate(data):
     V = q1 / (4 * np.pi * e0 * d1) + q2 / (4 * np.pi * e0 * d2)
 
     # Put the solution into data['correct_answers']
-    data["correct_answers"]["V"] = ext.round_sig(V, 3)
-
-    # Write the solutions formatted using scientific notation while keeping 3 sig figs.
-    data["correct_answers"]["Vstr"] = "{:.2e}".format(V)
-
-    # To display an attribution, use ext.attribution(displayAttribution, source, volume, chapter).
-    # displatAttribution is a string and should be either 'true' or 'false'.  The source parameter
-    # is also a string.  Currently, the allowed values are 'original' (the default) and 'OSUP' for
-    # Open Stax University Physics.  The volume and chapter parameters are integers and the default
-    # values are zero.
-    data["params"]["attribution"] = ext.attribution(
-        displayAttribution, source, volume, chapter
-    )
-
-
-# Access the submitted answers
-varstr = ["V"]
-stringData = ["V"]
-units = ["$\\rm\ V$"]  # Use LaTeX notation
-
-
-def parse(data):
-    # Call a function to check if the submitted answers should be re-expressed using scientific notation.
-    cnt = 0
-    for k in varstr:
-        data["submitted_answers"][k + "str"] = ext.sigFigCheck(
-            data["submitted_answers"][k], stringData[cnt], units[cnt]
-        )
-        cnt += 1
-
-
-# Provide hints.
-def grade(data):
-    # Call a function to check for easily-identifiable errors.
-    # The syntax is ext.ErrorCheck(errorCheck, submittedAnswer, correctAnswer, LaTeXsyntax, relativeTolerance)
-    # To enable error checking, set check = 'true'.
-    cnt = 0
-    for k in varstr:
-        data["feedback"][k] = ext.ErrorCheck(
-            errorCheck,
-            data["submitted_answers"][k],
-            data["correct_answers"][k],
-            stringData[cnt],
-            rtol,
-        )
-        cnt += 1
+    data["correct_answers"]["V"] = V #ext.round_sig(V, 3)
---
+++ templateCourses/physicsEM/questions/twoParticlesFindPotential/server.py
@@ -0,0 +1,54 @@
+import random
+import numpy as np
+
+def generate(data):
+
+    pA = [200,150]
+    # distance from each particle to the center
+    d = random.choice([6,7])
+    dplot = d*20
+     
+    pP = [pA[0] - dplot, pA[1]]
+    pM = [pA[0] + dplot, pA[1]]
+    data["params"]["pA"] = pA
+    data["params"]["pP"] = pP
+    data["params"]["pM"] = pM
+    data["params"]["d"] = str(d)+" cm"
+
+    # point to find the potential
+    p_pos = random.choice([0,1]) #0: point in x-axis; 1: point in y-axis
+    p_sign = random.choice([-1,1])
+      
+    offset = 0 
+    if p_pos == 0:
+        dP = random.choice([2,3,4])
+        p = [pA[0] + p_sign*dP*20, pA[1]]   
+        d1 = d + p_sign*dP
+        d2 = d - p_sign*dP
+    else:
+        dP = random.choice([4,5])
+        p = [pA[0] , pA[1] + p_sign*dP*20]
+        if p_sign == 1:
+            offset = -10
+        else:
+            offset = 15
+        d1 = np.sqrt(d**2 + dP**2)  
+        d2  = d1
+
+    data["params"]["dP"] = str(dP)+" cm"
+    data["params"]["p"] = p
+    data["params"]["offset"] = offset
+
+    # # Compute the solution
+    q1 = random.randint(2,6)
+    data["params"]["q1"] = str(q1)
+    q1 = q1*1e-3  # C
+
+    q2 = -random.randint(7,14)
+    data["params"]["q2"] = str(q2)
+    q2 = q2*1e-3  # C
+
+    e0 = 8.85e-12
+    V = q1 / (4 * np.pi * e0 * d1) + q2 / (4 * np.pi * e0 * d2)
+    data["correct_answers"]["V"] = V
+
