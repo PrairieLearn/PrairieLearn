e6cd0584596d6a7adbcb635de4a96a6539a3e4d7
Mariana Silva <mfsilva@illinois.edu>
2023-06-19 20:46:51 -0500

    adjusted mechanics questions graph support line

--- templateCourses/physicsMechanics/questions/forceTimePlot/server.py
+++ templateCourses/physicsMechanics/questions/forceTimePlot/server.py
@@ -2,21 +2,6 @@ import random
 
 import numpy as np
 
-
-def create_dict_labels(axis, loc, lab, offsetx=None, offsety=None):
-    text = '{"axis": "' + axis + '", "pos": ' + str(loc) + ', "lab": "' + lab + '" '
-    if offsetx is not None:
-        text += ', "offsetx": ' + str(offsetx)
-    if offsety is not None:
-        text += ', "offsety": ' + str(offsety)
-    text += "}"
-    return text
-
-
-def create_dict_xy_coord(p):
-    return '{"x": ' + str(p[0]) + ',"y": ' + str(p[1]) + "}"
-
-
 def generate(data):
 
     ratiox = 100
@@ -105,16 +90,34 @@ def generate(data):
         loc_y = [v1, v2]  # location of the text
         lab_y = [str(v1 / ratioy), str(v2 / ratioy)]  # text
     # data params (no need to change this)
-    labels = "["
+    supp_lines = "["
+    labels = '['
     for i in range(len(loc_x)):
         labels += create_dict_labels("x", loc_x[i], lab_x[i])
         labels += ","
     for i in range(len(loc_y)):
         labels += create_dict_labels("y", loc_y[i], lab_y[i], offsetx=-20)
+        supp_lines += '{"y": ' + str(loc_y[i]) + '}'
         if i != len(loc_y) - 1:
             labels += ","
+            supp_lines += ","
     labels += "]"
+    supp_lines += ']'
 
     data["params"]["label_V"] = labels
+    data["params"]["supp_lines"] = supp_lines
 
     return data
+
+def create_dict_labels(axis, loc, lab, offsetx=None, offsety=None):
+    text = '{"axis": "' + axis + '", "pos": ' + str(loc) + ', "lab": "' + lab + '" '
+    if offsetx is not None:
+        text += ', "offsetx": ' + str(offsetx)
+    if offsety is not None:
+        text += ', "offsety": ' + str(offsety)
+    text += "}"
+    return text
+
+
+def create_dict_xy_coord(p):
+    return '{"x": ' + str(p[0]) + ',"y": ' + str(p[1]) + "}"
