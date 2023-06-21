f3c3086ed88befb59c538d7bd8be850edadd8341
caleqw2 <caleqw2@illinois.edu>
2023-06-21 15:15:15 -0500

    Restrict view access to questions in instance question list

--- apps/prairielearn/src/pages/studentAssessmentInstanceHomework/studentAssessmentInstanceHomework.sql
+++ apps/prairielearn/src/pages/studentAssessmentInstanceHomework/studentAssessmentInstanceHomework.sql
@@ -7,6 +7,13 @@ WHERE
   ai.id = $assessment_instance_id;
 
 -- BLOCK get_questions
+WITH group_role_ids AS (
+    SELECT group_role_id
+    FROM group_user_roles AS gur
+    JOIN groups AS g ON gur.group_id = g.id
+    JOIN assessment_instances AS ai ON g.id = ai.group_id
+    WHERE gur.user_id = $user_id AND ai.id = $assessment_instance_id
+)
 SELECT
   iq.*,
   ((lag(z.id) OVER w) IS DISTINCT FROM z.id) AS start_new_zone,
@@ -35,7 +42,14 @@ SELECT
   qo.sequence_locked AS sequence_locked,
   (lag(aq.effective_advance_score_perc) OVER w) AS prev_advance_score_perc,
   (lag(qo.question_number) OVER w) AS prev_title,
-  (lag(qo.sequence_locked) OVER w) AS prev_sequence_locked
+  (lag(qo.sequence_locked) OVER w) AS prev_sequence_locked,
+  (
+    SELECT bool_or(aqrp.can_view)
+    FROM assessment_questions AS aq
+    JOIN assessment_question_role_permissions AS aqrp ON aqrp.assessment_question_id = aq.id
+    WHERE aq.id = iq.assessment_question_id
+      AND aqrp.group_role_id IN (SELECT group_role_id FROM group_role_ids)
+  ) AS can_view
 FROM
   instance_questions AS iq
   JOIN assessment_questions AS aq ON (aq.id = iq.assessment_question_id)
