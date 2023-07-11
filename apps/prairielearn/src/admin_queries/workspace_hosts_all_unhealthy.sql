6454251d42f4f1a8994ec53ca927e31f2cb847bc
Nathan Walters <nathan@prairielearn.com>
2023-07-11 12:13:14 -0700

    Add workspace host logs (#7333)
    * Create workspace_host_logs table
    
    * Drop enum_log_level
    
    * Make date column non-nullable
    
    * Add logs to admin query
    
    * Add more logs
    
    * Adding logs; refactoring away from sprocs
    
    * Move recapturing logic to lib
    
    * Move extra host draining logic to lib
    
    * Move terminable hosts logic to lib
    
    * Finish refactor
    
    * Test for more logs
    
    * Improve tests and logs
    
    * Add index
    
    * Shut down database after test
    
    * Fix type errors
    
    * Drop unnecessary migration
    
    * Refactor workspace host transitions
    
    * Final cleanup and refactoring
    
    * Use db-types
    
    * Mark workspace_host.instance_id as non-nullable
    
    * Reformat files

--- apps/prairielearn/src/admin_queries/workspace_hosts_all_unhealthy.sql
+++ apps/prairielearn/src/admin_queries/workspace_hosts_all_unhealthy.sql
@@ -1,14 +1,31 @@
-UPDATE workspace_hosts AS wh
-SET
-  state = 'unhealthy',
-  state_changed_at = NOW(),
-  unhealthy_at = NOW(),
-  unhealthy_reason = $unhealthy_reason
-WHERE
-  wh.state IN ('launching', 'ready', 'draining')
-RETURNING
-  wh.id AS workspace_host_id,
-  wh.instance_id,
-  wh.state,
-  wh.unhealthy_at,
-  wh.unhealthy_reason;
+WITH
+  updated_workspace_hosts AS (
+    UPDATE workspace_hosts AS wh
+    SET
+      state = 'unhealthy',
+      state_changed_at = NOW(),
+      unhealthy_at = NOW(),
+      unhealthy_reason = $unhealthy_reason
+    WHERE
+      wh.state IN ('launching', 'ready', 'draining')
+    RETURNING
+      wh.id AS workspace_host_id,
+      wh.instance_id,
+      wh.state,
+      wh.unhealthy_at,
+      wh.unhealthy_reason
+  ),
+  logs AS (
+    INSERT INTO
+      workspace_host_logs (workspace_host_id, state, message)
+    SELECT
+      workspace_host_id,
+      state,
+      unhealthy_reason
+    FROM
+      updated_workspace_hosts
+  )
+SELECT
+  *
+FROM
+  updated_workspace_hosts;
