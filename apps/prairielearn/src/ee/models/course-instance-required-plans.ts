b4d0aa9760361e7861366f6ba516cf383aa89977
Nathan Walters <nwalters512@gmail.com>
2023-07-21 11:50:36 -0700

    Add missing context to audit logs

--- apps/prairielearn/src/ee/models/course-instance-required-plans.ts
+++ apps/prairielearn/src/ee/models/course-instance-required-plans.ts
@@ -1,9 +1,14 @@
 import { loadSqlEquiv, queryRow } from '@prairielearn/postgres';
 import { PlanName } from '../lib/billing/plans-types';
-import { CourseInstanceRequiredPlanSchema } from '../../lib/db-types';
+import { CourseInstanceRequiredPlanSchema, IdSchema } from '../../lib/db-types';
 import { insertAuditLog } from '../../models/audit-log';
 
 const sql = loadSqlEquiv(__filename);
+
+const CourseInstanceRequiredPlanWithContextSchema = CourseInstanceRequiredPlanSchema.extend({
+  course_id: IdSchema,
+  institution_id: IdSchema,
+});
 
 export async function insertCourseInstanceRequiredPlan(
   course_instance_id: string,
@@ -13,14 +18,14 @@ export async function insertCourseInstanceRequiredPlan(
   const newRequiredPlan = await queryRow(
     sql.insert_required_plan_for_course_instance,
     { course_instance_id, plan_name: plan },
-    CourseInstanceRequiredPlanSchema,
+    CourseInstanceRequiredPlanWithContextSchema,
   );
   await insertAuditLog({
     authn_user_id,
     table_name: 'course_instance_required_plans',
     action: 'insert',
-    institution_id: null, // TODO: get value for this
-    course_id: null, // TODO: get value for this
+    institution_id: newRequiredPlan.institution_id,
+    course_id: newRequiredPlan.course_id,
     course_instance_id,
     new_state: newRequiredPlan,
     row_id: newRequiredPlan.id,
@@ -39,14 +44,14 @@ export async function deleteCourseInstanceRequiredPlan(
       course_instance_id,
       plan_name: plan,
     },
-    CourseInstanceRequiredPlanSchema,
+    CourseInstanceRequiredPlanWithContextSchema,
   );
   await insertAuditLog({
     authn_user_id,
     table_name: 'course_instance_required_plans',
     action: 'delete',
-    institution_id: null, // TODO: get value for this
-    course_id: null, // TODO: get value for this
+    institution_id: deletedRequiredPlan.institution_id,
+    course_id: deletedRequiredPlan.institution_id,
     course_instance_id,
     old_state: deletedRequiredPlan,
     row_id: deletedRequiredPlan.id,
