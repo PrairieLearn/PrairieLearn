0392c065dfe484cc17d64602f7c9384c794a79a4
Nathan Walters <nathan@prairielearn.com>
2023-07-21 09:36:11 -0700

    Fix localhost cookies for Safari (#8202)

--- apps/prairielearn/src/lib/authn.js
+++ apps/prairielearn/src/lib/authn.js
@@ -4,6 +4,7 @@ const sqldb = require('@prairielearn/postgres');
 const { generateSignedToken } = require('@prairielearn/signed-token');
 
 const { config } = require('../lib/config');
+const { shouldSecureCookie } = require('../lib/cookie');
 const { InstitutionSchema, UserSchema } = require('./db-types');
 
 const sql = sqldb.loadSqlEquiv(__filename);
@@ -66,7 +67,7 @@ module.exports.loadUser = async (req, res, authnParams, optionsParams = {}) => {
     res.cookie('pl_authn', pl_authn, {
       maxAge: config.authnCookieMaxAgeMilliseconds,
       httpOnly: true,
-      secure: true,
+      secure: shouldSecureCookie(req),
     });
   }
 
