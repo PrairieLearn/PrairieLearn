b447ee2f6b22e37ec3908c4be44625e78d719e56
Nathan Walters <nwalters512@gmail.com>
2023-08-07 11:57:22 -0700

    Improve verification

--- apps/prairielearn/src/ee/webhooks/stripe/index.ts
+++ apps/prairielearn/src/ee/webhooks/stripe/index.ts
@@ -1,26 +1,34 @@
 import express = require('express');
 import asyncHandler = require('express-async-handler');
+import error = require('@prairielearn/error');
 
 import { config } from '../../../lib/config';
 import { getStripeClient } from '../../lib/billing/stripe';
 
 const router = express.Router({ mergeParams: true });
 
-router.post(
-  '/',
-  express.raw({ type: 'application/json' }),
-  asyncHandler(async (req, res) => {
-    if (!config.stripeWebhookSigningSecret) {
-      throw new Error('Stripe is not configured.');
-    }
+function constructEvent(req: express.Request) {
+  if (!config.stripeWebhookSigningSecret) {
+    throw new Error('Stripe is not configured.');
+  }
 
-    const stripe = getStripeClient();
-
-    const event = stripe.webhooks.constructEvent(
+  const stripe = getStripeClient();
+  try {
+    return stripe.webhooks.constructEvent(
       req.body,
       req.headers['stripe-signature'] as string,
       config.stripeWebhookSigningSecret,
     );
+  } catch (err) {
+    throw error.make(400, `Webhook error: ${err.message}`);
+  }
+}
+
+router.post(
+  '/',
+  express.raw({ type: 'application/json' }),
+  asyncHandler(async (req, res) => {
+    const event = constructEvent(req);
 
     console.log(event);
 
