97c2d5578461529160abbf5b2ab7bc771d7f0225
Nathan Walters <nwalters512@gmail.com>
2023-08-15 16:26:29 -0700

    Use Matt's version of the query

--- apps/prairielearn/src/pages/administratorInstitutions/administratorInstitutions.sql
+++ apps/prairielearn/src/pages/administratorInstitutions/administratorInstitutions.sql
@@ -2,19 +2,22 @@
 SELECT
   to_jsonb(i.*) AS institution,
   coalesce(
-    jsonb_agg(
-      ap.name
-      ORDER BY
-        ap.name
-    ) FILTER (
+    (
+      SELECT
+        jsonb_agg(
+          ap.name
+          ORDER BY
+            ap.name
+        )
+      FROM
+        institution_authn_providers AS iap
+        JOIN authn_providers AS ap ON (ap.id = iap.authn_provider_id)
       WHERE
-        ap.id IS NOT NULL
+        iap.institution_id = i.id
     ),
     '[]'::jsonb
   ) AS authn_providers
 FROM
   institutions AS i
-  LEFT JOIN institution_authn_providers AS iap ON (iap.institution_id = i.id)
-  LEFT JOIN authn_providers AS ap ON (ap.id = iap.authn_provider_id)
-GROUP BY
-  i.id;
+ORDER BY
+  i.id ASC;
