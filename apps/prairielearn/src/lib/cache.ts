873f8e199aa9dcc256b384c0dbe7392379528d93
Nathan Sarang-Walters <nathan@prairielearn.com>
2023-08-24 09:07:24 -0700

    Make cache TTL mandatory (#8386)
    * Make cache TTL mandatory
    
    * Scope question cache keys
    
    * Remove undefined callbacks

--- apps/prairielearn/src/lib/cache.ts
+++ apps/prairielearn/src/lib/cache.ts
@@ -41,12 +41,12 @@ export async function init() {
   }
 }
 
-export function set(key: string, value: any, maxAgeMS?: number) {
+export function set(key: string, value: any, maxAgeMS: number) {
   if (!cacheEnabled) return;
 
   switch (cacheType) {
     case 'memory': {
-      cache.set(key, JSON.stringify(value), { ttl: maxAgeMS ?? undefined });
+      cache.set(key, JSON.stringify(value), { ttl: maxAgeMS });
       break;
     }
 
@@ -58,7 +58,7 @@ export function set(key: string, value: any, maxAgeMS?: number) {
       // We don't log the error because it contains the cached value,
       // which can be huge and which fills up the logs.
       client
-        .set(key, JSON.stringify(value), { PX: maxAgeMS ?? undefined })
+        .set(key, JSON.stringify(value), { PX: maxAgeMS })
         .catch((_err) => logger.error('Cache set error', { key, maxAgeMS }));
       break;
     }
