d8162342e2cefd38fccc083b1e6906dc9c8cb805
Firas Moosvi <firas@moosvi.com>
2023-08-26 13:13:27 -0700

    address a reviewer comment in the render function

--- apps/prairielearn/elements/pl-sketch/pl-sketch.py
+++ apps/prairielearn/elements/pl-sketch/pl-sketch.py
@@ -6,25 +6,25 @@ import prairielearn as pl
 def prepare(element_html, data):
     return data
 
-
 def render(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
     width = pl.get_integer_attrib(element, "width", 600)
     height = pl.get_integer_attrib(element, "height", 700)
-    if data["panel"] == "question":
-        if len(data["raw_submitted_answers"]) == 0:
-            skp_json = ""
-        else:
-            skp_json = data["raw_submitted_answers"]
 
-        html_params = {
-            "uuid": pl.get_uuid(),
-            "sketchpad_json": skp_json,
-            "width": width,
-            "height": height,
-        }
-
-        with open("pl-sketch.mustache", "r") as f:
-            return chevron.render(f, html_params).strip()
-    else:
+    if data["panel"] != "question":
         return ""
+
+    if len(data["raw_submitted_answers"]) == 0:
+        skp_json = ""
+    else:
+        skp_json = data["raw_submitted_answers"]
+
+    html_params = {
+        "uuid": pl.get_uuid(),
+        "sketchpad_json": skp_json,
+        "width": width,
+        "height": height,
+    }
+
+    with open("pl-sketch.mustache", "r") as f:
+        return chevron.render(f, html_params).strip()
