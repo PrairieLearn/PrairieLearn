b768f4a9a4aa9e15a4f9a94966fc6b681c206246
Nathan Walters <nwalters512@gmail.com>
2023-09-06 11:45:52 -0700

    Improve custom rule

--- packages/eslint-plugin-prairielearn/src/index.ts
+++ packages/eslint-plugin-prairielearn/src/index.ts
@@ -3,5 +3,3 @@ import noAwsDirectConstruction from './rules/no-aws-direct-construction';
 export const rules = {
   'no-aws-direct-construction': noAwsDirectConstruction,
 };
-
-console.log('loaded!!');
--- packages/eslint-plugin-prairielearn/src/rules/no-aws-direct-construction.ts
+++ packages/eslint-plugin-prairielearn/src/rules/no-aws-direct-construction.ts
@@ -1,23 +1,32 @@
+function isIdentifierClient(identifierName: string, packageName: string): boolean {
+  // If the identifier ends with "Client", include it in the set.
+  if (identifierName.endsWith('Client')) {
+    return true;
+  }
+
+  // If the identifier matches the package name directly, include it in the set.
+  const clientName = packageName.replace('@aws-sdk/client-', '');
+  const packageIdentifier = clientName.replace(/-/g, '').toLowerCase();
+  if (identifierName.toLowerCase() === packageIdentifier) {
+    return true;
+  }
+
+  return false;
+}
+
 export default {
   create(context: any) {
     const awsClientImports = new Set();
 
     return {
+      // Handle `import ...` statements
       ImportDeclaration(node: any) {
         const importSource = node.source.value;
         if (importSource.startsWith('@aws-sdk/client-')) {
           node.specifiers.forEach((specifier: any) => {
             if (specifier.type === 'ImportSpecifier') {
               const specifierName = specifier.imported.name;
-              // If the identifier ends with "Client", include it in the set.
-              if (specifierName.endsWith('Client')) {
-                awsClientImports.add(specifierName);
-              }
-
-              // If the identifier matches the package name directly, include it in the set.
-              const packageName = importSource.replace('@aws-sdk/client-', '');
-              const packageIdentifier = packageName.replace(/-/g, '').toLowerCase();
-              if (specifierName.toLowerCase() === packageIdentifier) {
+              if (isIdentifierClient(specifierName, importSource)) {
                 awsClientImports.add(specifierName);
               }
             }
@@ -25,6 +34,29 @@ export default {
         }
         if (awsClientImports.size > 0) {
           console.log(awsClientImports);
+        }
+      },
+      // Handle `const ... = require(...)` statements
+      VariableDeclarator(node: any) {
+        if (
+          node.init &&
+          node.init.type === 'CallExpression' &&
+          node.init.callee.name === 'require' &&
+          node.init.arguments.length === 1 &&
+          node.init.arguments[0].type === 'Literal' &&
+          typeof node.init.arguments[0].value === 'string' &&
+          node.init.arguments[0].value.startsWith('@aws-sdk/client-')
+        ) {
+          if (node.id.type !== 'ObjectPattern') {
+            throw new Error('Unexpected node type');
+          }
+
+          node.id.properties.forEach((property: any) => {
+            const specifierName = property.key.name;
+            if (isIdentifierClient(specifierName, node.init.arguments[0].value)) {
+              awsClientImports.add(property.value.name);
+            }
+          });
         }
       },
       NewExpression(node: any) {
