2ff15907c94b623d85fa55bb1f867a6ccf1d2d6c
Dave Mussulman <mussulma@gmail.com>
2023-09-06 08:52:20 -0500

    add session destroy

--- apps/prairielearn/src/pages/authLogout/authLogout.ts
+++ apps/prairielearn/src/pages/authLogout/authLogout.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import asyncHandler = require('express-async-handler');
+import util = require('util');
 import { config } from '../../lib/config';
+
 
 const router = Router();
 
 router.get(
   '/',
   asyncHandler(async (req, res, _next) => {
-
     res.clearCookie('pl_authn');
     res.clearCookie('pl_assessmentpw');
 
@@ -19,6 +20,11 @@ router.get(
       // until the next time the user authenticates.
       res.cookie('pl_disable_auto_authn', '1');
     }
+
+    await util.promisify(req.session.destroy);
+    // I don't like hardcoding this cookie name, but I didn't see how to get
+    // the name from express-session. Hacky until we replace with our own.
+    res.clearCookie('connect.sid');
 
     const redirect = req.query.redirect;
     if (redirect && typeof redirect === 'string') {
