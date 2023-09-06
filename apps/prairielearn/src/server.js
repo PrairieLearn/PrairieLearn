5727987e0ebe6eb336e9d84f0b896aa32b8678cd
Dave Mussulman <mussulma@gmail.com>
2023-09-06 08:44:30 -0500

    convert authLogout to typescript

--- apps/prairielearn/src/server.js
+++ apps/prairielearn/src/server.js
@@ -492,7 +492,7 @@ module.exports.initExpress = function () {
       res.locals.navPage = 'logout';
       next();
     },
-    require('./pages/authLogout/authLogout'),
+    require('./pages/authLogout/authLogout').default,
   ]);
   // disable SEB until we can fix the mcrypt issues
   // app.use('/pl/downloadSEBConfig', require('./pages/studentSEBConfig/studentSEBConfig'));
