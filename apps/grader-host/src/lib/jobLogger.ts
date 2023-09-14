564cda382c0ab41175ebfb5676953875cecc3020
Nathan Walters <nwalters512@gmail.com>
2023-09-14 16:39:31 -0700

    Truncate log buffers

--- apps/grader-host/src/lib/jobLogger.ts
+++ apps/grader-host/src/lib/jobLogger.ts
@@ -8,16 +8,42 @@ interface WinstonBufferedLogger extends winston.Logger {
   getBuffer(): Buffer;
 }
 
+interface BufferedWritableStreamOptions extends WritableOptions {
+  maxBuffer: number;
+}
+
 export class BufferedWritableStream extends Writable {
   buffer: any[];
+  maxBuffer: number | null;
+  bufferSize: number;
 
-  constructor(options?: WritableOptions) {
+  constructor(options?: BufferedWritableStreamOptions) {
     super(options);
     this.buffer = [];
+    this.maxBuffer = options?.maxBuffer ?? null;
+    this.bufferSize = 0;
   }
 
   _write(chunk: any, _encoding: BufferEncoding, callback: (error?: Error | null) => void) {
-    this.buffer.push(chunk);
+    // If we've already reached the max buffer size, just drop the chunk.
+    if (this.bufferSize === this.maxBuffer) {
+      callback();
+      return;
+    }
+
+    const buffer = Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk);
+
+    if (this.maxBuffer && this.bufferSize + buffer.length > this.maxBuffer) {
+      const remainingBytes = this.maxBuffer - this.bufferSize;
+      this.buffer.push(buffer.subarray(0, remainingBytes));
+      this.bufferSize = this.maxBuffer;
+
+      callback();
+      return;
+    }
+
+    this.bufferSize += buffer.length;
+    this.buffer.push(buffer);
     callback();
   }
 
@@ -29,7 +55,10 @@ export class BufferedWritableStream extends Writable {
 export function makeJobLogger(): WinstonBufferedLogger {
   const transports: Transport[] = [];
 
-  const bufferedStream = new BufferedWritableStream();
+  const bufferedStream = new BufferedWritableStream({
+    // Collect at most 1MB of logs.
+    maxBuffer: 1024 * 1024,
+  });
 
   if (config.useConsoleLoggingForJobs) {
     transports.push(
