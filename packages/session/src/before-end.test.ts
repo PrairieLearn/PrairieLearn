76c6b4f33834ca135350017fc6ee2e5dd40b3a36
Nathan Sarang-Walters <nathan@prairielearn.com>
2023-09-19 11:05:22 -0700

    Implement custom session functionality (#8444)
    * Scaffold out new sessions package
    
    * Basic setting of cookies
    
    * Session stores implemented
    
    * Get all tests passing
    
    * Remove dead code
    
    * Work on test coverage
    
    * Improve functionality and tests
    
    * Implement more functionality
    
    * WIP cookie options
    
    * Commit end-of-week progress
    
    * Improve tests, fix types
    
    * Simplify things
    
    * Use constants for defaults
    
    * Dedupe
    
    * Use async UID generation
    
    * Add more context about why custom solution is needed
    
    * Dedupe

---
+++ packages/session/src/before-end.test.ts
@@ -0,0 +1,34 @@
+import express, { type Request, type Response, type NextFunction } from 'express';
+import fetch from 'node-fetch';
+import { assert } from 'chai';
+
+import { beforeEnd } from './before-end';
+import { withServer } from './test-utils';
+
+describe('beforeEnd', () => {
+  it('handles errors correctly', async () => {
+    const app = express();
+    app.use((_req, res, next) => {
+      beforeEnd(res, next, async () => {
+        throw new Error('oops');
+      });
+
+      next();
+    });
+
+    app.get('/', (_req, res) => res.sendStatus(200));
+
+    let error: Error | null = null;
+    app.use((err: any, _req: Request, _res: Response, next: NextFunction) => {
+      error = err;
+      next();
+    });
+
+    await withServer(app, async ({ url }) => {
+      const res = await fetch(url);
+
+      assert.equal(res.status, 200);
+      assert.equal(error?.message, 'oops');
+    });
+  });
+});
