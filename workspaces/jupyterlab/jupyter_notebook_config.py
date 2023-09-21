d6a9b16d3803a912bfc13ff83b7e68bcc44a3e31
Nicolas Nytko <nnytko2@illinois.edu>
2023-09-21 12:51:21 -0500

    Bump Jupyter workspace version

--- workspaces/jupyterlab/jupyter_notebook_config.py
+++ workspaces/jupyterlab/jupyter_notebook_config.py
@@ -1,14 +1,14 @@
 import os
 
-c.NotebookApp.base_url = "/"
+c.ServerApp.base_url = '/'
 if "WORKSPACE_BASE_URL" in os.environ:
-    c.NotebookApp.base_url = os.environ["WORKSPACE_BASE_URL"]
+    c.NotebookApp.base_url = os.environ['WORKSPACE_BASE_URL']
 
-c.NotebookApp.ip = "0.0.0.0"
-c.NotebookApp.open_browser = False
-c.NotebookApp.password = ""
-c.NotebookApp.port = 8080
-c.NotebookApp.token = ""
-c.NotebookApp.allow_origin = "*"
+c.ServerApp.ip = "0.0.0.0"
+c.ServerApp.open_browser = False
+c.ServerApp.password = ''
+c.ServerApp.port = 8080
+c.ServerApp.token = ''
+c.ServerApp.allow_origin = '*'
 c.FileContentsManager.delete_to_trash = False
-c.FileCheckpoints.checkpoint_dir = "/tmp/ipynb_checkpoints"
+c.FileCheckpoints.checkpoint_dir = '/tmp/ipynb_checkpoints'
