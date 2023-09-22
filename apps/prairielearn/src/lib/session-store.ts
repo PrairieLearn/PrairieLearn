0b767bad8d9d074f9366bfb55aba1631a5a0219d
Nathan Walters <nwalters512@gmail.com>
2023-09-22 16:13:07 -0700

    Add metrics

--- apps/prairielearn/src/lib/session-store.ts
+++ apps/prairielearn/src/lib/session-store.ts
@@ -1,12 +1,32 @@
 import { SessionStore } from '@prairielearn/session';
 import { loadSqlEquiv, queryAsync, queryOptionalRow } from '@prairielearn/postgres';
+import * as opentelemetry from '@prairielearn/opentelemetry';
 
 import { UserSessionSchema } from './db-types';
 
 const sql = loadSqlEquiv(__filename);
 
 export class PostgresSessionStore implements SessionStore {
+  private setCounter: opentelemetry.Counter;
+  private getCounter: opentelemetry.Counter;
+  private destroyCounter: opentelemetry.Counter;
+
+  constructor() {
+    const meter = opentelemetry.metrics.getMeter('prairielearn');
+    this.setCounter = opentelemetry.getCounter(meter, 'session_store.set', {
+      valueType: opentelemetry.ValueType.INT,
+    });
+    this.getCounter = opentelemetry.getCounter(meter, 'session_store.get', {
+      valueType: opentelemetry.ValueType.INT,
+    });
+    this.destroyCounter = opentelemetry.getCounter(meter, 'session_store.destroy', {
+      valueType: opentelemetry.ValueType.INT,
+    });
+  }
+
   async set(session_id: string, data: any, expires_at: Date) {
+    this.setCounter.add(1);
+
     await queryAsync(sql.set_session, {
       session_id,
       data: JSON.stringify(data),
@@ -16,6 +36,8 @@ export class PostgresSessionStore implements SessionStore {
   }
 
   async get(session_id: string) {
+    this.getCounter.add(1);
+
     const session = await queryOptionalRow(sql.get_session, { session_id }, UserSessionSchema);
 
     if (!session) {
@@ -29,6 +51,8 @@ export class PostgresSessionStore implements SessionStore {
   }
 
   async destroy(session_id: string) {
+    this.destroyCounter.add(1);
+
     await queryAsync(sql.destroy_session, { session_id });
   }
 }
