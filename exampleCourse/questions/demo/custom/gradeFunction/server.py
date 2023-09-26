d749063e16599ba4ae1c78c9c37f85b5707d6707
Jonatan Schroeder <jonatan@yorku.ca>
2023-09-26 11:27:05 -0400

    Review comments

--- exampleCourse/questions/demo/custom/gradeFunction/server.py
+++ exampleCourse/questions/demo/custom/gradeFunction/server.py
@@ -9,16 +9,16 @@ def generate(data):
 
 
 def parse(data):
+    # use get() for submitted_answers in case no answer was submitted
+    # get the submitted answer, defaulting to empty string if it's missing
     sub = data["submitted_answers"].get("c", "")
     if len(sub.replace("0", "").replace("1", "")) > 0:
         # format_errors tags the answer as invalid, which will keep the
         # question from being graded.
-        # We use the "c" key in case we have multiple feedbacks for different
-        # answers.
         data["format_errors"][
             "c"
         ] = "Your answer should not contain characters other than '0' and '1'"
-    if sub != "" and sub[0] == "0":
+    if sub[:1] == "0":
         # feedback does not affect the grading process and can be used for
         # neutral comments that show up even if the student selects "Save Only"
         # instead of "Save and Grade".
@@ -28,7 +28,6 @@ def parse(data):
 
 
 def grade(data):
-    # use get() for submitted_answers in case no answer was submitted
     sub = data["submitted_answers"].get("c", "").lstrip("0")
     if sub == data["correct_answers"]["c"]:
         data["score"] = 1
@@ -36,11 +35,8 @@ def grade(data):
         data["score"] = 0
 
         # Store some feedback to display to the student.
-        # We use the "c" key in case we have multiple feedbacks for different answers.
         # This feedback is shown to students on every submission,
         # even if they have attempts remaining, so it shouldn't give away the answer.
-
-        # get the submitted answer, defaulting to empty string if it's missing
         if len(sub) != len(data["correct_answers"]["c"]):
             data["feedback"]["c"] = "Your answer has the wrong length"
         else:
