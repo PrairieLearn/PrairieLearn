91c92b370d7d1473326585d2720a8266677daa45
Jonatan Schroeder <jonatan@yorku.ca>
2023-09-26 11:02:35 -0400

    Add example course question that changes feedback on parse

--- exampleCourse/questions/demo/custom/gradeFunction/server.py
+++ exampleCourse/questions/demo/custom/gradeFunction/server.py
@@ -8,6 +8,14 @@ def generate(data):
     data["correct_answers"]["c"] = c
 
 
+def parse(data):
+    sub = data["submitted_answers"].get("c", "")
+    if len(sub.replace("0", "").replace("1", "")) > 0:
+        data["feedback"]["c"] = data["format_errors"][
+            "c"
+        ] = "Your answer should not contain characters other than '0' and '1'"
+
+
 def grade(data):
     # use get() for submitted_answers in case no answer was submitted
     if data["submitted_answers"].get("c", None) == data["correct_answers"]["c"]:
@@ -19,16 +27,14 @@ def grade(data):
         # We use the "c" key in case we have multiple feedbacks for different answers.
         # This feedback is shown to students on every submission,
         # even if they have attempts remaining, so it shouldn't give away the answer.
+        # Some feedback is already provided in the parse function.
 
         # get the submitted answer, defaulting to empty string if it's missing
-        sub = data["submitted_answers"].get("c", "")
-        if len(sub.replace("0", "").replace("1", "")) > 0:
-            data["feedback"][
-                "c"
-            ] = "Your answer should not contain characters other than '0' and '1'"
-        elif len(sub) != len(data["correct_answers"]["c"]):
-            data["feedback"]["c"] = "Your answer has the wrong length"
-        else:
-            data["feedback"][
-                "c"
-            ] = "Your answer was has the correct length and format, but the value is wrong"
+        if "c" not in data["feedback"]:
+            sub = data["submitted_answers"].get("c", "")
+            if len(sub) != len(data["correct_answers"]["c"]):
+                data["feedback"]["c"] = "Your answer has the wrong length"
+            else:
+                data["feedback"][
+                    "c"
+                ] = "Your answer was has the correct length and format, but the value is wrong"
