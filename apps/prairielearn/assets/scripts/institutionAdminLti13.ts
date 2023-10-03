d94c1ed59b70869c767c79c165ff1666dacf9d8a
Dave Mussulman <mussulma@gmail.com>
2023-10-03 16:42:01 -0500

    fix asset copy

---
+++ apps/prairielearn/assets/scripts/institutionAdminLti13.ts
@@ -0,0 +1,25 @@
+import { onDocumentReady, decodeData } from '@prairielearn/browser-utils';
+
+onDocumentReady(() => {
+  const platformDefaults = decodeData('platform_defaults_data');
+
+  const platformSelector = document.querySelector<HTMLSelectElement>('#choosePlatform');
+  const updateParams = document.querySelector<HTMLInputElement>('#update_params');
+
+  platformSelector.addEventListener('change', () => {
+    if (!updateParams.checked || platformSelector.value === 'Unknown') {
+      return;
+    }
+
+    const ip = document.querySelector<HTMLInputElement>('#issuer_params');
+
+    const platform_default = platformDefaults.find(
+      ({ platform }) => platform === platformSelector.value,
+    );
+    if (!platform_default) {
+      return;
+    }
+
+    ip.value = JSON.stringify(platform_default.issuer_params, null, 3);
+  });
+});
