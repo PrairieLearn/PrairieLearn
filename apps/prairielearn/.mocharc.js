aefb6854736e5ef0f43d9105d107bcdc9d9f979c
Nathan Walters <nwalters512@gmail.com>
2023-10-20 16:51:21 -0700

    Replace ts-node with tsx

--- apps/prairielearn/.mocharc.js
+++ apps/prairielearn/.mocharc.js
@@ -1,13 +1,5 @@
 module.exports = {
-  require: [
-    // Set up `TS_NODE_*` environment variables. This must be loaded before
-    // `ts-node/register`. Environment variables are implemented here instead
-    // of via `ENV=...` so that they're used even when executing Mocha directly,
-    // e.g. `yarn mocha path/to/file.ts`.
-    './src/tests/mocha-env.mjs',
-    'ts-node/register',
-    './src/tests/mocha-hooks.ts',
-  ],
+  require: ['tsx/cjs', './src/tests/mocha-hooks.ts'],
   timeout: '30000', // in milliseconds
   'watch-files': ['.'],
 };
