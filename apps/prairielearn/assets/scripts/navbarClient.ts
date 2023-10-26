2728891aed4637aa6119f974f8ad6ab620e1030d
Nathan Walters <nwalters512@gmail.com>
2023-10-26 16:59:12 -0700

    Convert navbar script into an asset

---
+++ apps/prairielearn/assets/scripts/navbarClient.ts
@@ -0,0 +1,154 @@
+import { onDocumentReady } from '@prairielearn/browser-utils';
+import Cookies from 'js-cookie';
+
+const expires = 30; // in units of days
+
+onDocumentReady(() => {
+  const usernameNav = document.getElementById('username-nav');
+  const accessAsAdministrator = usernameNav.dataset.accessAsAdministrator === 'true';
+  const viewType = usernameNav.dataset.viewType;
+  const authnCourseRole = usernameNav.dataset.authnCourseRole;
+  const authnCourseInstanceRole = usernameNav.dataset.authnCourseInstanceRole;
+  // Note: this corresponds to `user_with_requested_uid_has_instructor_access_to_course_instance`.
+  // Be careful what you use it for.
+  const hasInstructorAccess = usernameNav.dataset.hasInstructorAccess === 'true';
+
+  document.querySelector('#navbar-load-from-disk')?.addEventListener('click', () => {
+    Cookies.remove('pl_requested_uid', { path: '/' });
+    Cookies.remove('pl_requested_course_role', { path: '/' });
+    Cookies.remove('pl_requested_course_instance_role', { path: '/' });
+    Cookies.remove('pl_requested_mode', { path: '/' });
+    Cookies.remove('pl_requested_date', { path: '/' });
+    Cookies.set('pl_requested_data_changed', { path: '/' });
+  });
+
+  document.querySelector('#navbar-administrator-toggle')?.addEventListener('click', () => {
+    if (accessAsAdministrator) {
+      Cookies.set('pl_access_as_administrator', 'inactive', { path: '/', expires: expires });
+      Cookies.set('pl_requested_data_changed', { path: '/' });
+    } else {
+      Cookies.set('pl_access_as_administrator', 'active', { path: '/', expires: expires });
+      Cookies.set('pl_requested_data_changed', { path: '/' });
+    }
+    location.reload();
+  });
+
+  document.querySelector('#navbar-reset-view')?.addEventListener('click', () => {
+    Cookies.remove('pl_requested_uid', { path: '/' });
+    Cookies.remove('pl_requested_course_role', { path: '/' });
+    Cookies.remove('pl_requested_course_instance_role', { path: '/' });
+    Cookies.remove('pl_requested_mode', { path: '/' });
+    Cookies.remove('pl_requested_date', { path: '/' });
+    Cookies.set('pl_requested_data_changed', { path: '/' });
+
+    if (viewType === 'instructor') {
+      location.reload();
+    }
+  });
+
+  document.querySelector('#navbar-user-view-authn-instructor')?.addEventListener('click', () => {
+    Cookies.remove('pl_requested_uid', { path: '/' });
+    Cookies.remove('pl_requested_course_role', { path: '/' });
+    Cookies.remove('pl_requested_course_instance_role', { path: '/' });
+    Cookies.set('pl_requested_data_changed', { path: '/' });
+
+    if (viewType === 'instructor') {
+      location.reload();
+    }
+  });
+
+  document
+    .querySelector('#navbar-user-view-authn-student-no-rules')
+    ?.addEventListener('click', () => {
+      Cookies.remove('pl_requested_uid', { path: '/' });
+      Cookies.remove('pl_requested_course_role', { path: '/' });
+      Cookies.remove('pl_requested_course_instance_role', { path: '/' });
+      Cookies.set('pl_requested_data_changed', { path: '/' });
+
+      if (viewType === 'student') {
+        location.reload();
+      }
+    });
+
+  document.querySelector('#navbar-user-view-authn-student')?.addEventListener('click', () => {
+    Cookies.remove('pl_requested_uid', { path: '/' });
+    Cookies.set('pl_requested_course_role', 'None', { path: '/', expires: expires });
+    Cookies.set('pl_requested_course_instance_role', 'None', { path: '/', expires: expires });
+    Cookies.set('pl_requested_data_changed', { path: '/' });
+
+    if (viewType === 'student') {
+      location.reload();
+    }
+  });
+
+  document.querySelector('#navbar-user-view-instructor')?.addEventListener('click', () => {
+    Cookies.remove('pl_requested_course_role', { path: '/' });
+    Cookies.remove('pl_requested_course_instance_role', { path: '/' });
+    Cookies.set('pl_requested_data_changed', { path: '/' });
+
+    if (viewType === 'instructor') {
+      location.reload();
+    }
+  });
+
+  document.querySelector('#navbar-user-view-student-no-rules')?.addEventListener('click', () => {
+    Cookies.remove('pl_requested_course_role', { path: '/' });
+    Cookies.remove('pl_requested_course_instance_role', { path: '/' });
+    Cookies.set('pl_requested_data_changed', { path: '/' });
+
+    if (viewType === 'student') {
+      location.reload();
+    }
+  });
+
+  document.querySelector('#navbar-user-view-student')?.addEventListener('click', () => {
+    const courseRole = hasInstructorAccess ? 'None' : authnCourseRole;
+    const courseInstanceRole = hasInstructorAccess ? 'None' : authnCourseInstanceRole;
+
+    Cookies.set('pl_requested_course_role', courseRole, { path: '/', expires: expires });
+    Cookies.set('pl_requested_course_instance_role', courseInstanceRole, {
+      path: '/',
+      expires: expires,
+    });
+    Cookies.set('pl_requested_data_changed', { path: '/' });
+
+    if (viewType === 'student') {
+      location.reload();
+    }
+  });
+
+  const navbarUserEffectiveUidInput = document.querySelector<HTMLInputElement>(
+    '#navbar-user-effective-uid-input',
+  );
+  const navbarUserEffectiveUidButton = document.querySelector<HTMLButtonElement>(
+    '#navbar-user-effective-uid-button',
+  );
+
+  navbarUserEffectiveUidInput?.addEventListener('keyup', function (e) {
+    if (navbarUserEffectiveUidInput.value.trim() !== '') {
+      navbarUserEffectiveUidButton.removeAttribute('disabled');
+    } else {
+      navbarUserEffectiveUidButton.setAttribute('disabled', 'true');
+    }
+  });
+
+  navbarUserEffectiveUidInput?.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      const uid = navbarUserEffectiveUidInput.value.trim();
+      if (uid !== '') {
+        document.cookie = `pl_requested_uid=${uid}; path=/; expires=${expires}`;
+        document.cookie = 'pl_requested_data_changed; path=/';
+        location.reload();
+      }
+    }
+  });
+
+  navbarUserEffectiveUidButton?.addEventListener('click', function () {
+    const uid = navbarUserEffectiveUidInput.value.trim();
+    if (uid !== '') {
+      document.cookie = `pl_requested_uid=${uid}; path=/; expires=${expires}`;
+      document.cookie = 'pl_requested_data_changed; path=/';
+      location.reload();
+    }
+  });
+});
