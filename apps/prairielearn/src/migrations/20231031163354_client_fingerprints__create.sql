4de68e3e2bac7b0b606276911bf815d6469696d3
mylesw27 <myleswiegel27@gmail.com>
2023-10-31 13:22:37 -0700

    Create migration for client fingerprint table

---
+++ apps/prairielearn/src/migrations/20231031163354_client_fingerprints__create.sql
@@ -0,0 +1,28 @@
+CREATE TABLE
+  client_fingerprints (
+    id BIGSERIAL PRIMARY KEY,
+    ip_address INET NOT NULL,
+    user_agent TEXT NOT NULL,
+    accept TEXT NOT NULL,
+    accept_language TEXT NOT NULL
+  );
+
+-- add client_fingerprint_id to user_sessions
+ALTER TABLE user_sessions
+ADD COLUMN client_fingerprint_id BIGINT REFERENCES client_fingerprints (id);
+
+-- add client_fingerprint_id to page_view_logs
+ALTER TABLE page_view_logs
+ADD COLUMN client_fingerprint_id BIGINT REFERENCES client_fingerprints (id);
+
+-- add client_fingerprint_id to submissions
+ALTER TABLE submissions
+ADD COLUMN client_fingerprint_id BIGINT REFERENCES client_fingerprints (id);
+
+-- add client_fingerprint_id to assessment_instances
+ALTER TABLE assessment_instances
+ADD COLUMN last_client_fingerprint_id BIGINT REFERENCES client_fingerprints (id);
+
+-- add fingerprint count to assessment_instances
+ALTER TABLE assessment_instances
+ADD COLUMN client_fingerprint_id_change_count INT NOT NULL DEFAULT 0;
