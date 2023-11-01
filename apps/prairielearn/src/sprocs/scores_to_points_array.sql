fa5cc86b0cd784f06bd6e22339cccc82393f734c
Jonatan Schroeder <jonatan@yorku.ca>
2023-11-01 16:14:11 -0400

    Rename parameters for clarity

--- apps/prairielearn/src/sprocs/scores_to_points_array.sql
+++ apps/prairielearn/src/sprocs/scores_to_points_array.sql
@@ -1,19 +1,19 @@
 CREATE FUNCTION
     scores_to_points_array(
-        IN x DOUBLE PRECISION[],
-        IN y DOUBLE PRECISION[],
-        OUT product DOUBLE PRECISION[]
+        IN submission_scores DOUBLE PRECISION[],
+        IN points_list DOUBLE PRECISION[],
+        OUT submission_points DOUBLE PRECISION[]
     )
 AS $$
 DECLARE
     i integer;
     j integer := 1;
 BEGIN
-    FOR i in 1..coalesce(cardinality(x), 0) LOOP
-        IF x[i] IS NULL THEN
-            product[i] := NULL;
+    FOR i in 1..coalesce(cardinality(submission_scores), 0) LOOP
+        IF submission_scores[i] IS NULL THEN
+            submission_points[i] := NULL;
         ELSE
-            product[i] := x[i] * y[j];
+            submission_points[i] := submission_scores[i] * points_list[j];
             j := j + 1;
         END IF;
     END LOOP;
