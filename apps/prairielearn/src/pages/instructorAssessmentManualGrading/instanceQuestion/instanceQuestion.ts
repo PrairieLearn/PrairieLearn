1a52f03988be0ba2cbcfb9412347967175a853c4
Jonatan Schroeder <jonatan@yorku.ca>
2023-11-03 14:01:21 -0400

    Use general schema for body

--- apps/prairielearn/src/pages/instructorAssessmentManualGrading/instanceQuestion/instanceQuestion.ts
+++ apps/prairielearn/src/pages/instructorAssessmentManualGrading/instanceQuestion/instanceQuestion.ts
@@ -106,29 +106,69 @@ router.get(
   }),
 );
 
+const PostBodySchema = z.union([
+  z.object({
+    __action: z.literal('add_manual_grade'),
+    submission_id: IdSchema,
+    modified_at: z.string(),
+    rubric_item_selected_manual: IdSchema.or(IdSchema.array())
+      .nullish()
+      .transform((val) => (val == null ? [] : Array.isArray(val) ? val : [val])),
+    score_manual_adjust_points: z.coerce.number().nullish(),
+    use_score_perc: z.literal('on').optional(),
+    score_manual_points: z.coerce.number().nullish(),
+    score_manual_percent: z.coerce.number().nullish(),
+    score_auto_points: z.coerce.number().nullish(),
+    score_auto_percent: z.coerce.number().nullish(),
+    submission_note: z.string().nullish(),
+  }),
+  z.object({
+    __action: z.literal('modify_rubric_settings'),
+    use_rubric: z
+      .literal('true')
+      .optional()
+      .transform((val) => val === 'true'),
+    replace_auto_points: z.enum(['true', 'false']).transform((val) => val === 'true'),
+    starting_points: z.coerce.number(),
+    min_points: z.coerce.number(),
+    max_extra_points: z.coerce.number(),
+    tag_for_manual_grading: z
+      .literal('true')
+      .optional()
+      .transform((val) => val === 'true'),
+    rubric_item: z.record(
+      z.string(),
+      z.object({
+        id: z.string().optional(),
+        order: z.coerce.number(),
+        points: z.coerce.number(),
+        description: z.string(),
+        explanation: z.string().optional(),
+        grader_note: z.string().optional(),
+        always_show_to_students: z.string().transform((val) => val === 'true'),
+      }),
+    ),
+  }),
+  z.object({
+    __action: z
+      .string()
+      .startsWith('reassign_')
+      .transform((val) => val as `reassign_${string}`),
+  }),
+]);
+
 router.post(
   '/',
   asyncHandler(async (req, res) => {
     if (!res.locals.authz_data.has_course_instance_permission_edit) {
       throw error.make(403, 'Access denied (must be a student data editor)');
     }
-    if (req.body.__action === 'add_manual_grade') {
-      const body = z
-        .object({
-          submission_id: IdSchema,
-          modified_at: z.string(),
-          rubric_item_selected_manual: IdSchema.or(IdSchema.array())
-            .nullish()
-            .transform((val) => (val == null ? [] : Array.isArray(val) ? val : [val])),
-          score_manual_adjust_points: z.coerce.number().nullish(),
-          use_score_perc: z.literal('on').optional(),
-          score_manual_points: z.coerce.number().nullish(),
-          score_manual_percent: z.coerce.number().nullish(),
-          score_auto_points: z.coerce.number().nullish(),
-          score_auto_percent: z.coerce.number().nullish(),
-          submission_note: z.string().nullish(),
-        })
-        .parse(req.body);
+    const body = PostBodySchema.parse(
+      // Parse using qs, which allows deep objects to be created based on parameter names
+      // e.g., the key `rubric_item[cur1][points]` converts to `rubric_item: { cur1: { points: ... } ... }`
+      qs.parse(qs.stringify(req.body)),
+    );
+    if (body.__action === 'add_manual_grade') {
       const manual_rubric_data = res.locals.assessment_question.manual_rubric_id
         ? {
             rubric_id: res.locals.assessment_question.manual_rubric_id,
@@ -168,39 +208,7 @@ router.post(
           res.locals.instance_question.id,
         ),
       );
-    } else if (req.body.__action === 'modify_rubric_settings') {
-      const body = z
-        .object({
-          use_rubric: z
-            .literal('true')
-            .optional()
-            .transform((val) => val === 'true'),
-          replace_auto_points: z.enum(['true', 'false']).transform((val) => val === 'true'),
-          starting_points: z.coerce.number(),
-          min_points: z.coerce.number(),
-          max_extra_points: z.coerce.number(),
-          tag_for_manual_grading: z
-            .literal('true')
-            .optional()
-            .transform((val) => val === 'true'),
-          rubric_item: z.record(
-            z.string(),
-            z.object({
-              id: z.string().optional(),
-              order: z.coerce.number(),
-              points: z.coerce.number(),
-              description: z.string(),
-              explanation: z.string().optional(),
-              grader_note: z.string().optional(),
-              always_show_to_students: z.string().transform((val) => val === 'true'),
-            }),
-          ),
-        })
-        .parse(
-          // Parse using qs, which allows deep objects to be created based on parameter names
-          // e.g., the key `rubric_item[cur1][points]` converts to `rubric_item: { cur1: { points: ... } ... }`
-          qs.parse(qs.stringify(req.body)),
-        );
+    } else if (body.__action === 'modify_rubric_settings') {
       try {
         await manualGrading.updateAssessmentQuestionRubric(
           res.locals.instance_question.assessment_question_id,
@@ -217,8 +225,8 @@ router.post(
       } catch (err) {
         res.status(500).send({ err: String(err) });
       }
-    } else if (typeof req.body.__action === 'string' && req.body.__action.startsWith('reassign_')) {
-      const assigned_grader = req.body.__action.substring(9);
+    } else if (typeof body.__action === 'string' && body.__action.startsWith('reassign_')) {
+      const assigned_grader = body.__action.substring(9);
       await sqldb.queryAsync(sql.update_assigned_grader, {
         course_instance_id: res.locals.course_instance.id,
         assessment_id: res.locals.assessment.id,
@@ -239,7 +247,7 @@ router.post(
     } else {
       throw error.make(400, 'unknown __action', {
         locals: res.locals,
-        body: req.body,
+        body,
       });
     }
   }),
