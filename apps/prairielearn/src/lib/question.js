4ae9de7e79d0a45fd4428d8239711a4f817e4972
Jonatan Schroeder <jonatan@yorku.ca>
2023-11-10 16:54:35 -0500

    Revamp externalGrader.js library (#8824)
    * Revamp externalGrader.js library
    
    * Use assert instead of condition
    
    * Remove callback version of beginGradingJob[s]
    
    * Capture exception in Sentry
    
    * Remove from schema columns targeted for deletion (#8828)

--- apps/prairielearn/src/lib/question.js
+++ apps/prairielearn/src/lib/question.js
@@ -17,7 +17,7 @@ import { EncodedData } from '@prairielearn/browser-utils';
 
 import { config, setLocalsFromConfig } from './config';
 import { generateSignedToken } from '@prairielearn/signed-token';
-const externalGrader = require('./externalGrader');
+import * as externalGrader from './externalGrader';
 import * as manualGrading from './manualGrading';
 import * as ltiOutcomes from '../lib/ltiOutcomes';
 import * as sqldb from '@prairielearn/postgres';
@@ -707,7 +707,7 @@ export function gradeVariant(
       // data and grading_job might not be defined if we bailed out early above
       if (data && !data.broken && grading_job && grading_job.grading_method === 'External') {
         // We need to submit this external grading job.
-        externalGrader.beginGradingJob(grading_job.id, (err) => {
+        util.callbackify(externalGrader.beginGradingJob)(grading_job.id, (err) => {
           if (ERR(err, callback)) return;
           callback(null);
         });
@@ -772,7 +772,7 @@ export function saveAndGradeSubmission(
       if (grading_job_id !== undefined) {
         // We need to submit this grading job now that the
         // transaction has been committed
-        externalGrader.beginGradingJob(grading_job_id, (err) => {
+        util.callbackify(externalGrader.beginGradingJob)(grading_job_id, (err) => {
           if (ERR(err, callback)) return;
           callback(null, submission_id);
         });
