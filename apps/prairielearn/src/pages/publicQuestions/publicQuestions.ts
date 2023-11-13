b1b4927da2ee439d11010e162ee91832cc7b6171
Seth Poulsen <poulsenseth@yahoo.com>
2023-11-13 15:28:55 -0700

    fix import

--- apps/prairielearn/src/pages/publicQuestions/publicQuestions.ts
+++ apps/prairielearn/src/pages/publicQuestions/publicQuestions.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { QuestionsPage } from './publicQuestions.html';
 import { selectPublicQuestionsForCourse } from '../../models/questions';
-import { selectCourse } from '../../models/course';
+import { selectCourseById } from '../../models/course';
 import asyncHandler = require('express-async-handler');
 import { features } from '../../lib/features/index';
 import error = require('@prairielearn/error');
@@ -10,7 +10,7 @@ const router = Router({ mergeParams: true });
 router.get(
   '/',
   asyncHandler(async function (req, res) {
-    res.locals.course = await selectCourse({ course_id: req.params.course_id });
+    res.locals.course = await selectCourseById(req.params.course_id);
     const questionSharingEnabled = await features.enabled('question-sharing', {
       course_id: res.locals.course.id,
       institution_id: res.locals.course.institution_id,
