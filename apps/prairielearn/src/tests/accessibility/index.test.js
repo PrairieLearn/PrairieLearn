8df541324b457651982ed85d5a297d8834933254
Nathan Walters <nwalters512@gmail.com>
2023-11-14 13:56:53 -0800

    Skip accessibility tests for partials

--- apps/prairielearn/src/tests/accessibility/index.test.js
+++ apps/prairielearn/src/tests/accessibility/index.test.js
@@ -230,6 +230,11 @@ const SKIP_ROUTES = [
   '/pl/administrator/features/:feature',
   '/pl/administrator/features/:feature/modal',
 
+  // These are only HTML fragments rendered by HTMX; we can't test them as full
+  // HTML documents.
+  '/pl/navbar/course/:course_id/switcher',
+  '/pl/navbar/course/:course_id/course_instance_switcher/:course_instance_id?',
+
   // TODO: add tests for file editing/viewing.
   /\/file_edit\/\*$/,
   /\/file_view\/\*$/,
