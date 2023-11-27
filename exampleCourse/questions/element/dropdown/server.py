62513fe8377deebacac9cc9edc83b317ed6d84ee
Eliot Robson <eliot.robson24@gmail.com>
2023-11-27 17:07:08 -0600

    Switched dropdown question to use multiple choice element

--- exampleCourse/questions/element/dropdown/server.py
+++ exampleCourse/questions/element/dropdown/server.py
@@ -1,7 +1,5 @@
 def generate(data):
-    QUESTION1 = "aristotle"
-
-    data["params"][QUESTION1] = [
+    data["params"]["question1"] = [
         {"tag": "true", "ans": "whole"},
         {"tag": "false", "ans": "part"},
         {"tag": "false", "ans": "inverse"},
