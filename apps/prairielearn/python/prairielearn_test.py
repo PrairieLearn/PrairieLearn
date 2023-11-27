d4e4b0f43c4a5570885ee0ac228cf3e3958993d9
Eliot Robson <eliot.robson24@gmail.com>
2023-11-27 17:34:57 -0600

    Fixed test

--- apps/prairielearn/python/prairielearn_test.py
+++ apps/prairielearn/python/prairielearn_test.py
@@ -304,7 +304,7 @@ def test_grade_answer_parametrized_key_error_blank(
     question_data["format_errors"] = dict()
     pl.grade_answer_parameterized(question_data, question_name, grading_function)
 
-    assert question_data["submitted_answers"][question_name]["score"] == 0
+    assert question_data["partial_scores"][question_name]["score"] == 0.0
 
 
 @pytest.mark.repeat(100)
