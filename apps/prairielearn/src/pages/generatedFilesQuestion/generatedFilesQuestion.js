262d1a5e6770e1fb78466a78e0dc324952033ad9
Seth Poulsen <poulsenseth@yahoo.com>
2023-11-28 15:14:20 -0700

    asyncify generatedFiles endpoint

--- apps/prairielearn/src/pages/generatedFilesQuestion/generatedFilesQuestion.js
+++ apps/prairielearn/src/pages/generatedFilesQuestion/generatedFilesQuestion.js
@@ -1,7 +1,10 @@
+// @ts-check
 import { selectCourseById } from '../../models/course';
 import { selectQuestionById } from '../../models/question';
+import { promisify } from 'util';
+
+const asyncHandler = require('express-async-handler');
 const error = require('@prairielearn/error');
-var ERR = require('async-stacktrace');
 var express = require('express');
 var router = express.Router({ mergeParams: true });
 
@@ -10,49 +13,41 @@ var sqldb = require('@prairielearn/postgres');
 
 var sql = sqldb.loadSqlEquiv(__filename);
 
-router.get('/variant/:variant_id/*', function (req, res, next) {
-  Promise.all(
-    res.locals.public
-      ? [selectCourseById(req.params.course_id), selectQuestionById(req.params.question_id)]
-      : [Promise.resolve(res.locals.course), Promise.resolve(res.locals.question)],
-  )
-    .then(([course, questionData]) => {
-      res.locals.course = course;
-      res.locals.question = questionData;
+router.get(
+  '/variant/:variant_id/*',
+  asyncHandler(async function (req, res) {
+    if (res.locals.public) {
+      res.locals.course = await selectCourseById(req.params.course_id);
+      res.locals.question = await selectQuestionById(req.params.question_id);
+    }
 
-      if (res.locals.public && !res.locals.question.shared_publicly) {
-        return next(error.make(404, 'Not Found'));
-      }
+    if (res.locals.public && !res.locals.question.shared_publicly) {
+      throw error.make(404, 'Not Found');
+    }
 
-      var variant_id = req.params.variant_id;
-      var filename = req.params[0];
-      var params = {
-        // The instance question generally won't be present if this is used on
-        // an instructor route.
-        has_instance_question: !!res.locals.instance_question,
-        instance_question_id: res.locals.instance_question?.id,
-        question_id: res.locals.question.id,
-        variant_id: variant_id,
-      };
-      sqldb.queryOneRow(sql.select_variant, params, function (err, result) {
-        if (ERR(err, next)) return;
-        var variant = result.rows[0];
+    var variant_id = req.params.variant_id;
+    var filename = req.params[0];
+    var params = {
+      // The instance question generally won't be present if this is used on
+      // an instructor route.
+      has_instance_question: !!res.locals.instance_question,
+      instance_question_id: res.locals.instance_question?.id,
+      question_id: res.locals.question.id,
+      variant_id: variant_id,
+    };
+    const result = await sqldb.queryOneRowAsync(sql.select_variant, params);
+    const variant = result.rows[0];
 
-        question.getFile(
-          filename,
-          variant,
-          res.locals.question,
-          res.locals.course,
-          res.locals.authn_user.user_id,
-          function (err, fileData) {
-            if (ERR(err, next)) return;
-            res.attachment(filename);
-            res.send(fileData);
-          },
-        );
-      });
-    })
-    .catch((err) => next(err));
-});
+    const fileData = await promisify(question.getFile)(
+      filename,
+      variant,
+      res.locals.question,
+      res.locals.course,
+      res.locals.authn_user.user_id,
+    );
+    res.attachment(filename);
+    res.send(fileData);
+  }),
+);
 
 module.exports = router;
