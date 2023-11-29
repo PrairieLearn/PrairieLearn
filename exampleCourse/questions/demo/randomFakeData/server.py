041af211ced59334a390b7934a52c9f3bd2e247f
Nathan Walters <nwalters512@gmail.com>
2023-11-28 19:26:13 -0800

    Simplify implementation

--- exampleCourse/questions/demo/randomFakeData/server.py
+++ exampleCourse/questions/demo/randomFakeData/server.py
@@ -6,19 +6,14 @@ from faker import Faker
 
 def generate(data):
     fake = Faker()
-    year = 2000
-
     # Start with `set` to ensure uniqueness
     birthdays = set()
-    names = set()
 
+    year = 2000
     while len(birthdays) < 4:
         birthdays.add(
             fake.date_between(datetime.date(year, 1, 1), datetime.date(year, 12, 31))
         )
-
-    while len(names) < 4:
-        names.add(fake.name())
 
     # Once sorted, the first birthday belongs to the youngest employee
     birthdays = sorted(birthdays)
@@ -28,7 +23,7 @@ def generate(data):
     for alias, birthday in zip(
         ["youngest", "employee1", "employee2", "employee3"], birthdays
     ):
-        name = names.pop()
+        name = fake.unique.name()
         data["params"][alias] = name
         # Store in array to facilitate rendering list of employees in question.html template
         employees.append({"name": name, "birthday": birthday.strftime("%B %d, %Y")})
