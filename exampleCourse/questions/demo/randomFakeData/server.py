cd61dbc4b11179efc129983b87d073c5c32579b6
Nathan Walters <nwalters512@gmail.com>
2023-11-28 19:17:09 -0800

    Ensure uniqueness of random data

--- exampleCourse/questions/demo/randomFakeData/server.py
+++ exampleCourse/questions/demo/randomFakeData/server.py
@@ -6,14 +6,19 @@ from faker import Faker
 
 def generate(data):
     fake = Faker()
+    year = 2000
+
     # Start with `set` to ensure uniqueness
     birthdays = set()
+    names = set()
 
-    year = 2000
     while len(birthdays) < 4:
         birthdays.add(
             fake.date_between(datetime.date(year, 1, 1), datetime.date(year, 12, 31))
         )
+
+    while len(names) < 4:
+        names.add(fake.name())
 
     # Once sorted, the first birthday belongs to the youngest employee
     birthdays = sorted(birthdays)
@@ -23,7 +28,7 @@ def generate(data):
     for alias, birthday in zip(
         ["youngest", "employee1", "employee2", "employee3"], birthdays
     ):
-        name = fake.name()
+        name = names.pop()
         data["params"][alias] = name
         # Store in array to facilitate rendering list of employees in question.html template
         employees.append({"name": name, "birthday": birthday.strftime("%B %d, %Y")})
