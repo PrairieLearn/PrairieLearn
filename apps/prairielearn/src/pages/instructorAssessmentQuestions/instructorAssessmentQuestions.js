afa850a29a617653d7f2514a87960a79ed51d40e
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2023-12-13 17:35:39 -0800

    Modernize instructorAssessmentQuestions page

--- apps/prairielearn/src/pages/instructorAssessmentQuestions/instructorAssessmentQuestions.js
+++ apps/prairielearn/src/pages/instructorAssessmentQuestions/instructorAssessmentQuestions.js
@@ -1,33 +1,40 @@
 // @ts-check
-const ERR = require('async-stacktrace');
+const asyncHandler = require('express-async-handler');
 import * as express from 'express';
-import * as path from 'path';
-const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
 const _ = require('lodash');
 import AnsiUp from 'ansi_up';
+import { z } from 'zod';
 
 import * as sqldb from '@prairielearn/postgres';
+import { AssessmentQuestionSchema } from '../../lib/db-types';
 
 const ansiUp = new AnsiUp();
 const router = express.Router();
 const sql = sqldb.loadSqlEquiv(__filename);
 
-router.get('/', function (req, res, next) {
-  debug('GET /');
-  const params = {
-    assessment_id: res.locals.assessment.id,
-    course_id: res.locals.course.id,
-  };
-  sqldb.query(sql.questions, params, function (err, result) {
-    if (ERR(err, next)) return;
-    res.locals.questions = _.map(result.rows, (row) => {
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const questions = await sqldb.queryRows(
+      sql.questions,
+      {
+        assessment_id: res.locals.assessment.id,
+        course_id: res.locals.course.id,
+      },
+      AssessmentQuestionSchema.extend({
+        number: z.string().nullable(),
+        sync_errors: z.string().nullable(),
+        sync_errors_ansified: z.string().optional(),
+        sync_warnings: z.string().nullable(),
+        sync_warnings_ansified: z.string().optional(),
+      }),
+    );
+    res.locals.questions = questions.map((row) => {
       if (row.sync_errors) row.sync_errors_ansified = ansiUp.ansi_to_html(row.sync_errors);
       if (row.sync_warnings) row.sync_warnings_ansified = ansiUp.ansi_to_html(row.sync_warnings);
-      return row;
+      res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
     });
-    debug('render page');
-    res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
-  });
-});
+  }),
+);
 
 export default router;
