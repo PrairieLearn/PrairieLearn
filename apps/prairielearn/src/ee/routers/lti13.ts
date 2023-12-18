b78fba3568fdfcd5c2ec25a24b05e2910a190453
Dave Mussulman <mussulma@gmail.com>
2023-12-18 11:50:49 -0600

    Bug fix for feature checking by institution

--- apps/prairielearn/src/ee/routers/lti13.ts
+++ apps/prairielearn/src/ee/routers/lti13.ts
@@ -5,15 +5,17 @@ import lti13CourseNavigation from '../pages/lti13CourseNavigation/lti13CourseNav
 import { features } from '../../lib/features';
 import middlewareAuthn = require('../../middlewares/authn');
 import csrfToken = require('../../middlewares/csrfToken');
+import { selectLti13Instance } from '../models/lti13Instance';
 
 const router = Router({ mergeParams: true });
 
 router.use(
   '/:lti13_instance_id/',
   asyncHandler(async (req, res, next) => {
+    const lti13_instance = await selectLti13Instance(req.params.lti13_instance_id);
     if (
       await features.enabled('lti13', {
-        institution_id: req.params.institution_id,
+        institution_id: lti13_instance.institution_id,
       })
     ) {
       next();
