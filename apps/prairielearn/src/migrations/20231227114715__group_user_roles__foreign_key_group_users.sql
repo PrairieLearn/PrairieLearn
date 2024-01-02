59c2b8bf9299462f7962e0713fd6a31ec505bcb1
Jonatan Schroeder <jonatan@yorku.ca>
2024-01-02 17:39:19 -0500

    Explicit ON UPDATE CASCADE

--- apps/prairielearn/src/migrations/20231227114715__group_user_roles__foreign_key_group_users.sql
+++ apps/prairielearn/src/migrations/20231227114715__group_user_roles__foreign_key_group_users.sql
@@ -11,4 +11,4 @@ WHERE
   );
 
 ALTER TABLE group_user_roles
-ADD FOREIGN KEY (group_id, user_id) REFERENCES group_users (group_id, user_id) ON DELETE CASCADE;
+ADD FOREIGN KEY (group_id, user_id) REFERENCES group_users (group_id, user_id) ON DELETE CASCADE ON UPDATE CASCADE;
