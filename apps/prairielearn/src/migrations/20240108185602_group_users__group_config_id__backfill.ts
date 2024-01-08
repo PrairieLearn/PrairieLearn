eead88795b3806a9e1c235e5ea3ddd489f085205
Nathan Walters <nwalters512@gmail.com>
2024-01-08 12:59:41 -0600

    Fix reference to old migration name

--- apps/prairielearn/src/migrations/20240108185602_group_users__group_config_id__backfill.ts
+++ apps/prairielearn/src/migrations/20240108185602_group_users__group_config_id__backfill.ts
@@ -1,5 +1,5 @@
 import { enqueueBatchedMigration } from '@prairielearn/migrations';
 
 export default async function () {
-  await enqueueBatchedMigration('20231129193713_group_users__group_config_id__backfill');
+  await enqueueBatchedMigration('20240108185602_group_users__group_config_id__backfill');
 }
