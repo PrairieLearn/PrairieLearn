7d28f95d36edf374833b380763a2267b9c0138d2
Nathan Walters <nwalters512@gmail.com>
2024-01-10 17:03:59 -0800

    Use correct function to finalize

--- apps/prairielearn/src/migrations/20240111004522_group_users__group_config_id__backfill__finalize.ts
+++ apps/prairielearn/src/migrations/20240111004522_group_users__group_config_id__backfill__finalize.ts
@@ -1,5 +1,5 @@
-import { enqueueBatchedMigration } from '@prairielearn/migrations';
+import { finalizeBatchedMigration } from '@prairielearn/migrations';
 
 export default async function () {
-  await enqueueBatchedMigration('20240108185602_group_users__group_config_id__backfill');
+  await finalizeBatchedMigration('20240108185602_group_users__group_config_id__backfill');
 }
