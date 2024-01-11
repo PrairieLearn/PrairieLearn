1b9e3372647cfc4320572105f8a62119ee5f0e0b
Nathan Walters <nwalters512@gmail.com>
2024-01-10 17:10:57 -0800

    Reformat migration

--- apps/prairielearn/src/migrations/20240111010531_groups__group_config_id__not_null.sql
+++ apps/prairielearn/src/migrations/20240111010531_groups__group_config_id__not_null.sql
@@ -2,13 +2,15 @@
 -- `group_config_id` was NULL. This is invalid, since `groups` rows aren't
 -- reachable via our joins with such a value defined. So that we can mark
 -- this column as NOT NULL, we'll delete those rows.
-DELETE FROM groups WHERE group_config_id IS NULL;
+DELETE FROM groups
+WHERE
+  group_config_id IS NULL;
 
 -- Declare `groups.group_config_id` as NOT NULL, since it is now backfilled.
 -- Use the approach described here to avoid a long table lock:
 -- https://dba.stackexchange.com/questions/267947/how-can-i-set-a-column-to-not-null-without-locking-the-table-during-a-table-scan/268128#268128
-
-ALTER TABLE groups ADD CONSTRAINT groups_group_config_id_not_null CHECK (group_config_id IS NOT NULL) NOT VALID;
+ALTER TABLE groups
+ADD CONSTRAINT groups_group_config_id_not_null CHECK (group_config_id IS NOT NULL) NOT VALID;
 
 ALTER TABLE groups VALIDATE CONSTRAINT groups_group_config_id_not_null;
 
@@ -26,4 +28,6 @@ BEGIN
 END;
 $$;
 
-ALTER TABLE groups ALTER COLUMN group_config_id SET NOT NULL;
+ALTER TABLE groups
+ALTER COLUMN group_config_id
+SET NOT NULL;
