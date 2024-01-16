24bd04c3f08a18b9868fbe7f708af5f3dab276d6
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-01-15 16:04:42 -0800

    remove comment 'browser detection'

--- apps/prairielearn/src/server.js
+++ apps/prairielearn/src/server.js
@@ -180,7 +180,6 @@ module.exports.initExpress = function () {
     next();
   });
 
-  // browser detection - data format is https://github.com/bowser-js/bowser/blob/master/docs/global.html#L2063
   app.use(function (req, res, next) {
     if (req.headers['user-agent']) {
       res.locals.userAgent = Bowser.parse(req.headers['user-agent']);
