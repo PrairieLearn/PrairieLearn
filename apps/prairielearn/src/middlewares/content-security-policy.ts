f598b356db4a876345631bed081613b4190ce6e7
Nathan Walters <nwalters512@gmail.com>
2024-01-17 14:16:58 -0800

    Add link to CSP docs

--- apps/prairielearn/src/middlewares/content-security-policy.ts
+++ apps/prairielearn/src/middlewares/content-security-policy.ts
@@ -3,7 +3,10 @@ import { Router } from 'express';
 const router = Router();
 
 router.all('/*', function (req, res, next) {
-  // Prevent PrairieLearn from being rendered in an iframe.
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  //
+  // Currently, we only use CSP to prevent PrairieLearn from being rendered in
+  // an iframe.
   res.header('Content-Security-Policy', "frame-ancestors: 'none';");
 
   next();
