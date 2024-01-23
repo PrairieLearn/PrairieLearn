695e7fbd6fd45d4ea443cd4fea94a17e68d39198
Nathan Walters <nwalters512@gmail.com>
2024-01-23 14:15:00 -0800

    Reformat

--- apps/prairielearn/src/migrations/20240123213531_group_user_roles__cascade_deletes.sql
+++ apps/prairielearn/src/migrations/20240123213531_group_user_roles__cascade_deletes.sql
@@ -1,5 +1,4 @@
 -- Add new constraints that include cascading updates and deletes
-
 ALTER TABLE group_user_roles
 ADD CONSTRAINT group_user_roles_group_id_new_fkey FOREIGN KEY (group_id) REFERENCES groups (id) ON UPDATE CASCADE ON DELETE CASCADE;
 
@@ -10,7 +9,6 @@ ALTER TABLE group_user_roles
 ADD CONSTRAINT group_user_roles_user_id_new_fkey FOREIGN KEY (user_id) REFERENCES users (user_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- Drop the old constraints.
-
 ALTER TABLE group_user_roles
 DROP CONSTRAINT IF EXISTS group_user_roles_group_id_fkey;
 
@@ -21,7 +19,6 @@ ALTER TABLE group_user_roles
 DROP CONSTRAINT IF EXISTS group_user_roles_user_id_fkey;
 
 -- Rename the new constraints to the old names.
-
 ALTER TABLE group_user_roles
 RENAME CONSTRAINT group_user_roles_group_id_new_fkey TO group_user_roles_group_id_fkey;
 
