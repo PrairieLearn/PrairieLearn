29c59559baee06c19b38ed3b968b5c13ef0176cc
Nathan Walters <nwalters512@gmail.com>
2024-02-01 12:01:39 -0800

    Get token and read manifest

--- packages/actions-report-image-sizes/index.mjs
+++ packages/actions-report-image-sizes/index.mjs
@@ -6,9 +6,41 @@ function getImages() {
   return images.split(',');
 }
 
+async function getDockerHubToken(image) {
+  const tokenResponse = await fetch(
+    `https://auth.docker.io/token?service=registry.docker.io&scope=repository:${image}:pull`,
+  );
+  const { token } = await tokenResponse.json();
+  return token;
+}
+
+async function getImageManifest(token, image, version) {
+  const manifestResponse = await fetch(
+    `https://registry-1.docker.io/v2/${image}/manifests/${version}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  return await manifestResponse.json();
+}
+
+async function getImageSizes(image) {
+  const token = await getDockerHubToken(image);
+  console.log(token);
+  const manifest = await getImageManifest(token, image, 'latest');
+  console.log(manifest);
+  return 12345;
+}
+
 try {
   const images = getImages();
   const title = core.getInput('title');
+
+  for (const image of images) {
+    console.log(await getImageSizes(image));
+  }
 
   console.log('Hello, world!', images, title);
 } catch (e) {
