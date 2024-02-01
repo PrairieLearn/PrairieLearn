c9a734f2aee568293a4e73b5b273e4a0f628d3f0
Nathan Walters <nwalters512@gmail.com>
2024-02-01 11:42:23 -0800

    Trying again

--- packages/actions-report-image-sizes/index.mjs
+++ packages/actions-report-image-sizes/index.mjs
@@ -1,19 +1,20 @@
 import './setup.mjs';
 
-import core from '@actions/core';
-import github from '@actions/github';
+// import core from '@actions/core';
+// import github from '@actions/github';
 
 function getImages() {
-  const images = core.getInput('images');
+  // const images = core.getInput('images');
+  const images = '';
   return images.split(',');
 }
 
 try {
   const images = getImages();
-  const title = core.getInput('title');
+  // const title = core.getInput('title');
 
   console.log('Hello, world!', images, title);
 } catch (e) {
   console.error(e);
-  core.setFailed(e.message);
+  // core.setFailed(e.message);
 }
