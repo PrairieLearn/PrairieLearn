dae1f6439ec53edc9d34755320929d655c2f5ff7
Jonatan Schroeder <jonatan@yorku.ca>
2024-02-04 17:29:04 -0500

    Use modern query function

--- apps/prairielearn/src/cron/calculateAssessmentQuestionStats.js
+++ apps/prairielearn/src/cron/calculateAssessmentQuestionStats.js
@@ -1,18 +1,19 @@
+// @ts-check
 import { callbackify } from 'util';
 
 import { logger } from '@prairielearn/logger';
-import { loadSqlEquiv, queryAsync } from '@prairielearn/postgres';
+import { loadSqlEquiv, queryRows } from '@prairielearn/postgres';
 
 import { updateAssessmentQuestionStatsForAssessment } from '../lib/assessment';
+import { IdSchema } from '../lib/db-types';
 
 const sql = loadSqlEquiv(__filename);
 
 export async function runAsync() {
-  const result = await queryAsync(sql.select_assessments, {});
-  const assessments = result.rows;
-  for (const assessment of assessments) {
-    logger.verbose(`calculateAssessmentQuestionStats: processing assessment_id = ${assessment.id}`);
-    await updateAssessmentQuestionStatsForAssessment(assessment.id);
+  const assessment_ids = await queryRows(sql.select_assessments, {}, IdSchema);
+  for (const assessment_id of assessment_ids) {
+    logger.verbose(`calculateAssessmentQuestionStats: processing assessment_id = ${assessment_id}`);
+    await updateAssessmentQuestionStatsForAssessment(assessment_id);
   }
 }
 export const run = callbackify(runAsync);
