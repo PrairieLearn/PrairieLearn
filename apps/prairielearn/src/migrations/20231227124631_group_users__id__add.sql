09e91362e8ecfd5083ca31f61a9873a7eb444c9b
Jonatan Schroeder <jonatan@yorku.ca>
2024-02-06 19:34:28 -0500

    Remove premature update of group_user_id in group_user_roles

--- apps/prairielearn/src/migrations/20231227124631_group_users__id__add.sql
+++ apps/prairielearn/src/migrations/20231227124631_group_users__id__add.sql
@@ -9,17 +9,3 @@ DROP INDEX group_users_group_id_key;
 
 ALTER TABLE group_user_roles
 ADD COLUMN group_user_id BIGINT REFERENCES group_users (id) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- This update will need to be repeated in a future PR.
-UPDATE group_user_roles gur
-SET
-  group_user_id = gu.id
-FROM
-  group_users gu
-WHERE
-  gu.group_id = gur.group_id
-  AND gu.user_id = gur.user_id;
-
-DELETE FROM group_user_roles
-WHERE
-  group_user_id IS NULL;
