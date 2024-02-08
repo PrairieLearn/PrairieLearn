5bdf6a9db49ca52cf387b03141e18f46859df8c7
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2024-02-08 14:57:27 -0800

    Convert course settings page to async/await (#9395)
    * Update callback functions to async/await
    
    * Remove unused debug import
    
    * Move to pathExists instead of fs.access
    
    * Remove console.log

--- apps/prairielearn/src/pages/instructorCourseAdminSettings/instructorCourseAdminSettings.js
+++ apps/prairielearn/src/pages/instructorCourseAdminSettings/instructorCourseAdminSettings.js
@@ -1,9 +1,8 @@
 //@ts-check
 import * as express from 'express';
 import * as path from 'path';
-const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
+const asyncHandler = require('express-async-handler');
 import * as fs from 'fs-extra';
-import * as async from 'async';
 const ERR = require('async-stacktrace');
 import { CourseInfoEditor } from '../../lib/editors';
 import { logger } from '@prairielearn/logger';
@@ -11,43 +10,22 @@ import * as error from '@prairielearn/error';
 
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-  debug('GET /');
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const pathExists = await fs.pathExists(res.locals.course.path);
+    if (!pathExists) {
+      res.locals.needToSync = true;
+    } else {
+      const jsonExists = await fs.pathExists(path.join(res.locals.course.path, 'infoCourse.json'));
+      if (!jsonExists) {
+        res.locals.noInfo = true;
+      }
+    }
 
-  async.series(
-    [
-      (callback) => {
-        fs.access(res.locals.course.path, (err) => {
-          if (err) {
-            if (err.code === 'ENOENT') {
-              res.locals.needToSync = true;
-            } else {
-              return ERR(err, callback);
-            }
-          }
-          callback(null);
-        });
-      },
-      (callback) => {
-        if (res.locals.needToSync) return callback(null);
-        fs.access(path.join(res.locals.course.path, 'infoCourse.json'), (err) => {
-          if (err) {
-            if (err.code === 'ENOENT') {
-              res.locals.noInfo = true;
-            } else {
-              return ERR(err, callback);
-            }
-          }
-          callback(null);
-        });
-      },
-    ],
-    (err) => {
-      if (ERR(err, next)) return;
-      res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
-    },
-  );
-});
+    res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+  }),
+);
 
 router.post('/', (req, res, next) => {
   if (!res.locals.authz_data.has_course_permission_edit || res.locals.course.example_course) {
@@ -56,9 +34,7 @@ router.post('/', (req, res, next) => {
     );
   }
 
-  debug(`Responding to post with action ${req.body.__action}`);
   if (req.body.__action === 'add_configuration') {
-    debug(`Responding to action add_configuration`);
     const editor = new CourseInfoEditor({
       locals: res.locals,
     });
