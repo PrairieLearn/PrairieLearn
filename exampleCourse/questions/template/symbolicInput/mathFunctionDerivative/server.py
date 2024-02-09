05baf5016d884974a921a67e0ce9b4c2cf69faef
Mariana Silva <mfsilva@illinois.edu>
2024-02-09 13:52:54 -0600

    removed duplicated question

--- exampleCourse/questions/template/symbolicInput/mathFunctionDerivative/server.py
+++ exampleCourse/questions/template/symbolicInput/mathFunctionDerivative/server.py
@@ -25,6 +25,3 @@ def generate(data):
     data["params"]["f"] = sympy.latex(f)
     data["correct_answers"]["df"] = str(df)
 
-
-if __name__ == "__main__":
-    generate({"params": {}, "correct_answers": {}})
