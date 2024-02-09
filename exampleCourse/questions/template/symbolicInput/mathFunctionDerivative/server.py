154d916177c6e4e4f5e445d5d4d5e778f5129745
Nathan Walters <nwalters512@gmail.com>
2024-02-09 15:16:06 -0800

    Clean up symbolic input question

--- exampleCourse/questions/template/symbolicInput/mathFunctionDerivative/server.py
+++ exampleCourse/questions/template/symbolicInput/mathFunctionDerivative/server.py
@@ -3,25 +3,24 @@ import sympy
 
 
 def generate(data):
-    # Create a variable
+    # Create a variable.
     x = sympy.symbols("x")
 
-    # Randomize the degree
+    # Randomize the degree.
     degree = numpy.random.random_integers(1, 5)
 
-    # Randomize the coefficients (make sure the leading coefficient is non-zero)
+    # Randomize the coefficients; make sure the leading coefficient is non-zero.
     coeffs = numpy.random.random_integers(-9, 9, degree + 1)
     if coeffs[0] == 0:
         coeffs[0] = 1
 
-    # Create the polynomial
+    # Create the polynomial.
     f = sympy.Poly(coeffs, x).as_expr()
 
-    # Find derivative with respect to x
+    # Find derivative with respect to x.
     df = sympy.diff(f, x)
 
-    # Modify data and return
+    # Store the parameters and correct answer.
     data["params"]["x"] = sympy.latex(x)
     data["params"]["f"] = sympy.latex(f)
     data["correct_answers"]["df"] = str(df)
-
