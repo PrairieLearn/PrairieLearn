954465907a7031bbae53b8346fd964991c161761
Nathan Walters <nwalters512@gmail.com>
2024-02-09 15:27:26 -0800

    Tweak matrix component input question

--- exampleCourse/questions/template/matrixComponentInput/graphAdjacencyMatrix/server.py
+++ exampleCourse/questions/template/matrixComponentInput/graphAdjacencyMatrix/server.py
@@ -4,15 +4,15 @@ import prairielearn as pl
 
 def generate(data):
     n = 4
-    choice = np.random.choice(np.arange(2, dtype="int"), p=[0.7, 0.3])
-    if choice == 0:
-        # random graph
+
+    # Randomly decide to generate a random or ring matrix.
+    choice = np.random.choice(["random", "ring"], p=[0.7, 0.3])
+
+    if choice == "random":
         correct = np.random.choice([0, 1], size=(n, n), p=[0.5, 0.5])
-    elif choice == 1:
-        # ring
+    elif choice == "ring":
         shift = np.random.choice([1, -1]) * np.random.randint(1, n - 2)
         correct = np.roll(np.diag(np.ones(n)), shift)
 
-    data["correct_answers"]["mat"] = pl.to_json(correct.T)
-    data["params"]["mat"] = data["correct_answers"]["mat"]
-    return data
+    data["params"]["matrix"] = pl.to_json(correct.T)
+    data["correct_answers"]["matrix"] = data["params"]["matrix"]
