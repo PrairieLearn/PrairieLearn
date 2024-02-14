4dd1daea7f84a3a3dc9bb1267c1d682f017269f8
Jonatan Schroeder <jonatan@yorku.ca>
2024-02-14 13:19:52 -0500

    Modernize performance sync library (#9422)
    * Convert sync performance util to TypeScript
    
    * Add minimal types to allow typecheck to pass
    
    * Modernize performance sync library
    
    * Simplify implementation of timed function
    
    * Renamed timed function (remove prefix)

--- apps/prairielearn/src/sync/performance.ts
+++ apps/prairielearn/src/sync/performance.ts
@@ -1,69 +1,36 @@
-const scopedData = {};
+const scopedData: Record<string, Record<string, number>> = {};
 
-/**
- * @param {string} scopeName
- */
-export function makePerformance(scopeName) {
+interface Performance {
+  start: (name: string) => void;
+  end: (name: string) => void;
+  timed: <T>(name: string, asyncFunc: () => Promise<T>) => Promise<T>;
+}
+
+export function makePerformance(scopeName: string): Performance {
   if (!scopedData[scopeName]) {
     scopedData[scopeName] = {};
   }
 
   const scope = scopedData[scopeName];
 
-  /**
-   * @param {string} name
-   */
   function start(name: string): void {
-    scope[name] = new Date();
+    scope[name] = Date.now();
   }
 
-  /**
-   * @param {string} name
-   */
   function end(name: string): void {
-    if (!(name in scope)) {
-      return;
-    }
-    if (process.env.PROFILE_SYNC) {
+    if (name in scope && process.env.PROFILE_SYNC) {
       console.log(`${name} took ${Date.now() - scope[name]}ms`);
     }
   }
 
-  /**
-   * @deprecated
-   * @param {string} name
-   * @param {(callback: (err: Error | null) => void) => void} func
-   * @param {*} callback
-   */
-  function timedFunc(name, func, callback) {
+  async function timed<T>(name: string, asyncFunc: () => Promise<T>): Promise<T> {
     start(name);
-    func((err) => {
-      end(name);
-      callback(err);
-    });
-  }
-
-  /**
-   * @template T
-   * @param {string} name
-   * @param {() => Promise<T>} asyncFunc
-   * @returns {Promise<T>}
-   */
-  async function timedAsync<T>(name: string, asyncFunc: () => Promise<T>): Promise<T> {
-    start(name);
-    let res;
     try {
-      res = await asyncFunc();
+      return await asyncFunc();
     } finally {
       end(name);
     }
-    return res;
   }
 
-  return {
-    start,
-    end,
-    timedFunc,
-    timedAsync,
-  };
+  return { start, end, timed };
 }
