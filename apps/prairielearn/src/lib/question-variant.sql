d6ee667fa6de66c06b5df61c94bb832ca4046d01
Jonatan Schroeder <jonatan@yorku.ca>
2024-02-20 17:12:18 -0500

    Convert submission insertion and grading selection sprocs to lib (#9434)
    * Convert submission selection sproc to lib function
    
    * Convert submissions_insert sproc to lib
    
    * Create new variable for extended schema
    
    * Fix call to variants_ensure_open
    
    * Replace variants_ensure_open with simple check
    
    * Convert sproc call with simple check
    
    * Fix typos in queries
    
    * Fix check for manual-only grading
    
    * Convert variants_insert to lib
    
    * Avoid duplication of variant selection
    
    * Remove unnecessary parameter

---
+++ apps/prairielearn/src/lib/question-variant.sql
@@ -0,0 +1,76 @@
+-- BLOCK select_instance_question_data
+SELECT
+  aq.question_id,
+  ai.group_id,
+  ai.user_id,
+  iq.assessment_instance_id,
+  a.course_instance_id,
+  iq.open AS instance_question_open,
+  ai.open AS assessment_instance_open
+FROM
+  instance_questions AS iq
+  JOIN assessment_questions AS aq ON (aq.id = iq.assessment_question_id)
+  JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
+  JOIN assessments AS a ON (a.id = ai.assessment_id)
+WHERE
+  iq.id = $instance_question_id;
+
+-- BLOCK next_variant_number
+SELECT
+  max(v.number) + 1
+FROM
+  variants AS v
+WHERE
+  v.instance_question_id = $instance_question_id;
+
+-- BLOCK insert_workspace
+INSERT INTO
+  workspaces DEFAULT
+VALUES
+RETURNING
+  id;
+
+-- BLOCK insert_variant
+INSERT INTO
+  variants (
+    instance_question_id,
+    question_id,
+    course_instance_id,
+    user_id,
+    group_id,
+    number,
+    variant_seed,
+    params,
+    true_answer,
+    options,
+    broken,
+    broken_at,
+    authn_user_id,
+    workspace_id,
+    course_id,
+    client_fingerprint_id
+  )
+VALUES
+  (
+    $instance_question_id,
+    $question_id,
+    $course_instance_id,
+    $user_id,
+    $group_id,
+    $number,
+    $variant_seed,
+    $params,
+    $true_answer,
+    $options,
+    $broken,
+    CASE
+      WHEN $broken THEN NOW()
+      ELSE NULL
+    END,
+    $authn_user_id,
+    $workspace_id,
+    $course_id,
+    $client_fingerprint_id
+  )
+RETURNING
+  id;
