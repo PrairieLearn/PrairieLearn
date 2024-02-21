3e5b6b479b855d078d33d58fa15e61fdbe7eda1c
elisacarrillo <elisamc2@illinois.edu>
2024-02-21 14:40:02 -0600

    moved unique tag in migration

--- apps/prairielearn/src/migrations/20230531184500_assessment_access_policies.sql
+++ apps/prairielearn/src/migrations/20230531184500_assessment_access_policies.sql
@@ -6,13 +6,13 @@ CREATE TABLE IF NOT EXISTS assessment_access_policies (
   assessment_id BIGINT NOT NULL REFERENCES assessments (id) ON UPDATE CASCADE ON DELETE CASCADE,
   user_id BIGINT REFERENCES users (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
   group_id BIGINT REFERENCES groups (id) ON UPDATE CASCADE ON DELETE CASCADE,
-  UNIQUE (assessment_id, user_id),
-  UNIQUE (assessment_id, group_id),
   start_date TIMESTAMP WITH TIME ZONE NOT NULL,
   end_date TIMESTAMP WITH TIME ZONE NOT NULL,
   credit INTEGER NOT NULL,
   note TEXT,
   created_by BIGINT NOT NULL REFERENCES users (user_id),
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  UNIQUE (assessment_id, user_id),
+  UNIQUE (assessment_id, group_id),
   CHECK (num_nonnulls (user_id, group_id) = 1)
 );
