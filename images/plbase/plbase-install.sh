677d3b4df6c2f9a4cb8d0982b3cdd1e5ee5f91c9
Nathan Walters <nwalters512@gmail.com>
2024-02-23 14:31:47 -0800

    Test removing R from base image

--- images/plbase/plbase-install.sh
+++ images/plbase/plbase-install.sh
@@ -51,19 +51,10 @@ arch=`uname -m`
 curl -LO https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-${arch}.sh
 bash Miniforge3-Linux-${arch}.sh -b -p /usr/local -f
 
-# If R package installation is specifically disabled, we'll avoid installing anything R-related.
-if [[ "${SKIP_R_PACKAGES}" != "yes" ]]; then
-    echo "installing R..."
-    conda install --channel r r-base r-essentials
-
-    echo "installing Python packages..."
-    python3 -m pip install --no-cache-dir -r /python-requirements.txt
-else
-    echo "R package installation is disabled"
-    sed '/rpy2/d' /python-requirements.txt > /py_req_no_r.txt # Remove rpy2 package.
-    echo "installing Python packages..."
-    python3 -m pip install --no-cache-dir -r /py_req_no_r.txt
-fi
+echo "R package installation is disabled"
+sed '/rpy2/d' /python-requirements.txt > /py_req_no_r.txt # Remove rpy2 package.
+echo "installing Python packages..."
+python3 -m pip install --no-cache-dir -r /py_req_no_r.txt
 
 # `pyarrow` and `rpy2` conflict in a horrible way:
 #
