4f7d01daafb4d125aa260fc70d4f803e4c99ad0b
Nathan Walters <nwalters512@gmail.com>
2024-02-26 11:20:42 -0800

    Improve multiple choice random prompt question

--- exampleCourse/questions/template/multiple-choice/random-prompt-true-false/server.py
+++ exampleCourse/questions/template/multiple-choice/random-prompt-true-false/server.py
@@ -4,30 +4,16 @@ import random
 def generate(data):
     # Create a list of question prompts and the corresponding answers.
     scenarios = [
-        {
-            "question": "North",
-            "answer": True,
-        },
-        {
-            "question": "South",
-            "answer": False,
-        },
-        {
-            "question": "West",
-            "answer": False,
-        },
-        {
-            "question": "East",
-            "answer": False,
-        },
+        ("North", True),
+        ("South", False),
+        ("West", False),
+        ("East", False),
     ]
 
-    # Randomize the order of the scenarios.
-    random.shuffle(scenarios)
+    # Select a random scenario.
+    scenario = random.choice(scenarios)
 
-    # First shuffled scenario is the one we will take as correct.
-    data["params"]["question_prompt"] = scenarios[0]["question"]
-
-    # Depending on the truth statement, set the appropriate answer.
-    data["params"]["true_answer"] = scenarios[0]["answer"]
-    data["params"]["false_answer"] = not scenarios[0]["answer"]
+    # Depending on the truth statement, set the appropriate prompt and answers.
+    data["params"]["question_prompt"] = scenario[0]
+    data["params"]["true_answer"] = scenario[1]
+    data["params"]["false_answer"] = not scenario[1]
