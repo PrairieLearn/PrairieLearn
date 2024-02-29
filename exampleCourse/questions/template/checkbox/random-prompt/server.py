654994af88d1f247afc574e5ae8568ff7366ee5b
Eliot Robson <eliot.robson24@gmail.com>
2024-02-29 13:48:20 -0600

    Improved checkbox template question generation

--- exampleCourse/questions/template/checkbox/random-prompt/server.py
+++ exampleCourse/questions/template/checkbox/random-prompt/server.py
@@ -1,25 +1,40 @@
+import math
 import random
 
+import numpy as np
 
-def is_prime(a):
-    if a == 1:
-        return False
-    return all(a % i for i in range(2, a))
+
+def primes_from_2_to(n):
+    """
+    Return a set of primes p, 2 <= p < n.
+    From https://codereview.stackexchange.com/a/42439
+    """
+    assert n >= 6  # Only works for this case
+
+    prime = np.ones(n // 3 + (n % 6 == 2), dtype=bool)
+    for i in range(3, math.isqrt(n) + 1, 3):
+        if prime[i // 3]:
+            p = (i + 1) | 1
+            prime[p * p // 3 :: 2 * p] = False
+            prime[p * (p - 2 * (i & 1) + 4) // 3 :: 2 * p] = False
+    result = (3 * prime.nonzero()[0] + 1) | 1
+    result[0] = 3
+    return set(np.r_[2, result])
 
 
 def generate(data):
+    size_limit = 20
     concept = random.choice(["prime", "even", "odd"])
     data["params"]["concept"] = concept
 
-    is_odd_correct = str(concept == "odd").lower()
-    is_even_correct = str(concept == "even").lower()
+    if concept == "prime":
+        correct_set = primes_from_2_to(size_limit)
+    elif concept == "even":
+        correct_set = {num for num in range(1, size_limit) if num % 2 == 0}
+    elif concept == "odd":
+        correct_set = {num for num in range(1, size_limit) if num % 2 != 0}
 
-    options = []
-    for num in range(1, 20):
-        if concept == "prime":
-            options.append({"correct": str(is_prime(num)).lower(), "answer": str(num)})
-        else:
-            correct = is_even_correct if num % 2 == 0 else is_odd_correct
-            options.append({"correct": correct, "answer": str(num)})
-
-    data["params"]["options"] = options
+    data["params"]["options"] = [
+        {"correct": num in correct_set, "answer": str(num)}
+        for num in range(1, size_limit)
+    ]
