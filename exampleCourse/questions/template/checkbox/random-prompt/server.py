cae513934e1a9a3d6e78d9c6ef04c4ae4f25109b
Nathan Walters <nwalters512@gmail.com>
2024-03-05 14:09:32 -0800

    Hardcode list of primes

--- exampleCourse/questions/template/checkbox/random-prompt/server.py
+++ exampleCourse/questions/template/checkbox/random-prompt/server.py
@@ -1,25 +1,4 @@
-import math
 import random
-
-import numpy as np
-
-
-def primes_from_2_to(n):
-    """
-    Return a set of primes p, 2 <= p < n.
-    From https://codereview.stackexchange.com/a/42439
-    """
-    assert n >= 6  # Only works for this case
-
-    prime = np.ones(n // 3 + (n % 6 == 2), dtype=bool)
-    for i in range(3, math.isqrt(n) + 1, 3):
-        if prime[i // 3]:
-            p = (i + 1) | 1
-            prime[p * p // 3 :: 2 * p] = False
-            prime[p * (p - 2 * (i & 1) + 4) // 3 :: 2 * p] = False
-    result = (3 * prime.nonzero()[0] + 1) | 1
-    result[0] = 3
-    return set(np.r_[2, result])
 
 
 def generate(data):
@@ -28,7 +7,9 @@ def generate(data):
     data["params"]["concept"] = concept
 
     if concept == "prime":
-        correct_set = primes_from_2_to(size_limit)
+        # For the sake of this example, we use a hardcoded list of all primes
+        # up to 20. Update this list if you change `size_limit`.
+        correct_set = {2, 3, 5, 7, 11, 13, 17, 19}
     elif concept == "even":
         correct_set = {num for num in range(1, size_limit) if num % 2 == 0}
     elif concept == "odd":
