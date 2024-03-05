33f1580f59b7b1153e7d4980e4db947b5d7cbf99
Nathan Walters <nwalters512@gmail.com>
2024-03-05 15:29:41 -0800

    Use pluralization for pl-string-input template

--- exampleCourse/questions/template/string-input/random/server.py
+++ exampleCourse/questions/template/string-input/random/server.py
@@ -2,7 +2,23 @@ import random
 
 
 def generate(data):
-    a = random.randint(100, 200)
-    ans = "{0:b}".format(a)
-    data["params"]["a"] = a
-    data["correct_answers"]["b"] = ans
+    # Define a list of irregular singular words and their plural forms.
+    words = {
+        "mouse": "mice",
+        "goose": "geese",
+        "child": "children",
+        "tooth": "teeth",
+        "woman": "women",
+        "man": "men",
+        "foot": "feet",
+        "person": "people",
+        "leaf": "leaves",
+        "cactus": "cacti",
+    }
+
+    # Select a random singular word from the list.
+    singular_word = random.choice(list(words.keys()))
+
+    # Store the singular word and its corresponding plural form.
+    data["params"]["singular_word"] = singular_word
+    data["correct_answers"]["plural_word"] = words[singular_word]
