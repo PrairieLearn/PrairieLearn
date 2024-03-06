f958e99d20ce1646b7fef0c081e5afe180885c97
Nathan Walters <nwalters512@gmail.com>
2024-03-06 09:04:41 -0800

    Show IDs for institutions and course instances too

--- apps/prairielearn/src/pages/administratorFeatures/administratorFeatures.html.ts
+++ apps/prairielearn/src/pages/administratorFeatures/administratorFeatures.html.ts
@@ -281,7 +281,7 @@ export function AddFeatureGrantModalBody({
                 value="${institution.id}"
                 ${institution.id === institution_id ? 'selected' : ''}
               >
-                ${institution.long_name} (${institution.short_name})
+                ${institution.long_name}: ${institution.short_name} (${institution.id})
               </option>
             `;
           })}
@@ -328,7 +328,7 @@ export function AddFeatureGrantModalBody({
                 value="${course_instance.id}"
                 ${course_instance.id === course_instance_id ? 'selected' : ''}
               >
-                ${course_instance.long_name} (${course_instance.short_name})
+                ${course_instance.long_name}: ${course_instance.short_name} (${course_instance.id})
               </option>
             `;
           })}
