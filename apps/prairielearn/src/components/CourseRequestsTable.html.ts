aca324917981ce38b8e55ed649cdb4da379fb459
Nathan Walters <nwalters512@gmail.com>
2024-03-07 14:23:18 -0800

    Fix colspan

--- apps/prairielearn/src/components/CourseRequestsTable.html.ts
+++ apps/prairielearn/src/components/CourseRequestsTable.html.ts
@@ -149,7 +149,7 @@ export function CourseRequestsTable({
                 ${row.jobs.length > 0
                   ? html`
                       <tr>
-                        <td colspan="${showAll ? 11 : 10}" class="p-0">
+                        <td colspan="${showAll ? 10 : 9}" class="p-0">
                           <div id="course-requests-job-list-${row.id}" class="collapse">
                             <table class="table table-sm table-active mb-0">
                               <thead>
