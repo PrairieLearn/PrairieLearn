46b2beca3ca428ba6f0d2677ec904a1492ff2600
elisacarrillo <elisamc2@illinois.edu>
2024-03-12 13:59:34 -0400

    abstraction of group_id and user_id getting in add and edit

--- apps/prairielearn/src/pages/instructorAssessmentAccessOverrides/instructorAssessmentAccessOverrides.js
+++ apps/prairielearn/src/pages/instructorAssessmentAccessOverrides/instructorAssessmentAccessOverrides.js
@@ -9,22 +9,46 @@ import { selectUserByUid } from '../../models/user';
 const router = express.Router();
 const sql = sqldb.loadSqlEquiv(__filename);
 
-// async function getUserIdAndCheckEnrollment({ uid, course_instance_id }) {
-//   const user = await selectUserByUid(uid);
-//   if (!user) {
-//     throw error.make(400, `User ${uid} does not exist.`);
-//   }
+async function getUserOrGroupId({course_instance_id, assessment, uid, group_name}) {
+  if (assessment.group_work) { 
+    if (!group_name || uid) {
+      throw error.make(400, 'Group name is required for group work assessments.');
+    }
+    const group_result = await sqldb.queryZeroOrOneRowAsync(sql.select_group_in_assessment, {
+      group_name: group_name,
+      course_instance_id: course_instance_id,
+      assessment_id: assessment.id,
+    });
+    
+    if (group_result.rows.length > 0) {
+      return {user_id: null, group_id: group_result.rows[0].id};
+    } else {
+      throw error.make(400, 'Group not found in this assessment.');
+    }
+  }
+  if (uid) {
+    if (group_name) {
+      throw error.make(400, 'Student User ID is required for individual work assessments.');
+    }
+    const checkEnrollment = await userIsEnrolledInCourseInstance({
+      uid: uid,
+      course_instance_id: course_instance_id,
+    });
+    if (!checkEnrollment) {
+      throw error.make(400, `User ${uid} is not enrolled in this course instance.`);
+    }
+    const user = await selectUserByUid(uid);
+    if (!user) {
+      throw error.make(400, `User ${uid} does not exist.`);
+    }
+    return {user_id: user.user_id, group_id: null};
+  } 
+  else {
+    throw error.make(400, 'Student User ID or Group Name is required.');
+  }
+  
+}
 
-//   const enrollment = await getEnrollmentForUserInCourseInstance({
-//     user_id: user.user_id,
-//     course_instance_id: course_instance_id,
-//   });
-//   if (!enrollment) {
-//     throw error.make(400, `User ${uid} is not enrolled in this course instance.`);
-//   }
-
-//   return user.user_id;
-// }
 
 async function userIsEnrolledInCourseInstance({ uid, course_instance_id }) {
   const user = await selectUserByUid(uid);
@@ -58,21 +82,12 @@ router.post(
       throw error.make(403, 'Access denied (must be a student data editor)');
     }
     if (req.body.__action === 'add_new_override') {
-      let user_id = null;
-      if (req.body.student_uid) {
-        const checkEnrollment = await userIsEnrolledInCourseInstance({
-          uid: req.body.student_uid,
-          course_instance_id: res.locals.course_instance.id,
-        });
-        if (!checkEnrollment) {
-          throw error.make(400, `User ${req.body.student_uid} is not enrolled in this course instance.`);
-        }
-        const user = await selectUserByUid(req.body.student_uid);
-        if (!user) {
-          throw error.make(400, `User ${req.body.student_uid} does not exist.`);
-        }
-        user_id = user.user_id;
-      }
+      const {user_id, group_id} = await getUserOrGroupId({
+        course_instance_id: res.locals.course_instance.id,
+        assessment: res.locals.assessment,
+        uid: req.body.student_uid,
+        group_name: req.body.group_name,
+      });
       const params = {
         assessment_id: res.locals.assessment.id,
         created_by: res.locals.authn_user.user_id,
@@ -81,35 +96,10 @@ router.post(
         group_name: req.body.group_name || null,
         note: req.body.note || null,
         start_date: new Date(req.body.start_date),
-        group_id: null,
+        group_id: group_id || null,
         user_id: user_id || null,
       };
-      // First, validate if group belongs to the assessment
-      if (res.locals.assessment.group_work) {
-        if (!params.group_name || params.user_id) {
-          throw error.make(400, 'Group name is required for group work assessments.');
-        }
-        const group_result = await sqldb.queryZeroOrOneRowAsync(sql.select_group_in_assessment, {
-          group_name: params.group_name,
-          course_instance_id: res.locals.course_instance.id,
-          assessment_id: res.locals.assessment.id,
-        });
-        // Get the group_id from the result
-        if (group_result.rows.length > 0) {
-          params.group_id = group_result.rows[0].id;
-        } else {
-          params.group_id = null;
-        }
-
-        // If group does not belong to assessments and indirectly course instances, return error
-        if (!params.group_id) {
-          throw error.make(400, 'Group not found in this assessment.');
-        }
-      } else {
-        if (!params.user_id || params.group_name) {
-          throw error.make(400, 'Student User ID is required for individual work assessments.');
-        }
-      }
+      
       await sqldb.queryAsync(sql.insert_assessment_access_policy, params);
       res.redirect(req.originalUrl);
     } else if (req.body.__action === 'delete_override') {
@@ -119,13 +109,12 @@ router.post(
       });
       res.redirect(req.originalUrl);
     } else if (req.body.__action === 'edit_override') {
-      let user_id = null;
-      if (req.body.student_uid) {
-        user_id = await getUserIdAndCheckEnrollment({
-          uid: req.body.student_uid,
-          course_instance_id: res.locals.course_instance.id,
-        });
-      }
+      const {user_id, group_id} = await getUserOrGroupId({
+        course_instance_id: res.locals.course_instance.id,
+        assessment: res.locals.assessment,
+        uid: req.body.student_uid,
+        group_name: req.body.group_name,
+      });
 
       const edit_params = {
         assessment_id: res.locals.assessment.id,
@@ -134,37 +123,11 @@ router.post(
         group_name: req.body.group_name || null,
         note: req.body.note || null,
         start_date: new Date(req.body.start_date),
-        group_id: null,
+        group_id: group_id || null,
         user_id: user_id || null,
-        assessment_access_policies_id: req.body.policy_id,
+        assessment_access_policies_id: req.body.policy_id
       };
-
-      // Validate if group belongs to the assessment, otherwise check if student is enrolled in assessment
-      if (res.locals.assessment.group_work) {
-        if (!edit_params.group_name || edit_params.user_id) {
-          throw error.make(400, 'Group name is required for group work assessments.');
-        }
-        const group_result = await sqldb.queryAsync(sql.select_group_in_assessment, {
-          group_name: edit_params.group_name,
-          course_instance_id: res.locals.course_instance.id,
-          assessment_id: res.locals.assessment.id,
-        });
-        // Get the group_id from the result
-        if (group_result.rows.length > 0) {
-          edit_params.group_id = group_result.rows[0].id;
-        } else {
-          edit_params.group_id = null;
-        }
-
-        // If group does not belong to assessments and indirectly course instances, return error
-        if (!edit_params.group_id) {
-          throw error.make(400, 'Group does not belong to the current course instance.');
-        }
-      } else {
-        if (!edit_params.user_id || edit_params.group_name) {
-          throw error.make(400, 'Student UID is required for individual work assessments.');
-        }
-      }
+      
       await sqldb.queryAsync(sql.update_assessment_access_policy, edit_params);
       res.redirect(req.originalUrl);
     }
