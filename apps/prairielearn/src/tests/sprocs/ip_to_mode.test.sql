942e49c9ec7985a0182e35b93c934744255d71d5
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-03-14 15:57:01 -0700

    Clean up setup query

--- apps/prairielearn/src/tests/sprocs/ip_to_mode.test.sql
+++ apps/prairielearn/src/tests/sprocs/ip_to_mode.test.sql
@@ -16,14 +16,6 @@ WITH
     RETURNING
       *
   ),
-  new_locations_2 AS (
-    INSERT INTO
-      pt_locations (filter_networks)
-    VALUES
-      (TRUE)
-    RETURNING
-      *
-  ),
   new_location_networks AS (
     INSERT INTO
       pt_location_networks (location_id, network)
@@ -35,15 +27,6 @@ WITH
     RETURNING
       *
   ),
-  new_location_networks_2 AS (
-    INSERT INTO
-      pt_location_networks (location_id, network)
-    SELECT
-      id,
-      '10.1.0.0/16'
-    FROM
-      new_locations_2
-  ),
   new_sessions AS (
     INSERT INTO
       pt_sessions (location_id, date)
@@ -52,17 +35,6 @@ WITH
       NOW()
     FROM
       new_locations
-    RETURNING
-      *
-  ),
-  new_sessions_2 AS (
-    INSERT INTO
-      pt_sessions (location_id, date)
-    SELECT
-      id,
-      NOW()
-    FROM
-      new_locations_2
     RETURNING
       *
   ),
@@ -102,28 +74,6 @@ WITH
       new_enrollments AS e,
       new_exams AS x,
       new_sessions AS s
-    RETURNING
-      *
-  ),
-  new_reservation_2 AS (
-    INSERT INTO
-      pt_reservations (
-        enrollment_id,
-        exam_id,
-        session_id,
-        access_start,
-        access_end
-      )
-    SELECT
-      e.id,
-      x.id,
-      s.id,
-      now() - interval '1 hour',
-      now() + interval '1 hour'
-    FROM
-      new_enrollments AS e,
-      new_exams AS x,
-      new_sessions_2 AS s
     RETURNING
       *
   )
