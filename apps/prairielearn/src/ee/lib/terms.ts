be2ae45765208c2f79e240e0ba5e67eea5ee852b
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-04-09 14:05:51 -0700

    Add feature flag

--- apps/prairielearn/src/ee/lib/terms.ts
+++ apps/prairielearn/src/ee/lib/terms.ts
@@ -4,6 +4,7 @@ import { callRow } from '@prairielearn/postgres';
 import { ModeSchema, type User } from '../../lib/db-types';
 import { HttpRedirect } from '../../lib/redirect';
 import { setCookie } from '../../lib/cookie';
+import { features } from '../../lib/features';
 
 function hasUserAcceptedTerms(user: User): boolean {
   // At the moment, we only have one revision of our terms and conditions, so
@@ -27,6 +28,12 @@ function hasUserAcceptedTerms(user: User): boolean {
  */
 export async function shouldRedirectToTermsPage(user: User, ip: string) {
   if (hasUserAcceptedTerms(user)) return false;
+
+  const featureEnabled = await features.enabled('terms-clickthrough', {
+    institution_id: user.institution_id,
+    user_id: user.user_id,
+  });
+  if (!featureEnabled) return false;
 
   const mode = await callRow('ip_to_mode', [ip, new Date(), user.user_id], ModeSchema);
   return mode === 'Public';
