87fb427d8d5ee2556237dd04439bbdadb58e58b5
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-04-10 09:16:18 -0700

    Convert selectAndAuthzAssessment to async/await (#9708)
    * Convert selectAndAuthzAssessment to async/await
    
    * Use ESM exports

--- apps/prairielearn/src/middlewares/selectAndAuthzAssessment.js
+++ apps/prairielearn/src/middlewares/selectAndAuthzAssessment.js
@@ -1,22 +1,20 @@
-var ERR = require('async-stacktrace');
-var _ = require('lodash');
+// @ts-check
+const asyncHandler = require('express-async-handler');
+import * as _ from 'lodash';
 
-var sqldb = require('@prairielearn/postgres');
-const error = require('@prairielearn/error');
+import * as sqldb from '@prairielearn/postgres';
+import * as error from '@prairielearn/error';
 
-var sql = sqldb.loadSqlEquiv(__filename);
+const sql = sqldb.loadSqlEquiv(__filename);
 
-module.exports = function (req, res, next) {
-  var params = {
+export default asyncHandler(async (req, res, next) => {
+  const result = await sqldb.queryZeroOrOneRowAsync(sql.select_and_auth, {
     assessment_id: req.params.assessment_id,
     course_instance_id: res.locals.course_instance.id,
     authz_data: res.locals.authz_data,
     req_date: res.locals.req_date,
-  };
-  sqldb.queryZeroOrOneRow(sql.select_and_auth, params, function (err, result) {
-    if (ERR(err, next)) return;
-    if (result.rowCount === 0) return next(error.make(403, 'Access denied'));
-    _.assign(res.locals, result.rows[0]);
-    next();
   });
-};
+  if (result.rowCount === 0) throw error.make(403, 'Access denied');
+  _.assign(res.locals, result.rows[0]);
+  next();
+});
