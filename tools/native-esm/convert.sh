bb3d0d16453e4d6ac0e051a271cd87795e026485
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-04-23 15:25:30 -0700

    WIP rewriting script

--- tools/native-esm/convert.sh
+++ tools/native-esm/convert.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+ROOT_DIR="$(realpath "${SCRIPT_DIR}/../..")"
+
+# Fix imports of modules which use an "index" file
+
 # Add `.js` to all imports of local files
-ast-grep scan --rule rewrite-local-imports-js.yml -U apps/
-asg-grep scan --rule rewrite-local-imports-ts.yml -U apps/
+ast-grep scan --rule $SCRIPT_DIR/rewrite-local-imports-js.yml -U $ROOT_DIR/apps/ $ROOT_DIR/packages/
+ast-grep scan --rule $SCRIPT_DIR/rewrite-local-imports-ts.yml -U $ROOT_DIR/apps/ $ROOT_DIR/packages/
