af41351117403615d75d58fc669b28147b91f280
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-04-24 09:21:55 -0700

    Generate TS rules from JS ones

--- tools/native-esm/convert.sh
+++ tools/native-esm/convert.sh
@@ -5,15 +5,31 @@ set -e
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 ROOT_DIR="$(realpath "${SCRIPT_DIR}/../..")"
 
+# `ast-grep` rules don't support multiple languages:
+# https://github.com/ast-grep/ast-grep/issues/525
+#
+# To keep ourselves sane, we'll consruct the TypeScript rules from the
+# JavaScript rules.
+for file in $SCRIPT_DIR/*.js.yml; do
+  ts_file="${file%.js.yml}.ts.yml"
+  cp "$file" "$ts_file"
+
+  # Replace `language: JavaScript` with `language: TypeScript`
+  sed -i '' 's/language: JavaScript/language: TypeScript/' "$ts_file"
+done
+
 # Fix imports of modules which use an "index" file (inside `/lib` directory)
-ast-grep scan --rule $SCRIPT_DIR/fix-index-files-in-lib-js.yml -U $ROOT_DIR/apps/prairielearn/src/lib/*.{js,ts}
-ast-grep scan --rule $SCRIPT_DIR/fix-index-files-in-lib-ts.yml -U $ROOT_DIR/apps/prairielearn/src/lib/*.{js,ts}
+ast-grep scan --rule $SCRIPT_DIR/fix-index-files-in-lib.js.yml -U $ROOT_DIR/apps/prairielearn/src/lib/*.{js,ts}
+ast-grep scan --rule $SCRIPT_DIR/fix-index-files-in-lib.ts.yml -U $ROOT_DIR/apps/prairielearn/src/lib/*.{js,ts}
 
 # Fix imports of modules which use an "index" file (outside `/lib` directory)
-ast-grep scan --rule $SCRIPT_DIR/fix-index-files-outside-lib-js.yml -U $ROOT_DIR/apps/prairielearn/src
-ast-grep scan --rule $SCRIPT_DIR/fix-index-files-outside-lib-ts.yml -U $ROOT_DIR/apps/prairielearn/src
+ast-grep scan --rule $SCRIPT_DIR/fix-index-files-outside-lib.js.yml -U $ROOT_DIR/apps/prairielearn/src
+ast-grep scan --rule $SCRIPT_DIR/fix-index-files-outside-lib.ts.yml -U $ROOT_DIR/apps/prairielearn/src
 
 # Add `.js` to all imports of local files
-ast-grep scan --rule $SCRIPT_DIR/rewrite-local-imports-js.yml -U $ROOT_DIR/apps/ $ROOT_DIR/packages/
-ast-grep scan --rule $SCRIPT_DIR/rewrite-local-imports-ts.yml -U $ROOT_DIR/apps/ $ROOT_DIR/packages/
+ast-grep scan --rule $SCRIPT_DIR/rewrite-local-imports.js.yml -U $ROOT_DIR/apps/ $ROOT_DIR/packages/
+ast-grep scan --rule $SCRIPT_DIR/rewrite-local-imports.ts.yml -U $ROOT_DIR/apps/ $ROOT_DIR/packages/
+
+ast-grep -p 'loadSqlEquiv(__filename)' -r 'loadSqlEquiv(import.meta.url)' -U $ROOT_DIR/apps/
+
 
