1b317a3b89ac13f86cebba9648b7623c37398d1b
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-04-24 09:28:48 -0700

    Handle more cases

--- tools/native-esm/convert.sh
+++ tools/native-esm/convert.sh
@@ -18,13 +18,17 @@ for file in $SCRIPT_DIR/*.js.yml; do
   sed -i '' 's/language: JavaScript/language: TypeScript/' "$ts_file"
 done
 
-# Fix imports of modules which use an "index" file (inside `/lib` directory)
-ast-grep scan --rule $SCRIPT_DIR/fix-index-files-in-lib.js.yml -U $ROOT_DIR/apps/prairielearn/src/lib/*.{js,ts}
-ast-grep scan --rule $SCRIPT_DIR/fix-index-files-in-lib.ts.yml -U $ROOT_DIR/apps/prairielearn/src/lib/*.{js,ts}
+# Fix imports of modules in `/lib` which use an "index" file (inside `/lib` directory)
+ast-grep scan --rule $SCRIPT_DIR/fix-lib-index-files-in-lib.js.yml -U $ROOT_DIR/apps/prairielearn/src/lib/*.{js,ts}
+ast-grep scan --rule $SCRIPT_DIR/fix-lib-index-files-in-lib.ts.yml -U $ROOT_DIR/apps/prairielearn/src/lib/*.{js,ts}
 
-# Fix imports of modules which use an "index" file (outside `/lib` directory)
-ast-grep scan --rule $SCRIPT_DIR/fix-index-files-outside-lib.js.yml -U $ROOT_DIR/apps/prairielearn/src
-ast-grep scan --rule $SCRIPT_DIR/fix-index-files-outside-lib.ts.yml -U $ROOT_DIR/apps/prairielearn/src
+# Fix imports of modules in `lib/` which use an "index" file (outside `/lib` directory)
+ast-grep scan --rule $SCRIPT_DIR/fix-lib-index-files-outside-lib.js.yml -U $ROOT_DIR/apps/prairielearn/src
+ast-grep scan --rule $SCRIPT_DIR/fix-lib-index-files-outside-lib.ts.yml -U $ROOT_DIR/apps/prairielearn/src
+
+# Fix imports of things like `sprocs` and `cron`
+ast-grep scan --rule $SCRIPT_DIR/fix-index-files.js.yml -U $ROOT_DIR/apps/prairielearn/src
+ast-grep scan --rule $SCRIPT_DIR/fix-index-files.ts.yml -U $ROOT_DIR/apps/prairielearn/src
 
 # Add `.js` to all imports of local files
 ast-grep scan --rule $SCRIPT_DIR/rewrite-local-imports.js.yml -U $ROOT_DIR/apps/ $ROOT_DIR/packages/
