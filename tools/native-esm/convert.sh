7c0250dc3cd04b970e8b5c87dcb116a069400a81
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-04-24 11:03:10 -0700

    More tool refinements

--- tools/native-esm/convert.sh
+++ tools/native-esm/convert.sh
@@ -30,15 +30,20 @@ ast-grep scan --rule $SCRIPT_DIR/fix-index-files.js.yml -U $ROOT_DIR/apps/prairi
 ast-grep scan --rule $SCRIPT_DIR/fix-index-files.ts.yml -U $ROOT_DIR/apps/prairielearn/src
 
 # Add `.js` to all imports of local files
-ast-grep scan --rule $SCRIPT_DIR/rewrite-local-imports.js.yml -U $ROOT_DIR/apps/ $ROOT_DIR/packages/
-ast-grep scan --rule $SCRIPT_DIR/rewrite-local-imports.ts.yml -U $ROOT_DIR/apps/ $ROOT_DIR/packages/
+ast-grep scan --rule $SCRIPT_DIR/rewrite-local-imports.js.yml -U $ROOT_DIR/apps/*/src $ROOT_DIR/packages/*/src
+ast-grep scan --rule $SCRIPT_DIR/rewrite-local-imports.ts.yml -U $ROOT_DIR/apps/*/src $ROOT_DIR/packages/*/src
+ast-grep scan --rule $SCRIPT_DIR/rewrite-local-dynamic-imports.js.yml -U $ROOT_DIR/apps/*/src $ROOT_DIR/packages/*/src
+ast-grep scan --rule $SCRIPT_DIR/rewrite-local-dynamic-imports.ts.yml -U $ROOT_DIR/apps/*/src $ROOT_DIR/packages/*/src
 
 # Fix `loadSqlEquiv` calls
-ast-grep -p 'loadSqlEquiv(__filename)' -r 'loadSqlEquiv(import.meta.url)' -U $ROOT_DIR/apps/
-ast-grep -p '$SQLDB.loadSqlEquiv(__filename)' -r '$SQLDB.loadSqlEquiv(import.meta.url)' -U $ROOT_DIR/apps/
+ast-grep -p 'loadSqlEquiv(__filename)' -r 'loadSqlEquiv(import.meta.url)' -U $ROOT_DIR/apps/*/src
+ast-grep -p '$SQLDB.loadSqlEquiv(__filename)' -r '$SQLDB.loadSqlEquiv(import.meta.url)' -U $ROOT_DIR/apps/*/src
 
 # Fix `lodash` imports
-ast-grep -p "import * as _ from 'lodash'" -r "import _ from 'lodash'" -U $ROOT_DIR/apps/
+ast-grep -p "import * as _ from 'lodash'" -r "import _ from 'lodash'" -U $ROOT_DIR/apps/*/src
 
 # Convert requires to default imports
-ast-grep -p 'const $VAR = require($MODULE)' -r 'import $VAR from $MODULE' -U $ROOT_DIR/apps/ $ROOT_DIR/packages/
+ast-grep -p 'const $VAR = require($MODULE)' -r 'import $VAR from $MODULE' -U $ROOT_DIR/apps/*/src $ROOT_DIR/packages/*/src
+
+# Convert TS-stype requires to default imports
+ast-grep scan --rule $SCRIPT_DIR/rewrite-typescript-cjs-import.yml -U $ROOT_DIR/apps/*/src $ROOT_DIR/apps/prairielearn/assets
