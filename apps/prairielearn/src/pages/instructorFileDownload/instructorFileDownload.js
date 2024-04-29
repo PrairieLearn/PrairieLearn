46fcfb5340d5db74e0f475e63eaa622d4bd3a575
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2024-04-29 16:55:04 -0700

    Update nullable constraint in query schema

--- apps/prairielearn/src/pages/instructorFileDownload/instructorFileDownload.js
+++ apps/prairielearn/src/pages/instructorFileDownload/instructorFileDownload.js
@@ -7,13 +7,16 @@ import { decodePath } from '../../lib/uri-util';
 
 const router = express.Router();
 
-router.get('/*', asyncHandler(async (req, res) => {
-  if (!res.locals.authz_data.has_course_permission_view) {
-    throw new error.HttpStatusError(403, 'Access denied (must be course viewer)');
-  }
-  if (req.query.type) res.type(req.query.type.toString());
-  if (req.query.attachment) res.attachment(req.query.attachment.toString());
-  res.sendFile(decodePath(req.params[0]), { root: res.locals.course.path });
-}));
+router.get(
+  '/*',
+  asyncHandler(async (req, res) => {
+    if (!res.locals.authz_data.has_course_permission_view) {
+      throw new error.HttpStatusError(403, 'Access denied (must be course viewer)');
+    }
+    if (req.query.type) res.type(req.query.type.toString());
+    if (req.query.attachment) res.attachment(req.query.attachment.toString());
+    res.sendFile(decodePath(req.params[0]), { root: res.locals.course.path });
+  }),
+);
 
 export default router;
--- apps/prairielearn/src/pages/instructorInstanceAdminAccess/instructorInstanceAdminAccess.js
+++ apps/prairielearn/src/pages/instructorInstanceAdminAccess/instructorInstanceAdminAccess.js
@@ -8,17 +8,24 @@ import * as sqldb from '@prairielearn/postgres';
 const router = express.Router();
 const sql = sqldb.loadSqlEquiv(__filename);
 
-router.get('/', asyncHandler(async (req, res) => {
-  res.locals.access_rules = await sqldb.queryRows(sql.course_instance_access_rules, {
-    course_instance_id: res.locals.course_instance.id,
-  }, z.object({
-    uids: z.string(),
-    start_date: z.string(),
-    end_date: z.string(),
-    institution: z.string(),
-  }));
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    res.locals.access_rules = await sqldb.queryRows(
+      sql.course_instance_access_rules,
+      {
+        course_instance_id: res.locals.course_instance.id,
+      },
+      z.object({
+        uids: z.string(),
+        start_date: z.string(),
+        end_date: z.string(),
+        institution: z.string(),
+      }),
+    );
 
-  res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
-}));
+    res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+  }),
+);
 
 export default router;
