5b3c9e247097d4fc232ed604d82bbe884ab4ec2d
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2024-05-02 16:05:21 -0700

    Modernize instructor course admin sets

--- apps/prairielearn/src/pages/instructorCourseAdminSets/instructorCourseAdminSets.ts
+++ apps/prairielearn/src/pages/instructorCourseAdminSets/instructorCourseAdminSets.ts
@@ -1,10 +1,10 @@
-// @ts-check
-const asyncHandler = require('express-async-handler');
+import asyncHandler = require('express-async-handler');
 
 import * as express from 'express';
 
 import * as sqldb from '@prairielearn/postgres';
 import { AssessmentSetSchema } from '../../lib/db-types';
+import { InstructorCourseAdminSets } from './instructorCourseAdminSets.html';
 
 const router = express.Router();
 const sql = sqldb.loadSqlEquiv(__filename);
@@ -12,13 +12,13 @@ const sql = sqldb.loadSqlEquiv(__filename);
 router.get(
   '/',
   asyncHandler(async (req, res) => {
-    res.locals.assessment_sets = await sqldb.queryRows(
+    const assessmentSets = await sqldb.queryRows(
       sql.select_assessment_sets,
       { course_id: res.locals.course.id },
       AssessmentSetSchema,
     );
 
-    res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
+    res.send(InstructorCourseAdminSets({ resLocals: res.locals, assessmentSets }));
   }),
 );
 
