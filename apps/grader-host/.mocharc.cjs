aedc51250646c831944736b0476d9cabc354a275
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-05-07 15:24:28 -0700

    Remove unused flag

--- apps/grader-host/.mocharc.cjs
+++ apps/grader-host/.mocharc.cjs
@@ -1,7 +1,3 @@
 module.exports = {
-  'node-option': [
-    'import=tsx/esm',
-    // Silence the warning that we get from using the above option.
-    'no-warnings',
-  ],
+  'node-option': ['import=tsx/esm'],
 };
--- apps/prairielearn/.mocharc.cjs
+++ apps/prairielearn/.mocharc.cjs
@@ -19,11 +19,7 @@ const isRunningOnDist = process.argv
 process.env.TSX_TSCONFIG_PATH = './src/tsconfig.json';
 
 module.exports = {
-  'node-option': [
-    'import=tsx/esm',
-    // Silence the warning that we get from using the above option.
-    'no-warnings',
-  ],
+  'node-option': ['import=tsx/esm'],
   require: [isRunningOnDist ? './dist/tests/mocha-hooks.js' : './src/tests/mocha-hooks.ts'].filter(
     Boolean,
   ),
