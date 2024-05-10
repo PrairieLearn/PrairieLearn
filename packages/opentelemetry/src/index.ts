c51f3aa4f6bb3f5a6261fb638b63871c4a1e2ed8
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-05-09 17:25:57 -0700

    Fix OpenTelemetry with ESM code (#9839)
    * Fix OpenTelemetry instrumentation when using ESM
    
    * Fix Jaeger exporting; upgrade all packages
    
    * Add comment
    
    * Add changeset

--- packages/opentelemetry/src/index.ts
+++ packages/opentelemetry/src/index.ts
@@ -29,3 +29,15 @@ export {
   createObservableValueGauges,
   createObservableValueGaugesOptions,
 } from './metrics';
+
+// Extremely stupid workaround for the fact that the OpenTelemetry instrumentations
+// don't currently have robust support for ESM. This hack ensures that instrumented
+// packages are loaded for the first time via CJS, which the OpenTelemetry
+// instrumentation can intercept and patch. Subsequent imports can be ESM, but that
+// doesn't matter because the instrumentation has already been applied.
+//
+// OpenTelemetry introduced experimental support for ESM in
+// https://github.com/open-telemetry/opentelemetry-js/pull/3698. This requires
+// the usage of `--experimental-loader` to load the instrumentations, which
+// we currently want to avoid.
+import './commonjs-preloads.js';
