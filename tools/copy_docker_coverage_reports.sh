a05d046002649ef601072b570b3d2eb2b30acf38
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-05-17 07:46:21 -0700

    Refine script comments

--- tools/copy_docker_coverage_reports.sh
+++ tools/copy_docker_coverage_reports.sh
@@ -10,19 +10,20 @@ if [ "$#" -ne 1 ]; then
 fi
 CONTAINER_NAME=$1
 
-# Construct a list of all coverage reports in the container
+# Construct a list of all coverage reports in the container.
 docker container exec $CONTAINER_NAME bash -c "find /PrairieLearn/apps -name cobertura-coverage.xml" > /tmp/coverage_reports.txt
 docker container exec $CONTAINER_NAME bash -c "find /PrairieLearn/packages -name cobertura-coverage.xml" > /tmp/coverage_reports.txt
 
+# Print the list for debugging.
 echo "Coverage reports in container:"
 cat /tmp/coverage_reports.txt
 echo ""
 
-# Get the working directory of this script file.
+# Compute the absolute path of the root directory of the repository.
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIR=$(realpath $SCRIPT_DIR/..)
 
-# Copy each coverage report to the host
+# Copy each coverage report to the appropriate location on the host.
 while read -r COVERAGE_REPORT; do
     RELATIVE_PATH=$(echo $COVERAGE_REPORT | sed 's/\/PrairieLearn\///')
     HOST_COVERAGE_REPORT=$ROOT_DIR/$RELATIVE_PATH
