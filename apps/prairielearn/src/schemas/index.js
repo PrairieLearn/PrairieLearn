5cd7490169b58dc9894c170ecfabaa21e1a19afc
Jonatan Schroeder <jonatan@yorku.ca>
2024-05-21 16:13:00 -0400

    Use explicit type: json in imports

--- apps/prairielearn/src/schemas/index.js
+++ apps/prairielearn/src/schemas/index.js
@@ -1,16 +1,16 @@
 // @ts-check
 
-export { default as infoNewsItem } from './schemas/infoNewsItem.json';
-export { default as infoAssessment } from './schemas/infoAssessment.json';
-export { default as infoCourse } from './schemas/infoCourse.json';
-export { default as infoCourseInstance } from './schemas/infoCourseInstance.json';
-export { default as infoElementCore } from './schemas/infoElementCore.json';
-export { default as infoElementCourse } from './schemas/infoElementCourse.json';
-export { default as infoElementExtension } from './schemas/infoElementExtension.json';
-export { default as infoQuestion } from './schemas/infoQuestion.json';
-export { default as questionOptionsCalculation } from './schemas/questionOptionsCalculation.json';
-export { default as questionOptionsCheckbox } from './schemas/questionOptionsCheckbox.json';
-export { default as questionOptionsFile } from './schemas/questionOptionsFile.json';
-export { default as questionOptionsMultipleChoice } from './schemas/questionOptionsMultipleChoice.json';
-export { default as questionOptionsMultipleTrueFalse } from './schemas/questionOptionsMultipleTrueFalse.json';
-export { default as questionOptionsv3 } from './schemas/questionOptionsv3.json';
+export { default as infoNewsItem } from './schemas/infoNewsItem.json' with { type: 'json' };
+export { default as infoAssessment } from './schemas/infoAssessment.json' with { type: 'json' };
+export { default as infoCourse } from './schemas/infoCourse.json' with { type: 'json' };
+export { default as infoCourseInstance } from './schemas/infoCourseInstance.json' with { type: 'json' };
+export { default as infoElementCore } from './schemas/infoElementCore.json' with { type: 'json' };
+export { default as infoElementCourse } from './schemas/infoElementCourse.json' with { type: 'json' };
+export { default as infoElementExtension } from './schemas/infoElementExtension.json' with { type: 'json' };
+export { default as infoQuestion } from './schemas/infoQuestion.json' with { type: 'json' };
+export { default as questionOptionsCalculation } from './schemas/questionOptionsCalculation.json' with { type: 'json' };
+export { default as questionOptionsCheckbox } from './schemas/questionOptionsCheckbox.json' with { type: 'json' };
+export { default as questionOptionsFile } from './schemas/questionOptionsFile.json' with { type: 'json' };
+export { default as questionOptionsMultipleChoice } from './schemas/questionOptionsMultipleChoice.json' with { type: 'json' };
+export { default as questionOptionsMultipleTrueFalse } from './schemas/questionOptionsMultipleTrueFalse.json' with { type: 'json' };
+export { default as questionOptionsv3 } from './schemas/questionOptionsv3.json' with { type: 'json' };
