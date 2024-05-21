ab7449f1ad1a7006e54b8d7b44ad71f9c24229a9
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-05-21 13:17:52 -0700

    Rewrite question per code review

--- exampleCourse/questions/template/number-input/multiple-answer-inputs/server.py
+++ exampleCourse/questions/template/number-input/multiple-answer-inputs/server.py
@@ -1,4 +1,3 @@
-import itertools
 import math
 import random
 
@@ -6,28 +5,26 @@ import sympy
 
 
 def generate(data):
-    # Select 2 random coefficients between -15 and 15, excluding 0.
-    a, b = random.sample(sorted(itertools.chain(range(-15, 0), range(1, 16))), 2)
+    # We want to pick three random coefficients a, b, and c such that the
+    # resulting polynomial has two real roots. We'll loop until we manage
+    # to find such coefficients.
+    while True:
+        a = random.randint(-15, 15)
+        b = random.randint(-15, 15)
+        c = random.randint(-15, 15)
 
-    # We want to ensure that the polynomial has 2 real roots.
-    # To do this, we make sure that the discriminant is positive.
-    # Solving `b**2 - 4*a*c > 0` for `c`, we get `c < b**2 / (4 * a)`.
-    # Note that the sign of `a` determines the direction of the inequality.
-    c_bound = b**2 / (4 * a)
-    if a > 0:
-        c_bound = math.floor(c_bound)
-        c = random.randint(min(-15, c_bound - 15), c_bound)
-    else:
-        c_bound = math.ceil(c_bound)
-        c = random.randint(c_bound, max(15, c_bound + 15))
+        if a != 0 and b != 0 and c != 0 and b**2 - 4 * a * c > 0:
+            break
 
-    # Generate the polynomial.
+    # Use sympy to generate a LaTeX representation of the polynomial for display.
+    # This avoids the need to manually handle things like negative signs and
+    # coefficients of 1.
     x = sympy.symbols("x")
-    polynomial = sympy.expand(a * x**2 + b * x + c)
+    data["params"]["polynomial"] = sympy.latex(a * x**2 + b * x + c)
 
-    # Compute the roots of the polynomial.
-    roots = [float(root.evalf()) for root in sympy.solve(polynomial, x)]
+    # Calculate the roots of the polynomial with the quadratic formula.
+    smaller_root = (-b - math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+    larger_root = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
 
-    data["params"]["polynomial"] = str(sympy.latex(polynomial))
-    data["correct_answers"]["smaller_root"] = min(roots)
-    data["correct_answers"]["larger_root"] = max(roots)
+    data["correct_answers"]["smaller_root"] = smaller_root
+    data["correct_answers"]["larger_root"] = larger_root
