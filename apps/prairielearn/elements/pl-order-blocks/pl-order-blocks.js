043c0e0ef439dac6b9a918ade0674b4886c1bc8f
fowlinbasket <foul.in.basket@gmail.com>
2024-05-22 14:49:07 -0600

    updated formatting

--- apps/prairielearn/elements/pl-order-blocks/pl-order-blocks.js
+++ apps/prairielearn/elements/pl-order-blocks/pl-order-blocks.js
@@ -28,59 +28,54 @@ window.PLOrderBlocks = function (uuid, options) {
     // TODO: make a single event listener for the whole page (using currFocusedBlockIdx) instead of each individual block
     for (let block of blocks) {
       block.addEventListener('click', () => block.focus()); // TODO: Delete this line after adding keyboard controls for focusing different blocks
-      block.addEventListener(
-        'keydown',
-        (ev) => {
-          console.log(ev.key);
-          pressedKeys[ev.key] = true;
-          switch (ev.key) {
-            case 'ArrowDown':
-              if (block.nextElementSibling) {
-                block.nextElementSibling.insertAdjacentElement('afterend', block);
+      block.addEventListener('keydown', (ev) => {
+        console.log(ev.key);
+        pressedKeys[ev.key] = true;
+        switch (ev.key) {
+          case 'ArrowDown':
+            if (block.nextElementSibling) {
+              block.nextElementSibling.insertAdjacentElement('afterend', block);
+            }
+            ev.preventDefault();
+            break;
+          case 'ArrowUp':
+            if (block.previousElementSibling) {
+              block.previousElementSibling.insertAdjacentElement('beforebegin', block);
+            }
+            ev.preventDefault();
+            break;
+          case 'Backspace':
+            if (inDropzone(block)) {
+              block.style.marginLeft = '0px';
+              $(optionsElementId)[0].insertAdjacentElement('afterbegin', block);
+            }
+            ev.preventDefault();
+            break;
+          case 'Enter':
+            if (!inDropzone(block)) {
+              $(dropzoneElementId)[0].insertAdjacentElement('afterbegin', block);
+            }
+            ev.preventDefault();
+            break;
+          case 'Tab':
+            if (inDropzone(block) && enableIndentation) {
+              let currentIndent = getIndentation(block);
+              if (pressedKeys['Shift'] && currentIndent > 0) {
+                block.style.marginLeft = (currentIndent - 1) * TABWIDTH + 'px';
+              } else if (currentIndent < maxIndent) {
+                block.style.marginLeft = (currentIndent + 1) * TABWIDTH + 'px';
               }
-              ev.preventDefault();
-              break;
-            case 'ArrowUp':
-              if (block.previousElementSibling) {
-                block.previousElementSibling.insertAdjacentElement('beforebegin', block);
-              }
-              ev.preventDefault();
-              break;
-            case 'Backspace':
-              if (inDropzone(block)) {
-                block.style.marginLeft = '0px';
-                $(optionsElementId)[0].insertAdjacentElement('afterbegin', block);
-              }
-              ev.preventDefault();
-              break;
-            case 'Enter':
-              if (!inDropzone(block)) {
-                $(dropzoneElementId)[0].insertAdjacentElement('afterbegin', block);
-              }
-              ev.preventDefault();
-              break;
-            case 'Tab':
-              if (inDropzone(block) && enableIndentation) {
-                let currentIndent = getIndentation(block);
-                if (pressedKeys['Shift'] && currentIndent > 0) {
-                  block.style.marginLeft = (currentIndent - 1) * TABWIDTH + 'px';
-                } else if (currentIndent < maxIndent) {
-                  block.style.marginLeft = (currentIndent + 1) * TABWIDTH + 'px';
-                }
-              }
-              ev.preventDefault();
-              break;
-            default:
-              break;
-          }
-          block.focus();
-        });
-      block.addEventListener(
-        'keyup',
-        (ev) => {
-          pressedKeys[ev.key] = false;
+            }
+            ev.preventDefault();
+            break;
+          default:
+            break;
         }
-      )
+        block.focus();
+      });
+      block.addEventListener('keyup', (ev) => {
+        pressedKeys[ev.key] = false;
+      });
     }
   }
 
