a4924933e52f7945e67629578e5be92e882abcfb
Michael Holmes <a02395591@aggies.usu.edu>
2024-05-24 12:45:31 -0600

    Allow course sharing name to be changed (#9860)
    * Untested check to see if the course sharing name can be changed
    
    * requirements doc
    
    * Added ability to change the sharing name (IP)
    
    * Final commit for 'updating course sharing name'
    
    * Deleted personal notes file
    
    Delete apps/prairielearn/src/pages/instructorCourseAdminSharing/FunctionalityToAdd.md
    
    * Changed formatting to match original
    
    * Updated call names and added check for imported questions at sharing page load
    
    * testing POST request
    
    * Added untested automated tests for updating the sharing name when a question has not been shared and when one has
    
    * Working POST request to check for imported questions
    
    * address PR comments
    
    * handle case where sharing_name hasn't been chosen yet
    
    * close div tag
    
    Co-authored-by: Nathan Sarang-Walters <nwalters512@gmail.com>
    
    * Changed z.any() to z.boolean() for 'selectCanChooseSharingName'
    
    * Moved the sharing name input and added a label for it
    
    * Reformatted using Prettier
    
    ---------
    
    Co-authored-by: Seth Poulsen <poulsenseth@yahoo.com>
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>
    Co-authored-by: Nathan Sarang-Walters <nwalters512@gmail.com>

--- apps/prairielearn/src/pages/instructorCourseAdminSharing/instructorCourseAdminSharing.sql
+++ apps/prairielearn/src/pages/instructorCourseAdminSharing/instructorCourseAdminSharing.sql
@@ -69,3 +69,24 @@ SET
   sharing_name = $sharing_name
 WHERE
   id = $course_id;
+
+-- BLOCK select_shared_question_exists
+SELECT
+  EXISTS (
+    SELECT
+      1
+    FROM
+      questions AS q
+    WHERE
+      q.shared_publicly
+      AND course_id = $course_id
+    UNION
+    SELECT
+      1
+    FROM
+      sharing_sets AS ss
+      JOIN sharing_set_questions AS ssq ON ss.id = ssq.sharing_set_id
+      JOIN questions AS q ON q.id = ssq.question_id
+    WHERE
+      ss.course_id = $course_id
+  );
