eea1950d62ca5977d80371e2db36070f5914b8dd
Jonatan Schroeder <jonatan@yorku.ca>
2024-05-29 11:52:43 -0400

    Add JSDoc to `encodePath`

--- apps/prairielearn/src/lib/uri-util.js
+++ apps/prairielearn/src/lib/uri-util.js
@@ -4,9 +4,12 @@ import * as path from 'path';
 import { logger } from '@prairielearn/logger';
 
 /**
+ * Encodes a path for use in a URL. This is a middle-ground between `encodeURI`
+ * and `encodeURIComponent`, in that all characters encoded by
+ * `encodeURIComponent` are encoded, but slashes are not encoded.
  *
- * @param {string} originalPath
- * @returns {string}
+ * @param {string} originalPath path of the file that is the basis for the encoding
+ * @returns {string} Encoded path
  */
 export function encodePath(originalPath) {
   try {
