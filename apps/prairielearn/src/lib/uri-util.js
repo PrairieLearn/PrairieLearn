11ea6ef4c96ff2eec9b059532fed78349c7c5743
Jonatan Schroeder <jonatan@yorku.ca>
2024-05-29 15:29:48 -0400

    Fix double decoding of instructor files (#9907)
    * Fix double decoding of instructor files
    
    * Add JSDoc to `encodePath`
    
    * Add CI test for upload and delete
    
    * Add CI tests for view and download
    
    * Remove id parameter from renamefile test
    
    * Rearrange ID for upload file
    
    * Rename parameter for testUploadFile

--- apps/prairielearn/src/lib/uri-util.js
+++ apps/prairielearn/src/lib/uri-util.js
@@ -3,31 +3,19 @@ import * as path from 'path';
 
 import { logger } from '@prairielearn/logger';
 
+/**
+ * Encodes a path for use in a URL. This is a middle-ground between `encodeURI`
+ * and `encodeURIComponent`, in that all characters encoded by
+ * `encodeURIComponent` are encoded, but slashes are not encoded.
+ *
+ * @param {string} originalPath path of the file that is the basis for the encoding
+ * @returns {string} Encoded path
+ */
 export function encodePath(originalPath) {
   try {
-    let encodedPath = [];
-    path
-      .normalize(originalPath)
-      .split(path.sep)
-      .forEach((dir) => {
-        encodedPath.push(encodeURIComponent(dir));
-      });
-    return encodedPath.join('/');
+    return path.normalize(originalPath).split(path.sep).map(encodeURIComponent).join('/');
   } catch (err) {
     logger.error(`encodePath: returning empty string because failed to encode ${originalPath}`);
-    return '';
-  }
-}
-
-export function decodePath(originalPath) {
-  try {
-    let decodedPath = [];
-    originalPath.split(path.sep).forEach((dir) => {
-      decodedPath.push(decodeURIComponent(dir));
-    });
-    return decodedPath.join('/');
-  } catch (err) {
-    logger.error(`decodePath: returning empty string because failed to decode ${originalPath}`);
     return '';
   }
 }
