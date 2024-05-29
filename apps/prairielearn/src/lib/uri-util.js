5295bdd92f71453fa80c95c0d0f986f40f81e820
Jonatan Schroeder <jonatan@yorku.ca>
2024-05-29 11:43:11 -0400

    Fix double decoding of instructor files

--- apps/prairielearn/src/lib/uri-util.js
+++ apps/prairielearn/src/lib/uri-util.js
@@ -3,31 +3,16 @@ import * as path from 'path';
 
 import { logger } from '@prairielearn/logger';
 
+/**
+ *
+ * @param {string} originalPath
+ * @returns {string}
+ */
 export function encodePath(originalPath) {
   try {
-    let encodedPath = [];
-    path
-      .normalize(originalPath)
-      .split(path.sep)
-      .forEach((dir) => {
-        encodedPath.push(encodeURIComponent(dir));
-      });
-    return encodedPath.join('/');
+    return path.normalize(originalPath).split(path.sep).map(encodeURIComponent).join('/');
   } catch (err) {
     logger.error(`encodePath: returning empty string because failed to encode ${originalPath}`);
-    return '';
-  }
-}
-
-export function decodePath(originalPath) {
-  try {
-    let decodedPath = [];
-    originalPath.split(path.sep).forEach((dir) => {
-      decodedPath.push(decodeURIComponent(dir));
-    });
-    return decodedPath.join('/');
-  } catch (err) {
-    logger.error(`decodePath: returning empty string because failed to decode ${originalPath}`);
     return '';
   }
 }
