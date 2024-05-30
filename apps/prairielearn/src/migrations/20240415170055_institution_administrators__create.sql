6b0a5f910048890a557212cea6c3d1f733928fb4
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-05-30 14:39:01 -0700

    Add index on institution_id

--- apps/prairielearn/src/migrations/20240415170055_institution_administrators__create.sql
+++ apps/prairielearn/src/migrations/20240415170055_institution_administrators__create.sql
@@ -4,3 +4,5 @@ CREATE TABLE IF NOT EXISTS institution_administrators (
   institution_id BIGINT NOT NULL REFERENCES institutions (id) ON DELETE CASCADE ON UPDATE CASCADE,
   UNIQUE (user_id, institution_id)
 );
+
+CREATE INDEX IF NOT EXISTS institution_administrators_institution_id_idx ON institution_administrators (institution_id);
