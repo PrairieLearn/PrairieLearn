770eef582ab65dd040df0a4e999b1c01372da90e
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-06-03 09:42:31 -0700

    Update packages/postgres/src/error.ts
    Co-authored-by: Jonatan Schroeder <jonatan@yorku.ca>

--- packages/postgres/src/error.ts
+++ packages/postgres/src/error.ts
@@ -1,12 +1,9 @@
 export function formatQueryWithErrorPosition(query: string, position: number | null | undefined) {
   if (position == null) return query;
 
-  const preSql = query.substring(0, position);
-  const postSql = query.substring(position);
-  const prevNewline = Math.max(0, preSql.lastIndexOf('\n') + 1);
-  let nextNewline = postSql.indexOf('\n');
-  if (nextNewline < 0) nextNewline = postSql.length;
-  nextNewline += preSql.length;
+  const prevNewline = Math.max(0, query.lastIndexOf('\n', position) + 1);
+  let nextNewline = query.indexOf('\n', position);
+  if (nextNewline < 0) nextNewline = query.length;
   const gap = ' '.repeat(Math.max(0, position - prevNewline - 1));
   return (
     query.substring(0, nextNewline) +
