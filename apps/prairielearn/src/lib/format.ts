816e3a18d440c99d156348e1bbc651a4f855265e
Jonatan Schroeder <jonatan@yorku.ca>
2024-06-03 13:52:48 -0400

    Modernize assessment score panel component

--- apps/prairielearn/src/lib/format.ts
+++ apps/prairielearn/src/lib/format.ts
@@ -5,3 +5,22 @@ export function formatFloat(x: number | null, numDecDigits = 2) {
     return '—';
   }
 }
+
+export function formatPoints(x: number | null, numDecDigits = 2) {
+  if (Number.isFinite(x) && typeof x === 'number') {
+    return formatFloat(
+      Math.floor(x * 10 ** numDecDigits) / 10 ** numDecDigits,
+      numDecDigits,
+    ).replace(/\.?0+$/, '');
+  } else {
+    return '—';
+  }
+}
+
+export function formatPointsOrList(v: number | number[] | null) {
+  if (Array.isArray(v)) {
+    return v.map((p) => formatPoints(p)).join(', ');
+  } else {
+    return formatPoints(v);
+  }
+}
