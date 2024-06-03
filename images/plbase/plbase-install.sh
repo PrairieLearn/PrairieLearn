43d749e8b61a129ea1a49e023e1f39ed5dcad156
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-06-03 11:41:43 -0700

    Disable machine-specific optimizations for pgvector

--- images/plbase/plbase-install.sh
+++ images/plbase/plbase-install.sh
@@ -54,7 +54,11 @@ dnf -y install postgresql15-server-devel
 cd /tmp
 git clone --branch v0.7.0 https://github.com/pgvector/pgvector.git
 cd pgvector
-make
+# This Docker image will be built in GitHub Actions but must run on a variety
+# of platforms, so we need to build it without machine-specific instructions.
+# https://github.com/pgvector/pgvector/issues/143
+# https://github.com/pgvector/pgvector/issues/143
+make OPTFLAGS=""
 make install
 rm -rf /tmp/pgvector
 dnf -y remove postgresql15-server-devel
