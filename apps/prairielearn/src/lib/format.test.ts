3fbdd1f0684c4e99e86cddb6cf82d9d610ad5188
Jonatan Schroeder <jonatan@yorku.ca>
2024-06-03 14:35:47 -0400

    Reorganize test for convention

--- apps/prairielearn/src/lib/format.test.ts
+++ apps/prairielearn/src/lib/format.test.ts
@@ -3,56 +3,74 @@ import { assert } from 'chai';
 import { formatFloat, formatPoints, formatPointsOrList } from './format.js';
 
 describe('format library', () => {
-  it('formatFloat', () => {
-    assert.equal(formatFloat(1), '1.00');
-    assert.equal(formatFloat(1.2), '1.20');
-    assert.equal(formatFloat(1.23), '1.23');
-    assert.equal(formatFloat(1.234), '1.23');
-    assert.equal(formatFloat(1.2345), '1.23');
-    assert.equal(formatFloat(1.2367), '1.24');
-    assert.equal(formatFloat(0), '0.00');
-    assert.equal(formatFloat(0.2), '0.20');
-    assert.equal(formatFloat(0.23), '0.23');
-    assert.equal(formatFloat(0.234), '0.23');
-    assert.equal(formatFloat(0.2345), '0.23');
+  describe('formatFloat', () => {
+    it('should format numbers correctly', () => {
+      assert.equal(formatFloat(1), '1.00');
+      assert.equal(formatFloat(1.2), '1.20');
+      assert.equal(formatFloat(1.23), '1.23');
+      assert.equal(formatFloat(1.234), '1.23');
+      assert.equal(formatFloat(1.2345), '1.23');
+      assert.equal(formatFloat(1.2367), '1.24');
+      assert.equal(formatFloat(0), '0.00');
+      assert.equal(formatFloat(0.2), '0.20');
+      assert.equal(formatFloat(0.23), '0.23');
+      assert.equal(formatFloat(0.234), '0.23');
+      assert.equal(formatFloat(0.2345), '0.23');
+    });
 
-    assert.equal(formatFloat(1.2346, 0), '1');
-    assert.equal(formatFloat(1.2346, 1), '1.2');
-    assert.equal(formatFloat(1.2346, 2), '1.23');
-    assert.equal(formatFloat(1.2346, 3), '1.235');
+    it('should format with varying number of decimal digits', () => {
+      assert.equal(formatFloat(1.2346, 0), '1');
+      assert.equal(formatFloat(1.2346, 1), '1.2');
+      assert.equal(formatFloat(1.2346, 2), '1.23');
+      assert.equal(formatFloat(1.2346, 3), '1.235');
+    });
 
-    assert.equal(formatFloat(null), '—');
+    it('should work with null input', () => {
+      assert.equal(formatFloat(null), '—');
+    });
   });
 
-  it('formatPoints', () => {
-    assert.equal(formatPoints(1), '1');
-    assert.equal(formatPoints(1.2), '1.2');
-    assert.equal(formatPoints(1.23), '1.23');
-    assert.equal(formatPoints(1.234), '1.23');
-    assert.equal(formatPoints(1.2345), '1.23');
-    assert.equal(formatPoints(1.2367), '1.23');
-    assert.equal(formatPoints(0), '0');
-    assert.equal(formatPoints(0.2), '0.2');
-    assert.equal(formatPoints(0.23), '0.23');
-    assert.equal(formatPoints(0.234), '0.23');
-    assert.equal(formatPoints(0.2345), '0.23');
+  describe('formatPoints', () => {
+    it('should format numbers correctly', () => {
+      assert.equal(formatPoints(1), '1');
+      assert.equal(formatPoints(1.2), '1.2');
+      assert.equal(formatPoints(1.23), '1.23');
+      assert.equal(formatPoints(1.234), '1.23');
+      assert.equal(formatPoints(1.2345), '1.23');
+      assert.equal(formatPoints(1.2367), '1.23');
+      assert.equal(formatPoints(0), '0');
+      assert.equal(formatPoints(0.2), '0.2');
+      assert.equal(formatPoints(0.23), '0.23');
+      assert.equal(formatPoints(0.234), '0.23');
+      assert.equal(formatPoints(0.2345), '0.23');
+    });
 
-    assert.equal(formatPoints(1.2345, 0), '1');
-    assert.equal(formatPoints(1.2345, 1), '1.2');
-    assert.equal(formatPoints(1.2345, 2), '1.23');
-    assert.equal(formatPoints(1.2345, 3), '1.234');
+    it('should format with varying number of decimal digits', () => {
+      assert.equal(formatPoints(1.2345, 0), '1');
+      assert.equal(formatPoints(1.2345, 1), '1.2');
+      assert.equal(formatPoints(1.2345, 2), '1.23');
+      assert.equal(formatPoints(1.2345, 3), '1.234');
+    });
 
-    assert.equal(formatPoints(null), '—');
+    it('should work with null input', () => {
+      assert.equal(formatPoints(null), '—');
+    });
   });
 
   it('formatPointsOrList', () => {
-    assert.equal(formatPointsOrList(1), '1');
-    assert.equal(formatPointsOrList(1.2345), '1.23');
+    it('should format numbers correctly', () => {
+      assert.equal(formatPointsOrList(1), '1');
+      assert.equal(formatPointsOrList(1.2345), '1.23');
+    });
 
-    assert.equal(formatPointsOrList(null), '—');
+    it('should work with null input', () => {
+      assert.equal(formatPointsOrList(null), '—');
+    });
 
-    assert.equal(formatPointsOrList([1, 2, 3]), '1, 2, 3');
-    assert.equal(formatPointsOrList([1.2345, 2.3456, 3.4567]), '1.23, 2.34, 3.45');
-    assert.equal(formatPointsOrList([1.2345, 2, 3.4]), '1.23, 2, 3.4');
+    it('should work with arrays of points', () => {
+      assert.equal(formatPointsOrList([1, 2, 3]), '1, 2, 3');
+      assert.equal(formatPointsOrList([1.2345, 2.3456, 3.4567]), '1.23, 2.34, 3.45');
+      assert.equal(formatPointsOrList([1.2345, 2, 3.4]), '1.23, 2, 3.4');
+    });
   });
 });
