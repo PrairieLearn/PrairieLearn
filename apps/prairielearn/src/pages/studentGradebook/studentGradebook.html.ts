84077ec64211b2b888ccd111f0aad21e18f7bd21
Jonatan Schroeder <jonatan@yorku.ca>
2024-06-04 12:23:03 -0400

    Remove columns not in use

--- apps/prairielearn/src/pages/studentGradebook/studentGradebook.html.ts
+++ apps/prairielearn/src/pages/studentGradebook/studentGradebook.html.ts
@@ -11,22 +11,12 @@ import {
 } from '../../lib/db-types.js';
 
 export const StudentGradebookRowSchema = z.object({
-  assessment_id: AssessmentSchema.shape.id,
-  assessment_number: AssessmentSchema.shape.number,
-  assessment_order_by: AssessmentSchema.shape.order_by,
   assessment_group_work: AssessmentSchema.shape.group_work,
   title: z.string(),
-  assessment_set_id: AssessmentSetSchema.shape.id,
-  assessment_set_abbreviation: AssessmentSetSchema.shape.abbreviation,
-  assessment_set_name: AssessmentSetSchema.shape.name,
   assessment_set_heading: AssessmentSetSchema.shape.heading,
   assessment_set_color: AssessmentSetSchema.shape.color,
-  assessment_set_number: AssessmentSetSchema.shape.number,
   label: z.string(),
-  assessment_instance_id: AssessmentInstanceSchema.shape.id,
-  assessment_instance_number: AssessmentInstanceSchema.shape.number,
   assessment_instance_score_perc: AssessmentInstanceSchema.shape.score_perc,
-  assessment_instance_open: AssessmentInstanceSchema.shape.open,
   show_closed_assessment_score: AssessmentAccessRulesSchema.shape.show_closed_assessment_score,
   start_new_set: z.boolean(),
 });
