538b4bbf854f2ed7dde38eb85533b899a690c2ae
Seth Poulsen <poulsenseth@yahoo.com>
2024-06-14 15:07:56 -0500

    PR comments

--- apps/prairielearn/src/lib/editors.ts
+++ apps/prairielearn/src/lib/editors.ts
@@ -1054,9 +1054,12 @@ export class QuestionCopyEditor extends Editor {
     debug(`Write info.json with new title and uuid`);
     infoJson.title = names.longName;
     infoJson.uuid = this.uuid;
+
+    // Even when copying a question within a course, we don't want to preserve
+    // sharing settings because they cannot be undone
     delete infoJson['sharingSets'];
-    delete infoJson['publiclyShared'];
-    delete infoJson['publiclySharedWithSource'];
+    delete infoJson['sharedPublicly'];
+    delete infoJson['sharedPubliclyWithSource'];
     await fs.writeJson(path.join(questionPath, 'info.json'), infoJson, { spaces: 4 });
 
     return {
@@ -1128,9 +1131,11 @@ export class QuestionTransferEditor extends Editor {
     debug(`Write info.json with new title and uuid`);
     infoJson.title = questionTitle;
     infoJson.uuid = this.uuid;
+
+    // We do not want to preserve sharing settings when copying a question to another course
     delete infoJson['sharingSets'];
-    delete infoJson['publiclyShared'];
-    delete infoJson['publiclySharedWithSource'];
+    delete infoJson['sharedPublicly'];
+    delete infoJson['sharedPubliclyWithSource'];
     await fs.writeJson(path.join(questionPath, 'info.json'), infoJson, { spaces: 4 });
 
     return {
