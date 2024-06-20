33c99ed1a6f75545cd48e21de537261e474360f4
Seth Poulsen <poulsenseth@yahoo.com>
2024-06-20 09:36:55 +0200

    continue implementing

--- exampleCourse/questions/demo/symbolicProofBlocks/server.py
+++ exampleCourse/questions/demo/symbolicProofBlocks/server.py
@@ -1,15 +1,19 @@
 plob = __import__("pl-order-blocks")
 
 tag_to_boxes = {
-    '2': ['closed_form'],
-    '3': ['base_right', 'base_left']
+    "2": ["closed_form"],
+    "3": ["base_right", "base_left"],
+    "4": ["inductive_step"],
 }
 
+
 def generate(data):
-    data['correct_answers']['closed_form'] = 'n / (n + 1)'
-    data['correct_answers']['base_left'] = '1/2'
-    data['correct_answers']['base_right'] = '1/2'
+    data["correct_answers"]["closed_form"] = "n / (n + 1)"
+    data["correct_answers"]["base_left"] = "1/2"
+    data["correct_answers"]["base_right"] = "1/2"
+    data["correct_answers"]["inductive_step"] = "(k + 1) / (k + 2)"
+
 
 def grade(data):
     # print(data['partial_scores'])
-    plob.grade('sum-closed-form', data, tag_to_boxes)
+    plob.grade("sum-closed-form", data, tag_to_boxes)
