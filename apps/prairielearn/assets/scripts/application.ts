41ed37327dfd9abab23347c7d191c067ff37c3e6
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-06-21 09:12:16 -0700

    Automatically fix input groups

--- apps/prairielearn/assets/scripts/application.ts
+++ apps/prairielearn/assets/scripts/application.ts
@@ -1,10 +1,49 @@
-const BOOTSTRAP_LEGACY_ATTRIBUTES = ['toggle', 'target', 'html', 'placement', 'content', 'trigger'];
+import { observe } from 'selector-observer';
 
-$(() => {
-  console.log('application javascript');
+import { onDocumentReady } from '@prairielearn/browser-utils';
+
+const BOOTSTRAP_LEGACY_ATTRIBUTES = [
+  'toggle',
+  'target',
+  'html',
+  'placement',
+  'content',
+  'trigger',
+  'dismiss',
+];
+
+onDocumentReady(() => {
   BOOTSTRAP_LEGACY_ATTRIBUTES.forEach((attr) => {
     $(`[data-${attr}]`).each((i, el) => {
-      el.setAttribute(`data-bs-${attr}`, el.dataset[attr]);
+      const val = el.dataset[attr];
+      if (val) {
+        el.setAttribute(`data-bs-${attr}`, val);
+      }
     });
+  });
+
+  // In Bootstrap 5, elements can be added as direct children of `.input-group`
+  // without using a wrapping `.input-group-prepend` or `.input-group-append`.
+  // This bit of JavaScript will re-parent the children of `.input-group-*` into
+  // the containing `.input-group` element.
+  observe('.input-group-prepend, .input-group-append', {
+    add(el) {
+      if (!(el instanceof HTMLElement)) return;
+      if (!el.parentElement?.classList.contains('input-group')) return;
+
+      for (const child of Array.from(el.children)) {
+        el.parentElement?.insertBefore(child, el);
+      }
+      el.remove();
+
+      const elementClass = el.classList.contains('input-group-prepend')
+        ? 'input-group-prepend'
+        : 'input-group-append';
+
+      console.warn(
+        `Bootstrap 5 no longer requires ${elementClass} elements to be wrapped in an input-group. Please update your HTML to remove the wrapping ${elementClass} element.`,
+        el,
+      );
+    },
   });
 });
