567a2c78f508bf8a6ae384c1e6c30ab9492ee9cf
Jonatan Schroeder <jonatan@yorku.ca>
2024-06-28 12:04:14 -0400

    Convert markdown processing to a package

--- packages/markdown/src/index.test.ts
+++ packages/markdown/src/index.test.ts
@@ -1,14 +1,10 @@
 import { assert } from 'chai';
 
-import * as markdown from './markdown.js';
+import * as markdown from './index.js';
 
-async function testMarkdownQuestion(question, expected) {
-  const actual = markdown.processQuestion(question);
-  assert.equal(actual?.trim(), expected);
-}
-
-async function testMarkdown(original, expected) {
-  await testMarkdownQuestion(`<markdown>\n${original}\n</markdown>`, expected);
+async function testMarkdown(original: string, expected: string) {
+  const actual = await markdown.markdownToHtml(original);
+  assert.equal(actual.toString().trim(), expected);
 }
 
 describe('Markdown processing', () => {
@@ -16,54 +12,6 @@ describe('Markdown processing', () => {
     const question = '# Hello, world!\nThis **works**.';
     const expected = '<h1>Hello, world!</h1>\n<p>This <strong>works</strong>.</p>';
     await testMarkdown(question, expected);
-  });
-
-  it('handles multiple <markdown> tags', async () => {
-    const question = '<markdown>`nice`</markdown><markdown>`also nice`</markdown>';
-    const expected = '<p><code>nice</code></p><p><code>also nice</code></p>';
-    await testMarkdownQuestion(question, expected);
-  });
-
-  it('handles code blocks', async () => {
-    const question = '<markdown>\n```\nint a = 12;\n```\n</markdown>';
-    const expected = '<pl-code>int a = 12;</pl-code>';
-    await testMarkdownQuestion(question, expected);
-  });
-
-  it('handles code blocks with language', async () => {
-    const question = '<markdown>\n```cpp\nint a = 12;\n```\n</markdown>';
-    const expected = '<pl-code language="cpp">int a = 12;</pl-code>';
-    await testMarkdownQuestion(question, expected);
-  });
-
-  it('handles code blocks with highlighted lines', async () => {
-    const question = '<markdown>\n```{1,2-3}\nint a = 12;\n```\n</markdown>';
-    const expected = '<pl-code highlight-lines="1,2-3">int a = 12;</pl-code>';
-    await testMarkdownQuestion(question, expected);
-  });
-
-  it('handles code blocks with language and highlighted lines', async () => {
-    const question = '<markdown>\n```cpp{1,2-3}\nint a = 12;\n```\n</markdown>';
-    const expected = '<pl-code language="cpp" highlight-lines="1,2-3">int a = 12;</pl-code>';
-    await testMarkdownQuestion(question, expected);
-  });
-
-  it('handles escaped <markdown> tags', async () => {
-    const question = '<markdown>```html\n<markdown#></markdown#>\n```</markdown>';
-    const expected = '<pl-code language="html">&#x3C;markdown>&#x3C;/markdown></pl-code>';
-    await testMarkdownQuestion(question, expected);
-  });
-
-  it('handles weird escaped <markdown> tags', async () => {
-    const question = '<markdown>```html\n<markdown###></markdown###>\n```</markdown>';
-    const expected = '<pl-code language="html">&#x3C;markdown##>&#x3C;/markdown##></pl-code>';
-    await testMarkdownQuestion(question, expected);
-  });
-
-  it('handles empty <markdown> tags', async () => {
-    const question = 'before\n<markdown></markdown>\n*between*\n<markdown>`second`</markdown>';
-    const expected = 'before\n\n*between*\n<p><code>second</code></p>';
-    await testMarkdownQuestion(question, expected);
   });
 
   it('handles inline latex with underscores', async () => {
@@ -136,6 +84,24 @@ describe('Markdown processing', () => {
   it('handles HTML closing tags that do not open properly', async () => {
     const question = 'testing with </div><p>new line';
     const expected = '<p>testing with </p><p>new line</p>';
+    await testMarkdown(question, expected);
+  });
+
+  it('sanitizes HTML script tags', async () => {
+    const question = 'testing<script>alert("XSS")</script>';
+    const expected = '<p>testing</p>';
+    await testMarkdown(question, expected);
+  });
+
+  it('sanitizes HTML javascript event tags', async () => {
+    const question = 'testing<img src="x" onerror="alert(\'XSS\')">';
+    const expected = '<p>testing<img src="x"></p>';
+    await testMarkdown(question, expected);
+  });
+
+  it('sanitizes HTML javascript iframes', async () => {
+    const question = 'testing<iframe src="javascript:alert(\'delta\')"></iframe>';
+    const expected = '<p>testing</p>';
     await testMarkdown(question, expected);
   });
 });
