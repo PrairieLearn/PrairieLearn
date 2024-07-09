a41c8f068a837bf0d59945f7056e27d194c6c827
Jacob Levine <jacoblevine18@gmail.com>
2024-07-09 17:59:15 -0500

    add revision console + question saving

--- apps/prairielearn/src/ee/lib/aiQuestionGeneration.ts
+++ apps/prairielearn/src/ee/lib/aiQuestionGeneration.ts
@@ -3,20 +3,86 @@ import { OpenAI } from 'openai';
 import { loadSqlEquiv, queryRows } from '@prairielearn/postgres';
 
 import { QuestionGenerationContextEmbeddingSchema } from '../../lib/db-types.js';
-import { createServerJob } from '../../lib/server-jobs.js';
+import { ServerJob, createServerJob } from '../../lib/server-jobs.js';
 
 import { createEmbedding, openAiUserFromAuthn, vectorToString } from './contextEmbeddings.js';
 
 const sql = loadSqlEquiv(import.meta.url);
 
+function promptPreamble(context: string): string {
+  return `# Introduction
+        
+        You are an assistant that helps instructors write questions for PrairieLearn.
+        
+        A question has a \`question.html\` file that can contain standard HTML, CSS, and JavaScript. It also includes PrairieLearn elements like \`<pl-multiple-choice>\` and \`<pl-number-input>\`.
+        
+        A question may also have a \`server.py\` file that can randomly generate unique parameters and answers, and which can also assign grades to student submissions. \`server.py\` may be omitted if it's not necessary.
+        
+        ## Generating random parameters
+        
+        \`server.py\` may define a \`generate\` function. \`generate\` has a single parameter \`data\` which can be modified by reference. It has the following properties:
+        
+        - \`params\`: A dictionary. Random parameters, choices, etc. can be written here for later retrieval.
+        
+        ## Using random parameters
+        
+        Parameters can be read in \`question.html\` with Mustache syntax. For instance, if \`server.py\` contains \`data["params"]["answer"]\`, it can be read with \`{{ params.answer }}\` in \`question.html\`.
+        
+        # Context
+        
+        Here is some context that may help you respond to the user. This context may include example questions, documentation, or other information that may be helpful.
+        
+        ${context}
+        
+        `;
+}
+
+async function makeContext(client: OpenAI, prompt: string, authnUserId): Promise<string> {
+  const embedding = await createEmbedding(client, prompt, openAiUserFromAuthn(authnUserId));
+
+  const docs = await queryRows(
+    sql.select_nearby_documents,
+    { embedding: vectorToString(embedding), limit: 5 },
+    QuestionGenerationContextEmbeddingSchema,
+  );
+
+  const contextDocs = docs.map((doc) => doc.doc_text);
+  const context = contextDocs.join('\n\n');
+  return context;
+}
+
+function extractFromCompletion(completion, job: ServerJob): void {
+  const completionText = completion.choices[0].message.content;
+
+  job.info(`completion is ${completionText}`);
+
+  job.info(`used ${completion?.usage?.total_tokens} OpenAI tokens to generate response.`);
+
+  const pythonSelector = /```python\n(?<code>([^`]|`[^`]|``[^`]|\n)*)```/;
+  const htmlSelector = /```html\n(?<code>([^`]|`[^`]|``[^`]|\n)*)```/;
+
+  const html = completionText?.match(htmlSelector)?.groups?.code;
+  const python = completionText?.match(pythonSelector)?.groups?.code;
+
+  if (html !== undefined) {
+    job.info(`extracted html file: ${html}`);
+    job.data['html'] = html;
+  }
+
+  if (python !== undefined) {
+    job.info(`extracted python file: ${python}`);
+    job.data['python'] = python;
+  }
+}
+
 /**
- * Generates the HTML and Python code for a new question using an LLM
+ * Generates the HTML and Python code for a new question using an LLM.
  *
- * @param client the OpenAI client to use
- * @param courseId the ID of the current course
- * @param authnUserId the authenticated user's ID
- * @param prompt the prompt for how to generate a question
- * @returns a server job ID for the generation task
+ * @param client The OpenAI client to use.
+ * @param courseId The ID of the current course.
+ * @param authnUserId The authenticated user's ID.
+ * @param prompt The prompt for how to generate a question.
+ * @returns A server job ID for the generation task and a promise to return the associated saved data on completion.
  */
 export async function generateQuestion(
   client: OpenAI,
@@ -30,45 +96,13 @@ export async function generateQuestion(
     description: 'Generate a question with AI',
   });
 
-  serverJob.executeInBackground(async (job) => {
+  const jobPromise = serverJob.execute(async (job) => {
     job.info(`prompt is ${prompt}`);
-    const embedding = await createEmbedding(client, prompt, openAiUserFromAuthn(authnUserId));
-    job.info(embedding.toString());
 
-    const docs = await queryRows(
-      sql.select_nearby_documents,
-      { embedding: vectorToString(embedding), limit: 5 },
-      QuestionGenerationContextEmbeddingSchema,
-    );
-
-    const contextDocs = docs.map((doc) => doc.doc_text);
-    const context = contextDocs.join('\n\n');
+    const context = await makeContext(client, prompt, authnUserId);
 
     const sysPrompt = `
-# Introduction
-
-You are an assistant that helps instructors write questions for PrairieLearn.
-
-A question has a \`question.html\` file that can contain standard HTML, CSS, and JavaScript. It also includes PrairieLearn elements like \`<pl-multiple-choice>\` and \`<pl-number-input>\`.
-
-A question may also have a \`server.py\` file that can randomly generate unique parameters and answers, and which can also assign grades to student submissions. \`server.py\` may be omitted if it's not necessary.
-
-## Generating random parameters
-
-\`server.py\` may define a \`generate\` function. \`generate\` has a single parameter \`data\` which can be modified by reference. It has the following properties:
-
-- \`params\`: A dictionary. Random parameters, choices, etc. can be written here for later retrieval.
-
-## Using random parameters
-
-Parameters can be read in \`question.html\` with Mustache syntax. For instance, if \`server.py\` contains \`data["params"]["answer"]\`, it can be read with \`{{ params.answer }}\` in \`question.html\`.
-
-# Context
-
-Here is some context that may help you respond to the user. This context may include example questions, documentation, or other information that may be helpful.
-
-${context}
-
+${promptPreamble(context)}
 # Prompt
 
 A user will now request your help in creating a question. Respond in a friendly but concise way. Include \`question.html\` and \`server.py\` in Markdown code fences in your response, and tag each code fence with the language (either \`html\` or \`python\`). Omit \`server.py\` if the question does not require it (for instance, if the question does not require randomization).
@@ -87,14 +121,98 @@ Keep in mind you are not just generating an example; you are generating an actua
       user: openAiUserFromAuthn(authnUserId),
     });
 
-    const completionText = completion.choices[0].message.content;
-
-    job.info(`completion is ${completionText}`);
-
-    job.info(`used ${completion?.usage?.total_tokens} OpenAI tokens to generate response.`);
-
-    // TODO: Process generated documents
+    extractFromCompletion(completion, job);
+    job.data['prompt'] = prompt;
+    job.data['generation'] = completion.choices[0].message.content;
+    job.data['context'] = context;
   });
 
-  return serverJob.jobSequenceId;
+  return { jobSequenceId: serverJob.jobSequenceId, completionPromise: jobPromise };
+}
+
+/**
+ * Revises a question using the LLM based on user input.
+ *
+ * @param client The OpenAI client to use.
+ * @param courseId The ID of the current course.
+ * @param authnUserId The authenticated user's ID.
+ * @param originalPrompt The prompt creating the original generation.
+ * @param revisionPrompt A prompt with user instructions on how to revise the question.
+ * @param originalHTML The question.html file to revise.
+ * @param originalPython The server.py file to revise.
+ * @returns A server job ID for the generation task and a promise to return the associated saved data on completion.
+ */
+export async function regenerateQuestion(
+  client: OpenAI,
+  courseId: string | undefined,
+  authnUserId: string,
+  originalPrompt: string,
+  revisionPrompt: string,
+  originalHTML: string,
+  originalPython: string,
+) {
+  const serverJob = await createServerJob({
+    courseId,
+    type: 'llm_question_regen',
+    description: 'Revise a question using the LLM',
+  });
+
+  const jobPromise = serverJob.execute(async (job) => {
+    job.info(`prompt is ${revisionPrompt}`);
+
+    const context = await makeContext(client, originalPrompt, authnUserId);
+    const sysPrompt = `
+        ${promptPreamble(context)}
+        # Previous Generations
+
+        A user previously used the assistant to generate a question with following prompt:
+
+        ${originalPrompt}
+
+        You generated the following:
+
+       ${
+         originalHTML === undefined
+           ? ''
+           : `\`\`\`html
+        ${originalHTML}
+        \`\`\``
+       }
+
+      ${
+        originalPython === undefined
+          ? ''
+          : `\`\`\`python
+        ${originalPython}
+        \`\`\``
+      }
+        
+        # Prompt
+        
+        A user will now request your help in in revising the question that you generated. Respond in a friendly but concise way. Include \`question.html\` and \`server.py\` in Markdown code fences in your response, and tag each code fence with the language (either \`html\` or \`python\`). Omit \`server.py\` if the question does not require it (for instance, if the question does not require randomization).
+        
+        Keep in mind you are not just generating an example; you are generating an actual question that the user will use directly.
+        `;
+
+    job.info(`system prompt is: ${sysPrompt}`);
+
+    //TODO [very important]: normalize to prevent prompt injection attacks
+
+    const completion = await client.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: sysPrompt },
+        { role: 'user', content: revisionPrompt },
+      ],
+      user: openAiUserFromAuthn(authnUserId),
+    });
+
+    extractFromCompletion(completion, job);
+    job.data['prompt'] = revisionPrompt;
+    job.data['originalPrompt'] = originalPrompt;
+    job.data['generation'] = completion.choices[0].message.content;
+    job.data['context'] = context;
+  });
+
+  return { jobSequenceId: serverJob.jobSequenceId, completionPromise: jobPromise };
 }
--- apps/prairielearn/src/ee/pages/instructorAiGenerateQuestion/instructorAiGenerateQuestion.ts
+++ apps/prairielearn/src/ee/pages/instructorAiGenerateQuestion/instructorAiGenerateQuestion.ts
@@ -3,15 +3,63 @@ import asyncHandler from 'express-async-handler';
 import { OpenAI } from 'openai';
 
 import * as error from '@prairielearn/error';
+import { loadSqlEquiv, queryRow } from '@prairielearn/postgres';
 
 import { config } from '../../../lib/config.js';
+import { QuestionSchema, IdSchema } from '../../../lib/db-types.js';
+import { QuestionAddEditor } from '../../../lib/editors.js';
 import { features } from '../../../lib/features/index.js';
-import { generateQuestion } from '../../lib/aiQuestionGeneration.js';
+import { idsEqual } from '../../../lib/id.js';
+import { selectJobsByJobSequenceId } from '../../../lib/server-jobs.js';
+import { generateQuestion, regenerateQuestion } from '../../lib/aiQuestionGeneration.js';
 import { syncContextDocuments } from '../../lib/contextEmbeddings.js';
 
-import { AiGeneratePage } from './instructorAiGenerateQuestion.html.js';
+import { AiGeneratePage, GenerationResults } from './instructorAiGenerateQuestion.html.js';
+import { z } from 'zod';
+
+type IdType = z.infer<typeof IdSchema>
 
 const router = express.Router();
+
+const sql = loadSqlEquiv(import.meta.url);
+
+export async function saveGeneratedQuestion(
+  res,
+  htmlFileContents: string | undefined,
+  pythonFileContents: string | undefined,
+): Promise <IdType | undefined> {
+  const files = {};
+
+  if (htmlFileContents) {
+    files['question.html'] = htmlFileContents;
+  }
+
+  if (pythonFileContents) {
+    files['server.py'] = pythonFileContents;
+  }
+
+  const editor = new QuestionAddEditor({
+    locals: res.locals,
+    files,
+  });
+
+  const serverJob = await editor.prepareServerJob();
+
+  try {
+    await editor.executeWithServerJob(serverJob);
+  } catch (err) {
+    res.redirect(res.locals.urlPrefix + '/edit_error/' + serverJob.jobSequenceId);
+    return;
+  }
+
+  const result = await queryRow(
+    sql.select_added_question,
+    { uuid: editor.uuid, course_id: res.locals.course.id.toString() },
+    QuestionSchema,
+  );
+
+  return result.id;
+}
 
 function assertCanCreateQuestion(resLocals: Record<string, any>) {
   // Do not allow users to edit without permission
@@ -52,22 +100,84 @@ router.post(
     if (!config.openAiApiKey || !config.openAiOrganization) {
       throw new error.HttpStatusError(403, 'Not implemented (feature not available)');
     }
+
     const client = new OpenAI({
       apiKey: config.openAiApiKey,
       organization: config.openAiOrganization,
     });
 
-    if (req.body.__action === 'generate_question') {
-      const jobSequenceId = await generateQuestion(
+    if (req.body.__action === 'ai_generate') {
+      const jobSeq = await generateQuestion(
         client,
         res.locals.course ? res.locals.course.id : undefined,
         res.locals.authn_user.user_id,
         req.body.prompt,
       );
-      res.redirect(res.locals.urlPrefix + '/jobSequence/' + jobSequenceId);
+
+      const data = await jobSeq.completionPromise;
+      if (data) {
+        res.send(
+          GenerationResults(data.data.html, data.data.python, jobSeq.jobSequenceId, res.locals),
+        );
+      } else {
+        throw new error.HttpStatusError(500, `Job Sequence ${jobSeq.jobSequenceId} failed.`);
+      }
     } else if (req.body.__action === 'sync_context_documents') {
       const jobSequenceId = await syncContextDocuments(client, res.locals.authn_user.user_id);
       res.redirect('/pl/jobSequence/' + jobSequenceId);
+    } else if (req.body.__action === 'ai_regen') {
+      const genJobs = await selectJobsByJobSequenceId(req.body.unsafe_sequence_job_id);
+      if (
+        genJobs.length !== 1 ||
+        !genJobs[0]?.course_id ||
+        !idsEqual(genJobs[0]?.course_id, res.locals.course.id)
+      ) {
+        throw new error.HttpStatusError(
+          403,
+          `Course for job sequence ${req.body.unsafe_sequence_job_id} does not match the current course.`,
+        );
+      }
+
+      const jobSeq = await regenerateQuestion(
+        client,
+        res.locals?.course?.course_id,
+        res.locals.authn_user.user_id,
+        genJobs[0]?.data?.prompt,
+        req.body.prompt,
+        genJobs[0]?.data?.html,
+        genJobs[0]?.data?.python,
+      );
+
+      const data = await jobSeq.completionPromise;
+      if (data) {
+        res.send(
+          GenerationResults(data.data.html, data.data.python, jobSeq.jobSequenceId, res.locals),
+        );
+      } else {
+        res.redirect('/pl/jobSequence/' + jobSeq.jobSequenceId);
+      }
+    } else if (req.body.__action === 'save_question') {
+      const genJobs = await selectJobsByJobSequenceId(req.body.unsafe_sequence_job_id);
+      if (
+        genJobs.length !== 1 ||
+        !genJobs[0]?.course_id ||
+        !idsEqual(genJobs[0]?.course_id, res.locals.course.id)
+      ) {
+        throw new error.HttpStatusError(
+          403,
+          `Course for job sequence ${req.body.unsafe_sequence_job_id} does not match the current course.`,
+        );
+      }
+
+      const qid = await saveGeneratedQuestion(
+        res,
+        genJobs[0]?.data?.html,
+        genJobs[0]?.data?.python,
+      );
+
+      if (qid){
+        res.redirect(res.locals.urlPrefix + '/question/' + qid + '/settings');
+      }
     } else {
       throw new error.HttpStatusError(400, `Unknown action: ${req.body.__action}`);
     }
