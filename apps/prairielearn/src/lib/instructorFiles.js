b187265838f1be7aefac4796704dcfc1119dc131
Jonatan Schroeder <jonatan@yorku.ca>
2024-07-10 11:40:15 -0400

    Add type annotations to instructor files library

--- apps/prairielearn/src/lib/instructorFiles.js
+++ apps/prairielearn/src/lib/instructorFiles.js
@@ -16,126 +16,135 @@ import { encodePath } from './uri-util.js';
  * @param {import('express').Response} res
  */
 export function getPaths(req, res) {
-  let paths = {
-    coursePath: res.locals.course.path,
-    courseId: res.locals.course.id,
-  };
+  /** @type {string} */
+  const coursePath = res.locals.course.path;
+  /** @type {string} */
+  const courseId = res.locals.course.id;
 
+  /** @type {string} */
+  let rootPath;
+  /** @type {string[]} */
+  let invalidRootPaths;
+  /** @type {string[]} */
+  let cannotMove;
+  /** @type {string} */
+  let clientDir;
+  /** @type {string} */
+  let serverDir;
+  /** @type {string|undefined} */
+  let testsDir;
+  /** @type {string} */
+  let urlPrefix;
   if (res.locals.navPage === 'course_admin') {
-    paths.rootPath = res.locals.course.path;
-    paths.invalidRootPaths = [
-      path.join(paths.rootPath, 'questions'),
-      path.join(paths.rootPath, 'courseInstances'),
-    ];
-    paths.cannotMove = [path.join(paths.rootPath, 'infoCourse.json')];
-    paths.clientDir = path.join(paths.rootPath, 'clientFilesCourse');
-    paths.serverDir = path.join(paths.rootPath, 'serverFilesCourse');
-    paths.urlPrefix = `${res.locals.urlPrefix}/course_admin`;
+    rootPath = res.locals.course.path;
+    invalidRootPaths = [path.join(rootPath, 'questions'), path.join(rootPath, 'courseInstances')];
+    cannotMove = [path.join(rootPath, 'infoCourse.json')];
+    clientDir = path.join(rootPath, 'clientFilesCourse');
+    serverDir = path.join(rootPath, 'serverFilesCourse');
+    urlPrefix = `${res.locals.urlPrefix}/course_admin`;
   } else if (res.locals.navPage === 'instance_admin') {
-    paths.rootPath = path.join(
+    rootPath = path.join(
       res.locals.course.path,
       'courseInstances',
       res.locals.course_instance.short_name,
     );
-    paths.invalidRootPaths = [path.join(paths.rootPath, 'assessments')];
-    paths.cannotMove = [path.join(paths.rootPath, 'infoCourseInstance.json')];
-    paths.clientDir = path.join(paths.rootPath, 'clientFilesCourseInstance');
-    paths.serverDir = path.join(paths.rootPath, 'serverFilesCourseInstance');
-    paths.urlPrefix = `${res.locals.urlPrefix}/instance_admin`;
+    invalidRootPaths = [path.join(rootPath, 'assessments')];
+    cannotMove = [path.join(rootPath, 'infoCourseInstance.json')];
+    clientDir = path.join(rootPath, 'clientFilesCourseInstance');
+    serverDir = path.join(rootPath, 'serverFilesCourseInstance');
+    urlPrefix = `${res.locals.urlPrefix}/instance_admin`;
   } else if (res.locals.navPage === 'assessment') {
-    paths.rootPath = path.join(
+    rootPath = path.join(
       res.locals.course.path,
       'courseInstances',
       res.locals.course_instance.short_name,
       'assessments',
       res.locals.assessment.tid,
     );
-    paths.invalidRootPaths = [];
-    paths.cannotMove = [path.join(paths.rootPath, 'infoAssessment.json')];
-    paths.clientDir = path.join(paths.rootPath, 'clientFilesAssessment');
-    paths.serverDir = path.join(paths.rootPath, 'serverFilesAssessment');
-    paths.urlPrefix = `${res.locals.urlPrefix}/assessment/${res.locals.assessment.id}`;
+    invalidRootPaths = [];
+    cannotMove = [path.join(rootPath, 'infoAssessment.json')];
+    clientDir = path.join(rootPath, 'clientFilesAssessment');
+    serverDir = path.join(rootPath, 'serverFilesAssessment');
+    urlPrefix = `${res.locals.urlPrefix}/assessment/${res.locals.assessment.id}`;
   } else if (res.locals.navPage === 'question') {
-    paths.rootPath = path.join(res.locals.course.path, 'questions', res.locals.question.qid);
-    paths.invalidRootPaths = [];
-    paths.cannotMove = [path.join(paths.rootPath, 'info.json')];
-    paths.clientDir = path.join(paths.rootPath, 'clientFilesQuestion');
-    paths.serverDir = path.join(paths.rootPath, 'serverFilesQuestion');
-    paths.testsDir = path.join(paths.rootPath, 'tests');
-    paths.urlPrefix = `${res.locals.urlPrefix}/question/${res.locals.question.id}`;
+    rootPath = path.join(res.locals.course.path, 'questions', res.locals.question.qid);
+    invalidRootPaths = [];
+    cannotMove = [path.join(rootPath, 'info.json')];
+    clientDir = path.join(rootPath, 'clientFilesQuestion');
+    serverDir = path.join(rootPath, 'serverFilesQuestion');
+    testsDir = path.join(rootPath, 'tests');
+    urlPrefix = `${res.locals.urlPrefix}/question/${res.locals.question.id}`;
   } else {
     throw new Error(`Invalid navPage: ${res.locals.navPage}`);
   }
 
+  let workingPath = rootPath;
   if (req.params[0]) {
     try {
-      paths.workingPath = path.join(res.locals.course.path, req.params[0]);
+      workingPath = path.join(res.locals.course.path, req.params[0]);
     } catch (err) {
       throw new Error(`Invalid path: ${req.params[0]}`);
     }
-  } else {
-    paths.workingPath = paths.rootPath;
   }
-  paths.workingPathRelativeToCourse = path.relative(res.locals.course.path, paths.workingPath);
-  paths.workingDirectory = path.dirname(paths.workingPathRelativeToCourse);
-  paths.workingFilename = path.basename(paths.workingPathRelativeToCourse);
+  const workingPathRelativeToCourse = path.relative(res.locals.course.path, workingPath);
+  const workingDirectory = path.dirname(workingPathRelativeToCourse);
+  const workingFilename = path.basename(workingPathRelativeToCourse);
 
-  if (paths.workingPath === paths.rootPath) {
-    paths.specialDirs = [];
-    if (paths.clientDir) {
-      paths.specialDirs.push({
-        label: 'Client',
-        path: paths.clientDir,
-        info: `This file will be placed in the subdirectory <code>${path.basename(
-          paths.clientDir,
-        )}</code> and will be accessible from the student's web browser.`,
-      });
-    }
-    if (paths.serverDir) {
-      paths.specialDirs.push({
-        label: 'Server',
-        path: paths.serverDir,
-        info: `This file will be placed in the subdirectory <code>${path.basename(
-          paths.serverDir,
-        )}</code> and will be accessible only from the server. It will not be accessible from the student's web browser.`,
-      });
-    }
-    if (paths.testsDir) {
-      paths.specialDirs.push({
-        label: 'Test',
-        path: paths.testsDir,
-        info: `This file will be placed in the subdirectory <code>${path.basename(
-          paths.testsDir,
-        )}</code> and will be accessible only from the server. It will not be accessible from the student's web browser. This is appropriate for code to support <a href='https://prairielearn.readthedocs.io/en/latest/externalGrading/'>externally graded questions</a>.`,
-      });
-    }
+  const specialDirs =
+    workingPath === rootPath
+      ? [
+          {
+            label: 'Client',
+            path: clientDir,
+            info: html`This file will be placed in the subdirectory
+              <code>${path.basename(clientDir)}</code> and will be accessible from the student's web
+              browser.`,
+          },
+          {
+            label: 'Server',
+            path: serverDir,
+            info: html`This file will be placed in the subdirectory
+              <code>${path.basename(serverDir)}</code> and will be accessible only from the server.
+              It will not be accessible from the student's web browser.`,
+          },
+        ]
+      : [];
+  if (workingPath === rootPath && testsDir) {
+    specialDirs.push({
+      label: 'Test',
+      path: testsDir,
+      info: html`This file will be placed in the subdirectory
+        <code>${path.basename(testsDir)}</code> and will be accessible only from the server. It will
+        not be accessible from the student's web browser. This is appropriate for code to support
+        <a href="https://prairielearn.readthedocs.io/en/latest/externalGrading/">
+          externally graded questions</a
+        >.`,
+    });
   }
 
-  if (!contains(paths.rootPath, paths.workingPath)) {
+  if (!contains(rootPath, workingPath)) {
     throw new error.AugmentedError('Invalid working directory', {
       info: html`
         <p>The working directory</p>
         <div class="container">
-          <pre class="bg-dark text-white rounded p-2">${paths.workingPath}</pre>
+          <pre class="bg-dark text-white rounded p-2">${workingPath}</pre>
         </div>
         <p>must be inside the root directory</p>
         <div class="container">
-          <pre class="bg-dark text-white rounded p-2">${paths.rootPath}</pre>
+          <pre class="bg-dark text-white rounded p-2">${rootPath}</pre>
         </div>
         <p>when looking at <code>${res.locals.navPage}</code> files.</p>
       `,
     });
   }
 
-  const found = paths.invalidRootPaths.find((invalidRootPath) =>
-    contains(invalidRootPath, paths.workingPath),
-  );
+  const found = invalidRootPaths.find((invalidRootPath) => contains(invalidRootPath, workingPath));
   if (found) {
     throw new error.AugmentedError('Invalid working directory', {
       info: html`
         <p>The working directory</p>
         <div class="container">
-          <pre class="bg-dark text-white rounded p-2">${paths.workingPath}</pre>
+          <pre class="bg-dark text-white rounded p-2">${workingPath}</pre>
         </div>
         <p>must <em>not</em> be inside the directory</p>
         <div class="container"><pre class="bg-dark text-white rounded p-2">${found}</pre></div>
@@ -145,28 +154,43 @@ export function getPaths(req, res) {
   }
 
   let curPath = res.locals.course.path;
-  paths.branch = [
+  const branch = [
     {
       name: path.basename(curPath),
       path: path.relative(res.locals.course.path, curPath),
-      canView: contains(paths.rootPath, curPath),
+      canView: contains(rootPath, curPath),
       encodedPath: encodePath(path.relative(res.locals.course.path, curPath)),
     },
-  ];
-  path
-    .relative(res.locals.course.path, paths.workingPath)
-    .split(path.sep)
-    .forEach((dir) => {
-      if (dir) {
+    ...path
+      .relative(res.locals.course.path, workingPath)
+      .split(path.sep)
+      .filter((dir) => dir)
+      .map((dir) => {
         curPath = path.join(curPath, dir);
-        paths.branch.push({
+        return {
           name: path.basename(curPath),
           path: path.relative(res.locals.course.path, curPath),
-          canView: contains(paths.rootPath, curPath),
+          canView: contains(rootPath, curPath),
           encodedPath: encodePath(path.relative(res.locals.course.path, curPath)),
-        });
-      }
-    });
+        };
+      }),
+  ];
 
-  return paths;
+  return {
+    coursePath,
+    courseId,
+    rootPath,
+    invalidRootPaths,
+    cannotMove,
+    clientDir,
+    serverDir,
+    testsDir,
+    urlPrefix,
+    workingPath,
+    workingPathRelativeToCourse,
+    workingDirectory,
+    workingFilename,
+    specialDirs,
+    branch,
+  };
 }
