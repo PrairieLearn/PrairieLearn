54528ecda6095a6246aa5d24a773912b2b1cdad2
Jonatan Schroeder <jonatan@yorku.ca>
2024-07-11 13:22:26 -0400

    Loosen CSP restrictions for embeddable PDF files

--- apps/prairielearn/src/middlewares/content-security-policy.ts
+++ apps/prairielearn/src/middlewares/content-security-policy.ts
@@ -12,6 +12,17 @@ router.all('/*', function (req, res, next) {
   // Added for backwards compatibility with older browsers.
   res.header('X-Frame-Options', 'DENY');
 
+  const originalResType: typeof res.type = res.type.bind(res);
+  res.type = (type) => {
+    if (type === 'application/pdf') {
+      // Loosen CSP restrictions for PDF files, as they can cause issues if the file
+      // is embedded, and are not expected to cause issues with XSS.
+      res.header('Content-Security-Policy', "frame-ancestors 'self';");
+      res.header('X-Frame-Options', 'SAMEORIGIN');
+    }
+    return originalResType(type);
+  };
+
   next();
 });
 
