d409308b4e4aa8af8a1ff37726918caca0d26fc7
Jonatan Schroeder <jonatan@yorku.ca>
2024-07-15 17:35:15 -0400

    Remove unnecessary awaits

--- apps/prairielearn/src/lib/markdown.test.ts
+++ apps/prairielearn/src/lib/markdown.test.ts
@@ -11,54 +11,54 @@ describe('Markdown processing', () => {
   it('renders basic markdown correctly', async () => {
     const question = '<markdown>\n# Hello, world!\nThis **works**.\n</markdown>';
     const expected = '<h1>Hello, world!</h1>\n<p>This <strong>works</strong>.</p>';
-    await testMarkdownQuestion(question, expected);
+    testMarkdownQuestion(question, expected);
   });
 
   it('handles multiple <markdown> tags', async () => {
     const question = '<markdown>`nice`</markdown><markdown>`also nice`</markdown>';
     const expected = '<p><code>nice</code></p><p><code>also nice</code></p>';
-    await testMarkdownQuestion(question, expected);
+    testMarkdownQuestion(question, expected);
   });
 
   it('handles code blocks', async () => {
     const question = '<markdown>\n```\nint a = 12;\n```\n</markdown>';
     const expected = '<pl-code>int a = 12;</pl-code>';
-    await testMarkdownQuestion(question, expected);
+    testMarkdownQuestion(question, expected);
   });
 
   it('handles code blocks with language', async () => {
     const question = '<markdown>\n```cpp\nint a = 12;\n```\n</markdown>';
     const expected = '<pl-code language="cpp">int a = 12;</pl-code>';
-    await testMarkdownQuestion(question, expected);
+    testMarkdownQuestion(question, expected);
   });
 
   it('handles code blocks with highlighted lines', async () => {
     const question = '<markdown>\n```{1,2-3}\nint a = 12;\n```\n</markdown>';
     const expected = '<pl-code highlight-lines="1,2-3">int a = 12;</pl-code>';
-    await testMarkdownQuestion(question, expected);
+    testMarkdownQuestion(question, expected);
   });
 
   it('handles code blocks with language and highlighted lines', async () => {
     const question = '<markdown>\n```cpp{1,2-3}\nint a = 12;\n```\n</markdown>';
     const expected = '<pl-code language="cpp" highlight-lines="1,2-3">int a = 12;</pl-code>';
-    await testMarkdownQuestion(question, expected);
+    testMarkdownQuestion(question, expected);
   });
 
   it('handles escaped <markdown> tags', async () => {
     const question = '<markdown>```html\n<markdown#></markdown#>\n```</markdown>';
     const expected = '<pl-code language="html">&#x3C;markdown>&#x3C;/markdown></pl-code>';
-    await testMarkdownQuestion(question, expected);
+    testMarkdownQuestion(question, expected);
   });
 
   it('handles weird escaped <markdown> tags', async () => {
     const question = '<markdown>```html\n<markdown###></markdown###>\n```</markdown>';
     const expected = '<pl-code language="html">&#x3C;markdown##>&#x3C;/markdown##></pl-code>';
-    await testMarkdownQuestion(question, expected);
+    testMarkdownQuestion(question, expected);
   });
 
   it('handles empty <markdown> tags', async () => {
     const question = 'before\n<markdown></markdown>\n*between*\n<markdown>`second`</markdown>';
     const expected = 'before\n\n*between*\n<p><code>second</code></p>';
-    await testMarkdownQuestion(question, expected);
+    testMarkdownQuestion(question, expected);
   });
 });
