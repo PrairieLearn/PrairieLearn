cb9680e29c6bd0e79001eaf3209d65902450caee
Jonatan Schroeder <jonatan@yorku.ca>
2024-07-15 14:32:21 -0400

    Change implementation to use on-headers

--- apps/prairielearn/src/middlewares/content-security-policy.ts
+++ apps/prairielearn/src/middlewares/content-security-policy.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import onHeaders from 'on-headers';
 
 const router = Router();
 
@@ -12,16 +13,14 @@ router.all('/*', function (req, res, next) {
   // Added for backwards compatibility with older browsers.
   res.header('X-Frame-Options', 'DENY');
 
-  const originalResType: typeof res.type = res.type.bind(res);
-  res.type = (type) => {
-    if (type === 'application/pdf') {
+  onHeaders(res, function () {
+    if (this.getHeader('Content-Type') === 'application/pdf') {
       // Loosen CSP restrictions for PDF files, as they can cause issues if the file
       // is embedded, and are not expected to cause issues with XSS.
       res.header('Content-Security-Policy', "frame-ancestors 'self';");
       res.header('X-Frame-Options', 'SAMEORIGIN');
     }
-    return originalResType(type);
-  };
+  });
 
   next();
 });
