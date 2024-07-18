16207034d18f63b88a64184f1939a9d21550bd8b
Jonatan Schroeder <jonatan@yorku.ca>
2024-07-18 15:11:48 -0400

    Use new component in file editor page

--- apps/prairielearn/src/pages/instructorFileEditor/instructorFileEditor.html.ts
+++ apps/prairielearn/src/pages/instructorFileEditor/instructorFileEditor.html.ts
@@ -1,6 +1,7 @@
 import { html, joinHtml, unsafeHtml } from '@prairielearn/html';
 import { renderEjs } from '@prairielearn/html-ejs';
 
+import { HeadContents } from '../../components/HeadContents.html.js';
 import { compiledScriptTag, nodeModulesAssetPath } from '../../lib/assets.js';
 import { config } from '../../lib/config.js';
 import { encodePath } from '../../lib/uri-util.js';
@@ -15,10 +16,7 @@ export function InstructorFileEditor({ resLocals }: { resLocals: Record<string, 
           name="ace-base-path"
           content="${nodeModulesAssetPath('ace-builds/src-min-noconflict/')}"
         />
-        ${renderEjs(import.meta.url, "<%- include('../partials/head'); %>", {
-          ...resLocals,
-          pageTitle: `Edit ${fileEdit?.fileName}`,
-        })}
+        ${HeadContents({ resLocals, pageTitle: `Edit ${fileEdit?.fileName}` })}
         ${compiledScriptTag('instructorFileEditorClient.ts')}
       </head>
 
