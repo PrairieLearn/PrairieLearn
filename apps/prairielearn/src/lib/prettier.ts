31cd682fd15152fa432549d37366eb6ff9067513
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-07-22 14:20:24 -0700

    Improve formatting algorithm

--- apps/prairielearn/src/lib/prettier.ts
+++ apps/prairielearn/src/lib/prettier.ts
@@ -9,6 +9,12 @@ export async function formatJsonWithPrettier(json: string): Promise<string> {
   const prettierBabelPlugin = await import('prettier/plugins/babel');
   const prettierEstreePlugin = await import('prettier/plugins/estree');
 
+  // We round-trip the JSON through the JS parser/stringifier to ensure that
+  // objects always start on a new line. This ensures Prettier doesn't collapse
+  // everything onto one line:
+  // https://prettier.io/docs/en/rationale.html#multi-line-objects
+  json = JSON.stringify(JSON.parse(json), null, 2);
+
   return prettier.format(json, {
     parser: 'json',
     plugins: [
