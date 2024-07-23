6fa6976d117f2707d069f82d3e2c7d7ab0e10b5e
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-07-23 13:10:44 -0700

    Add documentation

--- apps/prairielearn/src/migrations/20240718221554_enum_mode_reason__create.sql
+++ apps/prairielearn/src/migrations/20240718221554_enum_mode_reason__create.sql
@@ -1,1 +1,8 @@
-CREATE TYPE enum_mode_reason AS ENUM('Default', 'PrairieTest', 'Network');
+CREATE TYPE enum_mode_reason AS ENUM(
+  -- The user is in public mode.
+  'Default',
+  -- The user is in Exam mode because of a PrairieTest reservation.
+  'PrairieTest',
+  -- The user is in Exam mode because they're using a legacy exam mode network.
+  'Network'
+);
