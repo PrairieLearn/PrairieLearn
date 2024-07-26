bea0faa8f08d631700ade17eff1f8a499a62b093
Jonatan Schroeder <jonatan@yorku.ca>
2024-07-26 11:12:38 -0400

    Convert API files to TS

--- apps/prairielearn/src/api/v1/endpoints/courseInstanceInfo/index.ts
+++ apps/prairielearn/src/api/v1/endpoints/courseInstanceInfo/index.ts
@@ -1,16 +1,17 @@
-// @ts-check
+import * as path from 'node:path';
+
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
 
 import * as sqldb from '@prairielearn/postgres';
 
-const sql = sqldb.loadSqlEquiv(import.meta.url);
+const sql = sqldb.loadSql(path.join(import.meta.dirname, '..', 'queries.sql'));
 const router = Router({ mergeParams: true });
 
 router.get(
   '/',
   asyncHandler(async (req, res) => {
-    const result = await sqldb.queryOneRowAsync(sql.select_user_scores, {
+    const result = await sqldb.queryOneRowAsync(sql.select_course_instance_info, {
       course_instance_id: res.locals.course_instance.id,
     });
     res.status(200).send(result.rows[0].item);
--- apps/prairielearn/src/api/v1/prettyPrintJson.ts
+++ apps/prairielearn/src/api/v1/prettyPrintJson.ts
@@ -1,11 +1,12 @@
-// @ts-check
+import { NextFunction, Request, Response } from 'express';
 
-export default function (req, res, next) {
+export default function (req: Request, res: Response, next: NextFunction) {
   res.json = (body) => {
     if (!res.get('Content-Type')) {
       res.set('Content-Type', 'application/json');
     }
     res.send(JSON.stringify(body, null, 2));
+    return res;
   };
   next();
 }
