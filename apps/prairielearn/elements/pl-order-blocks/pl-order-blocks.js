3436e363bd58dbda192c3ad89a3b9a25304eb216
fowlinbasket <foul.in.basket@gmail.com>
2024-07-30 10:19:48 -0600

    Generalized selected keyboard controls

--- apps/prairielearn/elements/pl-order-blocks/pl-order-blocks.js
+++ apps/prairielearn/elements/pl-order-blocks/pl-order-blocks.js
@@ -155,35 +155,57 @@ window.PLOrderBlocks = function (uuid, options) {
               }
             });
             break;
-          case 'Delete':
-          case 'Backspace':
+          case 'ArrowLeft':
             handleKey(ev, block, () => {
               if (inDropzone(block)) {
-                block.style.marginLeft = '0px';
-              }
-              $(optionsElementId)[0].insertAdjacentElement('beforeend', block);
-              correctPairing(block);
-            });
-            break;
-          case 'Enter':
-            handleKey(ev, block, () => {
-              if (!inDropzone(block)) {
-                $(dropzoneElementId)[0].insertAdjacentElement('beforeend', block);
-              }
-            });
-            break;
-          case 'Tab':
-            handleKey(ev, block, () => {
-              if (inDropzone(block) && enableIndentation) {
                 const currentIndent = getIndentation(block);
-                if (ev.shiftKey) {
-                  setIndentation(block, currentIndent - 1);
+                if (currentIndent > 0) {
+                  setIndentation(block, getIndentation(block) - 1);
                 } else {
-                  setIndentation(block, currentIndent + 1);
+                  $(optionsElementId)[0].insertAdjacentElement('beforeend', block);
+                  correctPairing(block);
                 }
               }
             });
             break;
+          case 'ArrowRight':
+            handleKey(ev, block, () => {
+              if (!inDropzone(block)) {
+                $(dropzoneElementId)[0].insertAdjacentElement('beforeend', block);
+              } else if (enableIndentation) {
+                setIndentation(block, getIndentation(block) + 1);
+              }
+            });
+            break;
+          // case 'Delete':
+          // case 'Backspace':
+          //   handleKey(ev, block, () => {
+          //     if (inDropzone(block)) {
+          //       block.style.marginLeft = '0px';
+          //     }
+          //     $(optionsElementId)[0].insertAdjacentElement('beforeend', block);
+          //     correctPairing(block);
+          //   });
+          //   break;
+          // case 'Enter':
+          //   handleKey(ev, block, () => {
+          //     if (!inDropzone(block)) {
+          //       $(dropzoneElementId)[0].insertAdjacentElement('beforeend', block);
+          //     }
+          //   });
+          //   break;
+          // case 'Tab':
+          //   handleKey(ev, block, () => {
+          //     if (inDropzone(block) && enableIndentation) {
+          //       const currentIndent = getIndentation(block);
+          //       if (ev.shiftKey) {
+          //         setIndentation(block, currentIndent - 1);
+          //       } else {
+          //         setIndentation(block, currentIndent + 1);
+          //       }
+          //     }
+          //   });
+          //   break;
           case 'Escape':
             handleKey(ev, block, removeSelectedAttribute);
             break;
