6c257ee719ad9eb2b5f5fd80c3f6159b2135a005
Jonatan Schroeder <jonatan@yorku.ca>
2024-08-02 10:15:38 -0400

    Use Array.from second argument

--- apps/prairielearn/src/admin_queries/generate_uuids.ts
+++ apps/prairielearn/src/admin_queries/generate_uuids.ts
@@ -3,6 +3,6 @@ import { v4 as uuidv4 } from 'uuid';
 import type { AdministratorQueryResult } from './index.types.js';
 
 export default async function ({ count }: { count: string }): Promise<AdministratorQueryResult> {
-  const rows = Array.from(Array(Number(count))).map(() => ({ uuid: uuidv4() }));
+  const rows = Array.from(Array(Number(count)), () => ({ uuid: uuidv4() }));
   return { rows, columns: ['uuid'] };
 }
