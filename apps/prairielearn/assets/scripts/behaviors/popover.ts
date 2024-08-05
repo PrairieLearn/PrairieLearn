7251de35b985514484001316bbb3b080a2c0af64
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-08-05 15:57:08 -0700

    More popover work

--- apps/prairielearn/assets/scripts/behaviors/popover.ts
+++ apps/prairielearn/assets/scripts/behaviors/popover.ts
@@ -5,7 +5,11 @@ import { onDocumentReady } from '@prairielearn/browser-utils';
 onDocumentReady(() => {
   observe('[data-toggle="popover"]', {
     add(el) {
-      $(el).popover({ sanitize: false });
+      $(el)
+        .popover({ sanitize: false })
+        .on('shown.bs.popover', () => {
+          // TODO: automatically typeset MathJax if it's on the page.
+        });
     },
     remove(el) {
       $(el).popover('dispose');
