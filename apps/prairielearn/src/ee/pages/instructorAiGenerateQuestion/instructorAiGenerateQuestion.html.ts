8beb4ff10f70c6c873ab21dd9186b8392777ca1d
jlevine18 <jacoblevine18@gmail.com>
2024-08-13 16:52:33 -0500

    Allow loading of example question prompts (#10392)
    * prebuilts menu
    
    * styling
    
    * i want innerhtml so it displays
    
    * fix typos
    
    * Update apps/prairielearn/src/ee/pages/instructorAiGenerateQuestion/instructorAiGenerateQuestion.html.ts
    
    ---------
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>

--- apps/prairielearn/src/ee/pages/instructorAiGenerateQuestion/instructorAiGenerateQuestion.html.ts
+++ apps/prairielearn/src/ee/pages/instructorAiGenerateQuestion/instructorAiGenerateQuestion.html.ts
@@ -3,6 +3,29 @@ import { renderEjs } from '@prairielearn/html-ejs';
 
 import { HeadContents } from '../../../components/HeadContents.html.js';
 
+const examplePrompts = [
+  {
+    id: 'random_mc',
+    question:
+      'Write a multiple choice question asking the user to choose the median of 5 random numbers between 1 and 100. Display all of the numbers to the user, and then make each of the numbers a potential answer, with the median being the correct answer.',
+  },
+  {
+    id: 'random_int',
+    question:
+      'Write a question that asks the user to multiply two integers. You should randomly generate two integers A and B, display them to the user, and then ask the user to provide the product C = A * B in an integer input box. The correct answer should be the product of the two numbers that you generated.',
+  },
+  {
+    id: 'basic_int',
+    question:
+      'Write a question asking "What The Answer to the Ultimate Question of Life, the Universe, and Everything?". Provide an integer box for the user to answer. The correct answer is 42.',
+  },
+  {
+    id: 'implicit_specified_physics',
+    question:
+      'Write a question that asks the user to calculate how far a projectile will be launched. Display to the user an angle randomly generated between 30 and 60 degrees, and a velocity randomly generated between 10 and 20 m/s, and ask for the distance (in meters) that the object travels assuming no wind resistance.',
+  },
+];
+
 export function AiGeneratePage({ resLocals }: { resLocals: Record<string, any> }) {
   return html`
     <!doctype html>
@@ -43,6 +66,13 @@ export function AiGeneratePage({ resLocals }: { resLocals: Record<string, any> }
                   Create question
                 </button>
               </form>
+              Or choose a question from our list of example prompts:
+              <select id="user-prompt-example" onchange="setPromptToExample()">
+                <option value=""></option>
+                ${examplePrompts.map(
+                  (question) => html`<option value="${question.question}">${question.id}</option>`,
+                )}
+              </select>
               <div id="generation-results"></div>
               <br />
               <div>
@@ -54,6 +84,12 @@ export function AiGeneratePage({ resLocals }: { resLocals: Record<string, any> }
           </div>
         </main>
       </body>
+      <script>
+        function setPromptToExample() {
+          const prompt = document.getElementById('user-prompt-example').value;
+          document.getElementById('user-prompt-llm').value = prompt;
+        }
+      </script>
     </html>
   `.toString();
 }
