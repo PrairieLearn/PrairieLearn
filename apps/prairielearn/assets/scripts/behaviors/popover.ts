36a4ba4d119814135ba112ee4b6118864f9bea79
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-08-14 10:42:43 -0700

    Update recent popover changes for Bootstrap 5

--- apps/prairielearn/assets/scripts/behaviors/popover.ts
+++ apps/prairielearn/assets/scripts/behaviors/popover.ts
@@ -1,9 +1,8 @@
+import type { Popover } from 'bootstrap';
 import { on } from 'delegated-events';
 import { observe } from 'selector-observer';
 
-import { onDocumentReady } from '@prairielearn/browser-utils';
-
-const openPopoverTriggers = new Set<HTMLElement>();
+const openPopoverTriggers = new Set<Popover>();
 
 function getPopoverContainerForTrigger(trigger: HTMLElement): HTMLElement | null {
   const popoverId = trigger.getAttribute('aria-describedby');
@@ -23,21 +22,10 @@ function getPopoverTriggerForContainer(container: HTMLElement): HTMLElement | nu
 
 observe('[data-toggle="popover"]', {
   add(el) {
-    $(el).popover({ sanitize: false });
+    new window.bootstrap.Popover(el, { sanitize: false });
   },
   remove(el) {
-    $(el).popover('dispose');
-
-    if (el instanceof HTMLElement) {
-      // There can be race conditions when a popover trigger is removed where
-      // Bootstrap seems to lose track of the connection between the open popover
-      // and its trigger. To ensure that we aren't left with dangling popovers,
-      // we'll forcefully remove the popover container if it exists.
-      const container = getPopoverContainerForTrigger(el);
-      if (container) {
-        container.remove();
-      }
-    }
+    window.bootstrap.Popover.getInstance(el)?.dispose();
   },
 });
 
@@ -45,33 +33,33 @@ observe('[data-toggle="popover"]', {
 // `data-dismiss="popover"`. This behavior adds support for dismissing popovers
 // in such a declarative way.
 on('click', '[data-dismiss="popover"]', (event) => {
-  const popover = event.currentTarget.closest('.popover');
-  if (!popover || !(popover instanceof HTMLElement)) return;
+  const popoverContainer = event.currentTarget.closest('.popover');
+  if (!popoverContainer || !(popoverContainer instanceof HTMLElement)) return;
 
-  const trigger = getPopoverTriggerForContainer(popover);
+  const trigger = getPopoverTriggerForContainer(popoverContainer);
   if (!trigger) return;
 
-  $(trigger).popover('hide');
+  window.bootstrap.Popover.getInstance(trigger)?.hide();
 });
 
-// TODO: In Bootstrap 5, switch to using native events.
-onDocumentReady(() => {
-  $(document).on('show.bs.popover', () => {
-    openPopoverTriggers.forEach((popover) => $(popover).popover('hide'));
-    openPopoverTriggers.clear();
-  });
+on('show.bs.popover', 'body', () => {
+  openPopoverTriggers.forEach((popover) => popover.hide());
+  openPopoverTriggers.clear();
+});
 
-  $(document).on('inserted.bs.popover', (event) => {
-    const container = getPopoverContainerForTrigger(event.target);
+on('inserted.bs.popover', 'body', (event) => {
+  const container = getPopoverContainerForTrigger(event.target as HTMLElement);
 
-    // If MathJax is loaded on this page, attempt to typeset any math
-    // that may be in the popover.
-    if (container && typeof window.MathJax !== 'undefined') {
-      window.MathJax.typesetPromise([container]);
-    }
-  });
+  // If MathJax is loaded on this page, attempt to typeset any math
+  // that may be in the popover.
+  if (container && typeof window.MathJax !== 'undefined') {
+    window.MathJax.typesetPromise([container]);
+  }
+});
 
-  $(document).on('shown.bs.popover', (event) => {
-    openPopoverTriggers.add(event.target);
-  });
+on('shown.bs.popover', 'body', (event) => {
+  const popover = window.bootstrap.Popover.getInstance(event.target as HTMLElement);
+  if (popover) {
+    openPopoverTriggers.add(popover);
+  }
 });
