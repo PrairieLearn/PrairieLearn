038c7ff48facd6d80f001505fd5a7f043904de68
Dave Mussulman <mussulma@gmail.com>
2024-08-15 12:31:34 -0500

    mark LTI 1.3 routes that need SSO

--- apps/prairielearn/src/ee/routers/lti13.ts
+++ apps/prairielearn/src/ee/routers/lti13.ts
@@ -7,11 +7,24 @@ import { features } from '../../lib/features/index.js';
 import authnMiddleware from '../../middlewares/authn.js';
 import csrfToken from '../../middlewares/csrfToken.js';
 import lti13Auth from '../auth/lti13/lti13Auth.js';
+import { getInstitutionAuthenticationProviders } from '../lib/institution.js';
 import { selectLti13Instance } from '../models/lti13Instance.js';
 import lti13CourseNavigation from '../pages/lti13CourseNavigation/lti13CourseNavigation.js';
 import lti13InstancePages from '../pages/lti13Instance/lti13Instance.js';
 
 const router = Router({ mergeParams: true });
+
+const SSORequired = asyncHandler(async (req, res, next) => {
+  const lti13_instance = await selectLti13Instance(req.params.lti13_instance_id);
+  const instAuthProviders = await getInstitutionAuthenticationProviders(
+    lti13_instance.institution_id,
+  );
+
+  if (!instAuthProviders.some((a) => a.name === 'LTI 1.3')) {
+    throw new Error('Institution does not support LTI 1.3 SSO authentication');
+  }
+  next();
+});
 
 router.use(
   '/:lti13_instance_id/',
@@ -29,9 +42,10 @@ router.use(
   }),
 );
 
-router.use('/:lti13_instance_id/auth', lti13Auth);
+router.use('/:lti13_instance_id/auth', SSORequired, lti13Auth);
 router.use(
   '/:lti13_instance_id/course_navigation',
+  SSORequired,
   authnMiddleware, // authentication, set res.locals.authn_user
   csrfToken,
   lti13CourseNavigation,
