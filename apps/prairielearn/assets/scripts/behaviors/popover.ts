a67e5bbbe0960d570484f24f50f22bf4da0efed5
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-08-19 09:27:56 -0700

    Upgrade to Bootstrap 5 (#10095)
    * Start work on Bootstrap 5 compatibility layer
    
    * Add margin/padding compatibility classes
    
    * Patch a few more things
    
    * Automatically fix input groups
    
    * Backwards compat for form-group class
    
    * Add more compatibility
    
    * Add back missing script tag
    
    * More compatibility behavior
    
    * Improve styling of tables in cards
    
    * Add more compat
    
    * Add compat for form-row class
    
    * Add compat for close buttons
    
    * Remove compat CSS in favor of JavaScript
    
    * Improve compat layer and styles
    
    * Add more compat; begin reorganizing
    
    * WIP comment
    
    * Improvements to patching script
    
    * More compat work
    
    * Fix card header contrast with info background
    
    * Fix typechecking error
    
    * Call observe directly from script
    
    * Tweaks, fixes, and improvements
    
    * Remove unneeded script
    
    * Remove outdated code
    
    * Fix JSON formatting toast
    
    * WIP migrator infrastructure
    
    * Move everything to migrators
    
    * Add Bootstrap 4 opt-in feature flag
    
    * Fix badge-button styles
    
    * Reorganize and improve compat
    
    * Add selector-set as dependency
    
    * Replace usage of jQuery API
    
    * Remove outdated style
    
    * Add basic handling of form-inline
    
    * Update recent popover changes for Bootstrap 5
    
    * Add additional compat for borders
    
    * Use jQuery API to ensure Bootstrap 4 compatibility
    
    * Fix popover/tooltip behavior
    
    * Fix time limit edit popover behavior
    
    * Fix workspaces navbar
    
    * Update apps/prairielearn/assets/scripts/lib/bootstrap-compat-utils.ts
    
    Co-authored-by: Jonatan Schroeder <jonatan@yorku.ca>
    
    * Add back custom-file-input behavior
    
    ---------
    
    Co-authored-by: Jonatan Schroeder <jonatan@yorku.ca>

--- apps/prairielearn/assets/scripts/behaviors/popover.ts
+++ apps/prairielearn/assets/scripts/behaviors/popover.ts
@@ -3,65 +3,58 @@ import { observe } from 'selector-observer';
 
 import { onDocumentReady } from '@prairielearn/browser-utils';
 
+import { getPopoverContainerForTrigger, getPopoverTriggerForContainer } from '../lib/popover.js';
+
 const openPopoverTriggers = new Set<HTMLElement>();
 
-function getPopoverContainerForTrigger(trigger: HTMLElement): HTMLElement | null {
-  const popoverId = trigger.getAttribute('aria-describedby');
-  if (!popoverId) return null;
+// We need to wrap this in `onDocumentReady` because Bootstrap doesn't
+// add its jQuery API to the jQuery object until after this event.
+// `selector-observer` will start running its callbacks immediately, so they'd
+// otherwise execute too soon.
+onDocumentReady(() => {
+  observe('[data-toggle="popover"]', {
+    constructor: HTMLElement,
+    add(el) {
+      // We continue to use the jQuery interface to ensure compatibility with Bootstrap 4.
+      $(el).popover({ sanitize: false });
+    },
+    remove(el) {
+      // We continue to use the jQuery interface to ensure compatibility with Bootstrap 4.
+      $(el).popover('dispose');
 
-  const popoverContainer = document.querySelector<HTMLElement>(`#${popoverId}`);
-  return popoverContainer;
-}
-
-function getPopoverTriggerForContainer(container: HTMLElement): HTMLElement | null {
-  const popoverId = container.getAttribute('id');
-  if (!popoverId) return null;
-
-  const popoverTrigger = document.querySelector<HTMLElement>(`[aria-describedby="${popoverId}"]`);
-  return popoverTrigger;
-}
-
-observe('[data-toggle="popover"]', {
-  add(el) {
-    $(el).popover({ sanitize: false });
-  },
-  remove(el) {
-    $(el).popover('dispose');
-
-    if (el instanceof HTMLElement) {
       // There can be race conditions when a popover trigger is removed where
       // Bootstrap seems to lose track of the connection between the open popover
       // and its trigger. To ensure that we aren't left with dangling popovers,
       // we'll forcefully remove the popover container if it exists.
+      //
+      // TODO: Remove this once we're using the native Bootstrap 5 API.
       const container = getPopoverContainerForTrigger(el);
       if (container) {
         container.remove();
       }
-    }
-  },
-});
+    },
+  });
 
-// Bootstrap supports `data-dismiss="alert"` and `data-dismiss="modal"`, but not
-// `data-dismiss="popover"`. This behavior adds support for dismissing popovers
-// in such a declarative way.
-on('click', '[data-dismiss="popover"]', (event) => {
-  const popover = event.currentTarget.closest('.popover');
-  if (!popover || !(popover instanceof HTMLElement)) return;
+  // Bootstrap supports `data-dismiss="alert"` and `data-dismiss="modal"`, but not
+  // `data-dismiss="popover"`. This behavior adds support for dismissing popovers
+  // in such a declarative way.
+  on('click', '[data-dismiss="popover"]', (event) => {
+    const popoverContainer = event.currentTarget.closest('.popover');
+    if (!popoverContainer || !(popoverContainer instanceof HTMLElement)) return;
 
-  const trigger = getPopoverTriggerForContainer(popover);
-  if (!trigger) return;
+    const trigger = getPopoverTriggerForContainer(popoverContainer);
+    if (!trigger) return;
 
-  $(trigger).popover('hide');
-});
+    $(trigger).popover('hide');
+  });
 
-// TODO: In Bootstrap 5, switch to using native events.
-onDocumentReady(() => {
-  $(document).on('show.bs.popover', () => {
+  // We continue to use the jQuery API for event handling to ensure compatibility with Bootstrap 4.
+  $(document.body).on('show.bs.popover', () => {
     openPopoverTriggers.forEach((popover) => $(popover).popover('hide'));
     openPopoverTriggers.clear();
   });
 
-  $(document).on('inserted.bs.popover', (event) => {
+  $(document.body).on('inserted.bs.popover', (event) => {
     const container = getPopoverContainerForTrigger(event.target);
 
     // If MathJax is loaded on this page, attempt to typeset any math
@@ -71,7 +64,7 @@ onDocumentReady(() => {
     }
   });
 
-  $(document).on('shown.bs.popover', (event) => {
+  $(document.body).on('shown.bs.popover', (event) => {
     openPopoverTriggers.add(event.target);
   });
 });
