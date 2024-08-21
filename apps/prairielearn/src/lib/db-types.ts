12fdc12b2d5469cd31042f528308943c234c6897
Jonatan Schroeder <jonatan@yorku.ca>
2024-08-21 16:02:47 -0400

    Add support for admin queries with TS code (#10384)
    * Add support for admin queries with TS code
    
    * Move types to separate file
    
    * Create a sample "query" without SQL
    
    * Code reorganization, better robustness if result does not match format
    
    * Add types to admin query params
    
    * Use Array.from second argument
    
    * Add backwards compatibility in result object
    
    * Use separate column in list for type
    
    * Fix types
    
    * Use string representation of default function in sql column
    
    * Add tests for new behaviour
    
    * Remove testing file
    
    * Better handling of columns with special format
    
    * Rename check function
    
    * Format in README
    
    * Add comment
    
    * Convert all queries to TS code
    
    * Update README
    
    * Remove references to SQL column
    
    * Minor reword
    
    * Lint
    
    * Lint again (thanks VSCode...)
    
    * Rename util function to clarify it's a legacy use
    
    * One more lint
    
    * Use data-testid convention
    
    * Use a simple query in `generate_and_enroll_users`
    
    * Remove references to sql column
    
    * Rename query filename variable

--- apps/prairielearn/src/lib/db-types.ts
+++ apps/prairielearn/src/lib/db-types.ts
@@ -759,6 +759,18 @@ export const PlanGrantSchema = z.object({
 });
 export type PlanGrant = z.infer<typeof PlanGrantSchema>;
 
+export const QueryRunSchema = z.object({
+  authn_user_id: IdSchema,
+  date: DateFromISOString,
+  error: z.string().nullable(),
+  id: IdSchema,
+  name: z.string(),
+  params: z.record(z.string(), z.any()).nullable(),
+  result: z.record(z.string(), z.any()).nullable(),
+  // The sql column is deprecated and slated for removal in a near-future PR, so it is not included.
+});
+export type QueryRun = z.infer<typeof QueryRunSchema>;
+
 export const QuestionGenerationContextEmbeddingSchema = z.object({
   id: IdSchema,
   doc_text: z.string(),
