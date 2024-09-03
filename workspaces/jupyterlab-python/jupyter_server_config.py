168f6b83582b01480870a565d5eb52d8cbcd2b57
ZacWarham <zacwarham@gmail.com>
2024-09-03 13:22:29 -0700

    Adding check for RTC

--- workspaces/jupyterlab-python/jupyter_server_config.py
+++ workspaces/jupyterlab-python/jupyter_server_config.py
@@ -13,16 +13,19 @@ c.ServerApp.password = ""
 c.ServerApp.token = ""
 c.ServerApp.allow_origin = "*"
 
-# Environmental variable to set the file that opens at launch of workspace
-# File is assumed to be in the workspace folder of the relevant question
-if "LAUNCH_FILE_NAME" in os.environ:
-    c.LabApp.default_url= f"/lab/tree/{os.environ['LAUNCH_FILE_NAME']}"
-
 # RTC appears to be broken in the current version of `jupyter-collaboration`:
 # https://github.com/jupyterlab/jupyter-collaboration/issues/162
 # This opt-in will only be useful once the underlying issue is resolved.
 enable_rtc = bool(os.environ.get("ENABLE_REAL_TIME_COLLABORATION", False))
 c.YDocExtension.disable_rtc = not enable_rtc
+
+# Environmental variable to set the file that opens at launch of workspace
+# File is assumed to be in the workspace folder of the relevant question
+# RTC: needs to be appended if using the RTC Drive
+drive = "RTC:" if enable_rtc else ""
+
+if "LAUNCH_FILE_NAME" in os.environ:
+    c.LabApp.default_url= f"/lab/tree/{drive}{os.environ['LAUNCH_FILE_NAME']}"
 
 c.FileContentsManager.delete_to_trash = False
 c.FileCheckpoints.checkpoint_dir = "/tmp/ipynb_checkpoints"
