6fae3749c3a3b5c7d1ea7306e770603018897ed3
Victor Zhao <victorsss.orz@outlook.com>
2024-09-12 17:28:06 -0500

    format sql file

--- apps/prairielearn/src/ee/lib/ai-grading.sql
+++ apps/prairielearn/src/ee/lib/ai-grading.sql
@@ -52,19 +52,42 @@ VALUES
 
 -- BLOCK select_closest_embeddings
 SELECT
-  emb.submission_text, iq.score_perc, iq.id AS instance_question_id
+  emb.submission_text,
+  iq.score_perc,
+  iq.id AS instance_question_id
 FROM
-  (SELECT s.id, MAX(g.graded_at)
-    FROM (SELECT * FROM grading_jobs WHERE graded_by != $ai_grader_id) AS g
-    JOIN (SELECT * FROM submissions WHERE id != $submission_id) AS s ON (g.submission_id=s.id)
-    WHERE g.graded_at = s.graded_at
-    GROUP BY s.id) AS s_filter
-  JOIN submissions AS s ON (s_filter.id=s.id)
+  (
+    SELECT
+      s.id,
+      MAX(g.graded_at)
+    FROM
+      (
+        SELECT
+          *
+        FROM
+          grading_jobs
+        WHERE
+          graded_by != $ai_grader_id
+      ) AS g
+      JOIN (
+        SELECT
+          *
+        FROM
+          submissions
+        WHERE
+          id != $submission_id
+      ) AS s ON (g.submission_id = s.id)
+    WHERE
+      g.graded_at = s.graded_at
+    GROUP BY
+      s.id
+  ) AS s_filter
+  JOIN submissions AS s ON (s_filter.id = s.id)
   JOIN submission_grading_context_embeddings AS emb ON (emb.submission_id = s.id)
   JOIN variants AS v ON (s.variant_id = v.id)
-  JOIN instance_questions AS iq ON (v.instance_question_id=iq.id)
+  JOIN instance_questions AS iq ON (v.instance_question_id = iq.id)
 WHERE
-  iq.assessment_question_id=$assessment_question_id
+  iq.assessment_question_id = $assessment_question_id
 ORDER BY
   embedding <=> $embedding
 LIMIT
