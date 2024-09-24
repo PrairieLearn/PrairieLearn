50411c50b801079179f567fe8b55ebc86fdcb3b1
Seth Poulsen <poulsenseth@yahoo.com>
2024-09-23 23:45:39 -0600

    saving sharing sets

--- insert_sharing_info.py
+++ insert_sharing_info.py
@@ -1,31 +1,39 @@
 import csv
 import json
 
-# Reading the CSV file as a dictionary
-with open("file.csv", mode="r") as file:
-    csv_dict_reader = csv.DictReader(file)
+with open("./sharing_sets.csv", mode="r") as sharingSetsFile:
+    csv_dict_reader = csv.DictReader(sharingSetsFile)
 
-    # Each row will be a dictionary with the headers as keys
-    for row in csv_dict_reader:
-        print(row)
+    with open("infoCourse.json", mode="r+") as infoCourseFile:
+        infoCourse = json.loads(infoCourseFile.read())
+        # print(infoCourse)
+        if "sharingSets" not in infoCourse:
+            infoCourse["sharingSets"] = []
+
+        existingSets = set(ss["name"] for ss in infoCourse["sharingSets"])
+        for row in csv_dict_reader:
+            sharingSet = row["name"]
+            if sharingSet not in existingSets:
+                infoCourse["sharingSets"].append(
+                    {"name": sharingSet, "description": ""}
+                )
+
+        infoCourseFile.seek(0)
+        infoCourseFile.truncate()
+        infoCourseFile.write(json.dumps(infoCourse, indent="  "))
 
 
-json_string = '{"name": "John", "age": 30, "city": "New York"}'
-data = json.loads(json_string)
 
-print(data)
+# # Example dictionary to save as JSON
+# data = {
+#     "name": "John",
+#     "age": 30,
+#     "city": "New York",
+#     "hobbies": ["reading", "traveling", "coding"],
+# }
 
+# # Saving the dictionary as a formatted JSON file
+# with open("output.json", "w") as file:
+#     json.dump(data, file, indent="  ")  # Use '\t' to format with tabs
 
-# Example dictionary to save as JSON
-data = {
-    "name": "John",
-    "age": 30,
-    "city": "New York",
-    "hobbies": ["reading", "traveling", "coding"],
-}
-
-# Saving the dictionary as a formatted JSON file
-with open("output.json", "w") as file:
-    json.dump(data, file, indent="  ")  # Use '\t' to format with tabs
-
-print("JSON saved with tabbed formatting.")
+# print("JSON saved with tabbed formatting.")
