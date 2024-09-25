819cb5f1c60e5fb2263e2c303020f79588d21529
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2024-09-25 15:38:33 -0700

    rename qidField

--- apps/prairielearn/assets/scripts/instructorQuestionSettingsClient.ts
+++ apps/prairielearn/assets/scripts/instructorQuestionSettingsClient.ts
@@ -5,26 +5,26 @@ import { saveButtonEnabling } from './lib/saveButtonEnabling.js';
 
 onDocumentReady(() => {
   const qidField = document.querySelector('input[name="qid"]') as HTMLInputElement;
-  const otherQids = qidfield.dataset.otherValues?.split(',') ?? [];
+  const otherQids = qidField.dataset.otherValues?.split(',') ?? [];
   const questionSettingsForm = document.querySelector<HTMLFormElement>(
     'form[name="edit-question-settings-form"]',
   );
   const saveButton = document.querySelector<HTMLButtonElement>('#save-button');
 
   function validateId() {
-    const newValue = qidfield.value;
+    const newValue = qidField.value;
 
-    if (otherQids.includes(newValue) && newValue !== qidfield.defaultValue) {
-      qidfield.setCustomValidity('This ID is already in use');
+    if (otherQids.includes(newValue) && newValue !== qidField.defaultValue) {
+      qidField.setCustomValidity('This ID is already in use');
     } else {
-      qidfield.setCustomValidity('');
+      qidField.setCustomValidity('');
     }
 
-    qidfield.reportValidity();
+    qidField.reportValidity();
   }
 
-  qidfield.addEventListener('input', validateId);
-  qidfield.addEventListener('change', validateId);
+  qidField.addEventListener('input', validateId);
+  qidField.addEventListener('change', validateId);
 
   if (!questionSettingsForm || !saveButton) return;
   saveButtonEnabling(questionSettingsForm, saveButton);
