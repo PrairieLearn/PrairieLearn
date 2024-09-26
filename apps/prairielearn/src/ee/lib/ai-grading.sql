50394bdac8cdb4b17ed5b7ddc4fb158b760a4fec
Victor Zhao <victorsss.orz@outlook.com>
2024-09-26 16:05:00 -0500

    remove unused filter for selecting instance questions

--- apps/prairielearn/src/ee/lib/ai-grading.sql
+++ apps/prairielearn/src/ee/lib/ai-grading.sql
@@ -12,13 +12,11 @@ WHERE
 
 -- BLOCK select_all_instance_questions
 SELECT
-  iq.*
+  *
 FROM
   instance_questions AS iq
-  JOIN assessment_instances AS ai ON (ai.id = iq.assessment_instance_id)
 WHERE
-  ai.assessment_id = $assessment_id
-  AND iq.assessment_question_id = $assessment_question_id
+  iq.assessment_question_id = $assessment_question_id
   AND iq.status != 'unanswered';
 
 -- BLOCK select_last_submission_id
