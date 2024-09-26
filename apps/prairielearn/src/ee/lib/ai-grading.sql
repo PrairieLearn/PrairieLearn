1b65bfde8a411f6b6df7b076cc070f104484053f
Victor Zhao <victorsss.orz@outlook.com>
2024-09-26 11:17:00 -0500

    forgot to run prettier

--- apps/prairielearn/src/ee/lib/ai-grading.sql
+++ apps/prairielearn/src/ee/lib/ai-grading.sql
@@ -60,9 +60,19 @@ WHERE
 
 -- BLOCK create_embedding_for_submission
 INSERT INTO
-  submission_grading_context_embeddings (embedding, submission_id, submission_text, assessment_question_id)
+  submission_grading_context_embeddings (
+    embedding,
+    submission_id,
+    submission_text,
+    assessment_question_id
+  )
 VALUES
-  ($embedding, $submission_id, $submission_text, $assessment_question_id)
+  (
+    $embedding,
+    $submission_id,
+    $submission_text,
+    $assessment_question_id
+  )
 RETURNING
   *;
 
