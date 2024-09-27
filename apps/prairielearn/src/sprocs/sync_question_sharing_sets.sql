072b86990cbe61da2a597f709793912370ece544
Seth Poulsen <poulsenseth@yahoo.com>
2024-09-27 11:53:51 -0600

    get rid of loop

--- apps/prairielearn/src/sprocs/sync_question_sharing_sets.sql
+++ apps/prairielearn/src/sprocs/sync_question_sharing_sets.sql
@@ -3,18 +3,14 @@ CREATE FUNCTION
         IN new_question_sharing_sets JSONB[]
     ) RETURNS void
 AS $$
-DECLARE
-    question_sharing_sets_item JSONB;
 BEGIN
-    FOREACH question_sharing_sets_item IN ARRAY new_question_sharing_sets LOOP
-        INSERT INTO sharing_set_questions (
-            question_id,
-            sharing_set_id
-        ) SELECT
-            (question_sharing_sets_item->>0)::bigint,
-            sharing_set_id::bigint
-        FROM JSONB_ARRAY_ELEMENTS_TEXT(question_sharing_sets_item->1) AS sharing_set_id
-        ON CONFLICT (question_id, sharing_set_id) DO NOTHING;
-    END LOOP;
+    INSERT INTO sharing_set_questions (
+        question_id,
+        sharing_set_id
+    ) SELECT
+        (question_sharing_sets_item->>0)::bigint,
+        JSONB_ARRAY_ELEMENTS_TEXT(question_sharing_sets_item->1)::bigint AS sharing_set_id
+    FROM UNNEST(new_question_sharing_sets) AS question_sharing_sets_item
+    ON CONFLICT (question_id, sharing_set_id) DO NOTHING;
 END;
 $$ LANGUAGE plpgsql VOLATILE;
