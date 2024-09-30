329add2bfb66558d36b34c2b68d557de4c32f919
Seth Poulsen <poulsenseth@yahoo.com>
2024-09-30 15:22:12 -0600

    get course owner info

---
+++ get_course_owner_info.sql
@@ -0,0 +1,30 @@
+WITH
+  shared_question_exists AS (
+    SELECT
+      q.course_id
+    FROM
+      questions AS q
+    WHERE
+      q.shared_publicly
+    UNION
+    SELECT
+      q.course_id
+    FROM
+      sharing_sets AS ss
+      JOIN sharing_set_questions AS ssq ON ss.id = ssq.sharing_set_id
+      JOIN questions AS q ON q.id = ssq.question_id
+  )
+SELECT
+  c.id,
+  c.short_name AS course_short_name,
+  i.short_name AS institution_short_name,
+  u.uid,
+  u.email
+FROM
+  shared_question_exists
+  JOIN pl_courses as c on c.id = shared_question_exists.course_id
+  JOIN course_permissions AS cp ON cp.course_id = shared_question_exists.course_id
+  JOIN users AS u ON u.user_id = cp.user_id
+  JOIN institutions AS i on i.id = c.institution_id
+WHERE
+  cp.course_role = 'Owner';
