f3342ddec6023c2e7e4a340049616987bd32e5f3
Victor Zhao <victorsss.orz@outlook.com>
2024-10-03 17:11:06 -0500

    remove index on is_ai_graded

--- apps/prairielearn/src/migrations/20241001145611_add_ai_graded_column.sql
+++ apps/prairielearn/src/migrations/20241001145611_add_ai_graded_column.sql
@@ -1,4 +1,2 @@
 ALTER TABLE grading_jobs
 ADD COLUMN IF NOT EXISTS is_ai_graded BOOLEAN NOT NULL DEFAULT FALSE;
-
-CREATE INDEX IF NOT EXISTS grading_jobs_is_ai_graded_idx ON grading_jobs (is_ai_graded);
