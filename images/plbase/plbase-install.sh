413c5bfd6e26b354bcd914a1edc99ae46a757c76
Eliot Robson <eliot.robson24@gmail.com>
2024-10-03 15:32:06 -0500

    Pin miniforge version

--- images/plbase/plbase-install.sh
+++ images/plbase/plbase-install.sh
@@ -70,7 +70,7 @@ dnf -y autoremove
 echo "setting up conda..."
 cd /
 arch=`uname -m`
-curl -LO https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-${arch}.sh
+curl -LO https://github.com/conda-forge/miniforge/releases/tag/24.3.0-0/download/Miniforge3-Linux-${arch}.sh
 bash Miniforge3-Linux-${arch}.sh -b -p /usr/local -f
 
 echo "installing Python packages..."
