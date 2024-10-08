55f9bc08bdeb4861409e76e180f082584b595c6e
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-10-08 16:45:54 -0700

    Reformat with Prettier

--- packages/express-list-endpoints/src/index.js
+++ packages/express-list-endpoints/src/index.js
@@ -10,30 +10,27 @@
  * @property {string[]} middlewares Mounted middlewares
  */
 
-const regExpToParseExpressPathRegExp = /^\/\^\\\/(?:(:?[\w\\.-]*(?:\\\/:?[\w\\.-]*)*)|(\(\?:\([^)]+\)\)))\\\/.*/
-const regExpToReplaceExpressPathRegExpParams = /\(\?:\([^)]+\)\)/
-const regexpExpressParamRegexp = /\(\?:\([^)]+\)\)/g
-const regexpExpressPathParamRegexp = /(:[^)]+)\([^)]+\)/g
+const regExpToParseExpressPathRegExp =
+  /^\/\^\\\/(?:(:?[\w\\.-]*(?:\\\/:?[\w\\.-]*)*)|(\(\?:\([^)]+\)\)))\\\/.*/;
+const regExpToReplaceExpressPathRegExpParams = /\(\?:\([^)]+\)\)/;
+const regexpExpressParamRegexp = /\(\?:\([^)]+\)\)/g;
+const regexpExpressPathParamRegexp = /(:[^)]+)\([^)]+\)/g;
 
-const EXPRESS_ROOT_PATH_REGEXP_VALUE = '/^\\/?(?=\\/|$)/i'
-const STACK_ITEM_VALID_NAMES = [
-  'router',
-  'bound dispatch',
-  'mounted_app'
-]
+const EXPRESS_ROOT_PATH_REGEXP_VALUE = '/^\\/?(?=\\/|$)/i';
+const STACK_ITEM_VALID_NAMES = ['router', 'bound dispatch', 'mounted_app'];
 
 /**
  * Returns all the verbs detected for the passed route
  * @param {Route} route
  */
 const getRouteMethods = function (route) {
-  let methods = Object.keys(route.methods)
+  let methods = Object.keys(route.methods);
 
-  methods = methods.filter((method) => method !== '_all')
-  methods = methods.map((method) => method.toUpperCase())
+  methods = methods.filter((method) => method !== '_all');
+  methods = methods.map((method) => method.toUpperCase());
 
-  return methods
-}
+  return methods;
+};
 
 /**
  * Returns the names (or anonymous) of all the middlewares attached to the
@@ -43,9 +40,9 @@ const getRouteMethods = function (route) {
  */
 const getRouteMiddlewares = function (route) {
   return route.stack.map((item) => {
-    return item.handle.name || 'anonymous'
-  })
-}
+    return item.handle.name || 'anonymous';
+  });
+};
 
 /**
  * Returns true if found regexp related with express params
@@ -53,8 +50,8 @@ const getRouteMiddlewares = function (route) {
  * @returns {boolean}
  */
 const hasParams = function (expressPathRegExp) {
-  return regexpExpressParamRegexp.test(expressPathRegExp)
-}
+  return regexpExpressParamRegexp.test(expressPathRegExp);
+};
 
 /**
  * @param {Route} route Express route object to be parsed
@@ -62,32 +59,30 @@ const hasParams = function (expressPathRegExp) {
  * @return {Endpoint[]} Endpoints info
  */
 const parseExpressRoute = function (route, basePath) {
-  const paths = []
+  const paths = [];
 
   if (Array.isArray(route.path)) {
-    paths.push(...route.path)
+    paths.push(...route.path);
   } else {
-    paths.push(route.path)
+    paths.push(route.path);
   }
 
   /** @type {Endpoint[]} */
   const endpoints = paths.map((path) => {
-    const completePath = basePath && path === '/'
-      ? basePath
-      : `${basePath}${path}`
+    const completePath = basePath && path === '/' ? basePath : `${basePath}${path}`;
 
     /** @type {Endpoint} */
     const endpoint = {
       path: completePath.replace(regexpExpressPathParamRegexp, '$1'),
       methods: getRouteMethods(route),
-      middlewares: getRouteMiddlewares(route)
-    }
+      middlewares: getRouteMiddlewares(route),
+    };
 
-    return endpoint
-  })
+    return endpoint;
+  });
 
-  return endpoints
-}
+  return endpoints;
+};
 
 /**
  * @param {RegExp} expressPathRegExp
@@ -95,28 +90,27 @@ const parseExpressRoute = function (route, basePath) {
  * @returns {string}
  */
 const parseExpressPath = function (expressPathRegExp, params) {
-  let parsedRegExp = expressPathRegExp.toString()
-  let expressPathRegExpExec = regExpToParseExpressPathRegExp.exec(parsedRegExp)
-  let paramIndex = 0
+  let parsedRegExp = expressPathRegExp.toString();
+  let expressPathRegExpExec = regExpToParseExpressPathRegExp.exec(parsedRegExp);
+  let paramIndex = 0;
 
   while (hasParams(parsedRegExp)) {
-    const paramName = params[paramIndex].name
-    const paramId = `:${paramName}`
+    const paramName = params[paramIndex].name;
+    const paramId = `:${paramName}`;
 
-    parsedRegExp = parsedRegExp
-      .replace(regExpToReplaceExpressPathRegExpParams, paramId)
+    parsedRegExp = parsedRegExp.replace(regExpToReplaceExpressPathRegExpParams, paramId);
 
-    paramIndex++
+    paramIndex++;
   }
 
   if (parsedRegExp !== expressPathRegExp.toString()) {
-    expressPathRegExpExec = regExpToParseExpressPathRegExp.exec(parsedRegExp)
+    expressPathRegExpExec = regExpToParseExpressPathRegExp.exec(parsedRegExp);
   }
 
-  const parsedPath = expressPathRegExpExec[1].replace(/\\\//g, '/')
+  const parsedPath = expressPathRegExpExec[1].replace(/\\\//g, '/');
 
-  return parsedPath
-}
+  return parsedPath;
+};
 
 /**
  * @param {import('express').Express | import('express').Router | any} app
@@ -125,25 +119,27 @@ const parseExpressPath = function (expressPathRegExp, params) {
  * @returns {Endpoint[]}
  */
 const parseEndpoints = function (app, basePath, endpoints) {
-  const stack = app.stack || (app._router && app._router.stack)
+  const stack = app.stack || (app._router && app._router.stack);
 
-  endpoints = endpoints || []
-  basePath = basePath || ''
+  endpoints = endpoints || [];
+  basePath = basePath || '';
 
   if (!stack) {
     if (endpoints.length) {
-      endpoints = addEndpoints(endpoints, [{
-        path: basePath,
-        methods: [],
-        middlewares: []
-      }])
+      endpoints = addEndpoints(endpoints, [
+        {
+          path: basePath,
+          methods: [],
+          middlewares: [],
+        },
+      ]);
     }
   } else {
-    endpoints = parseStack(stack, basePath, endpoints)
+    endpoints = parseStack(stack, basePath, endpoints);
   }
 
-  return endpoints
-}
+  return endpoints;
+};
 
 /**
  * Ensures the path of the new endpoints isn't yet in the array.
@@ -157,22 +153,22 @@ const parseEndpoints = function (app, basePath, endpoints) {
 const addEndpoints = function (currentEndpoints, endpointsToAdd) {
   endpointsToAdd.forEach((newEndpoint) => {
     const existingEndpoint = currentEndpoints.find(
-      (endpoint) => endpoint.path === newEndpoint.path
-    )
+      (endpoint) => endpoint.path === newEndpoint.path,
+    );
 
     if (existingEndpoint !== undefined) {
       const newMethods = newEndpoint.methods.filter(
-        (method) => !existingEndpoint.methods.includes(method)
-      )
+        (method) => !existingEndpoint.methods.includes(method),
+      );
 
-      existingEndpoint.methods = existingEndpoint.methods.concat(newMethods)
+      existingEndpoint.methods = existingEndpoint.methods.concat(newMethods);
     } else {
-      currentEndpoints.push(newEndpoint)
+      currentEndpoints.push(newEndpoint);
     }
-  })
+  });
 
-  return currentEndpoints
-}
+  return currentEndpoints;
+};
 
 /**
  * @param {any[]} stack
@@ -183,30 +179,34 @@ const addEndpoints = function (currentEndpoints, endpointsToAdd) {
 const parseStack = function (stack, basePath, endpoints) {
   stack.forEach((stackItem) => {
     if (stackItem.route) {
-      const newEndpoints = parseExpressRoute(stackItem.route, basePath)
+      const newEndpoints = parseExpressRoute(stackItem.route, basePath);
 
-      endpoints = addEndpoints(endpoints, newEndpoints)
+      endpoints = addEndpoints(endpoints, newEndpoints);
     } else if (STACK_ITEM_VALID_NAMES.includes(stackItem.name)) {
-      const isExpressPathRegexp = regExpToParseExpressPathRegExp.test(stackItem.regexp)
+      const isExpressPathRegexp = regExpToParseExpressPathRegExp.test(stackItem.regexp);
 
-      let newBasePath = basePath
+      let newBasePath = basePath;
 
       if (isExpressPathRegexp) {
-        const parsedPath = parseExpressPath(stackItem.regexp, stackItem.keys)
+        const parsedPath = parseExpressPath(stackItem.regexp, stackItem.keys);
 
-        newBasePath += `/${parsedPath}`
-      } else if (!stackItem.path && stackItem.regexp && stackItem.regexp.toString() !== EXPRESS_ROOT_PATH_REGEXP_VALUE) {
-        const regExpPath = ` RegExp(${stackItem.regexp}) `
+        newBasePath += `/${parsedPath}`;
+      } else if (
+        !stackItem.path &&
+        stackItem.regexp &&
+        stackItem.regexp.toString() !== EXPRESS_ROOT_PATH_REGEXP_VALUE
+      ) {
+        const regExpPath = ` RegExp(${stackItem.regexp}) `;
 
-        newBasePath += `/${regExpPath}`
+        newBasePath += `/${regExpPath}`;
       }
 
-      endpoints = parseEndpoints(stackItem.handle, newBasePath, endpoints)
+      endpoints = parseEndpoints(stackItem.handle, newBasePath, endpoints);
     }
-  })
+  });
 
-  return endpoints
-}
+  return endpoints;
+};
 
 /**
  * Returns an array of strings with all the detected endpoints
@@ -214,9 +214,9 @@ const parseStack = function (stack, basePath, endpoints) {
  * @returns {Endpoint[]}
  */
 const expressListEndpoints = function (app) {
-  const endpoints = parseEndpoints(app)
+  const endpoints = parseEndpoints(app);
 
-  return endpoints
-}
+  return endpoints;
+};
 
-module.exports = expressListEndpoints
+module.exports = expressListEndpoints;
