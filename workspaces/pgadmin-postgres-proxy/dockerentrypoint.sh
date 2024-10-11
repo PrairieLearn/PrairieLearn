0edbd7c411392bd8b951787dd22e731ddb885340
Eric Huber <echuber2@illinois.edu>
2024-10-11 18:13:55 -0500

    add draft of Caddy config but still fixing issues in production

--- workspaces/pgadmin-postgres-proxy/dockerentrypoint.sh
+++ workspaces/pgadmin-postgres-proxy/dockerentrypoint.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
+echo "[run] starting Caddy reverse proxy"
+export ADJUSTED_BASE_URL=${WORKSPACE_BASE_URL%%/}
+echo "ADJUSTED_BASE_URL is: $ADJUSTED_BASE_URL"
+caddy run --config /caddy/Caddyfile &
+
 echo "[run] starting Postgres"
 service postgresql start
 psql -U postgres -d postgres -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 echo "[run] PostgreSQL started and configured"
 # copying pgpass servers.json didn't help with the pgadmin4 login, removing now, let students login using the localhost
-# python3 /usr/local/lib/python3.10/dist-packages/pgadmin4/setup.py load-servers "/etc/postgresql/14/main/servers.json" 
+# python3 /usr/local/lib/python3.10/dist-packages/pgadmin4/setup.py load-servers "/etc/postgresql/14/main/servers.json"
 pgadmin4
 echo "[run] pgadmin started and configured"
-
-
