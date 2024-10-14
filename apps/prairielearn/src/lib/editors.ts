dac12ce615a65c6d067910cb4c67be3c92547ac6
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-10-14 09:49:57 -0700

    Add missing code

--- apps/prairielearn/src/lib/editors.ts
+++ apps/prairielearn/src/lib/editors.ts
@@ -1242,7 +1242,9 @@ export class QuestionTransferEditor extends Editor {
 
     // We do not want to preserve sharing settings when copying a question to another course
     delete infoJson['sharingSets'];
+    delete infoJson['sharePublicly'];
     delete infoJson['sharedPublicly'];
+    delete infoJson['shareSourcePublicly'];
     delete infoJson['sharedSourcePublicly'];
     await fs.writeJson(path.join(questionPath, 'info.json'), infoJson, { spaces: 4 });
 
