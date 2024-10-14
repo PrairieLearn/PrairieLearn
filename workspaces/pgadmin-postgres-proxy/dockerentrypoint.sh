1d71aaa3b8cfac0233de08d6133db5ca88776cd1
Eric Huber <echuber2@illinois.edu>
2024-10-13 23:59:36 -0500

    start pgadmin fully before proxy to prevent PL killing workspace too soon

--- workspaces/pgadmin-postgres-proxy/dockerentrypoint.sh
+++ workspaces/pgadmin-postgres-proxy/dockerentrypoint.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 
-echo "[run] starting Caddy reverse proxy"
-export ADJUSTED_BASE_URL=${WORKSPACE_BASE_URL%%/}
-echo "ADJUSTED_BASE_URL is: $ADJUSTED_BASE_URL"
-caddy run --config /caddy/Caddyfile &
-
 echo "[run] starting Postgres"
 service postgresql start
 psql -U postgres -d postgres -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 echo "[run] PostgreSQL started and configured"
 # copying pgpass servers.json didn't help with the pgadmin4 login, removing now, let students login using the localhost
 # python3 /usr/local/lib/python3.10/dist-packages/pgadmin4/setup.py load-servers "/etc/postgresql/14/main/servers.json"
-pgadmin4
+date
+echo "[run] starting pgadmin..."
+pgadmin4 &
+while ! curl http://localhost:5050/misc/ping >/dev/null 2>&1 ; do echo "..." ; sleep 0.5s ; done
+date
 echo "[run] pgadmin started and configured"
+
+echo "[run] starting Caddy reverse proxy"
+export ADJUSTED_BASE_URL=${WORKSPACE_BASE_URL%%/}
+echo "ADJUSTED_BASE_URL is: $ADJUSTED_BASE_URL"
+exec caddy run --config /caddy/Caddyfile
