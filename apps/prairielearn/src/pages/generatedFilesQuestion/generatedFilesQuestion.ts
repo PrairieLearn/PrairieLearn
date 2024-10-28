e8068fcd32161cf32e563ea690ed34592b90786d
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2024-10-28 15:23:00 -0700

    Update apps/prairielearn/src/pages/generatedFilesQuestion/generatedFilesQuestion.ts
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>

--- apps/prairielearn/src/pages/generatedFilesQuestion/generatedFilesQuestion.ts
+++ apps/prairielearn/src/pages/generatedFilesQuestion/generatedFilesQuestion.ts
@@ -48,8 +48,8 @@ export default function (options = { publicEndpoint: false }) {
         variant,
         res.locals.question,
         res.locals.course,
-        // res.locals.user.user_id is not always available during shared question preview.
-        res.locals.user ? res.locals.user.user_id : res.locals.authn_user,
+        // `res.locals.user` isn't populated for publicly-shared question previews.
+        res.locals.user?.user_id ?? res.locals.authn_user.user_id,
         res.locals.authn_user.user_id,
       );
       res.attachment(filename);
