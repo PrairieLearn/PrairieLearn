0ab8286388ea995d928e358589b9948e8cce1f5c
ZacWarham <zacwarham@gmail.com>
2024-11-07 13:30:11 -0800

    Review comments

--- apps/prairielearn/src/components/Scorebar.html.ts
+++ apps/prairielearn/src/components/Scorebar.html.ts
@@ -9,19 +9,22 @@ export function Scorebar(
   }: { minWidth?: string; maxWidth?: string; classes?: string } = {},
 ) {
   if (score == null) return '';
+
+  const percentage = `${Math.floor(score * 100) / 100}%`;
+
   return html`
     <div
       class="progress border border-success ${classes}"
       style="min-width: ${minWidth}; max-width: ${maxWidth};"
     >
       <div class="progress-bar bg-success" style="width: ${Math.floor(Math.min(100, score))}%">
-        ${score >= 50 ? `${Math.round(score * 100) / 100}%` : ''}
+        ${score >= 50 ? percentage : ''}
       </div>
       <div
         class="d-flex flex-column justify-content-center text-center"
         style="width: ${100 - Math.floor(Math.min(100, score))}%"
       >
-        ${score >= 50 ? '' : `${Math.round(score * 100) / 100}%`}
+        ${score >= 50 ? '' : percentage}
       </div>
     </div>
   `;
