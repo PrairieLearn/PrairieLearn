7e06a0181d6b3ac10f8ce05783cd1c9c7c46df5e
Nico Ritschel <nico@elsl.de>
2024-11-07 15:55:27 -0600

    Lifted file browser into a component that is used by both the instructor and public question file browser

--- apps/prairielearn/src/components/FileBrowser.html.ts
+++ apps/prairielearn/src/components/FileBrowser.html.ts
@@ -1,22 +1,32 @@
+import * as path from 'node:path';
+
+import * as async from 'async';
+import { fileTypeFromFile } from 'file-type';
 import { filesize } from 'filesize';
+import fs from 'fs-extra';
+import hljs from 'highlight.js';
+import { isBinaryFile } from 'isbinaryfile';
 
 import { escapeHtml, html, type HtmlValue, joinHtml, unsafeHtml } from '@prairielearn/html';
+import { contains } from '@prairielearn/path-utils';
 
-import { HeadContents } from '../../components/HeadContents.html.js';
-import { Navbar } from '../../components/Navbar.html.js';
+import { compiledScriptTag, nodeModulesAssetPath } from '../lib/assets.js';
+import { config } from '../lib/config.js';
+import * as editorUtil from '../lib/editorUtil.js';
+import type { InstructorFilePaths } from '../lib/instructorFiles.js';
+import { encodePath } from '../lib/uri-util.js';
+
+import { HeadContents } from './HeadContents.html.js';
+import { Navbar } from './Navbar.html.js';
 import {
   AssessmentSyncErrorsAndWarnings,
   CourseInstanceSyncErrorsAndWarnings,
   CourseSyncErrorsAndWarnings,
   QuestionSyncErrorsAndWarnings,
-} from '../../components/SyncErrorsAndWarnings.html.js';
-import { SyncProblemButton } from '../../components/SyncProblemButton.html.js';
-import { compiledScriptTag, nodeModulesAssetPath } from '../../lib/assets.js';
-import { config } from '../../lib/config.js';
-import type { InstructorFilePaths } from '../../lib/instructorFiles.js';
-import { encodePath } from '../../lib/uri-util.js';
+} from './SyncErrorsAndWarnings.html.js';
+import { SyncProblemButton } from './SyncProblemButton.html.js';
 
-export interface FileInfo {
+interface FileInfo {
   id: number;
   name: string;
   path: string;
@@ -34,18 +44,18 @@ export interface FileInfo {
   contents?: string | null;
 }
 
-export interface DirectoryEntry {
+interface DirectoryEntry {
   id: string | number;
   name: string;
   path: string;
   canView: boolean;
 }
 
-export interface DirectoryEntryDirectory extends DirectoryEntry {
+interface DirectoryEntryDirectory extends DirectoryEntry {
   isFile: false;
 }
 
-export interface DirectoryEntryFile extends DirectoryEntry {
+interface DirectoryEntryFile extends DirectoryEntry {
   isFile: true;
   dir: string;
   canEdit: boolean;
@@ -57,12 +67,12 @@ export interface DirectoryEntryFile extends DirectoryEntry {
   sync_warnings: string | null;
 }
 
-export interface DirectoryListings {
+interface DirectoryListings {
   dirs: DirectoryEntryDirectory[];
   files: DirectoryEntryFile[];
 }
 
-export type FileUploadInfo = {
+type FileUploadInfo = {
   id: string | number;
   info?: HtmlValue;
 } & (
@@ -76,19 +86,155 @@ export type FileUploadInfo = {
     }
 );
 
-export interface FileDeleteInfo {
+interface FileDeleteInfo {
   id: string | number;
   name: string;
   path: string;
 }
 
-export interface FileRenameInfo {
+interface FileRenameInfo {
   id: string | number;
   name: string;
   dir: string;
 }
 
-export function InstructorFileBrowser({
+function isHidden(item: string) {
+  return item[0] === '.';
+}
+
+export async function browseDirectory({
+  paths,
+}: {
+  paths: InstructorFilePaths;
+}): Promise<DirectoryListings> {
+  const filenames = await fs.readdir(paths.workingPath);
+  const all_files = await async.mapLimit(
+    filenames
+      .sort()
+      .map((name, index) => ({ name, index }))
+      .filter((f) => !isHidden(f.name)),
+    3,
+    async (file: { name: string; index: number }) => {
+      const filepath = path.join(paths.workingPath, file.name);
+      const stats = await fs.lstat(filepath);
+      if (stats.isFile()) {
+        const editable = !(await isBinaryFile(filepath));
+        const movable = !paths.cannotMove.includes(filepath);
+        const relative_path = path.relative(paths.coursePath, filepath);
+        const sync_data = await editorUtil.getErrorsAndWarningsForFilePath(
+          paths.courseId,
+          relative_path,
+        );
+        return {
+          id: file.index,
+          name: file.name,
+          isFile: true,
+          path: relative_path,
+          dir: paths.workingPath,
+          canEdit: editable && paths.hasEditPermission,
+          canUpload: paths.hasEditPermission,
+          canDownload: true, // we already know the user is a course Viewer (checked on GET)
+          canRename: movable && paths.hasEditPermission,
+          canDelete: movable && paths.hasEditPermission,
+          canView: !paths.invalidRootPaths.some((invalidRootPath) =>
+            contains(invalidRootPath, filepath),
+          ),
+          sync_errors: sync_data.errors,
+          sync_warnings: sync_data.warnings,
+        } as DirectoryEntryFile;
+      } else if (stats.isDirectory()) {
+        return {
+          id: file.index,
+          name: file.name,
+          isFile: false,
+          path: path.relative(paths.coursePath, filepath),
+          canView: !paths.invalidRootPaths.some((invalidRootPath) =>
+            contains(invalidRootPath, filepath),
+          ),
+        } as DirectoryEntryDirectory;
+      } else {
+        return null;
+      }
+    },
+  );
+  return {
+    files: all_files.filter((f) => f?.isFile === true),
+    dirs: all_files.filter((f) => f?.isFile === false),
+  };
+}
+
+export async function browseFile({ paths }: { paths: InstructorFilePaths }): Promise<FileInfo> {
+  const filepath = paths.workingPath;
+  const movable = !paths.cannotMove.includes(filepath);
+  const file: FileInfo = {
+    id: 0,
+    name: path.basename(paths.workingPath),
+    path: path.relative(paths.coursePath, filepath),
+    dir: path.dirname(paths.workingPath),
+    canEdit: false, // will be overridden only if the file is a text file
+    canUpload: paths.hasEditPermission,
+    canDownload: true, // we already know the user is a course Viewer (checked on GET)
+    canRename: movable && paths.hasEditPermission,
+    canDelete: movable && paths.hasEditPermission,
+    canView: !paths.invalidRootPaths.some((invalidRootPath) => contains(invalidRootPath, filepath)),
+    isBinary: await isBinaryFile(paths.workingPath),
+    isImage: false,
+    isPDF: false,
+    isText: false,
+  };
+
+  if (file.isBinary) {
+    const type = await fileTypeFromFile(paths.workingPath);
+    if (type) {
+      if (type?.mime.startsWith('image')) {
+        file.isImage = true;
+      } else if (type?.mime === 'application/pdf') {
+        file.isPDF = true;
+      }
+    }
+  } else {
+    // This is probably a text file. If it's is larger that 1MB, don't
+    // attempt to read it; treat it like an opaque binary file.
+    const { size } = await fs.stat(paths.workingPath);
+    if (size > 1 * 1024 * 1024) {
+      return { ...file, isBinary: true };
+    }
+
+    file.isText = true;
+    file.canEdit = paths.hasEditPermission;
+
+    const fileContents = await fs.readFile(paths.workingPath);
+    const stringifiedContents = fileContents.toString('utf8');
+
+    // Try to guess the language from the file extension. This takes
+    // advantage of the fact that Highlight.js includes common file extensions
+    // as aliases for each supported language, and `getLanguage()` allows
+    // us to look up a language by its alias.
+    //
+    // If we don't get a match, we'll try to guess the language by running
+    // `highlightAuto()` on the first few thousand characters of the file.
+    //
+    // Note that we deliberately exclude `ml` and `ls` from the extensions
+    // that we try to guess from, as they're ambiguous (OCaml/Standard ML
+    // and LiveScript/Lasso, respectively). For more details, see
+    // https://highlightjs.readthedocs.io/en/latest/supported-languages.html
+    let language: string | undefined = undefined;
+    const extension = path.extname(paths.workingPath).substring(1);
+    if (!['ml', 'ls'].includes(extension) && hljs.getLanguage(extension)) {
+      language = extension;
+    } else {
+      const result = hljs.highlightAuto(stringifiedContents.slice(0, 2000));
+      language = result.language;
+    }
+    file.contents = hljs.highlight(stringifiedContents, {
+      language: language ?? 'plaintext',
+    }).value;
+  }
+
+  return file;
+}
+
+export function FileBrowser({
   resLocals,
   paths,
   isFile,
@@ -117,7 +263,7 @@ export function InstructorFileBrowser({
               course,
               urlPrefix,
             })
-          : navPage === 'question'
+          : navPage === 'question' || navPage === 'public_question'
             ? QuestionSyncErrorsAndWarnings({
                 authz_data,
                 question: resLocals.question,
@@ -132,7 +278,7 @@ export function InstructorFileBrowser({
         ? 'Course Instance Files'
         : navPage === 'assessment'
           ? 'Assessment Files'
-          : navPage === 'question'
+          : navPage === 'question' || navPage === 'public_question'
             ? `Files (${resLocals.question.qid})`
             : 'Files';
 
