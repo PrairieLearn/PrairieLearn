b925e9f59af29ab9e9c9dee474d555e7236c5e08
Nico Ritschel <nico@elsl.de>
2024-11-08 18:26:05 -0600

    Removed res.locals use from public file download

--- apps/prairielearn/src/pages/publicQuestionFileDownload/publicQuestionFileDownload.ts
+++ apps/prairielearn/src/pages/publicQuestionFileDownload/publicQuestionFileDownload.ts
@@ -8,22 +8,18 @@ import { selectQuestionById } from '../../models/question.js';
 
 const router = Router({ mergeParams: true });
 
-async function setLocals(req, res) {
-  const course = await selectCourseById(req.params.course_id);
-  res.locals.question = await selectQuestionById(req.params.question_id);
-
-  if (!res.locals.question.share_source_publicly || course.id !== res.locals.question.course_id) {
-    throw new error.HttpStatusError(404, 'Not Found');
-  }
-}
-
 router.get(
   '/*',
   asyncHandler(async (req, res) => {
-    await setLocals(req, res);
+    const course = await selectCourseById(req.params.course_id);
+    const question = await selectQuestionById(req.params.question_id);
+
+    if (!question.share_source_publicly || course.id !== question.course_id) {
+      throw new error.HttpStatusError(404, 'Not Found');
+    }
     if (req.query.type) res.type(req.query.type.toString());
     if (req.query.attachment) res.attachment(req.query.attachment.toString());
-    res.sendFile(req.params[0], { root: res.locals.course.path });
+    res.sendFile(req.params[0], { root: course.path });
   }),
 );
 
