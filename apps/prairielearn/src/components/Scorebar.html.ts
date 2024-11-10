d8d5dd840f614d0623c1953f5b1c816d2047e65f
ZacWarham <zacwarham@gmail.com>
2024-11-09 23:54:06 -0800

    Updating variable names and switching to PL `formatPoints` function

--- apps/prairielearn/src/components/Scorebar.html.ts
+++ apps/prairielearn/src/components/Scorebar.html.ts
@@ -1,30 +1,31 @@
 import { html } from '@prairielearn/html';
+import { formatPoints } from '../../src/lib/format.js';
 
 export function Scorebar(
-  score: number | null,
+  scorePerc: number | null,
   {
     minWidth = '5em',
     maxWidth = '20em',
     classes = '',
   }: { minWidth?: string; maxWidth?: string; classes?: string } = {},
 ) {
-  if (score == null) return '';
+  if (scorePerc == null) return '';
 
-  const percentage = `${Math.floor(score * 100) / 100}%`;
+  const scorePercString = `${formatPoints(scorePerc)}%`;
 
   return html`
     <div
       class="progress border border-success ${classes}"
       style="min-width: ${minWidth}; max-width: ${maxWidth};"
     >
-      <div class="progress-bar bg-success" style="width: ${Math.floor(Math.min(100, score))}%">
-        ${score >= 50 ? percentage : ''}
+      <div class="progress-bar bg-success" style="width: ${Math.floor(Math.min(100, scorePerc))}%">
+        ${scorePerc >= 50 ? scorePercString : ''}
       </div>
       <div
         class="d-flex flex-column justify-content-center text-center"
-        style="width: ${100 - Math.floor(Math.min(100, score))}%"
+        style="width: ${100 - Math.floor(Math.min(100, scorePerc))}%"
       >
-        ${score >= 50 ? '' : percentage}
+        ${scorePerc >= 50 ? '' : scorePercString}
       </div>
     </div>
   `;
