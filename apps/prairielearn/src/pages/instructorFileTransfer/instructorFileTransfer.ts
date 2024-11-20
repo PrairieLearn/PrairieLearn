78226e4c18f14a0f4c2e592418c03d7c208e1f27
Jonatan Schroeder <jonatanschroeder@gmail.com>
2024-11-20 11:44:01 -0500

    Convert page to TS

--- apps/prairielearn/src/pages/instructorFileTransfer/instructorFileTransfer.ts
+++ apps/prairielearn/src/pages/instructorFileTransfer/instructorFileTransfer.ts
@@ -1,4 +1,3 @@
-// @ts-check
 import * as path from 'path';
 
 import debugfn from 'debug';
@@ -9,20 +8,25 @@ import { flash } from '@prairielearn/flash';
 import * as sqldb from '@prairielearn/postgres';
 
 import { config } from '../../lib/config.js';
+import { type Course, type FileTransfer, FileTransferSchema } from '../../lib/db-types.js';
 import { QuestionTransferEditor } from '../../lib/editors.js';
 import { idsEqual } from '../../lib/id.js';
+import { selectCourseById } from '../../models/course.js';
 import { selectQuestionByUuid } from '../../models/question.js';
 
 const router = express.Router();
 const sql = sqldb.loadSqlEquiv(import.meta.url);
 const debug = debugfn('prairielearn:instructorFileTransfer');
 
-async function getFileTransfer(file_transfer_id, user_id) {
-  let file_transfer;
-  const result = await sqldb.queryOneRowAsync(sql.select_file_transfer, {
-    id: file_transfer_id,
-  });
-  file_transfer = result.rows[0];
+async function getFileTransfer(
+  file_transfer_id: string,
+  user_id: string,
+): Promise<FileTransfer & { from_course: Course }> {
+  const file_transfer = await sqldb.queryRow(
+    sql.select_file_transfer,
+    { id: file_transfer_id },
+    FileTransferSchema,
+  );
   if (file_transfer.transfer_type !== 'CopyQuestion') {
     throw new Error(`bad transfer_type: ${file_transfer.transfer_type}`);
   }
@@ -33,17 +37,14 @@ async function getFileTransfer(file_transfer_id, user_id) {
       } and ${user_id} (types: ${typeof file_transfer.user_id}, ${typeof user_id})`,
     );
   }
-  const courseResult = await sqldb.queryOneRowAsync(sql.select_course_from_course_id, {
-    course_id: file_transfer.from_course_id,
-  });
-  file_transfer.from_course = courseResult.rows[0];
-  return file_transfer;
+  const from_course = await selectCourseById(file_transfer.from_course_id);
+  return { ...file_transfer, from_course };
 }
 
 router.get(
   '/:file_transfer_id',
-  asyncHandler(async (req, res, next) => {
-    if (config.filesRoot == null) return next(new Error('config.filesRoot is null'));
+  asyncHandler(async (req, res) => {
+    if (config.filesRoot == null) throw new Error('config.filesRoot is null');
     const file_transfer = await getFileTransfer(
       req.params.file_transfer_id,
       res.locals.user.user_id,
@@ -55,7 +56,7 @@ router.get(
     const editor = new QuestionTransferEditor({
       locals: res.locals,
       from_qid: qid,
-      from_course_short_name: file_transfer.from_course.short_name,
+      from_course_short_name: file_transfer.from_course.short_name ?? '',
       from_path: path.join(config.filesRoot, file_transfer.storage_filename),
     });
     const serverJob = await editor.prepareServerJob();
