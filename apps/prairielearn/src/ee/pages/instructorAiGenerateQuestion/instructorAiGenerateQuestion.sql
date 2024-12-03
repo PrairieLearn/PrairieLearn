99b8bedf7d1114bc29ea9dc66c7b7e2ad4c0a394
Jacob Levine <jacoblevine18@gmail.com>
2024-12-03 15:24:56 -0600

    remove unused route

--- apps/prairielearn/src/ee/pages/instructorAiGenerateQuestion/instructorAiGenerateQuestion.sql
+++ apps/prairielearn/src/ee/pages/instructorAiGenerateQuestion/instructorAiGenerateQuestion.sql
@@ -1,13 +1,3 @@
--- BLOCK select_added_question
-SELECT
-  *
-FROM
-  questions AS q
-WHERE
-  q.uuid = $uuid
-  AND q.course_id = $course_id
-  AND q.deleted_at IS NULL;
-
 -- BLOCK select_question_by_qid_and_course
 SELECT
   *
