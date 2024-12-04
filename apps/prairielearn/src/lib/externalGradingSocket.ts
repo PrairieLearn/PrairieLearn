ce0ea8c9f7be07d23c3dd13bf9e5379e5c344f0e
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-12-04 10:22:59 -0800

    Render new contents for async panel rendering (#10974)
    * Render new contents for async panel rendering
    
    * Explicitly send as JSON; fix small issues
    
    * Drop CSRF token from validated properties

--- apps/prairielearn/src/lib/externalGradingSocket.ts
+++ apps/prairielearn/src/lib/externalGradingSocket.ts
@@ -74,7 +74,6 @@ export function connection(socket: Socket) {
         'submission_id',
         'url_prefix',
         'question_context',
-        'csrf_token',
         'authorized_edit',
       ])
     ) {
@@ -92,7 +91,6 @@ export function connection(socket: Socket) {
       user_id: msg.user_id,
       urlPrefix: msg.url_prefix,
       questionContext: msg.question_context,
-      csrfToken: msg.csrf_token,
       authorizedEdit: msg.authorized_edit,
       renderScorePanels: true,
     }).then(
@@ -156,8 +154,8 @@ function checkToken(token: string, variantId: string): boolean {
   const data = { variantId };
   const valid = checkSignedToken(token, data, config.secretKey, { maxAge: 24 * 60 * 60 * 1000 });
   if (!valid) {
-    logger.error(`CSRF token for variant ${variantId} failed validation.`);
-    Sentry.captureException(new Error(`CSRF token for variant ${variantId} failed validation.`));
+    logger.error(`Token for variant ${variantId} failed validation.`);
+    Sentry.captureException(new Error(`Token for variant ${variantId} failed validation.`));
   }
   return valid;
 }
