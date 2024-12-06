5ca16672fba8951dccbebe809f9c595b87ef909d
Jacob Levine <jacoblevine18@gmail.com>
2024-12-06 13:55:39 -0600

    fmt

--- apps/prairielearn/src/tests/accessibility/index.test.ts
+++ apps/prairielearn/src/tests/accessibility/index.test.ts
@@ -190,7 +190,7 @@ const SKIP_ROUTES = [
   '/pl/public/course/:course_id/question/:question_id/preview/variant/:variant_id/submission/:submission_id',
   '/pl/course_instance/:course_instance_id/instructor/ai_generate_editor/:question_id/variant/:variant_id/submission/:submission_id',
   '/pl/course/:course_id/ai_generate_editor/:question_id/variant/:variant_id/submission/:submission_id',
-  
+
   // These pages just redirect to other pages and thus don't have to be tested.
   '/pl/course_instance/:course_instance_id/instructor/assessment/:assessment_id/manual_grading/assessment_question/:assessment_question_id/next_ungraded',
   '/pl/course_instance/:course_instance_id/instructor/loadFromDisk',
