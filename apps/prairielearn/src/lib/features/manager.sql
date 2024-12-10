b4a88c32413831c47dd591aa8ec5aa398c1ed23a
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-12-09 16:24:33 -0800

    Simplify query

--- apps/prairielearn/src/lib/features/manager.sql
+++ apps/prairielearn/src/lib/features/manager.sql
@@ -1,43 +1,36 @@
 -- BLOCK is_feature_enabled
-WITH
-  has_feature_grant AS (
-    SELECT
-      COALESCE(
-        ARRAY_AGG(
-          enabled
-          ORDER BY
-            institution_id NULLS FIRST,
-            course_id NULLS FIRST,
-            course_instance_id NULLS FIRST,
-            user_id NULLS FIRST
-        ),
-        '{}'::boolean[]
-      ) AS enabled
-    FROM
-      feature_grants
-    WHERE
-      name = $name
-      AND (
-        institution_id IS NULL
-        OR $institution_id = institution_id
-      )
-      AND (
-        course_id IS NULL
-        OR $course_id = course_id
-      )
-      AND (
-        course_instance_id IS NULL
-        OR $course_instance_id = course_instance_id
-      )
-      AND (
-        user_id IS NULL
-        OR $user_id = user_id
-      )
-  )
 SELECT
-  has_feature_grant.enabled AS enabled
+  COALESCE(
+    ARRAY_AGG(
+      enabled
+      ORDER BY
+        institution_id NULLS FIRST,
+        course_id NULLS FIRST,
+        course_instance_id NULLS FIRST,
+        user_id NULLS FIRST
+    ),
+    '{}'::boolean[]
+  ) AS enabled
 FROM
-  has_feature_grant;
+  feature_grants
+WHERE
+  name = $name
+  AND (
+    institution_id IS NULL
+    OR $institution_id = institution_id
+  )
+  AND (
+    course_id IS NULL
+    OR $course_id = course_id
+  )
+  AND (
+    course_instance_id IS NULL
+    OR $course_instance_id = course_instance_id
+  )
+  AND (
+    user_id IS NULL
+    OR $user_id = user_id
+  );
 
 -- BLOCK toggle_feature
 INSERT INTO
