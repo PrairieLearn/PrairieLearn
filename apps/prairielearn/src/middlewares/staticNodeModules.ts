ce37d5ad9f37d1e906ba7e65c66b379e1e79376b
Pete Stenger <pete@stenger.io>
2024-12-13 00:55:24 -0600

    fixup build

--- apps/prairielearn/src/middlewares/staticNodeModules.ts
+++ apps/prairielearn/src/middlewares/staticNodeModules.ts
@@ -18,7 +18,7 @@ const NODE_MODULES_PATHS = [
  * The first argument is a path within `node_modules` to serve. This can be '.'
  * to serve all files, or a subdirectory like `mathjax/es5`.
  */
-export default function (servePath: string, options: ServeStaticOptions) {
+export default function (servePath: string, options?: ServeStaticOptions) {
   const router = express.Router();
 
   NODE_MODULES_PATHS.forEach((p) => {
