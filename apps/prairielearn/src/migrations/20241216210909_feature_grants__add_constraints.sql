7551d8830102c2b39b634269429e2f3200d41345
Nathan Sarang-Walters <nathan@prairielearn.com>
2024-12-16 16:30:52 -0800

    Fix constraints

--- apps/prairielearn/src/migrations/20241216210909_feature_grants__add_constraints.sql
+++ apps/prairielearn/src/migrations/20241216210909_feature_grants__add_constraints.sql
@@ -1,15 +1,15 @@
 ALTER TABLE feature_grants
 ADD CONSTRAINT feature_grants_course_id_not_null CHECK (
   NOT (
-    institution_id IS NOT NULL
-    AND course_id IS NULL
+    institution_id IS NULL
+    AND course_id IS NOT NULL
   )
 );
 
 ALTER TABLE feature_grants
 ADD CONSTRAINT feature_grants_course_instance_id_not_null CHECK (
   NOT (
-    course_id IS NOT NULL
-    AND course_instance_id IS NULL
+    course_id IS NULL
+    AND course_instance_id IS NOT NULL
   )
 );
