bce3db10ec019c501982e643a6c33639a7c2373c
Seth Poulsen <poulsenseth@yahoo.com>
2024-12-18 14:19:16 -0700

    refactor for some code reuse

--- apps/prairielearn/src/pages/instructorAssessmentQuestions/instructorAssessmentQuestions.html.ts
+++ apps/prairielearn/src/pages/instructorAssessmentQuestions/instructorAssessmentQuestions.html.ts
@@ -2,6 +2,10 @@ import { html } from '@prairielearn/html';
 import { run } from '@prairielearn/run';
 
 import { AssessmentBadge } from '../../components/AssessmentBadge.html.js';
+import {
+  AssessmentQuestionHeaders,
+  AssessmentQuestionNumber,
+} from '../../components/AssessmentQuestions.html.js';
 import { HeadContents } from '../../components/HeadContents.html.js';
 import { IssueBadge } from '../../components/IssueBadge.html.js';
 import { Modal } from '../../components/Modal.html.js';
@@ -133,51 +137,11 @@ function AssessmentQuestionsTable({
         <tbody>
           ${questions.map((question) => {
             return html`
-              ${question.start_new_zone
-                ? html`
-                    <tr>
-                      <th colspan="${nTableCols}">
-                        Zone ${question.zone_number}. ${question.zone_title}
-                        ${question.zone_number_choose == null
-                          ? '(Choose all questions)'
-                          : question.zone_number_choose === 1
-                            ? '(Choose 1 question)'
-                            : `(Choose ${question.zone_number_choose} questions)`}
-                        ${question.zone_has_max_points
-                          ? `(maximum ${question.zone_max_points} points)`
-                          : ''}
-                        ${question.zone_has_best_questions
-                          ? `(best ${question.zone_best_questions} questions)`
-                          : ''}
-                      </th>
-                    </tr>
-                  `
-                : ''}
-              ${question.start_new_alternative_group && question.alternative_group_size > 1
-                ? html`
-                    <tr>
-                      <td colspan="${nTableCols}">
-                        ${question.alternative_group_number}.
-                        ${question.alternative_group_number_choose == null
-                          ? 'Choose all questions from:'
-                          : question.alternative_group_number_choose === 1
-                            ? 'Choose 1 question from:'
-                            : `Choose ${question.alternative_group_number_choose} questions from:`}
-                      </td>
-                    </tr>
-                  `
-                : ''}
+              ${AssessmentQuestionHeaders(question, nTableCols)}
               <tr>
                 <td>
                   ${run(() => {
-                    const number =
-                      question.alternative_group_size === 1
-                        ? `${question.alternative_group_number}.`
-                        : html`
-                            <span class="ml-3">
-                              ${question.alternative_group_number}.${question.number_in_alternative_group}.
-                            </span>
-                          `;
+                    const number = AssessmentQuestionNumber(question);
                     const issueBadge = IssueBadge({
                       urlPrefix,
                       count: question.open_issue_count ?? 0,
--- apps/prairielearn/src/pages/publicAssessmentQuestions/publicAssessmentQuestions.html.ts
+++ apps/prairielearn/src/pages/publicAssessmentQuestions/publicAssessmentQuestions.html.ts
@@ -1,6 +1,10 @@
 import { html } from '@prairielearn/html';
 
 import { AssessmentBadge } from '../../components/AssessmentBadge.html.js';
+import {
+  AssessmentQuestionHeaders,
+  AssessmentQuestionNumber,
+} from '../../components/AssessmentQuestions.html.js';
 import { HeadContents } from '../../components/HeadContents.html.js';
 import { Navbar } from '../../components/Navbar.html.js';
 import { TagBadgeList } from '../../components/TagBadge.html.js';
@@ -74,7 +78,6 @@ function AssessmentQuestionsTable({
   course_sharing_name: string;
 }) {
   const nTableCols = 4;
-
   return html`
     <div class="table-responsive">
       <table class="table table-sm table-hover">
@@ -90,53 +93,13 @@ function AssessmentQuestionsTable({
         <tbody>
           ${questions.map((question) => {
             return html`
-              ${question.start_new_zone
-                ? html`
-                    <tr>
-                      <th colspan="${nTableCols}">
-                        Zone ${question.zone_number}. ${question.zone_title}
-                        ${question.zone_number_choose == null
-                          ? '(Choose all questions)'
-                          : question.zone_number_choose === 1
-                            ? '(Choose 1 question)'
-                            : `(Choose ${question.zone_number_choose} questions)`}
-                        ${question.zone_has_max_points
-                          ? `(maximum ${question.zone_max_points} points)`
-                          : ''}
-                        ${question.zone_has_best_questions
-                          ? `(best ${question.zone_best_questions} questions)`
-                          : ''}
-                      </th>
-                    </tr>
-                  `
-                : ''}
-              ${question.start_new_alternative_group && question.alternative_group_size > 1
-                ? html`
-                    <tr>
-                      <td colspan="${nTableCols}">
-                        ${question.alternative_group_number}.
-                        ${question.alternative_group_number_choose == null
-                          ? 'Choose all questions from:'
-                          : question.alternative_group_number_choose === 1
-                            ? 'Choose 1 question from:'
-                            : `Choose ${question.alternative_group_number_choose} questions from:`}
-                      </td>
-                    </tr>
-                  `
-                : ''}
+              ${AssessmentQuestionHeaders(question, nTableCols)}
               <tr>
                 <td>
                   <a
                     href="${urlPrefix}/public/course/${course_id}/question/${question.question_id}/preview"
                   >
-                    ${question.alternative_group_size === 1
-                      ? `${question.alternative_group_number}.`
-                      : html`
-                          <span class="ml-3">
-                            ${question.alternative_group_number}.${question.number_in_alternative_group}.
-                          </span>
-                        `}
-                    ${question.title}
+                    ${AssessmentQuestionNumber(question)}${question.title}
                   </a>
                 </td>
                 <td>${`@${course_sharing_name}/${question.display_name}`}</td>
