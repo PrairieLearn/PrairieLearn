d7bea9d776c0aa1bb7cae67e6c993aa52dba76a0
Pete Stenger <peter.a.stenger@gmail.com>
2024-12-18 13:11:24 -0500

    Convert some lib/etc. files to TypeScript (#11011)
    * fixup tsc
    
    * convert lib/code-caller, aws, and authn to ts
    
    * full TS conversion
    
    * convert some more smaller files
    
    * Revert changes to lib/code-caller
    
    * Revert "convert some more smaller files"
    
    This reverts commit 3c577d860f7aeeca843f69dd3c9af756b80bbf0f.
    
    * Revert "fixup tsc"
    
    This reverts commit 437d28e22c6e86b00afa7af3157e099f5f590008.
    
    * Resolve first wave of comments
    
    * add @smithy/types
    
    * fixup yarn dedupe
    
    * change to dev-dependency
    
    * got confused about multiple package.json's
    
    * resolve comments from @nwalters512

--- apps/prairielearn/src/lib/aws.ts
+++ apps/prairielearn/src/lib/aws.ts
@@ -1,10 +1,14 @@
-// @ts-check
 import { pipeline } from 'node:stream/promises';
 import * as path from 'path';
 
-import { S3 } from '@aws-sdk/client-s3';
+import {
+  S3,
+  type GetObjectOutput,
+  type CompleteMultipartUploadCommandOutput,
+} from '@aws-sdk/client-s3';
 import { fromNodeProviderChain } from '@aws-sdk/credential-providers';
 import { Upload } from '@aws-sdk/lib-storage';
+import { type NodeJsClient } from '@smithy/types';
 import debugfn from 'debug';
 import fs from 'fs-extra';
 
@@ -46,17 +50,22 @@ export const makeAwsClientConfig = awsConfigProvider.makeAwsClientConfig;
 /**
  * Upload a local file or directory to S3.
  *
- * @param {string} s3Bucket - The S3 bucket name.
- * @param {string} s3Path - The S3 destination path.
- * @param {string | null} localPath - The local source path.
- * @param {boolean=} isDirectory - Whether the upload source is a directory (defaults to false).
- * @param {Buffer | null} buffer - A file buffer if local source path falsy.
- * @returns {Promise<import('@aws-sdk/client-s3').CompleteMultipartUploadCommandOutput>}
+ * @param s3Bucket - The S3 bucket name.
+ * @param s3Path - The S3 destination path.
+ * @param localPath - The local source path.
+ * @param isDirectory - Whether the upload source is a directory (defaults to false).
+ * @param buffer - A file buffer if local source path falsy.
  */
-export async function uploadToS3(s3Bucket, s3Path, localPath, isDirectory = false, buffer = null) {
+export async function uploadToS3(
+  s3Bucket: string,
+  s3Path: string,
+  localPath: string | null,
+  isDirectory = false,
+  buffer: Buffer | null = null,
+): Promise<CompleteMultipartUploadCommandOutput> {
   const s3 = new S3(makeS3ClientConfig());
 
-  let body;
+  let body: fs.ReadStream | string | Buffer;
   if (isDirectory) {
     body = '';
     s3Path += s3Path.endsWith('/') ? '' : '/';
@@ -89,50 +98,42 @@ export async function uploadToS3(s3Bucket, s3Path, localPath, isDirectory = fals
 /**
  * Download a file or directory from S3.
  *
- * @param {string} s3Bucket - The S3 bucket name.
- * @param {string} s3Path - The S3 source path.
- * @param {string} localPath - The local target path.
- * @param {object?} options - Optional parameters, including owner and group (optional, defaults to {}).
+ * @param s3Bucket - The S3 bucket name.
+ * @param s3Path - The S3 source path.
+ * @param localPath - The local target path.
  */
-export async function downloadFromS3(s3Bucket, s3Path, localPath, options = {}) {
+export async function downloadFromS3(s3Bucket: string, s3Path: string, localPath: string) {
   if (localPath.endsWith('/')) {
     debug(`downloadFromS3: bypassing S3 and creating directory localPath=${localPath}`);
     await fs.promises.mkdir(localPath, { recursive: true });
-    if (options.owner != null && options.group != null) {
-      await fs.promises.chown(localPath, options.owner, options.group);
-    }
     return;
   }
 
   debug(`downloadFromS3: creating containing directory for file localPath=${localPath}`);
   await fs.promises.mkdir(path.dirname(localPath), { recursive: true });
-  if (options.owner != null && options.group != null) {
-    await fs.promises.chown(path.dirname(localPath), options.owner, options.group);
-  }
 
-  const s3 = new S3(makeS3ClientConfig());
+  const s3 = new S3(makeS3ClientConfig()) as NodeJsClient<S3>;
   const res = await s3.getObject({
     Bucket: s3Bucket,
     Key: s3Path,
   });
-  const s3Stream = /** @type {import('stream').Readable} */ (res.Body);
+  if (res.Body === undefined) {
+    throw new Error('No data returned from S3');
+  }
+  const s3Stream = res.Body;
   const fileStream = fs.createWriteStream(localPath);
 
   await pipeline(s3Stream, fileStream);
-
-  if (options.owner != null && options.group != null) {
-    await fs.chown(localPath, options.owner, options.group);
-  }
 }
 
 /**
  * Delete a file or directory from S3.
  *
- * @param {string} s3Bucket - The S3 bucket name.
- * @param {string} s3Path - The S3 target path.
- * @param {boolean=} isDirectory - Whether the deletion target is a directory (defaults to false).
+ * @param s3Bucket - The S3 bucket name.
+ * @param s3Path - The S3 target path.
+ * @param isDirectory - Whether the deletion target is a directory (defaults to false).
  */
-export async function deleteFromS3(s3Bucket, s3Path, isDirectory = false) {
+export async function deleteFromS3(s3Bucket: string, s3Path: string, isDirectory = false) {
   const s3 = new S3(makeS3ClientConfig());
 
   if (isDirectory) {
@@ -148,12 +149,16 @@ export async function deleteFromS3(s3Bucket, s3Path, isDirectory = false) {
 /**
  * Get a file from S3.
  *
- * @param {string} bucket - S3 bucket name.
- * @param {string} key - The S3 target path.
- * @param {boolean} buffer - Defaults to true to return buffer.
- * @return {Promise<Buffer | import('@aws-sdk/client-s3').GetObjectOutput['Body']>} Buffer or ReadableStream type from S3 file contents.
+ * @param bucket - S3 bucket name.
+ * @param key - The S3 target path.
+ * @param buffer - Defaults to true to return buffer.
+ * @return Buffer or ReadableStream type from S3 file contents.
  */
-export async function getFromS3(bucket, key, buffer = true) {
+export async function getFromS3(
+  bucket: string,
+  key: string,
+  buffer = true,
+): Promise<Buffer | GetObjectOutput['Body']> {
   const s3 = new S3(makeS3ClientConfig());
   const res = await s3.getObject({ Bucket: bucket, Key: key });
   if (!res.Body) throw new Error('No data returned from S3');
