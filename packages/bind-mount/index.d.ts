88d49c37a03a7b3b80e09c4db35ef32020aec431
Pete Stenger <pete@stenger.io>
2024-12-31 12:21:05 -0800

    remove compiled files

--- packages/bind-mount/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Creates a bind mount at `target` that points to `source`.
- */
-export declare function mount(source: string, target: string): Promise<void>;
-/**
- * Removes the bind mount at `target`.
- */
-export declare function umount(target: string): Promise<void>;
