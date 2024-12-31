f4aee515d1e9a30fb778e30762312d223fcfe18a
Pete Stenger <pete@stenger.io>
2024-12-31 10:16:35 -0800

    init bind-mount-esm

--- packages/bind-mount/index.d.ts
+++ packages/bind-mount/index.d.ts
@@ -1,13 +1,8 @@
 /**
  * Creates a bind mount at `target` that points to `source`.
- *
- * @param source
- * @param target
  */
-export function mount(source: string, target: string): Promise<void>;
-
+export declare function mount(source: string, target: string): Promise<void>;
 /**
  * Removes the bind mount at `target`.
- * @param target
  */
-export function umount(target: string): Promise<void>;
+export declare function umount(target: string): Promise<void>;
