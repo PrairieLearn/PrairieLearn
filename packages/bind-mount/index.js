b0afd08d25ee9d03d6f59e5d1621987438c04350
Pete Stenger <pete@stenger.io>
2024-12-31 11:12:58 -0800

    Runner prettier

--- packages/bind-mount/index.js
+++ packages/bind-mount/index.js
@@ -1,43 +1,40 @@
 let addon;
 try {
-    addon = await import('bindings').then((bindings) => bindings.default('addon'));
-}
-catch {
-    addon = null;
+  addon = await import('bindings').then((bindings) => bindings.default('addon'));
+} catch {
+  addon = null;
 }
 /**
  * Creates a bind mount at `target` that points to `source`.
  */
 export function mount(source, target) {
-    if (!addon) {
-        throw new Error('Failed to load native bindings');
-    }
-    return new Promise((resolve, reject) => {
-        addon.Mount(source, target, (err) => {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve();
-            }
-        });
+  if (!addon) {
+    throw new Error('Failed to load native bindings');
+  }
+  return new Promise((resolve, reject) => {
+    addon.Mount(source, target, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
     });
+  });
 }
 /**
  * Removes the bind mount at `target`.
  */
 export function umount(target) {
-    if (!addon) {
-        throw new Error('Failed to load native bindings');
-    }
-    return new Promise((resolve, reject) => {
-        addon.Umount(target, (err) => {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve();
-            }
-        });
+  if (!addon) {
+    throw new Error('Failed to load native bindings');
+  }
+  return new Promise((resolve, reject) => {
+    addon.Umount(target, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
     });
+  });
 }
