1bff37d72c57d012c9ad90045cd921f3ded4dc14
Pete Stenger <pete@stenger.io>
2025-01-01 00:55:50 -0800

    satisfy vite import quirks

--- apps/prairielearn/src/lib/paths.ts
+++ apps/prairielearn/src/lib/paths.ts
@@ -1,8 +1,18 @@
 import * as path from 'node:path';
 
-export const REPOSITORY_ROOT_PATH = path.resolve(import.meta.dirname, '..', '..', '..', '..');
+export const REPOSITORY_ROOT_PATH = path.resolve(
+  ...(import.meta.dirname ? [import.meta.dirname] : [import.meta.url.replace('file:', ''), '..']),
+  '..',
+  '..',
+  '..',
+  '..',
+);
 
-export const APP_ROOT_PATH = path.resolve(import.meta.dirname, '..', '..');
+export const APP_ROOT_PATH = path.resolve(
+  ...(import.meta.dirname ? [import.meta.dirname] : [import.meta.url.replace('file:', ''), '..']),
+  '..',
+  '..',
+);
 
 export const EXAMPLE_COURSE_PATH = path.resolve(REPOSITORY_ROOT_PATH, 'exampleCourse');
 
