790216ddd9bf33c348ee68eb782d2d846aaf95b0
Pete Stenger <pete@stenger.io>
2025-01-03 17:27:46 -0500

    add aliases to 2d

--- apps/prairielearn/python/prairielearn.py
+++ apps/prairielearn/python/prairielearn.py
@@ -1541,7 +1541,20 @@ def latex_from_2darray(
     return "".join(rv)
 
 
-def is_correct_ndarray2D_dd(a_sub, a_tru, digits=2):  # noqa: N802
+# These are deprecated aliases that will be removed in the future.
+def is_correct_ndarray2D_dd(*args, **kwargs):  # noqa: N802
+    return is_correct_ndarray2d_dd(*args, **kwargs)
+
+
+def is_correct_ndarray2D_sf(*args, **kwargs):  # noqa: N802
+    return is_correct_ndarray2d_sf(*args, **kwargs)
+
+
+def is_correct_ndarray2D_ra(*args, **kwargs):  # noqa: N802
+    return is_correct_ndarray2d_ra(*args, **kwargs)
+
+
+def is_correct_ndarray2d_dd(a_sub, a_tru, digits=2):
     # Check if each element is correct
     m = a_sub.shape[0]
     n = a_sub.shape[1]
@@ -1554,7 +1567,7 @@ def is_correct_ndarray2D_dd(a_sub, a_tru, digits=2):  # noqa: N802
     return True
 
 
-def is_correct_ndarray2D_sf(a_sub, a_tru, digits=2):  # noqa: N802
+def is_correct_ndarray2d_sf(a_sub, a_tru, digits=2):
     # Check if each element is correct
     m = a_sub.shape[0]
     n = a_sub.shape[1]
@@ -1567,7 +1580,7 @@ def is_correct_ndarray2D_sf(a_sub, a_tru, digits=2):  # noqa: N802
     return True
 
 
-def is_correct_ndarray2D_ra(a_sub, a_tru, rtol=1e-5, atol=1e-8):  # noqa: N802
+def is_correct_ndarray2d_ra(a_sub, a_tru, rtol=1e-5, atol=1e-8):
     # Check if each element is correct
     return np.allclose(a_sub, a_tru, rtol, atol)
 
