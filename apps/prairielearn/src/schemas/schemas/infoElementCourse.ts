0be109a038aaeef8aea52d4ec6fef9b9e2a6382f
Pete Stenger <pete@stenger.io>
2025-01-05 16:43:07 -0500

    add legacy element course schema

--- apps/prairielearn/src/schemas/schemas/infoElementCourse.ts
+++ apps/prairielearn/src/schemas/schemas/infoElementCourse.ts
@@ -1,58 +1,71 @@
 import { z } from 'zod';
 
-export default z
+const LegacyDependencySchema = z
+  .object({
+    comment: z
+      .union([z.string(), z.array(z.any()), z.object({}).catchall(z.any())])
+      .describe('Arbitrary comment for reference purposes.')
+      .optional(),
+    coreStyles: z
+      .array(z.string().describe('A .css file located in /public/stylesheets.'))
+      .describe(
+        '[DEPRECATED, DO NOT USE] The styles required by this element from /public/stylesheets.',
+      )
+      .optional(),
+    coreScripts: z
+      .array(z.string().describe('A .js file located in /public/javascripts.'))
+      .describe(
+        '[DEPRECATED, DO NOT USE] The scripts required by this element from /public/javascripts.',
+      )
+      .optional(),
+    nodeModulesStyles: z
+      .array(z.string().describe('A .css file located in /node_modules.'))
+      .describe('The styles required by this element from /node_modules.')
+      .optional(),
+    nodeModulesScripts: z
+      .array(z.string().describe('A .js file located in /node_modules.'))
+      .describe('The scripts required by this element from /node_modules.')
+      .optional(),
+    clientFilesCourseStyles: z
+      .array(z.string().describe('A .css file located in clientFilesCourse.'))
+      .describe('The styles required by this element from clientFilesCourse.')
+      .optional(),
+    clientFilesCourseScripts: z
+      .array(z.string().describe('A .js file located in clientFilesCourse.'))
+      .describe('The styles required by this element from clientFilesCourse.')
+      .optional(),
+    elementStyles: z
+      .array(z.string().describe("A .css file located in the element's directory."))
+      .describe("The styles required by this element from the element's directory.")
+      .optional(),
+    elementScripts: z
+      .array(z.string().describe("A .js file located in the element's directory."))
+      .describe("The scripts required by this element from the element's directory.")
+      .optional(),
+  })
+  .strict()
+  .describe("The element's client-side dependencies.");
+
+const DependencySchema = z.intersection(
+  LegacyDependencySchema,
+  z.object({
+    coreStyles: z.undefined({
+      invalid_type_error: 'DEPRECATED -- do not use.',
+    }),
+    coreScripts: z.undefined({
+      invalid_type_error: 'DEPRECATED -- do not use.',
+    }),
+  }),
+);
+
+const LegacyElementCourseSchema = z
   .object({
     comment: z
       .union([z.string(), z.array(z.any()), z.object({}).catchall(z.any())])
       .describe('Arbitrary comment for reference purposes.')
       .optional(),
     controller: z.string().describe("The name of the element's controller file."),
-    dependencies: z
-      .object({
-        comment: z
-          .union([z.string(), z.array(z.any()), z.object({}).catchall(z.any())])
-          .describe('Arbitrary comment for reference purposes.')
-          .optional(),
-        coreStyles: z
-          .array(z.string().describe('A .css file located in /public/stylesheets.'))
-          .describe(
-            '[DEPRECATED, DO NOT USE] The styles required by this element from /public/stylesheets.',
-          )
-          .optional(),
-        coreScripts: z
-          .array(z.string().describe('A .js file located in /public/javascripts.'))
-          .describe(
-            '[DEPRECATED, DO NOT USE] The scripts required by this element from /public/javascripts.',
-          )
-          .optional(),
-        nodeModulesStyles: z
-          .array(z.string().describe('A .css file located in /node_modules.'))
-          .describe('The styles required by this element from /node_modules.')
-          .optional(),
-        nodeModulesScripts: z
-          .array(z.string().describe('A .js file located in /node_modules.'))
-          .describe('The scripts required by this element from /node_modules.')
-          .optional(),
-        clientFilesCourseStyles: z
-          .array(z.string().describe('A .css file located in clientFilesCourse.'))
-          .describe('The styles required by this element from clientFilesCourse.')
-          .optional(),
-        clientFilesCourseScripts: z
-          .array(z.string().describe('A .js file located in clientFilesCourse.'))
-          .describe('The styles required by this element from clientFilesCourse.')
-          .optional(),
-        elementStyles: z
-          .array(z.string().describe("A .css file located in the element's directory."))
-          .describe("The styles required by this element from the element's directory.")
-          .optional(),
-        elementScripts: z
-          .array(z.string().describe("A .js file located in the element's directory."))
-          .describe("The scripts required by this element from the element's directory.")
-          .optional(),
-      })
-      .strict()
-      .describe("The element's client-side dependencies.")
-      .optional(),
+    dependencies: LegacyDependencySchema.optional(),
     dynamicDependencies: z
       .object({
         comment: z
@@ -80,3 +93,12 @@ export default z
   })
   .strict()
   .describe('Info files for v3 elements.');
+
+const ElementCourseSchema = z.intersection(
+  LegacyElementCourseSchema,
+  z.object({
+    dependencies: DependencySchema.optional(),
+  }),
+);
+
+export { LegacyElementCourseSchema, ElementCourseSchema };
