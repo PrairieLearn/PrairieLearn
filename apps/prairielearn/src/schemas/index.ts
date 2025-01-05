155ff7212c3ba54abc861a79fa2f4e39903419e3
Pete Stenger <pete@stenger.io>
2025-01-05 17:30:49 -0500

    fixup exports

--- apps/prairielearn/src/schemas/index.ts
+++ apps/prairielearn/src/schemas/index.ts
@@ -5,20 +5,29 @@ import _ from 'lodash';
 import { zodToJsonSchema } from 'zod-to-json-schema';
 
 import {
-  infoAssessmentSchema,
-  infoCourseSchema,
-  infoCourseInstanceSchema,
-  infoElementCoreSchema,
-  infoElementCourseSchema,
-  infoElementExtensionSchema,
-  infoNewsItemSchema,
-  infoQuestionSchema,
-  questionOptionsCalculationSchema,
-  questionOptionsCheckboxSchema,
-  questionOptionsFileSchema,
-  questionOptionsMultipleChoiceSchema,
-  questionOptionsMultipleTrueFalseSchema,
-  questionOptionsv3Schema,
+  /* no support for deprecated options */
+  AdminQuerySchema,
+  AssessmentSchema,
+  CourseSchema,
+  CourseInstanceSchema,
+  ElementCoreSchema,
+  ElementCourseSchema,
+  ElementExtensionSchema,
+  NewsItemSchema,
+  QuestionSchema,
+  CalculationQuestionOptionsSchema,
+  CheckboxQuestionOptionsSchema,
+  FileQuestionOptionsSchema,
+  MultipleChoiceQuestionOptionsSchema,
+  MultipleTrueFalseQuestionOptionsSchema,
+  QuestionOptionsv3Schema,
+  /* supports deprecated options */
+  LegacyAssessmentSchema,
+  LegacyCourseInstanceSchema,
+  LegacyElementCoreSchema,
+  LegacyElementCourseSchema,
+  LegacyElementExtensionSchema,
+  LegacyQuestionSchema,
 } from './schemas/index.js';
 /**
  * Reads a JSON file from the schemas directory.
@@ -29,75 +38,75 @@ function readSchema(filePath: string) {
   );
 }
 
-export const infoNewsItem = zodToJsonSchema(infoNewsItemSchema, {
+export const infoNewsItem = zodToJsonSchema(NewsItemSchema, {
   name: 'News Item Info',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
-export const infoAssessment = zodToJsonSchema(infoAssessmentSchema, {
+export const infoAssessment = zodToJsonSchema(LegacyAssessmentSchema, {
   name: 'Assessment info',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
-export const infoCourse = zodToJsonSchema(infoCourseSchema, {
+export const infoCourse = zodToJsonSchema(CourseSchema, {
   name: 'Course information',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
-export const infoCourseInstance = zodToJsonSchema(infoCourseInstanceSchema, {
+export const infoCourseInstance = zodToJsonSchema(LegacyCourseInstanceSchema, {
   name: 'Course instance information',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
-export const infoElementCore = zodToJsonSchema(infoElementCoreSchema, {
+export const infoElementCore = zodToJsonSchema(LegacyElementCoreSchema, {
   name: 'Element Info',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
-export const infoElementCourse = zodToJsonSchema(infoElementCourseSchema, {
+export const infoElementCourse = zodToJsonSchema(LegacyElementCourseSchema, {
   name: 'Element Info',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
-export const infoElementExtension = zodToJsonSchema(infoElementExtensionSchema, {
+export const infoElementExtension = zodToJsonSchema(LegacyElementExtensionSchema, {
   name: 'Element Extension Info',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
-export const infoQuestion = zodToJsonSchema(infoQuestionSchema, {
+export const infoQuestion = zodToJsonSchema(LegacyQuestionSchema, {
   name: 'Question Info',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
-export const questionOptionsCalculation = zodToJsonSchema(questionOptionsCalculationSchema, {
+export const questionOptionsCalculation = zodToJsonSchema(CalculationQuestionOptionsSchema, {
   name: 'Calculation question options',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
-export const questionOptionsCheckbox = zodToJsonSchema(questionOptionsCheckboxSchema, {
+export const questionOptionsCheckbox = zodToJsonSchema(CheckboxQuestionOptionsSchema, {
   name: 'MultipleChoice question options',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
-export const questionOptionsFile = zodToJsonSchema(questionOptionsFileSchema, {
+export const questionOptionsFile = zodToJsonSchema(FileQuestionOptionsSchema, {
   name: 'File question options',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
-export const questionOptionsMultipleChoice = zodToJsonSchema(questionOptionsMultipleChoiceSchema, {
+export const questionOptionsMultipleChoice = zodToJsonSchema(MultipleChoiceQuestionOptionsSchema, {
   name: 'MultipleChoice question options',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
 });
 export const questionOptionsMultipleTrueFalse = zodToJsonSchema(
-  questionOptionsMultipleTrueFalseSchema,
+  MultipleTrueFalseQuestionOptionsSchema,
   {
     name: 'MultipleTrueFalse question options',
     nameStrategy: 'title',
     target: 'jsonSchema2019-09',
   },
 );
-export const questionOptionsv3 = zodToJsonSchema(questionOptionsv3Schema, {
+export const questionOptionsv3 = zodToJsonSchema(QuestionOptionsv3Schema, {
   name: 'v3 question options',
   nameStrategy: 'title',
   target: 'jsonSchema2019-09',
@@ -106,11 +115,11 @@ export const questionOptionsv3 = zodToJsonSchema(questionOptionsv3Schema, {
 /* Diff the two */
 /**
  * Deep diff between two object-likes
- * @param  {Object} fromObject the original object
- * @param  {Object} toObject   the updated object
- * @return {Object}            a new object which represents the diff
+ * @param  fromObject the original object
+ * @param  toObject   the updated object
+ * @return            a new object which represents the diff
  */
-function deepDiff(fromObject, toObject) {
+function deepDiff(fromObject: any, toObject: any) {
   const changes = {};
 
   const buildPath = (path, obj, key) => (_.isUndefined(path) ? key : `${path}.${key}`);
@@ -150,25 +159,25 @@ const doTest = () => {
   // properties and additionalProperties are always set
   // deprecated is not supported in Zod
   // more explicit: e.g. enums still get a type: string. additionalProperties: true/false set.
-  console.log(deepDiff(infoNewsItem, readSchema('infoNewsItem.json')));
-  console.log(deepDiff(infoAssessment, readSchema('infoAssessment.json')));
-  console.log(deepDiff(infoCourse, readSchema('infoCourse.json')));
-  console.log(deepDiff(infoCourseInstance, readSchema('infoCourseInstance.json')));
-  console.log(deepDiff(infoElementCore, readSchema('infoElementCore.json')));
-  console.log(deepDiff(infoElementCourse, readSchema('infoElementCourse.json')));
-  console.log(deepDiff(infoElementExtension, readSchema('infoElementExtension.json')));
+  // console.log(deepDiff(infoNewsItem, readSchema('infoNewsItem.json')));
+  // console.log(deepDiff(infoAssessment, readSchema('infoAssessment.json')));
+  // console.log(deepDiff(infoCourse, readSchema('infoCourse.json')));
+  // console.log(deepDiff(infoCourseInstance, readSchema('infoCourseInstance.json')));
+  // console.log(deepDiff(infoElementCore, readSchema('infoElementCore.json')));
+  // console.log(deepDiff(infoElementCourse, readSchema('infoElementCourse.json')));
+  // console.log(deepDiff(infoElementExtension, readSchema('infoElementExtension.json')));
   console.dir(infoQuestion, { depth: null });
   console.log(deepDiff(infoQuestion, readSchema('infoQuestion.json')));
-  console.log(deepDiff(questionOptionsCalculation, readSchema('questionOptionsCalculation.json')));
-  console.log(deepDiff(questionOptionsCheckbox, readSchema('questionOptionsCheckbox.json')));
-  console.log(deepDiff(questionOptionsFile, readSchema('questionOptionsFile.json')));
-  console.log(
-    deepDiff(questionOptionsMultipleChoice, readSchema('questionOptionsMultipleChoice.json')),
-  );
-  console.log(
-    deepDiff(questionOptionsMultipleTrueFalse, readSchema('questionOptionsMultipleTrueFalse.json')),
-  );
-  console.log(deepDiff(questionOptionsv3, readSchema('questionOptionsv3.json')));
+  // console.log(deepDiff(questionOptionsCalculation, readSchema('questionOptionsCalculation.json')));
+  // console.log(deepDiff(questionOptionsCheckbox, readSchema('questionOptionsCheckbox.json')));
+  // console.log(deepDiff(questionOptionsFile, readSchema('questionOptionsFile.json')));
+  // console.log(
+  //   deepDiff(questionOptionsMultipleChoice, readSchema('questionOptionsMultipleChoice.json')),
+  // );
+  // console.log(
+  //   deepDiff(questionOptionsMultipleTrueFalse, readSchema('questionOptionsMultipleTrueFalse.json')),
+  // );
+  // console.log(deepDiff(questionOptionsv3, readSchema('questionOptionsv3.json')));
 };
 
 // doTest();
--- apps/prairielearn/src/schemas/schemas/index.ts
+++ apps/prairielearn/src/schemas/schemas/index.ts
@@ -1,33 +1,41 @@
-import adminQuery from './adminQuery.js';
-import infoAssessment from './infoAssessment.js';
-import infoCourse from './infoCourse.js';
-import infoCourseInstance from './infoCourseInstance.js';
-import infoElementCore from './infoElementCore.js';
-import infoElementCourse from './infoElementCourse.js';
-import infoElementExtension from './infoElementExtension.js';
-import infoNewsItem from './infoNewsItem.js';
-import infoQuestion from './infoQuestion.js';
-import questionOptionsCalculation from './questionOptionsCalculation.js';
-import questionOptionsCheckbox from './questionOptionsCheckbox.js';
-import questionOptionsFile from './questionOptionsFile.js';
-import questionOptionsMultipleChoice from './questionOptionsMultipleChoice.js';
-import questionOptionsMultipleTrueFalse from './questionOptionsMultipleTrueFalse.js';
-import questionOptionsv3 from './questionOptionsv3.js';
+import { AdminQuerySchema } from './adminQuery.js';
+import { AssessmentSchema, LegacyAssessmentSchema } from './infoAssessment.js';
+import { CourseSchema } from './infoCourse.js';
+import { CourseInstanceSchema, LegacyCourseInstanceSchema } from './infoCourseInstance.js';
+import { ElementCoreSchema, LegacyElementCoreSchema } from './infoElementCore.js';
+import { ElementCourseSchema, LegacyElementCourseSchema } from './infoElementCourse.js';
+import { ElementExtensionSchema, LegacyElementExtensionSchema } from './infoElementExtension.js';
+import { NewsItemSchema } from './infoNewsItem.js';
+import { QuestionSchema, LegacyQuestionSchema } from './infoQuestion.js';
+import { CalculationQuestionOptionsSchema } from './questionOptionsCalculation.js';
+import { CheckboxQuestionOptionsSchema } from './questionOptionsCheckbox.js';
+import { FileQuestionOptionsSchema } from './questionOptionsFile.js';
+import { MultipleChoiceQuestionOptionsSchema } from './questionOptionsMultipleChoice.js';
+import { MultipleTrueFalseQuestionOptionsSchema } from './questionOptionsMultipleTrueFalse.js';
+import { QuestionOptionsv3Schema } from './questionOptionsv3.js';
 
 export {
-  adminQuery as adminQuerySchema,
-  infoAssessment as infoAssessmentSchema,
-  infoCourse as infoCourseSchema,
-  infoCourseInstance as infoCourseInstanceSchema,
-  infoElementCore as infoElementCoreSchema,
-  infoElementCourse as infoElementCourseSchema,
-  infoElementExtension as infoElementExtensionSchema,
-  infoNewsItem as infoNewsItemSchema,
-  infoQuestion as infoQuestionSchema,
-  questionOptionsCalculation as questionOptionsCalculationSchema,
-  questionOptionsCheckbox as questionOptionsCheckboxSchema,
-  questionOptionsFile as questionOptionsFileSchema,
-  questionOptionsMultipleChoice as questionOptionsMultipleChoiceSchema,
-  questionOptionsMultipleTrueFalse as questionOptionsMultipleTrueFalseSchema,
-  questionOptionsv3 as questionOptionsv3Schema,
+  /* no support for deprecated options */
+  AdminQuerySchema,
+  AssessmentSchema,
+  CourseSchema,
+  CourseInstanceSchema,
+  ElementCoreSchema,
+  ElementCourseSchema,
+  ElementExtensionSchema,
+  NewsItemSchema,
+  QuestionSchema,
+  CalculationQuestionOptionsSchema,
+  CheckboxQuestionOptionsSchema,
+  FileQuestionOptionsSchema,
+  MultipleChoiceQuestionOptionsSchema,
+  MultipleTrueFalseQuestionOptionsSchema,
+  QuestionOptionsv3Schema,
+  /* supports deprecated options */
+  LegacyAssessmentSchema,
+  LegacyCourseInstanceSchema,
+  LegacyElementCoreSchema,
+  LegacyElementCourseSchema,
+  LegacyElementExtensionSchema,
+  LegacyQuestionSchema,
 };
