06312f4f2552ebab0310cda648747e1536bc0703
Dave Mussulman <mussulma@gmail.com>
2025-01-10 09:50:10 -0600

    review fixes

--- apps/prairielearn/src/middlewares/selectAndAuthzAssessment.html.ts
+++ apps/prairielearn/src/middlewares/selectAndAuthzAssessment.html.ts
@@ -18,19 +18,19 @@ export function AccessDenied({ resLocals }: { resLocals: Record<string, any> }) 
             <div class="card-header bg-primary text-white">Assessment unavailable</div>
             <div class="card-body">
               <p>This assessment's configuration does not allow you to access it right now.</p>
-              <p>This is the intended behavior based on your current time or other factors.</p>
-              <p>
+              <p>This is the intended behavior based on the current time or other factors.</p>
+              <div>
                 <a
                   href="/pl/course_instance/${resLocals.course_instance.id}/assessments"
                   class="btn btn-primary"
                 >
-                  Go to Assessments
+                  Go to assessments
                 </a>
                 <a href="${config.urlPrefix}" class="btn btn-primary">
                   <i class="fa fa-home" aria-hidden="true"></i>
                   PrairieLearn home
                 </a>
-              </p>
+              </div>
             </div>
           </div>
         </main>
