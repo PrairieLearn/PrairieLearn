cc0c05e34d39c6c22ce4d78699cbc9ad32c5ef78
Pete Stenger <pete@stenger.io>
2025-01-12 15:23:54 -0500

    ruff check --fix --unsafe-fixes

--- tools/api_download.py
+++ tools/api_download.py
@@ -50,13 +50,13 @@ def download_course_instance(args, logfile):
     log(logfile, f"starting download at {local_iso_time()} ...")
     start_time = time.time()
     course_instance_path = f"/course_instances/{args.course_instance_id}"
-    course_instance_info = get_and_save_json(
+    get_and_save_json(
         course_instance_path, "course_instance_info", args, logfile
     )
-    gradebook = get_and_save_json(
+    get_and_save_json(
         f"{course_instance_path}/gradebook", "gradebook", args, logfile
     )
-    course_instance_access_rules = get_and_save_json(
+    get_and_save_json(
         f"{course_instance_path}/course_instance_access_rules",
         "course_instance_access_rules",
         args,
@@ -74,7 +74,7 @@ def download_course_instance(args, logfile):
             logfile,
         )
 
-        assessment_access_rules = get_and_save_json(
+        get_and_save_json(
             f"{course_instance_path}/assessments/{assessment['assessment_id']}/assessment_access_rules",
             f"assessment_{assessment['assessment_id']}_access_rules",
             args,
@@ -82,21 +82,21 @@ def download_course_instance(args, logfile):
         )
 
         for assessment_instance in assessment_instances:
-            instance_questions = get_and_save_json(
+            get_and_save_json(
                 f"{course_instance_path}/assessment_instances/{assessment_instance['assessment_instance_id']}/instance_questions",
                 f"assessment_instance_{assessment_instance['assessment_instance_id']}_instance_questions",
                 args,
                 logfile,
             )
 
-            submissions = get_and_save_json(
+            get_and_save_json(
                 f"{course_instance_path}/assessment_instances/{assessment_instance['assessment_instance_id']}/submissions",
                 f"assessment_instance_{assessment_instance['assessment_instance_id']}_submissions",
                 args,
                 logfile,
             )
 
-            submission_log = get_and_save_json(
+            get_and_save_json(
                 f"{course_instance_path}/assessment_instances/{assessment_instance['assessment_instance_id']}/log",
                 f"assessment_instance_{assessment_instance['assessment_instance_id']}_log",
                 args,
