f175f0f72f1fd2d61468d98779337b53645d78e1
Pete Stenger <pete@stenger.io>
2025-01-13 17:59:20 -0500

    make typechecker happy

--- graders/python/python_autograder/pl_execute.py
+++ graders/python/python_autograder/pl_execute.py
@@ -212,9 +212,9 @@ def execute_code(
             import matplotlib as mpl
 
             mpl.use("Agg")
-            import matplotlib as mpl
+            import matplotlib.pyplot as plt
 
-            plot_value = mpl.pyplot
+            plot_value = plt
 
     # Re-seed before running tests
     set_random_seed()
