6bd83fc73df4b8aa47bcdab9c7b008e11bf7e855
Pete Stenger <pete@stenger.io>
2025-01-13 15:44:54 -0500

    Resolve RSE rules

--- apps/prairielearn/python/python_helper_sympy.py
+++ apps/prairielearn/python/python_helper_sympy.py
@@ -371,7 +371,7 @@ def sympy_check(
         elif isinstance(item, sympy.Float):
             raise HasFloatError(float(str_item))
         elif not allow_complex and item == sympy.I:
-            raise HasComplexError()
+            raise HasComplexError("complex values not allowed")
 
         work_stack.extend(item.args)
 
@@ -433,7 +433,7 @@ def evaluate_with_source(
     try:
         res = eval_expr(code, local_dict, global_dict)
     except Exception as exc:
-        raise BaseSympyError() from exc
+        raise BaseSympyError from exc
 
     # Finally, check for invalid symbols
     sympy_check(res, locals_for_eval, allow_complex=allow_complex)
