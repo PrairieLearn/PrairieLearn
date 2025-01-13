7923c8ed5bcc9d57090bef3d6bf5ce7a537e3df6
miguelaenlle <bot.developer3@gmail.com>
2025-01-13 13:20:08 -0600

    Removed bootstrap-version meta tag; updated bootstrap-compat-utils and affected files accordingly

--- apps/prairielearn/src/components/HeadContents.html.ts
+++ apps/prairielearn/src/components/HeadContents.html.ts
@@ -29,7 +29,6 @@ export function HeadContents(titleOptions: TitleOptions) {
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="bootstrap-version" content="5" />
     ${config.cookieDomain
       ? html`<meta name="cookie-domain" content="${config.cookieDomain}" />`
       : ''}
