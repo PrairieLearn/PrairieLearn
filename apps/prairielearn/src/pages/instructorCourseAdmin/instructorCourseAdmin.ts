1b26b931e0b10f288e9a36ccff9230cda0059342
miguelaenlle <bot.developer3@gmail.com>
2025-01-13 20:26:53 -0600

    Improved code commenting on instructorCourseAdmin.ts

--- apps/prairielearn/src/pages/instructorCourseAdmin/instructorCourseAdmin.ts
+++ apps/prairielearn/src/pages/instructorCourseAdmin/instructorCourseAdmin.ts
@@ -12,12 +12,14 @@ router.get(
       course_id: res.locals.course.id,
     });
 
-    if (courseHasCourseInstances) {
-      // Redirect to the instances page of the course
-      res.redirect(`/pl/course/${res.locals.course.id}/course_admin/instances`);
-    } else {
-      // Redirect to the questions page of the course
+    if (!courseHasCourseInstances) {
+      // For brand new courses, we believe users want to create questions first
+      // since questions are what make PrairieLearn unique, and many new users likely
+      // aren't thinking of immediately offering anything to students
       res.redirect(`/pl/course/${res.locals.course.id}/course_admin/questions`);
+    } else {
+      // Once users have created course instances, they should have easier access to them
+      res.redirect(`/pl/course/${res.locals.course.id}/course_admin/instances`);
     }
   }),
 );
