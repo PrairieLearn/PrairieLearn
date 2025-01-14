bef3e1470dbbd4ad728a0c6f197362e65ed4db54
miguelaenlle <bot.developer3@gmail.com>
2025-01-13 22:52:20 -0600

    Started UI implementation of the Onboarding Tasks checklist

--- apps/prairielearn/src/pages/instructorCourseAdminOnboarding/instructorCourseAdminOnboarding.html.ts
+++ apps/prairielearn/src/pages/instructorCourseAdminOnboarding/instructorCourseAdminOnboarding.html.ts
@@ -4,7 +4,19 @@ import { HeadContents } from '../../components/HeadContents.html.js';
 import { Navbar } from '../../components/Navbar.html.js';
 import { CourseSyncErrorsAndWarnings } from '../../components/SyncErrorsAndWarnings.html.js';
 
-export function InstructorCourseAdminOnboarding({ resLocals }: { resLocals: Record<string, any> }) {
+export function InstructorCourseAdminOnboarding({
+  courseHasCourseStaff,
+  courseHasQuestion,
+  courseHasCourseInstance,
+  courseHasAssessment,
+  resLocals,
+}: {
+  courseHasCourseStaff: boolean;
+  courseHasQuestion: boolean;
+  courseHasCourseInstance: boolean;
+  courseHasAssessment: boolean;
+  resLocals: Record<string, any>;
+}) {
   return html`
     <!doctype html>
     <html lang="en">
@@ -24,15 +36,83 @@ export function InstructorCourseAdminOnboarding({ resLocals }: { resLocals: Reco
               <h1>Onboarding Tasks</h1>
             </div>
             <div class="my-4 card-body text-left" style="text-wrap: balance;">
-              <p class="font-weight-bold">Complete course setup</p>
-              <p class="mb-0">
-                Here are some task suggestions to help you finish setting up your course.
-              </p>
-              <div>
+              <div class="container">
+                <p class="mb-3">
+                  Here are some task suggestions to help you finish setting up your course.
+                </p>
+                <div class="list-group">
+                  ${OnboardingStep({
+                    stepNumber: 1,
+                    header: 'Add Course Staff (Optional)',
+                    description:
+                      'Invite users to the course staff to help manage and deliver the course. If you are working alone, you can skip this step.',
+                    link: 'staff',
+                    complete: courseHasCourseStaff,
+                  })}
+                  ${OnboardingStep({
+                    stepNumber: 2,
+                    header: 'Create Your First Question',
+                    description:
+                      "A question is a problem or task that tests a student's understanding of a specific concept.",
+                    link: 'questions',
+                    complete: courseHasQuestion,
+                  })}
+                  ${OnboardingStep({
+                    stepNumber: 3,
+                    header: 'Create a Course Instance',
+                    description:
+                      'A course instance contains the assessments and other configuration for a single offering of a course.',
+                    link: 'instances',
+                    complete: courseHasCourseInstance,
+                  })}
+                  ${OnboardingStep({
+                    stepNumber: 4,
+                    header: 'Create an Assessment',
+                    description:
+                      "An assessment is a collection of questions designed to build or evaluate a student's knowledge.",
+                    link: 'test.com',
+                    complete: courseHasAssessment,
+                  })}
+                </div>
+              </div>
             </div>
           </div>
         </main>
       </body>
     </html>
   `.toString();
+}
+
+function OnboardingStep({
+  stepNumber,
+  header,
+  description,
+  link,
+  complete,
+}: {
+  stepNumber: number;
+  header: string;
+  description: string;
+  link: string;
+  complete: boolean;
+}) {
+  return html`
+    <div class="list-group-item">
+      <div class="d-flex align-items-center gap-3">
+        <input
+          type="checkbox"
+          class="custom-control-input"
+          id="customCheck1"
+          ${complete ? 'checked' : ''}
+          disabled
+        />
+        <div>
+          <a href="${link}">
+            <p class="my-0">${stepNumber}. ${header}</p>
+          </a>
+          <p class="text-muted my-0">${description}</p>
+        </div>
+      </div>
+    </div>
+  `;
 }
--- apps/prairielearn/src/pages/instructorCourseAdminOnboarding/instructorCourseAdminOnboarding.ts
+++ apps/prairielearn/src/pages/instructorCourseAdminOnboarding/instructorCourseAdminOnboarding.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import asyncHandler from 'express-async-handler';
 
+import { selectCourseHasCourseInstances } from '../../models/course-instances.js';
+
 import { InstructorCourseAdminOnboarding } from './instructorCourseAdminOnboarding.html.js';
 
 const router = express.Router();
@@ -8,7 +10,24 @@ const router = express.Router();
 router.get(
   '/',
   asyncHandler(async (req, res) => {
-    res.send(InstructorCourseAdminOnboarding({ resLocals: res.locals }));
+    const course_id = res.locals.course?.id ?? null;
+
+    const courseHasCourseStaff = false;
+    const courseHasQuestion = false;
+    const courseHasCourseInstance = course_id
+      ? await selectCourseHasCourseInstances({ course_id })
+      : false;
+    const courseHasAssessment = false;
+
+    res.send(
+      InstructorCourseAdminOnboarding({
+        resLocals: res.locals,
+        courseHasCourseStaff,
+        courseHasQuestion,
+        courseHasCourseInstance,
+        courseHasAssessment,
+      }),
+    );
   }),
 );
 
