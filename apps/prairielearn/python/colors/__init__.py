c28afcc076ce2dfa8f376db0db8f42fc86e65bfe
Pete Stenger <pete@stenger.io>
2025-01-14 14:47:28 -0500

    Revert to __init__.py files

--- apps/prairielearn/python/colors/__init__.py
+++ apps/prairielearn/python/colors/__init__.py
@@ -1,4 +1,7 @@
 # WARNING: This file is deprecated and will be removed in the future.
 # Please import from prairielearn.colors instead.
+import sys
+
+sys.path.append("..")
 
 from prairielearn.colors import *  # noqa: F403
--- apps/prairielearn/python/to_precision/__init__.py
+++ apps/prairielearn/python/to_precision/__init__.py
@@ -1,4 +1,7 @@
 # WARNING: This file is deprecated and will be removed in the future.
 # Please import from prairielearn.to_precision instead.
+import sys
+
+sys.path.append("..")
 
 from prairielearn.to_precision import *  # noqa: F403
