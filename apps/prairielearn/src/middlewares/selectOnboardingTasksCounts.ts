cf5c4a749c9358a3b14a35756dc18552cd8c2468
miguelaenlle <bot.developer3@gmail.com>
2025-01-16 15:05:15 -0600

    Triggered getOnboardingSteps to run only if there is a course id and if onboarding is shown

--- apps/prairielearn/src/middlewares/selectOnboardingTasksCounts.ts
+++ apps/prairielearn/src/middlewares/selectOnboardingTasksCounts.ts
@@ -5,11 +5,18 @@ import { getOnboardingSteps } from '../lib/onboarding.js';
 export default asyncHandler(async (req, res, next) => {
   const course_id = res.locals.course?.id;
 
-  const steps = await getOnboardingSteps({
-    course_id,
-  });
+  if (course_id && res.locals.course.show_onboarding) {
+    const steps = await getOnboardingSteps({
+      course_id,
+    });
 
-  res.locals.navbarCompleteOnboardingTasksCount = steps.filter((step) => step.isComplete).length;
-  res.locals.navbarTotalOnboardingTasksCount = steps.length;
+    res.locals.navbarCompleteOnboardingTasksCount = steps.filter((step) => step.isComplete).length;
+    res.locals.navbarTotalOnboardingTasksCount = steps.length;
+  } else {
+    // Defensively set to zero, since most uses of navbarCompleteOnboardingTasksCount and
+    // navbarTotalOnboardingTasksCount treat them as non-optional values.
+    res.locals.navbarCompleteOnboardingTasksCount = 0;
+    res.locals.navbarTotalOnboardingTasksCount = 0;
+  }
   next();
 });
