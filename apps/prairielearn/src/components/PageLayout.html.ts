bd07a918e750343590a2d5bbe31dd74a9966cd77
miguelaenlle <bot.developer3@gmail.com>
2025-01-17 15:40:54 -0600

    Adjusted JSDoc comments further

--- apps/prairielearn/src/components/PageLayout.html.ts
+++ apps/prairielearn/src/components/PageLayout.html.ts
@@ -5,18 +5,18 @@ import { Navbar } from './Navbar.html.js';
 import type { NavbarType, NavPage, NavSubPage } from './Navbar.types.js';
 
 /**
- * Manages rendering the head, navbar, and content of a page.
+ * Manages the rendering of the head, navbar, and content of a page.
  *
- * @param resLocals - The locals object from the Express response.
- * @param pageTitle - The title of the page in the browser.
- * @param navPage - The main page to highlight in the navbar.
- * @param {options.fullWidth} - Whether the main container should span the full width.
- * @param {options.marginBottom} - Whether the main container should have a bottom margin of mb-4 in Bootstrap.
- * @param {options.pageNote} - A note to display after the title, shown in parenthesis.
- * @param {options.navSubPage} - The subpage to highlight in the navbar.
- * @param {options.navbarType} - The type of navbar to render, based on the type of user.
- * @param {headContent} - Include scripts and other additional head content here.
- * @param content - The main content of the page within the main container.
+ * @param resLocals The locals object from the Express response.
+ * @param pageTitle The title of the page in the browser.
+ * @param navPage The main page to highlight in the navbar.
+ * @param options.fullWidth Whether the main container should span the entire width of the page.
+ * @param options.marginBottom Whether the main container should have a bottom margin of mb-4 in Bootstrap.
+ * @param options.pageNote A note to display after the pageTitle, shown in parenthesis.
+ * @param options.navSubPage The current subpage, used by the navbar.
+ * @param options.navbarType The type of navbar to render, based on the type of user.
+ * @param headContent Include scripts and other additional head content here.
+ * @param content The main content of the page within the main container.
  */
 export function PageLayout({
   resLocals,
