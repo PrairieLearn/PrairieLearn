0a723f528bb0e1d698d1f9416863ec90ee4fff96
Eliot Robson <eliot.robson24@gmail.com>
2025-01-19 10:54:40 -0600

    Ruff

--- exampleCourse/questions/demo/questionParamsRandomSymbolic/server.py
+++ exampleCourse/questions/demo/questionParamsRandomSymbolic/server.py
@@ -1,27 +1,27 @@
-import numpy
+import numpy as np
 import sympy
 
 
 def generate(data):
-    coeff_lower_bound = data['params'].get('coeff_lower_bound', -9)
-    coeff_upper_bound = data['params'].get('coeff_upper_bound', 9)
+    coeff_lower_bound = data["params"].get("coeff_lower_bound", -9)
+    coeff_upper_bound = data["params"].get("coeff_upper_bound", 9)
 
     # Create a variable
-    x = sympy.symbols('x')
+    x = sympy.symbols("x")
 
     # Randomize the degree
-    degree = numpy.random.random_integers(1,5)
+    degree = np.random.random_integers(1, 5)
 
     # Randomize the coefficients (make sure the leading coefficient is non-zero)
-    coeffs = numpy.random.random_integers(coeff_lower_bound,coeff_upper_bound,degree+1)
-    if coeffs[0]==0:
-        coeffs[0]=1
+    coeffs = np.random.random_integers(coeff_lower_bound, coeff_upper_bound, degree + 1)
+    if coeffs[0] == 0:
+        coeffs[0] = 1
 
     # Create the polynomial
-    f = sympy.Poly(coeffs,x).as_expr()
+    f = sympy.Poly(coeffs, x).as_expr()
 
     # Find derivative with respect to x
-    df = sympy.diff(f,x)
+    df = sympy.diff(f, x)
 
     # Modify data and return
     data["params"]["x"] = sympy.latex(x)
