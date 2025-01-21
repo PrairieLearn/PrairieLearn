c5bdf4e6ae8fb9591d6aec3eb2ea415f1fb8c564
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-01-21 10:41:34 -0800

    Refine example question

--- exampleCourse/questions/demo/workspace/vscode-cpp/solution.cpp
+++ exampleCourse/questions/demo/workspace/vscode-cpp/solution.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 using namespace std;
 
-/*
-  Write a program that outputs the perimeter of a rectangle of a user-provided integer length and width.
-  Your output must match the example exactly, including the whitespace and the newline character at the end.
-    Enter the length: 5
-    Enter the width: 4
-    The perimeter of the rectangle is: 18
-*/
-int main(void) {
+int main() {
     int length, width;
     cout << "Enter the length: ";
     cin >> length;
--- exampleCourse/questions/demo/workspace/vscode-cpp/workspace/source.cpp
+++ exampleCourse/questions/demo/workspace/vscode-cpp/workspace/source.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
-    
+int main() {
     return 0;
 }
