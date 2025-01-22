90dab6b87c22457e86ae12346158f8dfd79e2b2e
miguelaenlle <bot.developer3@gmail.com>
2025-01-21 19:44:53 -0600

    Updated PageLayout page; added NavbarContext

--- apps/prairielearn/src/components/PageLayout.html.ts
+++ apps/prairielearn/src/components/PageLayout.html.ts
@@ -2,24 +2,26 @@ import { html, type HtmlValue } from '@prairielearn/html';
 
 import { HeadContents } from './HeadContents.html.js';
 import { Navbar } from './Navbar.html.js';
-import type { NavbarType, NavPage, NavSubPage } from './Navbar.types.js';
+import type { NavContext } from './Navbar.types.js';
 
 export function PageLayout({
   resLocals,
   pageTitle,
-  navPage,
+  navContext,
   options = {
     marginBottom: true,
   },
   headContent,
+  preContent,
   content,
+  postContent,
 }: {
   /** The locals object from the Express response. */
   resLocals: Record<string, any>;
   /** The title of the page in the browser. */
   pageTitle: string;
-  /** The main page to highlight in the navbar. */
-  navPage: NavPage;
+  /** The information used to configure the navbar. */
+  navContext: NavContext;
   options?: {
     /** Whether the main container should span the entire width of the page. */
     fullWidth?: boolean;
@@ -27,19 +29,20 @@ export function PageLayout({
     marginBottom?: boolean;
     /** A note to display after the pageTitle, shown in parenthesis. */
     pageNote?: string;
-    /** The current subpage, accounted for in the navbar. */
-    navSubPage?: NavSubPage;
-    /** The type of navbar to render, based on the type of user. */
-    navbarType?: NavbarType;
     /** Enables an htmx extension for an element and all its children */
     hxExt?: string;
   };
   /** Include scripts and other additional head content here. */
   headContent?: HtmlValue;
+  /** The content of the page in the body before the main container. */
+  preContent?: HtmlValue;
   /** The main content of the page within the main container. */
   content: HtmlValue;
+  /** The content of the page in the body after the main container. */
+  postContent?: HtmlValue;
 }) {
   const marginBottom = options.marginBottom ?? true;
+
   return html`
     <!doctype html>
     <html lang="en">
@@ -54,9 +57,11 @@ export function PageLayout({
       <body ${options.hxExt ? `hx-ext="${options.hxExt}"` : ''}>
         ${Navbar({
           resLocals,
-          navPage,
-          navSubPage: options.navSubPage,
+          navPage: navContext.page,
+          navSubPage: navContext.subPage,
+          navbarType: navContext.type,
         })}
+        ${preContent}
         <main
           id="content"
           class="
@@ -66,6 +71,7 @@ export function PageLayout({
         >
           ${content}
         </main>
+        ${postContent}
       </body>
     </html>
   `.toString();
