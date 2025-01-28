0db8bbc9b70cecd4f274c7540d542d8403cf0e0f
Miguel Aenlle <66172204+miguelaenlle@users.noreply.github.com>
2025-01-27 20:57:41 -0600

    Implemented PageLayout for Assessment Admin Pages (#11205)
    * Started implementation of default unused assessment sets and question tags
    
    * Started pagelayout implementation
    
    * Implemented the page layout component and corresponding comments.
    
    * Adjusted JSDoc comments further
    
    * Adjusted comments
    
    * Removed console log and reverted changes on course-db.ts
    
    * Adjusted navSubPage comment
    
    * Adjusted class
    
    * Moved comments over properties of PageLayout
    
    * Fixed marginBottom default issue
    
    * Set assessments questions page to use full width
    
    * Started PageLayout updates for assessment admin pages
    
    * Implemented PageLayout on remaining pages
    
    * Addressed TODOs
    
    * Added proper subpage to instructorAssessmentUploads
    
    * Adjusted pageTitle values
    
    * Adjusted pageTitles and other minor details
    
    * Patch linetr issue in instructorAssessmentAccess

--- apps/prairielearn/src/pages/instructorAssessmentQuestions/instructorAssessmentQuestions.html.ts
+++ apps/prairielearn/src/pages/instructorAssessmentQuestions/instructorAssessmentQuestions.html.ts
@@ -4,10 +4,9 @@ import { html } from '@prairielearn/html';
 import { run } from '@prairielearn/run';
 
 import { AssessmentBadge } from '../../components/AssessmentBadge.html.js';
-import { HeadContents } from '../../components/HeadContents.html.js';
 import { IssueBadge } from '../../components/IssueBadge.html.js';
 import { Modal } from '../../components/Modal.html.js';
-import { Navbar } from '../../components/Navbar.html.js';
+import { PageLayout } from '../../components/PageLayout.html.js';
 import { AssessmentSyncErrorsAndWarnings } from '../../components/SyncErrorsAndWarnings.html.js';
 import { SyncProblemButton } from '../../components/SyncProblemButton.html.js';
 import { TagBadgeList } from '../../components/TagBadge.html.js';
@@ -57,64 +56,66 @@ export function InstructorAssessmentQuestions({
   resLocals: Record<string, any>;
   questions: AssessmentQuestionRow[];
 }) {
-  return html`
-    <!doctype html>
-    <html lang="en">
-      <head>
-        ${HeadContents({ resLocals })}
-        ${compiledScriptTag('instructorAssessmentQuestionsClient.ts')}
-      </head>
-      <body>
-        ${Navbar({ resLocals })}
-        <main id="content" class="container-fluid">
-          ${Modal({
-            id: 'resetQuestionVariantsModal',
-            title: 'Confirm reset question variants',
-            body: html`
-              <p>
-                Are your sure you want to reset all current variants of this question?
-                <strong>All ungraded attempts will be lost.</strong>
-              </p>
-              <p>Students will receive a new variant the next time they view this question.</p>
-            `,
-            footer: html`
-              <input type="hidden" name="__action" value="reset_question_variants" />
-              <input type="hidden" name="__csrf_token" value="${resLocals.__csrf_token}" />
-              <input
-                type="hidden"
-                name="unsafe_assessment_question_id"
-                class="js-assessment-question-id"
-                value=""
-              />
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-              <button type="submit" class="btn btn-danger">Reset question variants</button>
-            `,
-          })}
-          ${AssessmentSyncErrorsAndWarnings({
-            authz_data: resLocals.authz_data,
-            assessment: resLocals.assessment,
-            courseInstance: resLocals.course_instance,
-            course: resLocals.course,
-            urlPrefix: resLocals.urlPrefix,
-          })}
+  return PageLayout({
+    resLocals,
+    pageTitle: 'Questions',
+    headContent: [compiledScriptTag('instructorAssessmentQuestionsClient.ts')],
+    navContext: {
+      type: 'instructor',
+      page: 'assessment',
+      subPage: 'questions',
+    },
+    options: {
+      fullWidth: true,
+    },
+    content: html`
+      ${AssessmentSyncErrorsAndWarnings({
+        authz_data: resLocals.authz_data,
+        assessment: resLocals.assessment,
+        courseInstance: resLocals.course_instance,
+        course: resLocals.course,
+        urlPrefix: resLocals.urlPrefix,
+      })}
 
-          <div class="card mb-4">
-            <div class="card-header bg-primary text-white d-flex align-items-center">
-              <h1>${resLocals.assessment_set.name} ${resLocals.assessment.number}: Questions</h1>
-            </div>
-            ${AssessmentQuestionsTable({
-              questions,
-              assessmentType: resLocals.assessment.type,
-              urlPrefix: resLocals.urlPrefix,
-              hasCoursePermissionPreview: resLocals.authz_data.has_course_permission_preview,
-              hasCourseInstancePermissionEdit:
-                resLocals.authz_data.has_course_instance_permission_edit,
-            })}
-          </div>
-        </main>
-      </body>
-    </html>
-  `.toString();
+      <div class="card mb-4">
+        <div class="card-header bg-primary text-white d-flex align-items-center">
+          <h1>${resLocals.assessment_set.name} ${resLocals.assessment.number}: Questions</h1>
+        </div>
+        ${AssessmentQuestionsTable({
+          questions,
+          assessmentType: resLocals.assessment.type,
+          urlPrefix: resLocals.urlPrefix,
+          hasCoursePermissionPreview: resLocals.authz_data.has_course_permission_preview,
+          hasCourseInstancePermissionEdit: resLocals.authz_data.has_course_instance_permission_edit,
+        })}
+      </div>
+    `,
+    postContent: html`
+      ${Modal({
+        id: 'resetQuestionVariantsModal',
+        title: 'Confirm reset question variants',
+        body: html`
+          <p>
+            Are your sure you want to reset all current variants of this question?
+            <strong>All ungraded attempts will be lost.</strong>
+          </p>
+          <p>Students will receive a new variant the next time they view this question.</p>
+        `,
+        footer: html`
+          <input type="hidden" name="__action" value="reset_question_variants" />
+          <input type="hidden" name="__csrf_token" value="${resLocals.__csrf_token}" />
+          <input
+            type="hidden"
+            name="unsafe_assessment_question_id"
+            class="js-assessment-question-id"
+            value=""
+          />
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+          <button type="submit" class="btn btn-danger">Reset question variants</button>
+        `,
+      })}
+    `,
+  });
 }
 
 function AssessmentQuestionsTable({
