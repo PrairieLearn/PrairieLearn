f6f8eee5f48aece686b426e1500fda5a68aae1fc
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-01-29 15:14:15 -0800

    WIP DOM and styles for improved scrolling

--- apps/prairielearn/src/ee/pages/instructorAiGenerateDraftEditor/instructorAiGenerateDraftEditor.html.ts
+++ apps/prairielearn/src/ee/pages/instructorAiGenerateDraftEditor/instructorAiGenerateDraftEditor.html.ts
@@ -1,8 +1,9 @@
 import { html } from '@prairielearn/html';
 import { run } from '@prairielearn/run';
 
+import { HeadContents } from '../../../components/HeadContents.html.js';
 import { Modal } from '../../../components/Modal.html.js';
-import { PageLayout } from '../../../components/PageLayout.html.js';
+import { Navbar } from '../../../components/Navbar.html.js';
 import { QuestionContainer } from '../../../components/QuestionContainer.html.js';
 import { compiledScriptTag, nodeModulesAssetPath } from '../../../lib/assets.js';
 import { type Question, type AiQuestionGenerationPrompt } from '../../../lib/db-types.js';
@@ -18,119 +19,160 @@ export function InstructorAiGenerateDraftEditor({
   question: Question;
   variantId?: string | undefined;
 }) {
-  return PageLayout({
-    resLocals,
-    pageTitle: 'Generate question with AI',
-    navContext: {
-      type: 'instructor',
-      page: 'course_admin',
-      subPage: 'questions',
-    },
-    options: {
-      hxExt: 'loading-states',
-    },
-    headContent: html`
-      ${compiledScriptTag('question.ts')}
-      <script defer src="${nodeModulesAssetPath('mathjax/es5/startup.js')}"></script>
-    `,
-    content: html`
-      <div class="mb-3">
-        <a href="${resLocals.urlPrefix}/ai_generate_question_drafts" class="btn btn-sm btn-primary">
-          <i class="fa fa-arrow-left" aria-hidden="true"></i>
-          Back to draft questions
-        </a>
-      </div>
-      <div class="card">
-        <div
-          class="card-header bg-primary text-white d-flex align-items-center justify-content-between"
-        >
-          Generate question with AI
+  return html`
+    <!doctype html>
+    <html lang="en">
+      <head>
+        ${HeadContents({ resLocals })} ${compiledScriptTag('question.ts')}
+        <script defer src="${nodeModulesAssetPath('mathjax/es5/startup.js')}"></script>
+        <style>
+          body,
+          .app-container {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            overscroll-behavior: none;
+          }
 
-          <div>
-            <button
-              type="button"
-              class="btn btn-sm btn-light"
-              data-toggle="modal"
-              data-target="#finalizeModal"
-            >
-              <i class="fa fa-check" aria-hidden="true"></i>
-              Finalize question
-            </button>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="container">
-            <div class="row">
-              <div class="col-12 col-md-6">
-                <div>
-                  ${PromptHistory({ prompts, urlPrefix: resLocals.urlPrefix })}
-                  <form
-                    hx-post="${variantId
-                      ? html`${resLocals.urlPrefix}/ai_generate_editor/${question.id}?variant_id=${variantId}`
-                      : html`${resLocals.urlPrefix}/ai_generate_editor/${question.id}`}"
-                    hx-swap="outerHTML"
-                    hx-disabled-elt="button"
-                  >
-                    <input type="hidden" name="__csrf_token" value="${resLocals.__csrf_token}" />
-                    <input type="hidden" name="__action" value="regenerate_question" />
-                    <div class="form-group">
-                      <label for="user-prompt-llm">What needs to be changed?</label>
-                      <textarea name="prompt" id="user-prompt-llm" class="form-control"></textarea>
-                    </div>
-                    <button class="btn btn-primary">
-                      <span
-                        class="spinner-grow spinner-grow-sm d-none"
-                        role="status"
-                        aria-hidden="true"
-                        data-loading-class-remove="d-none"
-                      ></span>
-                      Adjust question
+          .app-grid {
+            display: grid;
+            grid-template-rows: min-content 1fr;
+            grid-template-areas: 'navbar' 'content';
+          }
+
+          .app-navbar {
+            grid-area: navbar;
+          }
+
+          .app-content {
+            grid-area: content;
+            overflow-y: auto;
+          }
+
+          @media (min-width: 768px) {
+            .app-grid {
+              height: 100%;
+            }
+          }
+        </style>
+      </head>
+      <body hx-ext="loading-states">
+        <div class="app-container">
+          <div class="app-grid">
+            <div class="app-navbar">${Navbar({ navPage: 'course_admin', resLocals })}</div>
+            <main id="content" class="container mb-4 app-content">
+              <div class="mb-3">
+                <a
+                  href="${resLocals.urlPrefix}/ai_generate_question_drafts"
+                  class="btn btn-sm btn-primary"
+                >
+                  <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                  Back to draft questions
+                </a>
+              </div>
+              <div class="card">
+                <div
+                  class="card-header bg-primary text-white d-flex align-items-center justify-content-between"
+                >
+                  Generate question with AI
+
+                  <div>
+                    <button
+                      type="button"
+                      class="btn btn-sm btn-light"
+                      data-toggle="modal"
+                      data-target="#finalizeModal"
+                    >
+                      <i class="fa fa-check" aria-hidden="true"></i>
+                      Finalize question
                     </button>
-                  </form>
-                </div>
-              </div>
-              <div class="col-12 col-md-6">
-                <ul class="nav nav-pills mb-2">
-                  <li class="nav-item">
-                    <a
-                      class="nav-link active"
-                      data-toggle="tab"
-                      aria-current="page"
-                      href="#question-preview"
-                      >Question preview</a
-                    >
-                  </li>
-                  <li class="nav-item">
-                    <a a class="nav-link" data-toggle="tab" href="#question-code"
-                      >Question source</a
-                    >
-                  </li>
-                </ul>
-                <div class="tab-content">
-                  <div role="tabpanel" id="question-preview" class="tab-pane active">
-                    ${QuestionContainer({ resLocals, questionContext: 'instructor' })}
-                  </div>
-                  <div role="tabpanel" id="question-code" class="tab-pane">
-                    ${QuestionCodePanel({
-                      htmlContents: prompts[prompts.length - 1].html,
-                      pythonContents: prompts[prompts.length - 1].python,
-                    })}
                   </div>
                 </div>
+                <div class="card-body">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-12 col-md-6">
+                        <div>
+                          ${PromptHistory({ prompts, urlPrefix: resLocals.urlPrefix })}
+                          <form
+                            hx-post="${variantId
+                              ? html`${resLocals.urlPrefix}/ai_generate_editor/${question.id}?variant_id=${variantId}`
+                              : html`${resLocals.urlPrefix}/ai_generate_editor/${question.id}`}"
+                            hx-swap="outerHTML"
+                            hx-disabled-elt="button"
+                          >
+                            <input
+                              type="hidden"
+                              name="__csrf_token"
+                              value="${resLocals.__csrf_token}"
+                            />
+                            <input type="hidden" name="__action" value="regenerate_question" />
+                            <div class="form-group">
+                              <label for="user-prompt-llm">What needs to be changed?</label>
+                              <textarea
+                                name="prompt"
+                                id="user-prompt-llm"
+                                class="form-control"
+                              ></textarea>
+                            </div>
+                            <button class="btn btn-primary">
+                              <span
+                                class="spinner-grow spinner-grow-sm d-none"
+                                role="status"
+                                aria-hidden="true"
+                                data-loading-class-remove="d-none"
+                              ></span>
+                              Adjust question
+                            </button>
+                          </form>
+                        </div>
+                      </div>
+                      <div class="col-12 col-md-6">
+                        <ul class="nav nav-pills mb-2">
+                          <li class="nav-item">
+                            <a
+                              class="nav-link active"
+                              data-toggle="tab"
+                              aria-current="page"
+                              href="#question-preview"
+                              >Question preview</a
+                            >
+                          </li>
+                          <li class="nav-item">
+                            <a a class="nav-link" data-toggle="tab" href="#question-code"
+                              >Question source</a
+                            >
+                          </li>
+                        </ul>
+                        <div class="tab-content">
+                          <div role="tabpanel" id="question-preview" class="tab-pane active">
+                            ${QuestionContainer({ resLocals, questionContext: 'instructor' })}
+                          </div>
+                          <div role="tabpanel" id="question-code" class="tab-pane">
+                            ${QuestionCodePanel({
+                              htmlContents: prompts[prompts.length - 1].html,
+                              pythonContents: prompts[prompts.length - 1].python,
+                            })}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  ${FinalizeModal({ csrfToken: resLocals.__csrf_token })}
+                </div>
+                <div class="card-footer">
+                  <a href="#" role="button" data-toggle="modal" data-target="#finalizeModal"
+                    >Finalize this question</a
+                  >
+                  to use it on assessments and make manual edits.
+                </div>
               </div>
-            </div>
+            </main>
           </div>
-          ${FinalizeModal({ csrfToken: resLocals.__csrf_token })}
         </div>
-        <div class="card-footer">
-          <a href="#" role="button" data-toggle="modal" data-target="#finalizeModal"
-            >Finalize this question</a
-          >
-          to use it on assessments and make manual edits.
-        </div>
-      </div>
-    `,
-  });
+      </body>
+    </html>
+  `.toString();
 }
 
 function PromptHistory({
