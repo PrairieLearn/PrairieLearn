4db533e2ae8b4b084c4d7740d771925eb82e9048
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-02-05 14:24:18 -0800

    Delete test component

--- apps/prairielearn/assets/scripts/split-bundles/react-fragments/OtherTestingComponent.tsx
+++
@@ -1,7 +0,0 @@
-import { registerReactFragment } from '../../behaviors/react-fragments/index.js';
-
-registerReactFragment('OtherTestingComponent', () => <h1>Hello, world!</h1>);
-
-(async () => {
-  console.log(import('lodash'));
-})().catch(console.error);
