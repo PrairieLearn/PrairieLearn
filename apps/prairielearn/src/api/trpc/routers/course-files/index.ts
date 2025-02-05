1e1718160e473eb62fc4f4e2f5ffcbc73d8e8373
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-02-05 09:20:28 -0800

    Rename file to match operation name

--- apps/prairielearn/src/api/trpc/routers/course-files/index.ts
+++ apps/prairielearn/src/api/trpc/routers/course-files/index.ts
@@ -1,7 +1,7 @@
 import { router } from '../../trpc.js';
 
+import { batchDeleteQuestions } from './batchDeleteQuestions.js';
 import { createQuestion } from './createQuestion.js';
-import { batchDeleteQuestions } from './deleteQuestions.js';
 import { updateQuestionFiles } from './updateQuestionFiles.js';
 
 export const courseFilesRouter = router({
