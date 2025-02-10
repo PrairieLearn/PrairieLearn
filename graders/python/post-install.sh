195ad1dd7c3371ef95516d2dae3e9b13dc2331a0
Pete Stenger <pete@stenger.io>
2025-02-10 15:24:33 -0600

    use /usr/bin/env bash

--- graders/python/post-install.sh
+++ graders/python/post-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "Setting up autograder..."
 useradd ag
 chmod +x /python_autograder/run.sh
--- workspaces/desktop/post-install.sh
+++ workspaces/desktop/post-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -ex
 
 cp /root/.profile /opt/defaults/.profile
