3ef3837d38b3a2a57894494358d5d8a6d6d2107a
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-02-10 14:50:25 -0800

    Working tags/topics syncing

--- apps/prairielearn/src/sync/fromDisk/tags.ts
+++ apps/prairielearn/src/sync/fromDisk/tags.ts
@@ -1,10 +1,30 @@
 import { z } from 'zod';
 
-import * as sqldb from '@prairielearn/postgres';
+import {
+  callAsync,
+  loadSqlEquiv,
+  queryAsync,
+  queryRows,
+  runInTransactionAsync,
+} from '@prairielearn/postgres';
+import { run } from '@prairielearn/run';
 
-import { IdSchema } from '../../lib/db-types.js';
+import { TagSchema, type Tag } from '../../lib/db-types.js';
 import { type CourseData } from '../course-db.js';
 import * as infofile from '../infofile.js';
+
+import { determineOperationsForEntities } from './entity-list.js';
+
+const sql = loadSqlEquiv(import.meta.url);
+
+interface DesiredTag {
+  // TODO: this must be non-null in the database schema.
+  name: string | null;
+  // TODO: make these all non-nullable once we make them non-null in the database schema.
+  color: string | null;
+  description: string | null;
+  number: number | null;
+}
 
 export async function sync(
   courseId: string,
@@ -19,13 +39,6 @@ export async function sync(
   );
   const deleteUnused = isInfoCourseValid && areAllInfoQuestionsValid;
 
-  let courseTags: string[] = [];
-  if (!infofile.hasErrors(courseData.course)) {
-    courseTags = (courseData.course.data?.tags ?? []).map((t) =>
-      JSON.stringify([t.name, t.description, t.color]),
-    );
-  }
-
   const knownQuestionTagsNames = new Set<string>();
   Object.values(courseData.questions).forEach((q) => {
     if (!infofile.hasErrors(q)) {
@@ -34,22 +47,79 @@ export async function sync(
   });
   const questionTagNames = [...knownQuestionTagsNames];
 
-  const newTags = await sqldb.callRow(
-    'sync_course_tags',
-    [!infofile.hasErrors(courseData.course), deleteUnused, courseTags, questionTagNames, courseId],
-    z.array(z.tuple([z.string(), IdSchema])),
-  );
+  const existingTags = await queryRows(sql.select_tags, { course_id: courseId }, TagSchema);
 
-  const tagIdsByName = new Map(newTags);
+  // Based on the set of desired tags, determine which ones must be
+  // added, updated, or deleted.
+  const {
+    entitiesToCreate: tagsToCreate,
+    entitiesToUpdate: tagsToUpdate,
+    entitiesToDelete: tagsToDelete,
+  } = determineOperationsForEntities<DesiredTag>({
+    courseEntities: courseData.course.data?.tags ?? [],
+    existingEntities: existingTags,
+    knownNames: knownQuestionTagsNames,
+    // TODO: add missing tests; confirm behavior for topics too.
+    makeImplicitEntity: (name) => ({
+      name,
+    }),
+    isInfoCourseValid,
+    deleteUnused,
+  });
+
+  const newTags = await runInTransactionAsync(async () => {
+    const insertedTags = await run(async () => {
+      if (tagsToCreate.length === 0) return [];
+
+      return queryRows(
+        sql.insert_tags,
+        {
+          course_id: courseId,
+          tags: tagsToCreate.map((t) =>
+            JSON.stringify([t.name, t.description, t.color, t.number, t.implicit]),
+          ),
+        },
+        TagSchema,
+      );
+    });
+
+    if (tagsToUpdate.length > 0) {
+      await queryAsync(sql.update_tags, {
+        course_id: courseId,
+        tags: tagsToUpdate.map((t) =>
+          JSON.stringify([t.name, t.description, t.color, t.number, t.implicit]),
+        ),
+      });
+    }
+
+    if (tagsToDelete.length > 0) {
+      await queryAsync(sql.delete_tags, {
+        course_id: courseId,
+        tags: tagsToDelete,
+      });
+    }
+
+    return insertedTags;
+  });
+
+  const tagIdsByName = new Map<string, Tag>();
+
+  for (const tag of existingTags) {
+    tagIdsByName.set(tag.name, tag);
+  }
+
+  for (const tag of newTags) {
+    tagIdsByName.set(tag.name, tag);
+  }
 
   const questionTagsParam: string[] = [];
   Object.entries(courseData.questions).forEach(([qid, question]) => {
     if (infofile.hasErrors(question)) return;
     const dedupedQuestionTagNames = new Set<string>();
     (question.data?.tags ?? []).forEach((t) => dedupedQuestionTagNames.add(t));
-    const questionTagIds = [...dedupedQuestionTagNames].map((t) => tagIdsByName.get(t));
+    const questionTagIds = [...dedupedQuestionTagNames].map((t) => tagIdsByName.get(t)?.id);
     questionTagsParam.push(JSON.stringify([questionIds[qid], questionTagIds]));
   });
 
-  await sqldb.callAsync('sync_question_tags', [questionTagsParam]);
+  await callAsync('sync_question_tags', [questionTagsParam]);
 }
