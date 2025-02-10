94d8dc1b70b3428d7ee9f0eb1a45c35e01ba4d2f
miguelaenlle <bot.developer3@gmail.com>
2025-02-10 14:56:09 -0600

    Revert changes to instructorAssessmentInstancesClient.tsx

--- apps/prairielearn/assets/scripts/instructorAssessmentInstancesClient.tsx
+++ apps/prairielearn/assets/scripts/instructorAssessmentInstancesClient.tsx
@@ -1,7 +1,7 @@
 import { Temporal } from '@js-temporal/polyfill';
 import { on } from 'delegated-events';
-import { Fragment, h, render } from 'preact';
-import { useState } from 'preact/hooks';
+import { h, render, Fragment } from 'preact';
+import React, { useState } from 'preact/hooks';
 
 import { onDocumentReady, templateFromAttributes } from '@prairielearn/browser-utils';
 import { formatDate } from '@prairielearn/formatter';
