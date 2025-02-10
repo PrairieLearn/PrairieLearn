33fc420f6bd471f87aea7ff1f7daeee587dd4ac5
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2025-02-10 09:28:47 -0800

    address review comments

--- apps/prairielearn/assets/scripts/lib/qrCodeButton.ts
+++ apps/prairielearn/assets/scripts/lib/qrCodeButton.ts
@@ -1,17 +1,23 @@
 import QR from 'qrcode-svg';
+import { observe } from 'selector-observer';
 
 import { onDocumentReady, parseHTMLElement } from '@prairielearn/browser-utils';
 
 onDocumentReady(() => {
-  const el = document.querySelector('.js-qrcode-button') as HTMLElement;
-  const content = el.dataset.qrCodeContent;
-  if (!content) return;
-  const qrCodeSvg = new QR({ content, container: 'svg-viewbox' }).svg();
-  new window.bootstrap.Popover(el, {
-    content: parseHTMLElement(document, qrCodeSvg),
-    html: true,
-    trigger: 'click',
-    container: 'body',
-    customClass: 'popover-narrow-fixed',
+  observe('.js-qrcode-button', {
+    add(el) {
+      if (!(el instanceof HTMLElement)) return;
+      const content = el.dataset.qrCodeContent;
+      if (content) {
+        const qrCodeSvg = new QR({ content, container: 'svg-viewbox' }).svg();
+        new window.bootstrap.Popover(el, {
+          content: parseHTMLElement(document, qrCodeSvg),
+          html: true,
+          trigger: 'click',
+          container: 'body',
+          customClass: 'popover-narrow-fixed',
+        });
+      }
+    },
   });
 });
