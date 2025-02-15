bb02f3219f138c50461a3bac9c096063b0979fc0
Miguel Aenlle <66172204+miguelaenlle@users.noreply.github.com>
2025-02-15 12:51:57 -0600

    PageLayout for institution admin pages and other missed pages (#11324)
    * Implemented PageLayout for institution admin pages
    
    * Added missing institution to resLocals
    
    * Implemented PageLayout for AI Question Generation page
    
    * Added PageLayout to Effective User page
    
    * Implemented PageLayout for error page
    
    * Removed unused imports from instructorEffectiveUser
    
    * Removed unused imports in instructorEffectiveUser HTML
    
    * Reverted changes on instructorEffectiveUser.ts
    
    * Fix for tags
    
    * Fix the for attributes of name, uid, and uin again

--- apps/prairielearn/src/ee/pages/administratorInstitutionAdmins/administratorInstitutionAdmins.html.ts
+++ apps/prairielearn/src/ee/pages/administratorInstitutionAdmins/administratorInstitutionAdmins.html.ts
@@ -1,7 +1,6 @@
 import { html } from '@prairielearn/html';
 
-import { HeadContents } from '../../../components/HeadContents.html.js';
-import { Navbar } from '../../../components/Navbar.html.js';
+import { PageLayout } from '../../../components/PageLayout.html.js';
 import { type Institution } from '../../../lib/db-types.js';
 
 export function AdministratorInstitutionAdmins({
@@ -11,26 +10,22 @@ export function AdministratorInstitutionAdmins({
   institution: Institution;
   resLocals: Record<string, any>;
 }) {
-  return html`
-    <!doctype html>
-    <html lang="en">
-      <head>
-        ${HeadContents({ resLocals, pageTitle: 'Admins - Institution Admin' })}
-      </head>
-      <body>
-        ${Navbar({
-          resLocals: { ...resLocals, institution },
-          navbarType: 'administrator_institution',
-          navPage: 'administrator_institution',
-          navSubPage: 'admins',
-        })}
-        <main class="container mb-4">
-          <p>
-            Institution administrators can be managed
-            <a href="/pl/institution/${institution.id}/admin/admins">here</a>.
-          </p>
-        </main>
-      </body>
-    </html>
-  `.toString();
+  return PageLayout({
+    resLocals: {
+      ...resLocals,
+      institution,
+    },
+    pageTitle: 'Admins - Institution Admin',
+    navContext: {
+      type: 'administrator_institution',
+      page: 'administrator_institution',
+      subPage: 'admins',
+    },
+    content: html`
+      <p>
+        Institution administrators can be managed
+        <a href="/pl/institution/${institution.id}/admin/admins">here</a>.
+      </p>
+    `,
+  });
 }
