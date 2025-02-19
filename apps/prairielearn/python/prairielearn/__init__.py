c80d4dc453070245cb257edbe2c2dfdba0a9eb09
Pete Stenger <peter.a.stenger@gmail.com>
2025-02-18 23:27:53 -0600

    python: Hide sidebar, split up unicode_utils (#11387)
    * improvements
    
    * test suite
    
    * ruff
    
    * update settings.json to indicate to copilot chat that we should use pytest
    
    * parametrize pytest
    
    * format
    
    * PR comments

--- apps/prairielearn/python/prairielearn/__init__.py
+++ apps/prairielearn/python/prairielearn/__init__.py
@@ -1,1 +1,6 @@
 from prairielearn.core import *  # noqa: F403
+
+# TODO: Re-export until downstream code is checked and updated
+from prairielearn.unicode_utils import (
+    escape_unicode_string as escape_unicode_string,  # noqa: PLC0414
+)
