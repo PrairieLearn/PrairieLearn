6ef235c2f968f51b2fd3afbfeacae299fb07a048
Pete Stenger <pete@stenger.io>
2025-02-19 00:53:23 -0600

    Update documentation

--- apps/prairielearn/python/prairielearn/__init__.py
+++ apps/prairielearn/python/prairielearn/__init__.py
@@ -3,8 +3,5 @@ from prairielearn.conversion_utils import *  # noqa: F403
 from prairielearn.extension_utils import *  # noqa: F403
 from prairielearn.grading_utils import *  # noqa: F403
 from prairielearn.misc_utils import *  # noqa: F403
-
-# TODO: Re-export until downstream code is checked and updated
-from prairielearn.unicode_utils import (
-    escape_unicode_string as escape_unicode_string,  # noqa: PLC0414
-)
+from prairielearn.sympy_utils import *  # noqa: F403
+from prairielearn.unicode_utils import *  # noqa: F403
