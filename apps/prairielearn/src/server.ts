f7456e2e970f4eea040cf304c3eb6e041341ac56
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-02-20 12:19:43 -0800

    Add route to redirect to question based on QID (#11408)
    * Add route to redirect to question based on QID
    
    * Add tests; ensure params are forwarded
    
    * Use route params instead of search params
    
    * Fix failing tests
    
    * Exclude pages from accessibility tests

--- apps/prairielearn/src/server.ts
+++ apps/prairielearn/src/server.ts
@@ -73,6 +73,7 @@ import * as serverJobs from './lib/server-jobs.js';
 import { PostgresSessionStore } from './lib/session-store.js';
 import * as socketServer from './lib/socket-server.js';
 import { SocketActivityMetrics } from './lib/telemetry/socket-activity-metrics.js';
+import { getSearchParams } from './lib/url.js';
 import * as workspace from './lib/workspace.js';
 import { markAllWorkspaceHostsUnhealthy } from './lib/workspaceHost.js';
 import { enterpriseOnly } from './middlewares/enterpriseOnly.js';
@@ -1118,7 +1119,7 @@ export async function initExpress(): Promise<Express> {
       res.redirect(
         url.format({
           pathname: `${req.params[0]}/preview`,
-          search: url.parse(req.originalUrl).search,
+          search: getSearchParams(req).toString(),
         }),
       );
     },
@@ -1596,7 +1597,7 @@ export async function initExpress(): Promise<Express> {
     res.redirect(
       url.format({
         pathname: `${req.params[0]}/preview`,
-        search: new URL(req.originalUrl, `${req.protocol}://${req.headers.host}/`).search,
+        search: getSearchParams(req).toString(),
       }),
     );
   });
