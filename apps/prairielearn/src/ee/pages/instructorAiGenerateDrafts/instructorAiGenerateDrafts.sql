a1060fad2fe1b128722c2c3ce3b78b821caa5f1c
Jake Levine <jlevine4@illinois.edu>
2025-02-20 16:49:05 -0600

    natural sort drafts

--- apps/prairielearn/src/ee/pages/instructorAiGenerateDrafts/instructorAiGenerateDrafts.sql
+++ apps/prairielearn/src/ee/pages/instructorAiGenerateDrafts/instructorAiGenerateDrafts.sql
@@ -13,7 +13,25 @@ WHERE
   AND q.draft IS TRUE
   AND q.deleted_at IS NULL
   AND q.qid IS NOT NULL
+  -- Source: http://www.rhodiumtoad.org.uk/junk/naturalsort.sql
 ORDER BY
+  (
+    SELECT
+      string_agg(
+        convert_to(
+          coalesce(
+            r[2],
+            length(length(r[1])::text) || length(r[1])::text || r[1]
+          ),
+          'SQL_ASCII'
+        ),
+        '\x00'
+      )
+    FROM
+      regexp_matches(qid, '0*([0-9]+)|([^0-9]+)', 'g') r
+  ) ASC,
+  -- In case two assessments have the same number, fall back to
+  -- lexical ordering by the qid to ensure a stable sort.
   qid ASC;
 
 -- BLOCK select_draft_questions_by_course_id
