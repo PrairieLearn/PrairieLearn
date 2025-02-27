888dcb2cb7be65a9b1ef398cc101ab6a0412ad99
Pete Stenger <pete@stenger.io>
2025-02-27 17:05:25 -0600

    test

--- docker/test_python.sh
+++ docker/test_python.sh
@@ -2,4 +2,4 @@
 
 make -s -C /PrairieLearn test-python
 # Generate coverage.xml for codecov
-cd /PrairieLearn && python3 -m coverage xml -o apps/prairielearn/python/coverage.xml
+cd /PrairieLearn && python3 -m coverage xml -o /PrairieLearn/apps/prairielearn/python/coverage.xml
