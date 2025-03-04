59ef8ab7317178e8b4b0319eb5ce041e35817c3c
miguelaenlle <bot.developer3@gmail.com>
2025-03-04 15:47:35 -0600

    Added mouseover once and focus once hx triggers to switcher buttons

--- apps/prairielearn/src/components/SideNav.html.ts
+++ apps/prairielearn/src/components/SideNav.html.ts
@@ -256,7 +256,7 @@ function CourseInstanceNav({
             data-boundary="window"
             hx-get="/pl/navbar/course/${resLocals.course.id}/course_instance_switcher/${resLocals
               .course_instance.id}"
-            hx-trigger="show.bs.dropdown once delay:200ms"
+            hx-trigger="mouseover once, focus once, show.bs.dropdown once delay:200ms"
             hx-target="#sideNavCourseInstancesDropdownContent"
           >
             <span> ${resLocals.course_instance.short_name} </span>
