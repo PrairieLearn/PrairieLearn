20fb96100353754f78997590515acac4af56eaa2
miguelaenlle <bot.developer3@gmail.com>
2025-03-04 17:23:31 -0600

    Added return of empty string when navPageTabs is empty in NavbarContext

--- apps/prairielearn/src/components/NavbarContext.html.ts
+++ apps/prairielearn/src/components/NavbarContext.html.ts
@@ -16,7 +16,10 @@ export function ContextNavigation({
   if (!navPage) return '';
 
   const navPagesTabs = getNavPageTabs(resLocals.has_enhanced_navigation);
-  const navPageTabs = navPagesTabs[navPage] ?? []; // Some navPages do not have tabs
+  const navPageTabs = navPagesTabs[navPage]; 
+  
+  // Some navPages do not have tabs
+  if (!navPageTabs) return '';
 
   return html`
     <nav>
