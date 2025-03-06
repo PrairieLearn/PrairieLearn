2182f941b3727a29907d450d666ae4f0ecbaf998
ZacWarham <zacwarham@gmail.com>
2025-03-06 13:26:18 -0800

    Revert "Partially move code to component for explanation"
    This reverts commit 9ac51341fe0a8d252828df1a312e359a0eb7e688.

--- apps/prairielearn/src/components/AssessmentSetHeading.html.ts
+++ apps/prairielearn/src/components/AssessmentSetHeading.html.ts
@@ -1,32 +1,8 @@
 import { html } from '@prairielearn/html';
-import type { AssessmentSet } from '../lib/db-types.js';
 
 export function MissingDefinition({ item }) {
   return html`<span class="text-muted">
     (Auto-generated from use in an assessment; add this assessment ${item.toLowerCase()} to your
-    infoCourse.json file to customize)</span>
-  `;
-}
-
-export function AssessmentSetHeading({
-  assessment_set,
- }: {
-  assessment_set: AssessmentSet
- }) {
-  return html`
-      <tr>
-        <td class="align-middle">${assessment_set.number}</td>
-        <td class="align-middle">
-          <span class="badge color-${assessment_set.color}">
-            ${assessment_set.abbreviation}
-          </span>
-        </td>
-        <td class="align-middle">${assessment_set.name}</td>
-        <td class="align-middle">
-          ${assessment_set.heading}
-          ${assessment_set.implicit && MissingDefinition({ item: 'Set' })}
-        </td>
-        <td class="align-middle">${assessment_set.color}</td>
-      </tr>
-  `;
+    infoCourse.json file to customize)</span
+  >`;
 }
