153918f74615a855673599d452500e67e906c598
Jonatan Schroeder <jonatan@yorku.ca>
2025-03-07 16:06:39 -0500

    Remove test for other Python file

--- graders/c/cgrader/entrypoint.sh
+++ graders/c/cgrader/entrypoint.sh
@@ -5,18 +5,9 @@
 # file if it exists. 
 
 if [[ -r /grade/tests/test.py ]]; then
-    python3 /grade/tests/test.py
-    exit $?
+    exec python3 /grade/tests/test.py
 fi
 
-# If /grade/tests/test.py does not exist, but there is a single .py file in the
-# /grade/tests directory, run that file. 
-
-single_py_file=$(find /grade/tests -maxdepth 1 -name '*.py' -type f -readable)
-if [[ $(echo "$single_py_file" | wc -w) -eq 1 ]]; then
-    python3 "$single_py_file"
-else
-    # If we cannot reliably identify the starting script, we will return an error.
-    mkdir -p /grade/results
-    echo '{"gradable": false, "message": "Error: Grader set up without an entrypoint and no default grading script."}' > /grade/results/results.json
-fi
+# If we cannot identify the starting script, we will return an error.
+mkdir -p /grade/results
+echo '{"gradable": false, "message": "Error: Grader set up without an entrypoint and no default grading script."}' > /grade/results/results.json
