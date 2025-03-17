a7d0586c5a35fd3b01b76d93c7b8bea9895c2c63
miguelaenlle <bot.developer3@gmail.com>
2025-03-17 16:18:35 -0500

    sideNavSettings used new_state directly for what's returned

--- apps/prairielearn/src/pages/sideNavSettings/sideNavSettings.ts
+++ apps/prairielearn/src/pages/sideNavSettings/sideNavSettings.ts
@@ -6,7 +6,7 @@ router.put('/', async (req, res) => {
   const new_state = Boolean(req.body.side_nav_expanded);
   req.session.side_nav_expanded = new_state;
   res.status(200).json({
-    side_nav_expanded: req.session.side_nav_expanded,
+    side_nav_expanded: new_state,
   });
 });
 
