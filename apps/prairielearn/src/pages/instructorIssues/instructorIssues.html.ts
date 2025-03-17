cd379d92e474bab0dc816d26237f15a92e5fe369
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-03-17 08:15:17 -0700

    Update apps/prairielearn/src/pages/instructorIssues/instructorIssues.html.ts
    Co-authored-by: Jonatan Schroeder <jonatan@yorku.ca>

--- apps/prairielearn/src/pages/instructorIssues/instructorIssues.html.ts
+++ apps/prairielearn/src/pages/instructorIssues/instructorIssues.html.ts
@@ -397,9 +397,9 @@ function FilterHelpModal() {
             </td>
             <td>
               Shows all issues with a question ID like <code>QID</code>; supports <code>*</code> as
-              a wildcard. For example <code>qid:graphConnectivity</code> would show all issues
+              a wildcard. For example, <code>qid:graphConnectivity</code> shows all issues
               associated with the question <code>graphConnectivity</code>, and
-              <code>qid:graph*</code> would show all issues associated with any question that starts
+              <code>qid:graph*</code> shows all issues associated with any question that starts
               with <code>graph</code>, such as <code>graphConnectivity</code> and
               <code>graphTheory</code>.
             </td>
