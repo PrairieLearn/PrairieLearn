c70cc8db681796c6e8b4e70069e2ce723696e558
ZacWarham <zacwarham@gmail.com>
2025-03-18 15:52:12 -0700

    make format

--- apps/prairielearn/src/components/TagDescription.html.ts
+++ apps/prairielearn/src/components/TagDescription.html.ts
@@ -2,18 +2,14 @@ import { html } from '@prairielearn/html';
 
 import { type Tag } from '../lib/db-types.js';
 
-export function TagDescription({
-    tag_description,
-}: {
-    tag_description: Tag;
-}) {
-    if(!tag_description.implicit) {
-        return tag_description.description;
-    }
+export function TagDescription({ tag_description }: { tag_description: Tag }) {
+  if (!tag_description.implicit) {
+    return tag_description.description;
+  }
 
-    return html`
+  return html`
     <span class="text-muted">
-        Auto-generated from use in a question; add this tag to your infoCourse.json file to customize
-      </span>
-    `;
+      Auto-generated from use in a question; add this tag to your infoCourse.json file to customize
+    </span>
+  `;
 }
--- apps/prairielearn/src/components/TopicDescription.html.ts
+++ apps/prairielearn/src/components/TopicDescription.html.ts
@@ -2,18 +2,15 @@ import { html } from '@prairielearn/html';
 
 import { type Topic } from '../lib/db-types.js';
 
-export function TopicDescription({
-    topic_description,
-}: {
-    topic_description: Topic;
-}) {
-    if(!topic_description.implicit) {
-        return topic_description.description;
-    }
+export function TopicDescription({ topic_description }: { topic_description: Topic }) {
+  if (!topic_description.implicit) {
+    return topic_description.description;
+  }
 
-    return html`
+  return html`
     <span class="text-muted">
-        Auto-generated from use in a question; add this topic to your infoCourse.json file to customize
-      </span>
-    `;
+      Auto-generated from use in a question; add this topic to your infoCourse.json file to
+      customize
+    </span>
+  `;
 }
