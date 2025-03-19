35c9cfd8751b388ca5f6d5ff1734bb0f8246a4bd
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-03-19 16:25:51 -0700

    Refactor existing LTI 1.3 tests

--- apps/prairielearn/src/ee/tests/lti13-auth.test.ts
+++ apps/prairielearn/src/ee/tests/lti13-auth.test.ts
@@ -1,5 +1,7 @@
 import { assert } from 'chai';
+import fetchCookie from 'fetch-cookie';
 import { step } from 'mocha-steps';
+import fetch from 'node-fetch';
 
 import { config } from '../../lib/config.js';
 import { features } from '../../lib/features/index.js';
@@ -7,6 +9,8 @@ import { fetchCheerio, getCSRFToken } from '../../tests/helperClient.js';
 import * as helperServer from '../../tests/helperServer.js';
 
 import { enableEnterpriseEdition } from './ee-helpers.js';
+
+const CLIENT_ID = '123456789';
 
 const siteUrl = 'http://localhost:' + config.serverPort;
 
@@ -45,6 +49,7 @@ describe('LTI 1.3 auth', () => {
         __action: 'update_platform',
         __csrf_token: getCSRFToken(createRes.$),
         platform: 'Canvas Production',
+        client_id: CLIENT_ID,
         issuer_params: JSON.stringify({
           issuer: 'https://canvas.instructure.com',
           jwks_uri: 'https://sso.canvaslms.com/api/lti/security/jwks',
@@ -81,5 +86,65 @@ describe('LTI 1.3 auth', () => {
       }),
     });
     assert.equal(updateRes.status, 200);
+  });
+
+  step('check metadata', async () => {
+    const url = `${siteUrl}/pl/lti13_instance/1/config`;
+    const data = await fetch(url).then((res) => res.json() as any);
+
+    console.log(data);
+    assert.equal(data.title, 'PrairieLearn');
+    assert.equal(data.oidc_initiation_url, `${siteUrl}/pl/lti13_instance/1/auth/login`);
+    assert.equal(data.target_link_uri, `${siteUrl}/pl/lti13_instance/1/auth/callback`);
+    assert.equal(data.public_jwk_url, `${siteUrl}/pl/lti13_instance/1/jwks`);
+    assert.isObject(data.custom_fields);
+    assert.equal(data.custom_fields.uin, '$Canvas.user.sisIntegrationId');
+  });
+
+  step('enable LTI 1.3 auth for institution', async () => {
+    const url = `${siteUrl}/pl/administrator/institution/1/sso`;
+    const res = await fetchCheerio(url);
+    assert.equal(res.status, 200);
+
+    const updateRes = await fetchCheerio(url, {
+      method: 'POST',
+      body: new URLSearchParams({
+        __csrf_token: getCSRFToken(res.$),
+        // The provider ID is hardcoded in a migration.
+        enabled_authn_provider_ids: '6',
+      }),
+    });
+    assert.equal(updateRes.status, 200);
+  });
+
+  step('simulate login via LTI', async () => {
+    const url = `${siteUrl}/pl/lti13_instance/1/auth/login`;
+    const res = await fetch(url, {
+      method: 'POST',
+      body: new URLSearchParams({
+        iss: 'https://canvas.instructure.com',
+        client_id: CLIENT_ID,
+        login_hint: 'student@example.edu',
+        target_link_uri: `${siteUrl}/pl/lti13_instance/1/auth/callback`,
+      }),
+      redirect: 'manual',
+    });
+    assert.equal(res.status, 302);
+    const location = res.headers.get('location');
+    assert.isTrue(location?.startsWith('https://sso.canvaslms.com/api/lti/authorize_redirect'));
+  });
+
+  step('simulate callback from LTI', async () => {
+    const url = `${siteUrl}/pl/lti13_instance/1/auth/callback`;
+    const res = await fetch(url, {
+      method: 'POST',
+      body: new URLSearchParams({
+        state: '...',
+        id_token: '...',
+      }),
+    });
+    console.log(res.status);
+    console.log(res.headers);
+    console.log(await res.text());
   });
 });
--- apps/prairielearn/src/tests/lti13.test.ts
+++ apps/prairielearn/src/tests/lti13.test.ts
@@ -36,6 +36,194 @@ async function withServer<T>(app: express.Express, port: number, fn: () => Promi
   }
 }
 
+async function makeLoginExecutor({
+  user,
+  fetchWithCookies,
+  oidcProviderPort,
+  keystore,
+}: {
+  user: {
+    name: string;
+    email: string;
+    uin: string;
+  };
+  fetchWithCookies: typeof fetch;
+  oidcProviderPort: number;
+  keystore: nodeJose.JWK.KeyStore;
+}) {
+  const startLoginResponse = await fetchWithCookies(`${siteUrl}/pl/lti13_instance/1/auth/login`, {
+    method: 'POST',
+    body: new URLSearchParams({
+      iss: siteUrl,
+      login_hint: 'fef15674-ae78-4763-b915-6fe3dbf42c67',
+      target_link_uri: `${siteUrl}/pl/lti13_instance/1/course_navigation`,
+    }),
+    redirect: 'manual',
+  });
+  assert.equal(startLoginResponse.status, 302);
+
+  const redirectUrl = new URL(startLoginResponse.headers.get('location') as string);
+  assert.equal(redirectUrl.hostname, 'localhost');
+  assert.equal(redirectUrl.pathname, '/auth');
+  assert.equal(redirectUrl.searchParams.get('client_id'), CLIENT_ID);
+  assert.equal(redirectUrl.searchParams.get('scope'), 'openid');
+  assert.equal(redirectUrl.searchParams.get('response_type'), 'id_token');
+  assert.equal(redirectUrl.searchParams.get('response_mode'), 'form_post');
+  assert.equal(
+    redirectUrl.searchParams.get('redirect_uri'),
+    `${siteUrl}/pl/lti13_instance/1/auth/callback`,
+  );
+  assert.equal(redirectUrl.searchParams.get('login_hint'), 'fef15674-ae78-4763-b915-6fe3dbf42c67');
+  assert.ok(redirectUrl.searchParams.get('nonce'));
+  assert.ok(redirectUrl.searchParams.get('state'));
+
+  const redirectUri = redirectUrl.searchParams.get('redirect_uri') as string;
+  const nonce = redirectUrl.searchParams.get('nonce') as string;
+  const state = redirectUrl.searchParams.get('state') as string;
+
+  const key = keystore.get('test');
+  const joseKey = await jose.importJWK(key.toJSON(true) as any);
+  const fakeIdToken = await new jose.SignJWT({
+    nonce,
+    // The below values are based on data observed by Dave during an actual
+    // login with Canvas.
+    'https://purl.imsglobal.org/spec/lti/claim/message_type': 'LtiResourceLinkRequest',
+    'https://purl.imsglobal.org/spec/lti/claim/version': '1.3.0',
+    'https://purl.imsglobal.org/spec/lti/claim/deployment_id':
+      '7fdce954-4c33-47c9-97b4-e435dbbed9bb',
+    // This MUST match the value in the login request.
+    'https://purl.imsglobal.org/spec/lti/claim/target_link_uri': `${siteUrl}/pl/lti13_instance/1/course_navigation`,
+    'https://purl.imsglobal.org/spec/lti/claim/resource_link': {
+      id: 'f6bc7a50-448c-4469-94f7-54d6ea882c2a',
+      title: 'Test Course',
+    },
+    'https://purl.imsglobal.org/spec/lti/claim/roles': [
+      'http://purl.imsglobal.org/vocab/lis/v2/institution/person#Instructor',
+      'http://purl.imsglobal.org/vocab/lis/v2/membership#Instructor',
+      'http://purl.imsglobal.org/vocab/lis/v2/system/person#User',
+    ],
+    'https://purl.imsglobal.org/spec/lti/claim/context': {
+      id: 'f6bc7a50-448c-4469-94f7-54d6ea882c2a',
+      type: ['http://purl.imsglobal.org/vocab/lis/v2/course#CourseOffering'],
+      label: 'TEST 101',
+      title: 'Test Course',
+    },
+    name: user.name,
+    email: user.email,
+    'https://purl.imsglobal.org/spec/lti/claim/custom': {
+      uin: user.uin,
+    },
+    'https://purl.imsglobal.org/spec/lti-ags/claim/endpoint': {
+      scope: [
+        'https://purl.imsglobal.org/spec/lti-ags/scope/lineitem',
+        'https://purl.imsglobal.org/spec/lti-ags/scope/score',
+      ],
+      errors: {
+        errors: {},
+      },
+      lineitems: `https://localhost:${oidcProviderPort}/api/lti/courses/1/line_items`,
+      validation_context: null,
+    },
+  })
+    .setProtectedHeader({ alg: 'RS256' })
+    .setIssuer(`http://localhost:${oidcProviderPort}`)
+    .setIssuedAt()
+    .setExpirationTime('1h')
+    .setSubject('a555090c-8355-4b58-b315-247612cc22f0')
+    .setAudience(CLIENT_ID)
+    .sign(joseKey);
+
+  // Run a server to respond to JWKS requests.
+  const app = express();
+  app.get('/jwks', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    // Pass `false` to `toJSON` to only include public keys.
+    res.end(JSON.stringify(keystore.toJSON(false)));
+  });
+
+  return {
+    login: async () => {
+      return await withServer(app, oidcProviderPort, async () => {
+        return await fetchWithCookies(redirectUri, {
+          method: 'POST',
+          body: new URLSearchParams({
+            nonce,
+            state,
+            id_token: fakeIdToken,
+          }),
+        });
+      });
+    },
+  };
+}
+
+async function createLti13Instance({
+  issuer_params,
+}: {
+  issuer_params: {
+    issuer: string;
+    authorization_endpoint: string;
+    jwks_uri: string;
+    token_endpoint: string;
+  };
+}) {
+  // Load the LTI admin page.
+  const ltiInstancesResponse = await fetchCheerio(
+    `${siteUrl}/pl/administrator/institution/1/lti13`,
+  );
+  assert.equal(ltiInstancesResponse.status, 200);
+
+  const newInstanceButton = ltiInstancesResponse.$('button:contains(Add a new LTI 1.3 instance)');
+  const newInstanceForm = newInstanceButton.closest('form');
+
+  // Create a new LTI instance.
+  const createInstanceResponse = await fetchCheerio(
+    `${siteUrl}/pl/administrator/institution/1/lti13`,
+    {
+      method: 'POST',
+      body: new URLSearchParams({
+        __csrf_token: newInstanceForm.find('input[name=__csrf_token]').val() as string,
+        __action: newInstanceButton.attr('value') as string,
+      }),
+    },
+  );
+  assert.equal(createInstanceResponse.status, 200);
+  const instanceUrl = createInstanceResponse.url;
+
+  const ltiInstanceResponse = await fetchCheerio(instanceUrl);
+  assert.equal(ltiInstanceResponse.status, 200);
+
+  const savePlatformOptionsButton = ltiInstanceResponse.$('button:contains(Save platform options)');
+  const platformOptionsForm = savePlatformOptionsButton.closest('form');
+
+  // Update the platform options.
+  const updatePlatformOptionsResponse = await fetchCheerio(instanceUrl, {
+    method: 'POST',
+    body: new URLSearchParams({
+      __csrf_token: platformOptionsForm.find('input[name=__csrf_token]').val() as string,
+      __action: platformOptionsForm.find('input[name=__action]').val() as string,
+      platform: 'Unknown',
+      issuer_params: JSON.stringify(issuer_params),
+      custom_fields: '{}',
+      client_id: CLIENT_ID,
+    }),
+  });
+  assert.equal(updatePlatformOptionsResponse.status, 200);
+
+  const addKeyButton = updatePlatformOptionsResponse.$('button:contains(Add key to keystore)');
+  const keystoreForm = addKeyButton.closest('form');
+
+  // Create a key
+  const createKeyResponse = await fetchCheerio(instanceUrl, {
+    method: 'POST',
+    body: new URLSearchParams({
+      __csrf_token: keystoreForm.find('input[name=__csrf_token]').val() as string,
+      __action: addKeyButton.attr('value') as string,
+    }),
+  });
+  assert.equal(createKeyResponse.status, 200);
+}
+
 describe('LTI 1.3', () => {
   let oidcProviderPort: number;
   let keystore: nodeJose.JWK.KeyStore;
@@ -65,80 +253,15 @@ describe('LTI 1.3', () => {
     config.features = {};
   });
 
-  step('create an LTI instance', async () => {
-    // Load the LTI admin page.
-    const ltiInstancesResponse = await fetchCheerio(
-      `${siteUrl}/pl/administrator/institution/1/lti13`,
-    );
-    assert.equal(ltiInstancesResponse.status, 200);
-
-    const newInstanceButton = ltiInstancesResponse.$('button:contains(Add a new LTI 1.3 instance)');
-    const newInstanceForm = newInstanceButton.closest('form');
-
-    // Create a new LTI instance.
-    const createInstanceResponse = await fetchCheerio(
-      `${siteUrl}/pl/administrator/institution/1/lti13`,
-      {
-        method: 'POST',
-        body: new URLSearchParams({
-          __csrf_token: newInstanceForm.find('input[name=__csrf_token]').val() as string,
-          __action: newInstanceButton.attr('value') as string,
-        }),
+  step('create and configure an LTI instance', async () => {
+    await createLti13Instance({
+      issuer_params: {
+        issuer: `http://localhost:${oidcProviderPort}`,
+        authorization_endpoint: `http://localhost:${oidcProviderPort}/auth`,
+        jwks_uri: `http://localhost:${oidcProviderPort}/jwks`,
+        token_endpoint: `http://localhost:${oidcProviderPort}/token`,
       },
-    );
-    assert.equal(createInstanceResponse.status, 200);
-
-    // Let's see how far we can get without customizing anything in the instance...
-  });
-
-  step('configure an LTI instance', async () => {
-    const ltiInstanceResponse = await fetchCheerio(
-      `${siteUrl}/pl/administrator/institution/1/lti13/1`,
-    );
-    assert.equal(ltiInstanceResponse.status, 200);
-
-    const savePlatformOptionsButton = ltiInstanceResponse.$(
-      'button:contains(Save platform options)',
-    );
-    const platformOptionsForm = savePlatformOptionsButton.closest('form');
-
-    // Update the platform options.
-    const updatePlatformOptionsResponse = await fetchCheerio(
-      `${siteUrl}/pl/administrator/institution/1/lti13/1`,
-      {
-        method: 'POST',
-        body: new URLSearchParams({
-          __csrf_token: platformOptionsForm.find('input[name=__csrf_token]').val() as string,
-          __action: platformOptionsForm.find('input[name=__action]').val() as string,
-          platform: 'Unknown',
-          issuer_params: JSON.stringify({
-            issuer: `http://localhost:${oidcProviderPort}`,
-            authorization_endpoint: `http://localhost:${oidcProviderPort}/auth`,
-            jwks_uri: `http://localhost:${oidcProviderPort}/jwks`,
-            token_endpoint: `http://localhost:${oidcProviderPort}/token`,
-          }),
-          custom_fields: '{}',
-          client_id: CLIENT_ID,
-        }),
-      },
-    );
-    assert.equal(updatePlatformOptionsResponse.status, 200);
-
-    const addKeyButton = updatePlatformOptionsResponse.$('button:contains(Add key to keystore)');
-    const keystoreForm = addKeyButton.closest('form');
-
-    // Create a key
-    const createKeyResponse = await fetchCheerio(
-      `${siteUrl}/pl/administrator/institution/1/lti13/1`,
-      {
-        method: 'POST',
-        body: new URLSearchParams({
-          __csrf_token: keystoreForm.find('input[name=__csrf_token]').val() as string,
-          __action: addKeyButton.attr('value') as string,
-        }),
-      },
-    );
-    assert.equal(createKeyResponse.status, 200);
+    });
   });
 
   step('enable LTI 1.3 as an authentication provider', async () => {
@@ -164,131 +287,29 @@ describe('LTI 1.3', () => {
   step('perform login', async () => {
     // `openid-client` relies on the session to store state, so we need to use
     // a cookie-aware version of fetch.
-    const fetchWithCookies = fetchCookie(fetchCheerio);
+    const fetchWithCookies = fetchCookie(fetch);
 
-    const startLoginResponse = await fetchWithCookies(`${siteUrl}/pl/lti13_instance/1/auth/login`, {
-      method: 'POST',
-      body: new URLSearchParams({
-        iss: siteUrl,
-        login_hint: 'fef15674-ae78-4763-b915-6fe3dbf42c67',
-        target_link_uri: `${siteUrl}/pl/lti13_instance/1/course_navigation`,
-      }),
-      redirect: 'manual',
-    });
-    assert.equal(startLoginResponse.status, 302);
-
-    const redirectUrl = new URL(startLoginResponse.headers.get('location') as string);
-    assert.equal(redirectUrl.hostname, 'localhost');
-    assert.equal(redirectUrl.pathname, '/auth');
-    assert.equal(redirectUrl.searchParams.get('client_id'), CLIENT_ID);
-    assert.equal(redirectUrl.searchParams.get('scope'), 'openid');
-    assert.equal(redirectUrl.searchParams.get('response_type'), 'id_token');
-    assert.equal(redirectUrl.searchParams.get('response_mode'), 'form_post');
-    assert.equal(
-      redirectUrl.searchParams.get('redirect_uri'),
-      `${siteUrl}/pl/lti13_instance/1/auth/callback`,
-    );
-    assert.equal(
-      redirectUrl.searchParams.get('login_hint'),
-      'fef15674-ae78-4763-b915-6fe3dbf42c67',
-    );
-    assert.ok(redirectUrl.searchParams.get('nonce'));
-    assert.ok(redirectUrl.searchParams.get('state'));
-
-    const redirectUri = redirectUrl.searchParams.get('redirect_uri') as string;
-    const nonce = redirectUrl.searchParams.get('nonce') as string;
-    const state = redirectUrl.searchParams.get('state') as string;
-
-    const key = keystore.get('test');
-    const joseKey = await jose.importJWK(key.toJSON(true) as any);
-    const fakeIdToken = await new jose.SignJWT({
-      nonce,
-      // The below values are based on data observed by Dave during an actual
-      // login with Canvas.
-      'https://purl.imsglobal.org/spec/lti/claim/message_type': 'LtiResourceLinkRequest',
-      'https://purl.imsglobal.org/spec/lti/claim/version': '1.3.0',
-      'https://purl.imsglobal.org/spec/lti/claim/deployment_id':
-        '7fdce954-4c33-47c9-97b4-e435dbbed9bb',
-      // This MUST match the value in the login request.
-      'https://purl.imsglobal.org/spec/lti/claim/target_link_uri': `${siteUrl}/pl/lti13_instance/1/course_navigation`,
-      'https://purl.imsglobal.org/spec/lti/claim/resource_link': {
-        id: 'f6bc7a50-448c-4469-94f7-54d6ea882c2a',
-        title: 'Test Course',
-      },
-      'https://purl.imsglobal.org/spec/lti/claim/roles': [
-        'http://purl.imsglobal.org/vocab/lis/v2/institution/person#Instructor',
-        'http://purl.imsglobal.org/vocab/lis/v2/membership#Instructor',
-        'http://purl.imsglobal.org/vocab/lis/v2/system/person#User',
-      ],
-      'https://purl.imsglobal.org/spec/lti/claim/context': {
-        id: 'f6bc7a50-448c-4469-94f7-54d6ea882c2a',
-        type: ['http://purl.imsglobal.org/vocab/lis/v2/course#CourseOffering'],
-        label: 'TEST 101',
-        title: 'Test Course',
-      },
-      name: 'Test User',
-      email: 'test-user@example.com',
-      'https://purl.imsglobal.org/spec/lti/claim/custom': {
+    const executor = await makeLoginExecutor({
+      user: {
+        name: 'Test User',
+        email: 'test-user@example.com',
         uin: '123456789',
       },
-      'https://purl.imsglobal.org/spec/lti-ags/claim/endpoint': {
-        scope: [
-          'https://purl.imsglobal.org/spec/lti-ags/scope/lineitem',
-          'https://purl.imsglobal.org/spec/lti-ags/scope/score',
-        ],
-        errors: {
-          errors: {},
-        },
-        lineitems: `https://localhost:${oidcProviderPort}/api/lti/courses/1/line_items`,
-        validation_context: null,
-      },
-    })
-      .setProtectedHeader({ alg: 'RS256' })
-      .setIssuer(`http://localhost:${oidcProviderPort}`)
-      .setIssuedAt()
-      .setExpirationTime('1h')
-      .setSubject('a555090c-8355-4b58-b315-247612cc22f0')
-      .setAudience(CLIENT_ID)
-      .sign(joseKey);
-
-    // Run a server to respond to JWKS requests.
-    const app = express();
-    app.get('/jwks', (req, res) => {
-      res.setHeader('Content-Type', 'application/json');
-      // Pass `false` to `toJSON` to only include public keys.
-      res.end(JSON.stringify(keystore.toJSON(false)));
+      fetchWithCookies,
+      oidcProviderPort,
+      keystore,
     });
 
-    const finishLoginResponse = await withServer(app, oidcProviderPort, async () => {
-      return await fetchWithCookies(redirectUri, {
-        method: 'POST',
-        body: new URLSearchParams({
-          nonce,
-          state,
-          id_token: fakeIdToken,
-        }),
-      });
-    });
+    const res = await executor.login();
+    assert.equal(res.status, 200);
 
-    assert.equal(finishLoginResponse.status, 200);
     // Inspect more into this response for output that is for Instructors vs for Students
     // Setup link to a course instance (database modification)
     // Confirm the redirect passes through to the course
 
-    const repeatLoginTestNonce = await withServer(app, oidcProviderPort, async () => {
-      return await fetchWithCookies(redirectUri, {
-        method: 'POST',
-        body: new URLSearchParams({
-          nonce,
-          state,
-          id_token: fakeIdToken,
-        }),
-        redirect: 'manual',
-      });
-    });
-
-    // This should fail for nonce reuse
-    assert.equal(repeatLoginTestNonce.status, 500);
+    // Logging in again should fail because of nonce reuse.
+    const repeatLoginRes = await executor.login();
+    assert.equal(repeatLoginRes.status, 500);
   });
 
   step('validate login', async () => {
