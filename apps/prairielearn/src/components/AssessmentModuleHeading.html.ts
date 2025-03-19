7d0be915b98d9a77f5cfe13badb7d49432b96772
ZacWarham <zacwarham@gmail.com>
2025-03-18 17:48:10 -0700

    make format

--- apps/prairielearn/src/components/AssessmentModuleHeading.html.ts
+++ apps/prairielearn/src/components/AssessmentModuleHeading.html.ts
@@ -7,9 +7,7 @@ export function AssessmentModuleHeading({
 }: {
   assessment_module: AssessmentModule;
 }) {
-  if (
-    !assessment_module.implicit || assessment_module.heading === 'Default module'
-  ) {
+  if (!assessment_module.implicit || assessment_module.heading === 'Default module') {
     return assessment_module.heading;
   }
 
