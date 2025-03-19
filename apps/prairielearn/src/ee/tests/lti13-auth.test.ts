e1b4d57984be5201cb2a57bc46d3e8d03d86e9a2
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-03-19 15:36:12 -0700

    Create full LTI 1.3 instance

--- apps/prairielearn/src/ee/tests/lti13-auth.test.ts
+++ apps/prairielearn/src/ee/tests/lti13-auth.test.ts
@@ -1,6 +1,5 @@
 import { assert } from 'chai';
 import { step } from 'mocha-steps';
-import fetch from 'node-fetch';
 
 import { config } from '../../lib/config.js';
 import { features } from '../../lib/features/index.js';
@@ -28,7 +27,7 @@ describe('LTI 1.3 auth', () => {
 
     // Create an instance.
     const csrfToken = getCSRFToken(res.$);
-    const createRes = await fetch(url, {
+    const createRes = await fetchCheerio(url, {
       method: 'POST',
       body: new URLSearchParams({
         __action: 'add_instance',
@@ -37,13 +36,44 @@ describe('LTI 1.3 auth', () => {
     });
     assert.equal(createRes.status, 200);
 
+    const instanceUrl = createRes.url;
+
+    // Update platform configuration.
+    const platformRes = await fetchCheerio(instanceUrl, {
+      method: 'POST',
+      body: new URLSearchParams({
+        __action: 'update_platform',
+        __csrf_token: getCSRFToken(createRes.$),
+        platform: 'Canvas Production',
+        issuer_params: JSON.stringify({
+          issuer: 'https://canvas.instructure.com',
+          jwks_uri: 'https://sso.canvaslms.com/api/lti/security/jwks',
+          token_endpoint: 'https://sso.canvaslms.com/login/oauth2/token',
+          authorization_endpoint: 'https://sso.canvaslms.com/api/lti/authorize_redirect',
+        }),
+        custom_fields: JSON.stringify({
+          uin: '$Canvas.user.sisIntegrationId',
+        }),
+      }),
+    });
+    assert.equal(platformRes.status, 200);
+
+    // Add a key to the keystore.
+    const keyRes = await fetchCheerio(instanceUrl, {
+      method: 'POST',
+      body: new URLSearchParams({
+        __action: 'add_key',
+        __csrf_token: getCSRFToken(platformRes.$),
+      }),
+    });
+    assert.equal(keyRes.status, 200);
+
     // Update the instance's attribute settings.
-    const createCsrfToken = getCSRFToken(res.$);
-    const updateRes = await fetch(url, {
+    const updateRes = await fetchCheerio(instanceUrl, {
       method: 'POST',
       body: new URLSearchParams({
         __action: 'save_pl_config',
-        __csrf_token: createCsrfToken,
+        __csrf_token: getCSRFToken(keyRes.$),
         uid_attribute: 'email',
         uin_attribute: '["https://purl.imsglobal.org/spec/lti/claim/custom"]["uin"]',
         name_attribute: 'name',
