8575f63c16c070e5627fd820c5a9b78db99014fc
Peter Stenger <pete@stenger.io>
2025-03-23 18:49:47 -0500

    Remove missing prairielearn.colors import

--- apps/prairielearn/python/prairielearn/__init__.py
+++ apps/prairielearn/python/prairielearn/__init__.py
@@ -1,5 +1,6 @@
 from prairielearn.attribute_utils import *  # noqa: F403
 from prairielearn.attribute_utils import _get_attrib as _get_attrib  # noqa: PLC0414
+from prairielearn.colors import *  # noqa: F403
 from prairielearn.conversion_utils import *  # noqa: F403
 from prairielearn.conversion_utils import (
     _string_from_complex_sigfig as _string_from_complex_sigfig,  # noqa: PLC0414
