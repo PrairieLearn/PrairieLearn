a64ae97addf394ea642c3d3992ab2ddc17eb853d
Jonatan Schroeder <jonatanschroeder@gmail.com>
2025-03-26 15:02:59 -0400

    Add JSdoc

--- apps/prairielearn/src/lib/timezones.ts
+++ apps/prairielearn/src/lib/timezones.ts
@@ -18,6 +18,11 @@ async function getAvailableTimezonesFromDB(): Promise<Timezone[]> {
   return availableTimezones;
 }
 
+/**
+ * Returns a list of all timezones supported by the database. The list is
+ * memoized so that, if it is needed more than once in the same session, the
+ * same list is returned.
+ */
 export async function getAvailableTimezones(): Promise<Timezone[]> {
   if (memoizedAvailableTimezones == null) {
     memoizedAvailableTimezones = await getAvailableTimezonesFromDB();
@@ -25,6 +30,16 @@ export async function getAvailableTimezones(): Promise<Timezone[]> {
   return memoizedAvailableTimezones;
 }
 
+/**
+ * Returns a filtered list of canonical timezones that are supported by both
+ * Postgres and the JavaScript Intl API. As per the specification of
+ * Intl.supportedValuesOf('timeZone'), the list includes only canonical timezone
+ * names, and does not include aliases or deprecated names.
+ *
+ * @param alwaysInclude - Optional array of timezone names to always include in
+ * the result, even if they're not canonical. These timezones are only included
+ * if they're supported by Postgres.
+ */
 export async function getCanonicalTimezones(alwaysInclude?: string[]): Promise<Timezone[]> {
   const availableTimezones = await getAvailableTimezones();
   const canonicalTimezones = Intl.supportedValuesOf('timeZone');
