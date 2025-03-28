fee6b2cc21d6551aad60d94a79e4a7405ec697f3
Pete Stenger <pete@stenger.io>
2025-03-27 19:33:01 -0500

    Resolve some weird behaviors

--- docs/scripts/gen_additional_readmes.py
+++ docs/scripts/gen_additional_readmes.py
@@ -1,5 +1,5 @@
 """
-This script generates copies README files into the PrairieLearn documentation.
+This script copies README files into the PrairieLearn documentation.
 It processes the README files from specified source directories, modifies links to point
 to the corresponding files on GitHub, and creates a navigation structure for the documentation.
 """
@@ -15,10 +15,12 @@ SOURCE_ROOT = "https://github.com/PrairieLearn/PrairieLearn/blob/master/"
 
 # Any links need to be replaced with a link to the file on GitHub
 relative_regex = r"\[(.*?)\]\((\..*?)\)"
+relative_regex_dir = r"\[(.*?)\]\((\..*?\/)\)"
 relative_regex_img = r"!\[(.*?)\]\((\..*?)\)"
 readmes_mapping = [
-    SimpleNamespace(**v) for v in mkdocs_gen_files.config.extra["addl_readmes"]
+    SimpleNamespace(**v) for v in mkdocs_gen_files.config.extra["additional_readmes"]
 ]
+doc_site_url = mkdocs_gen_files.config.site_url + "/en/latest/"
 
 
 def build_readme_nav() -> None:
@@ -38,16 +40,34 @@ def build_readme_nav() -> None:
             doc_readme_path.parent.mkdir(parents=True, exist_ok=True)
             with mkdocs_gen_files.open(doc_readme_path, "w") as f:
                 contents = path.read_text()
+                # Rewrite links to directories that point to a README file.
+                dir_matches = re.findall(relative_regex_dir, contents)
+                for [label, match] in dir_matches:
+                    readme_path = path.parent / match / "README.md"
+                    if readme_path.exists():
+                        contents = contents.replace(
+                            f"[{label}]({match})",
+                            f"[{label}]({match + 'README.md'})",
+                        )
+
+                # Rewrite links to images and files to point to the GitHub repository
+                # and add a "raw=true" query parameter to the image links.
                 contents = re.sub(
                     relative_regex_img,
                     rf"![\1]({SOURCE_ROOT}{path_relative_to_root.parent}/\2?raw=true)",
                     contents,
                 )
-                contents = re.sub(
-                    relative_regex,
-                    rf'[\1]({SOURCE_ROOT}{path_relative_to_root.parent}/\2 "External link")',
-                    contents,
-                )
+
+                file_matches = re.findall(relative_regex, contents)
+                for [label, match] in file_matches:
+                    # Rewrite links that don't point to internal documentation if we can
+                    if not match.endswith("README.md"):
+                        # This isn't fully accurate for determining if it's an internal link, but it works for our purposes
+                        contents = contents.replace(
+                            f"[{label}]({match})",
+                            f'[{label}]({SOURCE_ROOT}{path_relative_to_root.parent}/{match} "External link")',
+                        )
+
                 header, body = contents.split("\n", 1)
                 print(header, file=f)
                 print(robot_notice, file=f)
