f3dcf1850323378b4c0df846ebb45ade34930b55
Miguel Aenlle <66172204+miguelaenlle@users.noreply.github.com>
2025-03-28 17:18:43 -0500

    Update apps/prairielearn/src/pages/sideNavSettings/sideNavSettings.ts
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>

--- apps/prairielearn/src/pages/sideNavSettings/sideNavSettings.ts
+++ apps/prairielearn/src/pages/sideNavSettings/sideNavSettings.ts
@@ -3,11 +3,8 @@ import { Router } from 'express';
 const router = Router();
 
 router.put('/', async (req, res) => {
-  const new_state = Boolean(req.body.side_nav_expanded);
-  req.session.side_nav_expanded = new_state;
-  res.status(200).json({
-    side_nav_expanded: new_state,
-  });
+  req.session.side_nav_expanded = Boolean(req.body.side_nav_expanded);
+  res.sendStatus(204);
 });
 
 export default router;
