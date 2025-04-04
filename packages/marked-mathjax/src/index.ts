c86596f49bae56b4143ca168fc5836fc202ec7d9
Jonatan Schroeder <jonatan@yorku.ca>
2025-04-04 14:32:48 -0400

    Convert package to TS

--- packages/marked-mathjax/src/index.ts
+++ packages/marked-mathjax/src/index.ts
@@ -1,6 +1,8 @@
 const startMath = /(\$|\\\(|\\\[)/;
 
-export function addMathjaxExtension(marked, MathJax) {
+import { type Marked } from 'marked';
+
+export function addMathjaxExtension(marked: Marked, MathJax: any) {
   const mathjaxInput = MathJax.startup.getInputJax() ?? [];
   marked.use({
     renderer: {
@@ -8,21 +10,28 @@ export function addMathjaxExtension(marked, MathJax) {
       // should be ignored by MathJax. This includes escaped characters
       // like `\\` and `\$`, which we don't want MathJax to double-escape.
       // The text input is already escaped by marked itself.
-      text: ({ text, tokens }) => (tokens ? false : `<span class="mathjax_ignore">${text}</span>`),
+      text: (token) =>
+        'tokens' in token && token.tokens
+          ? false
+          : `<span class="mathjax_ignore">${token.text}</span>`,
     },
     extensions: [
       {
         name: 'math',
+        // Even though we support both inline (via `$` and `\(`) and block (via
+        // `$$` and `\[`) math, we treat all math as inline in the context of
+        // markdown processing. MathJax creates math blocks even if the
+        // delimiters are in the middle of a paragraph.
         level: 'inline',
         start: (src) => src.match(startMath)?.index,
         tokenizer(src) {
           // Check if the string starts with a math delimiter. If the
           // delimiter is further in the string, start() will take care of
           // calling the tokenizer again.
-          if (src.match(startMath)?.index !== 0) return false;
+          if (src.match(startMath)?.index !== 0) return undefined;
           // Use MathJax API to retrieve the math content.
           for (const inputJax of mathjaxInput) {
-            const foundMath = inputJax.findMath([src])?.find((math) => math.start?.n === 0);
+            const foundMath = inputJax.findMath([src])?.find((math: any) => math.start?.n === 0);
             if (foundMath) {
               const raw = src.substring(0, foundMath.end.n);
               return {
