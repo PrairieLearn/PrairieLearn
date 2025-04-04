a83688255ec654519adebb7009c1dc59d85fca7c
Jonatan Schroeder <jonatan@yorku.ca>
2025-04-04 14:46:48 -0400

    Add comment

--- packages/marked-mathjax/src/index.ts
+++ packages/marked-mathjax/src/index.ts
@@ -12,7 +12,7 @@ export function addMathjaxExtension(marked: Marked, MathJax: any) {
       // The text input is already escaped by marked itself.
       text: (token) =>
         'tokens' in token && token.tokens
-          ? false
+          ? false // If there are children, let default renderer handle it.
           : `<span class="mathjax_ignore">${token.text}</span>`,
     },
     extensions: [
