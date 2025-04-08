8d98da1788aee02d8d595a7bb9b87f24ab7929f2
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2025-04-08 16:14:05 -0700

    Add JSON comment columns

---
+++ apps/prairielearn/src/migrations/20250408215125_json_comments__add.sql
@@ -0,0 +1,34 @@
+ALTER TABLE assessment_access_rules
+ADD COLUMN json_comment text;
+
+ALTER TABLE assessment_questions
+ADD COLUMN json_comment text;
+
+ALTER TABLE assessment_sets
+ADD COLUMN json_comment text;
+
+ALTER TABLE assessments
+ADD COLUMN json_comment text;
+
+ALTER TABLE course_instance_access_rules
+ADD COLUMN json_comment text;
+
+ALTER TABLE course_instances
+ADD COLUMN json_comment text;
+
+ALTER TABLE pl_courses
+ADD COLUMN json_comment text;
+
+ALTER TABLE questions
+ADD COLUMN json_comment text,
+ADD COLUMN json_workspace_comment text,
+ADD COLUMN json_external_grading_comment text;
+
+ALTER TABLE tags
+ADD COLUMN json_comment text;
+
+ALTER TABLE topics
+ADD COLUMN json_comment text;
+
+ALTER TABLE zones
+ADD COLUMN json_comment text;
