542a14c7f89431ecf7f338a73c3624f13689d56a
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-04-14 15:52:56 -0700

    Work around compiler issue

--- images/plbase/plbase-install.sh
+++ images/plbase/plbase-install.sh
@@ -96,6 +96,13 @@ fi
 # Installing to a different directory is a preview feature
 uv python install --default --preview "cpython-3.10-${UV_ARCH}"
 uv venv --seed
+ls /usr/bin
+# `uv` has the incorrect path to the system's compilers in its generated data:
+# /root/.local/share/uv/python/cpython-3.10.17-linux-aarch64-gnu/lib/python3.10/_sysconfigdata__linux_aarch64-linux-gnu.py
+# These contain references to paths like `/usr/bin/aarch64-linux-gnu-gcc`, which
+# don't exist on Amazon Linux 2023. We need to override them with the correct paths.
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
 uv pip install --force-reinstall --no-cache-dir -r /python-requirements.txt
 
 # Clear various caches to minimize the final image size.
