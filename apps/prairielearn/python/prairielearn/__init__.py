c296dea09c170dfc71d9dd709d17055506945eeb
Pete Stenger <pete@stenger.io>
2025-04-14 16:36:53 -0500

    Un-export `_string_from_complex_sigfig`

--- apps/prairielearn/python/prairielearn/__init__.py
+++ apps/prairielearn/python/prairielearn/__init__.py
@@ -1,11 +1,9 @@
-# TODO: Update this in a future PR
-# from prairielearn.colors import *
 from prairielearn.conversion_utils import *  # noqa: F403
-from prairielearn.conversion_utils import (
-    _string_from_complex_sigfig as _string_from_complex_sigfig,  # noqa: PLC0414
-)
 from prairielearn.extension_utils import *  # noqa: F403
 from prairielearn.grading_utils import *  # noqa: F403
 from prairielearn.html_utils import *  # noqa: F403
 from prairielearn.misc_utils import *  # noqa: F403
 from prairielearn.question_utils import *  # noqa: F403
+
+# TODO: Update this in a future PR
+# from prairielearn.colors import *
