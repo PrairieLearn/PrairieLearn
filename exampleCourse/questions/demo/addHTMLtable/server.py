9a4fe4a9c27b3ad017d20c4cdcb0f8cc74e1d55f
Pete Stenger <pete@stenger.io>
2025-04-24 14:20:41 -0500

    Add missing tbody tag

--- exampleCourse/questions/demo/addHTMLtable/server.py
+++ exampleCourse/questions/demo/addHTMLtable/server.py
@@ -10,7 +10,7 @@ def generate(data):
     mytable = '<table style="width:30%"><thead><tr><th> Name </th> <th> Age </th> <th> Id </th></tr></thead><tbody>'
     for name, age, name_greek in zip(name_list, age_list, name_greek_list, strict=True):
         mytable += f"<tr><td> {name} </td><td> {age} </td><td> {name_greek} </td></tr>"
-    mytable += "</table>"
+    mytable += "</tbody></table>"
     data["params"]["mytable"] = mytable
 
     # Generate only the table content
