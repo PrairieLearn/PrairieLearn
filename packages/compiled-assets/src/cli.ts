4fd127828cf649ef8a363b9e9990761d4eeb71cd
miguelaenlle <bot.developer3@gmail.com>
2025-04-24 16:03:06 -0500

    Reverted changes to packages and yarn lock file

--- packages/compiled-assets/src/cli.ts
+++ packages/compiled-assets/src/cli.ts
@@ -7,7 +7,7 @@ import { program } from 'commander';
 import fs from 'fs-extra';
 import prettyBytes from 'pretty-bytes';
 
-import { type AssetsManifest, build } from './index.js';
+import { build } from './index.js';
 
 const gzip = promisify(zlib.gzip);
 const brotli = promisify(zlib.brotliCompress);
@@ -16,18 +16,18 @@ type CompressedSizes = Record<string, Record<string, number>>;
 
 async function writeCompressedAssets(
   destination: string,
-  manifest: AssetsManifest,
+  manifest: Record<string, string>,
 ): Promise<CompressedSizes> {
   const compressedSizes: CompressedSizes = {};
   await Promise.all(
-    Object.values(manifest).map(async (asset) => {
-      const destinationFilePath = path.resolve(destination, asset.assetPath);
+    Object.values(manifest).map(async (filePath) => {
+      const destinationFilePath = path.resolve(destination, filePath);
       const contents = await fs.readFile(destinationFilePath);
       const gzipCompressed = await gzip(contents);
       const brotliCompressed = await brotli(contents);
       await fs.writeFile(`${destinationFilePath}.gz`, gzipCompressed);
       await fs.writeFile(`${destinationFilePath}.br`, brotliCompressed);
-      compressedSizes[asset.assetPath] = {
+      compressedSizes[filePath] = {
         raw: contents.length,
         gzip: gzipCompressed.length,
         brotli: brotliCompressed.length,
@@ -46,10 +46,10 @@ program.command('build <source> <destination>').action(async (source, destinatio
 
   // Format the output into an object that we can pass to `console.table`.
   const results: Record<string, any> = {};
-  Object.entries(manifest).forEach(([entryPoint, asset]) => {
-    const sizes = compressedSizes[asset.assetPath];
+  Object.entries(manifest).forEach(([entryPoint, assetPath]) => {
+    const sizes = compressedSizes[assetPath];
     results[entryPoint] = {
-      'Output file': asset.assetPath,
+      'Output file': assetPath,
       Size: prettyBytes(sizes.raw),
       'Size (gzip)': prettyBytes(sizes.gzip),
       'Size (brotli)': prettyBytes(sizes.brotli),
--- packages/compiled-assets/src/index.ts
+++ packages/compiled-assets/src/index.ts
@@ -16,13 +16,7 @@ const DEFAULT_OPTIONS = {
   publicPath: '/build/',
 };
 
-export type AssetsManifest = Record<
-  string,
-  {
-    assetPath: string;
-    preloads: string[];
-  }
->;
+type AssetsManifest = Record<string, string>;
 
 export interface CompiledAssetsOptions {
   /**
@@ -40,13 +34,8 @@ export interface CompiledAssetsOptions {
 }
 
 let options: Required<CompiledAssetsOptions> = { ...DEFAULT_OPTIONS };
-
 let esbuildContext: esbuild.BuildContext | null = null;
 let esbuildServer: esbuild.ServeResult | null = null;
-
-let splitEsbuildContext: esbuild.BuildContext | null = null;
-let splitEsbuildServer: esbuild.ServeResult | null = null;
-
 let relativeSourcePaths: string[] | null = null;
 
 export async function init(newOptions: Partial<CompiledAssetsOptions>): Promise<void> {
@@ -86,34 +75,8 @@ export async function init(newOptions: Partial<CompiledAssetsOptions>): Promise<
       outdir: options.buildDirectory,
       entryNames: '[dir]/[name]',
     });
+
     esbuildServer = await esbuildContext.serve({ host: '0.0.0.0' });
-
-    const splitSourceGlob = path.join(
-      options.sourceDirectory,
-      'scripts',
-      'split-bundles',
-      '**',
-      '*.{js,ts,jsx,tsx}',
-    );
-    const splitSourcePaths = await globby(splitSourceGlob);
-
-    relativeSourcePaths.push(
-      ...splitSourcePaths.map((p) => path.relative(options.sourceDirectory, p)),
-    );
-
-    splitEsbuildContext = await esbuild.context({
-      entryPoints: splitSourcePaths,
-      target: 'es2017',
-      format: 'esm',
-      sourcemap: 'inline',
-      bundle: true,
-      splitting: true,
-      write: false,
-      outbase: options.sourceDirectory,
-      outdir: options.buildDirectory,
-      entryNames: '[dir]/[name]',
-    });
-    splitEsbuildServer = await splitEsbuildContext.serve({ host: '0.0.0.0' });
   }
 }
 
@@ -122,7 +85,6 @@ export async function init(newOptions: Partial<CompiledAssetsOptions>): Promise<
  */
 export async function close() {
   esbuildContext?.dispose();
-  splitEsbuildContext?.dispose();
 }
 
 export function assertConfigured(): void {
@@ -149,21 +111,15 @@ export function handler(): RequestHandler {
     });
   }
 
-  if (!esbuildServer || !splitEsbuildServer) {
+  if (!esbuildServer) {
     throw new Error('esbuild server not initialized');
   }
 
   const { port } = esbuildServer;
-  const { port: splitPort } = splitEsbuildServer;
 
   // We're running in dev mode, so we need to boot up esbuild to start building
   // and watching our assets.
   return function (req, res) {
-    const isSplitBundle =
-      req.url.startsWith('/scripts/split-bundles') ||
-      // Chunked assets must be served by the split server.
-      req.url.startsWith('/chunk-');
-
     // esbuild will reject requests that come from hosts other than the host on
     // which the esbuild dev server is listening:
     // https://github.com/evanw/esbuild/commit/de85afd65edec9ebc44a11e245fd9e9a2e99760d
@@ -180,7 +136,7 @@ export function handler(): RequestHandler {
     const proxyReq = http.request(
       {
         hostname: '127.0.0.1',
-        port: isSplitBundle ? splitPort : port,
+        port,
         path: req.url,
         method: req.method,
         headers,
@@ -223,12 +179,12 @@ function compiledPath(type: 'scripts' | 'stylesheets', sourceFile: string): stri
   }
 
   const manifest = readManifest();
-  const asset = manifest[sourceFilePath];
-  if (!asset) {
+  const assetPath = manifest[sourceFilePath];
+  if (!assetPath) {
     throw new Error(`Unknown ${type} asset: ${sourceFile}`);
   }
 
-  return options.publicPath + asset.assetPath;
+  return options.publicPath + assetPath;
 }
 
 export function compiledScriptPath(sourceFile: string): string {
@@ -247,27 +203,10 @@ export function compiledStylesheetTag(sourceFile: string): HtmlSafeString {
   return html`<link rel="stylesheet" href="${compiledStylesheetPath(sourceFile)}" />`;
 }
 
-export function compiledScriptPreloadPaths(sourceFile: string): string[] {
-  assertConfigured();
-
-  // In dev mode, we don't have a manifest, so we can't preload anything.
-  if (options.dev) return [];
-
-  const manifest = readManifest();
-  const asset = manifest[`scripts/${sourceFile}`];
-  if (!asset) {
-    throw new Error(`Unknown script asset: ${sourceFile}`);
-  }
-
-  return asset.preloads.map((preload) => options.publicPath + preload);
-}
-
-async function buildAssets(sourceDirectory: string, buildDirectory: string): Promise<Metafile> {
+async function buildAssets(sourceDirectory: string, buildDirectory: string) {
   await fs.ensureDir(buildDirectory);
 
-  const scriptFiles = await globby(path.join(sourceDirectory, 'scripts', '*.{js,jsx,ts,tsx}'));
-  const styleFiles = await globby(path.join(sourceDirectory, 'stylesheets', '*.css'));
-  const files = [...scriptFiles, ...styleFiles];
+  const files = await globby(path.join(sourceDirectory, '*/*.{js,jsx,ts,tsx,css}'));
   const buildResult = await esbuild.build({
     entryPoints: files,
     target: 'es2017',
@@ -283,93 +222,24 @@ async function buildAssets(sourceDirectory: string, buildDirectory: string): Pro
     outbase: sourceDirectory,
     outdir: buildDirectory,
     metafile: true,
-    plugins: [
-      {
-        name: 'nathan-resolve',
-        setup(build) {
-          build.onResolve({ filter: /.*/ }, (args) => {
-            console.log('resolve', args);
-            return undefined;
-          });
-        },
-      },
-    ],
   });
 
-  const scriptBundleFiles = await globby(
-    path.join(sourceDirectory, 'scripts', 'split-bundles', '**/*.{js,jsx,ts,tsx}'),
-  );
-  const chunkBuildResult = await esbuild.build({
-    entryPoints: scriptBundleFiles,
-    target: 'es2017',
-    format: 'esm',
-    sourcemap: 'linked',
-    bundle: true,
-    splitting: true,
-    minify: true,
-    entryNames: '[dir]/[name]-[hash]',
-    outbase: sourceDirectory,
-    outdir: buildDirectory,
-    metafile: true,
-    // TODO: for debugging, remove this.
-    plugins: [
-      {
-        name: 'nathan-resolve',
-        setup(build) {
-          build.onResolve({ filter: /.*/ }, (args) => {
-            console.log('resolve', args);
-            return undefined;
-          });
-        },
-      },
-    ],
-  });
-
-  // Merge the resulting metafiles.
-  const metafile: Metafile = {
-    inputs: { ...buildResult.metafile.inputs, ...chunkBuildResult.metafile.inputs },
-    outputs: { ...buildResult.metafile.outputs, ...chunkBuildResult.metafile.outputs },
-  };
-
-  return metafile;
+  return buildResult.metafile;
 }
 
 function makeManifest(
   metafile: Metafile,
   sourceDirectory: string,
   buildDirectory: string,
-): AssetsManifest {
-  const manifest: AssetsManifest = {};
-
-  console.dir(metafile.outputs, { depth: null });
-
+): Record<string, string> {
+  const manifest: Record<string, string> = {};
   Object.entries(metafile.outputs).forEach(([outputPath, meta]) => {
     if (!meta.entryPoint) return;
 
-    // Recursively walk the `imports` field to find all output files that
-    // need to be preloaded.
-    const preloads = new Set<string>();
-    const visit = (entry: (typeof meta)['imports'][number]) => {
-      if (!['import-statement', 'dynamic-import'].includes(entry.kind)) return;
-      if (preloads.has(entry.path)) return;
-      preloads.add(entry.path);
-      for (const imp of metafile.inputs[entry.path]?.imports ?? []) {
-        visit(imp);
-      }
-    };
-
-    for (const imp of meta.imports) {
-      visit(imp);
-    }
-
     const entryPath = path.relative(sourceDirectory, meta.entryPoint);
     const assetPath = path.relative(buildDirectory, outputPath);
-    manifest[entryPath] = {
-      assetPath,
-      preloads: [...preloads],
-    };
+    manifest[entryPath] = assetPath;
   });
-
   return manifest;
 }
 
