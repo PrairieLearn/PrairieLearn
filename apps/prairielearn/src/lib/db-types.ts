958f6ca2c66204a0b4fdec2b8716509029936db8
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-04-29 15:59:49 -0700

    Update apps/prairielearn/src/lib/db-types.ts

--- apps/prairielearn/src/lib/db-types.ts
+++ apps/prairielearn/src/lib/db-types.ts
@@ -734,7 +734,7 @@ export const Lti13InstanceSchema = z.object({
   name_attribute: z.string().nullable(),
   name: z.string(),
   platform: z.string(),
-  require_linked_lti_user: z.boolean().default(false),
+  require_linked_lti_user: z.boolean(),
   tool_platform_name: z.string().nullable(),
   uid_attribute: z.string().nullable(),
   uin_attribute: z.string().nullable(),
