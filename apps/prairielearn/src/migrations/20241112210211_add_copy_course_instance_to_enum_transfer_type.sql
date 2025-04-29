5d1e904d81f2c4ce672b4306515269a03c88d8f1
Seth Poulsen <poulsenseth@yahoo.com>
2025-04-29 16:20:17 -0600

    remove course instance copying code

--- apps/prairielearn/src/migrations/20241112210211_add_copy_course_instance_to_enum_transfer_type.sql
+++
@@ -1,2 +0,0 @@
--- Migration to add 'CopyCourseInstance' to enum_transfer_type
-ALTER TYPE enum_transfer_type ADD VALUE 'CopyCourseInstance';
--- apps/prairielearn/src/migrations/20241112215640_file_transfers__from_course_instance_id__add.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE file_transfers
-ADD COLUMN from_course_instance_id bigint;
