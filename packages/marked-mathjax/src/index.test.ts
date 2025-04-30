5a8ff142bb28ca4147d4e56fa83825540ed2140a
Jonatan Schroeder <jonatanschroeder@gmail.com>
2025-04-30 19:47:24 -0400

    Extend tests

--- packages/marked-mathjax/src/index.test.ts
+++ packages/marked-mathjax/src/index.test.ts
@@ -29,11 +29,12 @@ describe('Markdown processing', () => {
     addMathjaxExtension(marked, mathjax);
   });
 
-  // it('renders basic markdown correctly', async () => {
-  //   const question = '# Hello, world!\nThis **works**.';
-  //   const expected = '<h1>Hello, world!</h1>\n<p>This <strong>works</strong>.</p>';
-  //   await testMarkdown(question, expected);
-  // });
+  it('renders basic markdown correctly', async () => {
+    const question = '# Hello, world!\nThis **works**.';
+    const expected =
+      '<h1><span class="mathjax_ignore">Hello, world!</span></h1>\n<p><span class="mathjax_ignore">This </span><strong><span class="mathjax_ignore">works</span></strong><span class="mathjax_ignore">.</span></p>';
+    await testMarkdown(question, expected);
+  });
 
   it('handles inline latex with underscores', async () => {
     const question = '$a _{1_ 2}$';
@@ -47,107 +48,57 @@ describe('Markdown processing', () => {
     await testMarkdown(question, expected);
   });
 
-  // it('handles multiple lines of inline latex', async () => {
-  //   const question = '$a_{ 1 } = 3$ and\n$a_{ 2 } = 4$';
-  //   const expected = '<p>$a_{ 1 } = 3$ and\n$a_{ 2 } = 4$</p>';
-  //   await testMarkdown(question, expected);
-  // });
+  it('handles multiple lines of inline latex', async () => {
+    const question = '$a_{ 1 } = 3$ and\n\\(a_{ 2 } = 4\\)';
+    const expected =
+      '<p>$a_{ 1 } = 3$<span class="mathjax_ignore"> and\n</span>\\(a_{ 2 } = 4\\)</p>';
+    await testMarkdown(question, expected);
+  });
 
-  // it('handles block latex', async () => {
-  //   const question = '$$\na^2 + b^2 = c^2\n$$';
-  //   const expected = '$$\na^2 + b^2 = c^2\n$$';
-  //   await testMarkdown(question, expected);
-  // });
+  it('handles block latex', async () => {
+    const question = '$$\na^2 + b^2 = c^2\n$$';
+    const expected = '<p>$$\na^2 + b^2 = c^2\n$$</p>';
+    await testMarkdown(question, expected);
+  });
 
-  // it('handles block latex with asterisks', async () => {
-  //   const question = '$$\na **b** c\n$$';
-  //   const expected = '$$\na **b** c\n$$';
-  //   await testMarkdown(question, expected);
-  // });
+  it('handles block latex with asterisks', async () => {
+    const question = '$$\na **b** c\n$$';
+    const expected = '<p>$$\na **b** c\n$$</p>';
+    await testMarkdown(question, expected);
+  });
 
-  // it('handles two consecutive latex blocks', async () => {
-  //   const question = '$$\na **b** c\n$$\n$$\na+b=c\n$$';
-  //   const expected = '$$\na **b** c\n$$\n$$\na+b=c\n$$';
-  //   await testMarkdown(question, expected);
-  // });
+  it('handles two consecutive latex blocks', async () => {
+    const question = '$$\na **b** c\n$$\n$$\na+b=c\n$$';
+    const expected = '<p>$$\na **b** c\n$$<span class="mathjax_ignore">\n</span>$$\na+b=c\n$$</p>';
+    await testMarkdown(question, expected);
+  });
 
-  // it('handles block latex with asterisks and surrounding text', async () => {
-  //   const question = 'testing\n$$\na **b** c\n$$\ntesting';
-  //   const expected = '<p>testing</p>\n$$\na **b** c\n$$\n<p>testing</p>';
-  //   await testMarkdown(question, expected);
-  // });
+  it('handles block latex with asterisks and surrounding text', async () => {
+    const question = 'testing\n\\[\na **b** c\n\\]\ntesting';
+    const expected =
+      '<p><span class="mathjax_ignore">testing\n</span>\\[\na **b** c\n\\]<span class="mathjax_ignore">\ntesting</span></p>';
+    await testMarkdown(question, expected);
+  });
 
-  // it('handles GFM extension for tables', async () => {
-  //   const question = '| foo | bar |\n| --- | --- |\n| baz | bim |';
-  //   const expected =
-  //     '<table><thead><tr><th>foo</th><th>bar</th></tr></thead><tbody><tr><td>baz</td><td>bim</td></tr></tbody></table>';
-  //   await testMarkdown(question, expected);
-  // });
+  it('handles escapes', async () => {
+    const question =
+      'This line has a \\$ dollar sign \\$, a \\\\ single backslash and \\\\\\\\ double backslash.';
+    const expected =
+      '<p><span class="mathjax_ignore">This line has a </span><span class="mathjax_ignore">$</span><span class="mathjax_ignore"> dollar sign </span><span class="mathjax_ignore">$</span><span class="mathjax_ignore">, a </span><span class="mathjax_ignore">\\</span><span class="mathjax_ignore"> single backslash and </span><span class="mathjax_ignore">\\</span><span class="mathjax_ignore">\\</span><span class="mathjax_ignore"> double backslash.</span></p>';
+    await testMarkdown(question, expected);
+  });
 
-  // it('handles HTML tags', async () => {
-  //   const question = 'testing with <strong>bold</strong> and <em>italics</em> words';
-  //   const expected = '<p>testing with <strong>bold</strong> and <em>italics</em> words</p>';
-  //   await testMarkdown(question, expected);
-  // });
+  it('handles GFM extension for tables', async () => {
+    const question = '| foo | bar |\n| --- | --- |\n| baz | bim |';
+    const expected =
+      '<table>\n<thead>\n<tr>\n<th><span class="mathjax_ignore">foo</span></th>\n<th><span class="mathjax_ignore">bar</span></th>\n</tr>\n</thead>\n<tbody><tr>\n<td><span class="mathjax_ignore">baz</span></td>\n<td><span class="mathjax_ignore">bim</span></td>\n</tr>\n</tbody></table>';
+    await testMarkdown(question, expected);
+  });
 
-  // it('handles HTML tags that do not close properly', async () => {
-  //   const question = 'testing with <strong>bold</strong> and <em>italics words';
-  //   const expected = '<p>testing with <strong>bold</strong> and <em>italics words</em></p>';
-  //   await testMarkdown(question, expected);
-  // });
-
-  // it('handles HTML paragraphs that do not close properly', async () => {
-  //   const question = 'first paragraph<p>second paragraph<p>third paragraph';
-  //   const expected = '<p>first paragraph</p><p>second paragraph</p><p>third paragraph</p>';
-  //   await testMarkdown(question, expected);
-  // });
-
-  // it('handles HTML closing tags that do not open properly', async () => {
-  //   const question = 'testing with </div><p>new line';
-  //   const expected = '<p>testing with </p><p>new line</p>';
-  //   await testMarkdown(question, expected);
-  // });
-
-  // it('handles HTML closing tags that do not open properly', async () => {
-  //   const question = 'testing with </div><p>new line';
-  //   const expected = '<p>testing with </p><p>new line</p>';
-  //   await testMarkdown(question, expected);
-  // });
-
-  // it('removes p tag if content fits a single paragraph', async () => {
-  //   const question = 'test with **bold** and *italic* words';
-  //   const expected = 'test with <strong>bold</strong> and <em>italic</em> words';
-  //   await testMarkdown(question, expected, { inline: true });
-  // });
-
-  // it('keeps p tag if content has more than one paragraph', async () => {
-  //   const question = 'test with **bold** and *italic* words\n\nand a new paragraph';
-  //   const expected =
-  //     '<p>test with <strong>bold</strong> and <em>italic</em> words</p>\n<p>and a new paragraph</p>';
-  //   await testMarkdown(question, expected, { inline: true });
-  // });
-
-  // it('keeps external tag if it is not a paragraph', async () => {
-  //   const question = '* single item';
-  //   const expected = '<ul>\n<li>single item</li>\n</ul>';
-  //   await testMarkdown(question, expected, { inline: true });
-  // });
-
-  // it('sanitizes HTML script tags', async () => {
-  //   const question = 'testing<script>alert("XSS")</script>';
-  //   const expected = '<p>testing</p>';
-  //   await testMarkdown(question, expected);
-  // });
-
-  // it('sanitizes HTML javascript event tags', async () => {
-  //   const question = 'testing<img src="x" onerror="alert(\'XSS\')">';
-  //   const expected = '<p>testing<img src="x"></p>';
-  //   await testMarkdown(question, expected);
-  // });
-
-  // it('sanitizes HTML javascript iframes', async () => {
-  //   const question = 'testing<iframe src="javascript:alert(\'delta\')"></iframe>';
-  //   const expected = '<p>testing</p>';
-  //   await testMarkdown(question, expected);
-  // });
+  it('handles HTML tags', async () => {
+    const question = 'testing with <strong>bold</strong> and <em>italics</em> words';
+    const expected =
+      '<p><span class="mathjax_ignore">testing with </span><strong><span class="mathjax_ignore">bold</span></strong><span class="mathjax_ignore"> and </span><em><span class="mathjax_ignore">italics</span></em><span class="mathjax_ignore"> words</span></p>';
+    await testMarkdown(question, expected);
+  });
 });
