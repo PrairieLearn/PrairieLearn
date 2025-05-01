aca01208684a4ea741a1304b864cc9eda7d8b77e
miguelaenlle <bot.developer3@gmail.com>
2025-05-01 14:01:49 -0500

    Added bg-light back

--- apps/prairielearn/src/components/AssessmentNavigation.html.ts
+++ apps/prairielearn/src/components/AssessmentNavigation.html.ts
@@ -19,24 +19,26 @@ export function AssessmentNavigation({
   assessment: Assessment;
 }) {
   return html`
-    <button
-      type="button"
-      class="btn btn-ghost dropdown-toggle dropdown-menu-right d-flex justify-content-between align-items-center mt-2 ms-3"
-      style="max-width: 100%;"
-      aria-label="Change assessment"
-      aria-haspopup="true"
-      aria-expanded="false"
-      hx-get="/pl/assessments_switcher/course_instance/${courseInstanceId}/assessment/${assessment.id}${subPage
-        ? `?subPage=${subPage}`
-        : ''}"
-      hx-trigger="mouseover once, focus once, show.bs.dropdown once delay:200ms"
-      data-bs-toggle="modal"
-      data-bs-target="#assessmentNavigationModal"
-      hx-target="#assessmentNavigationModalContent"
-    >
-      <span class="h6 mb-0 me-1 overflow-hidden text-truncate">${assessment.title}</span>
-    </button>
-    ${AssessmentNavigationModal()}
+    <div class="dropdown bg-light pt-2 px-3">
+      <button
+        type="button"
+        class="btn btn-ghost dropdown-toggle dropdown-menu-right d-flex justify-content-between align-items-center mt-2 ms-3"
+        style="max-width: 100%;"
+        aria-label="Change assessment"
+        aria-haspopup="true"
+        aria-expanded="false"
+        hx-get="/pl/assessments_switcher/course_instance/${courseInstanceId}/assessment/${assessment.id}${subPage
+          ? `?subPage=${subPage}`
+          : ''}"
+        hx-trigger="mouseover once, focus once, show.bs.dropdown once delay:200ms"
+        data-bs-toggle="modal"
+        data-bs-target="#assessmentNavigationModal"
+        hx-target="#assessmentNavigationModalContent"
+      >
+        <span class="h6 mb-0 me-1 overflow-hidden text-truncate">${assessment.title}</span>
+      </button>
+      ${AssessmentNavigationModal()}
+    </div>
   `;
 }
 
