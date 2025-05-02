53508ae7c1df1d9a05c7feda6c535d0a445a60e5
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-05-02 16:51:26 -0700

    Simplification

--- apps/prairielearn/src/lib/submissions-upload.ts
+++ apps/prairielearn/src/lib/submissions-upload.ts
@@ -46,13 +46,13 @@ const ZodStringToJson = z.preprocess((val) => {
 
 const SubmissionCsvRowSchema = z.object({
   UID: z.string(),
-  UIN: z.string().optional().nullable(),
-  Name: z.string().optional().nullable(),
+  UIN: z.string().nullable(),
+  Name: z.string().nullable(),
   'Group name': z.string().optional().nullable(),
   'Assessment instance': ZodStringToNumber,
   Question: z.string(),
   'Question instance': ZodStringToNumber,
-  Seed: z.string().optional().nullable(),
+  Seed: z.string(),
   Params: ZodStringToJson,
   'True answer': ZodStringToJson,
   Options: ZodStringToJson,
@@ -61,24 +61,12 @@ const SubmissionCsvRowSchema = z.object({
   'Partial Scores': ZodStringToJson,
   'Override score': ZodStringToNumber,
   Credit: ZodStringToNumber,
-  Mode: z.string().optional().nullable(),
+  Mode: z.string(),
   'Grading requested date': ZodStringToDate,
   'Grading date': ZodStringToDate,
   Score: ZodStringToNumber,
   Correct: ZodStringToBoolean,
   Feedback: ZodStringToJson,
-  'Max points': ZodStringToNumber,
-  'Max auto points': ZodStringToNumber,
-  'Max manual points': ZodStringToNumber,
-  Assessment: z.string().optional().nullable(),
-  'Zone number': z.string().optional().nullable(),
-  'Zone title': z.string().optional().nullable(),
-  Variant: z.string().optional().nullable(),
-  'Assigned manual grader': z.string().optional().nullable(),
-  'Last manual grader': z.string().optional().nullable(),
-  'Rubric Grading': z.string().optional().nullable(),
-  'Question points': z.string().optional().nullable(),
-  'Question % score': z.string().optional().nullable(),
 });
 
 export async function uploadSubmissionsCsv(
@@ -110,10 +98,6 @@ export async function uploadSubmissionsCsv(
 
     job.info('Uploading submissions CSV for ' + assessment_label);
 
-    let output: string | null = null;
-    let outputCount = 0;
-    let outputThreshold = 100;
-
     let successCount = 0;
     let errorCount = 0;
 
@@ -122,144 +106,116 @@ export async function uploadSubmissionsCsv(
       encoding: 'utf8',
     });
     const csvConverter = csvtojson();
+    await csvConverter.fromStream(csvStream).subscribe(async (rawJson, number) => {
+      const lineNumber = number + 2;
+      job.verbose(`Processing CSV line ${lineNumber}: ${JSON.stringify(rawJson)}`);
 
-    try {
-      await csvConverter.fromStream(csvStream).subscribe(async (rawJson, number) => {
-        const lineNumber = number + 2;
-        const msg = `Processing CSV line ${lineNumber}: ${JSON.stringify(rawJson)}`;
-        if (output == null) {
-          output = msg;
+      try {
+        const row = SubmissionCsvRowSchema.parse(rawJson);
+
+        const user = await selectOrInsertUserByUid(row.UID);
+
+        let assessment_instance_id: string;
+
+        if (row['Group name']) {
+          const group_id_for_instance = await sqldb.queryRow(
+            sql.ensure_group,
+            {
+              group_name: row['Group name'],
+              course_instance_id,
+            },
+            IdSchema,
+          );
+
+          assessment_instance_id = await sqldb.queryRow(
+            sql.ensure_assessment_instance_group,
+            {
+              assessment_id,
+              group_id: group_id_for_instance,
+              instance_number: row['Assessment instance'] ?? 1,
+            },
+            IdSchema,
+          );
         } else {
-          output += '\n' + msg;
-        }
-
-        try {
-          const row = SubmissionCsvRowSchema.parse(rawJson);
-
-          const user = await selectOrInsertUserByUid(row.UID);
-
-          let assessment_instance_id: string;
-
-          if (row['Group name']) {
-            const group_id_for_instance = await sqldb.queryRow(
-              sql.ensure_group,
-              {
-                group_name: row['Group name'],
-                course_instance_id,
-              },
-              IdSchema,
-            );
-
-            assessment_instance_id = await sqldb.queryRow(
-              sql.ensure_assessment_instance_group,
-              {
-                assessment_id,
-                group_id: group_id_for_instance,
-                instance_number: row['Assessment instance'] ?? 1,
-              },
-              IdSchema,
-            );
-          } else {
-            assessment_instance_id = await sqldb.queryRow(
-              sql.ensure_assessment_instance_user,
-              {
-                assessment_id,
-                user_id: user.user_id,
-                instance_number: row['Assessment instance'] ?? 1,
-              },
-              IdSchema,
-            );
-          }
-
-          const question = await selectQuestionByQid({ course_id, qid: row.Question });
-          const assessmentQuestion = await sqldb.queryRow(
-            sql.select_assessment_question,
-            { assessment_id, question_id: question.id },
-            AssessmentQuestionSchema,
-          );
-
-          const instance_question_id = await sqldb.queryRow(
-            sql.insert_instance_question,
-            { assessment_instance_id, assessment_question_id: assessmentQuestion.id },
-            IdSchema,
-          );
-
-          const variant_id = await sqldb.queryRow(
-            sql.insert_variant,
+          assessment_instance_id = await sqldb.queryRow(
+            sql.ensure_assessment_instance_user,
             {
-              instance_question_id,
-              question_id: question.id,
-              authn_user_id: user.user_id,
+              assessment_id,
               user_id: user.user_id,
-              // TODO: handle groups.
-              group_id: null,
-              seed: row.Seed,
-              params: row.Params,
-              true_answer: row['True answer'],
-              options: row.Options,
-              course_id,
+              instance_number: row['Assessment instance'] ?? 1,
             },
             IdSchema,
           );
+        }
 
-          await sqldb.queryRow(
-            sql.insert_submission,
-            {
-              variant_id,
-              authn_user_id,
-              submitted_answer: row['Submitted answer'],
-              partial_scores: row['Partial Scores'],
-              override_score: row['Override score'],
-              credit: row.Credit,
-              mode: row.Mode,
-              grading_requested_at: row['Grading requested date'],
-              graded_at: row['Grading date'],
-              score: row.Score,
-              correct: row.Correct,
-              feedback: row.Feedback,
-              params: row.Params,
-              true_answer: row['True answer'],
-              submission_date: row['Submission date'] ?? new Date(),
-            },
-            IdSchema,
-          );
+        const question = await selectQuestionByQid({ course_id, qid: row.Question });
+        const assessmentQuestion = await sqldb.queryRow(
+          sql.select_assessment_question,
+          { assessment_id, question_id: question.id },
+          AssessmentQuestionSchema,
+        );
 
-          successCount++;
-        } catch (err) {
-          errorCount++;
-          let errorMsg = `Error processing CSV line ${lineNumber}: ${JSON.stringify(rawJson)}\n`;
-          if (err instanceof z.ZodError) {
-            errorMsg += `Validation Error: ${err.errors
+        const instance_question_id = await sqldb.queryRow(
+          sql.insert_instance_question,
+          { assessment_instance_id, assessment_question_id: assessmentQuestion.id },
+          IdSchema,
+        );
+
+        const variant_id = await sqldb.queryRow(
+          sql.insert_variant,
+          {
+            instance_question_id,
+            question_id: question.id,
+            authn_user_id: user.user_id,
+            user_id: user.user_id,
+            // TODO: handle groups.
+            group_id: null,
+            seed: row.Seed,
+            params: row.Params,
+            true_answer: row['True answer'],
+            options: row.Options,
+            course_id,
+          },
+          IdSchema,
+        );
+
+        await sqldb.queryRow(
+          sql.insert_submission,
+          {
+            variant_id,
+            authn_user_id,
+            submitted_answer: row['Submitted answer'],
+            partial_scores: row['Partial Scores'],
+            override_score: row['Override score'],
+            credit: row.Credit,
+            mode: row.Mode,
+            grading_requested_at: row['Grading requested date'],
+            graded_at: row['Grading date'],
+            score: row.Score,
+            correct: row.Correct,
+            feedback: row.Feedback,
+            params: row.Params,
+            true_answer: row['True answer'],
+            submission_date: row['Submission date'] ?? new Date(),
+          },
+          IdSchema,
+        );
+
+        successCount++;
+      } catch (err) {
+        errorCount++;
+        job.error(`Error processing CSV line ${lineNumber}: ${JSON.stringify(rawJson)}`);
+        if (err instanceof z.ZodError) {
+          job.error(
+            `Validation Error: ${err.errors
               .map((e) => `${e.path.join('.')}: ${e.message}`)
-              .join(', ')}`;
-          } else {
-            errorMsg += String(err);
-            // TODO: remove before merging.
-            errorMsg += `\n${err.stack}`;
-            console.error(err);
-          }
-
-          if (output == null) {
-            output = errorMsg;
-          } else {
-            output += '\n' + errorMsg;
-          }
-          job.error(errorMsg);
+              .join(', ')}`,
+          );
+        } else {
+          job.error(String(err));
         }
-
-        outputCount++;
-        if (outputCount >= outputThreshold) {
-          job.verbose(output ?? '');
-          output = null;
-          outputCount = 0;
-          outputThreshold *= 2;
-        }
-      });
-    } finally {
-      if (output != null) {
-        job.verbose(output);
       }
-    }
+    });
 
     if (errorCount === 0) {
       job.info(`Successfully processed ${successCount} submissions, with no errors`);
