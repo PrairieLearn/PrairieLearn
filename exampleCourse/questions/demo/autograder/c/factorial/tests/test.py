69ce427c8509d48196abc998afc104b5fbd1fc4d
James Foster <git@jgfoster.net>
2025-05-06 05:55:26 -0700

    Fix more lint issues.

--- exampleCourse/questions/demo/autograder/c/factorial/tests/test.py
+++ exampleCourse/questions/demo/autograder/c/factorial/tests/test.py
@@ -3,12 +3,14 @@
 import cgrader
 import clang.cindex
 
+
 # For debugging purposes, you can print the AST of the translation unit.
 # Comment out as much as possible (especially #include statements) limit output.
 def print_ast(node, indent=0):
     print(" " * indent + str(node.spelling) + " (" + str(node.kind) + ")")
     for child in node.get_children():
         print_ast(child, indent + 2)
+
 
 def find_factorial_function(node):
     if node.kind == clang.cindex.CursorKind.FUNCTION_DECL and node.spelling == "factorial":
@@ -19,6 +21,7 @@ def find_factorial_function(node):
             return found
     return None
 
+
 def find_if_statement(node):
     if node.kind == clang.cindex.CursorKind.IF_STMT:
         return node
@@ -28,6 +31,7 @@ def find_if_statement(node):
             return found
     return None
 
+
 def find_call_to_factorial(node):
     if node.kind == clang.cindex.CursorKind.CALL_EXPR and node.spelling == "factorial":
         return node
@@ -36,6 +40,7 @@ def find_call_to_factorial(node):
         if found is not None:
             return found
     return None
+
 
 def find_loop(node):
     if node.kind == clang.cindex.CursorKind.FOR_STMT or node.kind == clang.cindex.CursorKind.WHILE_STMT:
@@ -53,7 +58,7 @@ class QuestionGrader(cgrader.CPPGrader):
         translation_unit = index.parse("student.c", args=["-x", "c"])
 
         # Uncomment to print the AST
-        # print_ast(translation_unit.cursor) 
+        # print_ast(translation_unit.cursor)
         factorial_function = find_factorial_function(translation_unit.cursor)
         if factorial_function is None:
             self.add_test_result("factorial function not found", points=0, max_points=1)
@@ -76,7 +81,8 @@ class QuestionGrader(cgrader.CPPGrader):
             "student.c", "student", flags="-Wall -Wextra -pedantic -Werror",
             add_c_file="/grade/tests/main.c"
         )
-        self.test_run("./student", exp_output="SUCCESS", max_points=3);
+        self.test_run("./student", exp_output="SUCCESS", max_points=3)
+
 
 g = QuestionGrader()
 g.start()
