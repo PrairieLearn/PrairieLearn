6903c5d82a4d20f664819aad4edca1fc1d5d5e96
Jonatan Schroeder <jonatan@yorku.ca>
2025-05-06 12:27:04 -0400

    Reformat C file

--- exampleCourse/questions/demo/autograder/c/factorial/tests/main.c
+++ exampleCourse/questions/demo/autograder/c/factorial/tests/main.c
@@ -2,26 +2,24 @@
 
 extern int factorial(int x);
 
-START_TEST(test_factorial)
-{
-  ck_assert_int_eq(factorial(0), 1);
-  ck_assert_int_eq(factorial(1), 1);
-  ck_assert_int_eq(factorial(2), 2);
-  ck_assert_int_eq(factorial(5), 120);
-  ck_assert_int_eq(factorial(10), 3628800);
+START_TEST(test_factorial) {
+    ck_assert_int_eq(factorial(0), 1);
+    ck_assert_int_eq(factorial(1), 1);
+    ck_assert_int_eq(factorial(2), 2);
+    ck_assert_int_eq(factorial(5), 120);
+    ck_assert_int_eq(factorial(10), 3628800);
 }
 END_TEST
 
-int main(void)
-{
-  Suite *s = suite_create("factorial");
+int main(void) {
+    Suite *s = suite_create("factorial");
 
-  TCase *tc_factorial = tcase_create("Check factorial return value");
-  tcase_add_test(tc_factorial, test_factorial);
-  suite_add_tcase(s, tc_factorial);
+    TCase *tc_factorial = tcase_create("Check factorial return value");
+    tcase_add_test(tc_factorial, test_factorial);
+    suite_add_tcase(s, tc_factorial);
 
-  SRunner *sr = srunner_create(s);
-  srunner_run_all(sr, CK_NORMAL);
-  srunner_free(sr);
-  return 0;
+    SRunner *sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    srunner_free(sr);
+    return 0;
 }
