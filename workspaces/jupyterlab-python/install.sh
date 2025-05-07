f8b9ad6a6974f40c7391ed6082aaadc2f389a262
ZacWarham <zacwarham@gmail.com>
2025-05-06 18:42:12 -0700

    Python install changes

--- workspaces/jupyterlab-python/install.sh
+++ workspaces/jupyterlab-python/install.sh
@@ -3,14 +3,14 @@
 set -ex
 
 # Install dependencies and various libraries.
-apt-get update && apt-get -y upgrade
+apt-get update
 apt-get -y install graphviz graphviz-dev
 
 # Install all Python dependencies.
 pip3 install -r /requirements.txt
 
 # Clear various caches to minimize the final image size.
-apt-get clean
+apt-get clean && rm -rf /var/lib/apt/lists/*
 pip3 cache purge
 
 rm /requirements.txt /install.sh
