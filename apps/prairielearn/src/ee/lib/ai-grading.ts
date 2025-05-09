1c940c032da99ccc13dc4839b05fad75bd0d1ab5
Victor Zhao <victorsss.orz@outlook.com>
2025-05-08 19:51:50 -0500

    upload

--- apps/prairielearn/src/ee/lib/ai-grading.ts
+++ apps/prairielearn/src/ee/lib/ai-grading.ts
@@ -77,6 +77,9 @@ export async function aiGrade({
     job.info('Checking for embeddings for all submissions.');
     let newEmbeddingsCount = 0;
     for (const instance_question of instance_questions) {
+      if (instance_question.requires_manual_grading || instance_question.is_ai_graded) {
+        continue;
+      }
       const submission_id = await selectLastSubmissionId(instance_question.id);
       const submission_embedding = await selectEmbeddingForSubmission(submission_id);
       if (!submission_embedding) {
