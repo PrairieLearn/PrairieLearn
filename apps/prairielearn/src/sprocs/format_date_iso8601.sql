49842ce371ebed65eb56fed329f117c93f402506
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-05-09 09:59:16 -0700

    Revert changes to sprocs

--- apps/prairielearn/src/sprocs/format_date_iso8601.sql
+++ apps/prairielearn/src/sprocs/format_date_iso8601.sql
@@ -5,12 +5,6 @@ CREATE FUNCTION
     ) RETURNS text AS $$
 BEGIN
     EXECUTE 'SET LOCAL timezone TO ' || quote_literal(coalesce(display_timezone, 'UTC'));
-    RETURN to_char(d, 'YYYY-MM-DD"T"HH24:MI:SS') ||
-           (CASE
-                WHEN EXTRACT(TIMEZONE FROM d) >= 0 THEN '+'
-                ELSE '-'
-            END) ||
-           to_char(abs(EXTRACT(TIMEZONE_HOUR FROM d)), 'FM00') || ':' ||
-           to_char(abs(EXTRACT(TIMEZONE_MINUTE FROM d)), 'FM00');
+    RETURN to_char(d, 'YYYY-MM-DD') || 'T' || to_char(d, 'HH24:MI:SSOF');
 END;
 $$ LANGUAGE plpgsql VOLATILE;
