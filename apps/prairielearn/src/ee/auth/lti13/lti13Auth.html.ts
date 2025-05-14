ce4d3b6b8ad86b2c17fe2eccfd23888c8ac8d7f1
Dave Mussulman <mussulma@gmail.com>
2025-05-14 11:56:51 -0500

    cleanup

--- apps/prairielearn/src/ee/auth/lti13/lti13Auth.html.ts
+++ apps/prairielearn/src/ee/auth/lti13/lti13Auth.html.ts
@@ -70,9 +70,10 @@ export function Lti13AuthIframe({ parameters }: { parameters: Record<string, any
             </button>
           </form>
           <div id="message">
+            <p>If there are login errors, reload this page to start again.</p>
             ${isAssignmentSelection
-              ? 'Come back here when finished to complete the assignment linking.'
-              : 'If there are login errors, reload this page to start again.'}
+              ? html`<p>Come back here when finished to complete the assignment linking.</p>`
+              : ''}
           </div>
 
           <script>
@@ -86,39 +87,37 @@ export function Lti13AuthIframe({ parameters }: { parameters: Record<string, any
               messageDiv.textContent = 'Reload this page to access PrairieLearn again.';
             });
           </script>
-          ${isAssignmentSelection ? AssignmentSelectionEventListener() : ''}
+          ${isAssignmentSelection
+            ? html`
+                <script>
+                  window.addEventListener('message', function (event) {
+                    const responseForm = document.createElement('form');
+                    responseForm.method = 'POST';
+                    responseForm.action = event.data.return_url;
+
+                    const JWT = document.createElement('input');
+                    JWT.type = 'hidden';
+                    JWT.name = 'JWT';
+                    JWT.value = event.data.JWT;
+                    responseForm.appendChild(JWT);
+
+                    const submit = document.createElement('button');
+                    submit.type = 'submit';
+                    submit.textContent = 'Finish the process';
+                    submit.className = 'btn btn-primary';
+                    responseForm.appendChild(submit);
+
+                    messageDiv.replaceWith(responseForm);
+
+                    responseForm.submit(); // Make this non-interactive
+                  });
+                </script>
+              `
+            : ''}
         </main>
       </body>
     </html>
   `.toString();
-}
-
-function AssignmentSelectionEventListener() {
-  return html`
-    <script>
-      window.addEventListener('message', function (event) {
-        const responseForm = document.createElement('form');
-        responseForm.method = 'POST';
-        responseForm.action = event.data.return_url;
-
-        const JWT = document.createElement('input');
-        JWT.type = 'hidden';
-        JWT.name = 'JWT';
-        JWT.value = event.data.JWT;
-        responseForm.appendChild(JWT);
-
-        const submit = document.createElement('button');
-        submit.type = 'submit';
-        submit.textContent = 'Finish the process';
-        submit.className = 'btn btn-primary';
-        responseForm.appendChild(submit);
-
-        messageDiv.replaceWith(responseForm);
-
-        responseForm.submit(); // Make this non-interactive
-      });
-    </script>
-  `;
 }
 
 export function Lti13AuthRequired({
