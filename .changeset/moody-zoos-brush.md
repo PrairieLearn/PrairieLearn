8b1138e0cfe586d155b5f027cbc4faf5243710bd
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2025-05-15 16:19:30 -0700

    Add workspace options to question settings page (#11811)
    * Add workspace options to question settings page
    
    * remove unused class
    
    * update args and gradedFiles
    
    * update validation
    
    * Update apps/prairielearn/src/pages/instructorQuestionSettings/instructorQuestionSettings.html.ts
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>
    
    * Update apps/prairielearn/src/pages/instructorQuestionSettings/instructorQuestionSettings.html.ts
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>
    
    * address review comments
    
    * spelling
    
    * spelling
    
    * always store args as array
    
    * Address review comments
    
    * formatting
    
    * fix failing tests
    
    * address review comments
    
    * changeset
    
    * format
    
    * Update apps/prairielearn/src/pages/instructorQuestionSettings/instructorQuestionSettings.html.ts
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>
    
    * Update packages/zod/src/index.ts
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>
    
    * address review comments
    
    * update changeset
    
    * add tests for array schema
    
    * fix failing test
    
    * Update packages/zod/src/index.ts
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>
    
    * Update apps/prairielearn/src/lib/editors.ts
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>
    
    * address review comments
    
    * address review comments
    
    * update checkbox handling
    
    * Add explicit check for type of target
    
    ---------
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>

---
+++ .changeset/moody-zoos-brush.md
@@ -0,0 +1,5 @@
+---
+'@prairielearn/zod': minor
+---
+
+Added schemas for parsing integers and arrays.
--- apps/prairielearn/src/lib/db-types.ts
+++ apps/prairielearn/src/lib/db-types.ts
@@ -26,6 +26,9 @@ export const JsonCommentSchema = z.union([z.string(), z.array(z.any()), z.record
 // Enum schemas. These should be alphabetized by their corresponding enum name.
 // *******************************************************************************
 
+export const EnumGradingMethodSchema = z.enum(['Internal', 'External', 'Manual']);
+export type EnumGradingMethod = z.infer<typeof EnumGradingMethodSchema>;
+
 export const EnumJobStatusSchema = z.enum(['Running', 'Success', 'Error']);
 export type EnumJobStatus = z.infer<typeof EnumJobStatusSchema>;
 
@@ -823,7 +826,7 @@ export const QuestionSchema = z.object({
   external_grading_files: z.any().nullable(),
   external_grading_image: z.string().nullable(),
   external_grading_timeout: z.number().nullable(),
-  grading_method: z.enum(['Internal', 'External', 'Manual']),
+  grading_method: EnumGradingMethodSchema,
   id: IdSchema,
   json_comment: JsonCommentSchema.nullable(),
   json_external_grading_comment: z
