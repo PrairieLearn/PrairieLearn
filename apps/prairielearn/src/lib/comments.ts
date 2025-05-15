d3b16b823bf2971e9a8715c8701dfdecf8c76d4e
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2025-05-15 13:08:07 -0700

    Display access rule comments (#11947)
    * Display access rule comments
    
    * Add hidden column heading
    
    * Add hidden column heading
    
    * stringify objects
    
    * remove unused imports
    
    * address review comments
    
    * handle whitespace comment
    
    * Update apps/prairielearn/src/components/CommentPopover.html.ts
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>
    
    * address review comments
    
    * update imports
    
    * address nits
    
    * update imports
    
    * address comments
    
    ---------
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>

---
+++ apps/prairielearn/src/lib/comments.ts
@@ -0,0 +1,16 @@
+/**
+ * Returns a boolean indicating that the comment is not null or empty and should be rendered.
+ *
+ * @param comment - the comment being assessed
+ */
+export function isRenderableComment(comment: string | string[] | Record<string, any> | null) {
+  if (
+    !comment ||
+    (typeof comment === 'string' && comment.trim() === '') ||
+    (Array.isArray(comment) && comment.length === 0) ||
+    (typeof comment === 'object' && Object.keys(comment).length === 0)
+  ) {
+    return false;
+  }
+  return true;
+}
