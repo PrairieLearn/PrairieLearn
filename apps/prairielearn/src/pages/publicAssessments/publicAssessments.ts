405c1adc61e11bea79d54b8d86e1ccf36ab09e9f
Seth Poulsen <poulsenseth@yahoo.com>
2025-05-15 22:11:58 -0500

    Improve typing, use reslocals less
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>

--- apps/prairielearn/src/pages/publicAssessments/publicAssessments.ts
+++ apps/prairielearn/src/pages/publicAssessments/publicAssessments.ts
@@ -15,21 +15,20 @@ const router = express.Router({ mergeParams: true });
 router.get(
   '/',
   asyncHandler(async (req, res) => {
-    const course_instance_id = req.params.course_instance_id;
-    const courseInstance = await selectCourseInstanceById(course_instance_id);
+    const courseInstance = await selectCourseInstanceById(req.params.course_instance_id);
     if (courseInstance === null) {
       throw new error.HttpStatusError(404, 'Not Found');
     }
     res.locals.course_instance = courseInstance;
-    res.locals.course = await selectCourseById(res.locals.course_instance.course_id);
+    res.locals.course = await selectCourseById(courseInstance.course_id);
 
-    const isPublic = await selectCourseInstanceIsPublic(course_instance_id);
+    const isPublic = await selectCourseInstanceIsPublic(courseInstance.id);
     if (!isPublic) {
       throw new error.HttpStatusError(404, 'Course instance not public.');
     }
 
     const rows = await selectAssessments({
-      course_instance_id: res.locals.course_instance.id,
+      course_instance_id: courseInstance.id,
     });
 
     res.send(
