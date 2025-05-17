5a0ad124a2b2d09ee984c300538bc67a60d8a9dd
miguelaenlle <bot.developer3@gmail.com>
2025-05-17 08:25:09 -0500

    Moved update CI usage function to aiQuestionGeneration from ai.ts

--- apps/prairielearn/src/lib/ai.ts
+++ apps/prairielearn/src/lib/ai.ts
@@ -1,8 +1,4 @@
-import { loadSqlEquiv, queryAsync } from '@prairielearn/postgres';
-
 import { config } from './config.js';
-
-const sql = loadSqlEquiv(import.meta.url);
 
 /**
  * Compute the cost of a completion, in US dollars.
@@ -19,28 +15,4 @@ export function computeCompletionCost({
       config.costPerMillionCompletionTokens * completionTokens) /
     1e6
   );
-}
-
-/**
- * Create a new course instance usage record for an AI-based completion request.
- */
-export async function updateCourseInstanceUsagesForAiCompletion({
-  promptId,
-  authnUserId,
-  promptTokens = 0,
-  completionTokens = 0,
-}: {
-  promptId: string;
-  authnUserId: string;
-  promptTokens?: number;
-  completionTokens?: number;
-}) {
-  await queryAsync(sql.update_course_instance_usages_for_ai_question_generation, {
-    prompt_id: promptId,
-    authn_user_id: authnUserId,
-    cost_ai_question_generation: computeCompletionCost({
-      promptTokens,
-      completionTokens,
-    }),
-  });
 }
