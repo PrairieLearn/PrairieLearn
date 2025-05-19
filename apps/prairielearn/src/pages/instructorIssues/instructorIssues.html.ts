6b9bd25ad625b1e60100ef3626753876f79a0c25
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-05-19 10:10:40 -0700

    Consistently render linebreaks in issue student message

--- apps/prairielearn/src/pages/instructorIssues/instructorIssues.html.ts
+++ apps/prairielearn/src/pages/instructorIssues/instructorIssues.html.ts
@@ -276,7 +276,7 @@ function IssueRow({
                   </button>
                 `}
         </div>
-        <p class="mb-0">${getFormattedMessage(issue)}</p>
+        <p class="mb-0" style="white-space: pre-wrap;">${getFormattedMessage(issue)}</p>
         <small class="text-muted me-2">
           #${issue.id} reported
           ${issue.date
@@ -319,9 +319,8 @@ function IssueRow({
 
 function getFormattedMessage(issue: Issue) {
   if (!issue.student_message) return html`&mdash;`;
-
-  const message = joinHtml(issue.student_message.split(/\r?\n|\r/), html`<br />`);
-  return issue.manually_reported ? html`"${message}"` : message;
+  if (issue.manually_reported) return `"${issue.student_message}"`;
+  return issue.student_message;
 }
 
 function CloseMatchingIssuesModal({
