8579402fb10ba07093811ae6c08b9e6d3e28e109
skyler-oakeson <skyler.oakeson@gmail.com>
2025-05-20 13:07:57 -0600

    Changed special characters to frozenset and use intersection to find offending characters.

--- apps/prairielearn/elements/pl-order-blocks/pl-order-blocks.py
+++ apps/prairielearn/elements/pl-order-blocks/pl-order-blocks.py
@@ -97,6 +97,7 @@ SOLUTION_HEADER_DEFAULT = "Construct your solution here:"
 FILE_NAME_DEFAULT = "user_code.py"
 WEIGHT_DEFAULT = 1
 TAB_SIZE_PX = 50
+SPEC_CHAR = frozenset("*&^$@!~[]{}()|:@?/\\")
 FIRST_WRONG_FEEDBACK = {
     "incomplete": "Your answer is correct so far, but it is incomplete.",
     "wrong-at-block": r"""Your answer is incorrect starting at <span style="color:red;">block number {}</span>.
@@ -294,9 +295,7 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
             )
 
         tag, depends = get_graph_info(html_tags)
-        spec_char_list = list("*&^$@!~[]{}()|:@?/\\")
-        invalid_chars = [c for c in spec_char_list if c in tag]
-        if len(invalid_chars) != 0:
+        if SPEC_CHAR.intersection(frozenset(tag)):
             raise ValueError(
                 f'<pl-answer tag="{tag}"> tag attribute may not contain special characters: "*&^$@!~[]{{}}()|:@?/\\"'
             )
