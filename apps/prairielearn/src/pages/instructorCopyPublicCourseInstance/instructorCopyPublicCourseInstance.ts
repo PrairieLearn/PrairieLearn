59d3a8e9c70314749f0b228611d2669cc2cbbc71
Seth Poulsen <poulsenseth@yahoo.com>
2025-05-20 13:28:31 -0600

    simplify

--- apps/prairielearn/src/pages/instructorCopyPublicCourseInstance/instructorCopyPublicCourseInstance.ts
+++ apps/prairielearn/src/pages/instructorCopyPublicCourseInstance/instructorCopyPublicCourseInstance.ts
@@ -4,7 +4,6 @@ import asyncHandler from 'express-async-handler';
 import * as error from '@prairielearn/error';
 
 import { copyCourseInstanceBetweenCourses } from '../../lib/copy-content.js';
-import { idsEqual } from '../../lib/id.js';
 import { selectCourseInstanceById } from '../../models/course-instances.js';
 import { selectCourseById } from '../../models/course.js';
 
@@ -13,25 +12,13 @@ const router = Router();
 router.post(
   '/',
   asyncHandler(async (req, res) => {
-    // It doesn't make much sense to transfer a template course instance to
-    // the same template course, so we'll explicitly forbid that.
-
-    if (idsEqual(req.body.course_id, res.locals.course.id)) {
-      throw new error.HttpStatusError(
-        400,
-        'Template course instances cannot be copied to the same course.',
-      );
-    }
-
-    // This query will implicitly check that the course instance belongs to the given
-    // course. We ensure below that the course instance is in fact in a template course.
     const courseInstance = await selectCourseInstanceById(req.body.course_instance_id);
     if (courseInstance === null) {
       throw new error.HttpStatusError(404, 'Not Found');
     }
     const course = await selectCourseById(courseInstance.course_id);
 
-    if (!course.template_course && !courseInstance.share_source_publicly) {
+    if (!courseInstance.share_source_publicly) {
       throw new error.HttpStatusError(400, 'Copying this course instance is not permitted');
     }
 
