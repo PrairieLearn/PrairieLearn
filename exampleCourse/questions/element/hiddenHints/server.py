2ea0b3342751d88cd810b109fe0b620d2592248c
ThePageGuy <thepageguy@mailfence.com>
2025-05-21 16:35:16 -0700

    Changes base on Feedback

--- exampleCourse/questions/element/hiddenHints/server.py
+++ exampleCourse/questions/element/hiddenHints/server.py
@@ -1,14 +1,7 @@
 # Hint is Shown if the user clicks Save & Grade
 
 
+# You could also put this as a parse function 
 def grade(data):
-    if data["submitted_answers"]["c_2"] == 35:
-        data["feedback"]["show_hint_35"] = True
-
-
-# Hint is Shown if the user clicks Save only
-
-
-def parse(data):
-    if data["submitted_answers"]["c_2"] == 53:
-        data["feedback"]["show_hint_53"] = True
+    if "c_2" in data["submitted_answers"] and data["submitted_answers"]["c_2"] == 10:
+        data["feedback"]["show_hint_10"] = True
