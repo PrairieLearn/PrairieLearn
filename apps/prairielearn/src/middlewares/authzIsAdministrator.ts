ee10fcf8fa634b52c15f5b7dc5905456e3638d54
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-05-22 15:59:25 -0700

    More logging

--- apps/prairielearn/src/middlewares/authzIsAdministrator.ts
+++ apps/prairielearn/src/middlewares/authzIsAdministrator.ts
@@ -4,6 +4,9 @@ import { HttpStatusError } from '@prairielearn/error';
 
 export default function (req: Request, res: Response, next: NextFunction) {
   if (!res.locals.is_administrator) {
+    console.dir(res.locals, { depth: null });
+    console.log(req.headers);
+    console.log(req.cookies);
     return next(new HttpStatusError(403, 'Requires administrator privileges'));
   }
   next();
