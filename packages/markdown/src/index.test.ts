e72109bd5c5bfe0424702bf55e090518fa352514
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2025-05-22 14:37:13 -0700

    prettier

--- packages/markdown/src/index.test.ts
+++ packages/markdown/src/index.test.ts
@@ -141,7 +141,8 @@ describe('Markdown processing', () => {
   });
 
   it('sanitizes an HTML block if allowHtml is false', async () => {
-    const question = '_Before_ the block\n\n<div>HTML block to be sanitized</div>\n\n**After** the block';
+    const question =
+      '_Before_ the block\n\n<div>HTML block to be sanitized</div>\n\n**After** the block';
     const expected = '<p><em>Before</em> the block</p>\n<p><strong>After</strong> the block</p>';
     await testMarkdown(question, expected, { allowHtml: false });
   });
