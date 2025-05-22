58df5fd2037af374cbce10d3db63c093c80df20d
Pete Stenger <pete@stenger.io>
2025-05-22 16:18:29 -0400

    variant access

--- apps/prairielearn/src/tests/variantAccess.test.ts
+++ apps/prairielearn/src/tests/variantAccess.test.ts
@@ -240,27 +240,35 @@ describe('Variant access', () => {
     });
   });
 
-  test.sequential('create other variant from public question preview', async () => {
-    await withUser(OTHER_PUBLIC_USER, async () => {
-      const url = `${siteUrl}/pl/public/course/1/question/${question.id}/preview`;
-      const res = await fetchCheerio(url);
-      assert.equal(res.status, 200);
-      otherPublicVariantId = getVariantId(res.$);
-      otherPublicVariantWorkspaceUrl = getWorkspaceUrl(res.$);
-      otherPublicVariantSubmissionId = await makeSubmission(url, res.$);
-    });
-  });
+  test.sequential(
+    'create other variant from public question preview',
+    { timeout: 20_000 },
+    async () => {
+      await withUser(OTHER_PUBLIC_USER, async () => {
+        const url = `${siteUrl}/pl/public/course/1/question/${question.id}/preview`;
+        const res = await fetchCheerio(url);
+        assert.equal(res.status, 200);
+        otherPublicVariantId = getVariantId(res.$);
+        otherPublicVariantWorkspaceUrl = getWorkspaceUrl(res.$);
+        otherPublicVariantSubmissionId = await makeSubmission(url, res.$);
+      });
+    },
+  );
 
-  test.sequential('create variant from instructor question preview', async () => {
-    await withUser(INSTRUCTOR_USER, async () => {
-      const url = `${siteUrl}/pl/course/1/question/${question.id}/preview`;
-      const res = await fetchCheerio(url);
-      assert.equal(res.status, 200);
-      instructorVariantId = getVariantId(res.$);
-      instructorVariantWorkspaceUrl = getWorkspaceUrl(res.$);
-      instructorVariantSubmissionId = await makeSubmission(url, res.$);
-    });
-  });
+  test.sequential(
+    'create variant from instructor question preview',
+    { timeout: 20_000 },
+    async () => {
+      await withUser(INSTRUCTOR_USER, async () => {
+        const url = `${siteUrl}/pl/course/1/question/${question.id}/preview`;
+        const res = await fetchCheerio(url);
+        assert.equal(res.status, 200);
+        instructorVariantId = getVariantId(res.$);
+        instructorVariantWorkspaceUrl = getWorkspaceUrl(res.$);
+        instructorVariantSubmissionId = await makeSubmission(url, res.$);
+      });
+    },
+  );
 
   test.sequential('create variant from student assessment instance', async () => {
     await withUser(STUDENT_USER, async () => {
