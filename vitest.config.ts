f9cad1f38415a3a721a849be09d8e7a23bbea5a0
Pete Stenger <pete@stenger.io>
2025-05-23 13:17:15 -0400

    test

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, 'apps/grader-host/**', 'apps/workspace-host/**'],
+    exclude: [...configDefaults.exclude, 'apps/grader-host/**/*', 'apps/workspace-host/**/*'],
     coverage: {
       all: true,
       reporter: ['html', 'text-summary', 'cobertura'],
