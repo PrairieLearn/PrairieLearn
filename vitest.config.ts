023053958e8f4283bbcee95b1182e9e824c0ed6c
Pete Stenger <pete@stenger.io>
2025-05-23 13:01:04 -0400

    root coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { configDefaults, defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, 'apps/grader-host/**', 'apps/workspace-host/**'],
+    coverage: {
+      all: true,
+      reporter: ['html', 'text-summary', 'cobertura'],
+      include: ['packages/*/src/**', 'apps/prairielearn/src/**'],
+    },
   },
 });
