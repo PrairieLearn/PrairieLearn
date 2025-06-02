9e19dba4c768db7fe304e836647c1655e09e1971
Pete Stenger <pete@stenger.io>
2025-06-02 19:51:45 -0400

    attempt again

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
-import { configDefaults, defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, 'apps/grader-host/**/*', 'apps/workspace-host/**/*'],
+    include: ['apps/prairielearn/**/*.test.ts', 'packages/**/*.test.ts'],
     coverage: {
       all: true,
       reporter: ['html', 'text-summary', 'cobertura'],
-      include: ['packages/*/src/**', 'apps/prairielearn/src/**'],
+      include: ['apps/prairielearn/src/**'],
     },
   },
 });
