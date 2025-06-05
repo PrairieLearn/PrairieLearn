d8da74645f894f99ae81e221bcbf816df292e823
Pete Stenger <pete@stenger.io>
2025-06-05 15:54:29 -0400

    disable isolate

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    isolate: false,
     projects: ['packages/*', 'apps/prairielearn', 'apps/grader-host', 'apps/workspace-host'],
     include: ['apps/**/*.test.ts', 'packages/**/*.test.ts'],
     coverage: {
