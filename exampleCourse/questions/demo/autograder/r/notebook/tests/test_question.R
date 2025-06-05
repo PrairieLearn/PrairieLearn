cffb506e89c874c57e4b2a7e65ebbbfc053c957d
ZacWarham <zacwarham@gmail.com>
2025-06-05 16:57:03 -0700

    Remove additional comment

--- exampleCourse/questions/demo/autograder/r/notebook/tests/test_question.R
+++ exampleCourse/questions/demo/autograder/r/notebook/tests/test_question.R
@@ -1,15 +1,7 @@
-
 ## Package(s) we need
 
 library(tinytest)                       # load testrunner
 using(ttdo)                             # enable its 'diffobj' extension
-
-
-## Get student's response
-#plr::source_and_eval_safe_with_hiding("/grade/student/student.R",
-#                                      NULL,
-#                                      "ag",
-#                                      "/grade/tests/ans.R")
 
 ## @title Does the code evaluate?
 ## @score 1
