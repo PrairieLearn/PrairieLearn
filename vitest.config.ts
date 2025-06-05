d4ef30deda673d7735a3bf9eb19352ace80b5710
Pete Stenger <pete@stenger.io>
2025-06-05 17:11:44 -0400

    Attempt without specific includes

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   test: {
     isolate: false,
     projects: ['packages/*', 'apps/prairielearn', 'apps/grader-host', 'apps/workspace-host'],
-    include: ['apps/**/*.test.ts', 'packages/**/*.test.ts'],
     coverage: {
       all: true,
       reporter: ['html', 'text-summary', 'cobertura'],
