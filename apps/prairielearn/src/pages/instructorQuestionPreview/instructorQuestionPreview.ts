ab2914f116816234e5b9ce5edda7c543be2082f4
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-06-09 15:12:32 -0700

    Remove legacy question processors (#12132)
    * Remove legacy question processors
    
    * Remove references to old question processors
    
    * Update JSON schemas
    
    * Fix lint
    
    * Remove more remnants of feature flags

--- apps/prairielearn/src/pages/instructorQuestionPreview/instructorQuestionPreview.ts
+++ apps/prairielearn/src/pages/instructorQuestionPreview/instructorQuestionPreview.ts
@@ -11,11 +11,7 @@ import { setQuestionCopyTargets } from '../../lib/copy-content.js';
 import { IdSchema } from '../../lib/db-types.js';
 import { features } from '../../lib/features/index.js';
 import { reportIssueFromForm } from '../../lib/issues.js';
-import {
-  getAndRenderVariant,
-  renderPanelsForSubmission,
-  setRendererHeader,
-} from '../../lib/question-render.js';
+import { getAndRenderVariant, renderPanelsForSubmission } from '../../lib/question-render.js';
 import { processSubmission } from '../../lib/question-submission.js';
 import { getSearchParams } from '../../lib/url.js';
 import { logPageView } from '../../middlewares/logPageView.js';
@@ -113,7 +109,6 @@ router.get(
       renderSubmissionSearchParams.set('ai_grading_preview', 'true');
     }
 
-    setRendererHeader(res);
     res.send(
       InstructorQuestionPreview({
         normalPreviewUrl,
