7cc81482469b6a0e3e611584c07ee5e6b11d27d4
ZacWarham <zacwarham@gmail.com>
2025-06-09 23:55:37 -0700

    Convert notebook question to workspace

--- exampleCourse/questions/demo/workspace/jupyterlab-r/workspace/student.R
+++
@@ -1,8 +0,0 @@
-##
-## Please write your code below.
-##
-## Write a standard R expression assigning a variable,
-## i.e. write 'myVariable <- 1.23' or 'myVariable = 1.23'
-##
-## (We do not ask for 'myVariable' in the question.)
-##
