b37b87b8eef1324c19257444f1d776df8a120e7a
Peter Stenger <peter.a.stenger@gmail.com>
2025-06-12 12:39:42 -0500

    Update apps/prairielearn/vitest.config.ts

--- apps/prairielearn/vitest.config.ts
+++ apps/prairielearn/vitest.config.ts
@@ -32,7 +32,7 @@ export default defineConfig(({ mode }) => {
     if (isRunningOnDist) throw new Error('Cannot run docker-smoke-tests tests on dist files.');
     include = dockerSmokeTests;
   } else if (mode === 'exclude-docker-smoke-tests') {
-    if (isRunningOnDist) throw new Error('Cannot run only-local tests on dist files.');
+    if (isRunningOnDist) throw new Error('Cannot run exclude-docker-smoke-tests tests on dist files.');
     exclude = [...dockerSmokeTests, ...configDefaults.exclude];
   } else if (isRunningOnDist) {
     include = [join(import.meta.dirname, 'dist/**/*.test.js')];
