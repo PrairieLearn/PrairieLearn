6005631b395d734a5bcaee03b143ae6dcccbf0a6
Dave Mussulman <mussulma@gmail.com>
2025-06-13 13:20:41 -0500

    clarify lti11 feature flag scope

--- apps/prairielearn/src/lib/features/index.ts
+++ apps/prairielearn/src/lib/features/index.ts
@@ -12,7 +12,7 @@ const featureNames = [
   'ai-question-generation-course-toggle',
   // Can be applied to any context.
   'ai-question-generation',
-  // LTI 1.1. Can be applied to any context.
+  // LTI 1.1. Deprecated so keep scope to course instance, where possible.
   'lti11',
   // Should only be applied globally.
   'enhanced-navigation-user-toggle',
