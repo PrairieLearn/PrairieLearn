2c2b93d5e60a42fe54c14d8ee7932e6ff68ff0c4
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-06-13 09:31:23 -0700

    Combine branches

--- apps/prairielearn/src/pages/instructorAssessmentManualGrading/assessmentQuestion/assessmentQuestion.ts
+++ apps/prairielearn/src/pages/instructorAssessmentManualGrading/assessmentQuestion/assessmentQuestion.ts
@@ -3,6 +3,7 @@ import asyncHandler from 'express-async-handler';
 
 import * as error from '@prairielearn/error';
 import { loadSqlEquiv, queryAsync, queryRows } from '@prairielearn/postgres';
+import { run } from '@prairielearn/run';
 
 import { fillInstanceQuestionColumns } from '../../../ee/lib/ai-grading/ai-grading-stats.js';
 import { aiGrade } from '../../../ee/lib/ai-grading/ai-grading.js';
@@ -95,10 +96,10 @@ router.post(
     }
     if (req.body.__action === 'batch_action') {
       if (req.body.batch_action === 'ai_grade_assessment_selected') {
-        const ai_grading_enabled = await features.enabledFromLocals('ai-grading', res.locals);
-        if (!ai_grading_enabled) {
+        if (!(await features.enabledFromLocals('ai-grading', res.locals))) {
           throw new error.HttpStatusError(403, 'Access denied (feature not available)');
         }
+
         const instance_question_ids = Array.isArray(req.body.instance_question_id)
           ? req.body.instance_question_id
           : [req.body.instance_question_id];
@@ -163,11 +164,15 @@ router.post(
       } else {
         res.send({});
       }
-    } else if (req.body.__action === 'ai_grade_assessment') {
-      const ai_grading_enabled = await features.enabledFromLocals('ai-grading', res.locals);
-      if (!ai_grading_enabled) {
+    } else if (
+      ['ai_grade_assessment', 'ai_grade_assessment_graded', 'ai_grade_assessment_all'].includes(
+        req.body.__action,
+      )
+    ) {
+      if (!(await features.enabledFromLocals('ai-grading', res.locals))) {
         throw new error.HttpStatusError(403, 'Access denied (feature not available)');
       }
+
       const jobSequenceId = await aiGrade({
         question: res.locals.question,
         course: res.locals.course,
@@ -176,40 +181,14 @@ router.post(
         urlPrefix: res.locals.urlPrefix,
         authn_user_id: res.locals.authn_user.user_id,
         user_id: res.locals.user.user_id,
-        mode: 'ungraded',
+        mode: run(() => {
+          if (req.body.__action === 'ai_grade_assessment') return 'ungraded';
+          if (req.body.__action === 'ai_grade_assessment_graded') return 'human_graded';
+          if (req.body.__action === 'ai_grade_assessment_all') return 'all';
+          throw new Error(`Unknown action: ${req.body.__action}`);
+        }),
       });
-      res.redirect(res.locals.urlPrefix + '/jobSequence/' + jobSequenceId);
-    } else if (req.body.__action === 'ai_grade_assessment_graded') {
-      const ai_grading_enabled = await features.enabledFromLocals('ai-grading', res.locals);
-      if (!ai_grading_enabled) {
-        throw new error.HttpStatusError(403, 'Access denied (feature not available)');
-      }
-      const jobSequenceId = await aiGrade({
-        question: res.locals.question,
-        course: res.locals.course,
-        course_instance_id: res.locals.course_instance.id,
-        assessment_question: res.locals.assessment_question,
-        urlPrefix: res.locals.urlPrefix,
-        authn_user_id: res.locals.authn_user.user_id,
-        user_id: res.locals.user.user_id,
-        mode: 'human_graded',
-      });
-      res.redirect(res.locals.urlPrefix + '/jobSequence/' + jobSequenceId);
-    } else if (req.body.__action === 'ai_grade_assessment_all') {
-      const ai_grading_enabled = await features.enabledFromLocals('ai-grading', res.locals);
-      if (!ai_grading_enabled) {
-        throw new error.HttpStatusError(403, 'Access denied (feature not available)');
-      }
-      const jobSequenceId = await aiGrade({
-        question: res.locals.question,
-        course: res.locals.course,
-        course_instance_id: res.locals.course_instance.id,
-        assessment_question: res.locals.assessment_question,
-        urlPrefix: res.locals.urlPrefix,
-        authn_user_id: res.locals.authn_user.user_id,
-        user_id: res.locals.user.user_id,
-        mode: 'all',
-      });
+
       res.redirect(res.locals.urlPrefix + '/jobSequence/' + jobSequenceId);
     } else if (req.body.__action === 'toggle_ai_grading_mode') {
       await queryAsync(sql.toggle_ai_grading_mode, {
