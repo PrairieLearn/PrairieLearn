b394457d1b60c7aa59d7399709945175f3019696
Peter Stenger <peter.a.stenger@gmail.com>
2025-06-13 11:22:12 -0500

    Update packages/compiled-assets/src/index.ts
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>

--- packages/compiled-assets/src/index.ts
+++ packages/compiled-assets/src/index.ts
@@ -239,7 +239,7 @@ export function compiledStylesheetPath(sourceFile: string): string {
 }
 
 export function compiledScriptTag(sourceFile: string): HtmlSafeString {
-  // Creates a script tag for a source file processed with IIFE.
+  // Creates a script tag for an IIFE bundle.
   return html`<script src="${compiledScriptPath(sourceFile)}"></script>`;
 }
 
