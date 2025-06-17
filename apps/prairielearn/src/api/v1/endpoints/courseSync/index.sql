28ac13c9e7a07a91e5571ff7ec6b7c0301230533
Zac Warham <48937711+ZacWarham@users.noreply.github.com>
2025-06-17 13:25:22 -0700

    Update apps/prairielearn/src/api/v1/endpoints/courseSync/index.sql
    Co-authored-by: Nathan Sarang-Walters <nwalters512@gmail.com>

--- apps/prairielearn/src/api/v1/endpoints/courseSync/index.sql
+++ apps/prairielearn/src/api/v1/endpoints/courseSync/index.sql
@@ -1,20 +1,13 @@
 -- BLOCK select_job
-WITH
-  object_data AS (
-    SELECT
-      job_sequence_id,
-      start_date,
-      finish_date,
-      status,
-      output
-    FROM
-      jobs
-    WHERE
-      course_id = $course_id
-      AND job_sequence_id = $job_sequence_id
-      AND type = 'sync'
-  )
 SELECT
-  to_jsonb(object_data) AS item
+  job_sequence_id,
+  start_date,
+  finish_date,
+  status,
+  output
 FROM
-  object_data;
+  jobs
+WHERE
+  course_id = $course_id
+  AND job_sequence_id = $job_sequence_id
+  AND type = 'sync'
