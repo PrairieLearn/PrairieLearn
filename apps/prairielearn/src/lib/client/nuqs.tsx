3e80dda1eb5eb952bf64e953327df4a773375c81
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-06-23 16:25:01 -0700

    Add JSDoc comment

--- apps/prairielearn/src/lib/client/nuqs.tsx
+++ apps/prairielearn/src/lib/client/nuqs.tsx
@@ -19,6 +19,13 @@ function useExpressAdapterContext(): unstable_AdapterInterface {
 
 const NuqsExpressAdapter = unstable_createAdapterProvider(useExpressAdapterContext);
 
+/**
+ * `nuqs` needs to be aware of the current state of the URL search parameters
+ * during both server-side and client-side rendering. To make this work with
+ * our server-side rendering setup, we use a custom adapter that should be
+ * provided with the value of `new URL(...).search` on the server side. On the
+ * client, we use `NuqsReactAdapter`, which will read directly from `location.search`.
+ */
 export function NuqsAdapter({ children, search }: { children: React.ReactNode; search: string }) {
   if (typeof location === 'undefined') {
     // We're rendering on the server.
