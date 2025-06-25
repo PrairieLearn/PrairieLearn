499dc42eb27c96c6e52de6d6cb4baf17c0199b10
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2025-06-25 14:56:58 -0700

    Convert course topics page to Preact

---
+++ apps/prairielearn/assets/scripts/esm-bundles/react-fragments/InstructorCourseAdminTopicsTable.ts
@@ -0,0 +1,4 @@
+import { InstructorCourseAdminTopicsTable } from '../../../../src/pages/instructorCourseAdminTopics/components/InstructorCourseAdminTopicsTable.js';
+import { registerReactFragment } from '../../behaviors/react-fragments/index.js';
+
+registerReactFragment(InstructorCourseAdminTopicsTable);
---
+++ apps/prairielearn/src/pages/instructorCourseAdminTopics/instructorCourseAdminTopics.html.tsx
@@ -0,0 +1,44 @@
+import { PageLayout } from '../../components/PageLayout.html.js';
+import { CourseSyncErrorsAndWarnings } from '../../components/SyncErrorsAndWarnings.html.js';
+import { type Topic } from '../../lib/db-types.js';
+import { Hydrate } from '../../lib/preact.js';
+
+import { InstructorCourseAdminTopicsTable } from './components/InstructorCourseAdminTopicsTable.js';
+
+export function InstructorCourseAdminTopics({
+  resLocals,
+  topics,
+}: {
+  resLocals: Record<string, any>;
+  topics: Topic[];
+}) {
+  return PageLayout({
+    resLocals,
+    pageTitle: 'Topics',
+    navContext: {
+      type: 'instructor',
+      page: 'course_admin',
+      subPage: 'topics',
+    },
+    options: {
+      fullWidth: true,
+    },
+    content: (
+      <>
+        <div
+          // eslint-disable-next-line @eslint-react/dom/no-dangerously-set-innerhtml
+          dangerouslySetInnerHTML={{
+            __html: CourseSyncErrorsAndWarnings({
+              authz_data: resLocals.authz_data,
+              course: resLocals.course,
+              urlPrefix: resLocals.urlPrefix,
+            }).toString(),
+          }}
+        />
+        <Hydrate>
+          <InstructorCourseAdminTopicsTable topics={topics} />
+        </Hydrate>
+      </>
+    ),
+  });
+}
