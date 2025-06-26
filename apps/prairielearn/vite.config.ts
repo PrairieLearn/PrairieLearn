0201582a9b155270cea54f90f7f07be3a10688dc
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-06-26 15:31:40 -0700

    Fix preact-cjs error

--- apps/prairielearn/vite.config.ts
+++ apps/prairielearn/vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  ssr: {
+    // Force Vite to externalize even linked dependencies. For us, this means
+    // things in the `packages/` directory at the root of the repo.
+    external: true,
+  },
   plugins: [
     ...VitePluginExpress({
       appPath: './src/server.ts',
