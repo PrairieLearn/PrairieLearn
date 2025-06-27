dee01b4fcbaff3258265553718faeb001f153693
Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
2025-06-27 10:37:56 -0700

    remove react fragment

--- apps/prairielearn/assets/scripts/esm-bundles/react-fragments/InstructorCourseAdminTopicsTable.ts
+++
@@ -1,4 +0,0 @@
-import { InstructorCourseAdminTopicsTable } from '../../../../src/pages/instructorCourseAdminTopics/components/InstructorCourseAdminTopicsTable.js';
-import { registerReactFragment } from '../../behaviors/react-fragments/index.js';
-
-registerReactFragment(InstructorCourseAdminTopicsTable);
