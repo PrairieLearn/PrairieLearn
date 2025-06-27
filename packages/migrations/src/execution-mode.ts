a6837f91234e51365af77ed65bfc5e5cde115767
Peter Stenger <peter@prairielearn.com>
2025-06-27 16:57:49 -0500

    comment

--- packages/migrations/src/execution-mode.ts
+++ packages/migrations/src/execution-mode.ts
@@ -1,3 +1,5 @@
+// We don't compare against 'test' since we use the 'MODE' environment variable
+// for running a subset of our tests.
 const IS_VITEST = (import.meta as any).env && (import.meta as any).env.MODE !== 'development';
 const IS_VITE = (import.meta as any).env?.MODE === 'development';
 export const DEV_EXECUTION_MODE = IS_VITEST ? 'test' : IS_VITE ? 'hmr' : 'dev';
