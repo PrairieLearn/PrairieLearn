4347591b3f11e56c6d118c6abf0bd21525380f5a
Zac Warham <48937711+ZacWarham@users.noreply.github.com>
2025-07-01 18:03:30 -0700

    Update workspaces/jupyterlab-r/install.sh
    Co-authored-by: Nathan Sarang-Walters <nwalters512@gmail.com>

--- workspaces/jupyterlab-r/install.sh
+++ workspaces/jupyterlab-r/install.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # Install all R dependencies.
-xargs mamba install --yes < /requirements.txt
+xargs mamba install --yes r-base==4.4.3 r-irkernel==1.3.2
 
 # Clear various caches to minimize the final image size.
 mamba clean --all -f -y
