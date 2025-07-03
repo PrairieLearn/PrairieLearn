8e2c255d55aaad72b48926d8180f0f38d82f5181
Nico Ritschel <nico@elsl.de>
2025-07-03 13:59:25 -0700

    fix sample question

--- exampleCourse/questions/element/symbolicInput/server.py
+++ exampleCourse/questions/element/symbolicInput/server.py
@@ -13,7 +13,7 @@ def generate(data):
     data["correct_answers"]["c"] = pl.to_json(c)
     data["correct_answers"]["dx"] = "x"
     x = sympy.var("x")
-    data["correct_answers"]["lnx"] = pl.to_json(sympy.log(x) - 1)
+    data["correct_answers"]["lnx"] = pl.to_json(sympy.log(x) + 1)
     data["correct_answers"]["nosimplify"] = pl.to_json(2 * x)
     data["correct_answers"]["simplify"] = pl.to_json(x**2 + x + 1)
 
