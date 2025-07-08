c43d7182904236bc7817e0959e175f264ae362e0
Peter Stenger <peter@prairielearn.com>
2025-07-08 13:42:06 -0500

    wip

---
+++ apps/prairielearn/src/pages/studentDetail/studentDetail.html.tsx
@@ -0,0 +1,16 @@
+/**
+ * Overall info:
+ * UID, UIN, Name, Role
+ *
+ * Enrollment info:
+ * - when joined
+ * link to the student gradebook
+ * Shows badge, color, assessment name, points, score
+ *
+ * Do we want: Date started, Duration
+ * Fingerprint? no, individual cheating
+ *
+ * links to individual assessment instances: http://localhost:3000/pl/course_instance/2/instructor/assessment_instance/1
+ *
+ * borrow code from student gradebook
+ */
