c5a918d6470a5fb9ee59963a704460274ff25a5e
Peter Stenger <peter@prairielearn.com>
2025-07-09 13:53:13 -0500

    Switch safe-db-types to use pick

--- apps/prairielearn/src/lib/client/safe-db-types.ts
+++ apps/prairielearn/src/lib/client/safe-db-types.ts
@@ -8,27 +8,45 @@ import {
 
 /** Courses */
 
-export const RawStaffCourseSchema = RawCourseSchema.omit({
-  yearly_enrollment_limit: true,
-  sharing_token: true,
-});
-export const StaffCourseSchema = RawStaffCourseSchema.brand<'StaffCourse'>();
-export type StaffCourse = z.infer<typeof StaffCourseSchema>;
-
-export const RawStudentCourseSchema = RawStaffCourseSchema.omit({
+export const RawStaffCourseSchema = RawCourseSchema.pick({
   announcement_color: true,
   announcement_html: true,
   branch: true,
   commit_hash: true,
   course_instance_enrollment_limit: true,
+  created_at: true,
+  deleted_at: true,
+  display_timezone: true,
+  example_course: true,
+  id: true,
+  institution_id: true,
   json_comment: true,
+  options: true,
   path: true,
   repository: true,
   sharing_name: true,
+  short_name: true,
   show_getting_started: true,
   sync_errors: true,
   sync_job_sequence_id: true,
   sync_warnings: true,
+  template_course: true,
+  title: true,
+});
+export const StaffCourseSchema = RawStaffCourseSchema.brand<'StaffCourse'>();
+export type StaffCourse = z.infer<typeof StaffCourseSchema>;
+
+export const RawStudentCourseSchema = RawStaffCourseSchema.pick({
+  created_at: true,
+  deleted_at: true,
+  display_timezone: true,
+  example_course: true,
+  id: true,
+  institution_id: true,
+  options: true,
+  short_name: true,
+  template_course: true,
+  title: true,
 });
 export const StudentCourseSchema = RawStudentCourseSchema.brand<'StudentCourse'>();
 export type StudentCourse = z.infer<typeof StudentCourseSchema>;
@@ -40,14 +58,15 @@ export const StaffCourseInstanceSchema =
   RawStaffCourseInstanceSchema.brand<'StaffCourseInstance'>();
 export type StaffCourseInstance = z.infer<typeof StaffCourseInstanceSchema>;
 
-export const RawStudentCourseInstanceSchema = RawStaffCourseInstanceSchema.omit({
-  enrollment_limit: true,
-  json_comment: true,
-  share_source_publicly: true,
-  sync_errors: true,
-  sync_job_sequence_id: true,
-  sync_warnings: true,
-  uuid: true,
+export const RawStudentCourseInstanceSchema = RawStaffCourseInstanceSchema.pick({
+  assessments_group_by: true,
+  course_id: true,
+  deleted_at: true,
+  display_timezone: true,
+  hide_in_enroll_page: true,
+  id: true,
+  long_name: true,
+  short_name: true,
 });
 export const StudentCourseInstanceSchema =
   RawStudentCourseInstanceSchema.brand<'StudentCourseInstance'>();
@@ -55,17 +74,22 @@ export type StudentCourseInstance = z.infer<typeof StudentCourseInstanceSchema>;
 
 /** Users */
 
-const RawStaffUserSchema = RawUserSchema.omit({
-  deleted_at: true,
-  lti_context_id: true,
-  lti_course_instance_id: true,
-  lti_user_id: true,
-  stripe_customer_id: true,
-  terms_accepted_at: true,
+const RawStaffUserSchema = RawUserSchema.pick({
+  email: true,
+  institution_id: true,
+  name: true,
+  uid: true,
+  uin: true,
+  user_id: true,
 });
 export const StaffUserSchema = RawStaffUserSchema.brand<'StaffUser'>();
 export type StaffUser = z.infer<typeof StaffUserSchema>;
 
-const RawStudentUserSchema = RawStaffUserSchema.omit({ email: true, uin: true });
+const RawStudentUserSchema = RawStaffUserSchema.pick({
+  institution_id: true,
+  name: true,
+  uid: true,
+  user_id: true,
+});
 export const StudentUserSchema = RawStudentUserSchema.brand<'StudentUser'>();
 export type StudentUser = z.infer<typeof StudentUserSchema>;
