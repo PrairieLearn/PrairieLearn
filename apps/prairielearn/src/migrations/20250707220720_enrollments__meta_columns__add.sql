b1ff8df92e0665e65a1681be36ad13f65e5c0ccf
Peter Stenger <peter@prairielearn.com>
2025-07-09 15:57:02 -0500

    Improve enrollment migration

--- apps/prairielearn/src/migrations/20250707220720_enrollments__meta_columns__add.sql
+++ apps/prairielearn/src/migrations/20250707220720_enrollments__meta_columns__add.sql
@@ -1,39 +1,36 @@
--- Add new enum type for enrollment status if not exists
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'enum_enrollment_status') THEN
-        CREATE TYPE enum_enrollment_status AS ENUM ('invited', 'joined', 'removed', 'blocked');
-    END IF;
-END$$;
-
--- Add status column
+CREATE TYPE enum_enrollment_status AS ENUM ('invited', 'joined', 'removed', 'rejected', 'blocked');
 ALTER TABLE enrollments
 ADD COLUMN status enum_enrollment_status NOT NULL DEFAULT 'joined';
 
--- Add lti_synced column
 ALTER TABLE enrollments
 ADD COLUMN lti_synced BOOLEAN NOT NULL DEFAULT FALSE;
 
--- Add pending_uid column
 ALTER TABLE enrollments
 ADD COLUMN pending_uid TEXT;
 
--- Make user_id nullable
 ALTER TABLE enrollments
 ALTER COLUMN user_id
 DROP NOT NULL;
 
--- Forbid impossible states: invited + lti_synced, blocked + lti_synced
+-- Forbid impossible states: invited + lti_synced, rejected + lti_synced , blocked + lti_synced
 ALTER TABLE enrollments
-ADD CONSTRAINT enrollments_no_invited_synced CHECK (
+ADD CONSTRAINT enrollments_impossible_synced CHECK (
   NOT (
-    status = 'invited'
+    status IN ('invited', 'rejected', 'blocked')
     AND lti_synced
   )
-),
-ADD CONSTRAINT enrollments_no_blocked_synced CHECK (
-  NOT (
-    status = 'blocked'
-    AND lti_synced
-  )
+);
+
+ALTER TABLE enrollments
+ADD CONSTRAINT enrollments_user_id_null_only_if_invited CHECK (
+  -- To reject an invitation, you will need to sign in as the user and then reject the invitation.
+  -- So user_id can be NULL only if status is 'invited'.
+  (user_id IS NULL) = (status = 'invited')
+);
+
+-- Require exactly one of user_id and pending_uid to be NULL.
+ALTER TABLE enrollments
+ADD CONSTRAINT enrollments_exactly_one_null_user_id_pending_uid CHECK (
+  (user_id IS NULL AND pending_uid IS NOT NULL)
+  OR (user_id IS NOT NULL AND pending_uid IS NULL)
 );
