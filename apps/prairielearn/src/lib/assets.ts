e12e1582b72f4d570c5d120e02130184d8065c58
miguelaenlle <bot.developer3@gmail.com>
2025-07-10 11:42:53 -0500

    Addressed nitpick comments

--- apps/prairielearn/src/lib/assets.ts
+++ apps/prairielearn/src/lib/assets.ts
@@ -85,45 +85,40 @@ function getPackageVersion(packageName: string): string {
 
     // Get the resolved path to the package entrypoint, which will look something
     // like `/absolute/path/to/node_modules/package-name/index.js`.
-    try {
-      const pkgJsonPath = run(() => {
-        try {
-          const pkgPath = require.resolve(packageName);
+    const pkgJsonPath = run(() => {
+      try {
+        const pkgPath = require.resolve(packageName);
 
-          // Strip off everything after the last `/node_modules/`, then append the
-          // package name.
-          const nodeModulesToken = '/node_modules/';
-          const lastNodeModulesIndex = pkgPath.lastIndexOf(nodeModulesToken);
-          return path.resolve(
-            pkgPath.slice(0, lastNodeModulesIndex + nodeModulesToken.length),
-            packageName,
-            'package.json',
-          );
-        } catch (err) {
-          // Some packages (namely `cropperjs`) have invalid `package.json` files
-          // that refer to non-existent files. In this case, we can still try to
-          // recover from things by using the path that couldn't be resolved.
-          //
-          // In this case, `err.path` should point to the root of the package.
-          if (err.code === 'MODULE_NOT_FOUND' && err.path) {
-            // Check if the path is a directory
-            if (fs.lstatSync(err.path).isDirectory()) {
-              return path.resolve(err.path, 'package.json');
-            } else {
-              return path.resolve(path.dirname(err.path), 'package.json');
-            }
+        // Strip off everything after the last `/node_modules/`, then append the
+        // package name.
+        const nodeModulesToken = '/node_modules/';
+        const lastNodeModulesIndex = pkgPath.lastIndexOf(nodeModulesToken);
+        return path.resolve(
+          pkgPath.slice(0, lastNodeModulesIndex + nodeModulesToken.length),
+          packageName,
+          'package.json',
+        );
+      } catch (err) {
+        // Some packages (namely `cropperjs`) have invalid `package.json` files
+        // that refer to non-existent files. In this case, we can still try to
+        // recover from things by using the path that couldn't be resolved.
+        //
+        // In this case, `err.path` should point to the root of the package.
+        if (err.code === 'MODULE_NOT_FOUND' && err.path) {
+          // Check if the path is a directory
+          if (fs.lstatSync(err.path).isDirectory()) {
+            return path.resolve(err.path, 'package.json');
+          } else {
+            return path.resolve(path.dirname(err.path), 'package.json');
           }
-
-          throw err;
         }
-      });
 
-      const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf-8'));
-      return pkgJson.version;
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
+        throw err;
+      }
+    });
+
+    const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf-8'));
+    return pkgJson.version;
   }
 }
 
