f8aa9f39d2cca2fd4afc26906c1e992c382f0b48
Nico Ritschel <nico@elsl.de>
2025-07-14 22:44:21 -0500

    fix remaining lifecycle tests

--- apps/prairielearn/elements/pl-big-o-input/pl-big-o-input.py
+++ apps/prairielearn/elements/pl-big-o-input/pl-big-o-input.py
@@ -319,21 +319,33 @@ def test(element_html: str, data: pl.ElementTestData) -> None:
         }
 
     elif result == "incorrect":
-        data["raw_submitted_answers"][name] = f"{random.randint(4, 100):d} * {a_tru}"
-        bigo_type = pl.get_enum_attrib(element, "type", BigOType, BIG_O_TYPE_DEFAULT)
-
-        if bigo_type is BigOType.THETA:
+        if a_tru is not None and a_tru.strip() == "":
+            data["raw_submitted_answers"][name] = f"{random.randint(4, 100):d} * 1"
             data["partial_scores"][name] = {
-                "score": 0.25,
+                "score": 0,
                 "weight": weight,
-                "feedback": bou.THETA_CONSTANT_FACTORS_FEEDBACK,
+                "feedback": bou.INCORRECT_FEEDBACK,
             }
         else:
-            data["partial_scores"][name] = {
-                "score": 0.5,
-                "weight": weight,
-                "feedback": bou.CONSTANT_FACTORS_FEEDBACK,
-            }
+            data["raw_submitted_answers"][name] = (
+                f"{random.randint(4, 100):d} * {a_tru}"
+            )
+            bigo_type = pl.get_enum_attrib(
+                element, "type", BigOType, BIG_O_TYPE_DEFAULT
+            )
+
+            if bigo_type is BigOType.THETA:
+                data["partial_scores"][name] = {
+                    "score": 0.25,
+                    "weight": weight,
+                    "feedback": bou.THETA_CONSTANT_FACTORS_FEEDBACK,
+                }
+            else:
+                data["partial_scores"][name] = {
+                    "score": 0.5,
+                    "weight": weight,
+                    "feedback": bou.CONSTANT_FACTORS_FEEDBACK,
+                }
 
     elif result == "invalid":
         invalid_answer = random.choice([
