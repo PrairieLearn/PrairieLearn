99231408ee553a8119b8c8eb7fe49ed7217f9756
Peter Stenger <peter@prairielearn.com>
2025-07-20 23:20:32 -0500

    fixup column

--- apps/prairielearn/src/pages/administratorNetworks/administratorNetworks.ts
+++ apps/prairielearn/src/pages/administratorNetworks/administratorNetworks.ts
@@ -17,17 +17,15 @@ router.get(
     const result = await sqldb.queryRow(
       sql.select,
       [],
-      z.object({
-        networks: z.array(
-          z.object({
-            network: ExamModeNetworkSchema.shape.network,
-            start_date: z.string(),
-            end_date: z.string(),
-            location: ExamModeNetworkSchema.shape.location,
-            purpose: ExamModeNetworkSchema.shape.purpose,
-          }),
-        ),
-      }),
+      z.array(
+        z.object({
+          network: ExamModeNetworkSchema.shape.network,
+          start_date: z.string(),
+          end_date: z.string(),
+          location: ExamModeNetworkSchema.shape.location,
+          purpose: ExamModeNetworkSchema.shape.purpose,
+        }),
+      ),
     );
     Object.assign(res.locals, result);
     res.send(AdministratorNetworks({ resLocals: res.locals }));
