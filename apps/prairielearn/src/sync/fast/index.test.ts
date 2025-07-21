da57e13caefa4f8fb9d007f4bfcfedfd86674029
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-07-21 10:35:01 -0700

    Refactor

--- apps/prairielearn/src/sync/fast/index.test.ts
+++ apps/prairielearn/src/sync/fast/index.test.ts
@@ -1,27 +1,36 @@
 import { assert, describe, test } from 'vitest';
 
-import { isFastSyncPossible } from './index.js';
+import { getFastSyncStrategy } from './index.js';
 
 describe('isFastSyncPossible', () => {
   describe('question changes', () => {
     test('handles question JSON change', () => {
-      assert.isTrue(isFastSyncPossible(['questions/foo/info.json']));
+      const strategy = getFastSyncStrategy(['questions/foo/info.json']);
+      assert.equal(strategy?.type, 'QuestionJson');
+      assert.equal(strategy?.pathPrefix, 'questions/foo/info.json');
     });
 
     test('handles change to single file from a question', () => {
-      assert.isTrue(isFastSyncPossible(['questions/foo/server.py']));
+      const strategy = getFastSyncStrategy(['questions/foo/server.py']);
+      assert.equal(strategy?.type, 'QuestionFiles');
+      assert.equal(strategy?.pathPrefix, 'questions/foo/server.py');
     });
 
     test('handles changes to multiple files from the same question', () => {
-      assert.isTrue(isFastSyncPossible(['questions/foo/server.py', 'questions/foo/question.html']));
+      const strategy = getFastSyncStrategy([
+        'questions/foo/server.py',
+        'questions/foo/question.html',
+      ]);
+      assert.equal(strategy?.type, 'QuestionFiles');
+      assert.equal(strategy?.pathPrefix, 'questions/foo');
     });
 
     test('handles changes to files from multiple questions', () => {
-      assert.isFalse(isFastSyncPossible(['questions/foo/server.py', 'questions/bar/server.py']));
+      assert.isNull(getFastSyncStrategy(['questions/foo/server.py', 'questions/bar/server.py']));
     });
 
     test('handles changes to both question and non-question files', () => {
-      assert.isFalse(isFastSyncPossible(['questions/foo/server.py', 'infoCourse.json']));
+      assert.isNull(getFastSyncStrategy(['questions/foo/server.py', 'infoCourse.json']));
     });
   });
 });
--- apps/prairielearn/src/sync/fast/index.ts
+++ apps/prairielearn/src/sync/fast/index.ts
@@ -1,3 +1,5 @@
+import type { QuestionJson } from '../../schemas/infoQuestion.js';
+
 function longestCommonPathPrefix(paths: string[]) {
   if (paths.length === 0) return '';
   if (paths.length === 1) return paths[0];
@@ -19,12 +21,29 @@ function longestCommonPathPrefix(paths: string[]) {
   return commonSegments.join('/');
 }
 
-function isQuestionFastSyncPossible(changedFiles: string[]) {
-  if (!changedFiles.every((f) => f.startsWith('questions/'))) return false;
+function getQuestionFastSyncStrategy(
+  changedFiles: string[],
+): QuestionJsonFastSync | QuestionFilesFastSync | null {
+  if (!changedFiles.every((f) => f.startsWith('questions/'))) return null;
+
+  // If any JSON file was changed, exactly one JSON file must be changed,
+  // and only one file may be changed.
+  const jsonFileChanged = changedFiles.some((f) => f.endsWith('/info.json'));
+
+  if (jsonFileChanged) {
+    if (changedFiles.length === 1) {
+      return {
+        type: 'QuestionJson',
+        pathPrefix: changedFiles[0],
+      };
+    }
+
+    return null;
+  }
 
   const commonPrefix = longestCommonPathPrefix(changedFiles);
 
-  if (commonPrefix === 'questions') return false;
+  if (commonPrefix === 'questions') return null;
 
   // We can't be 100% sure yet - the common prefix might be a directory that
   // contains multiple questions. We won't know for sure until we actually
@@ -33,15 +52,27 @@ function isQuestionFastSyncPossible(changedFiles: string[]) {
   // There's another weird case where the prefix might be a subdirectory within
   // a question - for instance, if it's `questions/foo/tests`, where `foo` is
   // the question. We'll also have to handle this later.
-  return true;
+  return {
+    type: 'QuestionFiles',
+    pathPrefix: commonPrefix,
+  };
 }
 
-export function isFastSyncPossible(changedFiles: string[]) {
+interface QuestionJsonFastSync {
+  type: 'QuestionJson';
+  pathPrefix: string;
+}
+
+interface QuestionFilesFastSync {
+  type: 'QuestionFiles';
+  pathPrefix: string;
+}
+
+type FastSyncStrategy = QuestionJsonFastSync | QuestionFilesFastSync;
+
+export function getFastSyncStrategy(changedFiles: string[]): FastSyncStrategy | null {
   // We'll aim to handle two possible fast syncing cases for now:
   // - Just a question's `info.json` file has changed.
   // - Any non-JSON question files have changed.
-
-  if (isQuestionFastSyncPossible(changedFiles)) return true;
-
-  return false;
+  return getQuestionFastSyncStrategy(changedFiles) ?? null;
 }
