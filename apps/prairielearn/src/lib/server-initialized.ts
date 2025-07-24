6c2b23b6503188d1cc74867c190c2f7ff70afae4
Peter Stenger <peter@prairielearn.com>
2025-07-24 12:16:26 -0500

    cleanup

--- apps/prairielearn/src/lib/server-initialized.ts
+++ apps/prairielearn/src/lib/server-initialized.ts
@@ -1,22 +1,19 @@
 /**
  * This module is used to track whether the server has been initialized.
  *
- * Since vite won't reload this file in HMR mode, this will work.
+ * This file serves as external state for 'server.ts', and the server may perform unexpectedly
+ * if this file is modified while the server is running.
  */
-console.log('re-importing server-initialized');
 let serverState: 'started' | 'pending' | 'stopped' = 'stopped';
 
 export function isServerInitialized() {
-  console.log('isServerInitialized', serverState);
   return serverState === 'started';
 }
 
 export function isServerPending() {
-  console.log('isServerPending', serverState);
   return serverState === 'pending';
 }
 
 export function setServerState(state: 'started' | 'pending' | 'stopped') {
-  console.log('setServerState', state);
   serverState = state;
 }
