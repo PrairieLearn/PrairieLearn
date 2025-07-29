7fe24826f345f17d6e56d4a841436c8abe844f0b
miguelaenlle <bot.developer3@gmail.com>
2025-07-29 17:17:50 -0500

    Added functionality to bulk-grade all assessment questions

--- apps/prairielearn/src/pages/instructorAssessmentManualGrading/assessment/assessment.html.tsx
+++ apps/prairielearn/src/pages/instructorAssessmentManualGrading/assessment/assessment.html.tsx
@@ -76,11 +76,24 @@ export function ManualGradingAssessment({
         assessmentId: resLocals.assessment.id,
         urlPrefix: resLocals.urlPrefix,
       })}
+      <form method="POST" id="ai-grading-all">
+        <input type="hidden" name="__action" value="ai_grade_assessment_all" />
+        <input type="hidden" name="__csrf_token" value="${resLocals.__csrf_token}" />
+      </form>
       <div class="card mb-4">
-        <div class="card-header bg-primary text-white">
+        <div class="card-header bg-primary text-white align-items-center d-flex w-100 gap-2">
           <h1>
             ${resLocals.assessment_set.name} ${resLocals.assessment.number}: Manual Grading Queue
           </h1>
+          <div class="flex-1"></div>
+          <button
+            type="button"
+            class="btn btn-sm btn-light grading-tag-button"
+            name="ai-grade-all-questions"
+            onclick="$('#ai-grading-all').submit();"
+          >
+            AI grade all questions
+          </button>
         </div>
 
         <div class="table-responsive">
