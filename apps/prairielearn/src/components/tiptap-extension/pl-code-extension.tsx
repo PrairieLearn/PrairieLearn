82e68491352d4c1be169cc04b5fa9e1b20a32cc3
Peter Stenger <peter@prairielearn.com>
2025-07-30 13:52:53 -0500

    wip

---
+++ apps/prairielearn/src/components/tiptap-extension/pl-code-extension.tsx
@@ -0,0 +1,81 @@
+import { type Node, mergeAttributes } from '@tiptap/core';
+import { CodeBlock, type CodeBlockOptions } from '@tiptap/extension-code-block';
+import {
+  NodeViewContent,
+  NodeViewWrapper,
+  type ReactNodeViewProps,
+  ReactNodeViewRenderer,
+} from '@tiptap/react';
+
+/*
+<pl-code language="python">
+def square(x):
+    return x * x
+</pl-code>
+*/
+const PLCodeBlockComponent = (props: ReactNodeViewProps<HTMLDivElement>) => {
+  const _attrs = props.node.attrs as typeof plCodeAttrs;
+  const updateAttributes = props.updateAttributes as (attrs: PlCodeAttrs) => void;
+  return (
+    <NodeViewWrapper class="react-component">
+      <button type="button" onClick={() => updateAttributes({ language: 'python' })}>
+        Python
+      </button>
+      <div contenteditable="false">This is a code block</div>
+      <NodeViewContent className="content" />
+    </NodeViewWrapper>
+  );
+};
+
+interface PlCodeAttrs {
+  language?: string;
+  style?: string;
+  noHighlight?: boolean;
+  preventSelect?: boolean;
+  highlightLines?: number[];
+  highlightLinesColor?: string;
+  copyCodeButton?: boolean;
+  showLineNumbers?: boolean;
+  normalizeWhitespace?: boolean;
+}
+
+// The UI needs to be aware of the defaults on these attributes so that it can
+// render the correct UI.
+const plCodeAttrs: PlCodeAttrs = {
+  language: undefined,
+  style: 'friendly',
+  noHighlight: false,
+  preventSelect: false,
+  highlightLines: undefined,
+  highlightLinesColor: undefined,
+  copyCodeButton: false,
+  showLineNumbers: false,
+  normalizeWhitespace: false,
+} as const;
+
+export const PLCodeBlock = CodeBlock.extend({
+  name: 'plCodeBlock',
+  whitespace: 'pre',
+  code: true,
+
+  parseHTML() {
+    return [
+      {
+        tag: 'pl-code',
+        preserveWhitespace: 'full',
+      },
+    ];
+  },
+
+  renderHTML({ HTMLAttributes }: CodeBlockOptions) {
+    return ['pl-code', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0];
+  },
+  addNodeView() {
+    return ReactNodeViewRenderer(PLCodeBlockComponent);
+  },
+  addAttributes() {
+    return Object.fromEntries(
+      Object.entries(plCodeAttrs).map(([key, value]) => [key, { default: value }]),
+    );
+  },
+});
