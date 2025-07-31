5de3cc9ec4d4f53d90b6a78101a2b00923fc5669
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-07-31 10:59:09 -0700

    Downgrade r-base to fix incompatibility with r-irkernel

--- workspaces/jupyterlab-r/install.sh
+++ workspaces/jupyterlab-r/install.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # Install all R dependencies.
-xargs mamba install --yes r-base==4.5.1 r-irkernel==1.3.2
+xargs mamba install --yes r-base==4.4.3 r-irkernel==1.3.2
 
 # Clear various caches to minimize the final image size.
 mamba clean --all -f -y
