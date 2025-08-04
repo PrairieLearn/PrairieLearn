448d3c587e378d0f146742779fe74a90ba319aae
miguelaenlle <bot.developer3@gmail.com>
2025-08-04 13:57:19 -0500

    commenting and minor fixes

--- apps/prairielearn/elements/pl-image-capture/pl-image-capture.js
+++ apps/prairielearn/elements/pl-image-capture/pl-image-capture.js
@@ -500,7 +500,7 @@ const MAX_ZOOM_SCALE = 5;
       });
 
       const capturePreview = document.createElement('img');
-      capturePreview.className = 'capture-preview img-fluid bg-body-secondary w-100 transition-all';
+      capturePreview.className = 'capture-preview img-fluid bg-body-secondary w-100';
 
       capturePreview.src = dataUrl;
       capturePreview.alt = 'Captured image preview';
@@ -544,8 +544,8 @@ const MAX_ZOOM_SCALE = 5;
         zoomButtonsContainer.classList.remove('d-none');
 
         if (!this.imageCapturePreviewPanzoom) {
-          let rotation = 0;
-
+          // Initialize Panzoom on the parent of the image element, since
+          // the image itself may be rotated.
           this.imageCapturePreviewPanzoom = Panzoom(capturePreviewParent, {
             contain: 'outside',
             minScale: MIN_ZOOM_SCALE,
@@ -558,6 +558,8 @@ const MAX_ZOOM_SCALE = 5;
           zoomOutButton.addEventListener('click', () => {
             this.imageCapturePreviewPanzoom.zoomOut();
           });
+
+          let rotation = 0;
           viewerRotateClockwiseButton.addEventListener('click', () => {
             const capturePreviewImg = this.imageCaptureDiv.querySelector(
               '.js-uploaded-image-container .capture-preview',
@@ -567,9 +569,9 @@ const MAX_ZOOM_SCALE = 5;
               capturePreviewImg,
             });
 
+            // Don't use modulo to ensure that the animated rotation direction is always clockwise.
             rotation += 90;
             capturePreviewImg.style.transform = `rotate(${rotation}deg)`;
-            // this.imageCapturePreviewPanzoom.reset({ animate: false });
           });
 
           let panEnabled = false;
