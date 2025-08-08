a74b60349f8b328ff791a429290b081ef591a649
Peter Stenger <peter@prairielearn.com>
2025-08-07 17:40:30 -1000

    Fixup accessibility

--- exampleCourse/questions/element/details/server.py
+++ exampleCourse/questions/element/details/server.py
@@ -4,7 +4,7 @@ import prairielearn as pl
 
 def generate(data: pl.QuestionData) -> None:
     # Generate a DataFrame with some sample data
-    df = pd.read_csv("breast-cancer-train.csv")
+    df = pd.read_csv("breast-cancer-train-trim.csv")
 
     # Add the DataFrame to the question data
     data["params"]["df"] = pl.to_json(df)
