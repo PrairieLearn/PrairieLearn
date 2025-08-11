509a33855d10fc4c2ea3d9c4205fae8b923f885c
Dave Mussulman <mussulma@gmail.com>
2025-08-11 16:24:42 -0500

    remove unnecessary migration

--- apps/prairielearn/src/migrations/20250806214358_lti13_instances__access_token__delete.sql
+++
@@ -1,6 +0,0 @@
--- With #12583 upgrading to a newer openid-client library, the format of stored
--- access tokens changed. This migration clears any cached copies in the old format.
-UPDATE lti13_instances
-SET
-  access_token_expires_at = NULL,
-  access_tokenset = NULL;
