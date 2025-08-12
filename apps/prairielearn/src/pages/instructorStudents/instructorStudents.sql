9164d82fe15da92ce145adf7246e5e29e61ed666
Peter Stenger <peter@prairielearn.com>
2025-08-12 16:01:02 -0400

    fmt

--- apps/prairielearn/src/pages/instructorStudents/instructorStudents.sql
+++ apps/prairielearn/src/pages/instructorStudents/instructorStudents.sql
@@ -17,13 +17,17 @@ FROM
   enrollments AS e
   LEFT JOIN users AS u ON (u.user_id = e.user_id)
 WHERE
-  e.course_instance_id = $course_instance_id AND (
-    e.pending_uid = $uid OR u.uid = $uid
+  e.course_instance_id = $course_instance_id
+  AND (
+    e.pending_uid = $uid
+    OR u.uid = $uid
   );
 
 -- BLOCK upsert_enrollment_by_uid
-INSERT INTO enrollments (course_instance_id, status, pending_uid)
-VALUES ($course_instance_id, 'invited', $uid)
+INSERT INTO
+  enrollments (course_instance_id, status, pending_uid)
+VALUES
+  ($course_instance_id, 'invited', $uid)
 ON CONFLICT (course_instance_id, pending_uid) DO UPDATE
 SET
   lti_synced = FALSE,
