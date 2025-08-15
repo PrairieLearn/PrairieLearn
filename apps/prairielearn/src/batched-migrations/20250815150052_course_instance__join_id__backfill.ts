6772ab98582e6f32ff5585f051aaf00f3f8b7f06
Peter Stenger <peter@prairielearn.com>
2025-08-15 11:21:00 -0400

    Invitation join code

---
+++ apps/prairielearn/src/batched-migrations/20250815150052_course_instance__join_id__backfill.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+import { makeBatchedMigration } from '@prairielearn/migrations';
+import { loadSqlEquiv, queryAsync, queryRow } from '@prairielearn/postgres';
+
+const sql = loadSqlEquiv(import.meta.url);
+
+export default makeBatchedMigration({
+  async getParameters() {
+    const max = await queryRow(sql.select_bounds, z.bigint());
+    return {
+      max,
+      batchSize: 1000,
+    };
+  },
+
+  async execute(min: bigint, max: bigint) {
+    await queryAsync(sql.update_course_instances_join_id, {
+      min,
+      max,
+    });
+  },
+});
---
+++ apps/prairielearn/src/migrations/20250815151635_course_instance__join_id__nonnull.sql
@@ -0,0 +1,2 @@
+ALTER TABLE course_instances
+ALTER COLUMN join_id SET NOT NULL;
