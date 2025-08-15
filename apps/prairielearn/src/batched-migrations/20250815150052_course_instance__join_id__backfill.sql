c84b4b63f369d53cda78295ab3c0bc49909bfa83
Peter Stenger <peter@prairielearn.com>
2025-08-15 12:59:25 -0400

    format

--- apps/prairielearn/src/batched-migrations/20250815150052_course_instance__join_id__backfill.sql
+++ apps/prairielearn/src/batched-migrations/20250815150052_course_instance__join_id__backfill.sql
@@ -6,7 +6,13 @@ FROM
 
 -- BLOCK update_course_instances_join_id
 UPDATE course_instances
-SET join_id = SUBSTRING(MD5(RANDOM()::TEXT) FROM 1 FOR 12)
-WHERE join_id IS NULL
+SET
+  join_id = SUBSTRING(
+    MD5(RANDOM()::TEXT)
+    FROM
+      1 FOR 12
+  )
+WHERE
+  join_id IS NULL
   AND id >= $start
   AND id <= $end;
--- apps/prairielearn/src/migrations/20250815151635_course_instance__join_id__nonnull.sql
+++ apps/prairielearn/src/migrations/20250815151635_course_instance__join_id__nonnull.sql
@@ -1,2 +1,3 @@
 ALTER TABLE course_instances
-ALTER COLUMN join_id SET NOT NULL;
+ALTER COLUMN join_id
+SET NOT NULL;
