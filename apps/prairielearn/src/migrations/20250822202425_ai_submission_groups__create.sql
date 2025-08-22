7b5bb0e1a933e6873579387b225a310c974eac2e
miguelaenlle <bot.developer3@gmail.com>
2025-08-22 16:50:34 -0500

    Cleaned up database pg file

--- apps/prairielearn/src/migrations/20250822202425_ai_submission_groups__create.sql
+++ apps/prairielearn/src/migrations/20250822202425_ai_submission_groups__create.sql
@@ -1,8 +1,8 @@
 CREATE TABLE ai_submission_groups (
   assessment_question_id BIGINT NOT NULL REFERENCES assessment_questions (id) ON DELETE CASCADE ON UPDATE CASCADE,
   id BIGSERIAL PRIMARY KEY,
-  submission_group_name TEXT NOT NULL,
-  submission_group_description TEXT NOT NULL
+  submission_group_description TEXT NOT NULL,
+  submission_group_name TEXT NOT NULL
 );
 
 ALTER TABLE ai_submission_groups
