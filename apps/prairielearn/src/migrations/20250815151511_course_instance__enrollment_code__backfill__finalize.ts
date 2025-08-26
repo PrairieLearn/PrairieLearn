1974d9bc6de2c0ca682d5854b4dbcf99c173bc12
Peter Stenger <peter@prairielearn.com>
2025-08-26 10:38:42 -0500

    missing change

--- apps/prairielearn/src/migrations/20250815151511_course_instance__enrollment_code__backfill__finalize.ts
+++ apps/prairielearn/src/migrations/20250815151511_course_instance__enrollment_code__backfill__finalize.ts
@@ -1,5 +1,5 @@
 import { finalizeBatchedMigration } from '@prairielearn/migrations';
 
 export default async function () {
-  await finalizeBatchedMigration('20250815150052_course_instance__join_id__backfill');
+  await finalizeBatchedMigration('20250815150052_course_instance__enrollment_code__backfill');
 }
