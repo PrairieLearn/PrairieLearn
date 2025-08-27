57a3c3681deff10f4ea85d497b1ccb69ead40828
Peter Stenger <peter@prairielearn.com>
2025-08-27 12:02:16 -0500

    Cleanup helper

--- apps/prairielearn/src/models/audit-event.sql
+++ apps/prairielearn/src/models/audit-event.sql
@@ -1,22 +1,14 @@
--- BLOCK select_audit_events_by_subject_user_id_table_name_course_instance_id
+-- BLOCK select_audit_events_by_subject_user_id_table_names_course_instance_id
 SELECT
   *
 FROM
   audit_events
 WHERE
   subject_user_id = $subject_user_id
-  AND table_name = $table_name
-  AND course_instance_id = $course_instance_id
-ORDER BY
-  date DESC;
-
--- BLOCK select_audit_events_by_subject_user_id_course_instance_id
-SELECT
-  *
-FROM
-  audit_events
-WHERE
-  subject_user_id = $subject_user_id
+  AND table_name IN (
+    SELECT
+      unnest($table_names::text[])
+  )
   AND course_instance_id = $course_instance_id
 ORDER BY
   date DESC;
--- apps/prairielearn/src/models/audit-event.ts
+++ apps/prairielearn/src/models/audit-event.ts
@@ -4,26 +4,37 @@ import { type StaffAuditEvent, StaffAuditEventSchema } from '../lib/client/safe-
 
 const sql = loadSqlEquiv(import.meta.url);
 
+/**
+ * Selects audit events by subject user ID, table names, and course instance ID.
+ * Exactly one of `table_name` or `table_names` must be provided.
+ * @param params
+ * @param params.subject_user_id - The ID of the subject user.
+ * @param params.table_name - The name of the table to select audit events from.
+ * @param params.table_names - The names of the tables to select audit events from.
+ * @param params.course_instance_id - The ID of the course instance.
+ */
 export async function selectAuditEvents({
   subject_user_id,
   table_name,
+  table_names,
   course_instance_id,
 }: {
   subject_user_id: string;
   table_name?: string;
+  table_names?: string[];
   course_instance_id: string;
 }): Promise<StaffAuditEvent[]> {
-  if (!table_name) {
-    return await queryRows(
-      sql.select_audit_events_by_subject_user_id_course_instance_id,
-      { subject_user_id, course_instance_id },
-      StaffAuditEventSchema,
-    );
+  if (table_name && table_names) {
+    throw new Error('table_name and table_names cannot both be provided');
+  }
+  if (!table_name && !table_names) {
+    throw new Error('table_name or table_names must be provided');
   }
 
+  const table_names_for_query = table_name ? [table_name] : table_names;
   return await queryRows(
-    sql.select_audit_events_by_subject_user_id_table_name_course_instance_id,
-    { subject_user_id, table_name, course_instance_id },
+    sql.select_audit_events_by_subject_user_id_table_names_course_instance_id,
+    { subject_user_id, table_names: table_names_for_query, course_instance_id },
     StaffAuditEventSchema,
   );
 }
