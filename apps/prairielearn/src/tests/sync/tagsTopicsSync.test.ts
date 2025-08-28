a00a6534887308989318d7a4cabafd897af44108
Peter Stenger <peter@prairielearn.com>
2025-08-28 11:43:14 -0500

    Cleanup test

--- apps/prairielearn/src/tests/sync/tagsTopicsSync.test.ts
+++ apps/prairielearn/src/tests/sync/tagsTopicsSync.test.ts
@@ -224,12 +224,11 @@ describe('Tag/topic syncing', () => {
   it('adds corresponding default tags if used by questions but not specified in courseData', async () => {
     const courseData = util.getCourseData();
 
-    if (util.QUESTION_ID in courseData.questions) {
-      // The symbolic tag is in DEFAULT_TAGS but not in courseData
-      courseData.questions[util.QUESTION_ID].tags?.push('symbolic');
-      // Similarly, the drawing tag is in DEFAULT_TAGS but not in courseData
-      courseData.questions[util.QUESTION_ID].tags?.push('drawing');
-    }
+    // The symbolic tag is in DEFAULT_TAGS but not in courseData
+    courseData.questions[util.QUESTION_ID].tags!.push('symbolic');
+
+    // Similarly, the drawing tag is in DEFAULT_TAGS but not in courseData
+    courseData.questions[util.QUESTION_ID].tags!.push('drawing');
 
     await util.writeAndSyncCourseData(courseData);
 
