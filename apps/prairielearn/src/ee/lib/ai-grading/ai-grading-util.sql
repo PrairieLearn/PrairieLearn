7afe8bec73e64bb5eeb2a576fba235a911489d50
miguelaenlle <bot.developer3@gmail.com>
2025-09-01 20:27:37 -0500

    Fix sql query

--- apps/prairielearn/src/ee/lib/ai-grading/ai-grading-util.sql
+++ apps/prairielearn/src/ee/lib/ai-grading/ai-grading-util.sql
@@ -37,12 +37,12 @@ WHERE
   AND (
     NOT $closed_instance_questions_only
     OR ai.open = FALSE
-  );
+  )
+  AND (
+    NOT $ungrouped_instance_questions_only
+    OR iq.ai_submission_group_id IS NULL
+  )
 
-AND (
-  NOT $ungrouped_instance_questions_only
-  OR iq.ai_submission_group_id IS NOT NULL
-)
 -- BLOCK insert_ai_grading_job
 INSERT INTO
   ai_grading_jobs (
