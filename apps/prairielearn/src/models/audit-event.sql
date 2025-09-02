37f9acf3681c2f5f529f5b507e74c0963e33fa5d
Peter Stenger <peter@prairielearn.com>
2025-09-02 11:49:45 -0400

    Walkup audit events

--- apps/prairielearn/src/models/audit-event.sql
+++ apps/prairielearn/src/models/audit-event.sql
@@ -23,6 +23,97 @@ ORDER BY
   date DESC;
 
 -- BLOCK insert_audit_event
+WITH
+  agent AS (
+    SELECT
+      user_id
+    FROM
+      users
+    WHERE
+      user_id = $agent_user_id
+  ),
+  subject AS (
+    SELECT
+      user_id
+    FROM
+      users
+    WHERE
+      user_id = $subject_user_id
+  ),
+  groups AS (
+    SELECT
+      id,
+      course_instance_id
+    FROM
+      groups
+    WHERE
+      id = $group_id
+  ),
+  assessment_instance AS (
+    SELECT
+      id,
+      assessment_id
+    FROM
+      assessment_instances
+    WHERE
+      id = $assessment_instance_id
+  ),
+  assessment_question AS (
+    SELECT
+      id,
+      assessment_id
+    FROM
+      assessment_questions
+    WHERE
+      id = $assessment_question_id
+  ),
+  assessment AS (
+    SELECT
+      id,
+      course_instance_id
+    FROM
+      assessments
+    WHERE
+      id = coalesce(
+        $assessment_id,
+        assessment_instance.assessment_id,
+        assessment_question.assessment_id
+      )
+  ),
+  course_instance AS (
+    SELECT
+      id,
+      course_id
+    FROM
+      course_instances
+    WHERE
+      id = coalesce(
+        $course_instance_id,
+        groups.course_instance_id,
+        assessment.course_instance_id
+      )
+  ),
+  course AS (
+    SELECT
+      id,
+      institution_id
+    FROM
+      pl_courses
+    WHERE
+      id = coalesce($course_id, course_instance.course_id)
+  ),
+  institution AS (
+    SELECT
+      id
+    FROM
+      institutions
+    WHERE
+      id = coalesce(
+        $institution_id,
+        subject.institution_id,
+        course.institution_id
+      )
+  )
 INSERT INTO
   audit_events (
     action,
@@ -43,25 +134,37 @@ INSERT INTO
     assessment_question_id,
     group_id
   )
-VALUES
+SELECT
+  $action,
+  $action_detail,
+  $table_name,
+  subject.user_id AS subject_user_id,
+  course_instance.id AS course_instance_id,
+  $row_id,
+  $context,
+  $old_row,
+  $new_row,
+  $agent_authn_user_id,
+  agent.user_id AS agent_user_id,
+  institution.id AS institution_id,
+  course.id AS course_id,
+  assessment.id AS assessment_id,
+  assessment_instance.id AS assessment_instance_id,
+  assessment_question.id AS assessment_question_id,
+  groups.id AS group_id
+FROM
   (
-    $action,
-    $action_detail,
-    $table_name,
-    $subject_user_id,
-    $course_instance_id,
-    $row_id,
-    $context,
-    $old_row,
-    $new_row,
-    $agent_authn_user_id,
-    $agent_user_id,
-    $institution_id,
-    $course_id,
-    $assessment_id,
-    $assessment_instance_id,
-    $assessment_question_id,
-    $group_id
-  )
+    SELECT
+      1
+  ) AS tmp -- dummy row to make the LEFT JOINs work
+  LEFT JOIN subject ON (TRUE)
+  LEFT JOIN course_instance ON (TRUE)
+  LEFT JOIN agent ON (TRUE)
+  LEFT JOIN institution ON (TRUE)
+  LEFT JOIN course ON (TRUE)
+  LEFT JOIN assessment ON (TRUE)
+  LEFT JOIN assessment_instance ON (TRUE)
+  LEFT JOIN assessment_question ON (TRUE)
+  LEFT JOIN groups ON (TRUE)
 RETURNING
   *;
--- apps/prairielearn/src/models/audit-event.ts
+++ apps/prairielearn/src/models/audit-event.ts
@@ -1,4 +1,7 @@
-import { loadSqlEquiv, queryRow, queryRows } from '@prairielearn/postgres';
+import { z } from 'zod';
+
+import { loadSqlEquiv, queryOptionalRow, queryRow, queryRows } from '@prairielearn/postgres';
+import { run } from '@prairielearn/run';
 
 import { type StaffAuditEvent, StaffAuditEventSchema } from '../lib/client/safe-db-types.js';
 import { type EnumAuditEventAction } from '../lib/db-types.js';
@@ -48,87 +51,148 @@ export async function selectAuditEvents({
   );
 }
 
+const CurrentRowDataSchema = z.record(z.string(), z.any());
+type CurrentRowData = z.infer<typeof CurrentRowDataSchema>;
 /**
- * Inserts a new audit event. The required parameters are:
- * - action
- * - table_name
- * - row_id
- * - agent_authn_user_id (nullable)
- * - subject_user_id (nullable)
- *
- * @param params
- * @param params.action
- * @param params.action_detail - Detail about the action
- * @param params.agent_authn_user_id - ID of the authenticated user performing the action
- * @param params.agent_user_id - ID of the user performing the action
- * @param params.context - Additional context about the action
- * @param params.course_id
- * @param params.course_instance_id
- * @param params.group_id
- * @param params.institution_id
- * @param params.new_row
- * @param params.old_row
- * @param params.row_id - ID of the affected row
- * @param params.subject_user_id - ID of the affected user (often the user who performed the action)
- * @param params.table_name
- * @param params.assessment_id
- * @param params.assessment_instance_id
- * @param params.assessment_question_id
+ * Fetches the current row for a given table and row ID
  */
-export async function insertAuditEvent({
-  action,
-  action_detail,
-  agent_authn_user_id,
-  agent_user_id,
-  assessment_id,
-  assessment_instance_id,
-  assessment_question_id,
-  context = {},
-  course_id,
-  course_instance_id,
-  group_id,
-  institution_id,
-  new_row,
-  old_row,
-  row_id,
-  subject_user_id,
-  table_name,
-}: {
+async function getCurrentRowData(
+  table_name: string,
+  row_id: string,
+): Promise<CurrentRowData | null> {
+  const query = run(() => {
+    if (table_name === 'users') {
+      return 'SELECT * FROM users WHERE user_id = $row_id';
+    }
+    return `SELECT * FROM ${table_name} WHERE id = $row_id`;
+  });
+  return await queryOptionalRow(query, { row_id }, CurrentRowDataSchema);
+}
+
+interface InsertAuditEventParams {
   action: EnumAuditEventAction;
-  action_detail?: string;
   table_name: string;
   row_id: string;
-  course_instance_id?: string;
-  subject_user_id: string | null;
-  context?: Record<string, any>;
-  old_row?: Record<string, any>;
-  new_row?: Record<string, any>;
+  /** Most events should have an associated authorized user */
   agent_authn_user_id: string | null;
-  agent_user_id?: string;
-  institution_id?: string;
-  course_id?: string;
+  /** Most events should have an associated authenticated user */
+  agent_user_id: string | null;
+  /** only 'update' actions require an action_detail */
+  action_detail?: string;
+  /** Most events have no context */
+  context?: Record<string, any>;
+  /** Creation events have no old row */
+  old_row?: Record<string, any>;
+  /** Deletion events have no new row */
+  new_row?: Record<string, any>;
+  // The remaining fields depend on the action and table
+
+  subject_user_id?: string;
   assessment_id?: string;
   assessment_instance_id?: string;
   assessment_question_id?: string;
+  course_id?: string;
+  course_instance_id?: string;
   group_id?: string;
-}): Promise<StaffAuditEvent> {
+  institution_id?: string;
+}
+
+// To prevent mistakes where fields are omitted, we track which fields are required for each table.
+const requiredTableFields: Record<string, string[]> = {
+  course_instances: ['course_instance_id'],
+  pl_courses: ['course_id'],
+  users: ['subject_user_id'],
+  groups: ['group_id'],
+  assessment_instances: ['assessment_instance_id'],
+  assessment_questions: ['assessment_question_id'],
+  assessments: ['assessment_id'],
+  institutions: ['institution_id'],
+};
+/**
+ * Inserts a new audit event. This should be done after the action has been performed.
+ *
+ * @param params Parameters for the audit event
+ * @param params.action - The action that was performed
+ * @param params.action_detail - e.g. the column name that was updated
+ * @param params.agent_authn_user_id - ID of the authorized user performing the action
+ * @param params.agent_user_id - ID of the authorized user performing the action
+ * @param params.context - Additional context, typically empty
+ * @param params.course_id - Inferred from `course_instance_id`, `group_id`, `assessment_id`, `assessment_instance_id`, `assessment_question_id`
+ * @param params.course_instance_id - Inferred from `group_id`, `assessment_id`, `assessment_instance_id`, `assessment_question_id`
+ * @param params.group_id - ID of the affected group
+ * @param params.institution_id - Inferred from `subject_user_id`, `course_id`, `course_instance_id`, `group_id`, `assessment_id`, `assessment_instance_id`, `assessment_question_id`
+ * @param params.new_row - The new row data
+ * @param params.old_row - The old row data
+ * @param params.row_id - primary key ID of the affected row
+ * @param params.subject_user_id - ID of the affected user
+ * @param params.table_name - The name of the table that was affected
+ * @param params.assessment_id - Inferred from `assessment_instance_id`, `assessment_question_id`
+ * @param params.assessment_instance_id - ID of the affected assessment instance
+ * @param params.assessment_question_id - ID of the affected assessment question
+ */
+export async function insertAuditEvent(params: InsertAuditEventParams): Promise<StaffAuditEvent> {
+  const {
+    action,
+    action_detail = null,
+    agent_authn_user_id,
+    agent_user_id,
+    assessment_id = null,
+    assessment_instance_id = null,
+    assessment_question_id = null,
+    context = {},
+    course_id = null,
+    course_instance_id = null,
+    group_id = null,
+    institution_id = null,
+    new_row: given_new_row = null,
+    old_row = null,
+    row_id,
+    subject_user_id = null,
+    table_name,
+  } = params;
+  const new_row = given_new_row ?? (await getCurrentRowData(table_name, row_id));
+
+  // Depending on the action, certain fields are required.
+  if ((action === 'update' || action === 'delete') && !old_row) {
+    throw new Error('old_row is required for update and delete actions');
+  } else if (action === 'insert' && old_row) {
+    throw new Error('old_row is not allowed for insert actions');
+  }
+
+  if (action === 'insert' && !new_row) {
+    throw new Error('new_row is required for insert actions');
+  } else if (action === 'delete' && new_row) {
+    throw new Error('new_row is not allowed for delete actions');
+  }
+
+  if (action === 'update' && !action_detail) {
+    throw new Error('action_detail is required for update actions');
+  }
+
+  // Depending on the table, certain fields are required.
+  if (!requiredTableFields[table_name].every((field) => Boolean(params[field]))) {
+    throw new Error(
+      `${table_name} requires the following fields: ${requiredTableFields[table_name].join(', ')}`,
+    );
+  }
+
   return await queryRow(
     sql.insert_audit_event,
     {
       action,
-      action_detail: action_detail ?? null,
+      action_detail,
       agent_authn_user_id,
-      agent_user_id: agent_user_id ?? null,
-      assessment_id: assessment_id ?? null,
-      assessment_instance_id: assessment_instance_id ?? null,
-      assessment_question_id: assessment_question_id ?? null,
+      agent_user_id,
+      assessment_id,
+      assessment_instance_id,
+      assessment_question_id,
       context,
-      course_id: course_id ?? null,
-      course_instance_id: course_instance_id ?? null,
-      group_id: group_id ?? null,
-      institution_id: institution_id ?? null,
-      new_row: new_row ?? null,
-      old_row: old_row ?? null,
+      course_id,
+      course_instance_id,
+      group_id,
+      institution_id,
+      new_row,
+      old_row,
       row_id,
       subject_user_id,
       table_name,
