c3651e26a63be27cf1f8c9a6ffee99ef07574d45
Peter Stenger <peter@prairielearn.com>
2025-09-03 15:57:05 -0400

    split out

--- apps/prairielearn/src/migrations/20250822211537_enrollments__joined_at__backfill__finalize.ts
+++
@@ -1,5 +0,0 @@
-import { finalizeBatchedMigration } from '@prairielearn/migrations';
-
-export default async function () {
-  await finalizeBatchedMigration('20250822211536_enrollments__joined_at__backfill');
-}
