140c7f1463b23263803cf93df6ac4033f25fbf58
Dipkiran Pradhan <dipkiran26@gmail.com>
2025-09-04 09:45:38 -0600

    Create Public and Student link component (#12669)
    * Public and Student link component
    
    * Public and Student link component
    
    * New changes to comments
    
    * Fixing errors
    
    * Fixing errors
    
    * Error fix
    
    ---------
    
    Co-authored-by: Seth Poulsen <poulsenseth@yahoo.com>

--- apps/prairielearn/src/pages/instructorInstanceAdminSettings/instructorInstanceAdminSettings.html.tsx
+++ apps/prairielearn/src/pages/instructorInstanceAdminSettings/instructorInstanceAdminSettings.html.tsx
@@ -2,12 +2,12 @@ import { type HtmlSafeString, html } from '@prairielearn/html';
 
 import { DeleteCourseInstanceModal } from '../../components/DeleteCourseInstanceModal.js';
 import { GitHubButton } from '../../components/GitHubButton.js';
+import { PublicLinkSharing, StudentLinkSharing } from '../../components/LinkSharing.js';
 import { Modal } from '../../components/Modal.js';
 import { PageLayout } from '../../components/PageLayout.js';
 import { QRCodeModal } from '../../components/QRCodeModal.js';
 import { CourseInstanceSyncErrorsAndWarnings } from '../../components/SyncErrorsAndWarnings.js';
 import { compiledScriptTag } from '../../lib/assets.js';
-import { type CourseInstance } from '../../lib/db-types.js';
 import { renderHtml } from '../../lib/preact-html.js';
 import { hydrateHtml } from '../../lib/preact.js';
 import { type Timezone, formatTimezone } from '../../lib/timezones.js';
@@ -195,46 +195,23 @@ export function InstructorInstanceAdminSettings({
                 links to the course can be used for enrollment.
               </div>
             </div>
-            <div class="mb-3">
-              <label class="form-label" for="student_link">Student Link</label>
-              <span class="input-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="student_link"
-                  name="student_link"
-                  value="${studentLink}"
-                  disabled
-                />
-                <button
-                  type="button"
-                  class="btn btn-sm btn-outline-secondary btn-copy"
-                  data-clipboard-text="${studentLink}"
-                  aria-label="Copy student link"
-                >
-                  <i class="far fa-clipboard"></i>
-                </button>
-                <button
-                  type="button"
-                  class="btn btn-sm btn-outline-secondary"
-                  aria-label="Student Link QR Code"
-                  data-bs-toggle="modal"
-                  data-bs-target="#studentLinkModal"
-                >
-                  <i class="fas fa-qrcode"></i>
-                </button>
-              </span>
-              <small class="form-text text-muted">
-                This is the link that students will use to access the course. You can copy this link
-                to share with students.
-              </small>
-            </div>
+            ${StudentLinkSharing({
+              studentLink,
+              studentLinkMessage:
+                'This is the link that students will use to access the course. You can copy this link to share with students.',
+            })}
             ${enrollmentManagementEnabled
               ? renderHtml(<SelfEnrollmentSettings selfEnrollLink={selfEnrollLink} />)
               : ''}
-
             <h2 class="h4">Sharing</h2>
-            ${CourseInstanceSharing({ courseInstance: resLocals.course_instance, publicLink })}
+            ${resLocals.course_instance.share_source_publicly
+              ? PublicLinkSharing({
+                  publicLink,
+                  sharingMessage: "This course instance's source is publicly shared.",
+                  publicLinkMessage:
+                    'The link that other instructors can use to view this course instance.',
+                })
+              : html`<p>This course instance is not being shared.</p>`}
             ${EditConfiguration({
               hasCoursePermissionView: resLocals.authz_data.has_course_permission_view,
               hasCoursePermissionEdit: resLocals.authz_data.has_course_permission_edit,
@@ -442,58 +419,6 @@ function CourseInstanceActions({
           csrfToken={csrfToken}
         />,
       )}
-    </div>
-  `;
-}
-
-function CourseInstanceSharing({
-  courseInstance,
-  publicLink,
-}: {
-  courseInstance: CourseInstance;
-  publicLink: string;
-}) {
-  if (!courseInstance.share_source_publicly) {
-    return html`<p>This course instance is not being shared.</p>`;
-  }
-
-  return html`
-    <p>
-      <span class="badge color-green3 me-1">Public source</span>
-      This course instance's source is publicly shared.
-    </p>
-    <div class="mb-3">
-      <label for="publicLink">Public link</label>
-      <span class="input-group">
-        <input
-          type="text"
-          class="form-control"
-          id="publicLink"
-          name="publicLink"
-          value="${publicLink}"
-          disabled
-        />
-        <button
-          type="button"
-          class="btn btn-sm btn-outline-secondary btn-copy"
-          data-clipboard-text="${publicLink}"
-          aria-label="Copy public link"
-        >
-          <i class="far fa-clipboard"></i>
-        </button>
-        <button
-          type="button"
-          class="btn btn-sm btn-outline-secondary"
-          aria-label="Public Link QR Code"
-          data-bs-toggle="modal"
-          data-bs-target="#publicLinkModal"
-        >
-          <i class="fas fa-qrcode"></i>
-        </button>
-      </span>
-      <small class="form-text text-muted">
-        The link that other instructors can use to view this course instance.
-      </small>
     </div>
   `;
 }
