a4feaaa038d93b4e1688297e7b55c0b3862253b9
Nico Ritschel <nico@elsl.de>
2025-09-05 10:04:09 -0500

    New author select SQL that's less horrible (but not fully tested yet)

--- apps/prairielearn/src/sync/fromDisk/authors.sql
+++ apps/prairielearn/src/sync/fromDisk/authors.sql
@@ -22,7 +22,15 @@ FROM jsonb_to_recordset(to_jsonb(:authors::json)) AS author(
 ON CONFLICT (author_name, email, orcid, origin_course) DO NOTHING;
 
 -- BLOCK select_authors
-SELECT
-  *
-FROM
-  authors;
+SELECT a.*
+FROM jsonb_to_recordset(to_jsonb(:authors::json)) AS author(
+  name text,
+  email text,
+  orcid text,
+  originCourse bigint
+)
+JOIN authors a
+  ON a.author_name IS NOT DISTINCT FROM author.name
+ AND a.email IS NOT DISTINCT FROM author.email
+ AND a.orcid IS NOT DISTINCT FROM author.orcid
+ AND a.origin_course IS NOT DISTINCT FROM author.originCourse;
