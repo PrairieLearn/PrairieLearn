71d5c0bc10963727ea59fe94d0a8c8fd4edc71ef
Nico Ritschel <nico@elsl.de>
2025-09-05 14:45:28 -0500

    fix the most annoying SQL bug

--- apps/prairielearn/src/sync/fromDisk/authors.sql
+++ apps/prairielearn/src/sync/fromDisk/authors.sql
@@ -12,12 +12,12 @@ INSERT INTO authors (author_name, email, orcid, origin_course)
 SELECT name,
        email,
        orcid,
-       originCourse::bigint
+       "originCourse"::bigint
 FROM jsonb_to_recordset($authors::jsonb) AS author(
   name text,
   email text,
   orcid text,
-  originCourse text
+  "originCourse" text
 )
 ON CONFLICT (author_name, email, orcid, origin_course) DO NOTHING;
 
@@ -27,10 +27,10 @@ FROM jsonb_to_recordset($authors::jsonb) AS author(
   name text,
   email text,
   orcid text,
-  originCourse bigint
+  "originCourse" bigint
 )
 JOIN authors a
   ON a.author_name IS NOT DISTINCT FROM author.name
  AND a.email IS NOT DISTINCT FROM author.email
  AND a.orcid IS NOT DISTINCT FROM author.orcid
- AND a.origin_course IS NOT DISTINCT FROM author.originCourse;
+ AND a.origin_course IS NOT DISTINCT FROM author."originCourse";
