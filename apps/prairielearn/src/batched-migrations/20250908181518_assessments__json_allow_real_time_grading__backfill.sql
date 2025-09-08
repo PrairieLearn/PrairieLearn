8806119bb84d87acb0411cf63cc41d203015666c
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-09-08 11:38:04 -0700

    Run backfills

--- apps/prairielearn/src/batched-migrations/20250908181518_assessments__json_allow_real_time_grading__backfill.sql
+++ apps/prairielearn/src/batched-migrations/20250908181518_assessments__json_allow_real_time_grading__backfill.sql
@@ -9,9 +9,11 @@ UPDATE assessments
 SET
   json_allow_real_time_grading = FALSE
 WHERE
+  id >= $start
+  AND id <= $end
   -- The `json_` columns are meant to represent the exact state of whatever is
   -- in the JSON files. In this case, we can make some assumptions to avoid having
   -- to actually re-sync every course. Since real-time grading is enabled by
   -- default for all assessments, we'll assume that it's only worth preserving
   -- a value in the `json_` column if it's disallowed (false).
-  allow_real_time_grading = FALSE;
+  AND allow_real_time_grading = FALSE;
