c19ec1ed990d07bdc9deef6cf00bc76fb745e7f3
Peter Stenger <peter@prairielearn.com>
2025-09-09 14:58:39 -0400

    remove old files

--- apps/prairielearn/src/migrations/20250815150052_course_instance__enrollment_code__backfill.ts
+++
@@ -1,5 +0,0 @@
-import { enqueueBatchedMigration } from '@prairielearn/migrations';
-
-export default async function () {
-  await enqueueBatchedMigration('20250815150052_course_instance__enrollment_code__backfill');
-}
