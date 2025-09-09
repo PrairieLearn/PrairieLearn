f86c6fe7c70a0198b195ddd7b0687a9c510b1025
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-09-09 16:13:50 -0700

    Fix weird migration issue

--- apps/prairielearn/src/migrations/20250908205457_per_question_allow_real_time_grading.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE assessments
-ADD COLUMN IF NOT EXISTS json_allow_real_time_grading BOOLEAN;
-
-ALTER TABLE assessment_questions
-ADD COLUMN IF NOT EXISTS allow_real_time_grading BOOLEAN;
