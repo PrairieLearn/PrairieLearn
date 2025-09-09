b776d948111fc23bc6088c52d463fd8ef77b2fd3
Peter Stenger <peter@prairielearn.com>
2025-09-09 12:23:32 -0400

    rename files

--- apps/prairielearn/src/migrations/20250815150052_course_instances__enrollment_code__backfill.ts
+++ apps/prairielearn/src/migrations/20250815150052_course_instances__enrollment_code__backfill.ts
@@ -1,5 +1,5 @@
 import { enqueueBatchedMigration } from '@prairielearn/migrations';
 
 export default async function () {
-  await enqueueBatchedMigration('20250815150052_course_instance__enrollment_code__backfill');
+  await enqueueBatchedMigration('20250815150052_course_instances__enrollment_code__backfill');
 }
