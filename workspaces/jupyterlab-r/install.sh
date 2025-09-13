be8f5ce2b2b8ebc1195560471b78d599b12557a4
Zac Warham <48937711+ZacWarham@users.noreply.github.com>
2025-09-13 13:39:17 -0700

    Bump and cleanup install.sh

--- workspaces/jupyterlab-r/install.sh
+++ workspaces/jupyterlab-r/install.sh
@@ -3,9 +3,9 @@
 set -ex
 
 # Install all R dependencies.
-xargs mamba install --yes r-base==4.4.3 r-irkernel==1.3.2
+xargs mamba install --yes r-base==4.5.1 r-irkernel==1.3.2
 
 # Clear various caches to minimize the final image size.
 mamba clean --all -f -y
 
-rm /requirements.txt /install.sh
+rm /install.sh
