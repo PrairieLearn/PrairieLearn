c4098eb696e6c84cba0de518f34b51880334df61
Peter Stenger <peter@prairielearn.com>
2025-09-15 13:54:11 -0400

    Add comments

--- apps/prairielearn/src/migrations/20250618214947_grading_jobs__deleted_by__add.sql
+++ apps/prairielearn/src/migrations/20250618214947_grading_jobs__deleted_by__add.sql
@@ -1,3 +1,4 @@
 ALTER TABLE grading_jobs
+-- This is a false positive (https://github.com/sbdchd/squawk/issues/647)
 -- squawk-ignore adding-foreign-key-constraint
 ADD COLUMN deleted_by BIGINT REFERENCES users (user_id) ON UPDATE CASCADE ON DELETE CASCADE;
