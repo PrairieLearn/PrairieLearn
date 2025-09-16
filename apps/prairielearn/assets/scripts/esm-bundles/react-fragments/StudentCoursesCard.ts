2a6afa0f5789e41af09d4c74bd471434dd9d3922
Peter Stenger <peter@prairielearn.com>
2025-09-16 11:08:17 -1000

    Add student invitations to home page (#12854)
    * Invitation accept modal
    
    * fmt
    
    * fixup build
    
    * wip
    
    * wip
    
    * wip
    
    * fixup
    
    * oops
    
    * Only hydrate StudentCoursesCard
    
    * Update StudentCoursesCard.tsx
    
    Co-authored-by: Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
    
    * Update StudentCoursesCard.tsx
    
    Co-authored-by: Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
    
    * Update StudentCoursesCard.tsx
    
    Co-authored-by: Myles Wiegel <114321556+mylesw27@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Myles Wiegel <114321556+mylesw27@users.noreply.github.com>

---
+++ apps/prairielearn/assets/scripts/esm-bundles/react-fragments/StudentCoursesCard.ts
@@ -0,0 +1,4 @@
+import { StudentCoursesCard } from '../../../../src/pages/home/StudentCoursesCard.js';
+import { registerReactFragment } from '../../behaviors/react-fragments/index.js';
+
+registerReactFragment(StudentCoursesCard);
