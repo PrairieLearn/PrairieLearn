f2f102320f331622a7907684fa13922a6852ac77
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-09-16 19:26:40 -0700

    Fix debug file

--- packages/preact/src/debug.ts
+++ packages/preact/src/debug.ts
@@ -1,11 +1,7 @@
-// When imported, this file will set up Preact debugging and devtools.
-
-// if (process.env.NODE_ENV !== 'production') {
-//   // eslint-disable-next-line @typescript-eslint/no-require-imports
-//   require('preact/debug');
-// } else {
-//   // eslint-disable-next-line @typescript-eslint/no-require-imports
-//   require('preact/devtools');
-// }
-
-console.trace('TESTING');
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require('preact/debug');
+} else {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require('preact/devtools');
+}
