75ead2b7b2f12c216a2855ebd2b27d2ad066cf03
Peter Stenger <peter@prairielearn.com>
2025-09-18 14:24:17 -0400

    Show feedback sometimes

--- apps/prairielearn/elements/pl-order-blocks/pl-order-blocks.py
+++ apps/prairielearn/elements/pl-order-blocks/pl-order-blocks.py
@@ -282,6 +282,13 @@ def render(element_html: str, data: pl.QuestionData) -> str:
         if grading_method is GradingMethodType.EXTERNAL:
             return ""  # external grader is responsible for displaying results screen
 
+        score = None
+        feedback = None
+        if answer_name in data["partial_scores"]:
+            score = data["partial_scores"][answer_name]["score"]
+            feedback = data["partial_scores"][answer_name].get("feedback", "")
+        submission_was_graded = score is not None
+
         student_submission = [
             {
                 "inner_html": attempt["inner_html"],
@@ -289,15 +296,15 @@ def render(element_html: str, data: pl.QuestionData) -> str:
                 "badge_type": attempt.get("badge_type", ""),
                 "icon": attempt.get("icon", ""),
                 # We intentionally don't include distractor_feedback and ordering_feedback here.
+                "distractor_feedback": attempt.get("distractor_feedback", "")
+                if submission_was_graded
+                else "",
+                "ordering_feedback": attempt.get("ordering_feedback", "")
+                if submission_was_graded
+                else "",
             }
             for attempt in data["submitted_answers"].get(answer_name, [])
         ]
-
-        score = None
-        feedback = None
-        if answer_name in data["partial_scores"]:
-            score = data["partial_scores"][answer_name]["score"]
-            feedback = data["partial_scores"][answer_name].get("feedback", "")
 
         html_params = {
             "submission": True,
