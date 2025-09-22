654a43cee506464fdce823ac5eb5eab4f5025395
Peter Stenger <peter@prairielearn.com>
2025-09-22 12:48:04 -0700

    Update apps/prairielearn/src/migrations/20250922183053_enrollments__first_joined_at__check_constraint_add.sql
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>

--- apps/prairielearn/src/migrations/20250922183053_enrollments__first_joined_at__check_constraint_add.sql
+++ apps/prairielearn/src/migrations/20250922183053_enrollments__first_joined_at__check_constraint_add.sql
@@ -1,6 +1,6 @@
 ALTER TABLE enrollments
 ADD CONSTRAINT first_joined_at_not_null_if_joined_and_created_at_not_null CHECK (
-  -- old enrollments have a null created_at. Don't enforce the constraint for those.
+  -- Old enrollments have a null created_at. Don't enforce the constraint for those.
   created_at IS NULL
   -- If the status is 'invited', 'lti13_pending', or 'rejected', then the first_joined_at may or may not be null.
   -- Don't enforce the constraint for those.
