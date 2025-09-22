a6fb891c510406d386b25c9dd0becfbf7da88103
Peter Stenger <peter@prairielearn.com>
2025-09-22 14:41:01 -0400

    wip

--- apps/prairielearn/src/migrations/20250922183053_enrollments__first_joined_at__check_constraint_add.sql
+++ apps/prairielearn/src/migrations/20250922183053_enrollments__first_joined_at__check_constraint_add.sql
@@ -2,11 +2,10 @@ ALTER TABLE enrollments
 ADD CONSTRAINT first_joined_at_not_null_if_joined_and_created_at_not_null CHECK (
   -- old enrollments have a null created_at. Don't enforce the constraint for those.
   created_at IS NULL
-  -- Enforce that if the status is 'joined', 'removed', or 'rejected', then the first_joined_at is not null.
-  -- You can only be removed or rejected if you were joined at some point.
-
-  -- If the status is 'invited', 'lti13_pending', or 'rejected', then the first_joined_at may be null.
+  -- If the status is 'invited', 'lti13_pending', or 'rejected', then the first_joined_at may or may not be null.
   -- Don't enforce the constraint for those.
   OR status IN ('invited', 'lti13_pending', 'rejected')
+  -- Enforce that if the status is 'joined', 'removed', or 'rejected', then the first_joined_at is not null.
+  -- You can only be removed or rejected if you were joined at some point.
   OR first_joined_at IS NOT NULL
 ) NOT VALID;
