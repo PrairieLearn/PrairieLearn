094966eead8dd0a1f42b55a958d892b70731908d
Jonatan Schroeder <jonatan@yorku.ca>
2025-09-22 09:10:11 -0400

    Use existing assets test

--- apps/prairielearn/src/middlewares/staticNodeModules.ts
+++ apps/prairielearn/src/middlewares/staticNodeModules.ts
@@ -4,8 +4,7 @@ import express, { Router } from 'express';
 import { type ServeStaticOptions } from 'serve-static';
 
 import { APP_ROOT_PATH, REPOSITORY_ROOT_PATH } from '../lib/paths.js';
-
-export const NODE_MODULES_PATHS = [
+const NODE_MODULES_PATHS = [
   path.resolve(APP_ROOT_PATH, 'node_modules'),
   path.resolve(REPOSITORY_ROOT_PATH, 'node_modules'),
 ];
