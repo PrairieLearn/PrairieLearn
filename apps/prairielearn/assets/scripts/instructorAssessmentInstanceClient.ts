9033c89d69175f56cdb8d00ad436bd2496d60b88
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-09-23 17:05:30 -0700

    Remove unneeded changes

--- apps/prairielearn/assets/scripts/instructorAssessmentInstanceClient.ts
+++ apps/prairielearn/assets/scripts/instructorAssessmentInstanceClient.ts
@@ -5,12 +5,6 @@ onDocumentReady(() => {
     const button = (e as any).relatedTarget as HTMLElement;
     const modal = e.target as HTMLElement;
 
-    if (modal.querySelector('.js-instance-question-id') === null) {
-      // The version of this modal shown for Exam assessments doesn't have this element.
-      // Skip templating to avoid an error being logged.
-      return;
-    }
-
     templateFromAttributes(button, modal, {
       'data-instance-question-id': '.js-instance-question-id',
     });
