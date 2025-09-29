65135f6c1fc18406e0f78c318f79f37e703d288a
miguelaenlle <bot.developer3@gmail.com>
2025-09-29 09:58:14 -0500

    Finalized comment update

--- apps/prairielearn/src/migrations/20250927021914_instance_question_groups__create.sql
+++ apps/prairielearn/src/migrations/20250927021914_instance_question_groups__create.sql
@@ -7,9 +7,9 @@ CREATE TABLE instance_question_groups (
     assessment_question_id,
     instance_question_group_name
   ),
-  -- This lets the instance questions table create composite foreign key constraints on the
+  -- This allows instance_questions to have composite foreign key constraints on the
   -- assessment question ID and instance question group ID of its instance question group. 
-  -- These are used to ensure that the assessment question ID matches between the referencing 
-  -- instance question and referenced instance question group.
+  -- These composite foreign keys are used to ensure that instance questions have the same 
+  -- assessment question IDs as their corresponding instance question groups.
   UNIQUE (assessment_question_id, id)
 );
