9a87391268f1ed131dcd45f5e977044b062d1f5a
Jonatan Schroeder <jonatan@yorku.ca>
2025-10-04 13:16:41 -0400

    Export new functions

--- packages/browser-utils/src/index.ts
+++ packages/browser-utils/src/index.ts
@@ -2,4 +2,9 @@ export { onDocumentReady } from './on-document-ready.js';
 export { parseHTML, parseHTMLElement } from './parse-html.js';
 export { EncodedData, decodeData } from './encode-data.js';
 export { templateFromAttributes } from './template-from-attributes.js';
-export { trapFocus, focusFirstFocusableChild } from './focus.js';
+export {
+  trapFocus,
+  focusFirstFocusableChild,
+  focusElementOrCheckedOption,
+  isSameFocusContext,
+} from './focus.js';
