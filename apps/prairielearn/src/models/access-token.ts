0601d737ae449ba61ff6472bdc5c7593803b3388
ZacWarham <zacwarham@gmail.com>
2025-10-06 15:02:19 -0700

    Update to crypto

--- apps/prairielearn/src/models/access-token.ts
+++ apps/prairielearn/src/models/access-token.ts
@@ -1,14 +1,12 @@
 import * as crypto from 'crypto';
 
-import { v4 as uuidv4 } from 'uuid';
-
 import * as sqldb from '@prairielearn/postgres';
 
 export async function insertAccessToken(user_id: string, token_name: string) {
   const sql = sqldb.loadSqlEquiv(import.meta.url);
 
   const name = token_name;
-  const token = uuidv4();
+  const token = crypto.randomUUID();
   const token_hash = crypto.createHash('sha256').update(token, 'utf8').digest('hex');
 
   await sqldb.execute(sql.insert_access_token, {
