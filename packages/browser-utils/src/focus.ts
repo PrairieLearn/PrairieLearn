58168b8c11524fb56a6048f2523503b0730565ee
Jonatan Schroeder <jonatanschroeder@gmail.com>
2025-10-07 11:02:26 -0400

    Radios without a name are not in same group

--- packages/browser-utils/src/focus.ts
+++ packages/browser-utils/src/focus.ts
@@ -164,10 +164,14 @@ export function isSameFocusContext(element1: Element | null, element2: Element |
   if (element1.tagName === 'INPUT' && element2.tagName === 'INPUT') {
     const input1 = element1 as HTMLInputElement;
     const input2 = element2 as HTMLInputElement;
+    // https://html.spec.whatwg.org/multipage/input.html#radio-button-state-(type=radio)
     if (
       input1.type === 'radio' &&
       input2.type === 'radio' &&
+      // radios without a name are not in the same group, even if they share the same form
+      input1.name &&
       input1.name === input2.name &&
+      // either both have no form, or both have the same form
       input1.form === input2.form
     ) {
       return true;
