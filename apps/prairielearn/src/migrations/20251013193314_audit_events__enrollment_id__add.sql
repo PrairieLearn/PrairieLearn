49e5e5138ff5e0853004234b47c95740f16284b3
Peter Stenger <peter@prairielearn.com>
2025-10-13 21:12:24 -0400

    fixup

--- apps/prairielearn/src/migrations/20251013193314_audit_events__enrollment_id__add.sql
+++ apps/prairielearn/src/migrations/20251013193314_audit_events__enrollment_id__add.sql
@@ -2,7 +2,7 @@ ALTER TABLE audit_events
 ADD COLUMN enrollment_id BIGINT;
 
 ALTER TABLE audit_events
-ADD CONSTRAINT audit_events_enrollment_id_fkey FOREIGN KEY (enrollment_id) REFERENCES enrollments (id) ON DELETE CASCADE ON UPDATE CASCADE NOT VALID;
+ADD CONSTRAINT audit_events_enrollment_id_fkey FOREIGN KEY (enrollment_id) REFERENCES enrollments (id) ON DELETE SET NULL ON UPDATE CASCADE NOT VALID;
 
 -- Events that affect an enrollment in a course instance
 CREATE INDEX audit_events_table_name_enrollment_id_course_instance_id_idx ON audit_events (enrollment_id, table_name, course_instance_id)
