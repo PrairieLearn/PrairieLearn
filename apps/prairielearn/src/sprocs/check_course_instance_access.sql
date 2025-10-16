ec259456bf7bc46f3b8ced97aef43d8e0b264261
Peter Stenger <peter@prairielearn.com>
2025-10-16 12:02:48 -0400

    improve check

--- apps/prairielearn/src/sprocs/check_course_instance_access.sql
+++ apps/prairielearn/src/sprocs/check_course_instance_access.sql
@@ -13,11 +13,22 @@ WITH selected_course AS (
     WHERE ci.id = check_course_instance_access.course_instance_id
 )
 SELECT
-    COALESCE(bool_or(
-        check_course_instance_access_rule(ciar,
-            check_course_instance_access.uid, check_course_instance_access.user_institution_id,
-            selected_course.institution_id, check_course_instance_access.date)
-    ), FALSE)
+      -- Course instance access rules are deprecated in favor of publishing dates,
+      -- so we can short-circuit if the publishing dates are set.
+      -- Additionally, institution checks are moved to the enrollment system, so this shouldn't check that.
+      
+      (ci.publishing_publish_date IS NOT NULL
+      AND ci.publishing_publish_date <= check_course_instance_access.date
+      AND (
+        ci.publishing_archive_date IS NULL
+        OR ci.publishing_archive_date > check_course_instance_access.date
+      ) OR
+    COALESCE(
+    bool_or(
+      check_course_instance_access_rule(ciar,
+          check_course_instance_access.uid, check_course_instance_access.user_institution_id,
+          selected_course.institution_id, check_course_instance_access.date)
+    ), FALSE))
 FROM
     course_instance_access_rules AS ciar,
     selected_course
