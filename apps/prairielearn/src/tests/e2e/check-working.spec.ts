3f9c9b517564b1c9ab8fce8f2918db9f82cf0b1e
Peter Stenger <peter@prairielearn.com>
2025-10-21 15:04:06 -0400

    wip

--- apps/prairielearn/src/tests/e2e/check-working.spec.ts
+++ apps/prairielearn/src/tests/e2e/check-working.spec.ts
@@ -1,10 +1,7 @@
-import * as sqldb from '@prairielearn/postgres';
-
 // Importing from fixtures picks up our worker-scoped fixtures.
 import { expect, test } from './fixtures.js';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle('[DEV] Home | PrairieLearn');
-  console.log(sqldb.getConfig());
 });
