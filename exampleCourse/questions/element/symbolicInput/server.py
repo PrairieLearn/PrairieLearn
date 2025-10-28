3d9ffcb15491b005f3f521b8d1515cc462dd7a82
Nico Ritschel <nritschel@users.noreply.github.com>
2025-10-27 20:30:22 -0500

    Add support for absolute value bar (`|x|`) notation in `pl-symbolic-input` (#13113)
    * add abs post-processing from formula editor PR
    
    * fix tests
    
    * fixup
    
    * escape
    
    * fix
    
    * fix
    
    * improvements
    
    * revert
    
    ---------
    
    Co-authored-by: Peter Stenger <peter@prairielearn.com>

--- exampleCourse/questions/element/symbolicInput/server.py
+++ exampleCourse/questions/element/symbolicInput/server.py
@@ -36,3 +36,10 @@ def generate(data):
     test = sympy.Function("test")
     ans2 = test(sympy.sqrt(x**2 / sympy.E**x))
     data["correct_answers"]["formula_editor"] = pl.to_json(ans2)
+
+    # Nested absolute value expression: |x+|-x+1+2+3+4||
+    x = sympy.symbols("x")
+    inner_expr = -x + 1 + 2 + 3 + 4
+    outer_expr = x + sympy.Abs(inner_expr)
+    nested_abs_expr = sympy.Abs(outer_expr)
+    data["correct_answers"]["nested_abs"] = pl.to_json(nested_abs_expr)
