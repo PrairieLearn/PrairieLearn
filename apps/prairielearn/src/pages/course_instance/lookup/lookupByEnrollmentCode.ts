12a8fd40ab54150aae08cf3db9bb2f42044364ae
Peter Stenger <peter@prairielearn.com>
2025-10-29 13:00:51 -0400

    improve lookup

--- apps/prairielearn/src/pages/course_instance/lookup/lookupByEnrollmentCode.ts
+++ apps/prairielearn/src/pages/course_instance/lookup/lookupByEnrollmentCode.ts
@@ -2,7 +2,11 @@ import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
 import z from 'zod';
 
+import { dangerousFullAuthzForTesting } from '../../../lib/authzData.js';
+import type { User } from '../../../lib/db-types.js';
 import { selectOptionalCourseInstanceByEnrollmentCode } from '../../../models/course-instances.js';
+import { selectCourseById } from '../../../models/course.js';
+import { selectOptionalEnrollmentByUserId } from '../../../models/enrollment.js';
 
 const router = Router();
 
@@ -31,7 +35,26 @@ router.get(
 
     if (courseInstanceId && courseInstance.id !== courseInstanceId) {
       res.status(404).json({
-        error: 'No course found with this enrollment code',
+        error: 'This enrollment code is for a different course',
+      });
+      return;
+    }
+
+    const authnUser: User = res.locals.authn_user;
+
+    const existingEnrollment = await selectOptionalEnrollmentByUserId({
+      userId: res.locals.authn_user.user_id,
+      courseInstance,
+      requestedRole: 'Student', // TODO: Should be 'System'
+      authzData: dangerousFullAuthzForTesting(),
+    });
+
+    if (
+      existingEnrollment &&
+      !['invited', 'rejected', 'joined', 'removed'].includes(existingEnrollment.status)
+    ) {
+      res.status(403).json({
+        error: 'You are blocked from accessing this course',
       });
       return;
     }
@@ -39,7 +62,28 @@ router.get(
     // Check if self-enrollment is enabled for this course instance
     if (!courseInstance.self_enrollment_enabled) {
       res.status(403).json({
-        error: 'Self-enrollment is not enabled for this course',
+        error: 'Self-enrollment is disabled for this course',
+      });
+      return;
+    }
+
+    if (courseInstance.self_enrollment_restrict_to_institution) {
+      // Lookup the course
+      const course = await selectCourseById(courseInstance.course_id);
+      if (course.institution_id !== authnUser.institution_id) {
+        res.status(403).json({
+          error: 'Self-enrollment is restricted to users from the same institution',
+        });
+        return;
+      }
+    }
+
+    if (
+      courseInstance.self_enrollment_enabled_before_date &&
+      new Date() >= courseInstance.self_enrollment_enabled_before_date
+    ) {
+      res.status(403).json({
+        error: 'Self-enrollment is no longer allowed for this course',
       });
       return;
     }
