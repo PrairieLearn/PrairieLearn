85622c1313fa30625a26338b49b11d1caa0bfad3
Peter Stenger <peter@prairielearn.com>
2025-10-31 14:53:46 -0500

    split

--- apps/prairielearn/src/migrations/20250924190750_course_instances__modern_publishing__backfill.ts
+++
@@ -1,5 +0,0 @@
-import { enqueueBatchedMigration } from '@prairielearn/migrations';
-
-export default async function () {
-  await enqueueBatchedMigration('20250924190750_course_instances__modern_publishing__backfill');
-}
