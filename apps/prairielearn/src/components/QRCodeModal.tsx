c66eba0408e05639f1edc49ab47ccc3698154c12
miguelaenlle <bot.developer3@gmail.com>
2025-11-01 23:25:59 -0500

    Removed 200px qr-code-box

--- apps/prairielearn/src/components/QRCodeModal.tsx
+++ apps/prairielearn/src/components/QRCodeModal.tsx
@@ -40,10 +40,12 @@ export function QRCodeModal({
   show: boolean;
   onHide: () => void;
 }) {
-  const svg = useMemo(() => new QR({ content, container: 'svg-viewbox' }).svg(), [content]).replace(
-    '<svg ',
-    '<svg style="width:100%;height:100%;" ',
-  );
+  const svg = useMemo(() => new QR(
+    { content, container: 'svg-viewbox' }).svg().replace(
+      '<svg ',
+      '<svg style="width:100%;height:100%;" ',
+    ),
+  [content]);
   return (
     <Modal show={show} size="lg" aria-labelledby={`${id}-title`} backdrop="static" onHide={onHide}>
       <Modal.Header closeButton>
