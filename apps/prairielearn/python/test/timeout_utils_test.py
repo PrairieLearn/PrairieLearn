4719b576d070bfdbfe723b58ee34473509ed4bb3
Surg-Dev <sruggerio415@gmail.com>
2025-11-05 21:18:34 -0600

    add some test cases

---
+++ apps/prairielearn/python/test/timeout_utils_test.py
@@ -0,0 +1,40 @@
+import time
+
+import pytest
+from prairielearn.timeout_utils import (
+    ThreadingTimeout,
+    TimeoutExceptionError,
+    TimeoutState,
+)
+from sympy import Eq, exp, simplify, sin, symbols
+
+
+def busy_loop(timeout: float, busy_duration: float, *, swallow_exc: bool = True):
+    x = 0
+    with ThreadingTimeout(timeout, swallow_exc=swallow_exc) as ctx:
+        end = time.perf_counter() + busy_duration
+        while time.perf_counter() < end:
+            x += 0.1
+    return (ctx, x)
+
+
+def test_short_execution():
+    ctx, _ = busy_loop(0.4, 0.1)
+    assert ctx.state == TimeoutState.EXECUTED
+
+
+def test_timeout_exc():
+    with pytest.raises(TimeoutExceptionError):
+        busy_loop(0.1, 0.4, swallow_exc=False)
+
+
+def test_sympy_timeout():
+    x = symbols("x")
+    expr1 = exp(sin(x)) * (1 + sin(x) ** 10) ** 5  # type: ignore
+    expr2 = exp(sin(x)) * (1 + sin(x) ** 2) ** 125  # type: ignore
+
+    with ThreadingTimeout(0.5) as ctx:
+        eq = Eq(expr1, expr2)
+        result = simplify(eq.lhs - eq.rhs)  # type: ignore
+        assert result is not None
+    assert ctx.state == TimeoutState.TIMED_OUT
