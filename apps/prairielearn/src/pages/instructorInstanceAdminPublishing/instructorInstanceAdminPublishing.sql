a9002f7ac27f41cf7c0c51331f34e6b11175e77e
Peter Stenger <peter@prairielearn.com>
2025-11-06 13:45:08 -0600

    add back

--- apps/prairielearn/src/pages/instructorInstanceAdminPublishing/instructorInstanceAdminPublishing.sql
+++ apps/prairielearn/src/pages/instructorInstanceAdminPublishing/instructorInstanceAdminPublishing.sql
@@ -7,3 +7,34 @@ WHERE
   ciar.course_instance_id = $course_instance_id
 ORDER BY
   ciar.number;
+
+-- BLOCK remove_student_from_extension
+DELETE FROM course_instance_publishing_enrollment_extensions
+WHERE
+  course_instance_publishing_extension_id = $extension_id
+  AND enrollment_id = $enrollment_id;
+
+-- BLOCK update_extension
+UPDATE course_instance_publishing_extensions
+SET
+  name = $name,
+  end_date = CASE
+    WHEN $end_date = '' THEN NULL
+    ELSE $end_date::timestamp
+  END
+WHERE
+  id = $extension_id
+  AND course_instance_id = $course_instance_id;
+
+-- BLOCK add_user_to_extension
+INSERT INTO
+  course_instance_publishing_enrollment_extensions (
+    course_instance_publishing_extension_id,
+    enrollment_id
+  )
+VALUES
+  ($extension_id, $enrollment_id)
+ON CONFLICT (
+  course_instance_publishing_extension_id,
+  enrollment_id
+) DO NOTHING;
