2b672d4b8cdd94ae57f30450be34134ffb5f4d11
Peter Stenger <peter@prairielearn.com>
2025-11-10 15:55:38 -0500

    delete unneeded hydration

--- apps/prairielearn/assets/scripts/esm-bundles/hydrated-components/GroupInfoModal.ts
+++
@@ -1,5 +0,0 @@
-import { registerHydratedComponent } from '@prairielearn/preact/hydrated-component';
-
-import { GroupInfoModal } from '../../../../src/pages/instructorAssessmentManualGrading/assessmentQuestion/GroupInfoModal.js';
-
-registerHydratedComponent(GroupInfoModal);
