696e7bdfb0fb8385231c4b3f7b67569be5d3cb7d
Nico Ritschel <nico@elsl.de>
2025-11-17 12:10:28 -0600

    implementation

--- apps/prairielearn/elements/pl-symbolic-input/pl-symbolic-input.py
+++ apps/prairielearn/elements/pl-symbolic-input/pl-symbolic-input.py
@@ -29,6 +29,7 @@ DISPLAY_LOG_AS_LN_DEFAULT = False
 DISPLAY_SIMPLIFIED_EXPRESSION_DEFAULT = True
 IMAGINARY_UNIT_FOR_DISPLAY_DEFAULT = "i"
 ALLOW_TRIG_FUNCTIONS_DEFAULT = True
+ADDITIONAL_SIMPLIFICATIONS_DEFAULT = None
 SIZE_DEFAULT = 35
 SHOW_FORMULA_EDITOR_DEFAULT = False
 SHOW_HELP_TEXT_DEFAULT = True
@@ -40,6 +41,13 @@ SYMBOLIC_INPUT_MUSTACHE_TEMPLATE_NAME = "pl-symbolic-input.mustache"
 # This timeout is chosen to allow multiple sympy-based elements to grade on one page,
 # while not exceeding the global timeout enforced for Python execution.
 SYMPY_TIMEOUT = 3
+# Additional simplifications supported by SymPy
+SYMPY_ADDITIONAL_SIMPLIFICATIONS = {
+    "expand": sympy.expand,
+    "power": sympy.powsimp,
+    "trig": sympy.trigsimp,
+    "log": sympy.expand_log,
+}
 
 
 def prepare(element_html: str, data: pl.QuestionData) -> None:
@@ -55,6 +63,7 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
         "allow-complex",
         "imaginary-unit-for-display",
         "allow-trig-functions",
+        "additional-simplifications",
         "size",
         "formula-editor",
         "show-help-text",
@@ -124,6 +133,20 @@ def prepare(element_html: str, data: pl.QuestionData) -> None:
     )
     if imaginary_unit not in {"i", "j"}:
         raise ValueError("imaginary-unit-for-display must be either i or j")
+
+    additional_simplifications = psu.get_items_list(
+        pl.get_string_attrib(
+            element, "additional-simplifications", ADDITIONAL_SIMPLIFICATIONS_DEFAULT
+        )
+    )
+    # Note: it is an intentional decision to allow repeats in the list, as this might be (rarely) an
+    # intended way to work around SymPy limitations
+    if not all(
+        item in SYMPY_ADDITIONAL_SIMPLIFICATIONS for item in additional_simplifications
+    ):
+        raise ValueError(
+            "The 'additional-simplifications' contain one of more unsupported simplification(s). Please see the documentation for a full list of supported simplifications."
+        )
 
 
 def render(element_html: str, data: pl.QuestionData) -> str:
@@ -659,6 +682,11 @@ def grade(element_html: str, data: pl.QuestionData) -> None:
     allow_trig = pl.get_boolean_attrib(
         element, "allow-trig-functions", ALLOW_TRIG_FUNCTIONS_DEFAULT
     )
+    additional_simplifications = psu.get_items_list(
+        pl.get_string_attrib(
+            element, "additional-simplifications", ADDITIONAL_SIMPLIFICATIONS_DEFAULT
+        )
+    )
     weight = pl.get_integer_attrib(element, "weight", WEIGHT_DEFAULT)
 
     # Get true answer (if it does not exist, create no grade - leave it
@@ -705,6 +733,11 @@ def grade(element_html: str, data: pl.QuestionData) -> None:
             a_sub_sympy = psu.json_to_sympy(
                 a_sub, allow_complex=allow_complex, allow_trig_functions=allow_trig
             )
+
+        for simplification in additional_simplifications:
+            simp_f = SYMPY_ADDITIONAL_SIMPLIFICATIONS[simplification]
+            a_sub_sympy = simp_f(a_sub_sympy)
+            a_tru_sympy = simp_f(a_tru_sympy)
 
         return a_tru_sympy.equals(a_sub_sympy) is True, None
 
