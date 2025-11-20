5c0444ef4a78f1b5947c37d01e07008273ecc52d
Peter Stenger <peter@prairielearn.com>
2025-11-19 21:52:22 -0500

    clean old code

--- apps/prairielearn/src/ee/lib/context-parsers/documentation.ts
+++ apps/prairielearn/src/ee/lib/context-parsers/documentation.ts
@@ -31,57 +31,6 @@ function stringify(content: any) {
   });
 }
 
-function extractElementSections(ast: any) {
-  const elementSections: ElementSection[] = [];
-
-  // Find the first level-three heading.
-  let startIndex = ast.children.findIndex((child) => {
-    return child.type === 'heading' && child.depth === 3;
-  });
-
-  if (startIndex === -1) {
-    throw new Error('No element sections found');
-  }
-
-  while (startIndex !== -1) {
-    const heading = ast.children[startIndex];
-
-    // All element headings should be level 3.
-    if (heading.type !== 'heading' || heading.depth !== 3) {
-      throw new Error('Expected heading');
-    }
-
-    // Element headings should contain an `inlineCode` node.
-    const inlineCode = heading.children[0];
-    if (inlineCode.type !== 'inlineCode') {
-      throw new Error('Expected inline code');
-    }
-
-    const elementName = inlineCode.value;
-
-    // Find the next level 2/3 heading.
-    let endIndex = ast.children.findIndex((child, index) => {
-      return (
-        index > startIndex && child.type === 'heading' && (child.depth === 2 || child.depth === 3)
-      );
-    });
-
-    // If there is no next heading, use the end of the document.
-    if (endIndex === -1) {
-      endIndex = ast.children.length;
-    }
-
-    const content = ast.children.slice(startIndex + 1, endIndex);
-    elementSections.push({ elementName, content });
-
-    startIndex = ast.children.findIndex((child, index) => {
-      return index >= endIndex && child.type === 'heading' && child.depth === 3;
-    });
-  }
-
-  return elementSections;
-}
-
 function removeHeadingAndContent(headingName: string, contents: any[]) {
   const headingIndex = contents.findIndex((node) => {
     return (
@@ -104,34 +53,6 @@ function removeHeadingAndContent(headingName: string, contents: any[]) {
   }
 
   contents.splice(headingIndex, nextHeadingIndex - headingIndex);
-}
-
-function cleanElementSections(elementSections: ElementSection[]) {
-  elementSections.forEach((section) => {
-    // Remove sections that aren't useful for the context.
-    removeHeadingAndContent('Example implementations', section.content);
-    removeHeadingAndContent('See also', section.content);
-
-    // Remove thematic breaks.
-    section.content = section.content.filter((node) => node.type !== 'thematicBreak');
-
-    // Rewrite all headings so that they can be nested under L2 headings.
-    section.content.forEach((node) => {
-      if (node.type === 'heading') {
-        // Safety check: all headings should be at least level 4.
-        if (node.depth < 4) {
-          console.error(section.content);
-          throw new Error('Expected heading to be at least level 4');
-        }
-        node.depth -= 1;
-      }
-      return node;
-    });
-    return section;
-  });
-
-  // Remove deprecated elements.
-  return elementSections.filter((section) => !DEPRECATED_ELEMENTS.has(section.elementName));
 }
 
 function writeOutTables(elementSections: ElementSection[]) {
@@ -188,29 +109,6 @@ function writeOutTables(elementSections: ElementSection[]) {
     return section;
   });
   return elementSections;
-}
-
-export function buildContextForElementDocs(rawMarkdown: string): DocumentChunk[] {
-  const file = unified().use(remarkParse).use(remarkGfm).parse(rawMarkdown);
-
-  const elementSections = writeOutTables(cleanElementSections(extractElementSections(file)));
-
-  const contexts = elementSections.map((section) => {
-    section.content.unshift({
-      type: 'heading',
-      depth: 2,
-      children: [{ type: 'text', value: section.elementName }],
-    });
-
-    const markdown = stringify(section.content);
-
-    return {
-      chunkId: section.elementName,
-      text: markdown,
-    };
-  });
-
-  return contexts.filter((x) => ALLOWED_ELEMENTS.has(x.chunkId));
 }
 
 /**
