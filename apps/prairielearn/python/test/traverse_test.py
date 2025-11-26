839ccc4a48947167bfca8e57a6ae522a082d7485
Jonatan Schroeder <jonatan@yorku.ca>
2025-11-26 16:52:00 -0500

    Fix handling of closing tags after empty replacement in question render

--- apps/prairielearn/python/test/traverse_test.py
+++ apps/prairielearn/python/test/traverse_test.py
@@ -394,3 +394,14 @@ def test_traverse_and_replace_script_complex() -> None:
 
     html = traverse_and_replace(test_str, replace)
     assert html == test_str
+
+
+def test_traverse_and_replace_empty_at_end() -> None:
+    def replace(e: lxml.html.HtmlElement) -> ElementReplacement:
+        if e.tag == "strong":
+            return ""
+        return e
+
+    # This is a reproduction of a bug found in https://github.com/PrairieLearn/PrairieLearn/issues/13427
+    html = traverse_and_replace("<div><strong>Hello!</strong></div>", replace)
+    assert html == "<div></div>"
