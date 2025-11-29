866e90a58286d332533e6130590f9795e9683ad2
Peter Stenger <peter@prairielearn.com>
2025-11-29 13:21:59 -0500

    update

--- scripts/process_linkinator_output.py
+++ scripts/process_linkinator_output.py
@@ -2,8 +2,13 @@ import json
 import sys
 
 if __name__ == "__main__":
+    has_broken_links = False
     data = json.load(sys.stdin)
 
     for link in data["links"]:
         if "BROKEN" in link["state"]:
+            has_broken_links = True
             print(link["url"])
+
+    if has_broken_links:
+        sys.exit(1)
