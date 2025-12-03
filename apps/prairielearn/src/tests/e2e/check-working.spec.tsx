438a4025ea28692f54ae665cf2183993039e0b4b
Peter Stenger <peter@prairielearn.com>
2025-12-03 13:05:59 -0500

    bla

--- apps/prairielearn/src/tests/e2e/check-working.spec.tsx
+++ apps/prairielearn/src/tests/e2e/check-working.spec.tsx
@@ -1,10 +1,9 @@
-/** @jsxImportSource @prairielearn/preact-cjs */
-
 // Importing from fixtures picks up our worker-scoped fixtures.
 import { expect, test } from './fixtures.js';
 
 test('preact powered JSX is working', () => {
   // We need to ensure that Preact is used for JSX, not playwright's own JSX implementation.
+  // https://github.com/PrairieLearn/PrairieLearn/pull/13493
   // https://github.com/microsoft/playwright/issues/26936
   const foo = <div>Hello</div>;
   expect(foo.constructor).toBeUndefined();
