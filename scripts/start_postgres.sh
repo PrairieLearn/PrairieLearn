e1fbafd5f62973ecd319ee1c99853cfdadd0060c
Peter Stenger <peter@prairielearn.com>
2025-12-05 17:17:10 -0500

    Add postgres binaries to path

--- scripts/start_postgres.sh
+++ scripts/start_postgres.sh
@@ -8,35 +8,33 @@ else
     ACTION=$1
 fi
 
-PG_BIN="/usr/lib/postgresql/16/bin"
-
 # if we are trying to start but postgres is already running, exit with no action
 if [[ "$ACTION" == "start" ]]; then
-    if ${PG_BIN}/pg_isready -q; then
+    if pg_isready -q; then
         exit
     fi
 fi
 
 mkdir -p $PGDATA
 chown -f postgres:postgres $PGDATA
-su postgres -c "${PG_BIN}/pg_ctl status" > /dev/null 2>&1
+su postgres -c "pg_ctl status" > /dev/null 2>&1
 if [[ $? == 4 ]]; then
     echo "Making new postgres database at ${PGDATA}"
-    su postgres -c "${PG_BIN}/initdb" > /dev/null 2>&1
+    su postgres -c "initdb" > /dev/null 2>&1
     INIT_RESOLVE=0
     ACTION=start
 fi
 
 # Only locally start postgres if we weren't given a PGHOST environment variable
 if [[ -z "$PGHOST" ]]; then
-    su postgres -c "${PG_BIN}/pg_ctl --silent --log=${PGDATA}/postgresql.log ${ACTION}"
+    su postgres -c "pg_ctl --silent --log=${PGDATA}/postgresql.log ${ACTION}"
 fi
 
 if [[ "$ACTION" == "start" ]]; then
     # wait for postgres to start (local or PGHOST)
-    until ${PG_BIN}/pg_isready -q; do sleep 1; done
+    until pg_isready -q; do sleep 1; done
 fi
 
 if [[ $INIT_RESOLVE ]]; then
-    su postgres -c "${PG_BIN}/createuser -s root"
+    su postgres -c "createuser -s root"
 fi
