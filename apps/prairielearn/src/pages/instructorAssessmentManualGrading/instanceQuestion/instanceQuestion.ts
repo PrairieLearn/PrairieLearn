f9387721ca8fe2f0e671aa4f70b2a20f83cbe2ec
Jonatan Schroeder <jonatan@yorku.ca>
2025-12-08 19:58:09 -0500

    Warn on manual grading for assessment instance with non-100 credit submissions (#13527)
    * Warn on manual grading for assessment instance with non-100 credit submissions
    
    * Fix query name
    
    * Lint

--- apps/prairielearn/src/pages/instructorAssessmentManualGrading/instanceQuestion/instanceQuestion.ts
+++ apps/prairielearn/src/pages/instructorAssessmentManualGrading/instanceQuestion/instanceQuestion.ts
@@ -218,6 +218,12 @@ router.get(
 
     req.session.skip_graded_submissions = req.session.skip_graded_submissions ?? true;
 
+    const submissionCredits = await sqldb.queryRows(
+      sql.select_submission_credit_values,
+      { assessment_instance_id: res.locals.assessment_instance.id },
+      z.number(),
+    );
+
     res.send(
       InstanceQuestionPage({
         ...(await prepareLocalsForRender(req.query, res.locals)),
@@ -233,6 +239,7 @@ router.get(
             ? await calculateAiGradingStats(res.locals.assessment_question)
             : null,
         skipGradedSubmissions: req.session.skip_graded_submissions,
+        submissionCredits,
       }),
     );
   }),
