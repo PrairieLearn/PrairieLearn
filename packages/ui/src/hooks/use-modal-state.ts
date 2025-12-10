81bfaba0176b353970c02f729bb14a9be13d895e
Nathan Sarang-Walters <nathan@prairielearn.com>
2025-12-10 08:31:11 -0800

    Add JSDoc comment

--- packages/ui/src/hooks/use-modal-state.ts
+++ packages/ui/src/hooks/use-modal-state.ts
@@ -1,5 +1,13 @@
 import { useState } from 'preact/hooks';
 
+/**
+ * A hook to manage the state of a modal dialog that's rendered with a certain set of data.
+ *
+ * The main thing this hook achieves is to separate the "shown" state of the modal from the
+ * data that it's rendered with, so that when the modal is closed, the data is cleared only
+ * after the modal has finished closing (i.e., after any closing animation has completed).
+ * This avoids a flash of an empty modal if the data is cleared immediately when the modal is closed.
+ */
 export function useModalState<T>(initialData: T | null = null) {
   const [show, setShow] = useState<boolean>(false);
   const [data, setData] = useState<T | null>(initialData);
