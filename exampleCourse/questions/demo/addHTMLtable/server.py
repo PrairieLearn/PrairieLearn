b93eb4d6ef82c016addec0c3aae371b1f7cc0f5e
Jonatan Schroeder <jonatan@yorku.ca>
2025-12-16 16:21:57 -0500

    Add mustache loop option to HTML table example question

--- exampleCourse/questions/demo/addHTMLtable/server.py
+++ exampleCourse/questions/demo/addHTMLtable/server.py
@@ -6,6 +6,14 @@ def generate(data):
     age_list = [24, 18, 30, 45, 32]
     name_greek_list = ["$\\gamma$", "$\\mu$", "$\\sigma$", "$\\lambda$", "$\\alpha$"]
 
+    # Generate content for rendering in mustache
+    data["params"]["table_data"] = [
+        {"name": name, "age": age, "id": name_greek}
+        for name, age, name_greek in zip(
+            name_list, age_list, name_greek_list, strict=True
+        )
+    ]
+
     # Generate complete html string
     mytable = '<table style="width:30%"><thead><tr><th> Name </th> <th> Age </th> <th> Id </th></tr></thead><tbody>'
     for name, age, name_greek in zip(name_list, age_list, name_greek_list, strict=True):
