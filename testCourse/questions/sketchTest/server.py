ac99da006da11aa6164abfcb032c05efdd98f95a
Nico Ritschel <nico@elsl.de>
2026-01-03 18:10:07 +0100

    test question

---
+++ testCourse/questions/sketchTest/server.py
@@ -0,0 +1,12 @@
+import random
+import sympy
+from sympy import diff
+from sympy.polys.specialpolys import random_poly
+from sympy.abc import x
+from random import randint
+
+def generate(data):
+    pass
+    
+
+
