9b7375c2fdf15b8abfe6504734ae17b7272eaaeb
Peter Stenger <peter@prairielearn.com>
2026-01-06 15:20:31 -0500

    Clarify enrollment code modal for instructors (#13699)
    * Update capitalization
    
    * Add usage note
    
    * update text
    
    * Update apps/prairielearn/src/components/EnrollmentCodeForm.tsx
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>
    
    * spacing
    
    * Update configuration.md
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>
    
    * PR nit
    
    ---------
    
    Co-authored-by: Nathan Sarang-Walters <nathan@prairielearn.com>

--- apps/prairielearn/src/pages/home/components/StudentCoursesCard.tsx
+++ apps/prairielearn/src/pages/home/components/StudentCoursesCard.tsx
@@ -154,6 +154,7 @@ export function StudentCoursesCard({
       <EnrollmentCodeForm
         style="modal"
         show={showEnrollmentCodeModal}
+        showInstructorHelp={hasInstructorCourses}
         onHide={() => setShowEnrollmentCodeModal(false)}
       />
 
