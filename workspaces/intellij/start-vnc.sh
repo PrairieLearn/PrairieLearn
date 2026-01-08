eb733104f69b34452393cb7e8f6b75dba9431699
Jonatan Schroeder <jonatan@yorku.ca>
2026-01-08 14:38:14 -0500

    Fix Dockerfile

--- workspaces/intellij/start-vnc.sh
+++ workspaces/intellij/start-vnc.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Copy over config files
-cp -r /opt/defaults/config /home/prairielearner/.config
+# cp -r /opt/defaults/config /home/prairielearner/.config
 cp -r /opt/defaults/java /home/prairielearner/.java
 cp /opt/defaults/.profile /home/prairielearner/.profile
 cp /opt/defaults/.bashrc /home/prairielearner/.bashrc
 
-chown -R 1001:1001 /home/prairielearner/.config /home/prairielearner/.java
+chown -R 1001:1001 /home/prairielearner/.java
 
 # VNC -> Websocket proxy layer
 websockify :5901 localhost:5900 &
