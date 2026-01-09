9b79da60a9245239e1013e7cb5b9f6f51fdf1f9f
Nathan Sarang-Walters <nathan@prairielearn.com>
2026-01-09 14:10:05 -0800

    Combine files; address review comments

--- apps/prairielearn/src/middlewares/authzPublicCourseOrInstance.tsx
+++ apps/prairielearn/src/middlewares/authzPublicCourseOrInstance.tsx
@@ -1,13 +1,37 @@
 import { HttpStatusError } from '@prairielearn/error';
 import { run } from '@prairielearn/run';
 
-import { MissingSharingNameCard } from '../components/MissingSharingNameCard.js';
 import { PageLayout } from '../components/PageLayout.js';
 import { features } from '../lib/features/index.js';
 import { typedAsyncHandler } from '../lib/res-locals.js';
 import { selectOptionalCourseInstanceById } from '../models/course-instances.js';
 import { selectOptionalCourseById } from '../models/course.js';
 
+function MissingCourseSharingNameCard({ courseId }: { courseId: string }) {
+  return (
+    <div className="card mb-4">
+      <div className="card-header bg-primary text-white">Missing sharing name</div>
+      <div className="card-body">
+        <p className="mb-0">
+          This course doesn't have a sharing name. If you are an Owner of this course, please choose
+          a sharing name on the{' '}
+          <a href={`/pl/course/${courseId}/course_admin/sharing`}>course sharing settings page</a>.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+/**
+ * Middleware to authorize access to public course or course instance routes.
+ * Checks if the course or course instance exists, if question sharing is enabled,
+ * if the course has a sharing name, and if the course instance is shared publicly
+ * (if applicable).
+ *
+ * If authorization fails, responds with a 404 Not Found error.
+ *
+ * If it succeeds, sets res.locals.course and optionally res.locals.course_instance.
+ */
 export default typedAsyncHandler<'public-course' | 'public-course-instance'>(
   async (req, res, next) => {
     const { course, course_instance } = await run(async () => {
@@ -60,7 +84,7 @@ export default typedAsyncHandler<'public-course' | 'public-course-instance'>(
             type: 'public',
             page: 'error',
           },
-          content: <MissingSharingNameCard courseId={course.id} />,
+          content: <MissingCourseSharingNameCard courseId={course.id} />,
         }),
       );
       return;
