f2a5be5d5cf221a29f44aa09b65e9fdb3732d637
Nathan Sarang-Walters <nathan@prairielearn.com>
2026-01-12 11:25:46 -0800

    Remove pythonVenvSearchPaths config option

--- apps/prairielearn/src/lib/code-caller/python-path.ts
+++ apps/prairielearn/src/lib/code-caller/python-path.ts
@@ -5,16 +5,15 @@ import fs from 'fs-extra';
 import { REPOSITORY_ROOT_PATH } from '../paths.js';
 
 const PYTHON_VERSION = 'python3.10';
+const PYTHON_VENV_SEARCH_PATHS = ['.venv'];
 
 /**
- * Finds a Python executable by searching through the provided venv search paths.
- * Throws an error if no venv is found.
+ * Finds a Python executable in a virtual environment. Throws an error if no venv is found.
  *
- * @param pythonVenvSearchPaths - Array of paths to search for Python venvs
  * @returns The path to a Python executable
  */
-export async function getPythonPath(pythonVenvSearchPaths: string[]): Promise<string> {
-  for (const p of pythonVenvSearchPaths) {
+export async function getPythonPath(): Promise<string> {
+  for (const p of PYTHON_VENV_SEARCH_PATHS) {
     const venvPython = path.resolve(REPOSITORY_ROOT_PATH, path.join(p, 'bin', PYTHON_VERSION));
     if (await fs.pathExists(venvPython)) return venvPython;
   }
