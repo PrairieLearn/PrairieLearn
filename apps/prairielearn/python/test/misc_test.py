a97e62e08b55fee72c51238d9ed0b8ec1e82f788
Eliot Robson <eliot.robson24@gmail.com>
2026-01-14 21:24:17 -0500

    Update misc_test.py

--- apps/prairielearn/python/test/misc_test.py
+++ apps/prairielearn/python/test/misc_test.py
@@ -504,11 +504,15 @@ def test_grade_answer_parametrized_timeout(
         assert question_name in question_data["format_errors"]
         expected_error_substring = timeout_format_error or "Grading timed out"
         assert expected_error_substring in question_data["format_errors"][question_name]
-        assert question_data["partial_scores"][question_name]["score"] == 0.0
+        assert math.isclose(
+            question_data["partial_scores"][question_name]["score"], 0.0
+        )
     else:
         assert question_name not in question_data["format_errors"]
         expected_score = 1.0 if case.return_value else 0.0
-        assert question_data["partial_scores"][question_name]["score"] == expected_score
+        assert math.isclose(
+            question_data["partial_scores"][question_name]["score"], expected_score
+        )
 
         if case.has_feedback:
             assert (
