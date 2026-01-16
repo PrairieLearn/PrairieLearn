213bc0e2b83761c6cbbfd60c7fd3b60e11b381aa
Jonatan Schroeder <jonatan@yorku.ca>
2026-01-16 14:50:54 -0500

    Add comment on what makes example course special

--- apps/prairielearn/src/sync/fromDisk/courseInfo.ts
+++ apps/prairielearn/src/sync/fromDisk/courseInfo.ts
@@ -11,6 +11,18 @@ import * as infofile from '../infofile.js';
 const sql = sqldb.loadSqlEquiv(import.meta.url);
 
 function isExampleCourse(courseDir: string): boolean {
+  /* The example course is identified by its path relative to the repository
+   * root. When the course is tagged as an example course, it will be treated
+   * specially in various parts of the system. In particular:
+   *
+   * - Any user with staff permission in any course automatically gets viewer access to the example course;
+   * - Nobody can modify the example course content in the Web UI (admins and editors can at most sync);
+   * - The home page always shows the example course question in the list of courses with staff access, as long as one more course is listed;
+   * - Questions in the example course with QID starting in template/ are shown as templates everywhere;
+   * - Non-admins cannot change to a different effective user in the example course;
+   * - Variants in example course questions are only visible to the variant's creator, even if the current user has staff access;
+   * - Only administrators get API access to the example course.
+   */
   return (
     resolve(REPOSITORY_ROOT_PATH, config.exampleCoursePathIdentifier) ===
     resolve(REPOSITORY_ROOT_PATH, courseDir)
