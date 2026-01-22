f8edc0b9b3809f3037b6cbc9b0ec860002a2b86b
Peter Stenger <peter@prairielearn.com>
2026-01-22 16:19:34 -0500

    oops

--- apps/prairielearn/src/pages/instructorCourseAdminStaff/instructorCourseAdminStaff.sql
+++ apps/prairielearn/src/pages/instructorCourseAdminStaff/instructorCourseAdminStaff.sql
@@ -51,7 +51,7 @@ FROM
   LATERAL (
     SELECT
       COALESCE(ci.publishing_start_date, min(ar.start_date)) AS start_date,
-      COALESCE(ci.publishing_end_date, min(ar.end_date)) AS end_date
+      COALESCE(ci.publishing_end_date, max(ar.end_date)) AS end_date
     FROM
       course_instance_access_rules AS ar
     WHERE
