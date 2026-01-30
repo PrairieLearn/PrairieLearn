d93a4d863042f34d50c87e5109be00221a458561
Jonatan Schroeder <jonatan@yorku.ca>
2026-01-30 15:22:30 -0500

    Replace use of lodash

--- apps/prairielearn/src/tests/constantQuestionValue.test.ts
+++ apps/prairielearn/src/tests/constantQuestionValue.test.ts
@@ -1,5 +1,5 @@
 import * as cheerio from 'cheerio';
-import _ from 'lodash';
+import { keyBy } from 'es-toolkit';
 import fetch from 'node-fetch';
 import { afterAll, assert, beforeAll, describe, it } from 'vitest';
 import z from 'zod';
@@ -79,7 +79,7 @@ const questionsArray: TestQuestion[] = [
   { qid: 'partialCredit6_no_partial', type: 'Freeform', maxPoints: 11 },
 ];
 
-const questions = _.keyBy(questionsArray, 'qid');
+const questions = keyBy(questionsArray, (question) => question.qid);
 
 const assessmentMaxPoints = 49;
 
