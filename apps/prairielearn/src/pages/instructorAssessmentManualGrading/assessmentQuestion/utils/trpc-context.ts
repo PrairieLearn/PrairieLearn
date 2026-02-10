0e60166e7d71c9303d8b271a6572082969c27e74
Peter Stenger <peter@prairielearn.com>
2026-02-10 17:46:29 -0500

    fmt

--- apps/prairielearn/src/pages/instructorAssessmentManualGrading/assessmentQuestion/utils/trpc-context.ts
+++ apps/prairielearn/src/pages/instructorAssessmentManualGrading/assessmentQuestion/utils/trpc-context.ts
@@ -2,5 +2,4 @@ import { createTRPCContext } from '@trpc/tanstack-react-query';
 
 import type { ManualGradingAssessmentQuestionRouter } from '../trpc.js';
 
-export const { TRPCProvider, useTRPC } =
-  createTRPCContext<ManualGradingAssessmentQuestionRouter>();
+export const { TRPCProvider, useTRPC } = createTRPCContext<ManualGradingAssessmentQuestionRouter>();
