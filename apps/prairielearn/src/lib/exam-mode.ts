a323891fd5e67d850f5b0816fcd11ad0d4eeb42a
Jonatan Schroeder <jonatan@yorku.ca>
2026-03-11 13:50:12 -0400

    Remove requirement to have an IP

--- apps/prairielearn/src/lib/exam-mode.ts
+++ apps/prairielearn/src/lib/exam-mode.ts
@@ -22,7 +22,7 @@ export async function getModeForRequest(req: Request, res: Response): Promise<En
   }
 
   return await ipToMode({
-    ip: req.ip,
+    ip: req.ip ?? null,
     date: res.locals.req_date,
     authn_user_id: res.locals.authn_user.id,
   });
@@ -33,14 +33,10 @@ export async function ipToMode({
   date,
   authn_user_id,
 }: {
-  ip: string | null | undefined;
+  ip: string | null;
   date: Date;
   authn_user_id: string;
 }): Promise<EnumMode> {
-  // Express's types indicate that `ip` may be undefined in some cases. We want
-  // to ensure that we don't try to proceed without one.
-  if (ip == null) throw new Error('IP address is required');
-
   const hasPrairieTestReservation = await queryScalar(
     sql.select_active_prairietest_reservation,
     { ip, date, authn_user_id },
