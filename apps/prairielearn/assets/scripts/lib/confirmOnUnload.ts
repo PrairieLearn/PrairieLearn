771fd0d4ff28358ad92b36467dd55c7d1f700e6f
Peter Stenger <peter@prairielearn.com>
2026-03-12 11:04:22 -0400

    Get lint passing

--- apps/prairielearn/assets/scripts/lib/confirmOnUnload.ts
+++ apps/prairielearn/assets/scripts/lib/confirmOnUnload.ts
@@ -116,18 +116,10 @@ export function confirmOnUnload(form: HTMLFormElement): () => void {
     const isSameForm = form.dataset.originalFormData === getQuestionFormData(form);
 
     if (!isSameForm) {
-      // event.preventDefault() is used in Safari/Firefox, but not supported by Chrome/Edge/etc.
-      // Returning a string is supported in almost all browsers that support beforeunload.
-      // Newer versions of Chrome/Edge appear to no longer support returning a string,
-      // but they do seem to support setting `event.returnValue`.
+      // Calling preventDefault() is the standard way to trigger the confirmation dialog.
       // Safari on iOS does not support confirmation on beforeunload at all.
       // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event#compatibility_notes
-      // Note that per the spec, we must technically return a non-empty string,
-      // although the contents of the string should always be ignored.
-      // https://html.spec.whatwg.org/multipage/browsing-the-web.html#unloading-documents:event-beforeunload
       event.preventDefault();
-      event.returnValue = 'prompt';
-      return 'prompt';
     }
   };
   window.addEventListener('beforeunload', handleBeforeUnload);
