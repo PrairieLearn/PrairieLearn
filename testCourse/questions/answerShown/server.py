a27ae06492ba95d10db35578ac34b7a6215d7fcd
Nico Ritschel <nritschel@users.noreply.github.com>
2026-04-08 12:34:23 -0500

    Remove outdated comment in test question server.py

--- testCourse/questions/answerShown/server.py
+++ testCourse/questions/answerShown/server.py
@@ -1,5 +1,2 @@
 def render(data, _html):
-    # This rendering logic leads to an invalid submission on the first attempt,
-    # and a correct submission on the second attempt once the answer is shown.
-    # Note that this only works on the instructor preview page.
     return data["answer_shown"]
