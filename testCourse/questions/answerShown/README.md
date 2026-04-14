db9041e09c100c58848b19b6993f19c43eea4484
Nathan Sarang-Walters <nathan@prairielearn.com>
2026-04-14 13:05:12 -0700

    Avoid use of render() in correct_answer_shown test question

--- testCourse/questions/answerShown/README.md
+++
@@ -1,1 +0,0 @@
-This is a question that tests the correct_answer_shown key in data.
--- testCourse/questions/answerShown/server.py
+++
@@ -1,2 +0,0 @@
-def render(data, _html):
-    return f'The current value of data["correct_answer_shown"] is: {data["correct_answer_shown"]}'
