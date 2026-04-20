5678d145425ee4442662ddba2399b7eb062c7115
austinbillings-pl <austin@prairielearn.com>
2026-04-20 18:06:35 -0400

    Register CanvasImportForm hydrated component

---
+++ apps/prairielearn/assets/scripts/esm-bundles/hydrated-components/CanvasImportForm.ts
@@ -0,0 +1,5 @@
+import { registerHydratedComponent } from '@prairielearn/react/hydrated-component';
+
+import { CanvasImportForm } from '../../../../src/pages/instructorCanvasImport/components/CanvasImportForm.js';
+
+registerHydratedComponent(CanvasImportForm);
