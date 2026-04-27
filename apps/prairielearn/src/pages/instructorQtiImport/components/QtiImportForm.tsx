ff6fcb3512290da468f4acae46244713a5394c4e
austinbillings-pl <austin@prairielearn.com>
2026-04-27 17:05:36 -0400

    fmt

--- apps/prairielearn/src/pages/instructorQtiImport/components/QtiImportForm.tsx
+++ apps/prairielearn/src/pages/instructorQtiImport/components/QtiImportForm.tsx
@@ -607,11 +607,7 @@ function UploadStep({
         Import quiz and question content from Canvas or other learning management systems. Upload a
         quiz export (<code>.zip</code>) or a full course export (<code>.imscc</code>) to get
         started. (Supports the QTI 1.2 interchange format.){' '}
-        <a
-          href="https://docs.prairielearn.com/importingContent/"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://docs.prairielearn.com/importingContent/" target="_blank" rel="noreferrer">
           Learn more about importing content into PrairieLearn
         </a>
       </p>
