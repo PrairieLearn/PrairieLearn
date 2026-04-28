5fa86bee7fe750458981eafbd8d9ede57dcc3b38
Henry House <hajhouse@houseag.com>
2026-04-27 17:22:03 -0700

    pl-number-input: -unused param, tightening message
    apps/prairielearn/elements/pl-number-input/pl-number-input.mustache: Removing
    an unused parameter, mirroring the messaging of the short case to full case.

--- apps/prairielearn/elements/pl-number-input/pl-number-input.py
+++ apps/prairielearn/elements/pl-number-input/pl-number-input.py
@@ -264,7 +264,6 @@ def render(element_html: str, data: pl.QuestionData) -> str:
             info_params = {
                 "format": True,
                 "relabs": True,
-                "rtol": f"{rtol:g}",
                 "rtol_pct": f"{(rtol * 100):g}",
                 "nonzero_rtol": rtol != 0,
                 "atol": f"{atol:g}",
