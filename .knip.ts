0b479e186c3cf7734c995f02f0061585edb7420f
Peter Stenger <peter@prairielearn.com>
2026-05-04 17:45:59 -0500

    fix lint

--- .knip.ts
+++ .knip.ts
@@ -12,7 +12,6 @@ const REFERENCED_NODE_MODULES_DEPS = [
   'marked',
   'clipboard',
   'async',
-  'tom-select',
   'qrcode-svg',
   'socket.io-client',
   'lodash',
@@ -24,9 +23,20 @@ const REFERENCED_NODE_MODULES_DEPS = [
   'highlight.js',
 ];
 
+// Dependencies referenced from files that aren't referenced
+const DEAD_CODE_DEPS = ['@tiptap/extension-code-block'];
+
 // These packages aren't used in our own code, but we still want them installed
 // as they are used by elements in other courses.
-const FALSE_NEGATIVE_ELEMENT_DEPS = ['backbone', 'mersenne', 'numeric', 'popper.js'];
+const FALSE_NEGATIVE_ELEMENT_DEPS = [
+  'backbone',
+  'dropzone',
+  'lodash',
+  'mersenne',
+  'numeric',
+  'popper.js',
+  'showdown',
+];
 
 // These packages are used for their CLI tools, so we still want them installed.
 const FALSE_NEGATIVE_CLI_DEPS = [
@@ -90,9 +100,6 @@ for (const dep of REFERENCED_NODE_MODULES_DEPS) {
 
 const config: KnipConfig = {
   tags: ['-knipignore'],
-  // TODO: We need to fix dependency issues
-
-  ignore: ['**/*.json'],
   workspaces: {
     '.': {
       entry: ['scripts/*.{mts,mjs}'],
@@ -151,6 +158,7 @@ const config: KnipConfig = {
     ...packageDependencies,
     ...FALSE_NEGATIVE_ELEMENT_DEPS,
     ...FALSE_NEGATIVE_CLI_DEPS,
+    ...DEAD_CODE_DEPS,
   ],
   // TODO: enable these features
   exclude: ['binaries'],
