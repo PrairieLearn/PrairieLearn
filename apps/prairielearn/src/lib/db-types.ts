97c39865f4722848ab6153a3b601234dd8d3c746
Peter Stenger <peter@prairielearn.com>
2026-05-27 17:44:15 -0500

    fix

--- apps/prairielearn/src/lib/db-types.ts
+++ apps/prairielearn/src/lib/db-types.ts
@@ -1153,11 +1153,11 @@ export const InstitutionSchema = z.object({
 });
 export type Institution = z.infer<typeof InstitutionSchema>;
 
-export const InstitutionSettingsSchema = z.object({
+export const InstitutionSettingSchema = z.object({
   course_request_message: z.string().nullable(),
   institution_id: IdSchema,
 });
-export type InstitutionSettings = z.infer<typeof InstitutionSettingsSchema>;
+export type InstitutionSetting = z.infer<typeof InstitutionSettingSchema>;
 
 export const InstitutionAdministratorSchema = z.object({
   id: IdSchema,
--- apps/prairielearn/src/models/institution-settings.ts
+++ apps/prairielearn/src/models/institution-settings.ts
@@ -1,6 +1,6 @@
 import { loadSqlEquiv, queryOptionalRow, queryRow } from '@prairielearn/postgres';
 
-import { type InstitutionSettings, InstitutionSettingsSchema } from '../lib/db-types.js';
+import { type InstitutionSetting, InstitutionSettingSchema } from '../lib/db-types.js';
 
 const sql = loadSqlEquiv(import.meta.url);
 
@@ -8,11 +8,11 @@ export async function selectInstitutionSettings({
   institution_id,
 }: {
   institution_id: string;
-}): Promise<InstitutionSettings | null> {
+}): Promise<InstitutionSetting | null> {
   return await queryOptionalRow(
     sql.select_institution_settings,
     { institution_id },
-    InstitutionSettingsSchema,
+    InstitutionSettingSchema,
   );
 }
 
@@ -22,10 +22,10 @@ export async function updateInstitutionCourseRequestMessage({
 }: {
   institution_id: string;
   course_request_message: string | null;
-}): Promise<InstitutionSettings> {
+}): Promise<InstitutionSetting> {
   return await queryRow(
     sql.upsert_institution_settings,
     { institution_id, course_request_message },
-    InstitutionSettingsSchema,
+    InstitutionSettingSchema,
   );
 }
