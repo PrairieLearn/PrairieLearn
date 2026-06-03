2b15341d86a6281fa5247e5d6029947c686788c3
Peter Stenger <peter@prairielearn.com>
2026-06-03 12:46:19 -0500

    backout upload changes

--- apps/prairielearn/src/pages/instructorAssessmentUploads/instructorAssessmentUploads.ts
+++ apps/prairielearn/src/pages/instructorAssessmentUploads/instructorAssessmentUploads.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import * as error from '@prairielearn/error';
+import * as sqldb from '@prairielearn/postgres';
 
 import { config } from '../../lib/config.js';
 import { typedAsyncHandler } from '../../lib/res-locals.js';
@@ -11,9 +12,13 @@ import {
 import { uploadSubmissions } from '../../lib/submissions-upload.js';
 import { createAuthzMiddleware } from '../../middlewares/authzHelper.js';
 
-import { InstructorAssessmentUploads } from './instructorAssessmentUploads.html.js';
+import {
+  InstructorAssessmentUploads,
+  UploadJobSequenceSchema,
+} from './instructorAssessmentUploads.html.js';
 
 const router = Router();
+const sql = sqldb.loadSqlEquiv(import.meta.url);
 
 router.get(
   '/',
@@ -22,7 +27,12 @@ router.get(
     unauthorizedUsers: 'block',
   }),
   typedAsyncHandler<'assessment'>(async (req, res) => {
-    res.send(InstructorAssessmentUploads({ resLocals: res.locals }));
+    const uploadJobSequences = await sqldb.queryRows(
+      sql.select_upload_job_sequences,
+      { assessment_id: res.locals.assessment.id },
+      UploadJobSequenceSchema,
+    );
+    res.send(InstructorAssessmentUploads({ resLocals: res.locals, uploadJobSequences }));
   }),
 );
 
