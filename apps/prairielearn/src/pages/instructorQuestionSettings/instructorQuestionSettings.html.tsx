a09d09ba150817c3a4e1eb1cfcf148e757dccc7e
Jonatan Schroeder <jonatan@yorku.ca>
2026-06-23 10:36:17 -0400

    Change helper text

--- apps/prairielearn/src/pages/instructorQuestionSettings/instructorQuestionSettings.html.tsx
+++ apps/prairielearn/src/pages/instructorQuestionSettings/instructorQuestionSettings.html.tsx
@@ -718,8 +718,8 @@ export const InstructorQuestionSettingsForm = ({
                     Override URL rewrite image settings
                   </label>
                   <div className="small text-muted">
-                    Workspace images can specify if their URL should be rewritten by the workspace
-                    proxy. Override to change the settings for this question.
+                    Workspace images can specify how URLs should be handled by the workspace proxy.
+                    Override to change the default workspace settings for this question.
                   </div>
                 </div>
                 {workspaceRewriteUrlOverride && (
