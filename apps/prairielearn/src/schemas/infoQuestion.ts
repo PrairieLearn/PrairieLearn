11a67ed1df85a6de76c3311de53d43d8e5580217
Jonatan Schroeder <jonatan@yorku.ca>
2026-06-23 15:53:49 -0400

    Simplify schema description

--- apps/prairielearn/src/schemas/infoQuestion.ts
+++ apps/prairielearn/src/schemas/infoQuestion.ts
@@ -128,7 +128,7 @@ const WorkspaceOptionsJsonSchema = z
     rewriteUrl: z
       .boolean()
       .describe(
-        'If true, the URL will be rewritten such that the workspace container will see all requests as originating from /. If false, the workspace container will see all requests as originating from /pl/workspace/XXX/container/. If not specified, the URL rewrite setting is retrieved from the workspace image labels.',
+        'Controls whether workspace request URLs are rewritten before reaching the container. If not specified, the setting is retrieved from the workspace image labels.',
       )
       .optional(),
     gradedFiles: z
