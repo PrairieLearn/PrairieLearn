9f447f2928dd1465e5ba6323782fa94c5a8f242d
Nathan Sarang-Walters <nathan@prairielearn.com>
2026-06-23 14:22:55 -0700

    Use safe job sequence props

--- apps/prairielearn/src/components/JobSequenceResults.html.tsx
+++ apps/prairielearn/src/components/JobSequenceResults.html.tsx
@@ -6,7 +6,29 @@ import type { Course } from '../lib/db-types.js';
 import type { JobSequenceWithTokens } from '../lib/server-jobs.types.js';
 
 import { JobSequenceResults as JobSequenceResultsComponent } from './JobSequenceResults.js';
-import { JobSequenceResultsPropsSchema } from './JobSequenceResults.shared.js';
+import {
+  type JobSequenceResultsProps,
+  JobSequenceResultsPropsSchema,
+} from './JobSequenceResults.shared.js';
+
+export function getJobSequenceResultsProps({
+  course,
+  jobSequence,
+}: {
+  course: Course | undefined;
+  jobSequence: JobSequenceWithTokens;
+}): JobSequenceResultsProps {
+  return JobSequenceResultsPropsSchema.parse({
+    jobSequence: {
+      ...jobSequence,
+      jobs: jobSequence.jobs.map((job) => ({
+        ...job,
+        outputHtml: ansiToHtml(job.output),
+      })),
+    },
+    timeZone: course?.display_timezone || 'UTC',
+  });
+}
 
 /**
  * Renders the results of a job sequence with live updates.
@@ -18,35 +40,7 @@ export function JobSequenceResults({
   course: Course | undefined;
   jobSequence: JobSequenceWithTokens;
 }): HtmlSafeString {
-  const timeZone = course?.display_timezone || 'UTC';
-  const clientProps = JobSequenceResultsPropsSchema.parse({
-    jobSequence: {
-      authn_user_uid: jobSequence.authn_user_uid,
-      description: jobSequence.description,
-      id: jobSequence.id,
-      jobs: jobSequence.jobs.map((job) => ({
-        arguments: job.arguments,
-        command: job.command,
-        description: job.description,
-        error_message: job.error_message,
-        exit_code: job.exit_code,
-        exit_signal: job.exit_signal,
-        finish_date: job.finish_date,
-        id: job.id,
-        number_in_sequence: job.number_in_sequence,
-        outputHtml: ansiToHtml(job.output),
-        start_date: job.start_date,
-        status: job.status,
-        token: job.token,
-        working_directory: job.working_directory,
-      })),
-      legacy: jobSequence.legacy,
-      number: jobSequence.number,
-      token: jobSequence.token,
-      user_uid: jobSequence.user_uid,
-    },
-    timeZone,
-  });
+  const clientProps = getJobSequenceResultsProps({ course, jobSequence });
 
   return hydrateHtml(
     <JobSequenceResultsComponent
--- apps/prairielearn/src/components/JobSequenceResults.shared.ts
+++ apps/prairielearn/src/components/JobSequenceResults.shared.ts
@@ -1,31 +1,17 @@
 import { z } from 'zod';
 
-import { JobSchema, JobSequenceSchema } from '../lib/db-types.js';
+import { RawStaffJobSchema, RawStaffJobSequenceSchema } from '../lib/client/safe-db-types.js';
 
-const JobSequenceResultsJobSchema = z.object({
-  arguments: JobSchema.shape.arguments,
-  command: JobSchema.shape.command,
-  description: JobSchema.shape.description,
-  error_message: JobSchema.shape.error_message,
-  exit_code: JobSchema.shape.exit_code,
-  exit_signal: JobSchema.shape.exit_signal,
-  finish_date: JobSchema.shape.finish_date,
-  id: JobSchema.shape.id,
-  number_in_sequence: JobSchema.shape.number_in_sequence,
+const JobSequenceResultsJobSchema = RawStaffJobSchema.omit({
+  output: true,
+}).extend({
   outputHtml: z.string(),
-  start_date: JobSchema.shape.start_date,
-  status: JobSchema.shape.status,
   token: z.string(),
-  working_directory: JobSchema.shape.working_directory,
 });
 
-const JobSequenceResultsJobSequenceSchema = z.object({
+const JobSequenceResultsJobSequenceSchema = RawStaffJobSequenceSchema.extend({
   authn_user_uid: z.string().nullable(),
-  description: JobSequenceSchema.shape.description,
-  id: JobSequenceSchema.shape.id,
   jobs: JobSequenceResultsJobSchema.array(),
-  legacy: JobSequenceSchema.shape.legacy,
-  number: JobSequenceSchema.shape.number,
   token: z.string(),
   user_uid: z.string().nullable(),
 });
