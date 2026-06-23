f620468aba182af8f117108e31d169f988f1598e
Jonatan Schroeder <jonatan@yorku.ca>
2026-06-23 11:54:11 -0400

    Coalesce backfill to true

--- apps/prairielearn/src/batched-migrations/20260618121400_workspaces__url_rewrite__backfill.sql
+++ apps/prairielearn/src/batched-migrations/20260618121400_workspaces__url_rewrite__backfill.sql
@@ -10,7 +10,12 @@ WHERE
 -- BLOCK backfill_url_rewrite
 UPDATE workspaces AS w
 SET
-  url_rewrite = q.workspace_url_rewrite
+  -- All workspaces being backfilled were created before the label was used, so
+  -- they relied on the default value of TRUE for url_rewrite. Workspaces
+  -- created between the column addition and the label introduction (i.e., not
+  -- included in this backfill) may still have a null url_rewrite value, but
+  -- they will be set with a suitable value the next time they are started.
+  url_rewrite = COALESCE(q.workspace_url_rewrite, TRUE)
 FROM
   variants AS v
   JOIN questions AS q ON (q.id = v.question_id)
