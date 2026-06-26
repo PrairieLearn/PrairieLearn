c0177dbb9dd12d1ceee4d688f61ae6456852012e
Jonatan Schroeder <jonatan@yorku.ca>
2026-06-26 17:44:46 -0400

    Fix migration finalize

--- apps/prairielearn/src/migrations/20260625130846_questions__workspace_url_rewrite__null_sync__backfill__finalize.ts
+++ apps/prairielearn/src/migrations/20260625130846_questions__workspace_url_rewrite__null_sync__backfill__finalize.ts
@@ -1,7 +1,7 @@
-import { enqueueBatchedMigration } from '@prairielearn/migrations';
+import { finalizeBatchedMigration } from '@prairielearn/migrations';
 
 export default async function () {
-  await enqueueBatchedMigration(
+  await finalizeBatchedMigration(
     '20260624121359_questions__workspace_url_rewrite__null_sync__backfill',
   );
 }
