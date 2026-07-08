a21badae90f60fb0ea34b0596ae13508058a5e6d
Victor Zhao <victorsss.orz@outlook.com>
2026-07-08 00:18:47 -0500

    better way to define the new table?:

--- apps/prairielearn/src/migrations/20260707175253_create_user_settings.sql
+++ apps/prairielearn/src/migrations/20260707175253_create_user_settings.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_settings (
-  id bigserial PRIMARY KEY,
   enable_keyboard_shortcut BOOLEAN NOT NULL DEFAULT TRUE,
-  user_id BIGINT NOT NULL REFERENCES users ON DELETE CASCADE ON UPDATE CASCADE
+  user_id BIGINT NOT NULL PRIMARY KEY REFERENCES users ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS user_settings_user_id_idx ON user_settings (user_id);
