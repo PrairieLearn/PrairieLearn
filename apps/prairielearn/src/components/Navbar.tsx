5be8b67ee47631a8a3b92763bb053a2a8d018fb8
Nathan Sarang-Walters <nathan@prairielearn.com>
2026-07-22 12:31:45 -0700

    Make effective user input responsive

--- apps/prairielearn/src/components/Navbar.tsx
+++ apps/prairielearn/src/components/Navbar.tsx
@@ -621,7 +621,6 @@ function AuthnOverrides({
         type="email"
         placeholder="student@example.com"
         class="form-control form-control-sm me-2 flex-grow-1 js-effective-uid-input"
-        style="min-width: 12rem;"
         aria-label="UID"
       />
       <button
