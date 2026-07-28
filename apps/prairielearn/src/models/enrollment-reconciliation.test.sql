f57db57aba59935aaab905a715240d336d79aa05
Nathan Sarang-Walters <nathan@prairielearn.com>
2026-07-28 10:54:09 -0700

    Add enrollment identity reconciliation model

---
+++ apps/prairielearn/src/models/enrollment-reconciliation.test.sql
@@ -0,0 +1,178 @@
+-- BLOCK insert_enrollment
+INSERT INTO
+  enrollments (
+    course_instance_id,
+    first_joined_at,
+    is_guest,
+    pending_email,
+    pending_lti13_course_instance_id,
+    pending_lti13_sub,
+    pending_name,
+    pending_uid,
+    pending_uin,
+    status,
+    user_id
+  )
+VALUES
+  (
+    $course_instance_id,
+    $first_joined_at,
+    $is_guest,
+    $pending_email,
+    $pending_lti13_course_instance_id,
+    $pending_lti13_sub,
+    $pending_name,
+    $pending_uid,
+    $pending_uin,
+    $status,
+    $user_id
+  )
+RETURNING
+  *;
+
+-- BLOCK select_enrollments_by_ids
+SELECT
+  *
+FROM
+  enrollments
+WHERE
+  id = ANY ($enrollment_ids::bigint[])
+ORDER BY
+  id;
+
+-- BLOCK insert_lti13_course_instance
+INSERT INTO
+  lti13_course_instances (course_instance_id, deployment_id, context_id)
+VALUES
+  ($course_instance_id, $deployment_id, $context_id)
+RETURNING
+  *;
+
+-- BLOCK insert_student_label
+INSERT INTO
+  student_labels (course_instance_id, name, color, uuid)
+VALUES
+  ($course_instance_id, $name, 'blue1', $uuid)
+RETURNING
+  *;
+
+-- BLOCK insert_student_label_enrollment
+INSERT INTO
+  student_label_enrollments (enrollment_id, student_label_id)
+VALUES
+  ($enrollment_id, $student_label_id)
+ON CONFLICT (enrollment_id, student_label_id) DO NOTHING;
+
+-- BLOCK select_student_label_ids
+SELECT
+  student_label_id AS id
+FROM
+  student_label_enrollments
+WHERE
+  enrollment_id = $enrollment_id
+ORDER BY
+  student_label_id;
+
+-- BLOCK insert_publishing_extension
+INSERT INTO
+  course_instance_publishing_extensions (course_instance_id, name, end_date)
+VALUES
+  ($course_instance_id, $name, $end_date)
+RETURNING
+  *;
+
+-- BLOCK insert_publishing_extension_enrollment
+INSERT INTO
+  course_instance_publishing_extension_enrollments (
+    enrollment_id,
+    course_instance_publishing_extension_id
+  )
+VALUES
+  ($enrollment_id, $publishing_extension_id);
+
+-- BLOCK select_publishing_extension_ids
+SELECT
+  course_instance_publishing_extension_id AS id
+FROM
+  course_instance_publishing_extension_enrollments
+WHERE
+  enrollment_id = $enrollment_id
+ORDER BY
+  course_instance_publishing_extension_id;
+
+-- BLOCK insert_assessment_access_control_rule
+INSERT INTO
+  assessment_access_control_rules (assessment_id, number, target_type, uuid)
+VALUES
+  ($assessment_id, $number, 'enrollment', $uuid)
+RETURNING
+  *;
+
+-- BLOCK insert_assessment_access_control_enrollment
+INSERT INTO
+  assessment_access_control_enrollments (assessment_access_control_rule_id, enrollment_id)
+VALUES
+  ($rule_id, $enrollment_id)
+ON CONFLICT (assessment_access_control_rule_id, enrollment_id) DO NOTHING;
+
+-- BLOCK select_assessment_access_control_rule_ids
+SELECT
+  assessment_access_control_rule_id AS id
+FROM
+  assessment_access_control_enrollments
+WHERE
+  enrollment_id = $enrollment_id
+ORDER BY
+  assessment_access_control_rule_id;
+
+-- BLOCK select_audit_event_by_id
+SELECT
+  *
+FROM
+  audit_events
+WHERE
+  id = $audit_event_id;
+
+-- BLOCK select_enrollment_reconciliation_audit_events
+SELECT
+  *
+FROM
+  audit_events
+WHERE
+  table_name = 'enrollments'
+  AND row_id = ANY ($enrollment_ids::bigint[])
+  AND context ->> 'reason' = 'identity_reconciliation'
+ORDER BY
+  id;
+
+-- BLOCK select_audit_event_sequence_value
+SELECT
+  last_value::text
+FROM
+  audit_events_id_seq;
+
+-- BLOCK lock_enrollment
+SELECT
+  id
+FROM
+  enrollments
+WHERE
+  id = $enrollment_id
+FOR UPDATE;
+
+-- BLOCK set_local_application_name
+SELECT
+  set_config('application_name', $application_name, true);
+
+-- BLOCK select_waiting_reconciliation_lock
+SELECT
+  1
+FROM
+  pg_stat_activity
+WHERE
+  pid <> pg_backend_pid()
+  AND application_name = $application_name
+  AND wait_event_type = 'Lock'
+  AND query LIKE '%lock_enrollments_by_id%'
+LIMIT
+  1;
