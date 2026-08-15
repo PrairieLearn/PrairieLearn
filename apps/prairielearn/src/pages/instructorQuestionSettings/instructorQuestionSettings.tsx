3a327dcfd91ae065f794cfd48ede8a4179899479
Seth Poulsen <poulsenseth@yahoo.com>
2026-08-15 03:34:30 +0000

    Show public link on question settings page when publicly shared (#15529)
    * public question link on settings page
    
    * Revert public link badge label customization
    
    * fix inaccurae wording on page

--- apps/prairielearn/src/pages/instructorQuestionSettings/instructorQuestionSettings.tsx
+++ apps/prairielearn/src/pages/instructorQuestionSettings/instructorQuestionSettings.tsx
@@ -27,6 +27,7 @@ import {
   StaffTagSchema,
   StaffTopicSchema,
 } from '../../lib/client/safe-db-types.js';
+import { getQuestionPreviewUrl } from '../../lib/client/url.js';
 import { copyQuestionBetweenCourses } from '../../lib/copy-content.js';
 import { EnumGradingMethodSchema } from '../../lib/db-types.js';
 import { getOriginalHash } from '../../lib/editorUtil.js';
@@ -655,6 +656,11 @@ router.get(
 
     const questionGHLink = courseRepoContentUrl(course, `questions/${question.qid}`);
 
+    const publicLink = new URL(
+      getQuestionPreviewUrl({ courseId: course.id, questionId: question.id, isPublic: true }),
+      host,
+    ).href;
+
     const qids = await sqldb.queryScalars(sql.qids, { course_id: course.id }, z.string());
 
     const assessmentsWithQuestion = await sqldb.queryRows(
@@ -720,6 +726,7 @@ router.get(
               courseInstance={courseInstance}
               csrfToken={__csrf_token}
               questionGHLink={questionGHLink}
+              publicLink={publicLink}
               questionTest={{ path: questionTestPath, csrfToken: questionTestCsrfToken }}
               questionTags={parsedQuestionTags}
               qids={qids}
