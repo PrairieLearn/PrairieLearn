56c02eea9b102d2f2db43a2ab106e1dd0d0a78b5
Jonatan Schroeder <jonatan@yorku.ca>
2026-08-18 10:40:33 -0400

    Keep "useless" concat for separate lines

--- apps/prairielearn/src/tests/exam.test.ts
+++ apps/prairielearn/src/tests/exam.test.ts
@@ -1428,7 +1428,9 @@ describe('Exam assessment', { timeout: 60_000 }, function () {
   describe('36. assessment instance score_perc uploads', function () {
     describe('prepare the CSV upload data', function () {
       it('should succeed', function () {
-        locals.csvData = 'uid,instance,score_perc\ndev@example.com,1,43.7\n';
+        // Concat is done for easier read of separate lines.
+        // eslint-disable-next-line unicorn/no-useless-concat
+        locals.csvData = 'uid,instance,score_perc\n' + 'dev@example.com,1,43.7\n';
       });
     });
     helperQuestion.uploadAssessmentInstanceScores(locals);
@@ -1484,7 +1486,9 @@ describe('Exam assessment', { timeout: 60_000 }, function () {
   describe('37. assessment instance points uploads', function () {
     describe('prepare the CSV upload data', function () {
       it('should succeed', function () {
-        locals.csvData = 'uid,instance,points\ndev@example.com,1,29.6\n';
+        // Concat is done for easier read of separate lines.
+        // eslint-disable-next-line unicorn/no-useless-concat
+        locals.csvData = 'uid,instance,points\n' + 'dev@example.com,1,29.6\n';
       });
     });
     helperQuestion.uploadAssessmentInstanceScores(locals);
