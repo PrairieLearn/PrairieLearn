d10413518f22e4ba9703ca95c4aa3e2df22ad447
skyler-oakeson <skyler.oakeson@gmail.com>
2026-09-01 12:04:16 -0600

    Ensure that the assessmet has been deleted when navigating to the URL. Added a flag that ensures assessment.deleted_at == null when routing to the public assessment

--- apps/prairielearn/src/lib/publicAssessment.ts
+++ apps/prairielearn/src/lib/publicAssessment.ts
@@ -13,7 +13,7 @@ export async function setPublicAssessmentLocals(req: Request, res: Response) {
 
   if (
     !assessment?.share_source_publicly ||
-    assessment.course_instance_id !== res.locals.course_instance.id
+    assessment.course_instance_id !== res.locals.course_instance.id || assessment.deleted_at != null
   ) {
     throw new error.HttpStatusError(404, 'Not Found');
   }
