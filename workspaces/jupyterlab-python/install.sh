126f74016cd1f87e2a0dccf562b8d42550900b8b
Jonatan Schroeder <jonatan@yorku.ca>
2026-09-02 15:42:23 -0400

    Use env instead of requirements.txt note

--- workspaces/jupyterlab-python/install.sh
+++ workspaces/jupyterlab-python/install.sh
@@ -7,7 +7,8 @@ apt-get update
 apt-get -y install graphviz graphviz-dev
 
 # Install all Python dependencies.
-pip3 install -r /requirements.txt
+# Disable binary wheels for pygraphviz to avoid issues with bundled Graphviz/font stack
+PIP_NO_BINARY="pygraphviz" pip3 install -r /requirements.txt
 
 # Clear various caches to minimize the final image size.
 apt-get clean && rm -rf /var/lib/apt/lists/*
