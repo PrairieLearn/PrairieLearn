e21d84827056008b3bd0fffc426a44fb67fd151e
Serena Caraco <15524864+SybelBlue@users.noreply.github.com>
2026-09-11 09:48:03 -0700

    Add imaginary unit display option to big operator input

--- apps/prairielearn/src/lib/element-schemas/elements/pl-big-operator-input.ts
+++ apps/prairielearn/src/lib/element-schemas/elements/pl-big-operator-input.ts
@@ -17,6 +17,7 @@ const plBigOperatorInputAttributesSchema = z
     'correct-answer': z.string().regex(correctAnswerPattern).optional(),
     'custom-functions': z.string().optional(),
     'grading-method': z.enum(['exact', 'component', 'equivalent', 'none']).optional(),
+    'imaginary-unit-for-display': z.enum(['i', 'j']).default('i').optional(),
     'limit-size': integerFormat().optional(),
     'operator-latex': z.string().optional(),
     'prefix-latex': z.string().optional(),
