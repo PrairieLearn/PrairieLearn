e58e47ac2c37bd8605936096df00ef211f562b76
Serena Caraco <15524864+SybelBlue@users.noreply.github.com>
2026-09-23 11:24:35 -0700

    Test disconnected free-draw solution conversion

--- apps/prairielearn/elements/pl-sketch/pl_sketch_test.py
+++ apps/prairielearn/elements/pl-sketch/pl_sketch_test.py
@@ -1,4 +1,6 @@
+import base64
 import importlib
+import json
 from typing import Any
 
 pl_sketch = importlib.import_module("pl-sketch")
@@ -42,3 +44,34 @@ def test_correct_free_draw_function_solution_receives_full_credit() -> None:
     pl_sketch.test(element_html, data)
 
     assert data["partial_scores"]["graph"]["score"] == 1
+
+
+def test_free_draw_coordinate_solution_preserves_disconnected_curves() -> None:
+    element_html = """
+        <pl-sketch answers-name="graph" x-range="-5,5" y-range="-5,5" width="400" height="400">
+            <pl-sketch-tool id="curves" type="free-draw"></pl-sketch-tool>
+            <pl-sketch-grade type="count" tool-id="curves" count="2"></pl-sketch-grade>
+            <pl-sketch-solution
+                tool-id="curves"
+                coordinates="(-4,-3),(-3,-1),(-2,2),(-1,4)"
+            ></pl-sketch-solution>
+            <pl-sketch-solution
+                tool-id="curves"
+                coordinates="(1,4),(2,2),(3,-1),(4,-3)"
+            ></pl-sketch-solution>
+        </pl-sketch>
+    """
+    data = _make_question_data()
+    pl_sketch.prepare(element_html, data)
+    solution_curves = data["params"]["graph"]["solution_state"]["curves"]
+    expected_gradeable = [
+        {"spline": [[point["x"], point["y"]] for point in curve]}
+        for curve in solution_curves
+    ]
+    data["test_type"] = "correct"
+
+    pl_sketch.test(element_html, data)
+
+    raw_submission = data["raw_submitted_answers"]["graph-sketchresponse-submission"]
+    submission = json.loads(base64.b64decode(raw_submission).decode("utf-8"))
+    assert submission["gradeable"]["curves"] == expected_gradeable
