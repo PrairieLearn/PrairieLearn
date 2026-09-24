0b632d053fdae666c4ec6288678411977a6deeee
austinbillings-pl <austin@prairielearn.com>
2026-09-24 19:44:13 -0400

    Add shared print renderer and question HTML namespacing

---
+++ packages/printing/src/index.ts
@@ -0,0 +1,7 @@
+export { namespaceQuestionHtmls } from './namespaceQuestionHtmls.js';
+export type { QuestionHtmlToNamespace } from './namespaceQuestionHtmls.js';
+export { PAPER_SIZES, createPdfOutput } from './pdfOutput.js';
+export type { PaperSize } from './pdfOutput.js';
+export { PrintRenderer, QuestionBlockSizeOverflowError } from './printRenderer.js';
+export type { PrintablePageOutput } from './printablePageOutput.js';
+export type { PrintRendererOptions, RenderPdfOptions, RenderPageOptions } from './printRenderer.js';
